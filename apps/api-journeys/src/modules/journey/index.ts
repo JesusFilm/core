766cf814377c434ab7df6ae049c665a807339578
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-08-09 14:13:34 +1200

    run generate before build and test

--- apps/api-journeys/src/modules/journey/index.ts
+++ apps/api-journeys/src/modules/journey/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '.prisma/client'
+import { PrismaClient } from '.prisma/api-journeys-client'
 import { createModule, gql } from 'graphql-modules'
 import { JourneyModule } from './__generated__/types'
 
