c8250ac34c2bcadd3cc9664ec3b7cddd64926a9b
Christopher Morley <chris@morleyconsulting.net>
2021-08-23 14:54:36 -0700

    moved buildModuleSubgraphSchema into lib/shared/util-graphql

--- apps/api-journeys/src/modules/application/index.ts
+++ apps/api-journeys/src/modules/application/index.ts
@@ -1,5 +1,5 @@
 import { createApplication } from 'graphql-modules'
-import { buildModuleSubgraphSchema } from './buildModuleSubgraphSchema'
+import { buildModuleSubgraphSchema } from '../../lib/shared/util-graphql/buildModuleSubgraphSchema'
 import journey from '../journey'
 
 export default createApplication({
