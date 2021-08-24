97147206019aaad5f5047f9e33819085f18fd179
Gavin Walsh <walsh.gavin@gmail.com>
2021-08-24 16:22:15 -0400

    naming

--- apps/journeys/src/Transformer/Transformer.tsx
+++ apps/journeys/src/Transformer/Transformer.tsx
@@ -5,31 +5,31 @@ type Block = {
 }
 
 export function Transformer(blocks: Block[]): Block[] {
-    const store = new Map(); // stores data indexed by its id
-    const rels = new Map(); // stores array of children associated with id
-    const roots: string[] = []; // stores root nodes
+    const setup = new Map();
+    const relatedBlocks = new Map();
+    const rootBlocks: string[] = [];
     blocks.forEach(block => {
-        store.set(block.id, block);
-        !rels.get(block.id) ? rels.set(block.id, []) : undefined; // noOp.;
+        setup.set(block.id, block);
+        !relatedBlocks.get(block.id) ? relatedBlocks.set(block.id, []) : undefined; // noOp.;
         if (!block.parentId) {
-            roots.push(block.id)
+            rootBlocks.push(block.id)
             return;
         }
-        const parent = rels.get(block.parentId) || [];
+        const parent = relatedBlocks.get(block.parentId) || [];
         parent.push(block.id);
-        rels.set(block.parentId, parent);
+        relatedBlocks.set(block.parentId, parent);
     });
 
     function build(id: string): Block {
-        const blocks = store.get(id);
-        const children: string[] = rels.get(id);
+        const blocks = setup.get(id);
+        const children: string[] = relatedBlocks.get(id);
         if (children.length === 0) {
             return {...blocks}
         }
-        return {...blocks, children: children.map(c => build(c)) };
+        return {...blocks, children: children.map(child => build(child)) };
     }
 
-    return roots.map(r => build(r));
+    return rootBlocks.map(id => build(id));
 }
 
 export default Transformer;
