d81236cff774d78a008519adf9d4dbead151fd68
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-08-30 15:03:05 +1200

    Remove redundant wrapper

--- apps/journeys/src/app/app.spec.tsx
+++ apps/journeys/src/app/app.spec.tsx
@@ -1,26 +1,16 @@
 import { render } from '@testing-library/react'
 
-import { BrowserRouter } from 'react-router-dom'
-
 import App from './app'
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    )
+    const { baseElement } = render(<App />)
 
     expect(baseElement).toBeTruthy()
   })
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    )
+    const { getByText } = render(<App />)
 
     expect(getByText('Welcome to journeys!')).toBeTruthy()
   })
