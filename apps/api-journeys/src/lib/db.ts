d3dab2bc69803959fe37c8e7477c09f6bd108b9b
Spencer Oberstadt <soberstadt@gmail.com>
2021-08-31 17:14:43 -0400

    reuse gloabl prisma client in development

--- apps/api-journeys/src/lib/db.ts
+++ apps/api-journeys/src/lib/db.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '.prisma/api-journeys-client'
 
-const db = new PrismaClient()
+let db
+
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient()
+} else {
+  if (global.db != null) {
+    global.db = new PrismaClient()
+  }
+
+  db = global.db
+}
 
 export default db
