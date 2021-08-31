c0b02989ca0cf0e88ad2500c493f9010389a6523
John Geronimo <johnageronimo1@gmail.com>
2021-08-31 16:02:13 +1200

    Remove divs

--- apps/journeys/src/components/BlockRenderer/BlockRenderer.tsx
+++ apps/journeys/src/components/BlockRenderer/BlockRenderer.tsx
@@ -1,9 +1,9 @@
-import { RadioOption, RadioQuestion, Video } from '../blocks';
-import { BlockType } from '../../types';
-import { ReactElement } from 'react';
+import { RadioOption, RadioQuestion, Video } from '../blocks'
+import { BlockType } from '../../types'
+import { ReactElement } from 'react'
 
-export function BlockRenderer(block: BlockType): ReactElement | null {
-  return BlockSwitcher(block, 1000);
+export function BlockRenderer (block: BlockType): ReactElement | null {
+  return BlockSwitcher(block, 1000)
 }
 
 export const BlockSwitcher = (
@@ -12,14 +12,14 @@ export const BlockSwitcher = (
 ): ReactElement | null => {
   switch (block.__typename) {
     case 'Video':
-      return <Video {...block} key={key} />;
+      return <Video {...block} key={key} />
     case 'RadioOption':
-      return <RadioOption {...block} key={key} />;
+      return <RadioOption {...block} key={key} />
     case 'RadioQuestion':
-      return <RadioQuestion {...block} key={key} />;
+      return <RadioQuestion {...block} key={key} />
     default:
-      return null;
+      return null
   }
-};
+}
 
-export default BlockRenderer;
+export default BlockRenderer
