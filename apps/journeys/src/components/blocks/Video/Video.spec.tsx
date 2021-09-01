21a00778d6ef2592bc6d750b56f4820784f60730
John Geronimo <johnageronimo1@gmail.com>
2021-09-01 15:44:36 +1200

    Remove Vime

--- apps/journeys/src/components/blocks/Video/Video.spec.tsx
+++ apps/journeys/src/components/blocks/Video/Video.spec.tsx
@@ -1,13 +1,9 @@
 import { render } from '@testing-library/react'
-import { Video } from './Video'
-import transformer from '../../../libs/transformer'
-import { videos } from '../../../data'
-
-const transformed1 = transformer(videos)
+import { Video } from '.'
 
 describe('BlockRendererVideo', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Video {...transformed1[0]} />)
+    const { baseElement } = render(<Video />)
 
     expect(baseElement).toBeTruthy()
   })
--- apps/journeys/src/components/blocks/Video/Video.tsx
+++ apps/journeys/src/components/blocks/Video/Video.tsx
@@ -1,48 +1,5 @@
-import { ConductorContext } from '../../Conductor'
-import { VideoType, BlockType, GoTo } from '../../../types'
-import { BlockSwitcher } from '../../BlockRenderer'
-import '@vime/core/themes/default.css'
-import { useEffect, useRef, ReactElement } from 'react'
-import { Player, Video as VimeVideo, Ui, usePlayerContext, Controls, DefaultControls } from '@vime/react'
+import React, { ReactElement } from 'react'
 
-export const Video = ({
-  src,
-  children,
-  action
-}: VideoType): ReactElement => {
-  const player = useRef<HTMLVmPlayerElement>(null)
-
-  const [currentTime] = usePlayerContext(player, 'currentTime', 0)
-
-  useEffect(() => {
-    console.log(currentTime)
-  }, [currentTime])
-
-  return (
-    <ConductorContext.Consumer>
-      {({ goTo }: GoTo) => {
-        return (
-        <div>
-          <Player playsinline ref={player} autoplay muted onVmPlaybackEnded={() => goTo(action)}>
-            <VimeVideo poster="https://media.vimejs.com/poster.png">
-              <source
-                data-src={src}
-              />
-            </VimeVideo>
-            <Ui>
-              <DefaultControls activeDuration={100000} />
-              <Controls fullWidth pin="center" hidden={false} activeDuration={100000}>
-                  {
-                    (children != null) ? children.map((block: BlockType, index: number) => BlockSwitcher(block, index)) : undefined
-                  }
-              </Controls>
-            </Ui>
-          </Player>
-        </div>
-        )
-      }}
-    </ConductorContext.Consumer>
-  )
+export const Video = (): ReactElement => {
+  return <div>Render Video Here</div>
 }
-
-export default Video
