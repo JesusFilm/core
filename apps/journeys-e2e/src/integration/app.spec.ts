5d432ba87779a541b73cee1668ad52daf21d01c5
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-01 16:30:22 +1200

    fix cypress test

--- apps/journeys-e2e/src/integration/app.spec.ts
+++ apps/journeys-e2e/src/integration/app.spec.ts
@@ -4,10 +4,6 @@ describe('journeys', () => {
   beforeEach(() => cy.visit('/'))
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword')
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to journeys!')
+    getGreeting().contains('Block renderer and conductor samples!')
   })
 })
