3ae4cf27b8de0a24551db92027f0b0dfffe06e0c
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-01 15:30:53 +1200

    reset journeys-e2e

--- apps/journeys-e2e/src/integration/app.spec.ts
+++ apps/journeys-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('bob', () => {
+describe('journeys', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('bob', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to bob!');
+    getGreeting().contains('Welcome to journeys!');
   });
 });
