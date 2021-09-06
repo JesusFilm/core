06f4394e136af28c8a2d4b54e44abb67f7b0a4b1
John Geronimo <johnageronimo1@gmail.com>
2021-09-07 10:27:46 +1200

    Update test props

--- apps/journeys/src/components/blocks/RadioOption/RadioOption.spec.tsx
+++ apps/journeys/src/components/blocks/RadioOption/RadioOption.spec.tsx
@@ -11,16 +11,18 @@ const block: RadioOptionType = {
 describe('RadioOption', () => {
   it('should render option props', () => {
     const { getByText } = renderWithStore(
-      <RadioOption {...block} />
+      <RadioOption {...block} key="question1" selectedId="" />
     )
     expect(getByText(block.label)).toBeInTheDocument()
   })
 
   it('should handle click', () => {
     const { getByTestId, getByRole } = renderWithStore(
-      <RadioOption {...block} />
+      <RadioOption {...block} key="question1" selectedId="" />
     )
-    expect(getByTestId('RadioOptionRadioButtonUncheckedIcon')).toBeInTheDocument()
+    expect(
+      getByTestId('RadioOptionRadioButtonUncheckedIcon')
+    ).toBeInTheDocument()
     fireEvent.click(getByRole('button'))
     expect(getByTestId('RadioOptionCheckCircleIcon')).toBeInTheDocument()
   })
--- apps/journeys/src/components/blocks/RadioQuestion/RadioQuestions.stories.tsx
+++ apps/journeys/src/components/blocks/RadioQuestion/RadioQuestions.stories.tsx
@@ -1,13 +1,13 @@
-import { Story, Meta } from '@storybook/react';
-import { RadioQuestionType } from '../../../types';
-import { RadioQuestion } from './RadioQuestion';
-import * as RadioOption from '../RadioOption/RadioOption.stories';
+import { Story, Meta } from '@storybook/react'
+import { RadioQuestionType } from '../../../types'
+import { RadioQuestion } from './RadioQuestion'
+import * as RadioOption from '../RadioOption/RadioOption.stories'
 
-import { Provider } from 'react-redux';
-import { configureStoreWithState, RootState } from '../../../libs/store/store';
-import { PreloadedState } from 'redux';
+import { Provider } from 'react-redux'
+import { configureStoreWithState, RootState } from '../../../libs/store/store'
+import { PreloadedState } from 'redux'
 
-let preloadedState: PreloadedState<RootState>;
+let preloadedState: PreloadedState<RootState>
 
 const Demo = {
   component: RadioQuestion,
@@ -17,15 +17,15 @@ const Demo = {
       <Provider store={configureStoreWithState(preloadedState)}>
         <Story />
       </Provider>
-    ),
-  ],
-};
+    )
+  ]
+}
 
 const Template: Story<RadioQuestionType> = ({ ...props }) => (
   <RadioQuestion {...props} />
-);
+)
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
   id: 'id',
   label: 'Label',
@@ -33,14 +33,14 @@ Primary.args = {
   children: [
     RadioOption.Primary.args,
     RadioOption.Primary.args,
-    RadioOption.Primary.args,
+    RadioOption.Primary.args
   ],
   parent: {
-    id: 'parentId',
-  },
-};
+    id: 'parentId'
+  }
+}
 
-export const Secondary = Template.bind({});
+export const Secondary = Template.bind({})
 Secondary.args = {
   id: 'MoreQuestions',
   label: 'How can we help you know more about Jesus?',
@@ -48,8 +48,8 @@ Secondary.args = {
     'What do you think would be the next step to help you grow in your relationship with Jesus?',
   children: [RadioOption.DefaultLabel.args, RadioOption.LongLabel.args],
   parent: {
-    id: 'Step1',
-  },
-};
+    id: 'Step1'
+  }
+}
 
-export default Demo as Meta;
+export default Demo as Meta
