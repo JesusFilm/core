9dfeb48a81896694eb062b2874f03d8c0536981e
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2021-09-07 18:24:33 +1200

    Configure Storybook (#16)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>
    Co-authored-by: Gavin Walsh <walsh.gavin@gmail.com>
    Co-authored-by: John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
    Co-authored-by: John Geronimo <johnageronimo1@gmail.com>

--- apps/journeys/src/components/blocks/RadioQuestion/RadioQuestions.stories.tsx
+++ apps/journeys/src/components/blocks/RadioQuestion/RadioQuestions.stories.tsx
@@ -2,23 +2,12 @@ import { Story, Meta } from '@storybook/react'
 import { RadioQuestionType } from '../../../types'
 import { RadioQuestion } from './RadioQuestion'
 import * as RadioOption from '../RadioOption/RadioOption.stories'
-
-import { Provider } from 'react-redux'
-import { configureStoreWithState, RootState } from '../../../libs/store/store'
-import { PreloadedState } from 'redux'
-
-let preloadedState: PreloadedState<RootState>
+import { journeysConfig } from '../../../libs/storybook/decorators'
 
 const Demo = {
+  ...journeysConfig,
   component: RadioQuestion,
-  title: 'Journeys/Blocks/RadioQuestion',
-  decorators: [
-    Story => (
-      <Provider store={configureStoreWithState(preloadedState)}>
-        <Story/>
-      </Provider>
-    )
-  ]
+  title: 'Journeys/Blocks/RadioQuestion'
 }
 
 const Template: Story<RadioQuestionType> = ({ ...props }) => <RadioQuestion {...props} />
