fa0266bb2be8e2586cf91bc23ecebc28838bab52
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-09 10:17:23 +1200

    use NEXT_PUBLIC_GATEWAY_URL

--- apps/journeys/src/libs/client/client.ts
+++ apps/journeys/src/libs/client/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: process.env.NEXT_PUBLIC_GATEWAY_URL,
   cache: new InMemoryCache()
 })
 
