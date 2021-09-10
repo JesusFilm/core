f9e847c8f2b0952664f78dfe9fb6d2752b59261b
John Geronimo <johnageronimo1@gmail.com>
2021-09-10 13:56:24 +1200

    Set initial button types

--- libs/shared/ui/src/components/Button/Button.tsx
+++ libs/shared/ui/src/components/Button/Button.tsx
@@ -1,8 +1,19 @@
 import { ReactElement } from 'react'
 import { Button as MuiButton } from '@mui/material'
 
-export interface ButtonProps {
+interface ButtonType {
   label: string
+  variant?: 'contained' | 'outlined' | 'text'
+  color?: 'primary' | 'secondary'
+  size?: 'large' | 'medium' | 'small'
+  // Icon type string for now
+  startIcon?: 'IconType'
+  endIcon?: 'IconType'
+}
+
+export interface ButtonProps extends ButtonType {
+  loading?: boolean
+  disabled?: boolean
 }
 
 export function Button (props: ButtonProps): ReactElement {
