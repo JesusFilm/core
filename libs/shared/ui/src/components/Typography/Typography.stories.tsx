30f7749b3f236fd84048a94494c51c16fa08a48a
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-10 12:57:55 +1200

    Basic stories

--- libs/shared/ui/src/components/Typography/Typography.stories.tsx
+++ libs/shared/ui/src/components/Typography/Typography.stories.tsx
@@ -1,16 +1,73 @@
 import { Story, Meta } from '@storybook/react'
-import { Typography, TypographyProps } from './Typography'
+import { Typography, TypographyAlignments, TypographyColors, TypographyProps, TypographyVariants } from './Typography'
 
 const TypographyDemo = {
   component: Typography,
   title: 'shared-ui/Typography'
 }
 
-const Template: Story<TypographyProps> = (args) => <Typography {...args} />
+interface TypographyStoryProps extends TypographyProps {
+  variants: string[]
+}
 
-export const Primary = Template.bind({})
-Primary.args = {
-  content: 'hello world'
+const VariantTemplate: Story<TypographyStoryProps> = (args) => (
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column'
+  }}>
+    {args.variants.map((variant) => (
+      <Typography
+        {...args}
+        variant={variant as TypographyVariants}
+        content={`${variant}`}
+      />
+    ))}
+  </div>
+)
+
+export const Variants = VariantTemplate.bind({})
+Variants.args = {
+  variants: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'subtitle1', 'subtitle2', 'body1', 'body2', 'button', 'caption', 'overline']
+}
+
+const ColorTemplate: Story<TypographyStoryProps> = (args) => (
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column'
+  }}>
+    {args.variants.map((variant) => (
+      <Typography
+        {...args}
+        color={variant as TypographyColors}
+        content={`${variant}`}
+      />
+    ))}
+  </div>
+)
+
+export const Colors = ColorTemplate.bind({})
+Colors.args = {
+  variants: ['primary', 'secondary', 'error', 'warning', 'info', 'success']
+}
+
+const AlignmentTemplate: Story<TypographyStoryProps> = (args) => (
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column'
+  }}>
+     {args.variants.map((variant) => (
+       <Typography
+         {...args}
+         align={variant as TypographyAlignments}
+         content={`${variant}`}
+       />
+     ))}
+  </div>
+)
+
+export const Alignment = AlignmentTemplate.bind({})
+Alignment.args = {
+  variants: ['left', 'center', 'right']
 }
 
 export default TypographyDemo as Meta
--- libs/shared/ui/src/components/Typography/Typography.tsx
+++ libs/shared/ui/src/components/Typography/Typography.tsx
@@ -1,9 +1,17 @@
 import { ReactElement } from 'react'
 import { Typography as MuiTypography } from '@mui/material'
 
-/* eslint-disable-next-line */
+// Mocked out types for now, get from generated schema?
+export type TypographyVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle1' | 'subtitle2' | 'body1' | 'body2' | 'button' | 'caption' | 'overline'
+
+export type TypographyColors = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success'
+
+export type TypographyAlignments = 'left' | 'center' | 'right'
 export interface TypographyProps {
   content: string
+  variant?: TypographyVariants
+  color?: TypographyColors
+  align?: TypographyAlignments
 }
 
 export function Typography (props: TypographyProps): ReactElement {
