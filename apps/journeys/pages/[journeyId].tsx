2dea52dfad1e498c4a9d377f6bd1fc7d5772c805
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-14 12:44:49 +1200

    reset journeys folder

--- apps/journeys/pages/[journeyId].tsx
+++ apps/journeys/pages/[journeyId].tsx
@@ -9,79 +9,75 @@ import client from '../src/libs/client'
 import { gql } from '@apollo/client'
 import { GetJourney, GetJourney_journey as Journey } from '../__generated__/GetJourney'
 
-// interface JourneyPageProps {
-//   journey: Journey
-// }
+interface JourneyPageProps {
+  journey: Journey
+}
 
-// function JourneyPage ({ journey }: JourneyPageProps): ReactElement {
-//   return (
-//     <Container>
-//       <Provider store={store}>
-//         {(journey.blocks != null) && <Conductor blocks={transformer(journey.blocks)} />}
-//       </Provider>
-//     </Container>
-//   )
-// }
+function JourneyPage ({ journey }: JourneyPageProps): ReactElement {
+  return (
+    <Container>
+      <Provider store={store}>
+        {(journey.blocks != null) && <Conductor blocks={transformer(journey.blocks)} />}
+      </Provider>
+    </Container>
+  )
+}
 
-// export const getServerSideProps: GetServerSideProps<JourneyPageProps> = async (context) => {
-//   const { data } = await client.query<GetJourney>({
-//     query: gql`
-//       query GetJourney($id: ID!) {
-//         journey(id: $id) {
-//           id
-//           blocks {
-//             id
-//             parentBlockId
-//             ... on VideoBlock {
-//               src
-//               title
-//               provider
-//             }
-//             ... on RadioQuestionBlock {
-//               label
-//               description
-//               variant
-//             }
-//             ... on RadioOptionBlock {
-//               label
-//               action {
-//                 __typename
-//                 gtmEventName
-//                 ... on NavigateAction {
-//                   blockId
-//                 }
-//                 ... on NavigateToJourneyAction {
-//                   journeyId
-//                 }
-//                 ... on LinkAction {
-//                   url
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `,
-//     variables: {
-//       id: context.query.journeyId
-//     }
-//   })
+export const getServerSideProps: GetServerSideProps<JourneyPageProps> = async (context) => {
+  const { data } = await client.query<GetJourney>({
+    query: gql`
+      query GetJourney($id: ID!) {
+        journey(id: $id) {
+          id
+          blocks {
+            id
+            parentBlockId
+            ... on VideoBlock {
+              src
+              title
+              provider
+            }
+            ... on RadioQuestionBlock {
+              label
+              description
+              variant
+            }
+            ... on RadioOptionBlock {
+              label
+              action {
+                __typename
+                gtmEventName
+                ... on NavigateAction {
+                  blockId
+                }
+                ... on NavigateToJourneyAction {
+                  journeyId
+                }
+                ... on LinkAction {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      id: context.query.journeyId
+    }
+  })
 
-//   if (data.journey === null) {
-//     return {
-//       notFound: true
-//     }
-//   } else {
-//     return {
-//       props: {
-//         journey: data.journey
-//       }
-//     }
-//   }
-// }
-
-function JourneyPage () {
-  return <>HELLO</>
+  if (data.journey === null) {
+    return {
+      notFound: true
+    }
+  } else {
+    return {
+      props: {
+        journey: data.journey
+      }
+    }
+  }
 }
 
 export default JourneyPage
