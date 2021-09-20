751bf3cbe9f25aeb0d714ea64c8499f7d4a0203e
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-09-20 06:21:14 +0000

    add createApolloClient to handle token

--- apps/journeys/src/libs/client/client.ts
+++ apps/journeys/src/libs/client/client.ts
@@ -1,5 +1,33 @@
-import { ApolloClient } from '@apollo/client'
+import {
+  ApolloClient,
+  createHttpLink,
+  NormalizedCacheObject
+} from '@apollo/client'
 import { cache } from './cache'
+
+import { setContext } from '@apollo/client/link/context'
+
+export function createApolloClient(
+  token?: string
+): ApolloClient<NormalizedCacheObject> {
+  const httpLink = createHttpLink({
+    uri: process.env.NEXT_PUBLIC_GATEWAY_URL
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ?? ''
+      }
+    }
+  })
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache
+  })
+}
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GATEWAY_URL,
