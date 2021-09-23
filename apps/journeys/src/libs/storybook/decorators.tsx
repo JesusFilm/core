0c3471638df17730f00ce00bfd1ea9ec5e8c9782
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-23 06:20:28 +0000

    Revert linting fix in journeys decorator

--- apps/journeys/src/libs/storybook/decorators.tsx
+++ apps/journeys/src/libs/storybook/decorators.tsx
@@ -1,10 +1,9 @@
-import { parameters as rootParameters } from '../../../../../.storybook/preview'
+import { parameters as rootParameters } from "../../../../../.storybook/preview";
 
 // Must set parameters at component level for shared-storybook stories to work
 export const journeysConfig = {
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  decorators: [(Story: () => unknown) => Story()],
+  decorators: [(Story) => <Story />],
   parameters: {
-    ...rootParameters
-  }
-}
+    ...rootParameters,
+  },
+};
