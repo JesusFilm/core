73df2ac63ddbded347fdf3dd3b8d815a94cb4271
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-24 04:52:48 +0000

    Ran codegen

--- libs/shared/ui/__generated__/globalTypes.ts
+++ libs/shared/ui/__generated__/globalTypes.ts
@@ -7,6 +7,54 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum ButtonColor {
+  error = "error",
+  inherit = "inherit",
+  primary = "primary",
+  secondary = "secondary",
+}
+
+export enum ButtonSize {
+  large = "large",
+  medium = "medium",
+  small = "small",
+}
+
+export enum ButtonVariant {
+  contained = "contained",
+  text = "text",
+}
+
+export enum IconColor {
+  action = "action",
+  disabled = "disabled",
+  error = "error",
+  inherit = "inherit",
+  primary = "primary",
+  secondary = "secondary",
+}
+
+export enum IconName {
+  ArrowForward = "ArrowForward",
+  ChatBubbleOutline = "ChatBubbleOutline",
+  CheckCircle = "CheckCircle",
+  FormatQuote = "FormatQuote",
+  LiveTv = "LiveTv",
+  LockOpen = "LockOpen",
+  MenuBook = "MenuBook",
+  PlayArrow = "PlayArrow",
+  RadioButtonUnchecked = "RadioButtonUnchecked",
+  Translate = "Translate",
+}
+
+export enum IconSize {
+  inherit = "inherit",
+  lg = "lg",
+  md = "md",
+  sm = "sm",
+  xl = "xl",
+}
+
 export enum TypographyAlign {
   center = "center",
   left = "left",
