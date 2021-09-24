9fb2e51a7f6a9c77278bb9c286e3a806fcd21c1c
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-24 00:14:45 +0000

    Put back generated file

---
+++ apps/journeys/__generated__/GetJourneys.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetJourneys
+// ====================================================
+
+export interface GetJourneys_journeys {
+  __typename: "Journey";
+  id: string;
+  title: string;
+}
+
+export interface GetJourneys {
+  journeys: GetJourneys_journeys[];
+}
