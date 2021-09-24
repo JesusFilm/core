d809bffdbcfeaa9f3b17912a0837e5e7b949cc29
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-24 02:37:31 +0000

    Fix errors

--- apps/journeys/src/components/JourneysThemeProvider/JourneysThemeProvider.spec.tsx
+++ apps/journeys/src/components/JourneysThemeProvider/JourneysThemeProvider.spec.tsx
@@ -1,10 +1,11 @@
 import { render } from '@testing-library/react'
+import { ThemeName } from '../../../__generated__/globalTypes'
 import { JourneysThemeProvider } from '.'
 
 describe('journeys theme provider', () => {
   it('should render the component', () => {
     const { getByText } = render(
-      <JourneysThemeProvider>
+      <JourneysThemeProvider theme={'default' as ThemeName}>
         Hello from JourneysThemeProvider
       </JourneysThemeProvider>
     )
