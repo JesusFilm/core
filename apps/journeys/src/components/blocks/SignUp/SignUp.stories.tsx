2db66ae07220290f2d3bf0dce64976bdccbf8187
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-30 03:43:33 +0000

    SignUp is just the form

--- apps/journeys/src/components/blocks/SignUp/SignUp.stories.tsx
+++ apps/journeys/src/components/blocks/SignUp/SignUp.stories.tsx
@@ -1,12 +1,22 @@
+import { Typography } from '@mui/material'
 import { Story, Meta } from '@storybook/react'
+import { journeysConfig } from '../../../libs/storybook/decorators'
 import SignUp, { SignUpProps } from './SignUp'
 
 const Demo = {
+  ...journeysConfig,
   component: SignUp,
   title: 'Journeys/Blocks/SignUp'
 }
 
-const Template: Story<SignUpProps> = ({ ...props }) => <SignUp {...props} />
+const Template: Story<SignUpProps> = ({ ...props }) => (
+  <>
+    <div style={{ marginBottom: '32px' }}>
+      <Typography variant="h1">Sign up</Typography>
+    </div>
+    <SignUp {...props} />
+  </>
+)
 
 // TODO: Awaiting on final designs
 export const Default = Template.bind({})
