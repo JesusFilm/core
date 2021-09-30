291c54d1d0eca379e8f1c37b3828fb1f8221c13c
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-09-30 03:44:09 +0000

    Update TextField styling

--- libs/shared/ui/src/libs/themes/base/tokens/colors.ts
+++ libs/shared/ui/src/libs/themes/base/tokens/colors.ts
@@ -1,33 +1,34 @@
-import { ThemeOptions } from '@mui/material/styles'
+import { ThemeOptions, PaletteColorOptions } from '@mui/material/styles'
+
+const primary: PaletteColorOptions = {
+  light: '#6D6F81',
+  main: '#30313D',
+  dark: '#26262E',
+  contrastText: '#FFFFFF'
+}
+
+const secondary: PaletteColorOptions = {
+  light: '#FEFEFE',
+  main: '#BDBFCF',
+  dark: '#AAACBB',
+  contrastText: '#26262E'
+}
 
 export const baseColorsLight: Required<Pick<ThemeOptions, 'palette'>> = {
   palette: {
     mode: 'light',
-    // DEFAULT CONTAINER COLORS
     background: { default: '#FEFEFE' },
-    primary: {
-      light: '#6D6F81',
-      main: '#30313D',
-      dark: '#26262E',
-      contrastText: '#FFFFFF'
-    },
-    // OVERRIDE COLORS
-    secondary: {
-      light: '#FEFEFE',
-      main: '#BDBFCF',
-      dark: '#AAACBB',
-      contrastText: '#26262E'
-    },
+    primary,
+    secondary,
     error: {
       light: '#FC624E',
       main: '#EE4C37',
       dark: '#C52713',
       contrastText: '#FFFFFF'
     },
-    // BACKGROUND TEXT COLORS
     text: {
-      primary: '#26262E',
-      secondary: '#FFFFFF'
+      primary: primary.dark,
+      secondary: primary.main
     }
   }
 }
@@ -37,11 +38,11 @@ export const baseColorsDark: Required<Pick<ThemeOptions, 'palette'>> = {
     ...baseColorsLight.palette,
     mode: 'dark',
     background: { default: '#26262E' },
-    primary: baseColorsLight.palette.secondary,
-    secondary: baseColorsLight.palette.primary,
+    primary: secondary,
+    secondary: primary,
     text: {
-      primary: baseColorsLight.palette.text?.secondary,
-      secondary: baseColorsLight.palette.text?.primary
+      primary: secondary.light,
+      secondary: secondary.main
     }
   }
 }
--- libs/shared/ui/src/libs/themes/base/tokens/components.ts
+++ libs/shared/ui/src/libs/themes/base/tokens/components.ts
@@ -17,7 +17,39 @@ export const baseComponents: Pick<ThemeOptions, 'components'> = {
           }
         }
       ]
-    }
+    },
     // TODO: Add Button component override
+    MuiOutlinedInput: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          backgroundColor: 'rgba(173, 173, 173, 0.3)',
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0
+        },
+        notchedOutline: {
+          borderLeft: 'none',
+          borderTop: 'none',
+          borderRight: 'none',
+          borderWidth: 0,
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0
+        },
+        input: {
+          // color: ,
+          transform: `translate(0px, 6px) scale(1)`,
+          transition: `color 200ms cubic-bezier(0.0,0,0.2,1) 0ms,transform 200ms cubic-bezier(0.0,0,0.2,1) 0ms,max-width 200ms cubic-bezier(0.0,0,0.2,1) 0ms`
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        outlined: {
+          '&.MuiInputLabel-shrink': {
+            transform: `translate(14px, 5px) scale(0.75)`
+          }
+        }
+      }
+    }
   }
 }
