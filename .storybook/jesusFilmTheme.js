fa8e17f415dc69ab8f4aaa4cc75df6499eb5981a
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-10-04 22:45:56 +0000

    Add JesusFilm logo to storybook

---
+++ .storybook/jesusFilmTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Jesus Film UI',
+  brandUrl: 'https://www.jesusfilm.org/',
+  brandImage: 'https://www.jesusfilm.org/content/dam/jesusfilm/jfp-logo-red.svg'
+})
