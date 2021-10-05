227a416620f9ce2e7b454b7765449c9ab20101e1
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-10-05 03:20:09 +0000

    Add logo locally

--- .storybook/jesusFilmTheme.js
+++ .storybook/jesusFilmTheme.js
@@ -1,8 +1,9 @@
 import { create } from '@storybook/theming'
+import logo from './jfp-logo-red.svg'
 
 export default create({
   base: 'light',
   brandTitle: 'Jesus Film UI',
   brandUrl: 'https://www.jesusfilm.org/',
-  brandImage: 'https://www.jesusfilm.org/content/dam/jesusfilm/jfp-logo-red.svg'
+  brandImage: logo
 })
