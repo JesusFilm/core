ca69ef88e4349c58cbb5397ae92fd0c158c7fa08
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-10-10 23:53:33 +0000

    add transform tests

---
+++ apps/api-journeys/src/modules/response/transformResponse/transformResponse.spec.ts
@@ -0,0 +1,42 @@
+import { transformResponse } from '.'
+import { v4 as uuidv4 } from 'uuid'
+import { Response } from '.prisma/api-journeys-client'
+
+describe('transformResponse', () => {
+  it('returns transformed database response', () => {
+    const response: Response = {
+      id: uuidv4(),
+      type: 'SignUpResponse',
+      userId: uuidv4(),
+      blockId: uuidv4(),
+      extraAttrs: {
+        hello: 'world'
+      },
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    const transformedResponse = {
+      ...response,
+      hello: 'world',
+      __typename: 'SignUpResponse'
+    }
+    expect(transformResponse(response)).toEqual(transformedResponse)
+  })
+
+  it('handles extraAttrs not being an object', () => {
+    const response: Response = {
+      id: uuidv4(),
+      type: 'SignUpResponse',
+      userId: uuidv4(),
+      blockId: uuidv4(),
+      extraAttrs: 'hello',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    const transformedResponse = {
+      ...response,
+      __typename: 'SignUpResponse'
+    }
+    expect(transformResponse(response)).toEqual(transformedResponse)
+  })
+})
