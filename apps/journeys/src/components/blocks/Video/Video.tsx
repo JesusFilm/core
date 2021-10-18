f1ac6e7ac22b1b79a3f5e87774b0e4267f027dbf
John Geronimo <johnageronimo1@gmail.com>
2021-10-18 00:13:11 +0000

    Working on implementing mediaComponentId and languageId in URL

--- apps/journeys/src/components/blocks/Video/Video.tsx
+++ apps/journeys/src/components/blocks/Video/Video.tsx
@@ -28,6 +28,8 @@ interface VideoProps extends TreeBlock<VideoBlock> {
 export function Video({ id: blockId, mediaComponentId, languageId, autoplay, uuid = uuidv4 }: VideoProps): ReactElement {
   const videoNode = useRef<HTMLVideoElement>(null)
   const [videoResponseCreate] = useMutation<VideoResponseCreate>(VIDEO_RESPONSE_CREATE)
+  const url = `https://arc.gt/hls/${mediaComponentId}/${languageId}`
+  const [videoUrl, setVideoUrl] = useState<string | undefined>()
 
   const player = useRef<videojs.Player>()
   const [isReady, setIsReady] = useState<boolean | undefined>()
@@ -53,68 +55,75 @@ export function Video({ id: blockId, mediaComponentId, languageId, autoplay, uui
     })
   }, [blockId, uuid, videoResponseCreate])
 
-  useEffect(() => {
-    const initialOptions: videojs.PlayerOptions = {
-      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-      autoplay: autoplay ? 'muted' : false,
-      controls: true,
-      userActions: {
-        hotkeys: true,
-        doubleClick: true
-      },
-      controlBar: {
-        playToggle: true,
-        captionsButton: true,
-        chaptersButton: true,
-        subtitlesButton: true,
-        remainingTimeDisplay: true,
-        progressControl: {
-          seekBar: true
-        },
-        fullscreenToggle: true,
-        playbackRateMenuButton: true,
-        volumePanel: {
-          inline: true
-        }
-      },
-      sources: [
-        {
-          // src: `https://arc.gt/hls/${mediaComponentId}/${languageId}`
-          src: `https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/index.m3u8`
-        }
-      ],
-      fluid: true,
-      responsive: true,
-      playbackRates: [0.5, 1, 1.5, 2]
-    }
+  const validate = async (url: string): Promise<void> => {
+    return await fetch(url).then((response) => setVideoUrl(response.url))
+  }
 
-    if (videoNode.current != null) {
-      player.current = videojs(videoNode.current, {
-        ...initialOptions
-      })
-      player.current.on('ready', () => {
-        setIsReady(true)
-      })
-      player.current.on('playing', () => {
-        void handleVideoState(VideoResponseStateEnum.PLAYING)
-      })
-      player.current.on('pause', () => {
-        void handleVideoState(VideoResponseStateEnum.PAUSED)
-      })
-      player.current.on('ended', () => {
-        void handleVideoState(VideoResponseStateEnum.FINISHED)
-      })
-      player.current.on('timeupdate', () => {
-        // This is a simple test on capturing response with time
-        // TODO: Figure out what states do we want to cover?
-        if (player.current?.currentTime() >= 5) {
-          void handleVideoState(VideoResponseStateEnum.SECONDSWATCHED)
-          player.current?.pause()
-        }
-      })
-      player.current.on('autoplay-success', () => setAutoplaySuccess(true))
+  useEffect(() => {
+    void validate(url)
+
+    if (videoUrl !== undefined) {
+      const initialOptions: videojs.PlayerOptions = {
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        autoplay: autoplay ? 'muted' : false,
+        controls: true,
+        userActions: {
+          hotkeys: true,
+          doubleClick: true
+        },
+        controlBar: {
+          playToggle: true,
+          captionsButton: true,
+          chaptersButton: true,
+          subtitlesButton: true,
+          remainingTimeDisplay: true,
+          progressControl: {
+            seekBar: true
+          },
+          fullscreenToggle: true,
+          playbackRateMenuButton: true,
+          volumePanel: {
+            inline: true
+          }
+        },
+        sources: [
+          {
+            src: videoUrl
+          }
+        ],
+        fluid: true,
+        responsive: true,
+        playbackRates: [0.5, 1, 1.5, 2]
+      }
+
+      if (videoNode.current != null) {
+        player.current = videojs(videoNode.current, {
+          ...initialOptions
+        })
+        player.current.on('ready', () => {
+          setIsReady(true)
+        })
+        player.current.on('playing', () => {
+          void handleVideoState(VideoResponseStateEnum.PLAYING)
+        })
+        player.current.on('pause', () => {
+          void handleVideoState(VideoResponseStateEnum.PAUSED)
+        })
+        player.current.on('ended', () => {
+          void handleVideoState(VideoResponseStateEnum.FINISHED)
+        })
+        player.current.on('timeupdate', () => {
+          // This is a simple test on capturing response with time
+          // TODO: Figure out what states do we want to cover?
+          if (player.current?.currentTime() >= 5) {
+            void handleVideoState(VideoResponseStateEnum.SECONDSWATCHED)
+            player.current?.pause()
+          }
+        })
+        player.current.on('autoplay-success', () => setAutoplaySuccess(true))
+      }
     }
-  }, [videoNode, autoplay, mediaComponentId, languageId, handleVideoState])
+  }, [videoNode, autoplay, mediaComponentId, languageId, handleVideoState, videoUrl, url])
 
   useEffect(() => {
     if (
