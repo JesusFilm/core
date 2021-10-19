f964e862f0ea1dee5c582464a12cdc5578d43d0f
John Geronimo <johnageronimo1@gmail.com>
2021-10-19 22:47:16 +0000

    Added Video journeys with triggers

--- apps/journeys/src/components/Conductor/Conductor.stories.tsx
+++ apps/journeys/src/components/Conductor/Conductor.stories.tsx
@@ -282,4 +282,141 @@ export const Default = (): ReactElement => (
   </MockedProvider>
 )
 
+export const VideoJourney = (): ReactElement => (
+  <MockedProvider>
+    <Conductor
+      blocks={[
+        {
+          id: 'step1.id',
+          __typename: 'StepBlock',
+          parentBlockId: null,
+          locked: false,
+          nextBlockId: 'step2.id',
+          children: [
+            {
+              id: 'card1.id',
+              __typename: 'CardBlock',
+              parentBlockId: 'step1.id',
+              backgroundColor: null,
+              coverBlockId: null,
+              themeMode: null,
+              themeName: null,
+              children: [
+                {
+                  id: 'video1.id',
+                  __typename: 'VideoBlock',
+                  parentBlockId: 'card1.id',
+                  title: '',
+                  volume: 1,
+                  autoplay: true,
+                  mediaComponentId: '2_0-FallingPlates',
+                  languageId: '529',
+                  children: [
+                    {
+                      id: 'trigger.id',
+                      __typename: 'TriggerBlock',
+                      parentBlockId: 'video1.id',
+                      triggerStart: 10,
+                      triggerAction: {
+                        __typename: 'NavigateToBlockAction',
+                        gtmEventName: 'gtmEventName',
+                        blockId: 'step2.id'
+                      },
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 'step2.id',
+          __typename: 'StepBlock',
+          parentBlockId: null,
+          locked: true,
+          nextBlockId: 'step3.id',
+          children: [
+            {
+              id: 'card2.id',
+              __typename: 'CardBlock',
+              parentBlockId: 'step2.id',
+              backgroundColor: null,
+              coverBlockId: null,
+              themeMode: null,
+              themeName: null,
+              children: [
+                {
+                  id: 'radioQuestion1.id',
+                  __typename: 'RadioQuestionBlock',
+                  parentBlockId: 'step2.id',
+                  label: 'Step 2',
+                  description: 'Locked',
+                  children: [
+                    {
+                      id: 'radioOption1.id',
+                      __typename: 'RadioOptionBlock',
+                      parentBlockId: 'radioQuestion1.id',
+                      label: 'NextVideo',
+                      action: {
+                        __typename: 'NavigateToBlockAction',
+                        gtmEventName: 'gtmEventName',
+                        blockId: 'step3.id'
+                      },
+                      children: []
+                    },
+                    {
+                      id: 'radioOption3.id',
+                      __typename: 'RadioOptionBlock',
+                      parentBlockId: 'radioQuestion1.id',
+                      label: 'Step 3 (No nextBlockId)',
+                      action: {
+                        __typename: 'NavigateToBlockAction',
+                        gtmEventName: 'gtmEventName',
+                        blockId: ''
+                      },
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 'step3.id',
+          __typename: 'StepBlock',
+          parentBlockId: null,
+          locked: false,
+          nextBlockId: 'step4.id',
+          children: [
+            {
+              id: 'card3.id',
+              __typename: 'CardBlock',
+              parentBlockId: 'step3.id',
+              backgroundColor: null,
+              coverBlockId: null,
+              themeMode: null,
+              themeName: null,
+              children: [
+                {
+                  id: 'video2.id',
+                  __typename: 'VideoBlock',
+                  parentBlockId: 'card3.id',
+                  title: '',
+                  volume: 1,
+                  autoplay: true,
+                  mediaComponentId: '1_jf-0-0',
+                  languageId: '529',
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]}
+    />
+  </MockedProvider>
+)
+
 export default Demo as Meta
