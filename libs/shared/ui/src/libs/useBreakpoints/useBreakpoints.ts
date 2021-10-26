4ec1a0fed82bf42301b38713d62f34a88527a4f9
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-10-26 06:25:11 +0000

    Update to allow client-side rendering only

--- libs/shared/ui/src/libs/useBreakpoints/useBreakpoints.ts
+++ libs/shared/ui/src/libs/useBreakpoints/useBreakpoints.ts
@@ -3,11 +3,13 @@ import { Breakpoint, useTheme, useMediaQuery } from '@mui/material'
 export const useBreakpoints = (): { [key in Breakpoint]: boolean } => {
   const theme = useTheme()
 
+  // Update if we need to SSR
+  // https://mui.com/components/use-media-query/#server-side-rendering
   return {
-    xs: useMediaQuery(theme.breakpoints.only('xs')),
-    sm: useMediaQuery(theme.breakpoints.only('sm')),
-    md: useMediaQuery(theme.breakpoints.only('md')),
-    lg: useMediaQuery(theme.breakpoints.only('lg')),
-    xl: useMediaQuery(theme.breakpoints.only('xl'))
+    xs: useMediaQuery(theme.breakpoints.only('xs'), { noSsr: true }),
+    sm: useMediaQuery(theme.breakpoints.only('sm'), { noSsr: true }),
+    md: useMediaQuery(theme.breakpoints.only('md'), { noSsr: true }),
+    lg: useMediaQuery(theme.breakpoints.only('lg'), { noSsr: true }),
+    xl: useMediaQuery(theme.breakpoints.only('xl'), { noSsr: true })
   }
 }
