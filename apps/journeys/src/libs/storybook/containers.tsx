38655a8b4e7bd6d4d4d0fa89de9db3bb5a9de89f
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-11-01 02:56:06 +0000

    remove storycard dependency

--- apps/journeys/src/libs/storybook/containers.tsx
+++ apps/journeys/src/libs/storybook/containers.tsx
@@ -1,31 +1,22 @@
-import { useTheme } from '@mui/material'
+import { Paper } from '@mui/material'
 import { ReactElement, ReactNode } from 'react'
-import { ThemeName, ThemeMode } from '../../../__generated__/globalTypes'
-import { CardWrapper } from '../../components/blocks/Card'
 
 interface StoryCardProps {
-  id?: string
-  backgroundColor?: string
-  themeMode?: ThemeMode
   children: ReactNode
 }
 
-export const StoryCard = ({
-  id,
-  children,
-  backgroundColor,
-  themeMode
-}: StoryCardProps): ReactElement => {
-  const theme = useTheme()
-
+export const StoryCard = ({ children }: StoryCardProps): ReactElement => {
   return (
-    <CardWrapper
-      id={id ?? ''}
-      backgroundColor={backgroundColor ?? null}
-      themeName={ThemeName.base}
-      themeMode={themeMode ?? (theme.palette.mode as ThemeMode)}
+    <Paper
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        p: 7,
+        borderRadius: (theme) => theme.spacing(4)
+      }}
+      elevation={3}
     >
-      <div style={{ flexGrow: 1, overflow: 'auto' }}>{children}</div>
-    </CardWrapper>
+      {children}
+    </Paper>
   )
 }
