e91a7e98b882477c6f87fb82ae5eebc715385ecf
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-11-03 04:13:47 +0000

    Fix video stories so they run

--- apps/journeys/src/components/blocks/Video/Video.stories.tsx
+++ apps/journeys/src/components/blocks/Video/Video.stories.tsx
@@ -1,10 +1,11 @@
 import { Story, Meta } from '@storybook/react'
 import { Video, VIDEO_RESPONSE_CREATE } from '.'
-import { TreeBlock } from '../../../libs/transformer/transformer'
-import { journeysConfig, StoryCard } from '../../../libs/storybook'
+import { journeysConfig } from '../../../libs/storybook'
 import { GetJourney_journey_blocks_VideoBlock as VideoBlock } from '../../../../__generated__/GetJourney'
 import { MockedProvider } from '@apollo/client/testing'
 import { VideoResponseStateEnum } from '../../../../__generated__/globalTypes'
+import Conductor, { ConductorProps } from '../../Conductor'
+import { TreeBlock } from '../../../libs/transformer/transformer'
 
 const Demo = {
   ...journeysConfig,
@@ -12,7 +13,52 @@ const Demo = {
   title: 'Journeys/Blocks/Video'
 }
 
-const Template: Story<TreeBlock<VideoBlock>> = ({ ...props }) => (
+const videoBlock: TreeBlock<VideoBlock> = {
+  __typename: 'VideoBlock',
+  id: 'Video1',
+  parentBlockId: 'step1.id',
+  autoplay: true,
+  muted: true,
+  title: 'video1',
+  videoContent: {
+    __typename: 'VideoArclight',
+    // This is the redirected URL from this link
+    // https://arc.gt/hls/${mediaComponentId}/${languageId} => https://arc.gt/hls/2_0-FallingPlates/529
+    // Meaning that it's expected for it's tokens key and this link to expire
+    src: 'https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1226740748001/f31e0685-c9e6-4afb-9ed5-987b4c0ccde9/10s/master.m3u8?fastly_token=NjE4MWZmYmJfZjhhMGNkMGJhNGQyM2QwNjFjMGMxNzE0ZmY1NGUyMGYyZTkzYTc5YjlmMTZkNzgyODJlMDY2YmJjNWYzZmZjNg%3D%3D'
+  },
+  startAt: null,
+  endAt: null,
+  children: []
+}
+
+// TODO: Move to Conductor data and autogenerate
+const conductorProps = (content: TreeBlock[]): TreeBlock[] => {
+  return [
+    {
+      id: 'step1.id',
+      __typename: 'StepBlock',
+      parentBlockId: null,
+      locked: false,
+      nextBlockId: 'step2.id',
+      children: [
+        {
+          id: 'card1.id',
+          __typename: 'CardBlock',
+          parentBlockId: 'step1.id',
+          coverBlockId: 'null',
+          backgroundColor: null,
+          themeMode: null,
+          themeName: null,
+          fullscreen: false,
+          children: content
+        }
+      ]
+    }
+  ]
+}
+
+const Template: Story<ConductorProps> = ({ ...props }) => (
   <MockedProvider
     mocks={[
       {
@@ -35,58 +81,36 @@ const Template: Story<TreeBlock<VideoBlock>> = ({ ...props }) => (
       }
     ]}
   >
-    <StoryCard>
-      <Video {...props} />
-    </StoryCard>
+    <Conductor {...props} />
   </MockedProvider>
 )
 
-// add an active block around this for it to work
-
-export const Default = Template.bind({})
-Default.args = {
-  __typename: 'VideoBlock',
-  id: 'Video1',
-  parentBlockId: 'step1.id',
-  volume: 1,
-  autoplay: true,
-  muted: true,
-  videoContent: {
-    __typename: 'VideoArclight',
-    // This is the redirected URL from this link
-    // https://arc.gt/hls/${mediaComponentId}/${languageId} => https://arc.gt/hls/2_0-FallingPlates/529
-    // Meaning that it's expected for it's tokens key and this link to expire
-    src: 'https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1226740748001/f31e0685-c9e6-4afb-9ed5-987b4c0ccde9/10s/master.m3u8?fastly_token=NjE4MWZmYmJfZjhhMGNkMGJhNGQyM2QwNjFjMGMxNzE0ZmY1NGUyMGYyZTkzYTc5YjlmMTZkNzgyODJlMDY2YmJjNWYzZmZjNg%3D%3D'
-  }
+export const ArclightSource = Template.bind({})
+ArclightSource.args = {
+  blocks: conductorProps([videoBlock])
 }
 
-export const Src = Template.bind({})
-Src.args = {
-  __typename: 'VideoBlock',
-  id: 'video1.id',
-  parentBlockId: 'step1.id',
-  volume: 1,
-  autoplay: true,
-  videoContent: {
-    __typename: 'VideoGeneric',
-    src: 'https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/index.m3u8'
-  }
+export const ExternalSource = Template.bind({})
+ExternalSource.args = {
+  blocks: conductorProps([
+    {
+      ...videoBlock,
+      videoContent: {
+        __typename: 'VideoGeneric',
+        src: 'https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/index.m3u8'
+      }
+    }
+  ])
 }
 
 export const StartAt = Template.bind({})
 StartAt.args = {
-  __typename: 'VideoBlock',
-  id: 'video1.id',
-  parentBlockId: 'step1.id',
-  volume: 1,
-  autoplay: true,
-  videoContent: {
-    // This is the redirected URL from this link
-    // https://arc.gt/hls/${mediaComponentId}/${languageId} => https://arc.gt/hls/2_0-FallingPlates/529
-    // Meaning that it's expected for it's tokens key to expire
-    src: 'https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/1226740748001/f31e0685-c9e6-4afb-9ed5-987b4c0ccde9/10s/master.m3u8?fastly_token=NjE4MWZmYmJfZjhhMGNkMGJhNGQyM2QwNjFjMGMxNzE0ZmY1NGUyMGYyZTkzYTc5YjlmMTZkNzgyODJlMDY2YmJjNWYzZmZjNg%3D%3D'
-  },
-  startAt: 30
+  blocks: conductorProps([
+    {
+      ...videoBlock,
+      startAt: 20
+    }
+  ])
 }
 
 export default Demo as Meta
