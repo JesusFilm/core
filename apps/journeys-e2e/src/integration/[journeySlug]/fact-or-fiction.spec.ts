ec923fe7efd204018b848d1470506a14c878c48e
Siyang Cao <csiyang@yahoo.co.nz>
2021-11-05 19:58:21 +0000

    skeleton for fact or fiction e2e

---
+++ apps/journeys-e2e/src/integration/[journeySlug]/fact-or-fiction.spec.ts
@@ -0,0 +1,30 @@
+describe('fact-or-fiction joruney', () => {
+  it('checks the first step (fact or fiction)', () => {
+    // Click the button from the home page
+    // Check title
+  })
+  it('checks the second step (video start)', () => {
+    // Click button from previous step
+    // Check video exists
+    // Check video starts playing
+    // wait for 2:14
+  })
+  it('checks the 3rd step (a quick question)', () => {
+    // checks the text
+    // checks there are 2 buttons with the correct text
+  })
+  it('checks the 4th step (video continue)', () => {
+    // clicks a button (any)
+    // video exists
+    // video starts playing
+    // Wait 2.90 (end of video)
+  })
+  it('checks the 5th step (some facts)', () => {
+    // checks the text
+    // checks the button text, and symbol maybe?
+  })
+  it('checks the last step (who was this Jesus)', () => {
+    // checks button text
+    // clicking on one should navigate you to the next journey
+  })
+})
