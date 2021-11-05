3ca4d061f3d55810e34f537ada8380d1e8ce0cfa
Siyang Cao <csiyang@yahoo.co.nz>
2021-11-05 22:18:05 +0000

    optimised e2e fact-or-fiction

--- apps/journeys-e2e/src/integration/[journeySlug]/fact-or-fiction.spec.ts
+++ apps/journeys-e2e/src/integration/[journeySlug]/fact-or-fiction.spec.ts
@@ -7,7 +7,6 @@ describe('fact-or-fiction joruney', () => {
     cy.get('a').contains('Fact or Fiction').click()
     // this uncaught error will fail the test, catching it for now
     cy.on('uncaught:exception', (err, runnable) => {
-      console.log("we should probably fix this: ", err)
       return false
     })
     cy.get('h2').contains('Fact or Fiction').should('exist')
@@ -16,14 +15,12 @@ describe('fact-or-fiction joruney', () => {
 
   it('clicking on the button should trigger the video', () => {
     cy.get('button').contains('Explore Now').click({force: true}) // button has error, parent class has 'disaply: none' -- remove {force: true} to check
-    cy.get('video').should('have.prop', 'paused', false).and('have.prop', 'ended', false)
+    cy.get('video').should('have.prop', 'paused', false).and('have.prop', 'ended', false).then(($video) => {
+      $video[0].playbackRate = 10
+    })
   })
 
   it('video should trigger the next step after 2:14', () => {
-    cy.get('video').then(($video) => {
-      $video[0].playbackRate = 10
-    })
-    cy.wait(13000)
     cy.get('h3').contains('Can we trust the story of Jesus?').should('exist')
     cy.get('button').contains('Yes').should('exist')
     cy.get('button').contains('No').should('exist')
@@ -35,7 +32,6 @@ describe('fact-or-fiction joruney', () => {
   })
   
   it('video should trigger the next step after 2:53', () => {
-    cy.wait(16000)
     cy.get('h2').contains('Jesus in History').should('exist')
     cy.get('button').contains('One question remains').should('exist')
   })
@@ -47,6 +43,7 @@ describe('fact-or-fiction joruney', () => {
     cy.get('button').contains('A popular prophet').should('exist')
     cy.get('button').contains('A fake historical figure').should('exist')
   })
+
   it('should navigate to the next journey when any of the repsonses are clicked on', () => {
     cy.get('button').contains('The Son of God').click()
     cy.url().should('include', '/what-about-the-resurrection')
