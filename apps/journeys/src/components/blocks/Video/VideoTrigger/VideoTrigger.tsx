b78cb742b476bf7cb346502932b8fba42236c1bb
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-11-12 01:02:46 +0000

    simplify useEffect function

--- apps/journeys/src/components/blocks/Video/VideoTrigger/VideoTrigger.tsx
+++ apps/journeys/src/components/blocks/Video/VideoTrigger/VideoTrigger.tsx
@@ -1,6 +1,6 @@
 import videojs from 'video.js'
 import { TreeBlock } from '../../../../libs/transformer/transformer'
-import { ReactElement, useCallback, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { GetJourney_journey_blocks_VideoTriggerBlock as TriggerBlock } from '../../../../../__generated__/GetJourney'
 import { handleAction } from '../../../../libs/action'
 import { useRouter } from 'next/router'
@@ -17,31 +17,24 @@ export function Trigger({
   const router = useRouter()
   const [triggered, setTriggered] = useState(false)
 
-  const handleTriggerAction = useCallback((): void => {
-    if (!triggered) {
-      setTriggered(true)
-      handleAction(router, triggerAction)
-    }
-  }, [setTriggered, router, triggerAction, triggered])
-
   useEffect(() => {
-    if (player != null) {
+    if (player != null && !triggered) {
       const timeUpdate = (): void => {
         if (player.currentTime() >= triggerStart) {
+          setTriggered(true)
           player.pause()
-
           if (player.isFullscreen()) {
             player.exitFullscreen()
-            setTimeout(handleTriggerAction, 1000)
+            setTimeout(() => handleAction(router, triggerAction), 1000)
           } else {
-            handleTriggerAction()
+            handleAction(router, triggerAction)
           }
         }
       }
       player.on('timeupdate', timeUpdate)
       return () => player.off('timeupdate', timeUpdate)
     }
-  }, [player, triggerStart, handleTriggerAction])
+  }, [player, triggerStart, router, triggerAction, triggered])
 
   return <></>
 }
