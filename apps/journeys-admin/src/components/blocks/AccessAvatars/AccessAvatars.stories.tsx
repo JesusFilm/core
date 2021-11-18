8c6f709b703717c9a6fe31e0ec867da9c49d9d21
Siyang Cao <csiyang@yahoo.co.nz>
2021-11-18 02:08:18 +0000

    default story

--- apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.stories.tsx
+++ apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.stories.tsx
@@ -1,7 +1,7 @@
 import { journeyAdminConfig } from '../../../libs/storybook'
 import { AccessAvatars } from '.'
 import { Meta, Story } from '@storybook/react'
-import { ReactElement } from 'react'
+import { AvatarsArray } from './AccessAvatars'
 
 const AccessAvatarsDemo = {
   ...journeyAdminConfig,
@@ -9,39 +9,38 @@ const AccessAvatarsDemo = {
   title: 'JourneyAdmin/AccessAvatarDemo'
 }
 
-const Template: Story<ReactElement> = ({ ...props }) => (
-  <AccessAvatars {...props} />
-)
+const Template: Story = () => <AccessAvatars accessAvatarsProps={avatars} />
 
-export const Default = Template.bind({})
-Default.args = [
+export const Default: Story<AvatarsArray> = Template.bind({})
+
+const avatars = [
   {
     id: '1',
     firstName: '1',
     lastName: '1',
-    image: '1',
+    image: 'https://source.unsplash.com/random/300x300',
     email: '1'
   },
   {
-    id: '1',
-    firstName: '1',
-    lastName: '1',
-    image: '1',
-    email: '1'
+    id: '2',
+    firstName: '2',
+    lastName: '2',
+    image: 'https://source.unsplash.com/random/300x300',
+    email: '2'
   },
   {
-    id: '1',
-    firstName: '1',
-    lastName: '1',
-    image: '1',
-    email: '1'
+    id: '3',
+    firstName: '3',
+    lastName: '3',
+    image: 'https://source.unsplash.com/random/300x300',
+    email: '3'
   },
   {
-    id: '1',
-    firstName: '1',
-    lastName: '1',
-    image: '1',
-    email: '1'
+    id: '4',
+    firstName: '4',
+    lastName: '4',
+    image: 'https://source.unsplash.com/random/300x300',
+    email: '4'
   }
 ]
 
--- apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.tsx
+++ apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.tsx
@@ -1,12 +1,25 @@
 import { Avatar, AvatarGroup } from '@mui/material'
 import { ReactElement } from 'react'
 
-export function AccessAvatars(Users): ReactElement {
-  // TODO update prop type
+export interface AvatarProps {
+  id: string
+  firstName: string
+  lastName?: string
+  image?: string
+  email: string
+}
+
+export interface AvatarsArray {
+  accessAvatarsProps: AvatarProps[]
+}
+
+export function AccessAvatars({
+  accessAvatarsProps
+}: AvatarsArray): ReactElement {
   return (
     <AvatarGroup max={4}>
-      {Users.map(({ id, firstName, lastName, image, email }) => (
-        <Avatar key={id} alt={firstName} src={image} />
+      {accessAvatarsProps?.map((user) => (
+        <Avatar key={user.id} alt={user.firstName} src={user.image} />
       ))}
     </AvatarGroup>
   )
