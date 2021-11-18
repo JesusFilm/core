11f1c5f5bf665c869c100d1af65e8b54f75b4b5b
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-11-18 23:03:46 +0000

    Fix GridContainer file name

--- libs/journeys/ui/src/components/GridContainer/GridContainer.spec.tsx
+++ libs/journeys/ui/src/components/GridContainer/GridContainer.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react'
 import { GridContainer } from '.'
-import { TreeBlock } from '../../'
+import { TreeBlock } from '../..'
 import {
   GridDirection,
   GridJustifyContent,
