43f8193797b4cb1e9b7a8cf9904efb0bb66009c6
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-11-19 03:36:02 +0000

    update exports

--- libs/journeys/ui/src/libs/index.ts
+++ libs/journeys/ui/src/libs/index.ts
@@ -1,9 +1,10 @@
 export { handleAction } from './action'
 export { journeyUiConfig, simpleComponentConfig } from './storybook/config'
 export { StoryCard } from './storybook/containers'
-export { default as transformer, TreeBlock } from './transformer'
+export { transformer } from './transformer'
 export {
   useBlocks,
   nextActiveBlock,
   isActiveBlockOrDescendant
 } from './useBlocks/blocks'
+export type { TreeBlock } from './transformer'
