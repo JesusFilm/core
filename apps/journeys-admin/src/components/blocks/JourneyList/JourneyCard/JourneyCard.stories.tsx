f45c7e83c1fa4841ccab7476c10503b713cabbe2
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2021-11-22 14:16:48 +1300

    JourneyList scaffold (#125)
    * JourneyList scaffold
    
    * Draft added more to scaffold
    
    * Add to scaffolding
    
    * Fix tests
    
    * Tidy up how menu renders
    
    * Tidy up imports

---
+++ apps/journeys-admin/src/components/blocks/JourneyList/JourneyCard/JourneyCard.stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react'
+
+import { journeyAdminConfig } from '../../../../libs/storybook'
+import JourneyCard from './JourneyCard'
+import { defaultJourney } from '../journeyListData'
+
+const TestStory = {
+  ...journeyAdminConfig,
+  component: JourneyCard,
+  title: 'Journey-Admin/JourneyList/JourneyCard'
+}
+
+const Template: Story = () => <JourneyCard journey={defaultJourney} />
+
+export const StoryComponent = Template.bind({})
+
+export default TestStory as Meta
