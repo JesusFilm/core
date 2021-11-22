da21aa6bce183f086e65226c6b3e74dee0f12f45
Siyang Cao <csiyang@yahoo.co.nz>
2021-11-22 02:17:37 +0000

    updated tests

--- apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.spec.tsx
+++ apps/journeys-admin/src/components/blocks/AccessAvatars/AccessAvatars.spec.tsx
@@ -1,5 +1,19 @@
-import { AccessAvatars } from './AccessAvatars'
+import { render } from '@testing-library/react'
+import { AccessAvatars, AccessAvatarsProps } from './AccessAvatars'
 
-describe('Test', () => {
-  expect(true).toBeTruthy()
+describe('AccessAvatars', () => {
+  it('should render avatars', () => {
+    const props: AccessAvatarsProps = {
+      users: [
+        {
+          id: '1',
+          firstName: 'Amin',
+          lastName: 'Person',
+          image: 'https://source.unsplash.com/random/300x300',
+          email: 'amin@email.com'
+        }
+      ]
+    }
+    render(<AccessAvatars users={props.users} />)
+  })
 })
