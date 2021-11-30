9e1ac6cb749d23d2f53c0196f3b587be4ef9b1d4
jianwei1 <jianweichong08@gmail.com>
2021-11-30 01:33:30 +0000

    added singleJourneyNavBar component and stories

---
+++ apps/journeys-admin/src/components/JourneyList/JourneyCard/JourneyCardNavBar/JourneyCardNavBar.stories.tsx
@@ -0,0 +1,16 @@
+import { journeysAdminConfig } from '../../../../libs/storybook'
+import { Meta, Story } from '@storybook/react'
+import JourneyCardNavBar from './JourneyCardNavBar'
+
+const AppHeaderDemo = {
+    ...journeysAdminConfig,
+    component: JourneyCardNavBar,
+    title: 'Journeys-Admin/JourneyList/JourneyCard/JourneyCardNavBar'
+}
+
+const Template: Story = () => JourneyCardNavBar()
+
+export const StoryComponent = Template.bind({})
+
+
+export default AppHeaderDemo as Meta
