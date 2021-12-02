547f652bd75cf141271447fbd3158ee600518a83
Siyang Cao <csiyang@yahoo.co.nz>
2021-12-02 01:16:06 +0000

    removed padding

--- apps/journeys-admin/src/components/blocks/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.tsx
+++ apps/journeys-admin/src/components/blocks/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.tsx
@@ -14,50 +14,41 @@ export interface AccessAvatarsProps {
 }
 
 export function AccessAvatars({ users }: AccessAvatarsProps): ReactElement {
-  let padding = '0px'
-  if (users.length === 1) {
-    padding = '72px'
-  } else if (users.length === 2) {
-    padding = '36px'
-  } else {
-    padding = '0px'
-  }
   return (
     <AvatarGroup
       max={3}
       sx={{
         display: 'flex',
-        justifyContent: 'flex-end',
-        paddingRight: padding
+        justifyContent: 'flex-end'
       }}
     >
       {users.map((user) => (
         <Tooltip title={`${createToolTipTitle(user)}`} key={user.id}>
           <Avatar alt={user.firstName} src={user.image}>
-            { createFallbackLetter(user) }
+            {createFallbackLetter(user)}
           </Avatar>
         </Tooltip>
       ))}
     </AvatarGroup>
-  ) 
+  )
 }
- 
-function createToolTipTitle(user: AccessAvatar): string{
-  if(user.firstName !=null && user.lastName != null){
+
+function createToolTipTitle(user: AccessAvatar): string {
+  if (user.firstName != null && user.lastName != null) {
     return `${user.firstName} ${user.lastName}`
-  } else if(user.email !=null){
+  } else if (user.email != null) {
     return `${user.email}`
   } else {
-    return `No name or email available for this user`;
+    return `No name or email available for this user`
   }
 }
 
-function createFallbackLetter(user:AccessAvatar): string | null{
-  if(user.firstName !=null){
+function createFallbackLetter(user: AccessAvatar): string | null {
+  if (user.firstName != null) {
     return `${user.firstName[0].toUpperCase()}`
-  } else if(user.email !=null){
+  } else if (user.email != null) {
     return `${user.email[0].toUpperCase()}`
   } else {
-    return null;
+    return null
   }
 }
