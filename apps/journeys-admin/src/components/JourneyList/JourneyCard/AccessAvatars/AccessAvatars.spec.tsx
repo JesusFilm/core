80a9cecea8327e46629030530d9ea487ce43edcb
Siyang Cao <csiyang@yahoo.co.nz>
2021-12-02 23:25:31 +0000

    updated stories

--- apps/journeys-admin/src/components/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.spec.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.spec.tsx
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/react'
-import { AccessAvatars, AccessAvatarsProps} from './AccessAvatars'
+import { AccessAvatars, AccessAvatarsProps } from './AccessAvatars'
 import { user1, user6 } from './AccessAvatarsData'
 
 describe('AccessAvatars', () => {
   it('should render avatars', () => {
     const props: AccessAvatarsProps = {
-      users: [user1] 
+      users: [user1]
     }
     render(<AccessAvatars users={props.users} />)
   })
@@ -14,43 +14,41 @@ describe('AccessAvatars', () => {
     const props: AccessAvatarsProps = {
       users: [user1]
     }
-    const {getByAltText} = render(<AccessAvatars users={props.users} />)
-    expect(getByAltText('Amin')).toBeInTheDocument();
+    const { getByAltText } = render(<AccessAvatars users={props.users} />)
+    expect(getByAltText('Amin')).toBeInTheDocument()
   })
 
   it('should use first name and last as tooltip', () => {
     const props: AccessAvatarsProps = {
       users: [user1]
     }
-    const {getByLabelText} = render(<AccessAvatars users={props.users} />)
-    expect(getByLabelText('Amin Person')).toBeInTheDocument();
-  }) 
+    const { getByLabelText } = render(<AccessAvatars users={props.users} />)
+    expect(getByLabelText('Amin Person')).toBeInTheDocument()
+  })
 
   it('should use email as tooltip if user has no firstname', () => {
     const props: AccessAvatarsProps = {
       users: [user6]
     }
-    const {getByLabelText} = render(<AccessAvatars users={props.users} />)
-    expect(getByLabelText('drake@email.com')).toBeInTheDocument();
+    const { getByLabelText } = render(<AccessAvatars users={props.users} />)
+    expect(getByLabelText('drake@email.com')).toBeInTheDocument()
   })
 
   it('should use "No name or email available for this user" as tooltip if user has no firstname and no email', () => {
     const props: AccessAvatarsProps = {
-      users: [
-        {...user6, firstName: undefined, email: undefined}
-      ]
+      users: [{ ...user6, firstName: undefined, email: undefined }]
     }
-    const {getByLabelText} = render(<AccessAvatars users={props.users} />)
-    expect(getByLabelText('No name or email available for this user')).toBeInTheDocument();
+    const { getByLabelText } = render(<AccessAvatars users={props.users} />)
+    expect(getByLabelText('Anonymous')).toBeInTheDocument()
   })
 
   it('should display generic avatar icon if user has no firstname, no email and no image', () => {
     const props: AccessAvatarsProps = {
       users: [
-        {...user6, firstName: undefined, email: undefined, image: undefined}
+        { ...user6, firstName: undefined, email: undefined, image: undefined }
       ]
     }
-    const {getByTestId} = render(<AccessAvatars users={props.users} />)
-    expect(getByTestId('PersonIcon')).toBeInTheDocument();
-  })  
+    const { getByTestId } = render(<AccessAvatars users={props.users} />)
+    expect(getByTestId('PersonIcon')).toBeInTheDocument()
+  })
 })
--- apps/journeys-admin/src/components/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.stories.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyCard/AccessAvatars/AccessAvatars.stories.tsx
@@ -17,13 +17,18 @@ Single.args = {
   users: [user1]
 }
 
-export const Multiple: Story<AccessAvatarsProps> = Template.bind({})
-Multiple.args = {
+export const MaxAvatars: Story<AccessAvatarsProps> = Template.bind({})
+MaxAvatars.args = {
   users: [user1, user2, user3]
 }
 
-export const Fallback: Story<AccessAvatarsProps> = Template.bind({})
-Fallback.args = {
+export const OverFlowAvatars: Story<AccessAvatarsProps> = Template.bind({})
+OverFlowAvatars.args = {
+  users: [user1, user2, user3, user4, user5]
+}
+
+export const FallbackNoImage: Story<AccessAvatarsProps> = Template.bind({})
+FallbackNoImage.args = {
   users: [
     { ...user1, image: undefined },
     { ...user2, image: undefined },
@@ -31,29 +36,40 @@ Fallback.args = {
   ]
 }
 
-export const ExtraAvatars: Story<AccessAvatarsProps> = Template.bind({})
-ExtraAvatars.args = {
-  users: [user1, user2, user3, user4, user5]
-}
-
 // display user without first name. should display their email in the tooltip
-export const NoFirstName: Story<AccessAvatarsProps> = Template.bind({})
-NoFirstName.args = {
-  users: [{ ...user6, firstName: undefined }]
+export const FallbackNoFirstName: Story<AccessAvatarsProps> = Template.bind({})
+FallbackNoFirstName.args = {
+  users: [
+    { ...user1, firstName: undefined },
+    { ...user2, firstName: undefined },
+    { ...user3, firstName: undefined },
+    { ...user6, firstName: undefined }
+  ]
 }
 
 // display user without first name or image, should display email in the tooltip
 // and first letter of email as the fallback image
-export const NoNameOrImage: Story<AccessAvatarsProps> = Template.bind({})
-NoNameOrImage.args = {
-  users: [{ ...user6, firstName: undefined, image: undefined }]
+export const FallbackNoNameOrImage: Story<AccessAvatarsProps> = Template.bind(
+  {}
+)
+FallbackNoNameOrImage.args = {
+  users: [
+    { ...user1, firstName: undefined, image: undefined },
+    { ...user2, firstName: undefined, image: undefined },
+    { ...user3, firstName: undefined, image: undefined },
+    { ...user6, firstName: undefined, image: undefined }
+  ]
 }
 
 // display user without first name/email/image, should display generic avatar icon with
 // "No name or email available for this user" as tooltip
-export const NoNameEmailOrImage: Story<AccessAvatarsProps> = Template.bind({})
-NoNameEmailOrImage.args = {
+export const FallbackNoNameEmailOrImage: Story<AccessAvatarsProps> =
+  Template.bind({})
+FallbackNoNameEmailOrImage.args = {
   users: [
+    { ...user1, firstName: undefined, email: undefined, image: undefined },
+    { ...user2, firstName: undefined, email: undefined, image: undefined },
+    { ...user3, firstName: undefined, email: undefined, image: undefined },
     { ...user6, firstName: undefined, email: undefined, image: undefined }
   ]
 }
