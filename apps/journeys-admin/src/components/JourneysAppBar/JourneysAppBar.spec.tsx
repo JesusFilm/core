a87db35f2d891861cdbdf5405d5b114f07cbc4d9
Siyang <csiyang@yahoo.co.nz>
2021-12-08 03:22:44 +0000

    created data file for single journey

--- apps/journeys-admin/src/components/JourneysAppBar/JourneysAppBar.spec.tsx
+++ apps/journeys-admin/src/components/JourneysAppBar/JourneysAppBar.spec.tsx
@@ -1,22 +1,9 @@
 import { render } from '@testing-library/react'
-import { JourneyStatus } from '../../../__generated__/globalTypes'
-import { GetJourney_journey as Journey } from '../../../__generated__/GetJourney'
+import { defaultJourney } from '../SingleJourney/SingleJourneyData'
 import JourneysAppBar from '.'
 
 describe('JourneysAppBar', () => {
   it('should navigate back to journeys', () => {
-    const defaultJourney: Journey = {
-      __typename: 'Journey',
-      id: 'journey-id',
-      title: 'Journey Heading',
-      description: 'Description',
-      slug: 'default',
-      status: JourneyStatus.draft,
-      createdAt: '2021-11-19T12:34:56.647Z',
-      publishedAt: null,
-      blocks: null
-    }
-
     const { getByRole } = render(<JourneysAppBar journey={defaultJourney} />)
     expect(getByRole('link').getAttribute('href')).toEqual('/journeys')
   })
