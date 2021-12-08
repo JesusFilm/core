383264eac35b81764f1c970330e7c8f449fd1f2a
Siyang <csiyang@yahoo.co.nz>
2021-12-08 23:23:00 +0000

    updated typog variants

--- apps/journeys-admin/src/components/ThemeProvider/theme.ts
+++ apps/journeys-admin/src/components/ThemeProvider/theme.ts
@@ -4,22 +4,118 @@ export const theme = createTheme({
   typography: {
     fontFamily: "'Open Sans', sans-serif",
     h1: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '36px',
+      lineHeight: '40px'
     },
     h2: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 800,
+      fontStyle: 'normal',
+      fontSize: '28px',
+      lineHeight: '33px'
     },
     h3: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 800,
+      fontStyle: 'normal',
+      fontSize: '24px',
+      lineHeight: '28px'
     },
     h4: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 800,
+      fontStyle: 'normal',
+      fontSize: '22px',
+      lineHeight: '27px'
     },
     h5: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 800,
+      fontStyle: 'normal',
+      fontSize: '18px',
+      lineHeight: '23 px'
     },
     h6: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '14px',
+      lineHeight: '20px',
+      letterSpacing: '2px'
+    },
+    subtitle1: {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '18px',
+      lineHeight: '24px'
+    },
+    subtitle2: {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24   px'
+    },
+    body1: {
+      fontFamily: "'Open Sans', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '16px',
+      lineHeight: '24px'
+    },
+    body2: {
+      fontFamily: "'Open Sans', sans-serif",
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '14px',
+      lineHeight: '20px'
+    },
+    overline: {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '141x',
+      lineHeight: '16px',
+      letterSpacing: '3px'
+    },
+    caption: {
+      fontFamily: "'Open Sans', sans-serif",
+      fontWeight: 400,
+      fontStyle: 'normal',
+      fontSize: '12px',
+      lineHeight: '20px'
+    },
+    label: {
+      fontFamily: "'Roboto Mono', sans-serif",
+      fontWeight: 500,
+      fontStyle: 'normal',
+      fontSize: '36px',
+      lineHeight: '47.48px'
+    },
+    'button-lg': {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '18px',
+      lineHeight: '20px'
+    },
+    'button-md': {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 700,
+      fontStyle: 'normal',
+      fontSize: '15px',
+      lineHeight: '18px'
+    },
+    'button-sm': {
+      fontFamily: "'Montserrat', sans-serif",
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '14px',
+      lineHeight: '16px'
     }
   },
   palette: {
@@ -46,3 +142,28 @@ export const theme = createTheme({
     }
   }
 })
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    label: React.CSSProperties
+    'button-sm': React.CSSProperties
+    'button-md': React.CSSProperties
+    'button-lg': React.CSSProperties
+  }
+
+  interface TypographyVariantsOptions {
+    label: React.CSSProperties
+    'button-sm': React.CSSProperties
+    'button-md': React.CSSProperties
+    'button-lg': React.CSSProperties
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    label: true
+    'button-sm': true
+    'button-md': true
+    'button-lg': true
+  }
+}
