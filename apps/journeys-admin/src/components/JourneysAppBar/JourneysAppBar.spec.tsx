c2306ae3bf7eb2e471d5fe6ae173113f43349013
Siyang <csiyang@yahoo.co.nz>
2021-12-08 02:39:12 +0000

    fixed type error on journeyAppBar

--- apps/journeys-admin/src/components/JourneysAppBar/JourneysAppBar.spec.tsx
+++ apps/journeys-admin/src/components/JourneysAppBar/JourneysAppBar.spec.tsx
@@ -1,9 +1,22 @@
 import { render } from '@testing-library/react'
+import { JourneyStatus } from '../../../__generated__/globalTypes'
+import { GetJourney_journey as Journey } from '../../../__generated__/GetJourney'
 import JourneysAppBar from '.'
-import { defaultJourney } from '../JourneyList/journeyListData'
 
 describe('JourneysAppBar', () => {
   it('should navigate back to journeys', () => {
+    const defaultJourney: Journey = {
+      __typename: 'Journey',
+      id: 'journey-id',
+      title: 'Journey Heading',
+      description: 'Description',
+      slug: 'default',
+      status: JourneyStatus.draft,
+      createdAt: '2021-11-19T12:34:56.647Z',
+      publishedAt: null,
+      blocks: null
+    }
+
     const { getByRole } = render(<JourneysAppBar journey={defaultJourney} />)
     expect(getByRole('link').getAttribute('href')).toEqual('/journeys')
   })
