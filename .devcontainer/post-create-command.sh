dc5353024adc890aeaf6c12639ac2573092be948
Mike Allison <mike@captainofbass.com>
2021-12-08 20:15:05 +0000

    remove postgres

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -4,9 +4,6 @@
 sudo chown node -R /workspaces
 sudo chgrp node -R /workspaces
 
-# add default user to postgres
-psql -c "CREATE USER \"test-user\" WITH PASSWORD 'test-password' CREATEDB;"
-
 # install Nx CLI tool
 npm install -g nx
 
