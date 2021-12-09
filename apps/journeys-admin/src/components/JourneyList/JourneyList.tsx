cf38213f0548e726f306b8168d6f7e45f05e38a9
Siyang Cao <csiyang@yahoo.co.nz>
2021-12-09 20:09:44 +0000

    added responsive border for journeyList

--- apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
@@ -3,21 +3,24 @@ import { Box, Typography, Link } from '@mui/material'
 
 import { GetJourneys_journeys as Journey } from '../../../__generated__/GetJourneys'
 import JourneyCard from './JourneyCard'
+import { useBreakpoints } from '@core/shared/ui'
 export interface JourneysListProps {
   journeys: Journey[]
 }
 
 const JourneyList = ({ journeys }: JourneysListProps): ReactElement => {
   // const theme = useTheme()
+  const breakpoints = useBreakpoints()
+  const border = breakpoints.md ? 3 : 0
   return (
     <>
       {journeys.length > 0 ? (
         <Box
           sx={{
-            borderRadius: 3,
+            borderRadius: border,
             outline: 'solid',
             outlineColor: '#DEDFE0', // theme.palette.surface.dark
-            outlineWidth: 2,
+            outlineWidth: 1,
             overflow: 'hidden'
           }}
         >
