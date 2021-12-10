edc6256fc8b86b6b5e5521742e41eb185df7206c
Mike Allison <mike@captainofbass.com>
2021-12-10 21:02:44 +0000

    initial ecr test

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -13,5 +13,17 @@ npm install -g @nestjs/cli@^8.1.5
 # install Rover CLI tool
 ./tools/scripts/install-rover.sh
 
+install aws cli tools
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -rf aws
+
+# install kubectl
+curl -LO https://dl.k8s.io/release/v1.22.0/bin/linux/amd64/kubectl
+chmod 775 kubectl
+sudo mv kubectl /usr/local/bin
+
 # install all dependencies
 npm i
---
+++ resources/kube/arangodb/deploy-container.sh
@@ -0,0 +1,6 @@
+kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-deployment.yaml
+# To use `ArangoLocalStorage`, also run
+#kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-storage.yaml
+
+kubectl apply -f ./single-server.yaml
