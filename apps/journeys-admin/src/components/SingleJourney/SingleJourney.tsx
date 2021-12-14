6b2a88c8f9d0c9c180ed11de6c826009fcfbe824
Ziwei Liu <ziwei.liu.23@gmail.com>
2021-12-14 00:22:32 +0000

    Install date-fns over moment

--- apps/journeys-admin/src/components/SingleJourney/SingleJourney.tsx
+++ apps/journeys-admin/src/components/SingleJourney/SingleJourney.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react'
-import moment from 'moment'
 import { Box, Typography } from '@mui/material'
 import JourneysAppBar from '../JourneysAppBar'
 import { GetJourney_journey as Journey } from '../../../__generated__/GetJourney'
@@ -9,23 +8,12 @@ interface SingleJourneyProps {
 }
 
 const SingleJourney = ({ journey }: SingleJourneyProps): ReactElement => {
-  // TODO: Extract out into seperate utils helper
-  const date =
-    moment(journey.createdAt).format('YYYY') === moment().format('YYYY')
-      ? moment(journey.createdAt).format('MMM Do')
-      : moment(journey.createdAt).format('MMM Do, YYYY')
-
   return (
     <>
       <JourneysAppBar journey={journey} />
-      <Box sx={{ m: 10 }}>
-        <Typography variant={'body2'}>{date}</Typography>
-        <Typography variant={'h3'}>{journey.title}</Typography>
-        <Typography variant={'body2'}>{journey.description}</Typography>
-        <Typography variant={'caption'} data-testid={'status'}>
-          {journey.status.charAt(0).toUpperCase() + journey.status.slice(1)}
-        </Typography>
-        <Typography variant={'caption'}>{journey.locale}</Typography>
+      <Box sx={{ m: 8, mt: 18 }}>
+        <Typography variant={'h4'}>{journey.title}</Typography>
+        <Typography variant={'body1'}>{journey.description}</Typography>
       </Box>
     </>
   )
