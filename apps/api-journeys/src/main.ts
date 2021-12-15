0b0b5fc5c03742012103f726252ca86d93a73ba9
Mike Allison <mike@captainofbass.com>
2021-12-15 19:46:37 +0000

    fix port in api-users dockerfile remove old cf stacks remove serverless calls and npm packages

--- apps/api-journeys/src/main.ts
+++ apps/api-journeys/src/main.ts
@@ -1,13 +1,7 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { Server } from 'http'
-import * as express from 'express'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import * as serverlessExpress from 'aws-serverless-express'
-import { Context } from "aws-lambda"
 import { AppModule } from './app/app.module'
 
-let lambdaProxy: Server
 
 async function bootstrap(): Promise<void> {
   const globalPrefix = 'api'
@@ -19,27 +13,5 @@ async function bootstrap(): Promise<void> {
   })
 }
 
-async function bootStrapLambda(): Promise<any> {
-  const expressServer = express()
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressServer))
-  await nestApp.init()
-
-  return serverlessExpress.createServer(expressServer)
-}
-
-if (require.main === module) {// development
-  // eslint-disable-next-line
-  bootstrap() 
-} 
-
-export const handler = (event: any, context: Context): void => {
-  // eslint-disable-next-line
-  if (!lambdaProxy) {
-    bootStrapLambda().then((server) => {
-      lambdaProxy = server
-      serverlessExpress.proxy(lambdaProxy, event, context)
-    })
-  } else {
-    serverlessExpress.proxy(lambdaProxy, event, context)
-  }
-}
+// eslint-disable-next-line
+bootstrap() 
