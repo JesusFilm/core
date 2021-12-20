3b1964577c242e7f03844503d829c0f9b3d4423b
jianwei1 <jianweichong08@gmail.com>
2021-12-21 09:09:19 +1300

    updated some styling for the app bar (#172)
    * Remove primaryImageBlock from journeys
    
    * Journeys admin Nav bar (#152)
    
    * added singleJourneyNavBar component and stories
    
    * turned navbar back button into iconButton
    
    * reformatted JourneyCardNavBar, added an index.tsx file
    
    * added journeycardNavBar to single journey pagwe
    
    * started creating tests
    
    * wrote test for navBar
    
    * ran prettier, removed margin in _app.tsx
    
    * made changes to pull request
    
    * ran prettier
    
    * ran prettier, made recommended changes to pull request
    
    * removed boxshadow from SingleJourneyAppBar
    
    * reused appbar for journeys list and single journey page
    
    * C4/Single journey card overview - Edit Button (#150)
    
    * inital commit
    
    * added button
    
    * merged in main
    
    * made suggested changes
    
    * seperated import/export statement to fix breaking tests
    
    * C4/single journey overview (#162)
    
    - display journey cards
    - add button to edit
    - add styling for journey with no cards
    - stories/ tests
    
    * updated some styling for the app bar
    
    * updated theme color
    
    * updated how component is exported
    
    * updated to use variants
    
    * fixed broken imports
    
    * reverting some changes not relevant to appbar
    
    * added back spaces
    
    * ran codegen
    
    Co-authored-by: Ziwei Liu <ziwei.liu.23@gmail.com>
    Co-authored-by: Siyang Cao <67619008+csiyang@users.noreply.github.com>
    Co-authored-by: Siyang <csiyang@yahoo.co.nz>
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

---
+++ apps/journeys-admin/src/components/JourneysAppBar/JourneysAppBar.tsx
@@ -0,0 +1,51 @@
+import { ReactElement } from 'react'
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import Link from 'next/link'
+import { ChevronLeftRounded } from '@mui/icons-material'
+import { useBreakpoints } from '@core/shared/ui'
+
+export interface JourneysAppBarProps {
+  variant: 'list' | 'view'
+}
+
+export function JourneysAppBar({ variant }: JourneysAppBarProps): ReactElement {
+  const breakpoints = useBreakpoints()
+
+  return (
+    <AppBar
+      position="fixed"
+      color="default"
+      sx={{ display: 'flex', flexDirection: 'row' }}
+    >
+      {variant === 'view' ? (
+        <Toolbar sx={{ flexGrow: 1, mr: breakpoints.md ? '328px' : 0 }}>
+          <Link href={`/journeys`} passHref>
+            <IconButton
+              aria-label="back to journeys"
+              edge="start"
+              size="large"
+              color="inherit"
+              sx={{ mr: 2 }}
+            >
+              <ChevronLeftRounded />
+            </IconButton>
+          </Link>
+          <Typography
+            variant="subtitle1"
+            component="div"
+            noWrap
+            sx={{ ml: 2, flexGrow: 1 }}
+          >
+            Journey Details
+          </Typography>
+        </Toolbar>
+      ) : (
+        <Toolbar sx={{ flexGrow: 1 }}>
+          <Typography variant="subtitle1" component="div" noWrap sx={{ ml: 1 }}>
+            Journeys
+          </Typography>
+        </Toolbar>
+      )}
+    </AppBar>
+  )
+}
