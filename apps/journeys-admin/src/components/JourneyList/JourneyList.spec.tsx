1621b8318c349361959bba025be898fcfd247854
Siyang Cao <csiyang@yahoo.co.nz>
2021-12-20 02:06:28 +0000

    added back changes that got removed

--- apps/journeys-admin/src/components/JourneyList/JourneyList.spec.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyList.spec.tsx
@@ -29,19 +29,22 @@ describe('JourneyList', () => {
     expect(journeyCards[1].textContent).toContain('Old Journey Heading')
     expect(journeyCards[2].textContent).toContain('Published Journey heading')
     expect(journeyCards[3].textContent).toContain('This heading is very')
-  it('should render all journeys', () => {
-    const { getAllByLabelText } = render(
-      <JourneyList journeys={[defaultJourney, publishedJourney, oldJourney]} />
-    )
-    expect(getAllByLabelText('journey-card').length).toBe(3)
-  })
-  it('should render text when there are no journeys', () => {
-    const { getByText, getByRole } = render(<JourneyList journeys={[]} />)
+    it('should render all journeys', () => {
+      const { getAllByLabelText } = render(
+        <JourneyList
+          journeys={[defaultJourney, publishedJourney, oldJourney]}
+        />
+      )
+      expect(getAllByLabelText('journey-card').length).toBe(3)
+    })
+    it('should render text when there are no journeys', () => {
+      const { getByText, getByRole } = render(<JourneyList journeys={[]} />)
 
-    expect(getByText('No journeys to display.')).toBeInTheDocument()
-    expect(
-      getByText('Create a journey, then find it here.')
-    ).toBeInTheDocument()
-    expect(getByRole('button')).toHaveTextContent('Create a Journey')
+      expect(getByText('No journeys to display.')).toBeInTheDocument()
+      expect(
+        getByText('Create a journey, then find it here.')
+      ).toBeInTheDocument()
+      expect(getByRole('button')).toHaveTextContent('Create a Journey')
+    })
   })
 })
--- apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
@@ -1,8 +1,9 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { Box, Card, Typography, Button } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import { useBreakpoints } from '@core/shared/ui'
 import { GetJourneys_journeys as Journey } from '../../../__generated__/GetJourneys'
+import JourneySort, { SortBy } from './JourneySort'
 import { JourneyCard } from './JourneyCard'
 
 export interface JourneysListProps {
@@ -13,26 +14,51 @@ export function JourneyList({ journeys }: JourneysListProps): ReactElement {
   const breakpoints = useBreakpoints()
   const border = breakpoints.sm ? 3 : 0
 
+  const [sortBy, setSortBy] = useState(SortBy.UNDEFINED)
+
+  if (sortBy === SortBy.TITLE) {
+    journeys.sort((a, b) => {
+      return a.title.localeCompare(b.title)
+    })
+  } else {
+    journeys.sort((a, b) => {
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    })
+  }
+
   return (
     <>
       {journeys.length > 0 ? (
-        <Card variant="outlined" sx={{ borderRadius: border }}>
-          {journeys.map((journey, i) => {
-            return (
-              <Box
-                key={journey.id}
-                aria-label="journey-card"
-                sx={{
-                  '&:first-child': { borderTop: 'none' },
-                  borderTop: 1,
-                  borderColor: 'divider'
-                }}
-              >
-                <JourneyCard journey={journey} />
-              </Box>
-            )
-          })}
-        </Card>
+        <>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              mb: 6
+            }}
+          >
+            <Typography variant={'h3'}>All Journeys</Typography>
+            <JourneySort sortBy={sortBy} setSortBy={setSortBy} />
+          </Box>
+          <Card variant="outlined" sx={{ borderRadius: border }}>
+            {journeys.map((journey, i) => {
+              return (
+                <Box
+                  key={journey.id}
+                  aria-label="journey-card"
+                  sx={{
+                    '&:first-child': { borderTop: 'none' },
+                    borderTop: 1,
+                    borderColor: 'divider'
+                  }}
+                >
+                  <JourneyCard journey={journey} />
+                </Box>
+              )
+            })}
+          </Card>
+        </>
       ) : (
         <Card
           variant="outlined"
