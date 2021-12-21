f721b96f635a185754a9c28519bd34f4b2dc2f8f
Mike Allison <dj.mikeallison@gmail.com>
2021-12-21 08:55:29 +0000

    remove post api prefix

--- apps/api-users/src/main.ts
+++ apps/api-users/src/main.ts
@@ -3,12 +3,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 
 async function bootstrap(): Promise<void> {
-  const globalPrefix = 'api'
   const port = process.env.PORT ?? '4002'
   const app = await NestFactory.create(AppModule)
-  app.setGlobalPrefix(globalPrefix)
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
+    Logger.log('Listening at http://localhost:' + port + '/graphql')
   })
 }
 
