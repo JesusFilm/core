6e44549a60a6b4f5f58d0cbc80a3cc147601970d
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-12-22 01:19:59 +0000

    update firebase client

--- apps/api-journeys/src/lib/firebaseClient.ts
+++ apps/api-journeys/src/lib/firebaseClient.ts
@@ -1,12 +1,12 @@
 import * as admin from 'firebase-admin'
 
-export const firebaseClient = admin.initializeApp({
-  credential: admin.credential.cert(
-    JSON.parse(
-      process.env.GOOGLE_APPLICATION_JSON != null &&
-        process.env.GOOGLE_APPLICATION_JSON !== ''
-        ? process.env.GOOGLE_APPLICATION_JSON
-        : '{}'
-    )
-  )
-})
+export const firebaseClient = admin.initializeApp(
+  process.env.GOOGLE_APPLICATION_JSON != null &&
+    process.env.GOOGLE_APPLICATION_JSON !== ''
+    ? {
+        credential: admin.credential.cert(
+          JSON.parse(process.env.GOOGLE_APPLICATION_JSON)
+        )
+      }
+    : undefined
+)
