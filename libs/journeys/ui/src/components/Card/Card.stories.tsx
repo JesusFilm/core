913c448ff078c54acd60089b26ffb1e25eec4e73
John Geronimo <johnageronimo1@gmail.com>
2021-12-23 22:34:45 +0000

    Testing a higher threshold

--- libs/journeys/ui/src/components/Card/Card.stories.tsx
+++ libs/journeys/ui/src/components/Card/Card.stories.tsx
@@ -210,7 +210,7 @@ VideoCover.args = {
   ]
 }
 VideoCover.parameters = {
-  chromatic: { delay: 400, diffThreshold: 0.75 }
+  chromatic: { delay: 400, diffThreshold: 0.8 }
 }
 
 export const ImageBlur: Story<TreeBlock<CardFields>> = Template.bind({})
