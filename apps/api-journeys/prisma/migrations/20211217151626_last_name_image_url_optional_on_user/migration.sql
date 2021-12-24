53ec2dfc0b354fca8c5272583cb9b7bb615974f7
Mike Allison <mike@captainofbass.com>
2021-12-24 22:09:47 +0000

    merge remote main

--- apps/api-journeys/prisma/migrations/20211217151626_last_name_image_url_optional_on_user/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "lastName" DROP NOT NULL,
-ALTER COLUMN "imageUrl" DROP NOT NULL;
--- apps/api-journeys/prisma/migrations/20211221003440_id_added_to_user_journey/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The required column `id` was added to the `UserJourney` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
-
-*/
--- AlterTable
-ALTER TABLE "UserJourney" ADD COLUMN     "id" TEXT NOT NULL,
-ADD CONSTRAINT "UserJourney_pkey" PRIMARY KEY ("id");
