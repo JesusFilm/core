27228a4d6124827ca6e5f2a52b0349da926d6861
John Geronimo <johnageronimo1@gmail.com>
2022-01-11 22:44:04 +0000

    Added storybook and unit tests

---
+++ apps/journeys-admin/src/components/Editor/Add/Add.stories.tsx
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/react'
+import { journeysAdminConfig } from '../../../libs/storybook'
+import { Add } from '.'
+
+const AddButtonStory = {
+  ...journeysAdminConfig,
+  component: Add,
+  title: 'Journeys-Admin/Editor/Add'
+}
+
+const Template: Story = () => {
+  return <Add />
+}
+
+export const Default = Template.bind({})
+
+export default AddButtonStory as Meta
--- apps/journeys-admin/src/components/Editor/Add/Add.tsx
+++ apps/journeys-admin/src/components/Editor/Add/Add.tsx
@@ -4,7 +4,10 @@ import AddRounded from '@mui/icons-material/AddRounded'
 import { ActiveTab, EditorContext } from '../Context'
 
 export function Add(): ReactElement {
-  const { state: { activeTab }, dispatch } = useContext(EditorContext)
+  const {
+    state: { activeTab },
+    dispatch
+  } = useContext(EditorContext)
 
   const handleClick = (): void => {
     dispatch({ type: 'SetActiveTabAction', activeTab: ActiveTab.Blocks })
@@ -12,21 +15,23 @@ export function Add(): ReactElement {
 
   return activeTab === ActiveTab.Blocks ? (
     <></>
-  ) : (<Button
-    variant="contained"
-    size="small"
-    startIcon={<AddRounded />}
-    onClick={handleClick}
-    sx={{
-      position: 'absolute',
-      zIndex: 'modal',
-      width: 105,
-      height: 48,
-      boxShadow: 5,
-      right: 20,
-      bottom: 260
-    }}
-  >
-    ADD
-  </Button>)
+  ) : (
+    <Button
+      variant="contained"
+      size="small"
+      startIcon={<AddRounded />}
+      onClick={handleClick}
+      sx={{
+        position: 'absolute',
+        zIndex: 'modal',
+        width: 105,
+        height: 48,
+        boxShadow: 5,
+        right: 20,
+        bottom: 260
+      }}
+    >
+      ADD
+    </Button>
+  )
 }
