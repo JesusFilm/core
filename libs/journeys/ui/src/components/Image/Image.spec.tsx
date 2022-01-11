82510ee33fd03400c9cd5ade624ecb329fb4fdbb
jianwei1 <jianweichong08@gmail.com>
2022-01-11 00:40:46 +0000

    fixed a test

--- libs/journeys/ui/src/components/Image/Image.spec.tsx
+++ libs/journeys/ui/src/components/Image/Image.spec.tsx
@@ -7,7 +7,7 @@ describe('Image', () => {
   const block: TreeBlock<ImageFields> = {
     __typename: 'ImageBlock',
     id: 'Image1',
-    src: 'https://images.unsplash.c om/photo-1600133153574-25d98a99528c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
+    src: 'https://images.unsplash.com/photo-1600133153574-25d98a99528c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
     width: 1600,
     height: 1067,
     alt: 'random image from unsplash',
