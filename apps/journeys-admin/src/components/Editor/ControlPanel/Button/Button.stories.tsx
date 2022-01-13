eaac2f52b129cb0d5782be5981ccfb275aa53fdd
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-01-13 00:29:55 +0000

    add button to control panel

---
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import { Story, Meta } from '@storybook/react'
+import PaletteIcon from '@mui/icons-material/Palette'
+import { journeysAdminConfig } from '../../../../libs/storybook'
+import { Button } from '.'
+
+const ButtonStory = {
+  ...journeysAdminConfig,
+  component: Button,
+  title: 'Journeys-Admin/Editor/ControlPanel/Button'
+}
+
+export const Default: Story = () => {
+  return (
+    <Button
+      icon={<PaletteIcon />}
+      name="Style"
+      value="Dark"
+      description="Card Styling"
+    />
+  )
+}
+
+export const Selected: Story = () => {
+  return (
+    <Button
+      icon={<PaletteIcon />}
+      name="Style"
+      value="Dark"
+      description="Card Styling"
+      selected
+    />
+  )
+}
+
+export default ButtonStory as Meta
