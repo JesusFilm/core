4c95ab77940151b6e0476bf323f76d4f7fcc2962
Siyang <csiyang@yahoo.co.nz>
2022-01-14 01:01:20 +0000

    base component and stories

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Button.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Button.stories.tsx
@@ -1,15 +1,43 @@
 import { Story, Meta } from '@storybook/react'
-import { journeysAdminConfig } from '../../../../../../libs/storybook'
+import { TreeBlock } from '@core/journeys/ui'
+import { Stack } from '@mui/material'
+import { simpleComponentConfig } from '../../../../../../libs/storybook'
+import { GetJourneyForEdit_journey_blocks_ButtonBlock as ButtonBlock } from '../../../../../../../__generated__/GetJourneyForEdit'
 import { Button } from '.'
 
 const ButtonStory = {
-  ...journeysAdminConfig,
+  ...simpleComponentConfig,
   component: Button,
-  title: 'Journeys-Admin/Editor/ControlPanel/Attributes/Attribute'
+  title: 'Journeys-Admin/Editor/ControlPanel/Attributes/Button'
 }
 
 export const Default: Story = () => {
-  return <Button />
+  const block: TreeBlock<ButtonBlock> = {
+    id: 'button.id',
+    __typename: 'ButtonBlock',
+    parentBlockId: 'step1.id',
+    label: 'Button',
+    buttonVariant: null,
+    buttonColor: null,
+    size: null,
+    startIcon: null,
+    endIcon: null,
+    action: null,
+    children: []
+  }
+  return (
+    <Stack
+      direction="row"
+      spacing={4}
+      sx={{
+        overflowX: 'auto',
+        py: 5,
+        px: 6
+      }}
+    >
+      <Button {...block} />
+    </Stack>
+  )
 }
 
 export default ButtonStory as Meta
--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Button.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Button.tsx
@@ -1,9 +1,99 @@
 import { ReactElement } from 'react'
+import { TreeBlock } from '@core/journeys/ui'
+import ArrowDropDownCircleRoundedIcon from '@mui/icons-material/ArrowDropDownCircleRounded'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import { GetJourneyForEdit_journey_blocks_ButtonBlock as ButtonBlock } from '../../../../../../../__generated__/GetJourneyForEdit'
+import { Attribute } from '../..'
 
-export function Button(): ReactElement {
+export function Button({
+  id,
+  label,
+  buttonVariant,
+  buttonColor,
+  size,
+  startIcon,
+  endIcon,
+  action
+}: TreeBlock<ButtonBlock>): ReactElement {
   return (
-    <div>
-      <p>Button</p>
-    </div>
+    <>
+      <Attribute
+        id={`${id}-size`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Size"
+        value={size?.toString() ?? 'None'}
+        description="Button Size"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-start-icon`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Start Icon"
+        value={startIcon?.toString() ?? 'None'}
+        description="Button Start Icon"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-end-icon`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="End Icon"
+        value={endIcon?.toString() ?? 'None'}
+        description="Button End Icon"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-color`}
+        icon={
+          <Paper sx={{ borderRadius: 1000, overflow: 'hidden' }}>
+            <Box
+              data-testid="backgroundColorIcon"
+              sx={{
+                width: 25,
+                height: 25,
+                m: 1,
+                borderRadius: 1000,
+                backgroundColor: (theme) =>
+                  buttonColor ?? theme.palette.text.primary
+              }}
+            />
+          </Paper>
+        }
+        name="Color"
+        value={buttonColor?.toString() ?? 'None'}
+        description="Button Color"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-label`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Label"
+        value={label}
+        description="Button Label"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-variant`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Variant"
+        value={buttonVariant?.toString() ?? 'None'}
+        description="Button Variant"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-link-action`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Link Action"
+        value={action?.toString() ?? 'None'}
+        description="Button Link Action"
+        // onClick to open drawer
+      />
+    </>
   )
 }
