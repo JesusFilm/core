d5e1ac78f836c1afa043451ea2c5e123b4c0677d
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-01-17 14:28:59 +1300

    Attributes for typography block (#193)
    - Added attributes for for typography block edit
    - Added dynamic description depending on what block is being edited

---
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Typography/Typography.stories.tsx
@@ -0,0 +1,72 @@
+import { Story, Meta } from '@storybook/react'
+import { TreeBlock } from '@core/journeys/ui'
+import Stack from '@mui/material/Stack'
+import { simpleComponentConfig } from '../../../../../../libs/storybook'
+import { GetJourneyForEdit_journey_blocks_TypographyBlock as TypographyBlock } from '../../../../../../../__generated__/GetJourneyForEdit'
+import {
+  TypographyAlign,
+  TypographyColor,
+  TypographyVariant
+} from '../../../../../../../__generated__/globalTypes'
+import { Typography } from '.'
+
+const TypographyStory = {
+  ...simpleComponentConfig,
+  component: Typography,
+  title: 'Journeys-Admin/Editor/ControlPanel/Attributes/Typography'
+}
+
+export const Default: Story = () => {
+  const block: TreeBlock<TypographyBlock> = {
+    id: 'typography1.id',
+    __typename: 'TypographyBlock',
+    parentBlockId: null,
+    align: null,
+    color: null,
+    content: 'Typography',
+    variant: null,
+    children: []
+  }
+
+  return (
+    <Stack
+      direction="row"
+      spacing={4}
+      sx={{
+        overflowX: 'auto',
+        py: 5,
+        px: 6
+      }}
+    >
+      <Typography {...block} />
+    </Stack>
+  )
+}
+export const Filled: Story = () => {
+  const block: TreeBlock<TypographyBlock> = {
+    id: 'typography1.id',
+    __typename: 'TypographyBlock',
+    parentBlockId: null,
+    align: TypographyAlign.center,
+    color: TypographyColor.secondary,
+    content: 'Typography',
+    variant: TypographyVariant.h2,
+    children: []
+  }
+
+  return (
+    <Stack
+      direction="row"
+      spacing={4}
+      sx={{
+        overflowX: 'auto',
+        py: 5,
+        px: 6
+      }}
+    >
+      <Typography {...block} />
+    </Stack>
+  )
+}
+
+export default TypographyStory as Meta
