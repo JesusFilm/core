7eb8f80db2b3428cc8c1952f837d6f7ced3b0a8b
Siyang <csiyang@yahoo.co.nz>
2022-01-17 02:15:44 +0000

    created signUp block for attributes

---
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/SignUp/SignUp.tsx
@@ -0,0 +1,42 @@
+import { TreeBlock } from '@core/journeys/ui'
+import { ReactElement } from 'react'
+import ArrowDropDownCircleRoundedIcon from '@mui/icons-material/ArrowDropDownCircleRounded'
+import { Attribute } from '../..'
+import { GetJourneyForEdit_journey_blocks_SignUpBlock as SignUpBlock } from '../../../../../../../__generated__/GetJourneyForEdit'
+
+export function SignUp({
+  id,
+  submitLabel,
+  action,
+  submitIcon
+}: TreeBlock<SignUpBlock>): ReactElement {
+  return (
+    <>
+      <Attribute
+        id={`${id}-submit-label`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Submit Label"
+        value={submitLabel ?? 'None'}
+        description="Submit Label"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-submit-action`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Submit Action"
+        value={action?.__typename.toString() ?? 'None'}
+        description="Submit Action"
+        // onClick to open drawer
+      />
+
+      <Attribute
+        id={`${id}-submit-icon`}
+        icon={<ArrowDropDownCircleRoundedIcon />}
+        name="Submit Icon"
+        value={submitIcon?.name.toString() ?? 'None'}
+        description="Submit Icon"
+      />
+    </>
+  )
+}
