472413bac86922e1f2cd58f9cdac2c0eb6691001
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-01-18 00:58:15 +0000

    Add SignUp, Video and Image to cards

--- apps/journeys-admin/src/components/Editor/Editor.stories.tsx
+++ apps/journeys-admin/src/components/Editor/Editor.stories.tsx
@@ -304,25 +304,92 @@ const blocks: GetJourneyForEdit_journey_blocks[] = [
     id: 'card4.id',
     __typename: 'CardBlock',
     parentBlockId: 'step4.id',
-    coverBlockId: 'image4.id',
+    coverBlockId: null,
     backgroundColor: null,
     themeMode: null,
     themeName: null,
     fullscreen: false
   },
   {
-    id: 'typographyBlockId9',
-    __typename: 'TypographyBlock',
+    __typename: 'VideoBlock',
+    id: 'Video1',
     parentBlockId: 'card4.id',
+    autoplay: false,
+    muted: true,
+    title: 'video1',
+    videoContent: {
+      __typename: 'VideoArclight',
+      src: 'https://arc.gt/hls/2_0-FallingPlates/529'
+    },
+    startAt: null,
+    endAt: null,
+    posterBlockId: null
+  },
+  {
+    id: 'step5.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    locked: false,
+    nextBlockId: null
+  },
+  {
+    id: 'card5.id',
+    __typename: 'CardBlock',
+    parentBlockId: 'step5.id',
+    coverBlockId: null,
+    backgroundColor: null,
+    themeMode: null,
+    themeName: null,
+    fullscreen: false
+  },
+  {
+    __typename: 'ImageBlock',
+    id: 'Image1',
+    src: 'https://images.unsplash.com/photo-1600133153574-25d98a99528c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80',
+    width: 1600,
+    height: 1067,
+    alt: 'random image from unsplash',
+    parentBlockId: 'card5.id',
+    blurhash: 'L9AS}j^-0dVC4Tq[=~PATeXSV?aL'
+  },
+  {
+    __typename: 'SignUpBlock',
+    id: 'SignUp1',
+    parentBlockId: 'card5.id',
+    submitIcon: null,
+    submitLabel: null,
+    action: null
+  },
+  {
+    id: 'step6.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    locked: false,
+    nextBlockId: null
+  },
+  {
+    id: 'card6.id',
+    __typename: 'CardBlock',
+    parentBlockId: 'step6.id',
+    coverBlockId: 'image6.id',
+    backgroundColor: null,
+    themeMode: null,
+    themeName: null,
+    fullscreen: false
+  },
+  {
+    id: 'typographyBlockId1',
+    __typename: 'TypographyBlock',
+    parentBlockId: 'card6.id',
     align: null,
     color: null,
     content: 'a quote',
     variant: TypographyVariant.overline
   },
   {
-    id: 'typographyBlockId10',
+    id: 'typographyBlockId12',
     __typename: 'TypographyBlock',
-    parentBlockId: 'card4.id',
+    parentBlockId: 'card6.id',
     align: null,
     color: null,
     content:
@@ -330,28 +397,28 @@ const blocks: GetJourneyForEdit_journey_blocks[] = [
     variant: TypographyVariant.subtitle1
   },
   {
-    id: 'typographyBlockId11',
+    id: 'typographyBlockId13',
     __typename: 'TypographyBlock',
-    parentBlockId: 'card4.id',
+    parentBlockId: 'card6.id',
     align: null,
     color: null,
     content: '–  The Bible, John 3:17',
     variant: TypographyVariant.caption
   },
   {
-    id: 'image4.id',
+    id: 'image6.id',
     __typename: 'ImageBlock',
     src: 'https://images.unsplash.com/photo-1601142634808-38923eb7c560?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     width: 1920,
     height: 1080,
     alt: 'random image from unsplash',
-    parentBlockId: 'card4.id',
+    parentBlockId: 'card6.id',
     blurhash: 'LFALX]%g4Tf+?^jEMxo#00Mx%gjZ'
   },
   {
     __typename: 'ButtonBlock',
-    id: 'button2',
-    parentBlockId: 'card4.id',
+    id: 'button',
+    parentBlockId: 'card6.id',
     label: 'Start Over',
     buttonVariant: ButtonVariant.contained,
     buttonColor: ButtonColor.primary,
@@ -366,7 +433,7 @@ const blocks: GetJourneyForEdit_journey_blocks[] = [
     action: {
       __typename: 'NavigateToBlockAction',
       gtmEventName: 'gtmEventName',
-      blockId: 'step0.id'
+      blockId: 'step6.id'
     }
   }
 ]
