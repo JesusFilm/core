8fcb541fffd9482b63e519cd222ffffdb2c9d14a
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-01-20 21:16:36 +0000

    start adding sidebar

--- apps/journeys-admin/src/components/AppBar/AppBar.tsx
+++ apps/journeys-admin/src/components/AppBar/AppBar.tsx
@@ -3,8 +3,14 @@ import MuiAppBar from '@mui/material/AppBar'
 import IconButton from '@mui/material/IconButton'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
 import Link from 'next/link'
 import ChevronLeftRounded from '@mui/icons-material/ChevronLeftRounded'
+import ExploreRoundedIcon from '@mui/icons-material/ExploreRounded'
 
 export interface AppBarProps {
   backHref?: string
@@ -25,8 +31,14 @@ export function AppBar({
         position="fixed"
         color="default"
         sx={{
+          ml: { sm: '72px' },
           mr: { sm: showDrawer === true ? '328px' : 0 },
-          width: { sm: showDrawer === true ? 'calc(100% - 328px)' : '100%' }
+          width: {
+            sm:
+              showDrawer === true
+                ? 'calc(100% - 72px - 328px)'
+                : 'calc(100% - 72px)'
+          }
         }}
       >
         <Toolbar>
@@ -55,9 +67,33 @@ export function AppBar({
       </MuiAppBar>
       <Toolbar
         sx={{
+          ml: { sm: '72px' },
           mr: { sm: showDrawer === true ? '328px' : 0 }
         }}
       />
+      <Drawer
+        sx={{
+          width: '72px',
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: '72px',
+            boxSizing: 'border-box',
+            backgroundColor: '#25262E'
+          }
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar sx={{ border: 'transparent' }} />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <ExploreRoundedIcon />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+        <Divider sx={{ borderColor: '#383940' }} />
+      </Drawer>
     </>
   )
 }
