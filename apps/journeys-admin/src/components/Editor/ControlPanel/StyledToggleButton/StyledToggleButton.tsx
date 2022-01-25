621dbb4fcb809683aed3e33d0682e671e14a0a2e
Siyang <csiyang@yahoo.co.nz>
2022-01-25 21:52:33 +0000

    added some comments for color updated

--- apps/journeys-admin/src/components/Editor/ControlPanel/StyledToggleButton/StyledToggleButton.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/StyledToggleButton/StyledToggleButton.tsx
@@ -2,11 +2,11 @@ import { styled } from '@mui/material/styles'
 import ToggleButton from '@mui/material/ToggleButton'
 import { palette } from '../../../ThemeProvider/admin/tokens/colors'
 
-export const StyledToggleButton = styled(ToggleButton)(({ theme }) => ({
+export const StyledToggleButton = styled(ToggleButton)(() => ({
   textTransform: 'none',
-  backgroundColor: palette[0],
+  backgroundColor: palette[0], // 'primary.contastText'
   backgroundClip: 'padding-box',
-  color: palette[900],
+  color: palette[900], // 'secondary.dark'
   '&:first-of-type': {
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12
@@ -16,9 +16,9 @@ export const StyledToggleButton = styled(ToggleButton)(({ theme }) => ({
     borderBottomRightRadius: 12
   },
   '&.Mui-selected': {
-    backgroundColor: palette[100]
+    backgroundColor: palette[100] // 'background.default'
   },
   '&.hover': {
-    backgroundColor: palette[200]
+    backgroundColor: palette[200] // 'divider'
   }
 }))
