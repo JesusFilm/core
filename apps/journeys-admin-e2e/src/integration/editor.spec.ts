beef048ef57bf0db5a0d217be9d82baa86101725
jianwei1 <jianweichong08@gmail.com>
2022-01-26 21:56:37 +0000

    commiting

--- apps/journeys-admin-e2e/src/integration/editor.spec.ts
+++ apps/journeys-admin-e2e/src/integration/editor.spec.ts
@@ -1,3 +1,5 @@
+import 'cypress-iframe'
+
 describe('fact-or-fiction edit', () => {
   before(() => {
     cy.visit('/journeys/fact-or-fiction/edit')
@@ -35,11 +37,41 @@ describe('fact-or-fiction edit', () => {
   })
 
   // const nestedCard = '67a7feec-1699-40a5-9f28-22e473ed9348'
+  // const iframeParent = 'step-7a6c9d6a-3894-48da-9e30-27321b15402a'
+  it('gets iframe for card', () => {
+    // const iframeParent = 'step-' + card3
+    cy.get(`[data-testid="${'step-' + card3}"]`).within(() => {
+      cy.get('iframe')
+        .its('0.contentDocument')
+        .should('exist')
+        .its('body')
+        .should('not.be.undefined')
+        .then(cy.wrap)
+        .get('h6')
+        .should('exist')
+      // cy.frameLoaded()
+    })
+  })
+
+  // const getIframeDocument = () => {
+  //   return cy
+  //   .get('iframe')
+  //   .its('0.contentDocument').should('exist')
+  // }
+
+  // const getIframeBody = () => {
+  //   return getIframeDocument()
+  //   .its('body').should('not.be.undefined')
+  //   .then(cy.wrap)
+  // }
   it('should switch to block properties when a block on the card is clicked', () => {
     // navigate back to cards tab
     cy.get('button[role="tab"]').contains('Cards').click()
+
+    // get the iframe of the card, so we can click on block elements within it
+
     // click a block on the displayed card
-    cy.get('h6').contains('What do you think?')
+    // cy.get('h6').contains('What do you think?')
     // cy.get(`[id="${nestedCard}"]`)
   })
 })
