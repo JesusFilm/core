36ea6f41531e99efdd43f70f1f9af4a4d9a628df
John Geronimo <johnageronimo1@gmail.com>
2022-01-26 21:37:13 +0000

    Fix AQL query for blockservice

--- apps/api-journeys/src/app/modules/block/block.service.ts
+++ apps/api-journeys/src/app/modules/block/block.service.ts
@@ -9,14 +9,10 @@ import { Block, Journey } from '../../__generated__/graphql'
 export class BlockService extends BaseService {
   async forJourney(journey: Journey): Promise<Block[]> {
     const primaryImageBlockId = journey.primaryImageBlock?.id
-    const ignorePrimaryImageBlock =
-      primaryImageBlockId !== null
-        ? `AND block.id != ${primaryImageBlockId ?? 'null'}`
-        : ''
     const res = await this.db.query(aql`
       FOR block in ${this.collection}
         FILTER block.journeyId == ${journey.id}
-          AND block.journeyId != ${ignorePrimaryImageBlock}
+          AND block.id != ${primaryImageBlockId ?? null}
         SORT block.parentOrder ASC
         RETURN block
     `)
@@ -28,14 +24,20 @@ export class BlockService extends BaseService {
     parentBlockId?: string | null
   ): Promise<Block[]> {
     // Only StepBlocks should not have parentBlockId
-    const filterParentBlockId =
-      parentBlockId != null ? `AND block.parentBlockId == ${parentBlockId}` : ''
-    const res = await this.db.query(aql`
-      FOR block in ${this.collection}
-        FILTER block.journeyId == ${journeyId}
-          AND ${filterParentBlockId}
-        SORT block.parentOrder ASC
-        RETURN block
+    const res =
+      parentBlockId != null
+        ? await this.db.query(aql`
+        FOR block in ${this.collection}
+          FILTER block.journeyId == ${journeyId}
+            AND block.parentBlockId == ${parentBlockId}
+          SORT block.parentOrder ASC
+          RETURN block
+    `)
+        : await this.db.query(aql`
+        FOR block in ${this.collection}
+          FILTER block.journeyId == ${journeyId}
+          SORT block.parentOrder ASC
+          RETURN block
     `)
     return await res.all()
   }
