7fbb7461f802c43dbe70fe467ec3bb3bc5d70da2
Siyang <csiyang@yahoo.co.nz>
2022-01-31 22:53:34 +0000

    front end mutation to change button size

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.tsx
@@ -2,16 +2,37 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import { ReactElement, useState } from 'react'
 import capitalize from 'lodash/capitalize'
 import Typography from '@mui/material/Typography'
+import { gql, useMutation } from '@apollo/client'
 import { ButtonSize } from '../../../../../../../../__generated__/globalTypes'
 import { StyledToggleButton } from '../../../../StyledToggleButton'
+import { ButtonBlockUpdateSize } from '../../../../../../../../__generated__/ButtonBlockUpdateSize'
+import { useJourney } from '../../../../../../../libs/context'
 
 interface SizeProps {
   id: string
   size: ButtonSize | null
 }
 
+export const BUTTON_BLOCK_UPDATE = gql`
+  mutation ButtonBlockUpdateSize(
+    $id: ID!
+    $journeyId: ID!
+    $input: ButtonBlockUpdateInput!
+  ) {
+    buttonBlockUpdate(id: $id, journeyId: $journeyId, input: $input) {
+      id
+      size
+    }
+  }
+`
+
 export function Size({ id, size }: SizeProps): ReactElement {
+  const [buttonBlockUpdate] =
+    useMutation<ButtonBlockUpdateSize>(BUTTON_BLOCK_UPDATE)
+
+  const journey = useJourney()
   const [selected, setSelected] = useState(size ?? ButtonSize.medium)
+
   const order = ['small', 'medium', 'large']
   const sorted = Object.values(ButtonSize).sort(
     (a, b) => order.indexOf(a) - order.indexOf(b)
@@ -22,6 +43,13 @@ export function Size({ id, size }: SizeProps): ReactElement {
     size: ButtonSize
   ): Promise<void> {
     if (size != null) {
+      await buttonBlockUpdate({
+        variables: {
+          id,
+          journeyId: journey.id,
+          input: { size }
+        }
+      })
       setSelected(size)
     }
   }
