dfd7ed17a2684d6fc8b4b2927c6300497162d659
Siyang <csiyang@yahoo.co.nz>
2022-01-31 22:48:59 +0000

    updated stories and tests

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Color/Color.spec.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Color/Color.spec.tsx
@@ -1,20 +1,55 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import { BUTTON_BLOCK_UPDATE } from './Color'
 import { Color } from '.'
 
 describe('Button color selector', () => {
   it('should show button color properties', () => {
-    const { getByRole } = render(<Color id={'button-color-id'} color={null} />)
+    const { getByRole } = render(
+      <MockedProvider>
+        <Color id={'button-color-id'} color={null} />
+      </MockedProvider>
+    )
     expect(getByRole('button', { name: 'Default' })).toBeInTheDocument()
     expect(getByRole('button', { name: 'Primary' })).toHaveClass('Mui-selected')
     expect(getByRole('button', { name: 'Secondary' })).toBeInTheDocument()
     expect(getByRole('button', { name: 'Error' })).toBeInTheDocument()
   })
-  it('should change the color property', () => {
-    const { getByRole } = render(<Color id={'button-color-id'} color={null} />)
+  it('should change the color property', async () => {
+    const { getByRole } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: BUTTON_BLOCK_UPDATE,
+              variables: {
+                id: 'button-color-id',
+                journeyId: undefined,
+                input: {
+                  color: 'secondary'
+                }
+              }
+            },
+            result: {
+              data: {
+                buttonBlockUpdate: {
+                  id: 'button-color-id',
+                  color: 'secondary'
+                }
+              }
+            }
+          }
+        ]}
+      >
+        <Color id={'button-color-id'} color={null} />
+      </MockedProvider>
+    )
     expect(getByRole('button', { name: 'Primary' })).toHaveClass('Mui-selected')
     fireEvent.click(getByRole('button', { name: 'Secondary' }))
-    expect(getByRole('button', { name: 'Secondary' })).toHaveClass(
-      'Mui-selected'
+    await waitFor(() =>
+      expect(getByRole('button', { name: 'Secondary' })).toHaveClass(
+        'Mui-selected'
+      )
     )
   })
 })
--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Color/Color.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Color/Color.stories.tsx
@@ -1,5 +1,7 @@
 import { Story, Meta } from '@storybook/react'
+import { MockedProvider } from '@apollo/client/testing'
 import { simpleComponentConfig } from '../../../../../../../libs/storybook'
+import { BUTTON_BLOCK_UPDATE } from './Color'
 import { Color } from '.'
 
 const ColorStory = {
@@ -9,7 +11,74 @@ const ColorStory = {
 }
 
 export const Default: Story = () => {
-  return <Color id={'button-color-id'} color={null} />
+  return (
+    <MockedProvider
+      mocks={[
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-color-id',
+              journeyId: undefined,
+              input: {
+                color: 'secondary'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-color-id',
+                color: 'secondary'
+              }
+            }
+          }
+        },
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-color-id',
+              journeyId: undefined,
+              input: {
+                color: 'error'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-color-id',
+                color: 'error'
+              }
+            }
+          }
+        },
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-color-id',
+              journeyId: undefined,
+              input: {
+                color: 'inherit'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-color-id',
+                color: 'inherit'
+              }
+            }
+          }
+        }
+      ]}
+    >
+      <Color id={'button-color-id'} color={null} />
+    </MockedProvider>
+  )
 }
 
 export default ColorStory as Meta
