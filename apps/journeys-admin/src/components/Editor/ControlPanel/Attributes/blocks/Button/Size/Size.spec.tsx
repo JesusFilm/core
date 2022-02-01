95c529e01420b4c103ef69ea2453ebf6cb70875b
Siyang <csiyang@yahoo.co.nz>
2022-02-01 00:24:42 +0000

    added test and stories for size

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.spec.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.spec.tsx
@@ -1,17 +1,52 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import { BUTTON_BLOCK_UPDATE } from './Size'
 import { Size } from '.'
 
 describe('Button size selector', () => {
   it('should show button size properties', () => {
-    const { getByRole } = render(<Size id={'button-size-id'} size={null} />)
+    const { getByRole } = render(
+      <MockedProvider>
+        <Size id={'button-size-id'} size={null} />
+      </MockedProvider>
+    )
     expect(getByRole('button', { name: 'Small' })).toBeInTheDocument()
     expect(getByRole('button', { name: 'Medium' })).toHaveClass('Mui-selected')
     expect(getByRole('button', { name: 'Large' })).toBeInTheDocument()
   })
-  it('should change the size property', () => {
-    const { getByRole } = render(<Size id={'typography-size-id'} size={null} />)
+  it('should change the size property', async () => {
+    const { getByRole } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: BUTTON_BLOCK_UPDATE,
+              variables: {
+                id: 'button-size-id',
+                journeyId: undefined,
+                input: {
+                  size: 'small'
+                }
+              }
+            },
+            result: {
+              data: {
+                buttonBlockUpdate: {
+                  id: 'button-size-id',
+                  size: 'small'
+                }
+              }
+            }
+          }
+        ]}
+      >
+        <Size id={'button-size-id'} size={null} />
+      </MockedProvider>
+    )
     expect(getByRole('button', { name: 'Medium' })).toHaveClass('Mui-selected')
-    fireEvent.click(getByRole('button', { name: 'Large' }))
-    expect(getByRole('button', { name: 'Large' })).toHaveClass('Mui-selected')
+    fireEvent.click(getByRole('button', { name: 'Small' }))
+    await waitFor(() =>
+      expect(getByRole('button', { name: 'Small' })).toHaveClass('Mui-selected')
+    )
   })
 })
--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Size/Size.stories.tsx
@@ -1,5 +1,7 @@
 import { Story, Meta } from '@storybook/react'
+import { MockedProvider } from '@apollo/client/testing'
 import { simpleComponentConfig } from '../../../../../../../libs/storybook'
+import { BUTTON_BLOCK_UPDATE } from './Size'
 import { Size } from '.'
 
 const SizeStory = {
@@ -9,7 +11,74 @@ const SizeStory = {
 }
 
 export const Default: Story = () => {
-  return <Size id={'button-size-id'} size={null} />
+  return (
+    <MockedProvider
+      mocks={[
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-size-id',
+              journeyId: undefined,
+              input: {
+                size: 'small'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-size-id',
+                size: 'small'
+              }
+            }
+          }
+        },
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-size-id',
+              journeyId: undefined,
+              input: {
+                size: 'large'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-size-id',
+                size: 'large'
+              }
+            }
+          }
+        },
+        {
+          request: {
+            query: BUTTON_BLOCK_UPDATE,
+            variables: {
+              id: 'button-size-id',
+              journeyId: undefined,
+              input: {
+                size: 'inherit'
+              }
+            }
+          },
+          result: {
+            data: {
+              buttonBlockUpdate: {
+                id: 'button-size-id',
+                size: 'inherit'
+              }
+            }
+          }
+        }
+      ]}
+    >
+      <Size id={'button-size-id'} size={null} />
+    </MockedProvider>
+  )
 }
 
 export default SizeStory as Meta
