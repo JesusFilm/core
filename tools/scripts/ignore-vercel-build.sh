e580ac99601c1297b34f0c6c65bc3279cba626e0
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-02-01 02:43:16 +0000

    revert to doc format

--- tools/scripts/ignore-vercel-build.sh
+++ tools/scripts/ignore-vercel-build.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-# This script is used to ignore the vercel build. It should have a ENV APP_NAME set to the project name in Nx.
-npm install -g npm@latest
-
 # Determine version of Nx installed
 NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
 npm install -D @nrwl/workspace@$NX_VERSION --prefer-offline
+npm install -D typescript@$TS_VERSION --prefer-offline
 
 # Run the affected command, comparing latest commit to the one before that
-AFFECTED=$(npx nx affected:apps --plain --base HEAD~1 --head HEAD) 
-
-echo "$AFFECTED" | grep $APP_NAME -q
+npx nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q
 
 # Store result of the previous command (grep)
 IS_AFFECTED=$?
 
-echo "APP_NAME: $APP_NAME"
-echo "AFFECTED: $AFFECTED"
-echo "NX_VERSION: $NX_VERSION"
-
 if [ $IS_AFFECTED -eq 1 ]; then
-  echo "🛑 - build cancelled"
+  echo "🛑 - Build cancelled"
   exit 0
 elif [ $IS_AFFECTED -eq 0 ]; then
-  echo "✅ - build can proceed"
+  echo "✅ - Build can proceed"
   exit 1
 fi
