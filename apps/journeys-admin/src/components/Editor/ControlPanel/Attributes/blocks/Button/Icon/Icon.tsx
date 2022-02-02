f244f133768fd46c730c34d8239060456994cdc1
Siyang <csiyang@yahoo.co.nz>
2022-02-02 00:18:26 +0000

    moved name list into the main component

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/Icon.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/Icon.tsx
@@ -1,6 +1,12 @@
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { ReactElement, useState, useEffect } from 'react'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import capitalize from 'lodash/capitalize'
+import { Icon as CoreIcon } from '@core/journeys/ui'
 import {
   IconColor,
   IconSize,
@@ -8,7 +14,6 @@ import {
 } from '../../../../../../../../__generated__/globalTypes'
 import { SizeToggleGroup } from './SizeToggleGroup'
 import { ColorToggleGroup } from './ColorToggleGroup'
-import { NameList } from './NameList'
 
 interface IconProps {
   id: string
@@ -23,10 +28,18 @@ export function Icon({
   iconColor,
   iconSize
 }: IconProps): ReactElement {
-  const [checked, setChecked] = useState(iconName != null)
+  const [showProps, setShowProps] = useState(iconName != null)
+  const [name, setName] = useState('')
+
   useEffect(() => {
-    setChecked(iconName != null)
+    setShowProps(iconName != null)
+    setName(iconName != null ? iconName : '')
   }, [iconName])
+
+  function handleChange(event: SelectChangeEvent): void {
+    setName(event.target.value)
+    setShowProps(name === '')
+  }
 
   return (
     <Box sx={{ px: 6 }}>
@@ -35,9 +48,38 @@ export function Icon({
         <Typography variant="caption">Show/Hide Icon on Button</Typography>
       </Box>
 
-      <NameList id={id} name={iconName} disabled={!checked} />
+      <FormControl fullWidth hiddenLabel sx={{ pt: 4, pb: 9 }}>
+        <Select
+          labelId="icon-name-select"
+          id="icon-name-select"
+          value={name}
+          onChange={handleChange}
+          variant="filled"
+          displayEmpty
+          IconComponent={KeyboardArrowDownRoundedIcon}
+          inputProps={{ 'aria-label': 'icon-name-select' }}
+        >
+          <MenuItem value="">Select an icon...</MenuItem>
+          {Object.values(IconName).map((name) => {
+            return (
+              <MenuItem key={`button-icon-name-${name}`} value={name}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                  <CoreIcon
+                    __typename={'Icon'}
+                    name={name}
+                    color={null}
+                    size={null}
+                  />
 
-      {checked && (
+                  <Typography sx={{ pl: 3 }}>{capitalize(name)}</Typography>
+                </Box>
+              </MenuItem>
+            )
+          })}
+        </Select>
+      </FormControl>
+
+      {showProps && (
         <Box>
           <ColorToggleGroup id={id} color={iconColor} />
           <SizeToggleGroup id={id} size={iconSize} />
