a5e9d9ee57e92a1b4acb635348d31f6094deb961
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-02-03 20:13:47 +0000

    add check git status

---
+++ tools/scripts/check-git-status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this script is used by GitHub actions to check if changes were made by
+# linting or codegen commands.
+
+if [[ `git status --porcelain` ]]; then
+  # Changes
+  echo "🛑 - run codegen and linting locally and commit changes"
+  exit 0
+else
+  # No changes
+  echo "✅ - codegen and linting did not generate changes"
+  exit 1
+fi
