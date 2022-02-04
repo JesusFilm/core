479634bc620097d77ac6564353f06c4c06ba2af3
Siyang <csiyang@yahoo.co.nz>
2022-02-04 02:44:06 +0000

    updated how next step is selected and overlay

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateNext/NavigateNext.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateNext/NavigateNext.tsx
@@ -1,22 +1,32 @@
 import { ReactElement } from 'react'
 import { useEditor } from '@core/journeys/ui'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 import { CardPreview } from '../../../../../../../CardPreview'
 
 export function NavigateNext(): ReactElement {
   const { state } = useEditor()
 
-  const selectedIndex = state.steps.findIndex(
-    (step) => step === state.selectedStep
-  )
-  const nextIndex =
-    state.steps.length > selectedIndex + 1 ? selectedIndex + 1 : 0
-
   // update mutation to set action to navigate to next step
+  const nextStep = state.steps.find(
+    (step) => step.id === state.selectedStep?.nextBlockId
+  )
 
   return (
     <>
-      {/* Grey out cards, swiper focus on selected */}
-      <CardPreview selected={state.steps[nextIndex]} steps={state.steps} />
+      {nextStep != null ? (
+        <Box
+          sx={{
+            display: 'absolute',
+            backgroundColor: 'white',
+            opacity: '40%'
+          }}
+        >
+          <CardPreview selected={nextStep} steps={state.steps} />
+        </Box>
+      ) : (
+        <Typography variant="caption">No next step</Typography>
+      )}
     </>
   )
 }
