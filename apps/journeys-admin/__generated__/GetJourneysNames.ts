4ed0153ceac0e22414f24bbac85fb06d4d6f7573
Siyang <csiyang@yahoo.co.nz>
2022-02-04 04:01:51 +0000

    started writting out api

---
+++ apps/journeys-admin/__generated__/GetJourneysNames.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetJourneysNames
+// ====================================================
+
+export interface GetJourneysNames_journeys {
+  __typename: "Journey";
+  id: string;
+  title: string;
+}
+
+export interface GetJourneysNames {
+  journeys: GetJourneysNames_journeys[];
+}
--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateJourney/NavigateJourney.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateJourney/NavigateJourney.tsx
@@ -1,13 +1,27 @@
 import { ReactElement, useState } from 'react'
 import MenuItem from '@mui/material/MenuItem'
+import { gql, useQuery } from '@apollo/client'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { GetJourneysNames } from '../../../../../../../../../__generated__/GetJourneysNames'
+
+const GET_JOURNEYS_NAMES = gql`
+  query GetJourneysNames {
+    journeys: adminJourneys {
+      id
+      title
+    }
+  }
+`
 
 export function NavigateJourney(): ReactElement {
+  const { data } = useQuery<GetJourneysNames>(GET_JOURNEYS_NAMES)
   const [journeyName, setJourneyName] = useState('')
 
   // add query to get all journeys
-  const journeysList = ['hello', 'world']
+  // const journeysList = ['hello', 'world']
+  const journeysList = data.journeys
+  console.log(journeysList)
 
   function handleChange(event: SelectChangeEvent): void {
     setJourneyName(event.target.value)
