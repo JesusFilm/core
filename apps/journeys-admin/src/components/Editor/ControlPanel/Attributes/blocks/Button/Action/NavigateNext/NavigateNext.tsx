da65028811207fcc967537d635c9c43294629140
Siyang <csiyang@yahoo.co.nz>
2022-02-04 01:42:15 +0000

    logic for navigate to next step

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateNext/NavigateNext.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Action/NavigateNext/NavigateNext.tsx
@@ -4,10 +4,19 @@ import { CardPreview } from '../../../../../../../CardPreview'
 
 export function NavigateNext(): ReactElement {
   const { state } = useEditor()
+
+  const selectedIndex = state.steps.findIndex(
+    (step) => step === state.selectedStep
+  )
+  const nextIndex =
+    state.steps.length > selectedIndex + 1 ? selectedIndex + 1 : 0
+
+  // update mutation to set action to navigate to next step
+
   return (
     <>
-      {/* Grey out cards, show next step selected */}
-      <CardPreview selected={state.selectedStep} steps={state.steps} />
+      {/* Grey out cards, swiper focus on selected */}
+      <CardPreview selected={state.steps[nextIndex]} steps={state.steps} />
     </>
   )
 }
