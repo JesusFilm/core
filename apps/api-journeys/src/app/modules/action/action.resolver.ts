240a46dfde0ca1556ab0fd1dde4e8fa4f32274ef
John Geronimo <johnageronimo1@gmail.com>
2022-02-08 01:28:05 +0000

    Implementing type check for actions

--- apps/api-journeys/src/app/modules/action/action.resolver.ts
+++ apps/api-journeys/src/app/modules/action/action.resolver.ts
@@ -1,11 +1,9 @@
 import { ResolveField, Resolver } from '@nestjs/graphql'
 import { get } from 'lodash'
 import { Action } from '../../__generated__/graphql'
-import { BlockService } from '../block/block.service'
 
 @Resolver('Action')
 export class ActionResolver {
-  constructor(private readonly blockService: BlockService) {}
   @ResolveField()
   __resolveType(obj: Action): string {
     if (get(obj, 'blockId') != null) return 'NavigateToBlockAction'
--- apps/api-journeys/src/app/modules/action/linkToAction/linkToAction.resolver.ts
+++ apps/api-journeys/src/app/modules/action/linkToAction/linkToAction.resolver.ts
@@ -1,7 +1,10 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { UseGuards } from '@nestjs/common'
 import { KeyAsId } from '@core/nest/decorators'
+import { includes } from 'lodash'
+import { UserInputError } from 'apollo-server-errors'
 import { RoleGuard } from '../../../lib/roleGuard/roleGuard'
+
 import {
   Action,
   LinkActionInput,
@@ -23,6 +26,17 @@ export class LinkToActionResolver {
     @Args('journeyId') journeyId: string,
     @Args('input') input: LinkActionInput
   ): Promise<Action> {
+    const block = await this.blockService.get<{ __typename: string }>(id)
+
+    if (
+      !includes(
+        ['SignUpBlock', 'RadioOptionBlock', 'ButtonBlock', 'VideoTriggerBlock'],
+        block.__typename
+      )
+    ) {
+      throw new UserInputError('This block does not support link actions.')
+    }
+
     return await this.blockService.update(id, {
       action: { ...input, blockId: null, journeyId: null }
     })
