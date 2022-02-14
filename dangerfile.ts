b738542d5d3af0554d713c2fdc6f3f775f276f72
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-02-14 02:15:13 +0000

    fix: send correct params to GH API

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,16 +1,10 @@
 import { danger, warn, markdown } from 'danger'
 
 export default async () => {
-  const currentPR = await danger.github.api.pulls.get({
-    owner: danger.github.thisPR.owner,
-    repo: danger.github.thisPR.repo,
-    pull_number: danger.github.thisPR.number
-  })
-
+  // check lockfile updated when package changes
   const packageChanged = danger.git.modified_files.includes('package.json')
   const lockfileChanged =
     danger.git.modified_files.includes('package-lock.json')
-
   if (packageChanged && !lockfileChanged) {
     const message =
       'Changes were made to package.json, but not to package-lock.json'
@@ -18,6 +12,7 @@ export default async () => {
     warn(`${message} - <i>${idea}</i>`)
   }
 
+  // check max changes fall below threshold
   const CHANGE_THRESHOLD = 600
   const changeCount = danger.github.pr.additions + danger.github.pr.deletions
   if (changeCount > CHANGE_THRESHOLD) {
@@ -27,30 +22,7 @@ export default async () => {
     )
   }
 
-  if (danger.github.pr.assignee === null) {
-    fail('Please assign someone to merge this PR.')
-  }
-
-  if (
-    !danger.github.issue.labels.some((label) => label.name.includes('type:'))
-  ) {
-    fail('Please add type label to this PR.')
-  }
-
-  if (
-    !danger.github.issue.labels.some((label) =>
-      label.name.includes('priority:')
-    )
-  ) {
-    fail('Please add priority label to this PR.')
-  }
-
-  if (
-    !danger.github.issue.labels.some((label) => label.name.includes('effort:'))
-  ) {
-    fail('Please add effort label to this PR.')
-  }
-
+  // check PR has well formed title
   if (
     danger.github.pr.title.match(
       /^(fix|chore|docs|feature|fix|security|testing): .+/g
@@ -61,10 +33,58 @@ export default async () => {
     )
   }
 
+  // check PR has description
+  if (danger.github.pr.body.length < 10) {
+    fail('This pull request needs a description.')
+  }
+
+  // check PR has basecamp link
+  if (!danger.github.pr.body.includes('https://3.basecamp.com/')) {
+    warn(
+      'Is this PR related to a Basecamp issue? If so link it via the PR description.'
+    )
+  }
+
+  // check PR has assignee
+  if (danger.github.pr.assignee === null) {
+    fail('Please assign someone to merge this PR.')
+  }
+
+  // check PR has type label
+  if (
+    !danger.github.issue.labels.some((label) => label.name.includes('type:'))
+  ) {
+    fail('Please add type label to this PR.')
+  }
+
+  // check PR has priority label
+  if (
+    !danger.github.issue.labels.some((label) =>
+      label.name.includes('priority:')
+    )
+  ) {
+    fail('Please add priority label to this PR.')
+  }
+
+  // check PR has effort label
+  if (
+    !danger.github.issue.labels.some((label) => label.name.includes('effort:'))
+  ) {
+    fail('Please add effort label to this PR.')
+  }
+
+  // pull PR data from GitHub API
+  const currentPR = await danger.github.api.pulls.get({
+    ...danger.github.thisPR,
+    pull_number: danger.github.thisPR.number
+  })
+
+  // check PR has milestone
   if (currentPR.data.milestone === null) {
     fail('Please add milestone to this PR.')
   }
 
+  // check PR has reviewers
   if (currentPR.data.requested_reviewers == null) {
     fail('Please request reviewers for this PR.')
   }
