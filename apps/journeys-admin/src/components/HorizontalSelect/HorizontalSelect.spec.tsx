0d2d20117c84607fe0212282ecc6291b6aea7568
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-02-16 04:24:26 +0000

    fix: horizontal select add should to tests

--- apps/journeys-admin/src/components/HorizontalSelect/HorizontalSelect.spec.tsx
+++ apps/journeys-admin/src/components/HorizontalSelect/HorizontalSelect.spec.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import { HorizontalSelect } from '.'
 
 describe('HorizontalSelect', () => {
-  it('calls onChange when step is clicked on', () => {
+  it('should call onChange when step is clicked on', () => {
     const onChange = jest.fn()
     const { getByText } = render(
       <HorizontalSelect onChange={onChange}>
@@ -15,7 +15,7 @@ describe('HorizontalSelect', () => {
     expect(onChange).toHaveBeenCalledWith('step1.id')
   })
 
-  it('shows border around selected', () => {
+  it('should show border around selected', () => {
     const { getByText } = render(
       <HorizontalSelect onChange={jest.fn()} id="step1.id">
         <Box id="step1.id">Option 1</Box>
@@ -27,7 +27,7 @@ describe('HorizontalSelect', () => {
     )
   })
 
-  it('displays foolter', () => {
+  it('should display foolter', () => {
     const { getByTestId } = render(
       <HorizontalSelect
         onChange={jest.fn()}
