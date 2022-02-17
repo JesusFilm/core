1f3c2dfe27cb84e0aeac1bc379bc9d911af268d4
Siyang <csiyang@yahoo.co.nz>
2022-02-17 02:53:23 +0000

    test: updated stories for icon color

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/ColorToggleGroup/ColorToggleGroup.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/ColorToggleGroup/ColorToggleGroup.stories.tsx
@@ -1,7 +1,9 @@
 import { Story, Meta } from '@storybook/react'
 import { MockedProvider } from '@apollo/client/testing'
+import { TreeBlock } from '@core/journeys/ui'
 import { simpleComponentConfig } from '../../../../../../../../libs/storybook'
-import { IconType } from '..'
+import { IconFields } from '../../../../../../../../../__generated__/IconFields'
+import { IconName } from '../../../../../../../../../__generated__/globalTypes'
 import { ColorToggleGroup } from '.'
 
 const ColorToggleGroupStory = {
@@ -12,9 +14,19 @@ const ColorToggleGroupStory = {
 }
 
 export const Default: Story = () => {
+  const icon: TreeBlock<IconFields> = {
+    id: 'icon-id',
+    parentBlockId: 'buttonBlockId',
+    parentOrder: null,
+    __typename: 'IconBlock',
+    iconName: IconName.ArrowForwardRounded,
+    iconSize: null,
+    iconColor: null,
+    children: []
+  }
   return (
     <MockedProvider>
-      <ColorToggleGroup type={IconType.start} />
+      <ColorToggleGroup iconBlock={icon} />
     </MockedProvider>
   )
 }
