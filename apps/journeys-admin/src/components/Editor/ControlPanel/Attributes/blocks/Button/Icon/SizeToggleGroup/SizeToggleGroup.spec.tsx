e217ffe3eb0ab993eb7ee52b63cc82d9704e0f69
Siyang <csiyang@yahoo.co.nz>
2022-02-17 03:16:44 +0000

    test: added test and stories for icon size

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/SizeToggleGroup/SizeToggleGroup.spec.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/SizeToggleGroup/SizeToggleGroup.spec.tsx
@@ -2,47 +2,51 @@ import { render, fireEvent, waitFor } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import { EditorProvider, TreeBlock } from '@core/journeys/ui'
 import { GetJourney_journey_blocks_ButtonBlock as ButtonBlock } from '../../../../../../../../../__generated__/GetJourney'
-import { IconSize } from '../../../../../../../../../__generated__/globalTypes'
-import { IconType } from '..'
 import {
-  BUTTON_START_ICON_SIZE_UPDATE,
-  BUTTON_END_ICON_SIZE_UPDATE
-} from './SizeToggleGroup'
+  IconSize,
+  IconName
+} from '../../../../../../../../../__generated__/globalTypes'
+import { IconFields } from '../../../../../../../../../__generated__/IconFields'
+import { ICON_BLOCK_SIZE_UPDATE } from './SizeToggleGroup'
 import { SizeToggleGroup } from '.'
 
 describe('Button icon size selector', () => {
   const selectedBlock: TreeBlock<ButtonBlock> = {
     __typename: 'ButtonBlock',
-    id: 'id',
+    id: 'buttonBlockId',
     parentBlockId: 'parentBlockId',
     parentOrder: 0,
     label: 'test button',
     buttonVariant: null,
     buttonColor: null,
     size: null,
-    startIcon: null,
-    endIcon: null,
+    startIconId: 'icon-id',
+    endIconId: null,
     action: null,
     children: []
   }
-  it('should show icon size properties', () => {
-    const { getByRole } = render(
-      <MockedProvider>
-        <EditorProvider initialState={{ selectedBlock }}>
-          <SizeToggleGroup type={IconType.start} />
-        </EditorProvider>
-      </MockedProvider>
-    )
-    expect(getByRole('button', { name: 'Medium' })).toHaveClass('Mui-selected')
-    expect(getByRole('button', { name: 'Large' })).toBeInTheDocument()
-    expect(getByRole('button', { name: 'Small' })).toBeInTheDocument()
-  })
-  it('should change the start icon size property', async () => {
+
+  it('should change the  icon size', async () => {
+    const icon: TreeBlock<IconFields> = {
+      id: 'icon-id',
+      parentBlockId: 'buttonBlockId',
+      parentOrder: null,
+      __typename: 'IconBlock',
+      iconName: IconName.ArrowForwardRounded,
+      iconSize: null,
+      iconColor: null,
+      children: []
+    }
+
     const result = jest.fn(() => ({
       data: {
-        buttonBlockUpdate: {
-          id: 'id',
-          startIcon: { size: IconSize.sm }
+        iconBlockUpdate: {
+          id: 'icon-id',
+          journeyId: 'journeyId',
+          parentBlockId: 'buttonBlockId',
+          name: IconName.ArrowForwardRounded,
+          color: null,
+          size: IconSize.sm
         }
       }
     }))
@@ -52,14 +56,11 @@ describe('Button icon size selector', () => {
         mocks={[
           {
             request: {
-              query: BUTTON_START_ICON_SIZE_UPDATE,
+              query: ICON_BLOCK_SIZE_UPDATE,
               variables: {
                 id: 'id',
-                journeyId: undefined,
                 input: {
-                  startIcon: {
-                    size: IconSize.sm
-                  }
+                  size: IconSize.sm
                 }
               }
             },
@@ -68,47 +69,7 @@ describe('Button icon size selector', () => {
         ]}
       >
         <EditorProvider initialState={{ selectedBlock }}>
-          <SizeToggleGroup type={IconType.start} />
-        </EditorProvider>
-      </MockedProvider>
-    )
-    expect(getByRole('button', { name: 'Medium' })).toHaveClass('Mui-selected')
-    fireEvent.click(getByRole('button', { name: 'Small' }))
-    await waitFor(() => expect(() => expect(result).toHaveBeenCalled()))
-  })
-
-  it('should change the end icon size property', async () => {
-    const result = jest.fn(() => ({
-      data: {
-        buttonBlockUpdate: {
-          id: 'id',
-          endIcon: { size: IconSize.sm }
-        }
-      }
-    }))
-
-    const { getByRole } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: BUTTON_END_ICON_SIZE_UPDATE,
-              variables: {
-                id: 'id',
-                journeyId: undefined,
-                input: {
-                  endIcon: {
-                    size: IconSize.sm
-                  }
-                }
-              }
-            },
-            result
-          }
-        ]}
-      >
-        <EditorProvider initialState={{ selectedBlock }}>
-          <SizeToggleGroup type={IconType.end} />
+          <SizeToggleGroup iconBlock={icon} />
         </EditorProvider>
       </MockedProvider>
     )
--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/SizeToggleGroup/SizeToggleGroup.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Button/Icon/SizeToggleGroup/SizeToggleGroup.stories.tsx
@@ -1,7 +1,9 @@
 import { Story, Meta } from '@storybook/react'
 import { MockedProvider } from '@apollo/client/testing'
+import { TreeBlock } from '@core/journeys/ui'
 import { simpleComponentConfig } from '../../../../../../../../libs/storybook'
-import { IconType } from '..'
+import { IconFields } from '../../../../../../../../../__generated__/IconFields'
+import { IconName } from '../../../../../../../../../__generated__/globalTypes'
 import { SizeToggleGroup } from '.'
 
 const SizeToggleGroupStory = {
@@ -12,9 +14,19 @@ const SizeToggleGroupStory = {
 }
 
 export const Default: Story = () => {
+  const icon: TreeBlock<IconFields> = {
+    id: 'icon-id',
+    parentBlockId: 'buttonBlockId',
+    parentOrder: null,
+    __typename: 'IconBlock',
+    iconName: IconName.ArrowForwardRounded,
+    iconSize: null,
+    iconColor: null,
+    children: []
+  }
   return (
     <MockedProvider>
-      <SizeToggleGroup type={IconType.start} />
+      <SizeToggleGroup iconBlock={icon} />
     </MockedProvider>
   )
 }
