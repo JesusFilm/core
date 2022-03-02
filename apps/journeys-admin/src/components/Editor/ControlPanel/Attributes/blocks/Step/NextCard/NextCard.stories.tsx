b71cce58bedab297977bdf915d82ea427813ea50
Siyang <csiyang@yahoo.co.nz>
2022-03-02 20:57:47 +0000

    feat: added stories for NextCard, change naming

---
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Step/NextCard/NextCard.stories.tsx
@@ -0,0 +1,47 @@
+import { Story, Meta } from '@storybook/react'
+import { EditorProvider, TreeBlock } from '@core/journeys/ui'
+import { MockedProvider } from '@apollo/client/testing'
+import { Drawer } from '../../../../../Drawer'
+import { journeysAdminConfig } from '../../../../../../../libs/storybook'
+import { GetJourney_journey_blocks_StepBlock as StepBlock } from '../../../../../../../../__generated__/GetJourney'
+import { NextCard } from '.'
+
+const NextCardStory = {
+  ...journeysAdminConfig,
+  title: 'Journeys-Admin/Editor/ControlPanel/Attributes/Step/NextCard',
+  component: NextCard
+}
+
+export const Default: Story = () => {
+  const selectedBlock: TreeBlock<StepBlock> = {
+    id: 'step1.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    nextBlockId: null,
+    parentOrder: 0,
+    locked: false,
+    children: []
+  }
+  return (
+    <MockedProvider>
+      <EditorProvider
+        initialState={{
+          selectedBlock,
+          drawerChildren: (
+            <NextCard
+              id={selectedBlock.id}
+              nextBlockId={selectedBlock.nextBlockId}
+              locked={selectedBlock.locked}
+            />
+          ),
+          drawerTitle: 'Next Card Properties',
+          drawerMobileOpen: true
+        }}
+      >
+        <Drawer />
+      </EditorProvider>
+    </MockedProvider>
+  )
+}
+
+export default NextCardStory as Meta
