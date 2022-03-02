1fc7bed06595d6086695985e4487c50f17401d15
Siyang <csiyang@yahoo.co.nz>
2022-03-02 22:50:35 +0000

    refactor: changed NextCard to use ToggleOption

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Step/NextCard/NextCard.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Step/NextCard/NextCard.tsx
@@ -3,7 +3,10 @@ import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
 import Typography from '@mui/material/Typography'
 import { gql, useMutation } from '@apollo/client'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import { useJourney } from '../../../../../../../libs/context'
 import { StepBlockLockUpdate } from '../../../../../../../../__generated__/StepBlockLockUpdate'
+import { ToggleOption } from '../../../ToggleOption'
 
 export const STEP_BLOCK_LOCK_UPDATE = gql`
   mutation StepBlockLockUpdate(
@@ -33,27 +36,26 @@ export function NextCard({
     STEP_BLOCK_LOCK_UPDATE
   )
 
+  const journey = useJourney()
+
   async function handleChange(): Promise<void> {
-    // TODO: update
-    // if (selectedBlock != null && selectedBlock.__typename === 'StepBlock') {
-    //   await stepBlockLockUpdate({
-    //     variables: {
-    //       id: selectedBlock.id,
-    //       journeyId: journey.id,
-    //       input: {
-    //         locked: !checked,
-    //         nextBlockId: selectedBlock.nextBlockId
-    //       }
-    //     },
-    //     optimisticResponse: {
-    //       stepBlockUpdate: {
-    //         id: selectedBlock.id,
-    //         __typename: 'StepBlock',
-    //         locked: !checked
-    //       }
-    //     }
-    //   })
-    // }
+    await stepBlockLockUpdate({
+      variables: {
+        id: id,
+        journeyId: journey.id,
+        input: {
+          locked: !locked,
+          nextBlockId: nextBlockId
+        }
+      },
+      optimisticResponse: {
+        stepBlockUpdate: {
+          id,
+          __typename: 'StepBlock',
+          locked: !locked
+        }
+      }
+    })
   }
   return (
     <>
@@ -66,17 +68,21 @@ export function NextCard({
         <Typography variant="subtitle2" gutterBottom>
           Conditions
         </Typography>
+        <ToggleOption
+          heading={'Conditions'}
+          description={"Don't allow to skip the current card"}
+          checked={locked}
+          handleChange={handleChange}
+        >
+          <Box display={'flex'} alignItems={'center'} color={'text.secondary'}>
+            <InfoOutlinedIcon sx={{ mr: 4 }} />
+            <Typography variant="caption">
+              User can&apos;t skip interaction on the current card, like
+              watching video or interacting with questions.
+            </Typography>
+          </Box>
+        </ToggleOption>
       </Box>
     </>
   )
-}
-
-// Don&apos;t allow to skip the current card
-
-{
-  /* <InfoOutlinedIcon sx={{ mr: 4 }} />
-<Typography variant="caption">
-  User can&apos;t skip interaction on the current card, like watching
-  video or interacting with questions.
-</Typography> */
 }
