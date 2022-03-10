e6aeed9f4875eb5aee6139a5b7f65ef5da755725
Siyang <csiyang@yahoo.co.nz>
2022-03-10 21:48:32 +0000

    feat: logic for deleting card and block

--- apps/journeys-admin/src/components/Editor/EditToolbar/DeleteBlock/DeleteBlock.tsx
+++ apps/journeys-admin/src/components/Editor/EditToolbar/DeleteBlock/DeleteBlock.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react'
 import IconButton from '@mui/material/IconButton'
 import DeleteOutlineRounded from '@mui/icons-material/DeleteOutlineRounded'
 import { gql, useMutation } from '@apollo/client'
-import { useEditor, searchBlocks } from '@core/journeys/ui'
+import { useEditor, searchBlocks, TreeBlock } from '@core/journeys/ui'
 import MenuItem from '@mui/material/MenuItem'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemIcon from '@mui/material/ListItemIcon'
@@ -10,6 +10,7 @@ import { useSnackbar } from 'notistack'
 import { BlockDelete } from '../../../../../__generated__/BlockDelete'
 import { useJourney } from '../../../../libs/context'
 import { blockDeleteUpdate } from '../../../../libs/blockDeleteUpdate/blockDeleteUpdate'
+import { GetJourney_journey_blocks_StepBlock as StepBlock } from '../../../../../__generated__/GetJourney'
 
 export const BLOCK_DELETE = gql`
   mutation BlockDelete($id: ID!, $journeyId: ID!, $parentBlockId: ID!) {
@@ -32,61 +33,54 @@ export function DeleteBlock({
 
   const { id: journeyId } = useJourney()
   const {
-    state: { selectedBlock, selectedStep },
+    state: { selectedBlock, selectedStep, steps },
     dispatch
   } = useEditor()
 
-  const handleDeleteBlock = async (): Promise<void> => {
-    if (
-      selectedBlock == null ||
-      selectedBlock.__typename === 'CardBlock' ||
-      selectedStep == null
-    )
-      return
-
-    if (selectedBlock.parentBlockId == null) return
-
-    const parentBlock = searchBlocks(
-      [selectedStep],
-      selectedBlock.parentBlockId
-    )
-
-    if (parentBlock == null) return
-
-    const children = parentBlock?.children.filter(
-      ({ parentOrder }) => parentOrder != null
-    )
-    const indexToDelete = children?.findIndex(
-      ({ id }) => id === selectedBlock.id
-    )
-
-    let newSelectedBlock
-    switch (indexToDelete) {
-      case children.length - 1:
-        newSelectedBlock = children[indexToDelete - 1]
-        break
-      default:
-        newSelectedBlock = children[indexToDelete + 1]
-        break
+  function updateSelected(
+    parentOrder: number,
+    siblings,
+    deletedStep?: TreeBlock<StepBlock>
+  ): void {
+    if (siblings.length > 0) {
+      const toSetParentOrder = parentOrder > 0 ? parentOrder - 1 : 0
+      const blockToSelect = siblings.find(
+        (sibling) => sibling.parentOrder === toSetParentOrder
+      )
+      dispatch({
+        type: 'SetSelectedBlockByIdAction',
+        id: blockToSelect.id
+      })
+    } else if (deletedStep != null && steps.length > 0) {
+      const stepToSet =
+        steps.find((step) => step.nextBlockId === deletedStep.id) ?? steps[0]
+      dispatch({
+        type: 'SetSelectedStepAction',
+        step: stepToSet
+      })
     }
+  }
+
+  const handleDeleteBlock = async (): Promise<void> => {
+    if (selectedBlock == null) return
+
+    const toDeleteParentOrder = selectedBlock.parentOrder
 
     const { data } = await blockDelete({
       variables: {
         id: selectedBlock.id,
         journeyId,
-        parentBlockId: selectedBlock.parentBlockId
+        parentBlockId: selectedBlock.parentBlockId ?? 0
       },
       update(cache, { data }) {
         blockDeleteUpdate(selectedBlock, data?.blockDelete, cache, journeyId)
       }
     })
 
-    if (data?.blockDelete != null && newSelectedBlock != null) {
-      dispatch({
-        type: 'SetSelectedBlockByIdAction',
-        id: newSelectedBlock.id
-      })
-    }
+    data?.blockDelete != null &&
+      toDeleteParentOrder != null &&
+      updateSelected(toDeleteParentOrder, data.blockDelete, selectedStep)
+
     enqueueSnackbar('Block Deleted', {
       variant: 'success',
       preventDuplicate: true
