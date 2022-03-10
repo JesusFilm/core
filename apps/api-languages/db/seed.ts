8a662aeaa1a405c7efe8f89cbf0d48a6fb510924
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-03-10 00:30:49 +0000

    chore: add arclight api key env

--- apps/api-languages/db/seed.ts
+++ apps/api-languages/db/seed.ts
@@ -5,9 +5,32 @@ import { ArangoDB } from './db'
 
 interface Language {
   _key: string
+  name: Array<{ value: string; languageId: string; primary: boolean }>
+  bcp47?: string
+  iso3?: string
 }
 
 const db = ArangoDB()
+
+async function getLanguage(languageId: number): Promise<Language | undefined> {
+  const rst = await db.query(aql`
+  FOR item IN ${db.collection('languages')}
+    FILTER item._key == ${languageId.toString()}
+    LIMIT 1
+    RETURN item`)
+  return await rst.next()
+}
+
+async function getLanguageByBcp47(
+  bcp47: string
+): Promise<Language | undefined> {
+  const rst = await db.query(aql`
+  FOR item IN ${db.collection('languages')}
+    FILTER item.bcp47 == ${bcp47}
+    LIMIT 1
+    RETURN item`)
+  return await rst.next()
+}
 
 async function main(): Promise<void> {
   try {
@@ -16,40 +39,27 @@ async function main(): Promise<void> {
   const collection = db.collection('languages')
   const data = await (
     await fetch(
-      'https://api.arclight.org/v2/media-languages?page=1&limit=5000&filter=default&apiKey=50105582a2e5a6.72068725'
+      `https://api.arclight.org/v2/media-languages?limit=5000&apiKey=${
+        process.env.ARCLIGHT_API_KEY ?? ''
+      }`
     )
   ).json()
 
-  async function getLanguage(
-    languageId: number
-  ): Promise<Language | undefined> {
-    const rst = await db.query(aql`
-    FOR item IN ${collection}
-      FILTER item._key == ${languageId.toString()}
-      LIMIT 1
-      RETURN item`)
-    return await rst.next()
-  }
-
-  async function getLanguageByBcp47(
-    bcp47: string
-  ): Promise<Language | undefined> {
-    const rst = await db.query(aql`
-    FOR item IN ${collection}
-      FILTER item.bcp47 == ${bcp47}
-      LIMIT 1
-      RETURN item`)
-    return await rst.next()
-  }
-
   for (const mediaLanguage of data._embedded.mediaLanguages) {
     const { languageId, bcp47, iso3, nameNative } = mediaLanguage
-    const body = {
+    const body: Omit<Language, '_key'> = {
       bcp47: isEmpty(bcp47) ? undefined : bcp47,
       iso3: isEmpty(iso3) ? undefined : iso3,
-      nameNative
+      name: [
+        {
+          value: nameNative,
+          languageId: languageId.toString(),
+          primary: true
+        }
+      ]
     }
     const language = await getLanguage(languageId)
+
     if (language != null) {
       await collection.update(languageId.toString(), body)
     } else {
@@ -59,14 +69,19 @@ async function main(): Promise<void> {
 
   for (const mediaLanguage of data._embedded.mediaLanguages) {
     const { languageId, metadataLanguageTag, name } = mediaLanguage
+    const language = await getLanguage(languageId)
+    if (language == null) continue
+
     const metadataLanguage = await getLanguageByBcp47(metadataLanguageTag)
     if (metadataLanguage == null) continue
-    const body = {
+
+    const body: Omit<Language, '_key'> = {
       name: [
+        ...language.name,
         {
           value: name,
           languageId: metadataLanguage._key,
-          primary: true
+          primary: false
         }
       ]
     }
