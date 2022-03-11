a2af73756c101934fa789e73729c84a0826b2f96
Mike Allison <mike@captainofbass.com>
2022-03-11 00:06:21 +0000

    feat: image block

--- apps/journeys-admin/src/components/Editor/ImageBlockEditor/ImageBlockEditor.tsx
+++ apps/journeys-admin/src/components/Editor/ImageBlockEditor/ImageBlockEditor.tsx
@@ -43,7 +43,6 @@ export function ImageBlockEditor({
 
   const handleImageDelete = async (): Promise<void> => {
     if (onDelete != null) await onDelete()
-    await onDelete()
   }
 
   const formik = useFormik({
