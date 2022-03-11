6fdf1f1bf15c83b0e0ecf22c3f304d154c531c30
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2022-03-11 15:21:18 +1300

    feature: inline edit button (#339)
    * fix: card auto margin squash text field
    
    * fix: render new line characters
    
    * fix: conductor story data, used to check card auto margins
    
    * fix: export WrappersProps
    
    * fix: export WrapperProps
    
    * feat: create TypographyEdit component
    
    * feat: create InlineEdit component wrapper
    
    * fix: linting
    
    * fix: align and color styling for TypographyEdit
    
    * feat: create ButtonEdit component
    
    * fix: remove whitespace in button label
    
    * fix: add InlineEditWrapper test
    
    * fix: minHeight for all button sizes

--- apps/journeys-admin/src/components/Editor/Canvas/InlineEditWrapper/InlineEditWrapper.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Canvas/InlineEditWrapper/InlineEditWrapper.spec.tsx
@@ -4,14 +4,17 @@ import {
   EditorProvider,
   TreeBlock,
   ActiveFab,
+  Button,
   Typography
 } from '@core/journeys/ui'
+import { ButtonFields } from '../../../../../__generated__/ButtonFields'
+import { StepFields } from '../../../../../__generated__/StepFields'
 import { TypographyVariant } from '../../../../../__generated__/globalTypes'
 import { TypographyFields } from '../../../../../__generated__/TypographyFields'
 import { InlineEditWrapper } from '.'
 
-describe('TypographyEdit', () => {
-  const block: TreeBlock<TypographyFields> = {
+describe('InlineEditWrapper', () => {
+  const typographyBlock: TreeBlock<TypographyFields> = {
     __typename: 'TypographyBlock',
     id: 'typography.id',
     parentBlockId: 'parent.id',
@@ -22,24 +25,26 @@ describe('TypographyEdit', () => {
     align: null,
     children: []
   }
-  const step: TreeBlock = {
-    __typename: 'StepBlock',
-    id: 'step.id',
-    parentBlockId: null,
-    parentOrder: 0,
-    locked: true,
-    nextBlockId: null,
-    children: [block]
+
+  const step = (block: TreeBlock): TreeBlock<StepFields> => {
+    return {
+      __typename: 'StepBlock',
+      id: 'step.id',
+      parentBlockId: null,
+      parentOrder: 0,
+      locked: true,
+      nextBlockId: null,
+      children: [block]
+    }
   }
-  const props = {
-    block,
-    children: <Typography {...block} />
-  }
+
   it('renders children by default', () => {
     const { getByText } = render(
       <MockedProvider>
         <EditorProvider>
-          <InlineEditWrapper {...props} />
+          <InlineEditWrapper block={typographyBlock}>
+            <Typography {...typographyBlock} />
+          </InlineEditWrapper>
         </EditorProvider>
       </MockedProvider>
     )
@@ -47,24 +52,65 @@ describe('TypographyEdit', () => {
   })
 
   it('should edit typography on double click', async () => {
-    const { getByRole, getByText } = render(
+    const { getByDisplayValue, getByText } = render(
       <MockedProvider>
         <EditorProvider
           initialState={{
-            steps: [step],
+            steps: [step(typographyBlock)],
             activeFab: ActiveFab.Add
           }}
         >
-          <InlineEditWrapper {...props} />
+          <InlineEditWrapper block={typographyBlock}>
+            <Typography {...typographyBlock} />
+          </InlineEditWrapper>
         </EditorProvider>
       </MockedProvider>
     )
 
     fireEvent.click(getByText('test content'))
     fireEvent.click(getByText('test content'))
-    const input = getByRole('textbox')
+    const input = getByDisplayValue('test content')
     expect(input).toBeInTheDocument()
     // Check it doesn't deselected on click
+    fireEvent.click(input)
+    expect(input).toBeInTheDocument()
+  })
+
+  it('should edit button on double click', async () => {
+    const block: TreeBlock<ButtonFields> = {
+      __typename: 'ButtonBlock',
+      id: 'button.id',
+      parentBlockId: 'parent.id',
+      parentOrder: 0,
+      label: 'test label',
+      buttonVariant: null,
+      buttonColor: null,
+      size: null,
+      startIconId: null,
+      endIconId: null,
+      action: null,
+      children: []
+    }
+
+    const { getByDisplayValue, getByText } = render(
+      <MockedProvider>
+        <EditorProvider
+          initialState={{
+            steps: [step(block)],
+            activeFab: ActiveFab.Add
+          }}
+        >
+          <InlineEditWrapper block={block}>
+            <Button {...block} />
+          </InlineEditWrapper>
+        </EditorProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByText('test label'))
+    fireEvent.click(getByText('test label'))
+    const input = getByDisplayValue('test label')
+    expect(input).toBeInTheDocument()
     fireEvent.click(input)
     expect(input).toBeInTheDocument()
   })
