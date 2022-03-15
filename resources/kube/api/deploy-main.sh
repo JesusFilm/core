23f0d7aa8d39a3a5ade27a77e8db0b3640fa7160
Mike Allison <mike@captainofbass.com>
2022-03-15 20:08:31 +0000

    chore: k8s update

--- resources/kube/api/deploy-main.sh
+++
@@ -1,4 +0,0 @@
-cd /workspaces/core/resources/kube/api
-kubectl apply -f api-gateway-main-deployment.yaml
-kubectl apply -f api-journeys-main-deployment.yaml
-kubectl apply -f api-users-main-deployment.yaml
