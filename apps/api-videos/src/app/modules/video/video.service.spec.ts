7d6fd1564bc67016a74eb882c8d57579afabf50e
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-03-17 10:58:22 +1300

    feat: create api-videos (#348)
    Co-authored-by: John Geronimo <johnageronimo1@gmail.com>
    Co-authored-by: Mike Allison <mike@captainofbass.com>

---
+++ apps/api-videos/src/app/modules/video/video.service.spec.ts
@@ -0,0 +1,211 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { Database, aql } from 'arangojs'
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended'
+import { DocumentCollection } from 'arangojs/collection'
+import { ArrayCursor } from 'arangojs/cursor'
+import { AqlQuery } from 'arangojs/aql'
+import { VideoService } from './video.service'
+
+const DEFAULT_QUERY = aql`
+    FOR item IN 
+      
+      LIMIT ${0}, ${100}
+      RETURN {
+        _key: item._key,
+        title: item.title,
+        snippet: item.snippet,
+        description: item.description,
+        studyQuestions: item.studyQuestions,
+        image: item.image,
+        tagIds: item.tagIds,
+        variant: NTH(item.variants[* 
+          FILTER CURRENT.languageId == NOT_NULL(${null}, item.primaryLanguageId)
+          LIMIT 1 RETURN CURRENT
+        ], 0),
+        variantLanguages: item.variants[* RETURN { id : CURRENT.languageId }]
+      }
+    `.query
+
+const QUERY_WITH_TITLE = aql`
+    FOR item IN 
+      SEARCH ANALYZER(TOKENS(${'abc'}, "text_en") ALL == item.title.value, "text_en")
+      LIMIT ${0}, ${100}
+      RETURN {
+        _key: item._key,
+        title: item.title,
+        snippet: item.snippet,
+        description: item.description,
+        studyQuestions: item.studyQuestions,
+        image: item.image,
+        tagIds: item.tagIds,
+        variant: NTH(item.variants[* 
+          FILTER CURRENT.languageId == NOT_NULL(${null}, item.primaryLanguageId)
+          LIMIT 1 RETURN CURRENT
+        ], 0),
+        variantLanguages: item.variants[* RETURN { id : CURRENT.languageId }]
+      }
+    `.query
+
+const QUERY_WITH_AVAILABLE_VARIANT_LANGUAGE_IDS = aql`
+    FOR item IN 
+      SEARCH item.variants.languageId IN ${['en']}
+      LIMIT ${0}, ${100}
+      RETURN {
+        _key: item._key,
+        title: item.title,
+        snippet: item.snippet,
+        description: item.description,
+        studyQuestions: item.studyQuestions,
+        image: item.image,
+        tagIds: item.tagIds,
+        variant: NTH(item.variants[* 
+          FILTER CURRENT.languageId == NOT_NULL(${null}, item.primaryLanguageId)
+          LIMIT 1 RETURN CURRENT
+        ], 0),
+        variantLanguages: item.variants[* RETURN { id : CURRENT.languageId }]
+      }
+    `.query
+
+const QUERY_WITH_TITLE_AND_AVAILABLE_VARIANT_LANGUAGE_IDS = aql`
+    FOR item IN 
+      SEARCH ANALYZER(TOKENS(${'abc'}, "text_en") ALL == item.title.value, "text_en") AND item.variants.languageId IN ${[
+  'en'
+]}
+      LIMIT ${0}, ${100}
+      RETURN {
+        _key: item._key,
+        title: item.title,
+        snippet: item.snippet,
+        description: item.description,
+        studyQuestions: item.studyQuestions,
+        image: item.image,
+        tagIds: item.tagIds,
+        variant: NTH(item.variants[* 
+          FILTER CURRENT.languageId == NOT_NULL(${null}, item.primaryLanguageId)
+          LIMIT 1 RETURN CURRENT
+        ], 0),
+        variantLanguages: item.variants[* RETURN { id : CURRENT.languageId }]
+      }
+    `.query
+
+describe('VideoService', () => {
+  let service: VideoService
+  let db: DeepMockProxy<Database>
+
+  beforeEach(async () => {
+    db = mockDeep<Database>()
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        VideoService,
+        {
+          provide: 'DATABASE',
+          useFactory: () => db
+        }
+      ]
+    }).compile()
+
+    service = module.get<VideoService>(VideoService)
+    service.collection = mockDeep<DocumentCollection>()
+  })
+
+  describe('filterAll', () => {
+    it('should query', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(DEFAULT_QUERY)
+        expect(bindVars).toEqual({ value0: 0, value1: 100, value2: null })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(await service.filterAll()).toEqual([])
+    })
+
+    it('should query with page', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(DEFAULT_QUERY)
+        expect(bindVars).toEqual({ value0: 200, value1: 100, value2: null })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(await service.filterAll({ page: 3 })).toEqual([])
+    })
+
+    it('should query with limit', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(DEFAULT_QUERY)
+        expect(bindVars).toEqual({ value0: 0, value1: 200, value2: null })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(await service.filterAll({ limit: 200 })).toEqual([])
+    })
+
+    it('should query with title', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(QUERY_WITH_TITLE)
+        expect(bindVars).toEqual({
+          value0: 'abc',
+          value1: 0,
+          value2: 100,
+          value3: null
+        })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(await service.filterAll({ title: 'abc' })).toEqual([])
+    })
+
+    it('should query with availableVariantLanguageIds', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(QUERY_WITH_AVAILABLE_VARIANT_LANGUAGE_IDS)
+        expect(bindVars).toEqual({
+          value0: ['en'],
+          value1: 0,
+          value2: 100,
+          value3: null
+        })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(
+        await service.filterAll({ availableVariantLanguageIds: ['en'] })
+      ).toEqual([])
+    })
+
+    it('should query with title and availableVariantLanguageIds', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(
+          QUERY_WITH_TITLE_AND_AVAILABLE_VARIANT_LANGUAGE_IDS
+        )
+        expect(bindVars).toEqual({
+          value0: 'abc',
+          value1: ['en'],
+          value2: 0,
+          value3: 100,
+          value4: null
+        })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(
+        await service.filterAll({
+          title: 'abc',
+          availableVariantLanguageIds: ['en']
+        })
+      ).toEqual([])
+    })
+
+    it('should query with variantLanguageId', async () => {
+      db.query.mockImplementationOnce(async (q) => {
+        const { query, bindVars } = q as unknown as AqlQuery
+        expect(query).toEqual(DEFAULT_QUERY)
+        expect(bindVars).toEqual({
+          value0: 0,
+          value1: 100,
+          value2: 'en'
+        })
+        return { all: () => [] } as unknown as ArrayCursor
+      })
+      expect(await service.filterAll({ variantLanguageId: 'en' })).toEqual([])
+    })
+  })
+})
