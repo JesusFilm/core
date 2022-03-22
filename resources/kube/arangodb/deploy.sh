07fe4e0dd715679ba1d09bb9d54c31359c61f90c
Mike Allison <mike@captainofbass.com>
2022-03-21 18:21:35 -0700

    chore: k8s update (#389)

--- resources/kube/arangodb/deploy.sh
+++ resources/kube/arangodb/deploy.sh
@@ -1,6 +1,8 @@
-kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-crd.yaml
-kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.7/manifests/arango-crd.yaml
+kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.7/manifests/arango-deployment.yaml
 # To use `ArangoLocalStorage`, also run
-#kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.5/manifests/arango-storage.yaml
-cd /workspaces/core/resources/kube/arangodb
-kubectl apply -f ./arangodb-deployment.yaml 
+# kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.7/manifests/arango-storage.yaml
+# To use `ArangoDeploymentReplication`, also run
+# kubectl apply -f https://raw.githubusercontent.com/arangodb/kube-arangodb/1.2.7/manifests/arango-deployment-replication.yaml
+
+kubectl apply -f /workspaces/core/resources/kube/arangodb
