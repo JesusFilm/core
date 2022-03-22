b4caa22a07af712529b8b17bc4fed2932ca0c4ae
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2022-03-23 09:33:17 +1300

    refactor: removed extra divider (#398)
    * refactor: removed extra divider
    
    * fix: linting issue

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Card/BackgroundMedia/BackgroundMedia.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Card/BackgroundMedia/BackgroundMedia.tsx
@@ -1,12 +1,6 @@
 import { ReactElement, useEffect, useState, MouseEvent } from 'react'
 import Box from '@mui/material/Box'
-import {
-  ToggleButton,
-  ToggleButtonGroup,
-  Divider,
-  Stack,
-  styled
-} from '@mui/material'
+import { ToggleButton, ToggleButtonGroup, Stack, styled } from '@mui/material'
 import { Image as ImageIcon, Videocam } from '@mui/icons-material'
 import { useEditor, TreeBlock } from '@core/journeys/ui'
 import { GetJourney_journey_blocks_CardBlock as CardBlock } from '../../../../../../../../__generated__/GetJourney'
@@ -91,7 +85,6 @@ export function BackgroundMedia(): ReactElement {
           </ToggleButton>
         </StyledToggleButtonGroup>
       </Box>
-      <Divider sx={{ sm: 'none' }} />
       {blockType === 'ImageBlock' && (
         <BackgroundMediaImage cardBlock={cardBlock} />
       )}
