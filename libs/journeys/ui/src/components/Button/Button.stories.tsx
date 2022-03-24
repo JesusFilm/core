ff54cdcf87c08adeffc9a28c92f862218e77828f
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-03-24 21:54:38 +0000

    fix: linting

--- libs/journeys/ui/src/components/Button/Button.stories.tsx
+++ libs/journeys/ui/src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react'
+import Typography from '@mui/material/Typography'
 import { journeyUiConfig, StoryCard, TreeBlock } from '../..'
 import {
   ButtonVariant,
