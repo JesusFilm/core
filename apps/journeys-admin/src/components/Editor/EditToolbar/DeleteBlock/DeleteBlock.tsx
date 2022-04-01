995ca31fd92d07b2fcdc5e944148abdb8d155fae
Siyang <csiyang@yahoo.co.nz>
2022-04-01 01:31:26 +0000

    refactor: delete card dialog

--- apps/journeys-admin/src/components/Editor/EditToolbar/DeleteBlock/DeleteBlock.tsx
+++ apps/journeys-admin/src/components/Editor/EditToolbar/DeleteBlock/DeleteBlock.tsx
@@ -7,10 +7,11 @@ import MenuItem from '@mui/material/MenuItem'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import { useSnackbar } from 'notistack'
+import Typography from '@mui/material/Typography'
 import { BlockDelete } from '../../../../../__generated__/BlockDelete'
 import { useJourney } from '../../../../libs/context'
 import { blockDeleteUpdate } from '../../../../libs/blockDeleteUpdate/blockDeleteUpdate'
-import { DeleteDialog } from './DeleteDialog'
+import { Dialog } from '../../../Dialog'
 import getSelected from './utils/getSelected'
 
 export const BLOCK_DELETE = gql`
@@ -91,13 +92,24 @@ export function DeleteBlock({
         })
   }
 
+  const dialogProps = {
+    open: openDialog,
+    handleClose: handleCloseDialog,
+    dialogTitle: { title: 'Delete Card?' },
+    dialogAction: {
+      onSubmit: handleDeleteBlock,
+      submitLabel: 'Delete',
+      closeLabel: 'Cancel'
+    }
+  }
+
   return (
     <>
-      <DeleteDialog
-        handleDelete={handleDeleteBlock}
-        open={openDialog}
-        handleClose={handleCloseDialog}
-      />
+      <Dialog {...dialogProps}>
+        <Typography>
+          Are you sure you would like to delete this card?
+        </Typography>
+      </Dialog>
       {variant === 'button' ? (
         <IconButton
           id="delete-block-actions"
