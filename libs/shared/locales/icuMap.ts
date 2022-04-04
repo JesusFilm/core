9a752643e68b465d089fc67bddefedb7c94f2173
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-04-04 04:02:54 +0000

    docs: add comments

--- libs/shared/locales/icuMap.ts
+++ libs/shared/locales/icuMap.ts
@@ -1,4 +1,11 @@
 import langMap from 'langmap'
 
-// Centralise our locale mapping, extend here if needed
+/* 
+Centralise our locale mapping, extend here if needed
+
+import icuMap & call icuMap[locale].englishName or icuMap[locale].nativeName
+
+https://github.com/mozilla/language-mapping-list/blob/master/language-mapping-list.js
+*/
+
 export const icuMap = langMap
