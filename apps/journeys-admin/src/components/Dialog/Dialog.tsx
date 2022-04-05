fa9f470038f7048eb5ecc63bce7db7ceee888f97
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-04-06 11:30:07 +1200

    feat: common dialog component (#433)
    * feat: initial commit
    
    * feat: created component,
    
    * test: added stories
    
    * fix: long titles now wrap
    
    * fix: added prop type to stories
    
    * test: uadded tests
    
    * refactor: updated the design of the component
    
    * style: refactored and added style in a styled component
    
    * test: added story for info
    
    * refactor: updated naming to be clearer for dialog content
    
    * chore: minor adjustments from PR review
    
    * test: refactored the label for the form story to show how it can be used
    
    * style: adjusted padding bottom of content, added a story to test out this change
    
    * fix: close button size
    
    * feat: inline edit radio question (#413)
    
    * refactor: last child margin set from card instead of components theme
    
    * refactor: pass in editable input in typography
    
    * refactor: add selectable wrapper
    
    * fix: linting
    
    * fix: add styling in correct place
    
    * fix: remove incorrect styling
    
    * fix: add styling to stories and fix signup bottom margin
    
    * fix: update grid and signup styling
    
    * fix: no gap between grid items
    
    * fix: update button and radio question styling
    
    * fix: switch to Done Fab only for inline editable blocks
    
    * fix: typography edit styling
    
    * fix: make radio option selectable
    
    * refactor: update selectable tests
    
    * refactor: update radioQuestion tests
    
    * refactor: update inline edit block tests
    
    * refactor: fix type-check, update test
    
    * fix: remove redundant margin-top
    
    * fix: breaking tests
    
    * fix: match button margin with how it looks locally
    
    * feat: enabe inline editing for radioOption
    
    * fix: linting
    
    * fix: breaking build
    
    * fix: disable ripple on radio option
    
    * fix: simplify tests
    
    * feat: add inline editing to question
    
    * fix: add placeholder on option, make input styling consistent
    
    * fix: tests
    
    * refactor: extract out common inline edit input
    
    * feat: video list (#436)
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * feat: video-list
    
    * chore: add execution to post create command (#438)
    
    * fix: post-create
    
    * fix: revert arango to 3.8.6 (#441)
    
    * style: adjusted paddings for buttons
    
    * test: added story to test min height
    
    * refactor: made it more clear that close button can only appear when there is a title
    
    * test: updated spec for refactor
    
    * refactor: title use h6, updated theme for h6
    
    * fix: removed padding coming from close button
    
    * fix: import font weight 700
    
    * fix: remove redunant sx
    
    * chore: remove unused comment
    
    * fix: removed styling that was having unintended changes
    
    * fix: import for lint
    
    Co-authored-by: Ziwei Liu <10802634+storyworks@users.noreply.github.com>
    Co-authored-by: Mike Allison <mike@captainofbass.com>
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

---
+++ apps/journeys-admin/src/components/Dialog/Dialog.tsx
@@ -0,0 +1,107 @@
+import { ReactElement } from 'react'
+import { styled } from '@mui/material/styles'
+import MuiDialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import MuiDialogTitle from '@mui/material/DialogTitle'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+
+export interface DialogProps {
+  open: boolean
+  handleClose: () => void
+  dialogTitle?: DialogTitle
+  dialogAction?: DialogAction
+  divider?: boolean
+  children?: ReactElement
+}
+
+interface DialogAction {
+  onSubmit: () => void
+  submitLabel?: string
+  closeLabel?: string
+}
+
+interface DialogTitle {
+  title: string
+  closeButton?: boolean
+}
+
+const StyledDialog = styled(MuiDialog)({
+  '& .MuiDialogTitle-root': {
+    display: 'flex',
+    color: 'secondary.dark',
+    paddingTop: 20,
+    alignItems: 'center',
+    '& .MuiIconButton-root': {
+      marginLeft: 'auto',
+      padding: 0
+    }
+  },
+  '& .MuiDialogContent-root': {
+    paddingBottom: 24
+  },
+  '& .MuiDialogContent-dividers': {
+    padding: 24
+  },
+  '& .MuiDialogActions-root': {
+    padding: 8,
+    '& .MuiButton-root': {
+      padding: '10px 20px 10px 18px'
+    },
+    '& > *': {
+      '&:not(:first-of-type)': {
+        marginLeft: 0
+      }
+    }
+  },
+  '& .MuiTypography-body1': {
+    color: 'secondary.light'
+  }
+})
+
+export function Dialog({
+  open,
+  handleClose,
+  dialogTitle,
+  dialogAction,
+  divider,
+  children
+}: DialogProps): ReactElement {
+  function handleSubmit(): void {
+    dialogAction?.onSubmit()
+    handleClose()
+  }
+
+  return (
+    <StyledDialog
+      open={open}
+      maxWidth="sm"
+      fullWidth={true}
+      onClose={handleClose}
+    >
+      {dialogTitle != null && (
+        <MuiDialogTitle>
+          {dialogTitle.title}
+          {dialogTitle.closeButton != null && dialogTitle.closeButton && (
+            <IconButton size="medium" onClick={handleClose}>
+              <CloseRoundedIcon />
+            </IconButton>
+          )}
+        </MuiDialogTitle>
+      )}
+      <DialogContent dividers={divider}>{children}</DialogContent>
+      {dialogAction != null && (
+        <DialogActions>
+          {dialogAction.closeLabel != null && (
+            <Button onClick={handleClose}>{dialogAction.closeLabel}</Button>
+          )}
+          <Button onClick={handleSubmit}>
+            {dialogAction.submitLabel ?? 'Save'}
+          </Button>
+        </DialogActions>
+      )}
+    </StyledDialog>
+  )
+}
