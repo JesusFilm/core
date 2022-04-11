3f28dc1060d76887b2802bcf400d7fba8694c41b
siyang <csiyang@yahoo.co.nz>
2022-04-11 23:57:10 +0000

    feat: added mutation for seoDescription

--- apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/DescriptionEdit/DescriptionEdit.tsx
+++ apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/DescriptionEdit/DescriptionEdit.tsx
@@ -1,20 +1,49 @@
 import { ReactElement } from 'react'
+import { useMutation, gql } from '@apollo/client'
 import TextField from '@mui/material/TextField'
 import { Formik, Form } from 'formik'
 import { object, string } from 'yup'
 import noop from 'lodash/noop'
 import { useJourney } from '../../../../../libs/context'
+import { JourneySeoDescriptionUpdate } from '../../../../../../__generated__/JourneySeoDescriptionUpdate'
+
+export const JOURNEY_SEO_DESCRIPTION_UPDATE = gql`
+  mutation JourneySeoDescriptionUpdate($id: ID!, $input: JourneyUpdateInput!) {
+    journeyUpdate(id: $id, input: $input) {
+      id
+      seoDescription
+    }
+  }
+`
 
 export function DescriptionEdit(): ReactElement {
-  const { description } = useJourney()
+  const [journeyUpdate] = useMutation<JourneySeoDescriptionUpdate>(
+    JOURNEY_SEO_DESCRIPTION_UPDATE
+  )
+  const { description, seoDescription, id } = useJourney()
 
   async function handleSubmit(e: React.FocusEvent): Promise<void> {
     const target = e.target as HTMLInputElement
-    console.log('Description saved:', target.value)
-    // mutation here
+    await journeyUpdate({
+      variables: {
+        id,
+        input: {
+          seoDescription: target.value
+        }
+      },
+      optimisticResponse: {
+        journeyUpdate: {
+          id,
+          __typename: 'Journey',
+          seoDescription: target.value
+        }
+      }
+    })
   }
 
-  const initialValues = { socialDescription: description ?? '' }
+  const initialValues = {
+    socialDescription: seoDescription ?? description ?? ''
+  }
   const socialDescriptionSchema = object().shape({
     socialDescription: string().max(180, 'Character limit reached') // 180 characters just a few more words than 18 on average
   })
