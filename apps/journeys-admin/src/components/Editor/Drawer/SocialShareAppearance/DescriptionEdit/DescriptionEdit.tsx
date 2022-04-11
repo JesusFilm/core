f2be7d162a44a6d053f3046b56124b5f8f1e0d01
siyang <csiyang@yahoo.co.nz>
2022-04-11 02:22:17 +0000

    feat: form validation for title and description

--- apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/DescriptionEdit/DescriptionEdit.tsx
+++ apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/DescriptionEdit/DescriptionEdit.tsx
@@ -1,19 +1,61 @@
 import { ReactElement } from 'react'
 import TextField from '@mui/material/TextField'
+import { Formik, Form } from 'formik'
+import { object, string } from 'yup'
+import noop from 'lodash/noop'
 import { useJourney } from '../../../../../libs/context'
 
 export function DescriptionEdit(): ReactElement {
   const { description } = useJourney()
+
+  async function handleSubmit(e: React.FocusEvent): Promise<void> {
+    const target = e.target as HTMLInputElement
+    console.log('Description saved:', target.value)
+    // mutation here
+  }
+
+  const initialValues = { socialDescription: description ?? '' }
+  const socialDescriptionSchema = object().shape({
+    socialDescription: string().max(180, 'Character limit reached') // 180 characters just a few more words than 18 on average
+  })
+
   return (
-    <TextField
-      variant="filled"
-      label="Description"
-      helperText="Recommended length: up to 18 words"
-      fullWidth
-      value={description}
-      sx={{
-        pb: 6
-      }}
-    />
+    <Formik
+      initialValues={initialValues}
+      validationSchema={socialDescriptionSchema}
+      onSubmit={noop}
+    >
+      {({ values, touched, errors, handleChange, handleBlur }) => (
+        <Form>
+          <TextField
+            id="socialDescription"
+            name="socialDescription"
+            variant="filled"
+            label="Description"
+            fullWidth
+            multiline
+            maxRows={5}
+            value={values.socialDescription}
+            error={
+              touched.socialDescription === true &&
+              Boolean(errors.socialDescription)
+            }
+            helperText={
+              errors.socialDescription != null
+                ? errors.socialDescription
+                : 'Recommended length: up to 18 words'
+            }
+            onChange={handleChange}
+            onBlur={(e) => {
+              handleBlur(e)
+              errors.socialDescription == null && handleSubmit(e)
+            }}
+            sx={{
+              pb: 6
+            }}
+          />
+        </Form>
+      )}
+    </Formik>
   )
 }
--- apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/TitleEdit/TitleEdit.tsx
+++ apps/journeys-admin/src/components/Editor/Drawer/SocialShareAppearance/TitleEdit/TitleEdit.tsx
@@ -1,19 +1,58 @@
 import { ReactElement } from 'react'
 import TextField from '@mui/material/TextField'
+import { Formik, Form } from 'formik'
+import { object, string } from 'yup'
+import noop from 'lodash/noop'
 import { useJourney } from '../../../../../libs/context'
 
 export function TitleEdit(): ReactElement {
   const { title } = useJourney()
+
+  async function handleSubmit(e: React.FocusEvent): Promise<void> {
+    const target = e.target as HTMLInputElement
+    console.log('Title saved:', target.value)
+    // mutation here
+  }
+
+  const initialValues = { socialTitle: title ?? '' }
+  const socialTitleSchema = object().shape({
+    socialTitle: string().max(65, 'Character limit reached: 65') // 65 characters is about 2 lines of text on desktop view
+  })
+
   return (
-    <TextField
-      variant="filled"
-      label="Title"
-      helperText="Recommended length: 5 words"
-      fullWidth
-      value={title}
-      sx={{
-        pb: 4
-      }}
-    />
+    <Formik
+      initialValues={initialValues}
+      validationSchema={socialTitleSchema}
+      onSubmit={noop}
+    >
+      {({ values, touched, errors, handleChange, handleBlur }) => (
+        <Form>
+          <TextField
+            id="socialTitle"
+            name="socialTitle"
+            variant="filled"
+            label="Title"
+            fullWidth
+            multiline
+            maxRows={2}
+            value={values.socialTitle}
+            error={touched.socialTitle === true && Boolean(errors.socialTitle)}
+            helperText={
+              errors.socialTitle != null
+                ? errors.socialTitle
+                : 'Recommended length: 5 words'
+            }
+            onChange={handleChange}
+            onBlur={(e) => {
+              handleBlur(e)
+              errors.socialTitle == null && handleSubmit(e)
+            }}
+            sx={{
+              pb: 4
+            }}
+          />
+        </Form>
+      )}
+    </Formik>
   )
 }
