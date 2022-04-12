1fd62e391c3765b879da70c2d077dbfdc1ca4a50
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-04-12 04:04:18 +0000

    fix: reset dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -39,40 +39,7 @@ export default async () => {
   }
 
   // check PR has well-formed title
-  // Rules copied from @commitlint/config-conventional
-  const commitlintReport = await lint(danger.github.pr.title, {
-    'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
-    'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', 100],
-    'header-max-length': [2, 'always', 100],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test'
-      ]
-    ]
-  })
+  const commitlintReport = await lint(danger.github.pr.title)
   if (!commitlintReport.valid) {
     fail('Please ensure your PR title matches commitlint convention.')
     let errors = ''
