2519eba06dc2f264e7da17240ae676e685425e69
Mike Allison <mike@captainofbass.com>
2022-04-12 17:50:33 +0000

    fix: setupTests

---
+++ apps/watch/setupTests.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom'
+import './test/createMatchMedia'
+import { configure } from '@testing-library/react'
+
+configure({ asyncUtilTimeout: 2500 })
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: ({ src, alt }) => (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img src={src} alt={alt} />
+  )
+}))
