86e8642d0cac9a104405093654188e356a3ddfea
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-04-27 21:54:19 +0000

    fix: testing library apollo client create

--- apps/journeys/test/testingLibrary.tsx
+++ apps/journeys/test/testingLibrary.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from 'react'
 import { render, RenderOptions, RenderResult } from '@testing-library/react'
 import { ApolloProvider } from '@apollo/client'
-import client from '../src/libs/client'
+import { createApolloClient } from '../src/libs/client'
 
 function renderWithApolloClient(
   ui: React.ReactElement,
   { ...renderOptions }: Omit<RenderOptions, 'queries'> = {}
 ): RenderResult {
   function Wrapper({ children }): ReactElement {
+    const client = createApolloClient()
     return <ApolloProvider client={client}>{children}</ApolloProvider>
   }
   return render(ui, { wrapper: Wrapper, ...renderOptions })
