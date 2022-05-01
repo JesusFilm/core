784f22cf903c08c4b98f0920975cea926440abe7
siyang <csiyang@yahoo.co.nz>
2022-05-01 23:00:54 +0000

    refactor: useFormik to Formik

--- apps/journeys-admin/src/components/JourneyView/Menu/TitleDialog/TitleDialog.tsx
+++ apps/journeys-admin/src/components/JourneyView/Menu/TitleDialog/TitleDialog.tsx
@@ -1,10 +1,8 @@
 import { ReactElement } from 'react'
 import { useMutation, gql } from '@apollo/client'
-import FormControl from '@mui/material/FormControl'
-import FormLabel from '@mui/material/FormLabel'
 import TextField from '@mui/material/TextField'
 import { useSnackbar } from 'notistack'
-import { useFormik } from 'formik'
+import { Formik, Form } from 'formik'
 import { JourneyTitleUpdate } from '../../../../../__generated__/JourneyTitleUpdate'
 import { useJourney } from '../../../../libs/context'
 import { Dialog } from '../../../Dialog'
@@ -28,65 +26,65 @@ export function TitleDialog({ open, onClose }: TitleDialogProps): ReactElement {
   const journey = useJourney()
   const { enqueueSnackbar } = useSnackbar()
 
-  const handleSubmit = async (): Promise<void> => {
+  const handleSubmit = async (values): Promise<void> => {
     if (journey == null) return
-
-    const updatedJourney = { title: formik.values.title }
 
     try {
       await journeyUpdate({
-        variables: { id: journey.id, input: updatedJourney },
+        variables: { id: journey.id, input: { title: values.title } },
         optimisticResponse: {
           journeyUpdate: {
             id: journey.id,
             __typename: 'Journey',
-            ...updatedJourney
+            title: values.title
           }
         }
       })
+      onClose()
     } catch (error) {
       enqueueSnackbar('There was an error updating title', { variant: 'error' })
     }
   }
 
-  const handleClose = (): void => {
-    formik.resetForm()
-    onClose()
+  function handleClose(resetForm: () => void): () => void {
+    return () => {
+      resetForm()
+      onClose()
+    }
   }
-
-  const formik = useFormik({
-    initialValues: {
-      title: journey?.title ?? ''
-    },
-    onSubmit: handleSubmit,
-    enableReinitialize: true
-  })
 
   return (
     <>
-      <Dialog
-        open={open}
-        handleClose={handleClose}
-        dialogTitle={{ title: 'Edit Title' }}
-        dialogAction={{
-          onSubmit: handleSubmit,
-          closeLabel: 'Cancel'
-        }}
-      >
-        <form onSubmit={handleSubmit}>
-          <FormControl component="fieldset" sx={{ width: '100%' }}>
-            <FormLabel component="legend" aria-label="form-update-title" />
-            <TextField
-              id="title"
-              name="title"
-              hiddenLabel
-              value={formik.values.title}
-              variant="filled"
-              onChange={formik.handleChange}
-            />
-          </FormControl>
-        </form>
-      </Dialog>
+      {journey != null && (
+        <Formik
+          initialValues={{ title: journey.title }}
+          onSubmit={handleSubmit}
+        >
+          {({ values, handleChange, handleSubmit, resetForm }) => (
+            <Dialog
+              open={open}
+              handleClose={handleClose(resetForm)}
+              dialogTitle={{ title: 'Edit Title' }}
+              dialogAction={{
+                onSubmit: handleSubmit,
+                closeLabel: 'Cancel'
+              }}
+            >
+              <Form>
+                <TextField
+                  id="title"
+                  name="title"
+                  hiddenLabel
+                  fullWidth
+                  value={values.title}
+                  variant="filled"
+                  onChange={handleChange}
+                />
+              </Form>
+            </Dialog>
+          )}
+        </Formik>
+      )}
     </>
   )
 }
