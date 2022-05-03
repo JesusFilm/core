d385a901537337aa6f730be930778aece9ee0209
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-05-04 10:31:43 +1200

    fix: expanded card displays theme (#545)
    * fix: full card cover uses correct theme
    
    * fix: full card cover types
    
    * refactor: rename full card cover to expanded cover
    
    * refactor: moved box and styling into expanded cover component
    
    * refactor: cover and card cover reanmed to contained cover
    
    * test: added tetss for expanded cover
    
    * refactor: card components and tests
    
    Co-authored-by: John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
    Co-authored-by: Ziwei Liu <ziwei.liu.23@gmail.com>

---
+++ libs/journeys/ui/src/components/Card/ExpandedCover/ExpandedCover.tsx
@@ -0,0 +1,64 @@
+import { ReactElement, ReactNode } from 'react'
+import Box from '@mui/material/Box'
+
+interface ExpandedCoverProps {
+  children: ReactNode
+  backgroundBlur?: string
+}
+
+export function ExpandedCover({
+  children,
+  backgroundBlur
+}: ExpandedCoverProps): ReactElement {
+  return (
+    <Box
+      data-testid="ExpandedCover"
+      sx={{
+        flexGrow: 1,
+        overflow: 'auto',
+        display: 'flex',
+        padding: (theme) => ({
+          xs: theme.spacing(7),
+          sm: theme.spacing(7, 10),
+          md: theme.spacing(10)
+        }),
+        borderRadius: (theme) => theme.spacing(4),
+        justifyContent: 'center'
+      }}
+    >
+      <Box
+        sx={{
+          margin: 'auto',
+          width: '100%',
+          maxWidth: 500,
+          zIndex: 1,
+          '& > *': {
+            '&:first-child': { mt: 0 },
+            '&:last-child': { mb: 0 }
+          }
+        }}
+      >
+        {children}
+      </Box>
+      {backgroundBlur != null && (
+        <Box
+          data-testid="expandedBlurBackground"
+          sx={{
+            position: 'absolute',
+            width: '100%',
+            height: '110%',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '100% 100%',
+            backgroundPosition: '0% 0%',
+            left: 0,
+            top: '-10%',
+            zIndex: -1,
+            transform: 'scaleY(-1)',
+            backgroundBlendMode: 'hard-light',
+            backgroundImage: `url(${backgroundBlur}), url(${backgroundBlur})`
+          }}
+        />
+      )}
+    </Box>
+  )
+}
