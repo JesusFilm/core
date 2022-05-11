629fd4e70a05712e8363006a29440511a60bf272
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-05-11 13:21:32 +1200

    feat: add button click event - backend (#571)
    * feat: add button click event
    
    * fix: button resolver
    
    * chore: regnerate schema
    
    * refactor: resolver to pr feedback
    
    * fix: conflicts

--- apps/api-journeys/src/app/modules/event/event.module.ts
+++ apps/api-journeys/src/app/modules/event/event.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '@core/nest/database'
 import { EventService } from './event.service'
 import { EventResolver } from './event.resolver'
+import { ButtonClickEventResolver } from './button/button.resolver'
 import { JourneyViewEventResolver } from './journey/journey.resolver'
 import { RadioQuestionSubmissionEventResolver } from './radioQuestion/radioQuestion.resolver'
 import { SignUpSubmissionEventResolver } from './signUp/signUp.resolver'
@@ -13,6 +14,7 @@ import { StepViewEventResolver } from './step/step.resolver'
   providers: [
     EventService,
     EventResolver,
+    ButtonClickEventResolver,
     JourneyViewEventResolver,
     RadioQuestionSubmissionEventResolver,
     SignUpSubmissionEventResolver,
