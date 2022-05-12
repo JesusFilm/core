658600382f436306db687d4916fb01a28c5472b1
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-05-12 02:08:54 +0000

    fix: remove sudo

--- tools/scripts/vercel-build.sh
+++ tools/scripts/vercel-build.sh
@@ -3,9 +3,9 @@
 # this script is designed to run on Vercel build step (Amazon Linux 2)
 
 # install doppler
-sudo rpm --import 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key'
-curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/config.rpm.txt' | sudo tee /etc/yum.repos.d/doppler-cli.repo
-sudo yum update && sudo yum install doppler
+rpm --import 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key'
+curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/config.rpm.txt' | tee /etc/yum.repos.d/doppler-cli.repo
+yum install doppler -y
 
 # fetch secrets from doppler
 npx nx fetch-secrets $APP_NAME
