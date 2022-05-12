4f7e3c323d6592309e4629d2c0796d1879d46c05
Siyang <csiyang@yahoo.co.nz>
2022-05-12 20:32:33 +0000

    feat: add resolvers and fix test

--- apps/api-journeys/src/app/modules/event/video/video.resolver.ts
+++ apps/api-journeys/src/app/modules/event/video/video.resolver.ts
@@ -4,20 +4,54 @@ import { GqlAuthGuard } from '@core/nest/gqlAuthGuard'
 import { CurrentUserId } from '@core/nest/decorators'
 import {
   VideoPlayEvent,
-  VideoPlayEventCreateInput
+  VideoPlayEventCreateInput,
+  VideoMuteEvent,
+  VideoMuteEventCreateInput,
+  VideoFullscreenEvent,
+  VideoFullscreenEventCreateInput,
+  VideoViewEvent,
+  VideoViewEventCreateInput
 } from '../../../__generated__/graphql'
 import { EventService } from '../event.service'
 
-@Resolver('VideoPlayEvent')
-export class VideoPlayEventResolver {
+@Resolver('VideoEvent')
+@UseGuards(GqlAuthGuard)
+export class VideoEventResolver {
   constructor(private readonly eventService: EventService) {}
+
   @Mutation()
-  @UseGuards(GqlAuthGuard)
   async videoPlayEventCreate(
     @CurrentUserId() userId: string,
     @Args('input') input: VideoPlayEventCreateInput & { __typename }
   ): Promise<VideoPlayEvent> {
     input.__typename = 'VideoPlayEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoMuteEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoMuteEventCreateInput & { __typename }
+  ): Promise<VideoMuteEvent> {
+    input.__typename = 'VideoMuteEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoFullscreenEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoFullscreenEventCreateInput & { __typename }
+  ): Promise<VideoFullscreenEvent> {
+    input.__typename = 'VideoFullscreenEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoViewEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoViewEventCreateInput & { __typename }
+  ): Promise<VideoViewEvent> {
+    input.__typename = 'VideoViewEvent'
     return await this.eventService.save({ ...input, userId })
   }
 }
