8f8d21626da70ec3c7a7420dbba4e978956af395
Siyang <csiyang@yahoo.co.nz>
2022-05-12 23:01:09 +0000

    feat: added resolvers

--- apps/api-journeys/src/app/modules/event/video/video.resolver.ts
+++ apps/api-journeys/src/app/modules/event/video/video.resolver.ts
@@ -3,8 +3,20 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql'
 import { GqlAuthGuard } from '@core/nest/gqlAuthGuard'
 import { CurrentUserId } from '@core/nest/decorators'
 import {
+  VideoStartEvent,
+  VideoStartEventCreateInput,
   VideoPlayEvent,
-  VideoPlayEventCreateInput
+  VideoPlayEventCreateInput,
+  VideoPauseEvent,
+  VideoPauseEventCreateInput,
+  VideoCompleteEvent,
+  VideoCompleteEventCreateInput,
+  VideoExpandEvent,
+  VideoExpandEventCreateInput,
+  VideoCollapseEvent,
+  VideoCollapseEventCreateInput,
+  VideoProgressEvent,
+  VideoProgressEventCreateInput
 } from '../../../__generated__/graphql'
 import { EventService } from '../event.service'
 
@@ -12,12 +24,67 @@ import { EventService } from '../event.service'
 @UseGuards(GqlAuthGuard)
 export class VideoEventResolver {
   constructor(private readonly eventService: EventService) {}
+
+  @Mutation()
+  async videoStartEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoStartEventCreateInput & { __typename }
+  ): Promise<VideoStartEvent> {
+    input.__typename = 'VideoStartEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
   @Mutation()
   async videoPlayEventCreate(
     @CurrentUserId() userId: string,
     @Args('input') input: VideoPlayEventCreateInput & { __typename }
   ): Promise<VideoPlayEvent> {
     input.__typename = 'VideoPlayEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoPauseEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoPauseEventCreateInput & { __typename }
+  ): Promise<VideoPauseEvent> {
+    input.__typename = 'VideoPauseEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoCompleteEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoCompleteEventCreateInput & { __typename }
+  ): Promise<VideoCompleteEvent> {
+    input.__typename = 'VideoCompleteEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoExpandEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoExpandEventCreateInput & { __typename }
+  ): Promise<VideoExpandEvent> {
+    input.__typename = 'VideoExpandEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoCollapseEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoCollapseEventCreateInput & { __typename }
+  ): Promise<VideoCollapseEvent> {
+    input.__typename = 'VideoCollapseEvent'
+    return await this.eventService.save({ ...input, userId })
+  }
+
+  @Mutation()
+  async videoProgressEventCreate(
+    @CurrentUserId() userId: string,
+    @Args('input') input: VideoProgressEventCreateInput & { __typename }
+  ): Promise<VideoProgressEvent> {
+    input.__typename = 'VideoProgressEvent'
     return await this.eventService.save({ ...input, userId })
   }
 }
