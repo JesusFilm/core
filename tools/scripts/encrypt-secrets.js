a20e89a0ebd03de320c2c5beeabaf09d6c5a5e72
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-05-12 03:05:46 +0000

    fix: remove gitops-secrets

--- tools/scripts/encrypt-secrets.js
+++
@@ -1,8 +0,0 @@
-const secrets = require("gitops-secrets");
-
-async function main() {
-  const payload = await secrets.providers.doppler.fetch();
-  secrets.build(payload, { path: process.env.LIBS_SECRETS_PATH });
-}
-
-main();
