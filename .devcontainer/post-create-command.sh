a3a6c6840548e848ae76c437d5e33793942c346a
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-05-13 16:13:26 +1200

    chore: add doppler (#592)

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -22,3 +22,9 @@ npm i
 curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 sudo mv bin/act /usr/bin/
 rm -rf bin
+
+# install doppler
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
+echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
+sudo apt-get update && sudo apt-get install doppler
