12f233c2fe0f35547cde274e3ff7967ac310fc67
Siyang <csiyang@yahoo.co.nz>
2022-05-23 22:42:59 +0000

    refactor: extract get heading logic

---
+++ libs/journeys/ui/src/libs/getStepHeading/getStepHeading.spec.ts
@@ -0,0 +1,80 @@
+import { JourneyFields_blocks_StepBlock as StepBlock } from '../context/__generated__/JourneyFields'
+import { TreeBlock } from '..'
+import { getStepHeading } from '.'
+
+describe('getStepHeading', () => {
+  it('returns text of first typography block', () => {
+    const stepBlock: TreeBlock<StepBlock> = {
+      __typename: 'StepBlock',
+      id: 'step.id',
+      parentBlockId: null,
+      parentOrder: null,
+      locked: false,
+      nextBlockId: null,
+      children: [
+        {
+          __typename: 'CardBlock',
+          id: 'card.id',
+          parentBlockId: 'step.id',
+          parentOrder: null,
+          backgroundColor: null,
+          coverBlockId: null,
+          themeMode: null,
+          themeName: null,
+          fullscreen: false,
+          children: [
+            {
+              __typename: 'TypographyBlock',
+              id: 'typography1.id',
+              parentBlockId: 'card.id',
+              parentOrder: 0,
+              align: null,
+              color: null,
+              variant: null,
+              content: 'Heading',
+              children: []
+            },
+            {
+              __typename: 'TypographyBlock',
+              id: 'typograph2.id',
+              parentBlockId: 'card.id',
+              parentOrder: 1,
+              align: null,
+              color: null,
+              variant: null,
+              content: 'Description',
+              children: []
+            }
+          ]
+        }
+      ]
+    }
+    expect(getStepHeading(stepBlock)).toEqual('Heading')
+  })
+
+  it('returns undefined if there are no typography blocks', () => {
+    const stepBlock: TreeBlock<StepBlock> = {
+      __typename: 'StepBlock',
+      id: 'step.id',
+      parentBlockId: null,
+      parentOrder: null,
+      locked: false,
+      nextBlockId: null,
+      children: [
+        {
+          __typename: 'CardBlock',
+          id: 'card.id',
+          parentBlockId: 'step.id',
+          parentOrder: null,
+          backgroundColor: null,
+          coverBlockId: null,
+          themeMode: null,
+          themeName: null,
+          fullscreen: false,
+          children: []
+        }
+      ]
+    }
+    expect(getStepHeading(stepBlock)).toBeUndefined()
+  })
+})
---
+++ libs/journeys/ui/src/libs/getStepHeading/getStepHeading.ts
@@ -0,0 +1,21 @@
+import {
+  JourneyFields_blocks_StepBlock as StepBlock,
+  JourneyFields_blocks_TypographyBlock as TypographyBlock
+} from '../context/__generated__/JourneyFields'
+import { TreeBlock } from '..'
+
+function flatten(children: TreeBlock[]): TreeBlock[] {
+  return children.reduce<TreeBlock[]>(
+    (result, item) => [...result, item, ...flatten(item.children)],
+    []
+  )
+}
+
+export function getStepHeading(step: TreeBlock<StepBlock>): string | undefined {
+  const descendants = flatten(step?.children ?? [])
+  const heading = descendants.find(
+    (block) => block.__typename === 'TypographyBlock'
+  ) as TreeBlock<TypographyBlock> | undefined
+
+  return heading?.content
+}
