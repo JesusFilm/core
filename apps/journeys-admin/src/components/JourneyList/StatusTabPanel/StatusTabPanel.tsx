8aa57eda63a864ee6e3cf8fa1fe27b0216a97fb7
Siyang <csiyang@yahoo.co.nz>
2022-06-08 22:21:10 +0000

    feat: update chip positiona and styling

--- apps/journeys-admin/src/components/JourneyList/StatusTabPanel/StatusTabPanel.tsx
+++ apps/journeys-admin/src/components/JourneyList/StatusTabPanel/StatusTabPanel.tsx
@@ -2,8 +2,12 @@ import { ReactElement, SyntheticEvent, useState } from 'react'
 import { sortBy } from 'lodash'
 import Card from '@mui/material/Card'
 import { TabPanel, tabA11yProps } from '@core/shared/ui'
+import { Theme } from '@mui/material/styles'
 import Tab from '@mui/material/Tab'
 import Tabs from '@mui/material/Tabs'
+import Chip from '@mui/material/Chip'
+import Box from '@mui/material/Box'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { GetJourneys_journeys as Journey } from '../../../../__generated__/GetJourneys'
 import { JourneySort, SortOrder } from '../JourneySort'
 import { JourneyCard } from '../JourneyCard'
@@ -18,6 +22,7 @@ export function StatusTabPanel({
 }: StatusTabPanelProps): ReactElement {
   const [sortOrder, setSortOrder] = useState<SortOrder>()
   const [activeTab, setActiveTab] = useState(0)
+  const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
 
   const sortedJourneys =
     sortOrder === SortOrder.TITLE
@@ -37,10 +42,15 @@ export function StatusTabPanel({
     <>
       {journeys != null ? (
         <>
-          <JourneySort sortOrder={sortOrder} onChange={setSortOrder} />
+          {!smUp && (
+            <Box sx={{ my: 4, ml: 6 }}>
+              <JourneySort sortOrder={sortOrder} onChange={setSortOrder} />
+            </Box>
+          )}
           <Card
             variant="outlined"
             sx={{
+              mt: { xs: 0, sm: 6 },
               borderColor: 'divider',
               borderBottom: 'none',
               borderTopLeftRadius: { xs: 0, sm: 12 },
@@ -56,6 +66,18 @@ export function StatusTabPanel({
               data-testid="journey-list"
             >
               <Tab label="Active" {...tabA11yProps('status-panel', 0)} />
+              {smUp && (
+                <Box
+                  sx={{
+                    mr: 6,
+                    ml: 'auto',
+                    mt: 3,
+                    mb: 2
+                  }}
+                >
+                  <JourneySort sortOrder={sortOrder} onChange={setSortOrder} />
+                </Box>
+              )}
             </Tabs>
           </Card>
           <TabPanel name="status-panel" value={activeTab} index={0}>
@@ -64,6 +86,45 @@ export function StatusTabPanel({
         </>
       ) : (
         <>
+          {!smUp && (
+            <Box sx={{ my: 4, ml: 6 }}>
+              <Chip label="Sort By" variant="outlined" disabled />
+            </Box>
+          )}
+          <Card
+            variant="outlined"
+            sx={{
+              borderColor: 'divider',
+              borderBottom: 'none',
+              mt: { xs: 0, sm: 6 },
+              borderTopLeftRadius: { xs: 0, sm: 12 },
+              borderTopRightRadius: { xs: 0, sm: 12 },
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0
+            }}
+          >
+            <Tabs
+              value={activeTab}
+              aria-label="journey status tabs"
+              data-testid="journey-list"
+            >
+              <Tab label="Active" disabled />
+              <Tab label="Archived" disabled />
+              <Tab label="Deleted" disabled />
+              {smUp && (
+                <Box
+                  sx={{
+                    mr: 6,
+                    ml: 'auto',
+                    mt: 3,
+                    mb: 2
+                  }}
+                >
+                  <Chip label="Sort By" variant="outlined" disabled />
+                </Box>
+              )}
+            </Tabs>
+          </Card>
           <JourneyCard />
           <JourneyCard />
           <JourneyCard />
