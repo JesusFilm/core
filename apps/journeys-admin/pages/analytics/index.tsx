2e944bd35943a4c3894021c3a40226b2ef0c47c7
Siyang <csiyang@yahoo.co.nz>
2022-06-12 23:26:06 +0000

    feat: add anayltics page

---
+++ apps/journeys-admin/pages/analytics/index.tsx
@@ -0,0 +1,44 @@
+import { ReactElement } from 'react'
+import {
+  AuthAction,
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR
+} from 'next-firebase-auth'
+import { NextSeo } from 'next-seo'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'react-i18next'
+import { PageWrapper } from '../../src/components/PageWrapper'
+import i18nConfig from '../../next-i18next.config'
+
+function IndexPage(): ReactElement {
+  const { t } = useTranslation('apps-journeys-admin')
+  const AuthUser = useAuthUser()
+
+  return (
+    <>
+      <NextSeo title={t('Anayltics')} />
+      <PageWrapper title={t('Anayltics')} authUser={AuthUser}>
+        <div>Anayltics are coming</div>
+      </PageWrapper>
+    </>
+  )
+}
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
+})(async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(
+        locale ?? 'en',
+        ['apps-journeys-admin', 'libs-journeys-ui'],
+        i18nConfig
+      ))
+    }
+  }
+})
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN
+})(IndexPage)
