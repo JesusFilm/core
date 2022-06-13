ad2d6da80ba05555f8df1237153c99b307baf2f8
Siyang <csiyang@yahoo.co.nz>
2022-06-13 04:06:25 +0000

    feat: get power bi embed

--- apps/journeys-admin/src/components/AnalyticsFullReport/AnalyticsFullReport.tsx
+++ apps/journeys-admin/src/components/AnalyticsFullReport/AnalyticsFullReport.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, useState } from 'react'
 import { PowerBIEmbed } from 'powerbi-client-react'
 import { models } from 'powerbi-client'
+import { getPowerBiEmbed } from '@core/nest/powerBi'
 
 export function AnalyticsFullReport(): ReactElement {
   const [embedConfig, setEmbedConfig] =
@@ -11,6 +12,22 @@ export function AnalyticsFullReport(): ReactElement {
       accessToken: undefined,
       settings: undefined
     })
+
+  const reportConfig = getPowerBiEmbed(
+    {
+      apiUrl: 'https://api.powerbi.com/',
+      authorityUri: 'https://login.microsoftonline.com/common/v2.0',
+      clientId: '167a32da-68dd-421f-a075-6e333c0d5d09',
+      clientSecret: '0V28Q~BBaNSauuXAqzaD0Vh59dqV2s7EqL5niag0',
+      scope: 'https://analysis.windows.net/powerbi/api',
+      tenantId: '149cd8c2-5d4a-4dfa-8563-f5c0708c5eef',
+      workspaceId: '180ff7df-efa8-452b-bf76-487506308a75'
+    },
+    '26f7b2bf-e6de-4ccc-847e-2fe62acdd880',
+    'test-user-id'
+  )
+
+  console.log(reportConfig)
 
   return (
     <>
