6f8fc9e347246808c2026c7988d01822ec07c862
Siyang <csiyang@yahoo.co.nz>
2022-06-13 03:31:19 +0000

    feat: embed blank report

--- apps/journeys-admin/pages/analytics/index.tsx
+++ apps/journeys-admin/pages/analytics/index.tsx
@@ -8,13 +8,23 @@ import {
 import { NextSeo } from 'next-seo'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
+import dynamic from 'next/dynamic'
 import { PageWrapper } from '../../src/components/PageWrapper'
 import i18nConfig from '../../next-i18next.config'
-import { AnalyticsFullReport } from '../../src/components/AnalyticsFullReport'
 
 function IndexPage(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const AuthUser = useAuthUser()
+
+  // powerbi needs dynamic import, see issue: https://github.com/microsoft/powerbi-client-react/issues/65
+  const AnalyticsFullReport = dynamic(
+    async () =>
+      await import(
+        /* webpackChunkName: "DynamicAnalyticsFullReport" */
+        '../../src/components/AnalyticsFullReport'
+      ).then((res) => res.AnalyticsFullReport),
+    { ssr: false }
+  )
 
   return (
     <>
--- apps/journeys-admin/src/components/AnalyticsFullReport/AnalyticsFullReport.tsx
+++ apps/journeys-admin/src/components/AnalyticsFullReport/AnalyticsFullReport.tsx
@@ -1,5 +1,21 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
+import { PowerBIEmbed } from 'powerbi-client-react'
+import { models } from 'powerbi-client'
 
 export function AnalyticsFullReport(): ReactElement {
-  return <div>Full report</div>
+  const [embedConfig, setEmbedConfig] =
+    useState<models.IReportEmbedConfiguration>({
+      type: 'report',
+      embedUrl: undefined,
+      tokenType: models.TokenType.Embed,
+      accessToken: undefined,
+      settings: undefined
+    })
+
+  return (
+    <>
+      <div>Full report</div>
+      <PowerBIEmbed embedConfig={embedConfig} />
+    </>
+  )
 }
