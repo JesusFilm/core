3e6fa122207d0409d68652bf1ad11b4492c575a9
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-06-17 02:57:07 +0000

    chore: add comment

--- libs/shared/ui/src/libs/getLaunchDarklyClient/getLaunchDarklyClient.ts
+++ libs/shared/ui/src/libs/getLaunchDarklyClient/getLaunchDarklyClient.ts
@@ -2,6 +2,10 @@ import { init, LDClient } from 'launchdarkly-node-server-sdk'
 
 let launchDarklyClient: LDClient
 
+/**
+ * This requires the LAUNCH_DARKLY_SDK_KEY environment variable to be set.
+ * @returns a LaunchDarkly server-side client as a singleton.
+ */
 export async function getLaunchDarklyClient(): Promise<LDClient> {
   if (launchDarklyClient != null) return launchDarklyClient
 
