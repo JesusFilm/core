18e79764a279228c3d7748cfa0b9d39e4c1017d1
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-06-17 05:21:28 +0000

    feat: card and block duplication api

--- apps/api-journeys/src/app/modules/block/block.service.ts
+++ apps/api-journeys/src/app/modules/block/block.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common'
+import { v4 as uuidv4 } from 'uuid'
 import { aql } from 'arangojs'
 import { BaseService } from '@core/nest/database'
 import { DocumentCollection } from 'arangojs/collection'
-import { KeyAsId, idAsKey } from '@core/nest/decorators'
+import { KeyAsId, idAsKey, keyAsId } from '@core/nest/decorators'
 
 import { Block, Journey } from '../../__generated__/graphql'
 
@@ -84,6 +85,140 @@ export class BlockService extends BaseService {
       return await this.reorderSiblings(siblings)
     }
     return []
+  }
+
+  async duplicateBlock(
+    id: string,
+    journeyId: string,
+    parentOrder?: number
+  ): Promise<Block[]> {
+    const block = idAsKey(await this.get(id)) as Block & { _key: string }
+
+    if (block.journeyId === journeyId) {
+      const blockAndChildrenData = await this.getDuplicateBlockAndChildren(
+        id,
+        journeyId,
+        block.parentBlockId ?? null
+      )
+      const duplicateBlockAndChildren = await this.saveAll(blockAndChildrenData)
+      const duplicatedBlock = {
+        ...duplicateBlockAndChildren[0],
+        _key: duplicateBlockAndChildren[0].id
+      }
+      const duplicatedChildren = duplicateBlockAndChildren.slice(1)
+
+      const siblings = await this.getSiblingsInternal(
+        journeyId,
+        block.parentBlockId
+      )
+      const insertIndex =
+        parentOrder != null
+          ? Math.min(Math.max(parentOrder, 0), siblings.length + 1)
+          : siblings.length + 1
+      siblings.splice(insertIndex, 0, duplicatedBlock)
+      const reorderedBlocks = await this.reorderSiblings(siblings)
+
+      return reorderedBlocks.concat(duplicatedChildren)
+    }
+    return []
+  }
+
+  async getDuplicateBlockAndChildren(
+    id: string,
+    journeyId: string,
+    parentBlockId: string | null,
+    duplicateId?: string
+  ): Promise<Array<Block & { _key: string }>> {
+    const block = keyAsId(await this.get(id)) as Block & {
+      __typename: string
+      startIconId: string | null
+      endIconId: string | null
+      coverBlockId: string | null
+      posterBlockId: string | null
+    }
+    const duplicateBlockId = duplicateId ?? uuidv4()
+
+    const children = await this.getBlocksForParentId(block.id, journeyId)
+    // console.log('children', block.id, children)
+    const childIds = new Map()
+    children.forEach((block) => {
+      childIds.set(block.id, uuidv4())
+    })
+
+    const duplicateBlock =
+      block.__typename === 'ButtonBlock'
+        ? {
+            ...block,
+            _key: duplicateBlockId,
+            id: duplicateBlockId,
+            parentBlockId,
+            startIconId: childIds.get(block.startIconId) ?? null,
+            endIconId: childIds.get(block.endIconId) ?? null
+          }
+        : block.__typename === 'CardBlock'
+        ? {
+            ...block,
+            _key: duplicateBlockId,
+            id: duplicateBlockId,
+            parentBlockId,
+            coverBlockId: childIds.get(block.coverBlockId) ?? null
+          }
+        : block.__typename === 'VideoBlock'
+        ? {
+            ...block,
+            _key: duplicateBlockId,
+            id: duplicateBlockId,
+            parentBlockId,
+            posterBlockId: childIds.get(block.posterBlockId) ?? null
+          }
+        : block.__typename === 'StepBlock'
+        ? {
+            ...block,
+            _key: duplicateBlockId,
+            id: duplicateBlockId,
+            parentBlockId,
+            nextBlockId: null
+          }
+        : {
+            ...block,
+            _key: duplicateBlockId,
+            id: duplicateBlockId,
+            parentBlockId
+          }
+
+    const duplicateChildren = await Promise.all(
+      children.map(async (block) => {
+        return await this.getDuplicateBlockAndChildren(
+          block.id,
+          journeyId,
+          duplicateBlockId,
+          childIds.get(block.id)
+        )
+      })
+    )
+    const childrenFlatArray = duplicateChildren.reduce<
+      Array<Block & { _key: string }>
+    >((acc, val) => {
+      return acc.concat(val)
+    }, [])
+
+    return [duplicateBlock, ...childrenFlatArray]
+  }
+
+  @KeyAsId()
+  async getBlocksForParentId(
+    parentId: string,
+    journeyId: string
+  ): Promise<Block[]> {
+    const res = await this.db.query(aql`
+      FOR block IN ${this.collection}
+        FILTER block.journeyId == ${journeyId}
+          AND block.parentBlockId == ${parentId}
+        SORT block.parentOrder ASC
+        RETURN block
+    `)
+
+    return await res.all()
   }
 
   protected async removeAllBlocksForParentId(
