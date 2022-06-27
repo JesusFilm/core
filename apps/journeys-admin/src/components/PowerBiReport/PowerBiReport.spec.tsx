8333253516ede633f747b3a4d1f79d4fb23fc350
Siyang <csiyang@yahoo.co.nz>
2022-06-27 03:44:58 +0000

    fix: embed url

--- apps/journeys-admin/src/components/PowerBiReport/PowerBiReport.spec.tsx
+++ apps/journeys-admin/src/components/PowerBiReport/PowerBiReport.spec.tsx
@@ -18,7 +18,8 @@ describe('PowerBiReport', () => {
             result: {
               data: {
                 adminJourneysReport: {
-                  embedUrl: 'url',
+                  embedUrl:
+                    'https://app.powerbi.com/reportEmbed?reportId=f6bfd646-b718-44dc-a378-b73e6b528204&groupId=be8908da-da25-452e-b220-163f52476cdd&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLU5PUlRILUNFTlRSQUwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJhbmd1bGFyT25seVJlcG9ydEVtYmVkIjp0cnVlLCJjZXJ0aWZpZWRUZWxlbWV0cnlFbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWUsInNraXBab25lUGF0Y2giOnRydWV9fQ%3d%3d',
                   accessToken: 'token'
                 }
               }
