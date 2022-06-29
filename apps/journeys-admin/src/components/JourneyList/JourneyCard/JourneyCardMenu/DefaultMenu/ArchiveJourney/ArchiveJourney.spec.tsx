52a02dde7862269d0b962b99a9022b7bf748600b
Siyang <csiyang@yahoo.co.nz>
2022-06-29 00:09:33 +0000

    test: add unit test for archive journey

---
+++ apps/journeys-admin/src/components/JourneyList/JourneyCard/JourneyCardMenu/DefaultMenu/ArchiveJourney/ArchiveJourney.spec.tsx
@@ -0,0 +1,196 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import { SnackbarProvider } from 'notistack'
+import { JourneyStatus } from '../../../../../../../__generated__/globalTypes'
+import { JOURNEY_ARCHIVE, JOURNEY_UNARCHIVE } from './ArchiveJourney'
+import { ArchiveJourney } from '.'
+
+describe('ArchiveJourney', () => {
+  const handeClose = jest.fn()
+
+  it('should archive journey', async () => {
+    const result = jest.fn(() => ({
+      data: {
+        journeyArchive: {
+          id: 'journey-id',
+          __typename: 'Journey',
+          status: JourneyStatus.archived
+        }
+      }
+    }))
+
+    const { getByRole, getByText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: JOURNEY_ARCHIVE,
+              variables: {
+                ids: ['journey-id']
+              }
+            },
+            result
+          }
+        ]}
+      >
+        <SnackbarProvider>
+          <ArchiveJourney
+            status={JourneyStatus.draft}
+            id={'journey-id'}
+            published={false}
+            handleClose={handeClose}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('menuitem', { name: 'Archive' }))
+    await waitFor(() => expect(result).toHaveBeenCalled())
+    expect(getByText('Journey Archived')).toBeInTheDocument()
+    expect(handeClose).toHaveBeenCalled()
+  })
+
+  it('should show error if archive fails', async () => {
+    const { getByRole, getByText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: JOURNEY_ARCHIVE,
+              variables: {
+                ids: ['journey-id']
+              }
+            },
+            error: new Error('error')
+          }
+        ]}
+      >
+        <SnackbarProvider>
+          <ArchiveJourney
+            status={JourneyStatus.draft}
+            id={'journey-id'}
+            published={false}
+            handleClose={handeClose}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('menuitem', { name: 'Archive' }))
+    await waitFor(() => expect(getByText('error')).toBeInTheDocument())
+  })
+
+  it('should unarchive journey to draft', async () => {
+    const result = jest.fn(() => ({
+      data: {
+        journeyArchive: {
+          id: 'journey-id',
+          __typename: 'Journey',
+          status: JourneyStatus.draft
+        }
+      }
+    }))
+
+    const { getByRole, getByText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: JOURNEY_UNARCHIVE,
+              variables: {
+                ids: ['journey-id']
+              }
+            },
+            result
+          }
+        ]}
+      >
+        <SnackbarProvider>
+          <ArchiveJourney
+            status={JourneyStatus.archived}
+            id={'journey-id'}
+            published={false}
+            handleClose={handeClose}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('menuitem', { name: 'Unarchive' }))
+    await waitFor(() => expect(result).toHaveBeenCalled())
+    expect(getByText('Journey Unarchived')).toBeInTheDocument()
+    expect(handeClose).toHaveBeenCalled()
+  })
+
+  it('should unarchive journey to published', async () => {
+    const result = jest.fn(() => ({
+      data: {
+        journeyArchive: {
+          id: 'journey-id',
+          __typename: 'Journey',
+          status: JourneyStatus.published
+        }
+      }
+    }))
+
+    const { getByRole, getByText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: JOURNEY_UNARCHIVE,
+              variables: {
+                ids: ['journey-id']
+              }
+            },
+            result
+          }
+        ]}
+      >
+        <SnackbarProvider>
+          <ArchiveJourney
+            status={JourneyStatus.archived}
+            id={'journey-id'}
+            published={true}
+            handleClose={handeClose}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('menuitem', { name: 'Unarchive' }))
+    await waitFor(() => expect(result).toHaveBeenCalled())
+    expect(getByText('Journey Unarchived')).toBeInTheDocument()
+    expect(handeClose).toHaveBeenCalled()
+  })
+
+  it('should show error if unarchive fails', async () => {
+    const { getByRole, getByText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: JOURNEY_UNARCHIVE,
+              variables: {
+                ids: ['journey-id']
+              }
+            },
+            error: new Error('error')
+          }
+        ]}
+      >
+        <SnackbarProvider>
+          <ArchiveJourney
+            status={JourneyStatus.archived}
+            id={'journey-id'}
+            published={false}
+            handleClose={handeClose}
+          />
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('menuitem', { name: 'Unarchive' }))
+    await waitFor(() => expect(getByText('error')).toBeInTheDocument())
+  })
+})
