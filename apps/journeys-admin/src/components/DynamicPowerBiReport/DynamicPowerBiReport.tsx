67ceb9f9837e077320e0f577b227b878a1c2d99f
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-06-30 14:31:27 +1200

    feat: add analytics page (#717)
    * feat: embed report
    
    * chore: move to power bi folder
    
    * feat: add anayltics page
    
    * feat: add analytics full report component
    
    * chore: change name to getPowerBiEmbed
    
    * chore: add documentation
    
    * feat: add api for adminJourneysReport
    
    * feat: embed blank report
    
    * feat: get power bi embed
    
    * refactor: get token
    
    * fix: incorrect api call to power bi
    
    * fix: typing of dynamic import
    
    * refactor: extract power bi report loading
    
    * chore: debugging report
    
    * refactor: move mutation into common report
    
    * refactor: move common report into own folder
    
    * feat: report stretches to height
    
    * chore: fix spelling
    
    * test: add story
    
    * feat: add report skeleton
    
    * test: add test for mutations
    
    * test: unit test and story for powerBi report
    
    * test: add test for report skeleton
    
    * test: add default story to journeys full report
    
    * fix: journey full report story
    
    * test: add stories for journeys full report
    
    * test: unit test for journeys full report
    
    * chore: remove swc package changes
    
    * chore: remove redundant files
    
    * chore: codegen
    
    * docs: add comment for crypto import
    
    * refactor: remove journeys full report
    
    * feat: add feature flag check
    
    * chore: extract translations
    
    * fix: embed url
    
    * fix: test stub url
    
    * chore: minor fix up from PR feedback
    
    * chore: remove comment
    
    * fix: url casing
    
    * fix: row level security
    
    * fix: tests for roles
    
    * feat: change height based on report type
    
    * chore: increase delay for chromatic snap shots
    
    * feat: update role to editor
    
    * chore: parent component controls report height
    
    * chore: remove comment
    
    * feat: add analytics redirect if no flag
    
    * chore: increase snapshot delay
    
    * fix: move loading to remote component
    
    * refactor: names, stories, tests for updated reprot
    
    * test: remove redundant stories and disable snapshot for report
    
    * chore: add comment
    
    * chore: made adjustments from PR feedback
    
    * chore: lint fix
    
    * fix: import
    
    * chore: remove carried over component from different branch
    
    * fix: identify users on ld init
    
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

---
+++ apps/journeys-admin/src/components/DynamicPowerBiReport/DynamicPowerBiReport.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react'
+import dynamic from 'next/dynamic'
+import { ReportProps } from './Report/Report'
+
+export function DynamicPowerBiReport(props: ReportProps): ReactElement {
+  // PowerBI needs dynamic import
+  // See issue: https://github.com/microsoft/powerbi-client-react/issues/65
+  const Report = dynamic<ReportProps>(
+    async () =>
+      await import(
+        /* webpackChunkName: "DynamicReport" */
+        './Report'
+      ).then(({ Report }) => Report),
+    { ssr: false }
+  )
+
+  return <Report {...props} />
+}
