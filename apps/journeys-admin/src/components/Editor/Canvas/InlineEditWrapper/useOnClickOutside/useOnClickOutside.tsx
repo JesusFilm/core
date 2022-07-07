dc0319d74ee82624a3762a024d2dfb25bcb85814
Siyang <csiyang@yahoo.co.nz>
2022-07-07 03:22:16 +0000

    fix: add check for inncorrect double render of wrong ref when click done button

--- apps/journeys-admin/src/components/Editor/Canvas/InlineEditWrapper/useOnClickOutside/useOnClickOutside.tsx
+++ apps/journeys-admin/src/components/Editor/Canvas/InlineEditWrapper/useOnClickOutside/useOnClickOutside.tsx
@@ -4,12 +4,12 @@ export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   callback: () => void | Promise<void>
 ): RefObject<T> {
   const elementRef = useRef<T>(null)
-
   useEffect(() => {
     const handleClick = (event: MouseEvent): void => {
       if (
         elementRef.current != null &&
-        !elementRef.current.contains(event.target as HTMLElement)
+        !elementRef.current.contains(event.target as HTMLElement) &&
+        elementRef.current.classList.contains('Mui-focused')
       ) {
         void callback()
       }
