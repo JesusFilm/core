67746b6fdd0bdc76b5067e1193c670d7ff265d07
Mike Allison <mike@captainofbass.com>
2022-07-08 19:12:37 +0000

    fix: specs

--- apps/journeys-admin/src/components/JourneyView/Properties/JourneyDetails/JourneyDetails.spec.tsx
+++ apps/journeys-admin/src/components/JourneyView/Properties/JourneyDetails/JourneyDetails.spec.tsx
@@ -26,27 +26,6 @@ describe('journeyDetails', () => {
     )
     expect(getByText('English')).toBeInTheDocument()
   })
-  it('renders local language if no native language', () => {
-    const { getByText } = render(
-      <JourneyProvider
-        value={{
-          journey: {
-            createdAt: new Date('2021-11-19T12:34:56.647Z').toISOString(),
-            language: {
-              __typename: 'Language',
-              id: '529',
-              name: [
-                { value: 'English', primary: false, __typename: 'Translation' }
-              ]
-            }
-          } as unknown as Journey
-        }}
-      >
-        <JourneyDetails />
-      </JourneyProvider>
-    )
-    expect(getByText('English')).toBeInTheDocument()
-  })
 
   it('renders local language if no native language', () => {
     const { getByText } = render(
@@ -92,7 +71,7 @@ describe('journeyDetails', () => {
     expect(getByText('English')).toBeInTheDocument()
   })
 
-  it('renders both native language and local language', () => {
+  it('renders native language in parens if both exist', () => {
     const { getByText } = render(
       <JourneyProvider
         value={{
@@ -119,7 +98,7 @@ describe('journeyDetails', () => {
     expect(getByText('(English)')).toBeInTheDocument()
   })
 
-  it('renders local language if languages are the same', () => {
+  it('renders only local language if languages are the same', () => {
     const { queryByText } = render(
       <JourneyProvider
         value={{
