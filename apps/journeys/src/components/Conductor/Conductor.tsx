2fdec61e0d6afdc8f9bf3e15b11d9bae2e4a70bc
Siyang <csiyang@yahoo.co.nz>
2022-07-10 22:53:04 +0000

    fix: remove padding

--- apps/journeys/src/components/Conductor/Conductor.tsx
+++ apps/journeys/src/components/Conductor/Conductor.tsx
@@ -128,7 +128,7 @@ export function Conductor({ blocks }: ConductorProps): ReactElement {
         >
           <Box
             sx={{
-              pb: 6,
+              // pb: 6,
               pt: { sm: 0, xs: 6 },
               flexGrow: 1,
               display: 'flex',
