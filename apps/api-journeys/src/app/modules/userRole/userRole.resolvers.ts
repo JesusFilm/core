53bb0f8532389fc50e0a633274475b2d17069407
Siyang <csiyang@yahoo.co.nz>
2022-07-24 21:21:43 +0000

    feat: add resolver

--- apps/api-journeys/src/app/modules/userRole/userRole.resolvers.ts
+++ apps/api-journeys/src/app/modules/userRole/userRole.resolvers.ts
@@ -1,16 +1,9 @@
-import {
-  Args,
-  Resolver,
-  Mutation,
-  Parent,
-  Query,
-  ResolveField
-} from '@nestjs/graphql'
+import { Resolver, Query } from '@nestjs/graphql'
 import { CurrentUserId } from '@core/nest/decorators/CurrentUserId'
 import { UseGuards } from '@nestjs/common'
 import { GqlAuthGuard } from '@core/nest/gqlAuthGuard/GqlAuthGuard'
-import { AuthenticationError } from 'apollo-server-errors'
 import { JourneyService } from '../journey/journey.service'
+import { UserRole } from '../../__generated__/graphql'
 import { UserRoleService } from './userRole.service'
 
 @Resolver('UserRole')
@@ -19,4 +12,18 @@ export class UserRoleResolver {
     private readonly userRoleService: UserRoleService,
     private readonly journeyService: JourneyService
   ) {}
+
+  @Query()
+  @UseGuards(GqlAuthGuard)
+  async getUserRole(@CurrentUserId() userId: string): Promise<UserRole> {
+    const user: UserRole = await this.userRoleService.getUserRoleById(userId)
+
+    if (user != null) {
+      return user
+    } else {
+      return await this.userRoleService.save({
+        userId
+      })
+    }
+  }
 }
