9b3da37397c9c2dfdba8ebe8b8aa34a538c9e021
Mike Allison <dj.mikeallison@gmail.com>
2022-07-26 01:59:41 +0000

    feat: create api media

--- apps/api-journeys/src/app/__generated__/graphql.ts
+++ apps/api-journeys/src/app/__generated__/graphql.ts
@@ -767,8 +767,7 @@ export class UserRole {
     __typename?: 'UserRole';
     id: string;
     userId: string;
-    role?: Nullable<Role>;
-    user?: Nullable<User>;
+    roles?: Nullable<Role[]>;
 }
 
 export abstract class IMutation {
@@ -897,7 +896,7 @@ export abstract class IQuery {
 
     abstract journey(id: string, idType?: Nullable<IdType>): Nullable<Journey> | Promise<Nullable<Journey>>;
 
-    abstract getUserRole(id: string): Nullable<UserRole> | Promise<Nullable<UserRole>>;
+    abstract getUserRole(): Nullable<UserRole> | Promise<Nullable<UserRole>>;
 }
 
 export class User {
