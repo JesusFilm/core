256820b2b5b4cb72081a3ef9cabac7885c202b36
Siyang <csiyang@yahoo.co.nz>
2022-07-27 23:17:39 +0000

    feat: update filter with template field

--- apps/api-journeys/src/app/__generated__/graphql.ts
+++ apps/api-journeys/src/app/__generated__/graphql.ts
@@ -396,6 +396,7 @@ export class VideoProgressEventCreateInput {
 
 export class JourneysFilter {
     featured?: Nullable<boolean>;
+    publicTemplate?: Nullable<boolean>;
 }
 
 export class JourneyCreateInput {
