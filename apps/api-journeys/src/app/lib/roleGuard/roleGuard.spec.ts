38a4bfa0f3824c9f7fbbfb4fc4882a053a4e2f0b
John Geronimo <johnageronimo1@gmail.com>
2022-08-01 05:15:44 +0000

    fix: initial extension of the role guard

--- apps/api-journeys/src/app/lib/roleGuard/roleGuard.spec.ts
+++ apps/api-journeys/src/app/lib/roleGuard/roleGuard.spec.ts
@@ -1,8 +1,19 @@
 import { ExecutionContext } from '@nestjs/common'
 import { createMock, DeepMocked } from '@golevelup/ts-jest'
 import { AuthenticationError } from 'apollo-server-errors'
-import { UserJourney, UserJourneyRole } from '../../__generated__/graphql'
+import {
+  Journey,
+  JourneyStatus,
+  Role,
+  ThemeMode,
+  ThemeName,
+  UserJourney,
+  UserJourneyRole,
+  UserRole
+} from '../../__generated__/graphql'
 import { UserJourneyService } from '../../modules/userJourney/userJourney.service'
+import { UserRoleService } from '../../modules/userRole/userRole.service'
+import { JourneyService } from '../../modules/journey/journey.service'
 import { RoleGuard } from './roleGuard'
 
 describe('RoleGuard', () => {
@@ -11,6 +22,41 @@ describe('RoleGuard', () => {
     userId: '1',
     journeyId: '2',
     role: UserJourneyRole.owner
+  }
+
+  const userRole = {
+    id: '1',
+    userId: '1',
+    roles: [Role.publisher]
+  }
+
+  const journey = {
+    id: 'journey-id',
+    title: 'Journey Heading',
+    description: 'Description',
+    slug: 'default',
+    language: {
+      __typename: 'Language',
+      id: '529',
+      name: [
+        {
+          __typename: 'Translation',
+          value: 'English',
+          primary: true
+        }
+      ]
+    },
+    status: JourneyStatus.published,
+    createdAt: '2021-11-19T12:34:56.647Z',
+    publishedAt: null,
+    themeName: ThemeName.base,
+    themeMode: ThemeMode.light,
+    blocks: null,
+    primaryImageBlock: null,
+    seoTitle: null,
+    seoDescription: null,
+    userJourneys: [userJourney],
+    template: true
   }
 
   afterAll(() => {
@@ -34,12 +80,26 @@ describe('RoleGuard', () => {
     contextMock: { headers: { 'user-id'?: string }; req: Request }
   ): DeepMocked<ExecutionContext> => gqlMockFactory(args, contextMock)
 
-  const checkActor = async (
+  const fetchUserJourney = async (
     userJourneyService: UserJourneyService,
     journeyId: string,
     userId: string
   ): Promise<UserJourney> => {
     return userJourney
+  }
+
+  const fetchUserRole = async (
+    userRoleService: UserRoleService,
+    userId: string
+  ): Promise<UserRole> => {
+    return userRole
+  }
+
+  const fetchJourney = async (
+    journeyService: JourneyService,
+    journeyId: string
+  ): Promise<Journey> => {
+    return journey
   }
 
   describe('1 Role', () => {
@@ -51,7 +111,13 @@ describe('RoleGuard', () => {
           req: createMock<Request>()
         }
       )
-      const RoleGuardClass = RoleGuard('id', UserJourneyRole.owner, checkActor)
+      const RoleGuardClass = RoleGuard(
+        'id',
+        UserJourneyRole.owner,
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
       const roleGuard = new RoleGuardClass(gqlContext)
       expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
     })
@@ -63,7 +129,13 @@ describe('RoleGuard', () => {
           req: createMock<Request>()
         }
       )
-      const RoleGuardClass = RoleGuard('id', UserJourneyRole.owner, checkActor)
+      const RoleGuardClass = RoleGuard(
+        'id',
+        [UserJourneyRole.owner],
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
       const roleGuard = new RoleGuardClass(gqlContext)
       expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
     })
@@ -75,7 +147,13 @@ describe('RoleGuard', () => {
           req: createMock<Request>()
         }
       )
-      const RoleGuardClass = RoleGuard('id', UserJourneyRole.editor, checkActor)
+      const RoleGuardClass = RoleGuard(
+        'id',
+        UserJourneyRole.editor,
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
       const roleGuard = new RoleGuardClass(gqlContext)
       await expect(roleGuard.canActivate(gqlContext)).rejects.toThrow(
         new AuthenticationError(
@@ -97,7 +175,9 @@ describe('RoleGuard', () => {
       const RoleGuardClass = RoleGuard(
         'id',
         [UserJourneyRole.owner, UserJourneyRole.editor],
-        checkActor
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
       )
       const roleGuard = new RoleGuardClass(gqlContext)
       expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
@@ -114,11 +194,37 @@ describe('RoleGuard', () => {
       const RoleGuardClass = RoleGuard(
         'id',
         [UserJourneyRole.owner, UserJourneyRole.editor],
-        checkActor
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
       )
       const roleGuard = new RoleGuardClass(gqlContext)
       expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
     })
+
+    it('should return true on different roles', async () => {
+      const gqlContext = gqlContextMockFactory(
+        { id: '2' },
+        {
+          headers: { 'user-id': '1' },
+          req: createMock<Request>()
+        }
+      )
+      const RoleGuardClass = RoleGuard(
+        'id',
+        [
+          UserJourneyRole.owner,
+          UserJourneyRole.editor,
+          { role: [Role.publisher], attributes: { template: true } }
+        ],
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
+      const roleGuard = new RoleGuardClass(gqlContext)
+      expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
+    })
+
     it('should throw error', async () => {
       const gqlContext = gqlContextMockFactory(
         { id: '2' },
@@ -130,7 +236,9 @@ describe('RoleGuard', () => {
       const RoleGuardClass = RoleGuard(
         'id',
         [UserJourneyRole.inviteRequested, UserJourneyRole.editor],
-        checkActor
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
       )
       const roleGuard = new RoleGuardClass(gqlContext)
 
@@ -150,9 +258,86 @@ describe('RoleGuard', () => {
           req: createMock<Request>()
         }
       )
-      const RoleGuardClass = RoleGuard('id', UserJourneyRole.owner, checkActor)
+      const RoleGuardClass = RoleGuard(
+        'id',
+        UserJourneyRole.owner,
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
       const roleGuard = new RoleGuardClass(gqlContext)
       expect(await roleGuard.canActivate(gqlContext)).toEqual(false)
     })
   })
+
+  describe('user Role', () => {
+    it('should return true if user role includes publisher', async () => {
+      const gqlContext = gqlContextMockFactory(
+        { id: '2' },
+        {
+          headers: { 'user-id': '1' },
+          req: createMock<Request>()
+        }
+      )
+      const RoleGuardClass = RoleGuard(
+        'id',
+        Role.publisher,
+        fetchUserJourney,
+        fetchUserRole,
+        fetchJourney
+      )
+      const roleGuard = new RoleGuardClass(gqlContext)
+      expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
+    })
+  })
+
+  // describe('custome Roles', () => {
+  //   it('should return true if role is publisher and template true and status published', async () => {
+  //     const gqlContext = gqlContextMockFactory(
+  //       { id: '2' },
+  //       {
+  //         headers: { 'user-id': '1' },
+  //         req: createMock<Request>()
+  //       }
+  //     )
+  //     const RoleGuardClass = RoleGuard(
+  //       'id',
+  //       [
+  //         {
+  //           role: Role.publisher,
+  //           attributes: { template: true, status: JourneyStatus.published }
+  //         }
+  //       ],
+  //       fetchUserJourney,
+  //       fetchUserRole,
+  //       fetchJourney
+  //     )
+  //     const roleGuard = new RoleGuardClass(gqlContext)
+  //     expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
+  //   })
+
+  //   it('should return true if role is public and template true', async () => {
+  //     const gqlContext = gqlContextMockFactory(
+  //       { id: '2' },
+  //       {
+  //         headers: { 'user-id': '1' },
+  //         req: createMock<Request>()
+  //       }
+  //     )
+  //     const RoleGuardClass = RoleGuard(
+  //       'id',
+  //       [
+  //         {
+  //           role: 'public',
+  //           attributes: { template: true }
+  //         }
+  //       ],
+  //       fetchUserJourney,
+  //       fetchUserRole,
+  //       fetchJourney
+  //     )
+  //     const roleGuard = new RoleGuardClass(gqlContext)
+  //     expect(await roleGuard.canActivate(gqlContext)).toEqual(true)
+  //   })
+  // })
 })
--- apps/api-journeys/src/app/lib/roleGuard/roleGuard.ts
+++ apps/api-journeys/src/app/lib/roleGuard/roleGuard.ts
@@ -7,13 +7,22 @@ import {
   Inject
 } from '@nestjs/common'
 import { GqlExecutionContext } from '@nestjs/graphql'
-import { get, includes } from 'lodash'
+import { get, reduce } from 'lodash'
 import { AuthenticationError } from 'apollo-server-errors'
 import { UserJourneyService } from '../../modules/userJourney/userJourney.service'
-import { UserJourney, UserJourneyRole } from '../../__generated__/graphql'
+import {
+  Journey,
+  JourneyStatus,
+  Role,
+  UserJourney,
+  UserJourneyRole,
+  UserRole
+} from '../../__generated__/graphql'
+import { UserRoleService } from '../../modules/userRole/userRole.service'
+import { JourneyService } from '../../modules/journey/journey.service'
 
 // broken out into function for test injection
-export const checkActor = async (
+export const fetchUserJourney = async (
   userJourneyService: UserJourneyService,
   journeyId: string,
   userId: string
@@ -21,17 +30,133 @@ export const checkActor = async (
   return await userJourneyService.forJourneyUser(journeyId, userId)
 }
 
+export const fetchUserRole = async (
+  userRoleService: UserRoleService,
+  journeyId: string
+): Promise<UserRole> => {
+  return await userRoleService.getUserRoleById(journeyId)
+}
+
+export const fetchJourney = async (
+  journeyService: JourneyService,
+  userId: string
+): Promise<Journey> => {
+  return await journeyService.get(userId)
+}
+
+type DefinedRole = UserJourneyRole | Role | PublicRole
+
+interface CustomRole {
+  role: DefinedRole | DefinedRole[]
+  attributes?: Partial<Journey>
+}
+
+type Permission = DefinedRole | CustomRole
+
+export type PublicRole = 'public'
+
 export const RoleGuard = (
   journeyIdArgName: string,
-  roles: UserJourneyRole[] | UserJourneyRole,
-  ca: typeof checkActor = checkActor
+  permissions: Permission[] | Permission,
+  fuj: typeof fetchUserJourney = fetchUserJourney,
+  fur: typeof fetchUserRole = fetchUserRole,
+  fj: typeof fetchJourney = fetchJourney
 ): Type<CanActivate> => {
   @Injectable()
   class RolesGuard implements CanActivate {
     constructor(
       @Inject(UserJourneyService)
-      private readonly userJourneyService: UserJourneyService
+      private readonly userJourneyService: UserJourneyService,
+      @Inject(UserRoleService)
+      private readonly userRoleService: UserRoleService,
+      @Inject(JourneyService)
+      private readonly journeyService: JourneyService
     ) {}
+
+    publicRole(permission: Permission): boolean {
+      if (permission === 'public') return true
+      return false
+    }
+
+    userJourneyRole(permission: Permission, userJourney: UserJourney): boolean {
+      if (permission === userJourney.role) return true
+      return false
+    }
+
+    userRole(permission: Permission, userRole: UserRole): boolean {
+      const role = userRole.roles?.find((role) => role === Role.publisher)
+      if (role == null) return false
+      if (permission === role) return true
+      return false
+    }
+
+    customRole(
+      permission: Permission,
+      journey: Journey,
+      userJourney: UserJourney,
+      userRole: UserRole
+    ): boolean {
+      const customRole = permission as CustomRole
+      let userAccess
+
+      if (Array.isArray(customRole.role)) {
+        userAccess = reduce(
+          customRole.role,
+          (result, role) => {
+            if (result) return true
+            if (this.publicRole(role)) return true
+            if (this.userJourneyRole(role, userJourney)) return true
+            if (this.userRole(role, userRole)) return true
+            return false
+          },
+          false
+        )
+      } else {
+        userAccess =
+          this.publicRole(customRole.role) ||
+          this.userJourneyRole(customRole.role, userJourney) ||
+          this.userRole(customRole.role, userRole)
+      }
+      // check if journey has attributes
+
+      if (
+        customRole.role === Role.publisher &&
+        customRole.attributes ===
+          { template: journey.template, status: journey.status }
+      )
+        return true
+
+      if (
+        customRole.role === 'public' &&
+        customRole.attributes === { template: true }
+      )
+        return true
+
+      if (userAccess === true && customRole.attributes === journey.template)
+        return true
+      return false
+    }
+
+    checkAllowedAccess(
+      permissions: Permission[],
+      journey: Journey,
+      userJourney: UserJourney,
+      userRole: UserRole
+    ): boolean {
+      return reduce(
+        permissions,
+        (result, permission) => {
+          if (result) return true
+          if (this.publicRole(permission)) return true
+          if (this.userJourneyRole(permission, userJourney)) return true
+          if (this.userRole(permission, userRole)) return true
+          if (this.customRole(permission, journey, userJourney, userRole))
+            return true
+          return result
+        },
+        false
+      )
+    }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
       const ctx = GqlExecutionContext.create(context).getContext()
@@ -47,19 +172,29 @@ export const RoleGuard = (
       let result = false
       if (Array.isArray(journeyId)) {
         for (let i = 0; i < journeyId.length; i++) {
-          const actor = await ca(this.userJourneyService, journeyId[i], userId)
+          const journey = await fj(this.journeyService, journeyId[i])
+          const userRole = await fur(this.userRoleService, userId)
+          const userJourney = await fuj(
+            this.userJourneyService,
+            journeyId[i],
+            userId
+          )
 
-          result = Array.isArray(roles)
-            ? includes(roles, actor?.role)
-            : roles === actor?.role
+          const per = Array.isArray(permissions) ? permissions : [permissions]
+          result = this.checkAllowedAccess(per, journey, userJourney, userRole)
           if (!result) break
         }
       } else {
-        const actor = await ca(this.userJourneyService, journeyId, userId)
+        const userRole = await fur(this.userRoleService, userId)
+        const journey = await fj(this.journeyService, journeyId)
+        const userJourney = await fuj(
+          this.userJourneyService,
+          journeyId,
+          userId
+        )
 
-        result = Array.isArray(roles)
-          ? includes(roles, actor?.role)
-          : roles === actor?.role
+        const per = Array.isArray(permissions) ? permissions : [permissions]
+        result = this.checkAllowedAccess(per, journey, userJourney, userRole)
       }
       if (!result)
         throw new AuthenticationError(
@@ -71,3 +206,23 @@ export const RoleGuard = (
   const guard = mixin(RolesGuard)
   return guard
 }
+
+// check permission is public
+// return true
+
+// check permission is user journey role
+// return true if user has user journey role
+
+// check permission is role
+// return true if user has role
+
+// check permission is custom role and journey has attributes
+// if role is array for each the following
+// check permission is public
+// return true
+
+// check permission is user journey role
+// return true if user has user journey role
+
+// check permission is role
+// return true if user has role
