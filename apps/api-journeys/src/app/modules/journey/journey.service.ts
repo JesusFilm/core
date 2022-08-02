711200fc836b50d22805b82b1b940a2dd2eccded
Siyang <csiyang@yahoo.co.nz>
2022-08-02 03:31:34 +0000

    refactor: nullish logic for template

--- apps/api-journeys/src/app/modules/journey/journey.service.ts
+++ apps/api-journeys/src/app/modules/journey/journey.service.ts
@@ -16,12 +16,9 @@ export class JourneyService extends BaseService {
   journeyFilter(filter?: JourneysFilter): AqlQuery {
     const { featured, template } = filter ?? {}
 
-    // ? aql`AND journey.template == ${template === true}`
     return aql.join(
       [
-        template === true
-          ? aql`AND journey.template == true`
-          : aql`AND journey.template != true`,
+        aql`AND journey.template == ${template === true}`,
         featured === true && aql`AND journey.featuredAt != null`,
         featured === false && aql`AND journey.featuredAt == null`
       ].filter((x) => x !== false)
