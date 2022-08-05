45a9ac3336c215f6b84b749d4955a4edd6693c6e
Siyang <csiyang@yahoo.co.nz>
2022-08-05 08:48:12 +0000

    test: active templates

---
+++ apps/journeys-admin/src/components/TemplatesAdmin/TemplateStatusTabs/ActiveTemplates/ActiveTemplates.spec.tsx
@@ -0,0 +1,317 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { fireEvent, render, waitFor } from '@testing-library/react'
+import noop from 'lodash/noop'
+import { SnackbarProvider } from 'notistack'
+import { AuthUser } from 'next-firebase-auth'
+import {
+  defaultTemplate,
+  oldTemplate
+} from '../../../TemplateList/TemplateListData'
+import { ThemeProvider } from '../../../ThemeProvider'
+import { SortOrder } from '../../../JourneyList/JourneySort'
+import {
+  ARCHIVE_ACTIVE_JOURNEYS,
+  TRASH_ACTIVE_JOURNEYS
+} from '../../../JourneyList/StatusTabPanel/ActiveStatusTab/ActiveStatusTab'
+import {
+  ActiveTemplates,
+  GET_ACTIVE_PUBLISHER_TEMPLATES
+} from './ActiveTemplates'
+
+const activeTemplatesMock = {
+  request: {
+    query: GET_ACTIVE_PUBLISHER_TEMPLATES
+  },
+  result: {
+    data: {
+      journeys: [defaultTemplate, oldTemplate]
+    }
+  }
+}
+
+const noTemplatesMock = {
+  request: {
+    query: GET_ACTIVE_PUBLISHER_TEMPLATES
+  },
+  result: {
+    data: {
+      journeys: []
+    }
+  }
+}
+
+const authUser = { id: 'user-id1' } as unknown as AuthUser
+
+describe('ActiveTemplates', () => {
+  it('should render templates in descending createdAt date by default', async () => {
+    const { getAllByLabelText } = render(
+      <MockedProvider mocks={[activeTemplatesMock]}>
+        <ThemeProvider>
+          <SnackbarProvider>
+            <ActiveTemplates onLoad={noop} event="" />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </MockedProvider>
+    )
+    // TODO: wait until templateCard is merged
+    // await waitFor(() =>
+    //   expect(getAllByLabelText('journey-card')[0].textContent).toContain(
+    //     'January 1'
+    //   )
+    // )
+    // expect(getAllByLabelText('journey-card')[1].textContent).toContain(
+    //   'November 19, 2020'
+    // )
+  })
+
+  it('should order templates in alphabetical order', async () => {
+    const lowerCaseJourneyTitle = {
+      ...defaultTemplate,
+      title: 'a lower case title'
+    }
+
+    const { getAllByLabelText } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: GET_ACTIVE_PUBLISHER_TEMPLATES
+            },
+            result: {
+              data: {
+                journeys: [lowerCaseJourneyTitle, oldTemplate]
+              }
+            }
+          }
+        ]}
+      >
+        <ThemeProvider>
+          <SnackbarProvider>
+            <ActiveTemplates
+              onLoad={noop}
+              sortOrder={SortOrder.TITLE}
+              event=""
+            />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </MockedProvider>
+    )
+    // TODO: wait until templateCard is merged
+    // await waitFor(() =>
+    //   expect(getAllByLabelText('journey-card')[0].textContent).toContain(
+    //     'An Old Journey Heading'
+    //   )
+    // )
+    // expect(getAllByLabelText('journey-card')[1].textContent).toContain(
+    //   'a lower case title'
+    // )
+  })
+
+  // TODO: remove?
+  it('should ask users to add a new journey', async () => {
+    const { getByRole, getByText } = render(
+      <MockedProvider mocks={[noTemplatesMock]}>
+        <ThemeProvider>
+          <SnackbarProvider>
+            <ActiveTemplates onLoad={noop} event="" />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </MockedProvider>
+    )
+    // await waitFor(() =>
+    //   expect(getByText('No journeys to display.')).toBeInTheDocument()
+    // )
+    // expect(
+    //   getByText('Create a journey, then find it here.')
+    // ).toBeInTheDocument()
+    // expect(getByRole('button')).toBeInTheDocument()
+  })
+
+  it('should render loading skeleton', async () => {
+    const { getAllByLabelText } = render(
+      <MockedProvider mocks={[]}>
+        <ThemeProvider>
+          <SnackbarProvider>
+            <ActiveTemplates onLoad={noop} event="" />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </MockedProvider>
+    )
+    // TODO: wait until templateCard is merged
+    // await waitFor(() =>
+    //   expect(getAllByLabelText('journey-card')).toHaveLength(3)
+    // )
+  })
+
+  it('should call onLoad when query is loaded', async () => {
+    const onLoad = jest.fn()
+    render(
+      <MockedProvider mocks={[noTemplatesMock]}>
+        <ThemeProvider>
+          <SnackbarProvider>
+            <ActiveTemplates onLoad={onLoad} event="" />
+          </SnackbarProvider>
+        </ThemeProvider>
+      </MockedProvider>
+    )
+    await waitFor(() => expect(onLoad).toHaveBeenCalled())
+  })
+
+  describe('Archive All', () => {
+    const result = jest.fn(() => ({
+      data: {
+        journeysArchive: [{ id: defaultTemplate.id, status: 'archived' }]
+      }
+    }))
+    const archiveJourneysMock = {
+      request: {
+        query: ARCHIVE_ACTIVE_JOURNEYS,
+        variables: {
+          ids: []
+          // ids: [defaultTemplate.id, oldTemplate.id] // auth issue causing these ids not to be entered?
+        }
+      },
+      result
+    }
+    const onLoad = jest.fn()
+
+    it('should display the archive all dialog', () => {
+      const { getByText } = render(
+        <MockedProvider mocks={[activeTemplatesMock]}>
+          <ThemeProvider>
+            <SnackbarProvider>
+              <ActiveTemplates onLoad={noop} event="archiveAllActive" />
+            </SnackbarProvider>
+          </ThemeProvider>
+        </MockedProvider>
+      )
+
+      expect(getByText('Archive Templates')).toBeInTheDocument()
+    })
+
+    it('should archive all journeys', async () => {
+      const { getByText } = render(
+        <MockedProvider mocks={[activeTemplatesMock, archiveJourneysMock]}>
+          <ThemeProvider>
+            <SnackbarProvider>
+              <ActiveTemplates
+                onLoad={onLoad}
+                event="archiveAllActive"
+                authUser={authUser}
+              />
+            </SnackbarProvider>
+          </ThemeProvider>
+        </MockedProvider>
+      )
+      await waitFor(() => expect(onLoad).toHaveBeenCalled())
+      fireEvent.click(getByText('Archive'))
+      await waitFor(() => expect(result).toHaveBeenCalled())
+    })
+
+    it('should show error', async () => {
+      const { getByText } = render(
+        <MockedProvider
+          mocks={[
+            activeTemplatesMock,
+            { ...archiveJourneysMock, error: new Error('error') }
+          ]}
+        >
+          <SnackbarProvider>
+            <ThemeProvider>
+              <SnackbarProvider>
+                <ActiveTemplates
+                  onLoad={onLoad}
+                  event="archiveAllActive"
+                  authUser={authUser}
+                />
+              </SnackbarProvider>
+            </ThemeProvider>
+          </SnackbarProvider>
+        </MockedProvider>
+      )
+      await waitFor(() => expect(onLoad).toHaveBeenCalled())
+      fireEvent.click(getByText('Archive'))
+      await waitFor(() => expect(getByText('error')).toBeInTheDocument())
+    })
+  })
+
+  describe('Trash All', () => {
+    const result = jest.fn(() => ({
+      data: {
+        journeysTrash: [{ id: defaultTemplate.id, status: 'archived' }]
+      }
+    }))
+    const trashTemplatesMock = {
+      request: {
+        query: TRASH_ACTIVE_JOURNEYS,
+        variables: {
+          ids: []
+          // ids: [defaultTemplate.id, oldTemplate.id]
+        }
+      },
+      result
+    }
+    const onLoad = jest.fn()
+
+    it('should display the trash all dialog', () => {
+      const { getByText } = render(
+        <MockedProvider mocks={[activeTemplatesMock]}>
+          <ThemeProvider>
+            <SnackbarProvider>
+              <ActiveTemplates onLoad={noop} event="trashAllActive" />
+            </SnackbarProvider>
+          </ThemeProvider>
+        </MockedProvider>
+      )
+
+      expect(getByText('Trash Templates')).toBeInTheDocument()
+    })
+
+    it('should trash all journeys', async () => {
+      const { getByText } = render(
+        <MockedProvider
+          mocks={[activeTemplatesMock, trashTemplatesMock, noTemplatesMock]}
+        >
+          <ThemeProvider>
+            <SnackbarProvider>
+              <ActiveTemplates
+                onLoad={onLoad}
+                event="trashAllActive"
+                authUser={authUser}
+              />
+            </SnackbarProvider>
+          </ThemeProvider>
+        </MockedProvider>
+      )
+      await waitFor(() => expect(onLoad).toHaveBeenCalled())
+      fireEvent.click(getByText('Trash'))
+      await waitFor(() => expect(result).toHaveBeenCalled())
+    })
+
+    it('should show error', async () => {
+      const { getByText } = render(
+        <MockedProvider
+          mocks={[
+            activeTemplatesMock,
+            { ...trashTemplatesMock, error: new Error('error') }
+          ]}
+        >
+          <SnackbarProvider>
+            <ThemeProvider>
+              <SnackbarProvider>
+                <ActiveTemplates
+                  onLoad={onLoad}
+                  event="trashAllActive"
+                  authUser={authUser}
+                />
+              </SnackbarProvider>
+            </ThemeProvider>
+          </SnackbarProvider>
+        </MockedProvider>
+      )
+      await waitFor(() => expect(onLoad).toHaveBeenCalled())
+      fireEvent.click(getByText('Trash'))
+      await waitFor(() => expect(getByText('error')).toBeInTheDocument())
+    })
+  })
+})
