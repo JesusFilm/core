f48b9f8ba35e7ef99f78fbed49440df770ac1bfb
John Geronimo <johnageronimo1@gmail.com>
2022-08-05 03:54:11 +0000

    fix: failing vr test

--- apps/journeys-admin/src/components/TemplateView/TitleDescriptionDialog/TitleDescriptionDialog.stories.tsx
+++ apps/journeys-admin/src/components/TemplateView/TitleDescriptionDialog/TitleDescriptionDialog.stories.tsx
@@ -1,6 +1,9 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import { Story, Meta } from '@storybook/react'
 import { useState } from 'react'
 import { journeysAdminConfig } from '../../../libs/storybook'
+import { defaultJourney } from '../data'
 import { TitleDescriptionDialog } from './TitleDescriptionDialog'
 
 const TitleDescriptionDialogStory = {
@@ -16,7 +19,13 @@ const TitleDescriptionDialogStory = {
 const Template: Story = () => {
   const [open, setOpen] = useState(true)
 
-  return <TitleDescriptionDialog open={open} onClose={() => setOpen(false)} />
+  return (
+    <MockedProvider>
+      <JourneyProvider value={{ journey: defaultJourney, admin: true }}>
+        <TitleDescriptionDialog open={open} onClose={() => setOpen(false)} />
+      </JourneyProvider>
+    </MockedProvider>
+  )
 }
 export const Default = Template.bind({})
 
