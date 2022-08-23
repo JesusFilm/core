d849ea287bc4960732fb7935772b24fb209e0779
Siyang <csiyang@yahoo.co.nz>
2022-08-23 18:59:59 +0000

    feat: add template edit page

---
+++ apps/journeys-admin/pages/templates/[journeyId]/edit.tsx
@@ -0,0 +1,53 @@
+import { ReactElement } from 'react'
+import {
+  AuthAction,
+  useAuthUser,
+  withAuthUser,
+  withAuthUserTokenSSR
+} from 'next-firebase-auth'
+import { NextSeo } from 'next-seo'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
+import { PageWrapper } from '../../../src/components/PageWrapper'
+import i18nConfig from '../../../next-i18next.config'
+
+function TemplateEditPage(): ReactElement {
+  const AuthUser = useAuthUser()
+
+  return (
+    <>
+      <NextSeo title="Template Details" />
+      <PageWrapper title="Template Details" authUser={AuthUser}>
+        {/* Edit templates */}
+      </PageWrapper>
+    </>
+  )
+}
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
+})(async ({ AuthUser, locale }) => {
+  const ldUser = {
+    key: AuthUser.id as string,
+    firstName: AuthUser.displayName ?? undefined,
+    email: AuthUser.email ?? undefined
+  }
+  const launchDarklyClient = await getLaunchDarklyClient(ldUser)
+  const flags = (await launchDarklyClient.allFlagsState(ldUser)).toJSON() as {
+    [key: string]: boolean | undefined
+  }
+  return {
+    props: {
+      flags,
+      ...(await serverSideTranslations(
+        locale ?? 'en',
+        ['apps-journeys-admin', 'libs-journeys-ui'],
+        i18nConfig
+      ))
+    }
+  }
+})
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN
+})(TemplateEditPage)
