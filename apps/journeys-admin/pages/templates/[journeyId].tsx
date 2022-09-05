be917e3b30e306d0612e283f79f5c538ecdff2b2
Siyang <csiyang@yahoo.co.nz>
2022-09-05 01:28:47 +0000

    refactor: pages paths for templates

--- apps/journeys-admin/pages/templates/[journeyId].tsx
+++ apps/journeys-admin/pages/templates/[journeyId].tsx
@@ -11,13 +11,13 @@ import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
 import { PageWrapper } from '../../src/components/PageWrapper'
 import i18nConfig from '../../next-i18next.config'
 
-function TemplateDetailsAdmin(): ReactElement {
+function TemplateDetails(): ReactElement {
   const AuthUser = useAuthUser()
   return (
     <>
-      <NextSeo title="Template Details" />
-      <PageWrapper title="Template Details" authUser={AuthUser}>
-        {/* Template Details for publishers */}
+      <NextSeo title="Journey Template" />
+      <PageWrapper title="Journey Template" authUser={AuthUser}>
+        {/* Public Template Details */}
       </PageWrapper>
     </>
   )
@@ -49,4 +49,4 @@ export const getServerSideProps = withAuthUserTokenSSR({
 
 export default withAuthUser({
   whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN
-})(TemplateDetailsAdmin)
+})(TemplateDetails)
