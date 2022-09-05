4903109d2845df8666b2045415397edab6a36a25
Siyang <csiyang@yahoo.co.nz>
2022-09-05 23:06:13 +0000

    feat: use common tabs component

--- apps/journeys-admin/src/components/TemplateList/TemplateList.tsx
+++ apps/journeys-admin/src/components/TemplateList/TemplateList.tsx
@@ -1,13 +1,12 @@
 import { NextRouter } from 'next/router'
-import { ReactElement } from 'react'
-// import { ReactElement, useState, useEffect } from 'react'
+import { ReactElement, useState, useEffect } from 'react'
 import Container from '@mui/material/Container'
 import { AuthUser } from 'next-firebase-auth'
-// import { SortOrder } from '../JourneyList/JourneySort'
-import { TemplateStatusTabs } from './TemplateStatusTabs'
-// import { ActiveTemplates } from './TemplateStatusTabs/ActiveTemplates'
-// import { ArchivedTemplates } from './TemplateStatusTabs/ArchivedTemplates'
-// import { TrashedTemplates } from './TemplateStatusTabs/TrashedTemplates'
+import { SortOrder } from '../JourneyList/JourneySort'
+import { StatusTabPanel } from '../StatusTabPanel'
+import { ActiveTemplates } from './ActiveTemplates'
+import { ArchivedTemplates } from './ArchivedTemplates'
+import { TrashedTemplates } from './TrashedTemplates'
 
 interface TemplateListProps {
   event: string | undefined
@@ -20,30 +19,28 @@ export function TemplateList({
   router,
   authUser
 }: TemplateListProps): ReactElement {
-  // const [sortOrder, setSortOrder] = useState<SortOrder>()
-  // const [activeTabLoaded, setActiveTabLoaded] = useState(false)
-  // const [activeEvent, setActiveEvent] = useState(event)
+  const [sortOrder, setSortOrder] = useState<SortOrder>()
+  const [activeTabLoaded, setActiveTabLoaded] = useState(false)
+  const [activeEvent, setActiveEvent] = useState(event)
 
-  // useEffect(() => {
-  //   setActiveEvent(event)
-  // }, [event])
+  useEffect(() => {
+    setActiveEvent(event)
+  }, [event])
 
-  // function activeTabOnLoad(): void {
-  //   setActiveTabLoaded(true)
-  // }
+  function activeTabOnLoad(): void {
+    setActiveTabLoaded(true)
+  }
 
-  // const journeyListProps = {
-  //   onLoad: activeTabOnLoad,
-  //   sortOrder: sortOrder,
-  //   event: activeEvent,
-  //   authUser: authUser
-  // }
+  const journeyListProps = {
+    onLoad: activeTabOnLoad,
+    sortOrder: sortOrder,
+    event: activeEvent,
+    authUser: authUser
+  }
 
   return (
     <Container sx={{ px: { xs: 0, sm: 8 } }}>
-      {/* Remove */}
-      <TemplateStatusTabs event={event} router={router} authUser={authUser} />
-      {/* <StatusTabPanel
+      <StatusTabPanel
         activeList={<ActiveTemplates {...journeyListProps} />}
         archivedList={<ArchivedTemplates {...journeyListProps} />}
         trashedList={<TrashedTemplates {...journeyListProps} />}
@@ -52,7 +49,7 @@ export function TemplateList({
         setSortOrder={setSortOrder}
         sortOrder={sortOrder}
         router={router}
-      /> */}
+      />
     </Container>
   )
 }
--- apps/journeys-admin/src/components/TemplateList/TemplateStatusTabs/index.ts
+++
@@ -1,1 +0,0 @@
-export { TemplateStatusTabs } from './TemplateStatusTabs'
