19c7fdfcb38897b9a613931fb160771b6a98bc2c
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2022-09-07 15:03:28 +1200

    feat: date and preview (#843)
    * feat: date and preview
    
    * fix: added loading template state

---
+++ apps/journeys-admin/src/components/JourneyView/DatePreview/DatePreview.tsx
@@ -0,0 +1,49 @@
+import { ReactElement } from 'react'
+import { parseISO, intlFormat } from 'date-fns'
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import Skeleton from '@mui/material/Skeleton'
+import { useRouter } from 'next/router'
+
+export function DatePreview(): ReactElement {
+  const { journey } = useJourney()
+  const router = useRouter()
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      sx={{ alignItems: 'center' }}
+    >
+      <Typography variant="overline" sx={{ color: 'secondary.light' }}>
+        {journey != null ? (
+          intlFormat(parseISO(journey.createdAt), {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+          })
+        ) : (
+          <Skeleton variant="text" width="100px" />
+        )}
+      </Typography>
+      <Button
+        startIcon={<VisibilityIcon />}
+        variant="outlined"
+        size="small"
+        color="secondary"
+        disabled={journey == null}
+        style={{ borderRadius: 8 }}
+        onClick={async () =>
+          await router.push(
+            journey != null ? `/api/preview?slug=${journey.slug}` : ''
+          )
+        }
+      >
+        Preview
+      </Button>
+    </Stack>
+  )
+}
