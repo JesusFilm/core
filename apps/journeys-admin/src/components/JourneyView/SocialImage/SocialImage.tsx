6405a04a7a953150591cd33658f53ba50eb6eb16
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2022-09-09 10:09:24 +1200

    feat: journey view social image (#842)
    * feat: social image
    
    * fix: tests and remove usemediaquery
    
    * fix: styling issue
    
    * fix: use nextimage
    
    * feat: date and preview (#843)
    
    * feat: date and preview
    
    * fix: added loading template state
    
    * fix: loading state
    
    * fix: update image according to design

---
+++ apps/journeys-admin/src/components/JourneyView/SocialImage/SocialImage.tsx
@@ -0,0 +1,48 @@
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import Box from '@mui/material/Box'
+import { ReactElement } from 'react'
+import { NextImage } from '@core/shared/ui/NextImage'
+import ImageIcon from '@mui/icons-material/Image'
+
+export function SocialImage(): ReactElement {
+  const { journey } = useJourney()
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      {journey?.primaryImageBlock?.src != null ? (
+        <NextImage
+          src={journey.primaryImageBlock.src}
+          alt={journey?.primaryImageBlock.alt}
+          placeholder="blur"
+          blurDataURL={journey?.primaryImageBlock.blurhash}
+          width={213}
+          height={167}
+          objectFit="cover"
+          style={{
+            borderRadius: 12
+          }}
+        />
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 3,
+            width: 213,
+            height: 167,
+            backgroundColor: 'background.default'
+          }}
+        >
+          <ImageIcon fontSize="large" />
+        </Box>
+      )}
+    </Box>
+  )
+}
