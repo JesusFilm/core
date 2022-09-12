33cf4ffeadde2ce8aca16868554fef042c2216c5
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-09-12 00:46:59 +0000

    chore: run codegen

--- apps/journeys-admin/__generated__/GetJourney.ts
+++ apps/journeys-admin/__generated__/GetJourney.ts
@@ -353,13 +353,19 @@ export interface GetJourney_journey_blocks_VideoBlock {
   posterBlockId: string | null;
   fullsize: boolean | null;
   /**
-   * videoId and videoVariantLanguageId both need to be set to select a video
+   * videoId and videoVariantLanguageId both need to be set to select a video.
+   * Relates to videos from the Jesus Film Project video library.
    */
   videoId: string | null;
   /**
    * videoId and videoVariantLanguageId both need to be set to select a video
+   * Relates to videos from the Jesus Film Project video library.
    */
   videoVariantLanguageId: string | null;
+  /**
+   * video is only populated when videoID and videoVariant LanguageId are present.
+   * Relates to videos from the Jesus Film Project video library.
+   */
   video: GetJourney_journey_blocks_VideoBlock_video | null;
   /**
    * action that should be performed when the video ends
