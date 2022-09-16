aececebc65d1604e91400db860a914dffce0127d
Siyang <csiyang@yahoo.co.nz>
2022-09-16 03:15:40 +0000

    fix: block name

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/TextResponse/TextResponse.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/TextResponse/TextResponse.tsx
@@ -2,10 +2,12 @@ import { ReactElement } from 'react'
 import type { TreeBlock } from '@core/journeys/ui/block'
 import { useEditor } from '@core/journeys/ui/EditorProvider'
 import LinkRoundedIcon from '@mui/icons-material/LinkRounded'
-import { GetJourney_journey_blocks_TextFieldBlock as TextFieldBlock } from '../../../../../../../__generated__/GetJourney' // fix type
+import { GetJourney_journey_blocks_TextResponseBlock as TextResponseBlock } from '../../../../../../../__generated__/GetJourney'
 import { Attribute } from '../..'
 
-export function TextResponse(block: TreeBlock<TextFieldBlock>): ReactElement {
+export function TextResponse(
+  block: TreeBlock<TextResponseBlock>
+): ReactElement {
   const { id } = block
 
   const { dispatch } = useEditor()
@@ -22,7 +24,7 @@ export function TextResponse(block: TreeBlock<TextFieldBlock>): ReactElement {
       id={`${id}-text-field-options`}
       icon={<LinkRoundedIcon />} // change this when we find out icon
       name="Text Field"
-      value={block?.id} // use block.label after fixing type
+      value={block?.id} // use block.label when the compoent is created
       description="Label and Hint text"
       onClick={openDrawer}
     />
