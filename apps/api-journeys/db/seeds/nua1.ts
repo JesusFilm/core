46e91ccea4f442dd1e245ae4f1f2a5e5d247a983
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-09-20 03:27:28 +0000

    fix: update seed

--- apps/api-journeys/db/seeds/nua1.ts
+++ apps/api-journeys/db/seeds/nua1.ts
@@ -115,6 +115,14 @@ export async function nua1(): Promise<void> {
       color: 'primary',
       align: 'left',
       parentOrder: 2
+    },
+    {
+      journeyId: journey._key,
+      __typename: 'TextResponseBlock',
+      parentBlockId: card1._key,
+      label: 'Your answer here',
+      submitLabel: 'Submit',
+      parentOrder: 3
     }
   ])
 
@@ -138,7 +146,7 @@ export async function nua1(): Promise<void> {
       gtmEventName: 'click',
       blockId: step2._key
     },
-    parentOrder: 3
+    parentOrder: 4
   })
 
   const icon1a = await db.collection('blocks').save({
