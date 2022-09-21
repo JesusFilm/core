e993c6deb8dd05f998703563959c18a3275fb6b2
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-09-21 02:38:53 +0000

    test: full coverage of source file

--- apps/journeys-admin/src/components/Editor/VideoBlockEditor/Source/Source.spec.tsx
+++ apps/journeys-admin/src/components/Editor/VideoBlockEditor/Source/Source.spec.tsx
@@ -3,6 +3,7 @@ import { MockedProvider } from '@apollo/client/testing'
 import { GET_VIDEOS } from '../../VideoLibrary/VideoFromLocal/VideoFromLocal'
 import { GET_VIDEO } from '../../VideoLibrary/VideoFromLocal/LocalDetails/LocalDetails'
 import { videos } from '../../VideoLibrary/VideoFromLocal/data'
+import { VideoBlockSource } from '../../../../../__generated__/globalTypes'
 import { Source } from '.'
 
 jest.mock('@mui/material/useMediaQuery', () => ({
@@ -10,69 +11,68 @@ jest.mock('@mui/material/useMediaQuery', () => ({
   default: jest.fn()
 }))
 
+const getVideosMock = {
+  request: {
+    query: GET_VIDEOS,
+    variables: {
+      offset: 0,
+      limit: 5,
+      where: {
+        availableVariantLanguageIds: ['529'],
+        title: null
+      }
+    }
+  },
+  result: {
+    data: {
+      videos
+    }
+  }
+}
+
+const getVideoMock = {
+  request: {
+    query: GET_VIDEO,
+    variables: {
+      id: '2_0-Brand_Video',
+      languageId: '529'
+    }
+  },
+  result: {
+    data: {
+      video: {
+        id: '2_0-Brand_Video',
+        image:
+          'https://d1wl257kev7hsz.cloudfront.net/cinematics/2_Acts7302-0-0.mobileCinematicHigh.jpg',
+        primaryLanguageId: '529',
+        title: [
+          {
+            primary: true,
+            value: 'Jesus Taken Up Into Heaven'
+          }
+        ],
+        description: [
+          {
+            primary: true,
+            value:
+              'Jesus promises the Holy Spirit; then ascends into the clouds.'
+          }
+        ],
+        variant: {
+          id: 'variantA',
+          duration: 144,
+          hls: 'https://arc.gt/opsgn'
+        }
+      }
+    }
+  }
+}
+
 describe('Source', () => {
   it('calls onChange when video selected', async () => {
     const onChange = jest.fn()
     const { getByRole, getByText } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: GET_VIDEOS,
-              variables: {
-                offset: 0,
-                limit: 5,
-                where: {
-                  availableVariantLanguageIds: ['529'],
-                  title: null
-                }
-              }
-            },
-            result: {
-              data: {
-                videos
-              }
-            }
-          },
-          {
-            request: {
-              query: GET_VIDEO,
-              variables: {
-                id: '2_0-Brand_Video',
-                languageId: '529'
-              }
-            },
-            result: {
-              data: {
-                video: {
-                  id: '2_0-Brand_Video',
-                  image:
-                    'https://d1wl257kev7hsz.cloudfront.net/cinematics/2_Acts7302-0-0.mobileCinematicHigh.jpg',
-                  primaryLanguageId: '529',
-                  title: [
-                    {
-                      primary: true,
-                      value: 'Jesus Taken Up Into Heaven'
-                    }
-                  ],
-                  description: [
-                    {
-                      primary: true,
-                      value:
-                        'Jesus promises the Holy Spirit; then ascends into the clouds.'
-                    }
-                  ],
-                  variant: {
-                    id: 'variantA',
-                    duration: 144,
-                    hls: 'https://arc.gt/opsgn'
-                  }
-                }
-              }
-            }
-          }
-        ]}
-      >
+      <MockedProvider mocks={[getVideosMock, getVideoMock]}>
         <Source selectedBlock={null} onChange={onChange} />
       </MockedProvider>
     )
@@ -91,5 +91,78 @@ describe('Source', () => {
         endAt: 144
       })
     )
+  })
+
+  it('shows YouTube video', async () => {
+    const onChange = jest.fn()
+    const { getByRole } = render(
+      <MockedProvider>
+        <Source
+          selectedBlock={{
+            id: 'video1.id',
+            __typename: 'VideoBlock',
+            parentBlockId: 'card1.id',
+            description:
+              'This is episode 1 of an ongoing series that explores the origins, content, and purpose of the Bible.',
+            duration: 348,
+            endAt: 348,
+            fullsize: true,
+            image: 'https://i.ytimg.com/vi/ak06MSETeo4/default.jpg',
+            muted: false,
+            autoplay: true,
+            startAt: 0,
+            title: 'What is the Bible?',
+            videoId: 'ak06MSETeo4',
+            videoVariantLanguageId: null,
+            parentOrder: 0,
+            action: null,
+            source: VideoBlockSource.youTube,
+            video: null,
+            posterBlockId: 'poster1.id',
+            children: []
+          }}
+          onChange={onChange}
+        />
+      </MockedProvider>
+    )
+    expect(
+      getByRole('button', {
+        name: 'What is the Bible? What is the Bible? YouTube'
+      })
+    ).toBeInTheDocument()
+  })
+
+  it('shows select video button when block present without video', async () => {
+    const onChange = jest.fn()
+    const { getByRole } = render(
+      <MockedProvider>
+        <Source
+          selectedBlock={{
+            id: 'video1.id',
+            __typename: 'VideoBlock',
+            parentBlockId: 'card1.id',
+            description: null,
+            duration: 348,
+            endAt: 348,
+            fullsize: true,
+            image: null,
+            muted: false,
+            autoplay: true,
+            startAt: 0,
+            title: null,
+            videoId: null,
+            videoVariantLanguageId: null,
+            parentOrder: 0,
+            action: null,
+            source: VideoBlockSource.internal,
+            video: null,
+            posterBlockId: 'poster1.id',
+            children: []
+          }}
+          onChange={onChange}
+        />
+      </MockedProvider>
+    )
+    expect(getByRole('button', { name: 'Select Video' })).toBeInTheDocument()
   })
 })
