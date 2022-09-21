dcdd3fbb8c350f45e5b5e7f23f68d9eef28d7e65
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2022-09-21 17:58:43 +1200

    feat: text response block (#898)
    * feat: textfield submission event
    
    * fix: rename field to response
    
    * fix: update codegen
    
    * fix: remodel text response
    
    * feat: codegen
    
    * feat: add text response fields
    
    * feat: test onBlur
    
    * feat: add basic TextResponse
    
    * feat: add translation file
    
    * fix: add placeholder translations
    
    * fix: remove placeholder translations
    
    * fix: extract-translations
    
    * fix: add minRows prop, label prop
    
    * fix: remove label prop
    
    * feat: codegen for minRows
    
    * fix: remove onBlur related changes
    
    * fix: extract translations
    
    * fix: type-check
    
    * feat: fix build
    
    * feat: test add snackbar provider
    
    * fix: revert snackbar change
    
    * fix: remove seed data
    
    * fix: add hint
    
    * feat: add stories
    
    * feat: add unit tests
    
    * fix: revert cleanup changes
    
    * fix: clean up tests
    
    * fix: update seed
    
    * fix: remove error message persist, loading snapshot fix
    
    * fix: add maxLength
    
    * fix: add test, remove seed
    
    * fix: add block to journey-admins cache
    
    * fix: tidy up after testing

---
+++ libs/journeys/ui/src/components/TextResponse/TextResponse.spec.tsx
@@ -0,0 +1,277 @@
+import { ReactElement } from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import { MockedProvider, MockedResponse } from '@apollo/client/testing'
+import { SnackbarProvider } from 'notistack'
+import TagManager from 'react-gtm-module'
+import { ApolloLoadingProvider } from '../../../test/ApolloLoadingProvider'
+import type { TreeBlock } from '../../libs/block'
+import { activeBlockVar, treeBlocksVar } from '../../libs/block'
+import { JourneyProvider } from '../../libs/JourneyProvider'
+import { handleAction } from '../../libs/action'
+import { BlockFields_StepBlock as StepBlock } from '../../libs/block/__generated__/BlockFields'
+import {
+  TextResponse,
+  TEXT_RESPONSE_SUBMISSION_EVENT_CREATE
+} from './TextResponse'
+import { TextResponseFields } from './__generated__/TextResponseFields'
+
+jest.mock('../../libs/action', () => {
+  const originalModule = jest.requireActual('../../libs/action')
+  return {
+    __esModule: true,
+    ...originalModule,
+    handleAction: jest.fn()
+  }
+})
+
+jest.mock('react-gtm-module', () => ({
+  __esModule: true,
+  default: {
+    dataLayer: jest.fn()
+  }
+}))
+
+const mockedDataLayer = TagManager.dataLayer as jest.MockedFunction<
+  typeof TagManager.dataLayer
+>
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      push: () => null
+    }
+  }
+}))
+
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str
+    }
+  }
+}))
+
+const block: TreeBlock<TextResponseFields> = {
+  __typename: 'TextResponseBlock',
+  id: 'textResponse0.id',
+  parentBlockId: '0',
+  parentOrder: 0,
+  label: 'Your answer here',
+  hint: null,
+  minRows: null,
+  submitIconId: null,
+  submitLabel: null,
+  action: {
+    __typename: 'LinkAction',
+    parentBlockId: 'textResponse0.id',
+    gtmEventName: 'textResponse',
+    url: '#'
+  },
+  children: []
+}
+
+const submissionSuccess = {
+  request: {
+    query: TEXT_RESPONSE_SUBMISSION_EVENT_CREATE,
+    variables: {
+      input: {
+        id: 'uuid',
+        blockId: 'textResponse0.id',
+        value: 'My response'
+      }
+    }
+  },
+  result: {
+    data: {
+      textResponseSubmissionEventCreate: {
+        id: 'uuid',
+        blockId: 'textResponse0.id',
+        value: 'My response'
+      }
+    }
+  }
+}
+
+interface TextResponseMockProps {
+  mocks?: Array<MockedResponse<Record<string, unknown>>>
+}
+
+const TextResponseMock = ({
+  mocks = []
+}: TextResponseMockProps): ReactElement => (
+  <MockedProvider mocks={mocks} addTypename={false}>
+    <JourneyProvider>
+      <SnackbarProvider>
+        <TextResponse {...block} uuid={() => 'uuid'} />
+      </SnackbarProvider>
+    </JourneyProvider>
+  </MockedProvider>
+)
+
+describe('TextResponse', () => {
+  it('should have 1000 character max length', async () => {
+    const { getByLabelText } = render(
+      <TextResponseMock mocks={[submissionSuccess]} />
+    )
+
+    const responseField = getByLabelText('Your answer here')
+
+    await waitFor(() => {
+      expect(responseField).toHaveAttribute('maxlength', '1000')
+    })
+  })
+
+  it('should redirect when form submit suceeds', async () => {
+    const { getByLabelText, getByRole } = render(
+      <TextResponseMock mocks={[submissionSuccess]} />
+    )
+
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: 'My response' } })
+    fireEvent.click(submit)
+
+    await waitFor(() => {
+      expect(handleAction).toBeCalledWith(
+        {
+          push: expect.any(Function)
+        },
+        {
+          __typename: 'LinkAction',
+          parentBlockId: 'textResponse0.id',
+          gtmEventName: 'textResponse',
+          url: '#'
+        }
+      )
+    })
+  })
+
+  it('should be in a loading state when waiting for response', async () => {
+    const { getByRole, getByLabelText } = render(
+      <ApolloLoadingProvider>
+        <TextResponseMock mocks={[submissionSuccess]} />
+      </ApolloLoadingProvider>
+    )
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: 'My response' } })
+
+    expect(submit).not.toHaveClass('MuiLoadingButton-loading')
+
+    fireEvent.click(submit)
+
+    await waitFor(() => expect(submit).toHaveClass('MuiLoadingButton-loading'))
+  })
+
+  it('should not create submission event if input is empty', async () => {
+    const result = jest.fn(() => ({
+      data: {
+        textResponseSubmissionEventCreate: {
+          id: 'uuid',
+          blockId: 'textResponse0.id',
+          value: ' '
+        }
+      }
+    }))
+    const { getByLabelText, getByRole } = render(
+      <TextResponseMock mocks={[{ ...submissionSuccess, result }]} />
+    )
+
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: ' ' } })
+    fireEvent.click(submit)
+
+    await waitFor(() => {
+      expect(result).not.toBeCalled()
+      expect(handleAction).toBeCalled()
+    })
+  })
+
+  it('should create submission event on click', async () => {
+    const result = jest.fn(() => ({
+      data: {
+        textResponseSubmissionEventCreate: {
+          id: 'uuid',
+          blockId: 'textResponse0.id',
+          value: 'My response'
+        }
+      }
+    }))
+
+    const { getByLabelText, getByRole } = render(
+      <TextResponseMock mocks={[{ ...submissionSuccess, result }]} />
+    )
+
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: 'My response' } })
+    fireEvent.click(submit)
+
+    await waitFor(() => {
+      expect(result).toBeCalled()
+    })
+  })
+
+  it('should add submission event to dataLayer', async () => {
+    const activeBlock: TreeBlock<StepBlock> = {
+      __typename: 'StepBlock',
+      id: 'Step1',
+      parentBlockId: null,
+      parentOrder: 0,
+      locked: true,
+      nextBlockId: null,
+      children: []
+    }
+    activeBlockVar(activeBlock)
+    treeBlocksVar([activeBlock])
+
+    const { getByLabelText, getByRole } = render(
+      <TextResponseMock mocks={[submissionSuccess]} />
+    )
+
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: 'My response' } })
+    fireEvent.click(submit)
+
+    await waitFor(() => {
+      expect(mockedDataLayer).toHaveBeenCalledWith({
+        dataLayer: {
+          event: 'text_response_submission',
+          eventId: 'uuid',
+          blockId: 'textResponse0.id',
+          stepName: 'Step {{number}}'
+        }
+      })
+    })
+  })
+
+  it('should show error when submit fails', async () => {
+    const submissionError = {
+      ...submissionSuccess,
+      error: new Error()
+    }
+
+    const { getByRole, getByLabelText, getByText } = render(
+      <TextResponseMock mocks={[submissionError]} />
+    )
+    const responseField = getByLabelText('Your answer here')
+    const submit = getByRole('button')
+
+    fireEvent.change(responseField, { target: { value: 'My response' } })
+    fireEvent.click(submit)
+
+    expect(
+      await waitFor(() =>
+        getByText('Could not send response, please try again.')
+      )
+    ).toBeInTheDocument()
+  })
+})
