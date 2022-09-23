533481424be07dcb8ceacf07f6cb54cf06807773
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-09-23 01:45:14 +0000

    fix: access message temporarily pops up

--- apps/journeys-admin/src/components/TemplateLibrary/TemplateLibrary.tsx
+++ apps/journeys-admin/src/components/TemplateLibrary/TemplateLibrary.tsx
@@ -19,51 +19,58 @@ export function TemplateLibrary({
   journeys,
   templates
 }: TemplateLibraryProps): ReactElement {
-  return journeys != null && journeys.length > 0 ? (
-    <Container
-      sx={{
-        pt: 6,
-        px: { xs: 0, sm: 8 }
-      }}
-    >
-      {templates != null ? (
-        <>
-          {templates.map((template) => (
-            <TemplateCard key={template.id} journey={template} />
-          ))}
-        </>
-      ) : (
-        <>
-          {[1, 2, 3].map((index) => (
-            <TemplateCard key={`templateCard${index}`} />
-          ))}
-        </>
-      )}
-    </Container>
-  ) : (
-    <Container maxWidth="sm" sx={{ mt: 20 }}>
-      <Stack direction="column" spacing={8} alignItems="center">
-        <NewReleasesRounded sx={{ fontSize: 60 }} />
-        <Typography variant="h1" align="center">
-          You need to be invited to create the first template
-        </Typography>
-        <Typography variant="subtitle2" align="center">
-          Someone with a full account should add you to their journey as an
-          editor, after that you will have full access
-        </Typography>
-        <Box>
-          <Button
-            variant="contained"
-            startIcon={<ContactSupportRounded />}
-            size="medium"
-            onClick={() => {
-              window.location.href = `mailto:support@nextstep.is?subject=Invite request for the NextStep builder`
+  return (
+    <>
+      {/* TODO: journey == null is for journey loading, fix this pattern */}
+      {journeys == null ||
+        (journeys?.length > 0 && (
+          <Container
+            sx={{
+              pt: 6,
+              px: { xs: 0, sm: 8 }
             }}
           >
-            Contact Support
-          </Button>
-        </Box>
-      </Stack>
-    </Container>
+            {templates != null ? (
+              <>
+                {templates.map((template) => (
+                  <TemplateCard key={template.id} journey={template} />
+                ))}
+              </>
+            ) : (
+              <>
+                {[1, 2, 3].map((index) => (
+                  <TemplateCard key={`templateCard${index}`} />
+                ))}
+              </>
+            )}
+          </Container>
+        ))}
+      {journeys?.length === 0 && (
+        <Container maxWidth="sm" sx={{ mt: 20 }}>
+          <Stack direction="column" spacing={8} alignItems="center">
+            <NewReleasesRounded sx={{ fontSize: 60 }} />
+            <Typography variant="h1" align="center">
+              You need to be invited to create the first template
+            </Typography>
+            <Typography variant="subtitle2" align="center">
+              Someone with a full account should add you to their journey as an
+              editor, after that you will have full access
+            </Typography>
+            <Box>
+              <Button
+                variant="contained"
+                startIcon={<ContactSupportRounded />}
+                size="medium"
+                onClick={() => {
+                  window.location.href = `mailto:support@nextstep.is?subject=Invite request for the NextStep builder`
+                }}
+              >
+                Contact Support
+              </Button>
+            </Box>
+          </Stack>
+        </Container>
+      )}
+    </>
   )
 }
