5d1d05058336f509bafa706429c26c78c3dbd5af
jernst7 <JEErnst15@gmail.com>
2022-10-04 23:06:20 +0000

    style: outlined request invite and back to admin panel buttons, aligned items to center in the card

--- apps/journeys-admin/src/components/AccessDenied/AccessDenied.tsx
+++ apps/journeys-admin/src/components/AccessDenied/AccessDenied.tsx
@@ -40,7 +40,13 @@ export function AccessDenied({
           </a>
         </Link>
       </Box>
-      <Card variant="outlined">
+      <Card variant="outlined" sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center'
+        }}>
         <CardContent>
           <Typography variant="h3" component="h1" gutterBottom>
             {title}
@@ -49,10 +55,10 @@ export function AccessDenied({
         </CardContent>
         <CardActions>
           {requestAccess ? (
-            <Button onClick={onClick}>Request Access</Button>
+            <Button variant="outlined" onClick={onClick}>Request Access</Button>
           ) : (
             <Link href="/" passHref>
-              <Button>Back to the Admin Panel</Button>
+              <Button variant="outlined" >Back to the Admin Panel</Button>
             </Link>
           )}
         </CardActions>
