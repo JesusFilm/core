4072824437abd20ee0b3c11d80016f374e4d473d
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-10-10 04:58:04 +0000

    fix: add journey rtl check

---
+++ libs/journeys/ui/src/libs/rtl/rtl.ts
@@ -0,0 +1,9 @@
+import { isRtl } from '../../../../../shared/ui/src/libs/rtl'
+import { JourneyFields as Journey } from '../JourneyProvider/__generated__/JourneyFields'
+
+export function getJourneyRtl(journey: Journey): boolean {
+  // Currently just check for bcp47 codes
+  const locale = journey.language.bcp47 ?? ''
+
+  return isRtl(locale)
+}
