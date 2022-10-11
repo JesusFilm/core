2085c82a7e233d5384d4565485936461151b2516
Mike Allison <mike@captainofbass.com>
2022-10-11 00:23:49 +0000

    feat: tf

--- modules/aws/route53/outputs.tf
+++ modules/aws/route53/outputs.tf
@@ -1,7 +1,3 @@
-# output "internal_service_dns" {
-#   value = aws_route53_zone.private_zone
-# }
-
 output "id" {
   value = aws_route53_zone.private_zone.id
 }
--- resources/aws/ecs/main/ecs.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecs_cluster" "main" {
-  name = local.env
-}
