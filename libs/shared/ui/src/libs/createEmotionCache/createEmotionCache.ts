2f3e85f1be0e8fe31cfa1152fd088a898d7b631c
John Geronimo <johnageronimo1@gmail.com>
2022-10-11 01:59:44 +0000

    feat: add rtl to journeys

--- libs/shared/ui/src/libs/createEmotionCache/createEmotionCache.ts
+++ libs/shared/ui/src/libs/createEmotionCache/createEmotionCache.ts
@@ -2,8 +2,6 @@ import createCache, { EmotionCache } from '@emotion/cache'
 import { prefixer } from 'stylis'
 import rtlPlugin from 'stylis-plugin-rtl'
 
-// todo: Add rtl check for stylisPlugins
-
 // prepend: true moves MUI styles to the top of the <head> so they're loaded first.
 // It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
 export function createEmotionCache(isRTL?: boolean): EmotionCache {
