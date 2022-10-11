62d167ca8536893fb28ad7325fc738095391a00a
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2022-10-12 11:58:39 +1300

    feat: add rtl to core from material ui  (#949)
    * feat: add rtl to cache provider
    
    * feat: add rtl to journeys
    
    * fix: rtl check in document
    
    * chore: build storybook

--- apps/journeys/pages/_app.tsx
+++ apps/journeys/pages/_app.tsx
@@ -12,16 +12,18 @@ import { SnackbarProvider } from 'notistack'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { appWithTranslation } from 'next-i18next'
 import { useTranslation } from 'react-i18next'
+import { getJourneyRtl } from '@core/journeys/ui/rtl'
 import { useApollo } from '../src/libs/apolloClient'
 import { firebaseClient } from '../src/libs/firebaseClient'
 import i18nConfig from '../next-i18next.config'
 
-const clientSideEmotionCache = createEmotionCache()
+const clientSideEmotionCache = (isRTL: boolean): EmotionCache =>
+  createEmotionCache(isRTL)
 
 function JourneysApp({
   Component,
   pageProps,
-  emotionCache = clientSideEmotionCache
+  emotionCache = clientSideEmotionCache(getJourneyRtl(pageProps.journey))
 }: AppProps & { emotionCache?: EmotionCache }): ReactElement {
   const { t } = useTranslation('apps-journeys')
   useEffect(() => {
