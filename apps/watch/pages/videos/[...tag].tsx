59e26a369e46ed392e23c4b2b4e9b6ec02c9b1b4
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2022-10-31 10:51:45 +1300

    fix: watch page wrapper (#1003)
    * fix: page wrapper
    
    * fix: remove header menu panel
    
    * fix: pagewrapper story

--- apps/watch/pages/videos/[...tag].tsx
+++ apps/watch/pages/videos/[...tag].tsx
@@ -11,6 +11,8 @@ import {
 import { Videos } from '../../src/components/Videos/Videos'
 import { LanguageProvider } from '../../src/libs/languageContext/LanguageContext'
 import { routeParser } from '../../src/libs/routeParser/routeParser'
+import { Header } from '../../src/components/Header'
+import { Footer } from '../../src/components/Footer/Footer'
 
 function VideoPage(): ReactElement {
   const router = useRouter()
@@ -21,34 +23,35 @@ function VideoPage(): ReactElement {
   return (
     <DarkThemeProvider>
       <LanguageProvider>
-        <PageWrapper />
-        <Container
-          maxWidth="xl"
-          style={{
-            position: 'absolute',
-            top: 100,
-            paddingLeft: 100,
-            paddingRight: 100,
-            margin: 0
-          }}
-        >
-          <style jsx global>{`
-            body {
-              background: ${darkTheme.palette.background.default};
-              color: ${darkTheme.palette.text.primary};
-            }
-          `}</style>
+        <PageWrapper header={<Header />} footer={<Footer />}>
+          <Container
+            maxWidth="xl"
+            style={{
+              position: 'absolute',
+              top: 100,
+              paddingLeft: 100,
+              paddingRight: 100,
+              margin: 0
+            }}
+          >
+            <style jsx global>{`
+              body {
+                background: ${darkTheme.palette.background.default};
+                color: ${darkTheme.palette.text.primary};
+              }
+            `}</style>
 
-          <Typography variant="h4">Videos</Typography>
-          <Videos />
-          <Box mt="3rem">
-            <div>Locale - {locale} </div>
-            <div>SeoFriendlyTag - {routes?.join('/')}</div>
-            <div>Tags - {tags.join(' ')}</div>
-            <div>AudioLanguange - {audioLanguage}</div>
-            <div>SubtitleLanguage - {subtitleLanguage}</div>
-          </Box>
-        </Container>
+            <Typography variant="h4">Videos</Typography>
+            <Videos />
+            <Box mt="3rem">
+              <div>Locale - {locale} </div>
+              <div>SeoFriendlyTag - {routes?.join('/')}</div>
+              <div>Tags - {tags.join(' ')}</div>
+              <div>AudioLanguange - {audioLanguage}</div>
+              <div>SubtitleLanguage - {subtitleLanguage}</div>
+            </Box>
+          </Container>
+        </PageWrapper>
       </LanguageProvider>
     </DarkThemeProvider>
   )
