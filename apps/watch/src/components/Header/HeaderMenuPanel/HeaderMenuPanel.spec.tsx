3c4f7609b1c4bcf9ad068552fc92b5f766a3220f
John Geronimo <johnageronimo1@gmail.com>
2022-11-01 00:37:36 +0000

    fix: component naming and use

--- apps/watch/src/components/Header/HeaderMenuPanel/HeaderMenuPanel.spec.tsx
+++ apps/watch/src/components/Header/HeaderMenuPanel/HeaderMenuPanel.spec.tsx
@@ -11,46 +11,46 @@ describe('HeaderMenuPanel', () => {
     expect(toggleDrawer).toHaveBeenCalled()
   })
   it('should redirect to Jesus Film About page', () => {
-    const { getByText } = render(
+    const { getByRole } = render(
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
-    expect(getByText('About').closest('a')).toHaveAttribute(
+    expect(getByRole('link', { name: 'About' }).closest('a')).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/about'
     )
   })
-  it('should redirect to Jesus Film Aive page', () => {
-    const { getByText } = render(
+  it('should redirect to Jesus Film Give page', () => {
+    const { getByRole } = render(
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
-    expect(getByText('Give').closest('a')).toHaveAttribute(
+    expect(getByRole('link', { name: 'Give' }).closest('a')).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/give'
     )
   })
   it('should redirect to Jesus Film Partner page', () => {
-    const { getByText } = render(
+    const { getByRole } = render(
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
-    expect(getByText('Partner').closest('a')).toHaveAttribute(
+    expect(getByRole('link', { name: 'Partner' }).closest('a')).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/partners'
     )
   })
   it('should redirect to Jesus Film Tools page', () => {
-    const { getByText } = render(
+    const { getByRole } = render(
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
-    expect(getByText('Tools').closest('a')).toHaveAttribute(
+    expect(getByRole('link', { name: 'Tools' }).closest('a')).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/tools'
     )
   })
   it('should redirect to Jesus Film Blog page', () => {
-    const { getByText } = render(
+    const { getByRole } = render(
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
-    expect(getByText('Blog').closest('a')).toHaveAttribute(
+    expect(getByRole('link', { name: 'Blog' }).closest('a')).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/blog'
     )
@@ -60,7 +60,7 @@ describe('HeaderMenuPanel', () => {
       <HeaderMenuPanel toggleDrawer={toggleDrawer} />
     )
     expect(
-      getByRole('button', { name: 'Give Now' }).closest('a')
+      getByRole('link', { name: 'Give Now' }).closest('a')
     ).toHaveAttribute(
       'href',
       'https://www.jesusfilm.org/how-to-help/ways-to-donate/give-now-2/?amount=&frequency=single&campaign-code=NXWJPO&designation-number=2592320&thankYouRedirect=https%3A%2F%2Fwww.jesusfilm.org%2Fcontent%2Fjf%2Fus%2Fdevelopment%2Fspecial%2Fthank-you-refer%2Fsocial-share.html'
