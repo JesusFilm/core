e0eac56b9d1c1624c57735e688c8041e3b01e7f5
Siyang <csiyang@yahoo.co.nz>
2022-11-01 04:40:53 +0000

    refactor: move get visitor to visitor service

--- apps/api-journeys/src/app/modules/event/event.service.ts
+++ apps/api-journeys/src/app/modules/event/event.service.ts
@@ -1,27 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { aql } from 'arangojs'
 import { BaseService } from '@core/nest/database/BaseService'
 import { DocumentCollection } from 'arangojs/collection'
-import { KeyAsId } from '@core/nest/decorators/KeyAsId'
-import { Visitor } from '../../__generated__/graphql'
 
 @Injectable()
 export class EventService extends BaseService {
   collection: DocumentCollection = this.db.collection('events')
-
-  @KeyAsId()
-  async getVisitorByUserIdAndJourneyId(
-    userId: string,
-    journeyId: string
-  ): Promise<Visitor> {
-    const res = await this.db.query(aql`
-      FOR v in visitors
-        FILTER v.userId == ${userId}
-        FOR j in journeys
-          FILTER j._key == ${journeyId} AND j.teamId == v.teamId
-          LIMIT 1
-          RETURN v
-    `)
-    return await res.next()
-  }
 }
