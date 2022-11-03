bc24d9a31d809ff87ea0df8dd6c19d5bc64b5083
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2022-11-03 13:45:37 +1300

    feat: events query (#1017)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

--- apps/api-journeys/src/app/modules/visitor/visitor.module.ts
+++ apps/api-journeys/src/app/modules/visitor/visitor.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common'
 import { DatabaseModule } from '@core/nest/database/DatabaseModule'
+import { EventService } from '../event/event.service'
 import { VisitorService } from './visitor.service'
 import { VisitorResolver } from './visitor.resolver'
 
 @Global()
 @Module({
   imports: [DatabaseModule],
-  providers: [VisitorService, VisitorResolver],
+  providers: [VisitorService, VisitorResolver, EventService],
   exports: [VisitorService]
 })
 export class VisitorModule {}
