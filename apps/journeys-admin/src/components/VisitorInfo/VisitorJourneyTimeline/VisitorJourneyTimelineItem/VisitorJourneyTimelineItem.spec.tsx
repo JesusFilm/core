3c7f574e3108e8fd17173a8829bedcf01e51b364
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-11-04 02:22:40 +0000

    test: visitor journey timeline item tests

--- apps/journeys-admin/src/components/VisitorInfo/VisitorJourneyTimeline/VisitorJourneyTimelineItem/VisitorJourneyTimelineItem.spec.tsx
+++ apps/journeys-admin/src/components/VisitorInfo/VisitorJourneyTimeline/VisitorJourneyTimelineItem/VisitorJourneyTimelineItem.spec.tsx
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react'
-import { VideoBlockSource } from '../../../../../__generated__/globalTypes'
+import {
+  MessagePlatform,
+  VideoBlockSource
+} from '../../../../../__generated__/globalTypes'
 import { GetVisitorEvents_visitor_events as Event } from '../../../../../__generated__/GetVisitorEvents'
 import { VisitorJourneyTimelineItem } from './VisitorJourneyTimelineItem'
 
@@ -16,6 +19,34 @@ describe('VisitorJourneyTimelineItem', () => {
     const { getByText } = render(<VisitorJourneyTimelineItem event={event} />)
     expect(getByText(event.label as string)).toBeInTheDocument()
     expect(getByText('Button clicked')).toBeInTheDocument()
+  })
+  it('shows chatOpenedEvent', () => {
+    const event: Event = {
+      __typename: 'ChatOpenEvent',
+      id: 'ChatOpenEventId',
+      journeyId: 'journeyId',
+      label: null,
+      value: 'facebook',
+      createdAt: '2022-11-02T03:20:26.368Z',
+      messagePlatform: MessagePlatform.facebook
+    }
+    const { getByText } = render(<VisitorJourneyTimelineItem event={event} />)
+    expect(getByText('Chat Opened')).toBeInTheDocument()
+    expect(getByText('Chat started on {{messagePlatform}}')).toBeInTheDocument()
+  })
+  it('shows chatOpenedEvent when messagePlatform is null', () => {
+    const event: Event = {
+      __typename: 'ChatOpenEvent',
+      id: 'ChatOpenEventId',
+      journeyId: 'journeyId',
+      label: null,
+      value: null,
+      createdAt: '2022-11-02T03:20:26.368Z',
+      messagePlatform: null
+    }
+    const { getByText } = render(<VisitorJourneyTimelineItem event={event} />)
+    expect(getByText('Chat Opened')).toBeInTheDocument()
+    expect(getByText('Chat started on {{messagePlatform}}')).toBeInTheDocument()
   })
   it('shows radioQuestionSubmissionEvent', () => {
     const event: Event = {
@@ -57,6 +88,21 @@ describe('VisitorJourneyTimelineItem', () => {
     const { getByText } = render(<VisitorJourneyTimelineItem event={event} />)
     expect(getByText('JESUS')).toBeInTheDocument()
     expect(getByText('YouTube')).toBeInTheDocument()
+    expect(getByText('Video completed')).toBeInTheDocument()
+  })
+  it('shows VideoCompleteEvent with null source', () => {
+    const event: Event = {
+      __typename: 'VideoCompleteEvent',
+      id: 'VideoCompleteEventId',
+      journeyId: 'journeyId',
+      label: 'JESUS',
+      value: null,
+      createdAt: '2022-11-02T03:20:26.368Z',
+      source: null
+    }
+    const { getByText } = render(<VisitorJourneyTimelineItem event={event} />)
+    expect(getByText('JESUS')).toBeInTheDocument()
+    expect(getByText('Video')).toBeInTheDocument()
     expect(getByText('Video completed')).toBeInTheDocument()
   })
   it('shows videoStartEvent', () => {
