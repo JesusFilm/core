72367dbc2e17223d782addda9e5a6695fe9e7de7
John Geronimo <johnageronimo1@gmail.com>
2022-11-04 01:45:33 +0000

    fix: styling issues

--- apps/watch/src/components/Hero/HomeHero/HomeHero.tsx
+++ apps/watch/src/components/Hero/HomeHero/HomeHero.tsx
@@ -3,15 +3,17 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import { useTheme } from '@mui/material/styles'
 import Image from 'next/image'
 
 import JesusHeader from '../../../../public/images/jesus-header.svg'
 
 export function HomeHero(): ReactElement {
+  const theme = useTheme()
   return (
     <Box
       sx={{
-        height: 777,
+        height: { xs: 502, lg: 777 },
         width: '100%',
         display: 'flex',
         alignItems: 'center',
@@ -31,7 +33,7 @@ export function HomeHero(): ReactElement {
           height: '100%',
           width: '100%',
           background:
-            'radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%)'
+            'linear-gradient(180deg, rgba(50, 50, 51, 0) 64%, rgba(38, 38, 38, 0.3) 76%, rgba(27, 27, 28, 0.46) 86%, #000000 100%), linear-gradient(90deg, #141414 16%, rgba(10, 10, 10, 0.5) 24%, rgba(4, 4, 4, 0.2) 31%, rgba(0, 0, 0, 0) 40%)'
         }}
       />
       <Container
@@ -39,25 +41,23 @@ export function HomeHero(): ReactElement {
         sx={{ display: 'flex', justifyContent: 'center' }}
       >
         <Stack
-          direction={{ xs: 'column', sm: 'column', md: 'column', lg: 'row' }}
+          direction={{ xs: 'column', lg: 'row' }}
           alignContent="center"
           sx={{ zIndex: 2 }}
         >
-          <Typography
-            variant="h1"
-            color="secondary.contrastText"
-            sx={{ zIndex: 1 }}
-          >
+          <Typography variant="h1" color="secondary.contrastText">
             Free Gospel Video{' '}
-            <span
-              style={{
+            <Typography
+              variant="h1"
+              component="span"
+              sx={{
                 textDecoration: 'underline',
-                textDecorationColor: '#EF3340',
+                textDecorationColor: theme.palette.primary.main,
                 textUnderlineOffset: 10
               }}
             >
               Streaming
-            </span>{' '}
+            </Typography>{' '}
             Library
           </Typography>
           <Typography
