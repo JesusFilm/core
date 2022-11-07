c8c87f2565a861bd18fd618110476add4113dc6d
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-11-07 07:33:42 +0000

    fix: test

--- apps/watch/src/components/Header/Header.spec.tsx
+++ apps/watch/src/components/Header/Header.spec.tsx
@@ -3,9 +3,10 @@ import { Header } from './Header'
 
 describe('Header', () => {
   it('should open navigation panel on menu icon click', () => {
-    const { getByTestId, getByText } = render(<Header />)
-    fireEvent.click(getByTestId('MenuIcon'))
-    expect(getByTestId('MenuIcon')).toHaveAccessibleName('open header menu')
+    const { getByText, getByRole } = render(<Header />)
+
+    fireEvent.click(getByRole('button', { name: 'open header menu' }))
+
     expect(getByText('About')).toBeInTheDocument()
   })
 })
