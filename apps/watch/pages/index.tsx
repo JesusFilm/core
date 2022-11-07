9d2c27bb543025b08f5a2516f756e67407fd2066
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2022-11-08 10:04:22 +1300

    fix: watch footer styling, add accessibility roles and tests (#1036)
    * fix: footer styling, add accessibility roles and tests
    
    * fix: remove redundant padding
    
    * fix: test

--- apps/watch/pages/index.tsx
+++ apps/watch/pages/index.tsx
@@ -1,54 +1,31 @@
 import { ReactElement } from 'react'
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
-import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
-import { ThemeName, ThemeMode } from '@core/shared/ui/themes'
 
 import { HomeHero } from '../src/components/Hero'
 import { Videos } from '../src/components/Videos'
 import { PageWrapper } from '../src/components/PageWrapper'
 import { VideoType } from '../__generated__/globalTypes'
 
-import { Footer } from '../src/components/Footer/Footer'
-import { Header } from '../src/components/Header'
-
 function HomePage(): ReactElement {
   return (
-    <PageWrapper header={<Header />} footer={<Footer />}>
-      <ThemeProvider
-        nested
-        themeName={ThemeName.website}
-        themeMode={ThemeMode.dark}
-      >
-        <HomeHero />
-      </ThemeProvider>
+    <PageWrapper hero={<HomeHero />}>
       <Box sx={{ paddingY: '4rem' }}>
-        <Container
-          sx={{
-            maxWidth: '100% !important',
-            width: '100%',
-            margin: 0,
-            paddingLeft: '100px !important',
-            paddingRight: '100px !important'
+        <Stack direction="row" justifyContent="space-between" mb={3}>
+          <Typography variant="h4">Series</Typography>
+          <Button variant="outlined">See All</Button>
+        </Stack>
+        <Videos
+          filter={{
+            availableVariantLanguageIds: ['529'],
+            types: [VideoType.playlist]
           }}
-        >
-          <Stack direction="row" justifyContent="space-between" mb={3}>
-            <Typography variant="h4">Series</Typography>
-            <Button variant="outlined">See All</Button>
-          </Stack>
-          <Videos
-            filter={{
-              availableVariantLanguageIds: ['529'],
-              types: [VideoType.playlist]
-            }}
-            limit={6}
-            showLoadMore={false}
-            layout="carousel"
-          />
-        </Container>
+          limit={6}
+          showLoadMore={false}
+          layout="carousel"
+        />
       </Box>
     </PageWrapper>
   )
