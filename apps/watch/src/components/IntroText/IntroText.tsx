e5ef8babc22950186d408f2bf5cd89319bc1ae86
Edmond Shen <egsmond@gmail.com>
2022-11-10 04:07:35 +0000

    feat: add introText to index

--- apps/watch/src/components/IntroText/IntroText.tsx
+++ apps/watch/src/components/IntroText/IntroText.tsx
@@ -8,7 +8,6 @@ export function IntroText(): ReactElement {
   return (
     <Box
       sx={{
-        background: '#111010',
         display: 'flex',
         width: '100%',
         alignItems: 'center',
@@ -29,14 +28,22 @@ export function IntroText(): ReactElement {
                 width: { xs: 38, lg: 14 }
               }}
             />
-            <Typography variant="subtitle2" color="secondary.contrastText">
+            <Typography
+              variant="subtitle2"
+              color="secondary.contrastText"
+              sx={{ opacity: 0.85 }}
+            >
               With 70% of the world not being able to speak English, there is a
               huge opportunity for the gospel to spread to unreached places. We
               have a vision to make it easier to watch, download and share
               Christian videos with people in their native heart language.
             </Typography>
           </Stack>
-          <Typography variant="subtitle1" color="secondary.contrastText">
+          <Typography
+            variant="subtitle1"
+            color="secondary.contrastText"
+            sx={{ opacity: 0.85 }}
+          >
             Jesus Film Project is a Christian ministry with a vision to reach
             the world with the gospel of Jesus Christ through evangelistic
             videos. Watch from over 2000 languages on any device and share it
