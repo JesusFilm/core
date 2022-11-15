238142650ce5c81f5651f081ea5bbe9b7bb62d75
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-11-15 21:21:42 +0000

    ci: update bash script

--- tools/scripts/check-git-diff.sh
+++ tools/scripts/check-git-diff.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# this script is used by GitHub actions to check if our current branch is out of date with origin main
+# this script is used by GitHub actions to check if our current branch is out of date with its base branch. Most commonly origin main, or a feature branch.
 
 DIFF=$(git diff --name-only origin/$GITHUB_HEAD_REF...origin/$GITHUB_BASE_REF)
 
 if [ ! -z "$DIFF" ]; then
   # Changes
   echo $DIFF
-  echo "🛑 - branch is not updated to latest main"
+  echo "🛑 - branch not updated with latest base changes"
   exit 1
 else
   # No changes
-  echo "✅ - branch is updated to latest main"
+  echo "✅ - branch is updated to latest base"
   exit 0
 fi
