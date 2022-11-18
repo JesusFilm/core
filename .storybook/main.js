a4d4fe00b93f57344d648a79055d1ebbe90260cd
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-11-18 00:36:06 +0000

    Revert "ci: build only affected stories"
    This reverts commit bf44c6366ac5d6158b7b4dbfc7bdbe0406b61550.

--- .storybook/main.js
+++ .storybook/main.js
@@ -42,33 +42,12 @@ const allStories = [
   ...storiesForProject['shared-ui']
 ]
 
-const affectedStories = () => {
-  const affectedApps = process.env.NX_AFFECTED_APPS ?? ''
-  const affectedLibs = process.env.NX_AFFECTED_LIBS ?? ''
-  const affectedProjects = [
-    ...affectedApps.split(' '),
-    ...affectedLibs.split(' ')
-  ]
-
-  const stories = affectedProjects
-    .map((project) => {
-      if (project === '') return []
-      return storiesForProject[project] ?? []
-    })
-    .flat()
-
-  return stories.length > 0 ? stories : undefined
-}
-
 module.exports = {
   staticDirs: [
     './static',
     { from: '../apps/watch/public/fonts', to: '/fonts' }
   ],
-  stories:
-    affectedStories() ||
-    storiesForProject[process.env.NX_TASK_TARGET_PROJECT] ||
-    allStories,
+  stories: allStories,
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
