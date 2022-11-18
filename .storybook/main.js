bf44c6366ac5d6158b7b4dbfc7bdbe0406b61550
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-11-18 00:24:00 +0000

    ci: build only affected stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -42,12 +42,33 @@ const allStories = [
   ...storiesForProject['shared-ui']
 ]
 
+const affectedStories = () => {
+  const affectedApps = process.env.NX_AFFECTED_APPS ?? ''
+  const affectedLibs = process.env.NX_AFFECTED_LIBS ?? ''
+  const affectedProjects = [
+    ...affectedApps.split(' '),
+    ...affectedLibs.split(' ')
+  ]
+
+  const stories = affectedProjects
+    .map((project) => {
+      if (project === '') return []
+      return storiesForProject[project] ?? []
+    })
+    .flat()
+
+  return stories.length > 0 ? stories : undefined
+}
+
 module.exports = {
   staticDirs: [
     './static',
     { from: '../apps/watch/public/fonts', to: '/fonts' }
   ],
-  stories: allStories,
+  stories:
+    affectedStories() ||
+    storiesForProject[process.env.NX_TASK_TARGET_PROJECT] ||
+    allStories,
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
