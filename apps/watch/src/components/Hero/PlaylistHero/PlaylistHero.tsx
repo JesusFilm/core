462303dc663f38bb073815c8c5f64de2f8231c23
Siyang <csiyang@yahoo.co.nz>
2022-12-02 01:53:46 +0000

    feat: add common overlay

--- apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.tsx
+++ apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.tsx
@@ -8,6 +8,7 @@ import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
 import IconButton from '@mui/material/IconButton'
 
 import { GetVideo_video as Video } from '../../../../__generated__/GetVideo'
+import { Overlay } from '../Overlay'
 
 interface Props {
   video: Video
@@ -32,18 +33,7 @@ export function PlaylistHero({ video }: Props): ReactElement {
       {image != null && (
         <Image src={image} alt="Home Hero" layout="fill" objectFit="cover" />
       )}
-      <Box
-        sx={{
-          zIndex: 1,
-          position: 'absolute',
-          height: '100%',
-          width: '100%',
-          background: {
-            xs: 'linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), linear-gradient(180.21deg, rgba(50, 50, 51, 0) 52.73%, rgba(38, 38, 38, 0.174648) 68.58%, rgba(27, 27, 28, 0.321668) 81.42%, rgba(0, 0, 0, 0.7) 99.82%), linear-gradient(89.75deg, rgba(20, 20, 20, 0.3) 7.11%, rgba(10, 10, 10, 0.148691) 27.28%, rgba(4, 4, 4, 0.0587687) 44.45%, rgba(0, 0, 0, 0) 66.08%), url(1_fj_1-0-0.mobileCinematicHigh.jpg)',
-            sm: 'linear-gradient(180.21deg, rgba(50, 50, 51, 0) 52.73%, rgba(38, 38, 38, 0.174648) 68.58%, rgba(27, 27, 28, 0.321668) 81.42%, rgba(0, 0, 0, 0.7) 99.82%), linear-gradient(89.75deg, rgba(20, 20, 20, 0.3) 7.11%, rgba(10, 10, 10, 0.148691) 27.28%, rgba(4, 4, 4, 0.0587687) 44.45%, rgba(0, 0, 0, 0) 66.08%)'
-          }
-        }}
-      />
+      <Overlay />
       <Container
         maxWidth="xxl"
         sx={{
