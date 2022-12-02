511e3390f188baa729dddb3462f25f3ff3ea18f6
Siyang <csiyang@yahoo.co.nz>
2022-12-02 02:05:56 +0000

    fix: tests

--- apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.spec.tsx
+++ apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.spec.tsx
@@ -1,19 +1,31 @@
 import { render } from '@testing-library/react'
-import { videos } from '../../Videos/testData'
+import { VideoLabel } from '../../../../__generated__/globalTypes'
 import { GetVideo_video as Video } from '../../../../__generated__/GetVideo'
 import { PlaylistHero } from '.'
 
 describe('PlaylistHero', () => {
-  const video = { ...videos[0], episodes: {} } as unknown as Video
+  const defaultVideo = {
+    label: VideoLabel.collection,
+    title: [{ value: 'Collection video title' }],
+    children: [1, 2, 3],
+    image:
+      'https://images.unsplash.com/photo-1669569713869-ff4a427a38ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3774&q=80'
+  } as unknown as Video
+
+  const seriesVideo = {
+    ...defaultVideo,
+    label: VideoLabel.series,
+    title: [{ value: 'Series video title' }]
+  } as unknown as Video
+
   it('should render hero for a collection', () => {
-    const { getByText } = render(<PlaylistHero video={video} />)
+    const { getByText } = render(<PlaylistHero video={defaultVideo} />)
 
     expect(getByText('collection')).toBeInTheDocument()
   })
 
-  // TODO: bring back once we can find difference between series and collection
-  it.skip('should render hero for a series', () => {
-    const { getByText } = render(<PlaylistHero video={video} />)
+  it('should render hero for a series', () => {
+    const { getByText } = render(<PlaylistHero video={seriesVideo} />)
 
     expect(getByText('series')).toBeInTheDocument()
   })
--- apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.stories.tsx
+++ apps/watch/src/components/Hero/PlaylistHero/PlaylistHero.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, Meta } from '@storybook/react'
 import { watchConfig } from '../../../libs/storybook'
-import { videos } from '../../Videos/testData'
 import { GetVideo_video as Video } from '../../../../__generated__/GetVideo'
+import { VideoLabel } from '../../../../__generated__/globalTypes'
 import { PlaylistHero } from '.'
 
 const PlaylistHeroStory = {
@@ -14,13 +14,24 @@ const Template: ComponentStory<typeof PlaylistHero> = ({ ...args }) => (
   <PlaylistHero {...args} />
 )
 
-const video = { ...videos[0], episodes: [] } as unknown as Video
+const defaultVideo = {
+  label: VideoLabel.collection,
+  title: [{ value: 'Collection video title' }],
+  children: [1, 2, 3],
+  image:
+    'https://images.unsplash.com/photo-1669569713869-ff4a427a38ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3774&q=80'
+} as unknown as Video
+
+const seriesVideo = {
+  ...defaultVideo,
+  label: VideoLabel.series,
+  title: [{ value: 'Series video title' }]
+}
 
 export const Default = Template.bind({})
-Default.args = { video }
+Default.args = { video: defaultVideo }
 
-// TODO: bring back once we can find difference between series and collection
-// export const Series = Template.bind({})
-// Series.args = { video }
+export const Series = Template.bind({})
+Series.args = { video: seriesVideo }
 
 export default PlaylistHeroStory as Meta
