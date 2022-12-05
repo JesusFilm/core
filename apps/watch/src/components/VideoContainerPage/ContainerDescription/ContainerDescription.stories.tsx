8dd9033b2e50d13c2fd17104128c8964399ded98
Dan <danchae03@gmail.com>
2022-12-05 21:15:57 +0000

    refactor: removed large input string on description story page

--- apps/watch/src/components/VideoContainerPage/ContainerDescription/ContainerDescription.stories.tsx
+++ apps/watch/src/components/VideoContainerPage/ContainerDescription/ContainerDescription.stories.tsx
@@ -2,9 +2,6 @@ import { ComponentStory, Meta } from '@storybook/react'
 import { watchConfig } from '../../../libs/storybook/config'
 import { ContainerDescription } from './ContainerDescription'
 
-const inputString =
-  'Following Jesus is a mini series designed to ground followers of Jesus in their faith and equip them to be fruitful members of Christ\'s body. Using storytelling techniques and incorporating images and clips from the "JESUS" film, "Following JESUS" teaches biblical principles and demonstrates how to live as a follower of Christ. Following Jesus is a mini series designed to ground followers of Jesus in their faith and equip them to be fruitful members of Christ\'s body. Using storytelling techniques and incorporating images and clips from the "JESUS" film, "Following JESUS" teaches biblical principles and demonstrates how to live as a follower of Christ. Following Jesus is a mini series designed to ground followers of Jesus in their faith and equip them to be fruitful members of Christ\'s body. Using storytelling techniques and incorporating images and clips from the "JESUS" film, "Following JESUS" teaches biblical principles and demonstrates how to live as a follower of Christ.'
-
 const ContainerDescriptionStory = {
   ...watchConfig,
   component: ContainerDescription,
@@ -21,7 +18,8 @@ const Template: ComponentStory<typeof ContainerDescription> = ({ ...args }) => (
 export const Default = Template.bind({})
 
 Default.args = {
-  value: inputString
+  value:
+    'This is where the description will go, to the left hand side of the share button'
 }
 
 export default ContainerDescriptionStory as Meta
