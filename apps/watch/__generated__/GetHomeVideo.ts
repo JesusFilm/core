11535155e2b641c97f9eaa5c8bc9b0b267af97d1
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2022-12-06 11:58:31 +1300

    refactor: home page refactor (#1122)

--- apps/watch/__generated__/GetHomeVideo.ts
+++ apps/watch/__generated__/GetHomeVideo.ts
@@ -9,6 +9,11 @@ import { VideoLabel } from "./globalTypes";
 // GraphQL query operation: GetHomeVideo
 // ====================================================
 
+export interface GetHomeVideo_video_title {
+  __typename: "Translation";
+  value: string;
+}
+
 export interface GetHomeVideo_video_imageAlt {
   __typename: "Translation";
   value: string;
@@ -19,30 +24,9 @@ export interface GetHomeVideo_video_snippet {
   value: string;
 }
 
-export interface GetHomeVideo_video_description {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_studyQuestions {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_title {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_variant_language_name {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_variant_language {
-  __typename: "Language";
+export interface GetHomeVideo_video_children {
+  __typename: "Video";
   id: string;
-  name: GetHomeVideo_video_variant_language_name[];
 }
 
 export interface GetHomeVideo_video_variant {
@@ -50,76 +34,26 @@ export interface GetHomeVideo_video_variant {
   id: string;
   duration: number;
   hls: string | null;
-  language: GetHomeVideo_video_variant_language;
   /**
    * slug is a permanent link to the video variant.
    */
   slug: string;
-}
-
-export interface GetHomeVideo_video_children_title {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_children_imageAlt {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_children_snippet {
-  __typename: "Translation";
-  value: string;
-}
-
-export interface GetHomeVideo_video_children_children {
-  __typename: "Video";
-  id: string;
-}
-
-export interface GetHomeVideo_video_children_variant {
-  __typename: "VideoVariant";
-  id: string;
-  duration: number;
-  hls: string | null;
-  /**
-   * slug is a permanent link to the video variant.
-   */
-  slug: string;
-}
-
-export interface GetHomeVideo_video_children {
-  __typename: "Video";
-  id: string;
-  label: VideoLabel;
-  title: GetHomeVideo_video_children_title[];
-  image: string | null;
-  imageAlt: GetHomeVideo_video_children_imageAlt[];
-  snippet: GetHomeVideo_video_children_snippet[];
-  /**
-   * slug is a permanent link to the video.
-   */
-  slug: string;
-  children: GetHomeVideo_video_children_children[];
-  variant: GetHomeVideo_video_children_variant | null;
 }
 
 export interface GetHomeVideo_video {
   __typename: "Video";
   id: string;
   label: VideoLabel;
+  title: GetHomeVideo_video_title[];
   image: string | null;
   imageAlt: GetHomeVideo_video_imageAlt[];
   snippet: GetHomeVideo_video_snippet[];
-  description: GetHomeVideo_video_description[];
-  studyQuestions: GetHomeVideo_video_studyQuestions[];
-  title: GetHomeVideo_video_title[];
-  variant: GetHomeVideo_video_variant | null;
   /**
    * slug is a permanent link to the video.
    */
   slug: string;
   children: GetHomeVideo_video_children[];
+  variant: GetHomeVideo_video_variant | null;
 }
 
 export interface GetHomeVideo {
