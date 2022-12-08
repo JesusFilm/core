5073d654cd44f5b6c6c39325e8e0bb55ff79a22f
Mike Allison <mike@captainofbass.com>
2022-12-08 01:48:45 +0000

    feat: watch videos getStaticProps

--- apps/watch/pages/videos.tsx
+++ apps/watch/pages/videos.tsx
@@ -1,8 +1,32 @@
+import { NormalizedCacheObject } from '@apollo/client'
+import { GetStaticProps } from 'next'
 import { ReactElement } from 'react'
 import { Videos } from '../src/components/VideosPage'
+import { GET_VIDEOS, limit } from '../src/components/VideosPage/VideosPage'
+import { createApolloClient } from '../src/libs/client'
 
+interface VideosPageProps {
+  initialApolloState: NormalizedCacheObject
+}
 function VideosPage(): ReactElement {
   return <Videos />
 }
 
+export const getStaticProps: GetStaticProps<VideosPageProps> = async () => {
+  const apolloClient = createApolloClient()
+  await apolloClient.query({
+    query: GET_VIDEOS,
+    variables: {
+      where: {},
+      offset: 0,
+      limit,
+      languageId: '529'
+    }
+  })
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract()
+    }
+  }
+}
 export default VideosPage
