e0365c1ff231057dd224bc9b3f23eee3e259942d
Edmond Shen <72168666+edmonday@users.noreply.github.com>
2022-12-10 03:32:56 +1300

    test: add test video content (#1140)

--- apps/watch/src/components/VideoContentPage/VideoContent/VideoContent.stories.tsx
+++ apps/watch/src/components/VideoContentPage/VideoContent/VideoContent.stories.tsx
@@ -1,4 +1,5 @@
 import { Meta, Story } from '@storybook/react'
+import { userEvent, screen } from '@storybook/testing-library'
 import { VideoProvider } from '../../../libs/videoContext'
 import { watchConfig } from '../../../libs/storybook'
 import { videos } from '../../Videos/testData'
@@ -10,12 +11,23 @@ const VideoContentStory = {
   title: 'Watch/VideoContentPage/VideoContent'
 }
 
-const Template: Story = () => (
-  <VideoProvider value={{ content: videos[0] }}>
+const Template: Story = (args) => (
+  <VideoProvider value={{ content: args.video }}>
     <VideoContent />
   </VideoProvider>
 )
 
 export const Default = Template.bind({})
+Default.args = {
+  video: { ...videos[0], studyQuestions: [] }
+}
+
+export const Discussion = Template.bind({})
+Discussion.args = {
+  video: { ...videos[0] }
+}
+Discussion.play = () => {
+  userEvent.click(screen.getByTestId('discussion'))
+}
 
 export default VideoContentStory as Meta
