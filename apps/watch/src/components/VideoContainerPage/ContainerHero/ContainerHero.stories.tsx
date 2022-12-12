e409c24035f9346fd168fdae37a280d40fc91735
Dan <danchae03@gmail.com>
2022-12-12 20:23:12 +0000

    refactor: provide context for containerHero storybook

--- apps/watch/src/components/VideoContainerPage/ContainerHero/ContainerHero.stories.tsx
+++ apps/watch/src/components/VideoContainerPage/ContainerHero/ContainerHero.stories.tsx
@@ -1,8 +1,8 @@
 import { ComponentStory, Meta } from '@storybook/react'
 import { noop } from 'lodash'
 import { watchConfig } from '../../../libs/storybook'
-import { VideoContentFields } from '../../../../__generated__/VideoContentFields'
-import { VideoLabel } from '../../../../__generated__/globalTypes'
+import { VideoProvider } from '../../../libs/videoContext'
+import { videos } from '../../Videos/testData'
 import { ContainerHero } from '.'
 
 const ContainerHeroStory = {
@@ -12,27 +12,14 @@ const ContainerHeroStory = {
 }
 
 const Template: ComponentStory<typeof ContainerHero> = ({ ...args }) => (
-  <ContainerHero {...args} />
+  <VideoProvider value={{ content: videos[1] }}>
+    <ContainerHero openDialog={noop} />
+  </VideoProvider>
 )
 
-const defaultVideo = {
-  label: VideoLabel.collection,
-  title: [{ value: 'Collection video title' }],
-  children: [1, 2, 3],
-  image:
-    'https://images.unsplash.com/photo-1669569713869-ff4a427a38ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3774&q=80'
-} as unknown as VideoContentFields
-
-const seriesVideo = {
-  ...defaultVideo,
-  label: VideoLabel.series,
-  title: [{ value: 'Series video title' }]
-}
-
 export const Default = Template.bind({})
-Default.args = { video: defaultVideo, openDialog: noop }
-
-export const Series = Template.bind({})
-Series.args = { video: seriesVideo, openDialog: noop }
+Default.args = {
+  content: videos[1]
+}
 
 export default ContainerHeroStory as Meta
