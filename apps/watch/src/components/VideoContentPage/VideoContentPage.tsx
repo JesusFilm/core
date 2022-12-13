aff9e4afbf93954ded05c955af9f93c7114ac383
Siyang <csiyang@yahoo.co.nz>
2022-12-13 04:09:28 +0000

    chore: remove redundant comment

--- apps/watch/src/components/VideoContentPage/VideoContentPage.tsx
+++ apps/watch/src/components/VideoContentPage/VideoContentPage.tsx
@@ -22,8 +22,6 @@ export function VideoContentPage(): ReactElement {
   const { container, children, slug, variant } = useVideo()
   const [openShare, setOpenShare] = useState(false)
 
-  children.forEach((child) => console.log(child.label))
-
   return (
     <PageWrapper hero={<VideoHero />}>
       <>
