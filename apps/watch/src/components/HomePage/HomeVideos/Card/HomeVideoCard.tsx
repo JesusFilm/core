fda53eb610f0d239d142fa6396c19bef3492c735
Tataihono Nikora <tataihono.nikora@gmail.com>
2022-12-14 16:26:10 +1300

    fix: home grid to match designs (#1150)

--- apps/watch/src/components/HomePage/HomeVideos/Card/HomeVideoCard.tsx
+++ apps/watch/src/components/HomePage/HomeVideos/Card/HomeVideoCard.tsx
@@ -1,41 +1,23 @@
 import { ReactElement } from 'react'
 import Typography from '@mui/material/Typography'
-import PlayArrow from '@mui/icons-material/PlayArrow'
+import PlayArrow from '@mui/icons-material/PlayArrowRounded'
 import { secondsToTimeFormat } from '@core/shared/ui/timeFormat'
 import Link from 'next/link'
 import Stack from '@mui/material/Stack'
 import { styled } from '@mui/material/styles'
 import ButtonBase from '@mui/material/ButtonBase'
+import NextImage from 'next/image'
+import Box from '@mui/material/Box'
+import { VideoLabel } from '../../../../../__generated__/globalTypes'
 import { VideoChildFields } from '../../../../../__generated__/VideoChildFields'
-
-export enum FilmType {
-  collection = 'Collection',
-  episode = 'Episode',
-  featureFilm = 'Feature Film',
-  segment = 'Segment',
-  series = 'Series',
-  shortFilm = 'Short Film'
-}
-
-const designationColors = {
-  collection: '#FF9E00',
-  episode: '#7283BE',
-  segment: '#7283BE',
-  featureFilm: '#FF9E00',
-  series: '#3AA74A',
-  shortFilm: '#FF9E00'
-}
 
 interface VideoListCardProps {
   video?: VideoChildFields
 }
 
 const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  borderRadius: 8,
   width: '100%',
-  minWidth: 266,
-  maxWidth: 338,
-  minHeight: 136,
-  maxHeight: 160,
   position: 'relative',
   '&:hover, &.Mui-focusVisible': {
     zIndex: 1,
@@ -48,61 +30,92 @@ const ImageButton = styled(ButtonBase)(({ theme }) => ({
   }
 }))
 
-const ImageSpan = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  objectFit: 'cover',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white
-}))
-
-const ImageSrc = styled('span')({
+const Layer = styled(Box)({
   position: 'absolute',
   left: 0,
   right: 0,
   top: 0,
   bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
-  borderRadius: '8px'
+  borderRadius: 8,
+  overflow: 'hidden'
 })
 
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
-  borderRadius: '8px',
-  border: '1px solid rgba(255, 255, 255, 0.18)'
-}))
-
 export function HomeVideoCard({ video }: VideoListCardProps): ReactElement {
+  let label = 'Item'
+  let color = '#FFF'
+  let childLabel = 'items'
+
+  switch (video?.label) {
+    case VideoLabel.collection:
+      label = 'Collection'
+      color = '#FF9E00'
+      break
+    case VideoLabel.episode:
+      label = 'Episode'
+      color = '#7283BE'
+      break
+    case VideoLabel.featureFilm:
+      label = 'Feature Film'
+      color = '#FF9E00'
+      childLabel = 'chapters'
+      break
+    case VideoLabel.segment:
+      label = 'Segment'
+      color = '#7283BE'
+      break
+    case VideoLabel.series:
+      label = 'Series'
+      color = '#3AA74A'
+      childLabel = 'episodes'
+      break
+    case VideoLabel.shortFilm:
+      label = 'Short Film'
+      color = '#FF9E00'
+      break
+  }
+
   return (
     <Link href={`/${video?.variant?.slug ?? ''}`} passHref>
-      <ImageButton focusRipple>
-        <ImageSrc
-          style={{
-            backgroundImage: `url(${video?.image ?? ''})`
+      <ImageButton
+        focusRipple
+        sx={{
+          height: {
+            xs: 166,
+            md: 136,
+            xl: 146
+          }
+        }}
+      >
+        {video?.image != null && (
+          <Layer>
+            <NextImage
+              src={video?.image}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="left top"
+            />
+          </Layer>
+        )}
+        <Layer
+          sx={{
+            background:
+              'linear-gradient(180deg, rgba(0, 0, 0, 0) 37.81%, rgba(0, 0, 0, 0.8) 100%)',
+            transition: (theme) => theme.transitions.create('opacity'),
+            boxShadow: 'inset 0px 0px 0px 1px rgba(255, 255, 255, 0.12)'
           }}
+          className="MuiImageBackdrop-root"
         />
-        <ImageBackdrop className="MuiImageBackdrop-root" />
-        <ImageSpan>
+        <Layer
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            p: 4
+          }}
+        >
           <Typography
             variant="h6"
             sx={{
-              position: 'absolute',
-              left: 14,
-              bottom: 42,
-              padding: 0,
               textShadow:
                 '0px 4px 4px rgba(0, 0, 0, 0.25), 0px 2px 3px rgba(0, 0, 0, 0.45)'
             }}
@@ -113,46 +126,50 @@ export function HomeVideoCard({ video }: VideoListCardProps): ReactElement {
             direction="row"
             justifyContent="space-between"
             alignItems="center"
-            p={0}
-            position="absolute"
-            left={14}
-            bottom={12}
-            right={14}
+            sx={{ minWidth: 0 }}
           >
             <Typography
               variant="overline2"
-              color={designationColors[video?.label ?? ''] ?? ''}
-              p={0}
+              color={color}
+              sx={{
+                textOverflow: 'ellipsis',
+                whiteSpace: 'nowrap',
+                overflow: 'hidden'
+              }}
             >
-              {FilmType[video?.label ?? '']}
+              {label}
             </Typography>
             <Stack
               direction="row"
               alignItems="center"
-              px="4px"
-              py="8px"
-              gap="2px"
-              borderRadius="8px"
-              height={29}
-              color="primary.contrastText"
-              bgcolor="rgba(0, 0, 0, 0.5)"
+              spacing={1}
+              sx={{
+                p: 2,
+                borderRadius: 2,
+                height: 29,
+                color: 'primary.contrastText',
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                flexShrink: 0
+              }}
             >
               {video?.children.length === 0 && (
-                <Stack direction="row">
+                <>
                   <PlayArrow sx={{ fontSize: '1rem' }} />
                   <Typography variant="body1" sx={{ lineHeight: '16px' }}>
-                    {secondsToTimeFormat(video?.variant?.duration ?? 0)}
+                    {secondsToTimeFormat(video?.variant?.duration ?? 0, {
+                      trimZeroes: true
+                    })}
                   </Typography>
-                </Stack>
+                </>
               )}
               {(video?.children.length ?? 0) > 0 && (
                 <Typography variant="body1">
-                  {video?.children.length} episodes
+                  {video?.children.length} {childLabel}
                 </Typography>
               )}
             </Stack>
           </Stack>
-        </ImageSpan>
+        </Layer>
       </ImageButton>
     </Link>
   )
