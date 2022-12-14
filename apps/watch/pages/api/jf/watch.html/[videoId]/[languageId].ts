842572a9bb1f482a47027a140e2c649e603a28d7
jianwei1 <jianweichong08@gmail.com>
2022-12-14 23:38:35 +0000

    chore: fixed query

--- apps/watch/pages/api/jf/watch.html/[videoId]/[languageId].ts
+++ apps/watch/pages/api/jf/watch.html/[videoId]/[languageId].ts
@@ -1,32 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { gql } from '@apollo/client'
 import { createApolloClient } from '../../../../../src/libs/client'
-import {
-  GetVideo,
-  GetVideo_video as Video
-} from '../../../../../__generated__/GetVideo'
+import { GetVideo } from '../../../../../__generated__/GetVideo'
 
 export const GET_VIDEO = gql`
-  # TODO: Use this code when Siyang's PR gets merged in
-  # query Video($videoId: ID!, $languageId: ID) {
-  #   video(id: $videoId, idType: $idType) {
-  #     id
-  #     variant(languageId: $languageId) {
-  #       id
-  #       slug {
-  #         value
-  #       }
-  #     }
-  #   }
-  # }
-  query Video($videoId: ID!, $languageId: ID) {
+  query GetVideo($videoId: ID!, $languageId: ID) {
     video(id: $videoId) {
       id
       variant(languageId: $languageId) {
         id
-      }
-      slug {
-        value
+        slug
       }
     }
   }
@@ -46,19 +29,10 @@ export default async function Handler(
       languageId: (languageId as string).replace(/\.html?/, '')
     }
   })
-
   // if video then redirect, else return 404
-  if (data.video != null) {
-    res.redirect(302, `/${data.video.slug[0].value}`)
+  if (data.video?.variant?.slug != null) {
+    res.redirect(302, `/${data.video.variant.slug}`)
   } else {
     res.status(404).json({ success: false })
   }
-  // TODO: use this logic below once Siyangs PR is merged in
-  // if (data.video?.variant?.slug) {
-  //   res.redirect(302, `/${data.video.variant.slug}`)
-  // } else {
-  //   res.status(404).json({ success: false })
-  // }
-  // const newSlug = data.video.slug[0].value
-  // res.status(200).json({ videoId, languageId, newSlug })
 }
