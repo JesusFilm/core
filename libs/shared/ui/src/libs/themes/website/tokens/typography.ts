33f0213d350fc5c7af7b61d1c3ff2cd566fa7e79
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2022-12-22 15:36:38 +1300

    fix: responsive typography (#1098)
    Co-authored-by: Dan <danchae03@gmail.com>
    Co-authored-by: DanChae03 <118027673+DanChae03@users.noreply.github.com>

--- libs/shared/ui/src/libs/themes/website/tokens/typography.ts
+++ libs/shared/ui/src/libs/themes/website/tokens/typography.ts
@@ -1,4 +1,5 @@
 import { ThemeOptions } from '@mui/material/styles'
+import { baseBreakpoints } from '../../base/tokens/breakpoints'
 
 // https://mui.com/material-ui/customization/typography/#adding-amp-disabling-variants
 declare module '@mui/material/styles' {
@@ -35,62 +36,98 @@ export const typography = {
   fontFamily: ['"Apercu", "Arial", sans-serif'].join(','),
   h1: {
     fontWeight: 700,
-    fontSize: 80,
-    lineHeight: pxToRem(86, 80),
-    letterSpacing: -1
+    fontSize: 36,
+    lineHeight: pxToRem(40, 36),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      fontSize: 80,
+      lineHeight: pxToRem(86, 80),
+      letterSpacing: -3
+    }
   },
+
   h2: {
     fontWeight: 700,
-    fontSize: 64,
-    lineHeight: pxToRem(67, 64),
-    letterSpacing: -1
+    fontSize: 27,
+    lineHeight: pxToRem(33, 36),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(67, 64),
+      fontSize: 64,
+      letterSpacing: -1
+    }
   },
   h3: {
     fontWeight: 700,
-    fontSize: 48,
-    lineHeight: pxToRem(48, 48)
+    fontSize: 27,
+    lineHeight: pxToRem(33, 36),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(48, 48),
+      fontSize: 48
+    }
   },
   h4: {
     fontWeight: 700,
-    fontSize: 36,
-    lineHeight: pxToRem(40, 36)
+    fontSize: 21,
+    lineHeight: pxToRem(29, 21),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(40, 36),
+      fontSize: 36
+    }
   },
   h5: {
     fontWeight: 700,
-    fontSize: 27,
-    lineHeight: pxToRem(33, 27)
+    fontSize: 18,
+    lineHeight: pxToRem(21, 18),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(33, 27),
+      fontSize: 33
+    }
   },
+  // TODO: H6 on Figma?
   h6: {
     fontWeight: 700,
     fontSize: 21,
     lineHeight: pxToRem(27, 21)
   },
   subtitle1: {
-    fontWeight: 400,
-    fontSize: 20,
-    lineHeight: pxToRem(27, 20)
+    fontWeight: 500,
+    fontSize: 16,
+    lineHeight: pxToRem(21, 16),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(27, 20),
+      fontSize: 20
+    }
   },
   subtitle2: {
-    fontWeight: 700,
-    fontSize: 18,
-    lineHeight: pxToRem(21, 18)
+    fontWeight: 500,
+    fontSize: 14,
+    lineHeight: pxToRem(20, 14),
+    [baseBreakpoints.breakpoints.up('md')]: {
+      fontWeight: 700,
+      lineHeight: pxToRem(21, 18),
+      fontSize: 18
+    }
   },
   body1: {
     fontSize: 16,
-    fontWeight: 400,
+    fontWeight: 500,
     lineHeight: pxToRem(21, 16)
   },
   body2: {
     fontSize: 14,
-    fontWeight: 400,
+    fontWeight: 500,
     lineHeight: pxToRem(20, 14)
   },
+  // TODO: use same mobile overline styles for both
   overline1: {
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: 700,
-    lineHeight: pxToRem(28, 18),
+    lineHeight: pxToRem(14, 14),
     letterSpacing: '0.12em',
-    textTransform: 'uppercase'
+    textTransform: 'uppercase',
+    [baseBreakpoints.breakpoints.up('md')]: {
+      lineHeight: pxToRem(28, 18),
+      fontSize: 18
+    }
   },
   overline2: {
     fontSize: 14,
