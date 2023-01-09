e3f9dc88988c71efa247bed1d2b5c005793a8a0f
John Geronimo <johnageronimo1@gmail.com>
2023-01-09 21:12:21 +0000

    fix: storybook issue

--- apps/watch/src/components/VideoContentPage/VideoHero/VideoHero.stories.tsx
+++ apps/watch/src/components/VideoContentPage/VideoHero/VideoHero.stories.tsx
@@ -29,7 +29,7 @@ export const Default = Template.bind({})
 
 export const VideoPlayer = Template.bind({})
 VideoPlayer.play = async () => {
-  const CustomPlayButton = screen.getByRole('button', { name: 'Play' })
+  const CustomPlayButton = screen.getAllByRole('button')[1]
   userEvent.click(CustomPlayButton)
 }
 
