bd68a1b1d34099e700554d2aacbb27839b703e01
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-01-10 12:12:04 +0000

    fix: remove user 0

--- infrastructure/modules/aws/ecs-task/main.tf
+++ infrastructure/modules/aws/ecs-task/main.tf
@@ -89,7 +89,6 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       environment = []
       mountPoints = []
       volumesFrom = []
-      user        = "0"
     },
     module.ecs_datadog_agent.container_definition
   ])
