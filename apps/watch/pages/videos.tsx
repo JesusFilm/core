c52d1aff9bb7245d9855ceeb5fdde6bf3c4ad17e
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2023-01-12 12:50:21 +1300

    fix: refactor to use one apollo provider (#1232)

--- apps/watch/pages/videos.tsx
+++ apps/watch/pages/videos.tsx
@@ -1,4 +1,4 @@
-import { ApolloProvider, NormalizedCacheObject } from '@apollo/client'
+import { NormalizedCacheObject } from '@apollo/client'
 import { GetStaticProps } from 'next'
 import { ReactElement } from 'react'
 import { Videos } from '../src/components/VideosPage'
@@ -7,19 +7,13 @@ import {
   GET_VIDEOS,
   limit
 } from '../src/components/VideosPage/VideosPage'
-import { createApolloClient, useApolloClient } from '../src/libs/apolloClient'
+import { createApolloClient } from '../src/libs/apolloClient'
 
 interface VideosPageProps {
   initialApolloState: NormalizedCacheObject
 }
-function VideosPage({ initialApolloState }: VideosPageProps): ReactElement {
-  const client = useApolloClient({ initialState: initialApolloState })
-
-  return (
-    <ApolloProvider client={client}>
-      <Videos />
-    </ApolloProvider>
-  )
+function VideosPage(): ReactElement {
+  return <Videos />
 }
 
 export const getStaticProps: GetStaticProps<VideosPageProps> = async () => {
