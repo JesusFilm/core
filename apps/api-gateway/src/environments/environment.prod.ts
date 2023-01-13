eaa519d6739fd724b784eef70157124f310a7ec3
Mike Allison <mike@captainofbass.com>
2023-01-12 20:08:21 -0800

    feat: managed federation (#1217)

--- apps/api-gateway/src/environments/environment.prod.ts
+++ apps/api-gateway/src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
-import { readFileSync } from 'fs'
 import { EnvironmentConfig } from './types'
 
 export const config: EnvironmentConfig = {
   production: true,
-  gatewayConfig: {
-    supergraphSdl: readFileSync('./schema.graphql').toString()
-  },
+  gatewayConfig: {},
   listenOptions: {
     port: 4000,
     host: '0.0.0.0'
