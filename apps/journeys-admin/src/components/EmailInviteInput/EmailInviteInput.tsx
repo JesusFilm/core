aca74eec46e0ef8238bf77fb58b7e9adf9d34311
Nicholas Yao <nicholasyaoyao@gmail.com>
2023-01-25 00:13:37 +0000

    feat: create form with 2 fields

---
+++ apps/journeys-admin/src/components/EmailInviteInput/EmailInviteInput.tsx
@@ -0,0 +1,42 @@
+import { ReactElement } from 'react'
+import { Dialog } from '@core/shared/ui/Dialog'
+import TextField from '@mui/material/TextField'
+import List from '@mui/material/List'
+
+interface EmailInviteInputProps {
+  open?: boolean
+  onClose: () => void
+}
+
+export function EmailInviteInput({
+  open,
+  onClose
+}: EmailInviteInputProps): ReactElement {
+  return (
+    <Dialog
+      open={open ?? false}
+      onClose={onClose}
+      dialogTitle={{ title: 'Add User With Email', closeButton: true }}
+    >
+      <List>
+        <TextField
+          autoFocus
+          margin="normal"
+          id="email"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          autoFocus
+          margin="normal"
+          id="name"
+          label="Display Name"
+          fullWidth
+          variant="outlined"
+        />
+      </List>
+    </Dialog>
+  )
+}
