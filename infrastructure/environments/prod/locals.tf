c736df05ed0a86760e304ee3c1a4bc663241e610
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-01-25 03:19:10 +0000

    fix: allow health check from external consumers

--- infrastructure/environments/prod/locals.tf
+++ infrastructure/environments/prod/locals.tf
@@ -4,6 +4,7 @@ locals {
     protocol          = "HTTP"
     path_pattern      = ["/*"]
     health_check_path = "/.well-known/apollo/server-health"
+    health_check_port = 80
     priority          = 1
   }
 }
