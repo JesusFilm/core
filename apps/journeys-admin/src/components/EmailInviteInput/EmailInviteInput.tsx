a8d2582dfd1a1a82abbf6584126e6fcda48d140b
Nicholas Yao <nicholasyaoyao@gmail.com>
2023-01-25 02:04:20 +0000

    feat: start to implement handleAddUser

--- apps/journeys-admin/src/components/EmailInviteInput/EmailInviteInput.tsx
+++ apps/journeys-admin/src/components/EmailInviteInput/EmailInviteInput.tsx
@@ -1,7 +1,18 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { Dialog } from '@core/shared/ui/Dialog'
 import TextField from '@mui/material/TextField'
 import List from '@mui/material/List'
+import Button from '@mui/material/Button'
+// import { gql, useQuery } from '@apollo/client'
+// import { GetUser } from '../../../__generated__/GetUser'
+
+// export const GET_USER = gql`
+//   query GetUser {
+//     me {
+//       id
+//     }
+//   }
+// `
 
 interface EmailInviteInputProps {
   open?: boolean
@@ -12,6 +23,10 @@ export function EmailInviteInput({
   open,
   onClose
 }: EmailInviteInputProps): ReactElement {
+  // const { data: id } = useQuery<GetUser>(GET_USER)
+  // console.log(id)
+  const handleAddUser = (): void => {}
+  const [email, setEmail] = useState('')
   return (
     <Dialog
       open={open ?? false}
@@ -27,6 +42,8 @@ export function EmailInviteInput({
           type="email"
           fullWidth
           variant="outlined"
+          value={email}
+          onChange={(newEmail) => setEmail(newEmail.target.value)}
         />
         <TextField
           margin="normal"
@@ -35,6 +52,7 @@ export function EmailInviteInput({
           fullWidth
           variant="outlined"
         />
+        <Button onClick={handleAddUser}>Submit</Button>
       </List>
     </Dialog>
   )
