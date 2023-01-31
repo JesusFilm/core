ebfffe419e35082e0d61b5042f11fbe59741bb9f
Nicholas Yao <nicholasyaoyao@gmail.com>
2023-01-31 20:05:29 +0000

    test: add test case

--- apps/api-users/src/app/modules/user/user.resolver.spec.ts
+++ apps/api-users/src/app/modules/user/user.resolver.spec.ts
@@ -5,6 +5,7 @@ import { UserService } from './user.service'
 
 describe('UserResolver', () => {
   let resolver: UserResolver
+  let service: UserService
 
   const user = {
     id: '1',
@@ -14,6 +15,10 @@ describe('UserResolver', () => {
     imageUrl: 'po'
   }
 
+  const userUpdate = {
+    acceptedTermsAt: '1970-01-01T00:00:00Z'
+  }
+
   beforeEach(async () => {
     const userService = {
       provide: UserService,
@@ -21,13 +26,15 @@ describe('UserResolver', () => {
         get: jest.fn(() => user),
         getByUserId: jest.fn(() => user),
         getAll: jest.fn(() => [user, user]),
-        save: jest.fn((input) => input)
+        save: jest.fn((input) => input),
+        update: jest.fn(() => user)
       })
     }
     const module: TestingModule = await Test.createTestingModule({
       providers: [UserResolver, userService]
     }).compile()
     resolver = module.get<UserResolver>(UserResolver)
+    service = module.get<UserService>(UserService)
   })
 
   describe('me', () => {
@@ -41,6 +48,13 @@ describe('UserResolver', () => {
       expect(
         await resolver.resolveReference({ __typename: 'User', id: user.id })
       ).toEqual(user)
+    })
+  })
+
+  describe('userUpdate', () => {
+    it('updates a User', async () => {
+      await resolver.userUpdate('1', userUpdate)
+      expect(service.update).toHaveBeenCalledWith('1', userUpdate)
     })
   })
 })
