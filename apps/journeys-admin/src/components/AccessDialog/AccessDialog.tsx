2e9ea841cd4f5c3ca1ad47860aa0c0846c93c9b7
Nicholas Yao <nicholasyaoyao@gmail.com>
2023-02-01 00:40:52 +0000

    Merge branch 'main' into 23-01-NY-feat-add-user-email-form

--- apps/journeys-admin/src/components/AccessDialog/AccessDialog.tsx
+++ apps/journeys-admin/src/components/AccessDialog/AccessDialog.tsx
@@ -1,20 +1,11 @@
 import { ReactElement, MouseEvent, useEffect, useState } from 'react'
 import Button from '@mui/material/Button'
-import Avatar from '@mui/material/Avatar'
-import Divider from '@mui/material/Divider'
 import Menu from '@mui/material/Menu'
-import List from '@mui/material/List'
-import MuiListItem from '@mui/material/ListItem'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import ListItemText from '@mui/material/ListItemText'
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import DraftsIcon from '@mui/icons-material/Drafts'
 import LinkIcon from '@mui/icons-material/Link'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import GroupAddIcon from '@mui/icons-material/GroupAdd'
 import { gql, useLazyQuery, useQuery } from '@apollo/client'
-import { compact } from 'lodash'
-import Skeleton from '@mui/material/Skeleton'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { Theme } from '@mui/material/styles'
 import { CopyTextField } from '@core/shared/ui/CopyTextField'
@@ -22,6 +13,7 @@ import { Dialog } from '@core/shared/ui/Dialog'
 import MenuItem from '@mui/material/MenuItem'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import {
   GetJourneyWithUserJourneys,
   GetJourneyWithUserJourneys_journey_userJourneys as UserJourney
@@ -29,9 +21,7 @@ import {
 import { UserJourneyRole } from '../../../__generated__/globalTypes'
 import { GetCurrentUser } from '../../../__generated__/GetCurrentUser'
 import { EmailInviteInput } from '../EmailInviteInput'
-import { RemoveUser } from './RemoveUser'
-import { ApproveUser } from './ApproveUser'
-import { PromoteUser } from './PromoteUser'
+import { UserJourneyList } from './UserJourneyList'
 
 export const GET_JOURNEY_WITH_USER_JOURNEYS = gql`
   query GetJourneyWithUserJourneys($id: ID!) {
@@ -97,6 +87,17 @@ export function AccessDialog({
       (userJourney) => userJourney.user?.email === currentUserData?.me?.email
     )?.role !== UserJourneyRole.owner
 
+  const usersList: UserJourney[] = []
+  const requestsList: UserJourney[] = []
+
+  data?.journey?.userJourneys?.forEach((userJourney) => {
+    if (userJourney.role === UserJourneyRole.inviteRequested) {
+      requestsList.push(userJourney)
+    } else {
+      usersList.push(userJourney)
+    }
+  })
+
   useEffect(() => {
     if (open === true) {
       void loadJourney()
@@ -104,239 +105,100 @@ export function AccessDialog({
   }, [open, loadJourney])
 
   return (
-    <>
-      <Dialog
-        open={open ?? false}
-        onClose={onClose}
-        dialogTitle={{
-          title: 'Invite Other Editors',
-          closeButton: true
-        }}
-        divider
-        fullscreen={!smUp}
-      >
-        <List sx={{ pt: 0 }}>
-          {!loading && (
-            <UserJourneyList
-              title="Requested Editing Rights"
-              userJourneys={data?.journey?.userJourneys?.filter(
-                ({ role }) => role === UserJourneyRole.inviteRequested
-              )}
-              disable={disable}
-            />
-          )}
-          <UserJourneyList
-            title="Users With Access"
-            loading={loading}
-            userJourneys={data?.journey?.userJourneys?.filter(
-              ({ role }) => role !== UserJourneyRole.inviteRequested
-            )}
-            disable={disable}
-          />
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            sx={{
-              mb: '16px',
-              mt: '20px'
-            }}
-          >
-            <GroupAddIcon />
-            <Typography
-              sx={{
-                fontFamily: 'Montserrat',
-                fontWeight: '600',
-                fontSize: '18px',
-                marginLeft: '12px'
-              }}
-            >
-              Add new using
-            </Typography>
-            <Button
-              variant="outlined"
-              size="small"
-              startIcon={
-                selectedInviteMethod === 'Link' ? <LinkIcon /> : <DraftsIcon />
-              }
-              endIcon={<KeyboardArrowDownIcon />}
-              sx={{
-                borderRadius: '16px',
-                width: '124px',
-                height: '32px',
-                color: '#26262E',
-                border: '1px solid #DEDFE0',
-                fontWeight: '400',
-                fontFamily: 'Open Sans',
-                fontSize: '16px',
-                padding: '4px',
-                marginLeft: '12px',
-                '&:hover': {
-                  borderColor: 'gray'
-                }
-              }}
-              onClick={handleClick}
-            >
-              {selectedInviteMethod}
-            </Button>
-          </Box>
-
-          <Menu anchorEl={anchorEl} open={menuOpen}>
-            <MenuItem onClick={handleMenuItemClick}>Link</MenuItem>
-            <MenuItem onClick={handleMenuItemClick}>Email</MenuItem>
-          </Menu>
-          <MuiListItem sx={{ p: 0 }}>
-            {selectedInviteMethod === 'Link' ? (
-              <CopyTextField
-                value={
-                  typeof window !== 'undefined'
-                    ? `${
-                        window.location.host.endsWith('.chromatic.com')
-                          ? 'https://admin.nextstep.is'
-                          : window.location.origin
-                      }/journeys/${journeyId}`
-                    : undefined
-                }
-                messageText="Editor invite link copied"
-                helperText="Anyone with this link can see journey and ask for editing rights.
-              You can accept or reject every request."
-              />
-            ) : (
-              <EmailInviteInput />
-            )}
-          </MuiListItem>
-        </List>
-      </Dialog>
-    </>
-  )
-}
-
-interface UserJourneyListProps {
-  title: string
-  loading?: boolean
-  userJourneys?: UserJourney[] | null
-  disable: boolean
-}
-
-function UserJourneyList({
-  title,
-  loading,
-  userJourneys,
-  disable
-}: UserJourneyListProps): ReactElement {
-  return (
-    <>
-      {((userJourneys?.length != null && userJourneys.length > 0) ||
-        loading === true) && (
-        <>
-          <MuiListItem sx={{ px: 0 }}>{title}</MuiListItem>
-        </>
-      )}
-      {loading === true &&
-        [0, 1, 2].map((i) => (
-          <MuiListItem sx={{ px: 0 }} key={i}>
-            <ListItemAvatar>
-              <Skeleton variant="circular" width={40} height={40} />
-            </ListItemAvatar>
-            <ListItemText
-              primary={<Skeleton variant="text" width="60%" />}
-              secondary={<Skeleton variant="text" width="30%" />}
-            />
-          </MuiListItem>
-        ))}
-      {userJourneys?.map((userJourney) => (
-        <ListItem
-          key={userJourney.id}
-          userJourney={userJourney}
-          disabled={disable}
+    <Dialog
+      open={open ?? false}
+      onClose={onClose}
+      dialogTitle={{
+        title: 'Invite Other Editors',
+        closeButton: true
+      }}
+      divider
+      fullscreen={!smUp}
+    >
+      <Stack spacing={4}>
+        <UserJourneyList
+          title="Requested Editing Rights"
+          userJourneys={requestsList}
+          disable={disable}
         />
-      ))}
-      <Divider sx={{ my: 2 }} />
-    </>
-  )
-}
 
-interface ListItemProps {
-  userJourney: UserJourney
-  disabled: boolean
-}
-
-function ListItem({ userJourney, disabled }: ListItemProps): ReactElement {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
-  const open = Boolean(anchorEl)
-  const { id, role, user } = userJourney
-
-  useEffect(() => {
-    return () => {
-      setAnchorEl(null)
-    }
-  }, [])
-
-  const handleClick = (event: MouseEvent<HTMLElement>): void => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = (): void => {
-    setAnchorEl(null)
-  }
-
-  const displayName = compact([user?.firstName, user?.lastName]).join(' ')
-
-  return (
-    <>
-      <MuiListItem
-        sx={{
-          px: 0,
-          '& > .MuiListItemSecondaryAction-root': {
-            right: 0
-          }
-        }}
-        secondaryAction={
-          <Button
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-            disabled={disabled || role === 'owner'}
-            endIcon={<ArrowDropDownIcon />}
+        <UserJourneyList
+          title="Users With Access"
+          loading={loading}
+          userJourneys={usersList}
+          disable={disable}
+        />
+        <Box
+          display="flex"
+          flexDirection="row"
+          alignItems="center"
+          sx={{
+            mb: '16px',
+            mt: '20px'
+          }}
+        >
+          <GroupAddIcon />
+          <Typography
             sx={{
-              color: 'text.primary',
-              typography: 'body2'
+              fontFamily: 'Montserrat',
+              fontWeight: '600',
+              fontSize: '18px',
+              marginLeft: '12px'
             }}
           >
-            {role === 'inviteRequested' && 'Manage Access'}
-            {role === 'owner' && 'Owner'}
-            {role === 'editor' && 'Editor'}
+            Add new using
+          </Typography>
+          <Button
+            variant="outlined"
+            size="small"
+            startIcon={
+              selectedInviteMethod === 'Link' ? <LinkIcon /> : <DraftsIcon />
+            }
+            endIcon={<KeyboardArrowDownIcon />}
+            sx={{
+              borderRadius: '16px',
+              width: '124px',
+              height: '32px',
+              color: '#26262E',
+              border: '1px solid #DEDFE0',
+              fontWeight: '400',
+              fontFamily: 'Open Sans',
+              fontSize: '16px',
+              padding: '4px',
+              marginLeft: '12px',
+              '&:hover': {
+                borderColor: 'gray'
+              }
+            }}
+            onClick={handleClick}
+          >
+            {selectedInviteMethod}
           </Button>
-        }
-      >
-        <ListItemAvatar>
-          <Avatar src={user?.imageUrl ?? undefined} alt={displayName}>
-            {displayName.charAt(0)?.toUpperCase()}
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary={displayName} secondary={user?.email} />
-      </MuiListItem>
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right'
-        }}
-      >
-        {role === 'inviteRequested' ? (
-          <ApproveUser id={id} onClick={handleClose} />
+        </Box>
+
+        <Menu anchorEl={anchorEl} open={menuOpen}>
+          <MenuItem onClick={handleMenuItemClick}>Link</MenuItem>
+          <MenuItem onClick={handleMenuItemClick}>Email</MenuItem>
+        </Menu>
+        {selectedInviteMethod === 'Link' ? (
+          <CopyTextField
+            value={
+              typeof window !== 'undefined'
+                ? `${
+                    window.location.host.endsWith('.chromatic.com')
+                      ? 'https://admin.nextstep.is'
+                      : window.location.origin
+                  }/journeys/${journeyId}`
+                : undefined
+            }
+            messageText="Editor invite link copied"
+            helperText="Anyone with this link can see journey and ask for editing rights.
+          You can accept or reject every request."
+          />
         ) : (
-          <PromoteUser id={id} onClick={handleClose} />
+          <EmailInviteInput />
         )}
-        <Divider />
-        <RemoveUser id={id} onClick={handleClose} />
-      </Menu>
-    </>
+      </Stack>
+    </Dialog>
   )
 }
