e393174fa1cb5f7ede11f0c104f5a866335f6312
Mike Allison <mike@captainofbass.com>
2023-02-01 00:48:27 +0000

    feat: api-media

--- apps/api-media/src/app/modules/cloudflare/image/image.service.ts
+++ apps/api-media/src/app/modules/cloudflare/image/image.service.ts
@@ -1,6 +1,8 @@
+import { URLSearchParams } from 'url'
 import { BaseService } from '@core/nest/database/BaseService'
 import { Injectable } from '@nestjs/common'
 import { DocumentCollection } from 'arangojs/collection'
+import fetch from 'node-fetch'
 
 interface CloudflareDirectCreatorUploadResponse {
   result: {
