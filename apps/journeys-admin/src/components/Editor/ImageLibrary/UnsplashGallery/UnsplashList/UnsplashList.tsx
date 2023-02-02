6dd0c646a7076f85785f5a84b1fa124bd8c823c5
John Geronimo <johnageronimo1@gmail.com>
2023-02-02 00:35:30 +0000

    fix: dependency cycle

--- apps/journeys-admin/src/components/Editor/ImageLibrary/UnsplashGallery/UnsplashList/UnsplashList.tsx
+++ apps/journeys-admin/src/components/Editor/ImageLibrary/UnsplashGallery/UnsplashList/UnsplashList.tsx
@@ -5,7 +5,7 @@ import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import Image from 'next/image'
-import { UnsplashImage } from '../UnsplashGallery'
+import type { UnsplashImage } from '../UnsplashGallery'
 
 interface UnsplashListProps {
   results: UnsplashImage[]
