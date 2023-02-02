e5bcfdd0dd98d6b56e4bba7f06feb8fbd29e41aa
JesseErnst <jesse@onsiteanalytics.co>
2023-02-02 01:22:22 +0000

    feat: filter UI finished

--- apps/watch/src/components/FilterContainer/FilterContainer.tsx
+++ apps/watch/src/components/FilterContainer/FilterContainer.tsx
@@ -4,9 +4,13 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableRow from '@mui/material/TableRow'
-import ArrowDropDown from '@mui/icons-material/ArrowDropDown'
-import ArrowDropUp from '@mui/icons-material/ArrowDropUp'
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUp from '@mui/icons-material/KeyboardArrowUp'
+import VolumeUp from '@mui/icons-material/VolumeUp'
+import Subtitles from '@mui/icons-material/Subtitles'
+import Title from '@mui/icons-material/Title'
 import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
 
 
 
@@ -21,52 +25,58 @@ export function FilterContainer(): ReactElement {
           <TableRow
             key={1}
           >
-            <TableCell onClick={() => setOpenRow(1)}>
-              <Typography >Languages</Typography>
-              <ArrowDropDown/>
+            <TableCell align={"center"} onClick={() => setOpenRow(1)}>
+              <Stack direction="row" justifyContent="center" spacing={2}>
+                <VolumeUp/>
+                <Typography >Languages</Typography>
+                {openRow === 1 ?<KeyboardArrowUp/> : <KeyboardArrowDown/>}
+              </Stack>
             </TableCell>
           </TableRow>
           {openRow === 1 ? 
             <TableRow
               key={1.5}
             >
-              <TableCell onClick={() => setOpenRow(0)}>
-                <Typography >Languages Open</Typography>
-                <ArrowDropUp/>
+              <TableCell align={"center"} onClick={() => setOpenRow(0)}>
+                <Typography >Languages Component Here</Typography>
               </TableCell>
             </TableRow> : <></>}
           <TableRow
             key={2}
           >
-            <TableCell onClick={() => setOpenRow(2)}>
-              <Typography >Subtitles</Typography>
-              <ArrowDropDown/>
-            </TableCell>
+            <TableCell align={"center"} onClick={() => setOpenRow(2)}>
+              <Stack direction="row" justifyContent="center" spacing={2}>
+                <Subtitles/>
+                <Typography >Subtitles</Typography>
+                {openRow === 2 ?<KeyboardArrowUp/> : <KeyboardArrowDown/>}
+              </Stack>
+              </TableCell>
           </TableRow>
           {openRow === 2 ? 
             <TableRow
               key={1.5}
             >
-              <TableCell onClick={() => setOpenRow(0)}>
-                <Typography >Subtitles Open</Typography>
-                <ArrowDropUp/>
+              <TableCell align={"center"} onClick={() => setOpenRow(0)}>
+                <Typography >Subtitles Component Here</Typography>
               </TableCell>
             </TableRow> : <></>}
           <TableRow
             key={3}
           >
-            <TableCell onClick={() => setOpenRow(3)}>
-              <Typography >Title</Typography>
-              <ArrowDropDown/>
+            <TableCell align={"center"} onClick={() => setOpenRow(3)}>
+              <Stack direction="row" justifyContent="center" spacing={2}>
+                <Title/>
+                <Typography >Title</Typography>
+                {openRow === 3 ?<KeyboardArrowUp/> : <KeyboardArrowDown/>}
+              </Stack>
             </TableCell>
           </TableRow>
           {openRow === 3 ? 
             <TableRow
               key={1.5}
             >
-              <TableCell onClick={() => setOpenRow(0)}>
-                <Typography >Title Open</Typography>
-                <ArrowDropUp/>
+              <TableCell align={"center"} onClick={() => setOpenRow(0)}>
+                <Typography >Title Component Here</Typography>
               </TableCell>
             </TableRow> : <></>}
         </TableBody>
