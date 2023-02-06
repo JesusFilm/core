b01145d67d6a4fdf53b40caa069992bff76b6f43
Mike Allison <mike@captainofbass.com>
2023-02-06 15:02:42 -0800

    feat: api-media unsplash (#1303)

--- apps/api-media/src/app/app.module.ts
+++ apps/api-media/src/app/app.module.ts
@@ -8,10 +8,12 @@ import {
 import { LoggerModule } from 'nestjs-pino'
 import { DatadogTraceModule } from 'nestjs-ddtrace'
 import { CloudflareImageModule } from './modules/cloudflare/image/image.module'
+import { UnsplashImageModule } from './modules/unsplash/image/image.module'
 
 @Module({
   imports: [
     CloudflareImageModule,
+    UnsplashImageModule,
     GraphQLModule.forRoot<ApolloFederationDriverConfig>({
       driver: ApolloFederationDriver,
       typePaths: [
