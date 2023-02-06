1fba4e3430bbd5bad67fca4d4c71a539ce8cd70e
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2023-02-07 12:06:26 +1300

    feat: approve invited user on journey related pages (#1293)

--- apps/journeys-admin/pages/index.tsx
+++ apps/journeys-admin/pages/index.tsx
@@ -12,8 +12,10 @@ import { useTranslation } from 'react-i18next'
 import { useRouter } from 'next/router'
 import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
 import { GetJourneys } from '../__generated__/GetJourneys'
+import { UserInviteAcceptAll } from '../__generated__/UserInviteAcceptAll'
 import { JourneyList } from '../src/components/JourneyList'
 import { PageWrapper } from '../src/components/PageWrapper'
+import { createApolloClient } from '../src/libs/apolloClient'
 import i18nConfig from '../next-i18next.config'
 import JourneyListMenu from '../src/components/JourneyList/JourneyListMenu/JourneyListMenu'
 
@@ -48,6 +50,15 @@ export const GET_JOURNEYS = gql`
           imageUrl
         }
       }
+    }
+  }
+`
+
+export const ACCEPT_USER_INVITE = gql`
+  mutation UserInviteAcceptAll {
+    userInviteAcceptAll {
+      id
+      acceptedAt
     }
   }
 `
@@ -106,6 +117,14 @@ export const getServerSideProps = withAuthUserTokenSSR({
   const flags = (await launchDarklyClient.allFlagsState(ldUser)).toJSON() as {
     [key: string]: boolean | undefined
   }
+
+  const token = await AuthUser.getIdToken()
+  const apolloClient = createApolloClient(token != null ? token : '')
+
+  await apolloClient.mutate<UserInviteAcceptAll>({
+    mutation: ACCEPT_USER_INVITE
+  })
+
   return {
     props: {
       flags,
