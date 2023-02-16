a55c8f6e4f7a54c96024a3ead680eebab20627da
Dan <danchae03@gmail.com>
2023-02-16 21:07:10 +0000

    feat: block delete from imageblock

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Image/Options/ImageOptions.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Image/Options/ImageOptions.tsx
@@ -8,6 +8,8 @@ import { useSnackbar } from 'notistack'
 import { GetJourney_journey_blocks_ImageBlock as ImageBlock } from '../../../../../../../../__generated__/GetJourney'
 import { ImageBlockEditor } from '../../../../../ImageBlockEditor'
 import { ImageBlockUpdate } from '../../../../../../../../__generated__/ImageBlockUpdate'
+import { blockDeleteUpdate } from '../../../../../../../libs/blockDeleteUpdate/blockDeleteUpdate'
+import { ImageBlockDelete } from '../../../../../../../../__generated__/ImageBlockDelete'
 
 export const IMAGE_BLOCK_UPDATE = gql`
   mutation ImageBlockUpdate(
@@ -23,6 +25,15 @@ export const IMAGE_BLOCK_UPDATE = gql`
       height
       parentOrder
       blurhash
+    }
+  }
+`
+
+export const IMAGE_BLOCK_DELETE = gql`
+  mutation ImageBlockDelete($id: ID!, $parentBlockId: ID!, $journeyId: ID!) {
+    blockDelete(id: $id, parentBlockId: $parentBlockId, journeyId: $journeyId) {
+      id
+      parentOrder
     }
   }
 `
@@ -43,6 +54,38 @@ export function ImageOptions({
     useMutation<ImageBlockUpdate>(IMAGE_BLOCK_UPDATE)
 
   const imageBlock = selectedBlock as TreeBlock<ImageBlock>
+
+  const [blockDelete] = useMutation<ImageBlockDelete>(IMAGE_BLOCK_DELETE)
+
+  const handleImageDelete = async (): Promise<void> => {
+    try {
+      await deleteCoverBlock()
+      enqueueSnackbar('Image Deleted', {
+        variant: 'success',
+        preventDuplicate: true
+      })
+    } catch (e) {
+      enqueueSnackbar(e.message, {
+        variant: 'error',
+        preventDuplicate: true
+      })
+    }
+  }
+
+  const deleteCoverBlock = async (): Promise<void> => {
+    if (journey == null) return
+
+    await blockDelete({
+      variables: {
+        id: imageBlock.id,
+        parentBlockId: imageBlock.parentBlockId,
+        journeyId: journey.id
+      },
+      update(cache, { data }) {
+        blockDeleteUpdate(imageBlock, data?.blockDelete, cache, journey.id)
+      }
+    })
+  }
 
   const updateImageBlock = async (block: ImageBlock): Promise<void> => {
     if (journey == null) return
@@ -75,6 +118,7 @@ export function ImageOptions({
       <ImageBlockEditor
         selectedBlock={imageBlock}
         onChange={updateImageBlock}
+        onDelete={handleImageDelete}
         loading={loading}
         noSource={noSource}
       />
