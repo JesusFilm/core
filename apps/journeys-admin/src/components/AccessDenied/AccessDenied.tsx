9136cb73bac4d1bfbeb43f856a07779f2520453c
Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
2023-02-20 10:47:02 +1300

    feat: access denied redesign (#1335)
    Co-authored-by: Ziwei Liu <10802634+storyworks@users.noreply.github.com>
    Co-authored-by: Ziwei Liu <ziwei.liu.23@gmail.com>

--- apps/journeys-admin/src/components/AccessDenied/AccessDenied.tsx
+++ apps/journeys-admin/src/components/AccessDenied/AccessDenied.tsx
@@ -1,73 +1,157 @@
 import { ReactElement } from 'react'
-import CardActions from '@mui/material/CardActions'
+import { useTranslation } from 'react-i18next'
 import Typography from '@mui/material/Typography'
+import List from '@mui/material/List'
+import Stack from '@mui/material/Stack'
+import LockRoundedIcon from '@mui/icons-material/LockRounded'
+import Divider from '@mui/material/Divider'
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded'
+import ContactSupportIcon from '@mui/icons-material/ContactSupport'
+import NextLink from 'next/link'
 import Button from '@mui/material/Button'
-import Container from '@mui/material/Container'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
 import Box from '@mui/material/Box'
-import Image from 'next/image'
-import Link from 'next/link'
-import logo from '../../../public/logo.svg'
+import { AccessDeniedListItem } from './AccessDeniedListItem'
 
 interface AccessDeniedProps {
-  title: string
-  description: string
-  onClick?: () => void
+  handleClick?: () => void
+  requestedAccess?: boolean
 }
 
 export function AccessDenied({
-  title,
-  description,
-  onClick
+  handleClick,
+  requestedAccess = false
 }: AccessDeniedProps): ReactElement {
-  const requestAccess = onClick != null
+  const { t } = useTranslation('apps-journeys-admin')
 
   return (
-    <Container maxWidth="xs" sx={{ py: 10 }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          pb: 10
-        }}
+    <>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        sx={{ width: '100%', height: '100vh' }}
       >
-        <Link href="/" passHref>
-          <a>
-            <Image src={logo} alt="Next Steps" height={68} width={152} />
-          </a>
-        </Link>
-      </Box>
-      <Card
-        variant="outlined"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textAlign: 'center'
-        }}
-      >
-        <CardContent>
-          <Typography variant="h3" component="h1" gutterBottom>
-            {title}
-          </Typography>
-          <Typography>{description}</Typography>
-        </CardContent>
-        <CardActions>
-          {requestAccess ? (
-            <Button variant="contained" onClick={onClick}>
-              Request Access
-            </Button>
-          ) : (
-            <Link href="/" passHref>
-              <Button variant="contained">Back to the Admin Panel</Button>
-            </Link>
-          )}
-        </CardActions>
-      </Card>
-    </Container>
+        <Box sx={{ width: { xs: '296px', sm: '587px', md: '759px' } }}>
+          <Stack
+            direction="row"
+            alignItems="center"
+            display={{ xs: 'none', sm: 'flex' }}
+            sx={{ mb: 8 }}
+          >
+            <LockRoundedIcon
+              sx={{ width: '40px', ml: 5, mr: 3, color: 'secondary.light' }}
+            />
+            <Typography variant="h3" color="text.primary">
+              {t(`You can't edit this journey`)}
+            </Typography>
+          </Stack>
+          <Stack
+            alignItems="center"
+            display={{ xs: 'flex', sm: 'none' }}
+            sx={{ mb: 7 }}
+          >
+            <LockRoundedIcon
+              sx={{
+                width: '40px',
+                ml: 5,
+                mr: 3,
+                color: '#AAACBB',
+                mb: 4.5
+              }}
+            />
+            <Typography variant="h4" color="text.primary" align="center">
+              {t(`You can't edit this journey`)}
+            </Typography>
+          </Stack>
+          <List
+            disablePadding
+            sx={{
+              borderRadius: '6px',
+              overflow: 'hidden'
+            }}
+          >
+            <AccessDeniedListItem
+              stepNumber={1}
+              heading={t('Request Access')}
+              description={t(
+                'Send an access request to the creator of this journey'
+              )}
+              stepActive
+              requestedAccess={requestedAccess}
+              handleRequestAccess={handleClick}
+            />
+            <Divider
+              sx={{
+                height: requestedAccess ? '1px' : '2px',
+                bgcolor: requestedAccess ? 'divider' : 'primary.main'
+              }}
+            />
+            <AccessDeniedListItem
+              stepNumber={2}
+              heading={t('Wait for Approval')}
+              description={t('The owner needs to approve you as an editor')}
+              stepActive={requestedAccess}
+            />
+            <Divider
+              sx={{
+                height: requestedAccess ? '2px' : '1px',
+                bgcolor: requestedAccess ? 'primary.main' : 'divider'
+              }}
+            />
+            <AccessDeniedListItem
+              stepNumber={3}
+              heading={t('Edit this journey')}
+              description={t(
+                'Once approved you can open this journey in your admin'
+              )}
+            />
+          </List>
+          <Stack direction="row" justifyContent="space-between" sx={{ mt: 7 }}>
+            <Box display={{ xs: 'none', sm: 'flex' }}>
+              <NextLink href="/" passHref>
+                <Button
+                  sx={{ color: 'primary.main' }}
+                  startIcon={<ChevronLeftRoundedIcon />}
+                  size="small"
+                >
+                  {t('Back to my journeys')}
+                </Button>
+              </NextLink>
+            </Box>
+            <Box display={{ xs: 'flex', sm: 'none' }}>
+              <NextLink href="/" passHref>
+                <Button
+                  sx={{ color: 'primary.main' }}
+                  startIcon={<ChevronLeftRoundedIcon />}
+                  size="small"
+                >
+                  {t('All journeys')}
+                </Button>
+              </NextLink>
+            </Box>
+            <NextLink
+              href="mailto:support@nextstep.is?subject=Need%20help%20with%20requesting%20editing%20access%20to%20the%20journey"
+              passHref
+            >
+              <Button
+                sx={{
+                  color: 'secondary.main',
+                  borderColor: 'secondary.main',
+                  '&:hover': {
+                    borderColor: 'secondary.main'
+                  }
+                }}
+                startIcon={
+                  <ContactSupportIcon sx={{ color: 'secondary.dark' }} />
+                }
+                size="small"
+                variant="outlined"
+              >
+                {t('Get Help')}
+              </Button>
+            </NextLink>
+          </Stack>
+        </Box>
+      </Stack>
+    </>
   )
 }
