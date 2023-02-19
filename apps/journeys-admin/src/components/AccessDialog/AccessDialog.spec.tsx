9634afff65511d14d8288f61963ca064c1a48fd6
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2023-02-20 12:02:38 +1300

    feat: next steps permissions (#1308)

--- apps/journeys-admin/src/components/AccessDialog/AccessDialog.spec.tsx
+++ apps/journeys-admin/src/components/AccessDialog/AccessDialog.spec.tsx
@@ -1,28 +1,138 @@
 import { MockedProvider } from '@apollo/client/testing'
 import { fireEvent, render, waitFor } from '@testing-library/react'
-import { noop } from 'lodash'
 import { SnackbarProvider } from 'notistack'
-import { UserJourneyRole } from '../../../__generated__/globalTypes'
-import { AccessDialog, GET_JOURNEY_WITH_USER_JOURNEYS } from './AccessDialog'
+import {
+  GET_JOURNEY_WITH_USER_JOURNEYS,
+  GET_USER_INVITES,
+  AccessDialog
+} from './AccessDialog'
 
 jest.mock('@mui/material/useMediaQuery', () => ({
   __esModule: true,
   default: () => true
 }))
 
+const user1 = { id: 'userId1', email: 'admin@email.com' }
+
 jest.mock('../../libs/useCurrentUser', () => ({
   __esModule: true,
   useCurrentUser: jest.fn().mockReturnValue({
     loadUser: jest.fn(),
     data: {
-      id: 'userId1',
       __typename: 'User',
-      email: 'amin@email.com'
+      ...user1
     }
   })
 }))
 
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str
+    }
+  }
+}))
+
+const mocks = [
+  {
+    request: {
+      query: GET_JOURNEY_WITH_USER_JOURNEYS,
+      variables: {
+        id: 'journeyId'
+      }
+    },
+    result: {
+      data: {
+        journey: {
+          id: 'journeyId',
+          userJourneys: [
+            {
+              __typename: 'UserJourney',
+              id: 'userJourneyId1',
+              role: 'owner',
+              user: {
+                ...user1,
+                firstName: 'Amin',
+                lastName: 'One',
+                imageUrl: 'https://bit.ly/3Gth4Yf'
+              }
+            },
+            {
+              __typename: 'UserJourney',
+              id: 'userJourneyId2',
+              role: 'editor',
+              user: {
+                id: 'userId2',
+                firstName: 'Horace',
+                lastName: 'Two',
+                imageUrl: 'https://bit.ly/3rgHd6a',
+                email: 'horace@email.com'
+              }
+            },
+            {
+              __typename: 'UserJourney',
+              id: 'userJourneyId3',
+              role: 'inviteRequested',
+              user: {
+                id: 'userId3',
+                firstName: 'Coral',
+                lastName: 'Three',
+                imageUrl: 'https://bit.ly/3nlwUwJ',
+                email: 'coral@email.com'
+              }
+            }
+          ]
+        }
+      }
+    }
+  },
+  {
+    request: {
+      query: GET_USER_INVITES,
+      variables: {
+        journeyId: 'journeyId'
+      }
+    },
+    result: {
+      data: {
+        userInvites: [
+          {
+            __typename: 'UserInvite',
+            id: 'invite.id',
+            journeyId: 'journey.id',
+            email: 'invite@email.com',
+            acceptedAt: null,
+            removedAt: null
+          }
+        ]
+      }
+    }
+  }
+]
+
 describe('AccessDialog', () => {
+  it('should display users and requested users', async () => {
+    const handleClose = jest.fn()
+    const { getByRole } = render(
+      <SnackbarProvider>
+        <MockedProvider addTypename mocks={mocks}>
+          <AccessDialog journeyId="journeyId" open onClose={handleClose} />
+        </MockedProvider>
+      </SnackbarProvider>
+    )
+
+    await waitFor(() => {
+      expect(getByRole('heading', { name: 'Editors' })).toBeInTheDocument()
+      expect(
+        getByRole('heading', { name: 'Requested Access' })
+      ).toBeInTheDocument()
+    })
+    expect(
+      getByRole('heading', { name: 'Invite Editor by' })
+    ).toBeInTheDocument()
+  })
+
   it('calls on close', () => {
     const handleClose = jest.fn()
     const { getByTestId } = render(
@@ -34,99 +144,5 @@ describe('AccessDialog', () => {
     )
     fireEvent.click(getByTestId('dialog-close-button'))
     expect(handleClose).toHaveBeenCalled()
-  })
-
-  it('should enable role edit if current user is the journey owner', async () => {
-    const { getByRole } = render(
-      <SnackbarProvider>
-        <MockedProvider
-          mocks={[
-            {
-              request: {
-                query: GET_JOURNEY_WITH_USER_JOURNEYS,
-                variables: {
-                  id: 'journeyId'
-                }
-              },
-              result: {
-                data: {
-                  journey: {
-                    id: 'journeyId',
-                    __typename: 'Journey',
-                    userJourneys: [
-                      {
-                        id: 'userJourneyId1',
-                        __typename: 'UserJourney',
-                        role: UserJourneyRole.owner,
-                        user: {
-                          id: 'userId1',
-                          __typename: 'User',
-                          firstName: 'Amin',
-                          lastName: 'One',
-                          imageUrl: 'https://bit.ly/3Gth4Yf',
-                          email: 'amin@email.com'
-                        }
-                      },
-                      {
-                        id: 'userJourneyId2',
-                        __typename: 'UserJourney',
-                        role: UserJourneyRole.editor,
-                        user: {
-                          id: 'userId2',
-                          __typename: 'User',
-                          firstName: 'Horace',
-                          lastName: 'Two',
-                          imageUrl: 'https://bit.ly/3rgHd6a',
-                          email: 'horace@email.com'
-                        }
-                      }
-                    ]
-                  }
-                }
-              }
-            }
-          ]}
-        >
-          <AccessDialog journeyId="journeyId" open onClose={noop} />
-        </MockedProvider>
-      </SnackbarProvider>
-    )
-    await waitFor(() =>
-      expect(getByRole('button', { name: 'Editor' })).not.toBeDisabled()
-    )
-  })
-
-  describe('copy to clipboard', () => {
-    const originalNavigator = { ...global.navigator }
-
-    beforeEach(() => {
-      Object.assign(navigator, {
-        clipboard: {
-          writeText: jest.fn()
-        }
-      })
-    })
-
-    afterEach(() => {
-      jest.resetAllMocks()
-      Object.assign(navigator, originalNavigator)
-    })
-
-    it('copies link to clipboard', async () => {
-      const { getByRole, getByText } = render(
-        <SnackbarProvider>
-          <MockedProvider>
-            <AccessDialog journeyId="journeyId" open onClose={noop} />
-          </MockedProvider>
-        </SnackbarProvider>
-      )
-      const link = 'http://localhost/journeys/journeyId'
-      expect(getByRole('textbox')).toHaveValue(link)
-      fireEvent.click(getByRole('button', { name: 'Copy' }))
-      expect(navigator.clipboard.writeText).toHaveBeenCalledWith(link)
-      await waitFor(() =>
-        expect(getByText('Editor invite link copied')).toBeInTheDocument()
-      )
-    })
   })
 })
