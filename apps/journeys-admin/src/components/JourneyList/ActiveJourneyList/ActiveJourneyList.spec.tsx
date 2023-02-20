c2b284ff58d1b2e5ba233e9dc2816ea62b80025c
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-02-21 11:32:01 +1300

    feat: filter new journeys (#1314)
    Co-authored-by: Ziwei Liu <ziwei.liu.23@gmail.com>
    Co-authored-by: Ziwei Liu <10802634+storyworks@users.noreply.github.com>
    Co-authored-by: Nicholas Yao <72791134+PShuckle@users.noreply.github.com>
    Co-authored-by: Nicholas Yao <nicholasyaoyao@gmail.com>

--- apps/journeys-admin/src/components/JourneyList/ActiveJourneyList/ActiveJourneyList.spec.tsx
+++ apps/journeys-admin/src/components/JourneyList/ActiveJourneyList/ActiveJourneyList.spec.tsx
@@ -5,13 +5,21 @@ import { SnackbarProvider } from 'notistack'
 import { AuthUser } from 'next-firebase-auth'
 import { defaultJourney, oldJourney } from '../journeyListData'
 import { ThemeProvider } from '../../ThemeProvider'
-import { SortOrder } from '../JourneySort'
 import {
   ActiveJourneyList,
   ARCHIVE_ACTIVE_JOURNEYS,
   GET_ACTIVE_JOURNEYS,
   TRASH_ACTIVE_JOURNEYS
 } from './ActiveJourneyList'
+
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str
+    }
+  }
+}))
 
 const activeJourneysMock = {
   request: {
@@ -38,70 +46,6 @@ const noJourneysMock = {
 const authUser = { id: 'user-id1' } as unknown as AuthUser
 
 describe('ActiveJourneyList', () => {
-  it('should render journeys in descending createdAt date by default', async () => {
-    const { getAllByLabelText } = render(
-      <MockedProvider mocks={[activeJourneysMock]}>
-        <ThemeProvider>
-          <SnackbarProvider>
-            <ActiveJourneyList onLoad={noop} event="" />
-          </SnackbarProvider>
-        </ThemeProvider>
-      </MockedProvider>
-    )
-
-    await waitFor(() =>
-      expect(getAllByLabelText('journey-card')[0].textContent).toContain(
-        'January 1'
-      )
-    )
-    expect(getAllByLabelText('journey-card')[1].textContent).toContain(
-      'November 19, 2020'
-    )
-  })
-
-  it('should order journeys in alphabetical order', async () => {
-    const lowerCaseJourneyTitle = {
-      ...defaultJourney,
-      title: 'a lower case title'
-    }
-
-    const { getAllByLabelText } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: GET_ACTIVE_JOURNEYS
-            },
-            result: {
-              data: {
-                journeys: [lowerCaseJourneyTitle, oldJourney]
-              }
-            }
-          }
-        ]}
-      >
-        <ThemeProvider>
-          <SnackbarProvider>
-            <ActiveJourneyList
-              onLoad={noop}
-              sortOrder={SortOrder.TITLE}
-              event=""
-            />
-          </SnackbarProvider>
-        </ThemeProvider>
-      </MockedProvider>
-    )
-
-    await waitFor(() =>
-      expect(getAllByLabelText('journey-card')[0].textContent).toContain(
-        'An Old Journey Heading'
-      )
-    )
-    expect(getAllByLabelText('journey-card')[1].textContent).toContain(
-      'a lower case title'
-    )
-  })
-
   it('should ask users to add a new journey', async () => {
     const { getByRole, getByText } = render(
       <MockedProvider mocks={[noJourneysMock]}>
@@ -166,7 +110,7 @@ describe('ActiveJourneyList', () => {
     })
 
     const result = jest.fn(() => ({
-      data: [{ id: defaultJourney.id, status: 'archived' }]
+      data: { journeysArchive: [{ id: defaultJourney.id, status: 'archived' }] }
     }))
     const archiveJourneysMock = {
       request: {
@@ -229,7 +173,7 @@ describe('ActiveJourneyList', () => {
 
   describe('Trash All', () => {
     const result = jest.fn(() => ({
-      data: [{ id: defaultJourney.id, status: 'archived' }]
+      data: { journeysTrash: [{ id: defaultJourney.id, status: 'archived' }] }
     }))
     const trashJourneysMock = {
       request: {
