4e2c5933ac086b153707ef995ce4677d85cb5664
Ziwei Liu <ziwei.liu.23@gmail.com>
2023-02-23 03:20:28 +0000

    fix: add controls to field props

--- libs/shared/ui/src/components/UXPin/MuiButton.stories.tsx
+++ libs/shared/ui/src/components/UXPin/MuiButton.stories.tsx
@@ -2,41 +2,102 @@ import React, { ComponentProps } from 'react'
 
 import { Meta, Story } from '@storybook/react'
 
-// import Icon from '@mui/material/Icon'
 import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
 import { sharedUiConfig } from '../../libs/sharedUiConfig'
+import { ThemeProvider } from '../ThemeProvider'
+import { ThemeMode, ThemeName } from '../../libs/themes'
 
-const MuiButtonDemo = {
+const MuiInputComponentsDemo = {
   ...sharedUiConfig,
-  title: 'Mui Components/Button',
+  title: 'Mui Components/Inputs',
   component: Button,
   parameters: {
-    ...sharedUiConfig.parameters,
+    theme: 'light',
+    layout: 'fullscreen',
     chromatic: {
       disableSnapshot: true
+    }
+  },
+  argTypes: {
+    color: {
+      control: { type: 'select' },
+      options: [
+        'inherit',
+        'primary',
+        'secondary',
+        'error',
+        'info',
+        'success',
+        'warning'
+      ]
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large']
     }
   }
 }
 
-const Template: Story<ComponentProps<typeof Button>> = (args) => {
-  return args.children != null ? (
-    <Button {...args}>{args.children}</Button>
-  ) : (
-    <Button {...args} />
+const ButtonTemplate: Story<
+  ComponentProps<typeof Button> & { themeMode: ThemeMode }
+> = (args) => {
+  return (
+    <ThemeProvider themeName={ThemeName.base} themeMode={args.themeMode}>
+      <Button {...args}>{args.children}</Button>
+    </ThemeProvider>
   )
 }
 
-export const MuiButton = Template.bind({})
+export const MuiButton = ButtonTemplate.bind({})
 MuiButton.args = {
   children: 'Button',
   color: 'primary',
   disabled: false,
   disableElevation: false,
-  fullWidth: true,
+  fullWidth: false,
   size: 'medium',
   variant: 'contained',
   href: '',
-  sx: {}
+  centerRipple: false,
+  disableRipple: false,
+  disableTouchRipple: false,
+  focusRipple: false,
+  sx: { ariaLabel: 'Button' },
+  themeMode: ThemeMode.light
+}
+MuiButton.argTypes = {
+  variant: {
+    control: { type: 'select' },
+    options: ['text', 'outlined', 'contained']
+  }
 }
 
-export default MuiButtonDemo as Meta
+const IconButtonTemplate: Story<
+  ComponentProps<typeof IconButton> & { themeMode: ThemeMode }
+> = (args) => {
+  return (
+    <ThemeProvider themeName={ThemeName.base} themeMode={args.themeMode}>
+      <IconButton {...args}>{args.children}</IconButton>
+    </ThemeProvider>
+  )
+}
+
+export const MuiIconButton = IconButtonTemplate.bind({})
+MuiIconButton.args = {
+  children: 'Button',
+  color: 'primary',
+  disabled: false,
+  edge: 'start',
+  size: 'medium',
+  variant: 'contained',
+  href: '',
+  centerRipple: false,
+  disableRipple: false,
+  disableTouchRipple: false,
+  focusRipple: false,
+  sx: { ariaLabel: 'Button' },
+  themeMode: ThemeMode.light
+}
+
+export default MuiInputComponentsDemo as Meta
