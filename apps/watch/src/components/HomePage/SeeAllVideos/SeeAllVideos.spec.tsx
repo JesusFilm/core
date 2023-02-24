018f10a74501b1e9f9b40374c59fef96856c5557
JesseErnst <jesse@onsiteanalytics.co>
2023-02-24 00:17:35 +0000

    fix: test update

--- apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.spec.tsx
+++ apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.spec.tsx
@@ -2,12 +2,6 @@ import { render } from '@testing-library/react'
 import { SeeAllVideos } from './SeeAllVideos'
 
 describe('SeeAllVideos', () => {
-  it('should render hardcoded text', () => {
-    const { getByText } = render(<SeeAllVideos />)
-    expect(getByText('Conversation Starters')).toBeInTheDocument()
-    expect(getByText('+53 Short Evangelical Films')).toBeInTheDocument()
-  })
-
   it('button should have correct link', () => {
     const { getByLabelText } = render(<SeeAllVideos />)
     expect(getByLabelText('all-videos-button')).toHaveAttribute(
