fccacecaa5f2edbb8b82f7c67cd7291d6811ea60
JesseErnst <jesse@onsiteanalytics.co>
2023-02-24 00:31:05 +0000

    fix: prettier

--- apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.tsx
+++ apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.tsx
@@ -5,18 +5,18 @@ import Link from 'next/link'
 
 export function SeeAllVideos(): ReactElement {
   return (
-    <Stack sx={{ pt: '54px', alignItems:"center"}}>
+    <Stack sx={{ pt: '54px', alignItems: 'center' }}>
       <Link href="/videos" passHref>
         <Button
-              size="small"
-              color="secondary"
-              variant="outlined"
-              aria-label="all-videos-button"
-              sx={{
-                width: '15%',
-                whiteSpace: 'nowrap'
-              }}
-            >
+          size="small"
+          color="secondary"
+          variant="outlined"
+          aria-label="all-videos-button"
+          sx={{
+            width: '15%',
+            whiteSpace: 'nowrap'
+          }}
+        >
           See All
         </Button>
       </Link>
