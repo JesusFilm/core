14d56248c8a83229bf772eb83a052ad3d71c555e
Mike Allison <mike@captainofbass.com>
2023-02-25 03:47:06 +0000

    fix: arangodb etl

--- apps/arangodb-bigquery-etl/bqexport.sh
+++ apps/arangodb-bigquery-etl/bqexport.sh
@@ -13,31 +13,30 @@ do
   bq --project_id jfp-data-warehouse load --replace --source_format NEWLINE_DELIMITED_JSON --autodetect nextsteps.$collection export/$collection.jsonl.gz
 done
 
+# The following collections require some custom queries or import information to avoid errors on import.
+
 # userJourneys
 echo "Exporting userJourneys from ArangoDB"
 arangoexport --type jsonl --compress-output true --overwrite true --server.authentication true --server.database $DATABASE_DB --server.username $DATABASE_USER --server.password $DATABASE_PASS --server.endpoint $DATABASE_URL --custom-query "FOR uj IN userJourneys RETURN { _key: uj._key, userId: uj.userId, journeyId: uj.journeyId, role: uj.role }"
 echo "Importing userJourneys to BigQuery"
 bq --project_id jfp-data-warehouse load --replace --source_format NEWLINE_DELIMITED_JSON nextsteps.userJourneys export/query.jsonl.gz _key:string,userId:string,journeyId:string,role:string
 
-# event response is very specific due to bq being unable to parse the variable fieldset
+# events
 echo "Exporting events from ArangoDB"
 arangoexport --type jsonl --compress-output true --overwrite true --server.authentication true --server.database $DATABASE_DB --server.username $DATABASE_USER --server.password $DATABASE_PASS --server.endpoint $DATABASE_URL --custom-query "FOR e IN events RETURN { _key: e._key, blockId: e.blockId, stepId: e.stepId, label: e.label, value: e.value, journeyId: e.journeyId, radioOptionBlockId: e.radioOptionBlockId, email: e.email, position: e.position, createdAt: e.createdAt }"
 echo "Importing events to BigQuery"
 bq --project_id jfp-data-warehouse load --replace --source_format NEWLINE_DELIMITED_JSON nextsteps.events export/query.jsonl.gz _key:string,blockId:string,stepId:string,label:string,value:string,journeyId:string,radioOptionBlockId:string,email:string,position:string,createdAt:string
 
-
-# blocks response is limited due to having very dynamic fieldsets and json that jq can't easily parse
+# blocks
 echo "Exporting blocks from ArangoDB"
 arangoexport --type jsonl --compress-output true --overwrite true --server.authentication true --server.database $DATABASE_DB --server.username $DATABASE_USER --server.password $DATABASE_PASS --server.endpoint $DATABASE_URL --custom-query "FOR b IN blocks RETURN { _key: b.key, parentOrder: b.parentOrder, journeyId: b.journeyId, __typename: b.__typename, nextBlockId: b.nextBlockId }"
 echo "Importing blocks to BigQuery"
 bq --project_id jfp-data-warehouse load --replace --source_format NEWLINE_DELIMITED_JSON --autodetect nextsteps.blocks export/query.jsonl.gz
 
-
-# videos requires field specificity, since it contains some nested arrays
+# videos
 echo "Exporting videos from ArangoDB"
 arangoexport --type jsonl --compress-output true --overwrite true --server.authentication true --server.database $DATABASE_DB --server.username $DATABASE_USER --server.password $DATABASE_PASS --server.endpoint $DATABASE_URL --custom-query "FOR v IN videos RETURN { _key: v._key, label: v.label, primaryLanguageId: v.primaryLanguageId, slug: v.slug, __typename: v.__typename }" 
 echo "Importing videos to BigQuery"
 bq --project_id jfp-data-warehouse load --replace --source_format NEWLINE_DELIMITED_JSON --autodetect nextsteps.videos export/query.jsonl.gz
-
 
 rm /export/*.gz
