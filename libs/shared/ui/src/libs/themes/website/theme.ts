8e64e8e5c3c429b89c481ded20abd50242da869e
Ziwei Liu <ziwei.liu.23@gmail.com>
2023-03-03 01:34:48 +0000

    fix: use width related breakpoints only

--- libs/shared/ui/src/libs/themes/website/theme.ts
+++ libs/shared/ui/src/libs/themes/website/theme.ts
@@ -1,14 +1,29 @@
 import { deepmerge } from '@mui/utils'
 import { createTheme } from '@mui/material/styles'
-import { baseBreakpoints } from '../base/tokens/breakpoints'
 import { baseSpacing } from '../base/tokens/spacing'
 import { websiteTypography } from './tokens/typography'
 import { websiteComponents } from './tokens/components'
 import { websiteColorsLight, websiteColorsDark } from './tokens/colors'
 
+// TODO: Decide on more permanent website breakpoints
+declare module '@mui/material/styles' {
+  interface BreakpointOverrides {
+    xxl: true
+  }
+}
+
 const websiteTheme = {
   ...baseSpacing,
-  ...baseBreakpoints,
+  breakpoints: {
+    values: {
+      xs: 0, // Mobile (P)
+      sm: 568, // Mobile (L)
+      md: 600, // Tablet (P)
+      lg: 961, // Tablet (L)
+      xl: 1200, // Laptop
+      xxl: 1400 // Desktop,
+    }
+  },
   ...websiteTypography,
   ...websiteComponents
 }
