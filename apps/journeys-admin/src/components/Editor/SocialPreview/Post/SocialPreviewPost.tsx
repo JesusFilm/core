770cb1bd5af1a8fb8c54fa32588af5fca7be9ab2
Mike Allison <dj.mikeallison@gmail.com>
2023-03-09 20:13:47 +0000

    feat: social preview

---
+++ apps/journeys-admin/src/components/Editor/SocialPreview/Post/SocialPreviewPost.tsx
@@ -0,0 +1,48 @@
+import { ReactElement } from 'react'
+import Image from 'next/image'
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import { JourneyFields } from '../../../../../__generated__/JourneyFields'
+
+interface SocialPreviewPostProps {
+  journey?: JourneyFields
+  avatar: ReactElement
+}
+export function SocialPreviewPost({
+  journey,
+  avatar
+}: SocialPreviewPostProps): ReactElement {
+  return (
+    <>
+      {journey != null && (
+        <Card sx={{ maxWidth: 200 }}>
+          <CardHeader avatar={avatar} />
+          {journey?.primaryImageBlock != null && (
+            <CardMedia>
+              <Image
+                src={journey?.primaryImageBlock.src}
+                alt={journey?.primaryImageBlock.alt}
+                width={180}
+                height={97}
+              />
+            </CardMedia>
+          )}
+          <CardContent>
+            <div>
+              <Typography variant="body1">YOUR.NEXTSTEP.IS</Typography>
+            </div>
+            <div>
+              <Typography variant="body1">{journey.seoTitle}</Typography>
+            </div>
+            {/* <div>
+              <Typography variant="body2">{journey.seoDescription}</Typography>
+            </div> */}
+          </CardContent>
+        </Card>
+      )}
+    </>
+  )
+}
--- apps/journeys-admin/src/components/Editor/SocialPreview/SocialPreview.tsx
+++ apps/journeys-admin/src/components/Editor/SocialPreview/SocialPreview.tsx
@@ -1,9 +1,21 @@
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
 import { ReactElement } from 'react'
+import Avatar from '@mui/material/Avatar'
+import { SocialPreviewPost } from './Post/SocialPreviewPost'
 
 export function SocialPreview(): ReactElement {
+  const { journey } = useJourney()
   return (
     <div>
       <h1>Social Preview</h1>
+      <SocialPreviewPost
+        journey={journey}
+        avatar={<Avatar sx={{ bgcolor: '#4267B2' }}>f</Avatar>}
+      />
+      <SocialPreviewPost
+        journey={journey}
+        avatar={<Avatar sx={{ bgcolor: '#1DA1F2' }}>tw</Avatar>}
+      />
     </div>
   )
 }
