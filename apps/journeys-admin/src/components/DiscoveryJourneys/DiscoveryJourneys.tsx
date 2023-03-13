76f3eb2e3a6f1391d034d6bb1b8a5a1e2a505e51
Siyang <csiyang@yahoo.co.nz>
2023-03-13 21:12:38 +0000

    style: adjust margin and padding for discovery journeys container

--- apps/journeys-admin/src/components/DiscoveryJourneys/DiscoveryJourneys.tsx
+++ apps/journeys-admin/src/components/DiscoveryJourneys/DiscoveryJourneys.tsx
@@ -4,7 +4,15 @@ import { EmbedJourney } from './EmbedJourney'
 
 export function DiscoveryJourneys(): ReactElement {
   return (
-    <Stack direction="row" sx={{ justifyContent: 'space-evenly' }}>
+    <Stack
+      direction="row"
+      sx={{
+        justifyContent: 'space-evenly',
+        pt: '15px',
+        ml: '-30px',
+        mr: '-30px'
+      }}
+    >
       <EmbedJourney slug="admin-left" />
       <EmbedJourney slug="admin-center" />
       <EmbedJourney slug="admin-right" />
