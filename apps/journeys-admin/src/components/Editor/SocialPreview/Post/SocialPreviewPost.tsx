5e5b262e43948100c977be3b44c63330fd79abd5
Mike Allison <dj.mikeallison@gmail.com>
2023-03-16 00:49:20 +0000

    feat: social preview

--- apps/journeys-admin/src/components/Editor/SocialPreview/Post/SocialPreviewPost.tsx
+++ apps/journeys-admin/src/components/Editor/SocialPreview/Post/SocialPreviewPost.tsx
@@ -5,6 +5,8 @@ import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import Box from '@mui/system/Box'
+import Stack from '@mui/material/Stack'
 import { JourneyFields } from '../../../../../__generated__/JourneyFields'
 
 interface SocialPreviewPostProps {
@@ -18,18 +20,38 @@ export function SocialPreviewPost({
   return (
     <>
       {journey != null && (
-        <Card sx={{ maxWidth: 200 }}>
-          <CardHeader avatar={avatar} />
-          {journey?.primaryImageBlock != null && (
-            <CardMedia>
+        <Card sx={{ maxWidth: 200, borderRadius: '4px' }}>
+          <CardHeader sx={{ px: '8px' }} avatar={avatar}>
+            <Stack>
+              <Box
+                sx={{
+                  width: '65px',
+                  height: 0,
+                  border: '8px solid #C2C2C2'
+                }}
+              ></Box>
+              <div></div>
+            </Stack>
+          </CardHeader>
+          <CardMedia sx={{ px: '8px' }}>
+            {journey?.primaryImageBlock?.src == null ? (
+              <Box
+                sx={{
+                  bgcolor: 'rgba(0, 0, 0, 0.1)',
+                  width: 180,
+                  height: 97,
+                  borderRadius: '6px'
+                }}
+              />
+            ) : (
               <Image
                 src={journey?.primaryImageBlock.src}
                 alt={journey?.primaryImageBlock.alt}
                 width={180}
                 height={97}
               />
-            </CardMedia>
-          )}
+            )}
+          </CardMedia>
           <CardContent>
             <div>
               <Typography variant="body1">YOUR.NEXTSTEP.IS</Typography>
