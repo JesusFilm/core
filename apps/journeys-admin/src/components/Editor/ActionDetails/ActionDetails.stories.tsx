9f9f85ebf84952b3cea18d357b2a85a31a9f2bda
John Geronimo <johnageronimo1@gmail.com>
2023-03-16 01:20:01 +0000

    fix: add placeholder value for goals

--- apps/journeys-admin/src/components/Editor/ActionDetails/ActionDetails.stories.tsx
+++ apps/journeys-admin/src/components/Editor/ActionDetails/ActionDetails.stories.tsx
@@ -1,6 +1,7 @@
 import { Story, Meta } from '@storybook/react'
 import { MockedProvider } from '@apollo/client/testing'
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
+import { ComponentProps } from 'react'
 import { simpleComponentConfig } from '../../../libs/storybook'
 import { journey } from './data'
 import { ActionDetails } from '.'
@@ -11,14 +12,22 @@ const ActionDetailsStory = {
   title: 'Journeys-Admin/Editor/ActionDetails'
 }
 
-const Template: Story = () => (
+const Template: Story<ComponentProps<typeof ActionDetails>> = (args) => (
   <MockedProvider>
     <JourneyProvider value={{ journey }}>
-      <ActionDetails url="https://www.google.com/" />
+      <ActionDetails {...args} />
     </JourneyProvider>
   </MockedProvider>
 )
 
 export const Default = Template.bind({})
+Default.args = {
+  url: 'https://www.google.com/'
+}
+
+export const Placeholder = Template.bind({})
+Placeholder.args = {
+  url: null
+}
 
 export default ActionDetailsStory as Meta
--- apps/journeys-admin/src/components/Editor/ActionDetails/ActionDetails.tsx
+++ apps/journeys-admin/src/components/Editor/ActionDetails/ActionDetails.tsx
@@ -1,17 +1,44 @@
 import { ReactElement } from 'react'
 import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 import { ActionCards } from './ActionCards'
 import { ActionEditor } from './ActionEditor'
 
 interface ActionDetailsProps {
-  url: string
+  url?: string
 }
 
 export function ActionDetails({ url }: ActionDetailsProps): ReactElement {
   return (
-    <Stack gap={2} sx={{ p: 6 }}>
-      <ActionEditor url={url} />
-      <ActionCards url={url} />
-    </Stack>
+    <>
+      {url == null ? (
+        <Stack gap={2} sx={{ p: 6 }}>
+          <Typography variant="subtitle2">What are Possible Goals?</Typography>
+          <Typography variant="body2" color="text.secondary">
+            Depending on the link you provide for the actions, the target of
+            your Journey will be determined automatically from the following
+            list:
+          </Typography>
+          <Typography variant="subtitle2">Start a conversation</Typography>
+          <Typography variant="body2" color="text.secondary">
+            If the goal is to go any chat platform
+          </Typography>
+          <Typography variant="subtitle2">Visit a website</Typography>
+          <Typography variant="body2" color="text.secondary">
+            This could be your church or ministry website, or whatever you want
+            to redirect the viewer to.
+          </Typography>
+          <Typography variant="subtitle2">Link to bible</Typography>
+          <Typography variant="body2" color="text.secondary">
+            If the target of the journey is to download the Bible
+          </Typography>
+        </Stack>
+      ) : (
+        <Stack gap={2} sx={{ p: 6 }}>
+          <ActionEditor url={url} />
+          <ActionCards url={url} />
+        </Stack>
+      )}
+    </>
   )
 }
