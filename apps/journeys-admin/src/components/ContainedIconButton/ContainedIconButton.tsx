7eca804b21854e0c214eeb19000de71c1501a392
tanflem <tanner.fleming@gmail.com>
2023-03-19 15:08:24 +0000

    feat: intial component commit

---
+++ apps/journeys-admin/src/components/ContainedIconButton/ContainedIconButton.tsx
@@ -0,0 +1,39 @@
+import { ReactElement, ReactNode } from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import AddRoundedIcon from '@mui/icons-material/AddRounded'
+import Card from '@mui/material/Card'
+import CardActionArea from '@mui/material/CardActionArea'
+import Stack from '@mui/material/Stack'
+import { ImageBlockThumbnail } from '../Editor/ImageBlockThumbnail'
+
+interface ContainedIconProps {
+  icon: ReactNode
+  label: string
+  onClick: ()=> void
+}
+
+export function ContainedIconButton({
+  icon,
+  label,
+  onClick
+}: ContainedIconProps): ReactElement {
+
+  return (
+    <>
+      <Card variant="outlined" sx={{ borderRadius: 2 }}>
+        <CardActionArea onClick={onClick}>
+          <Stack direction="row" alignItems="center" spacing={3} sx={{ p: 2 }}>
+            <Box>
+              <ImageBlockThumbnail Icon={icon} />
+            </Box>
+            <Box flexGrow={1} minWidth={0}>
+              <Typography variant="subtitle2">{label}</Typography>
+            </Box>
+            <AddRoundedIcon color="primary" />
+          </Stack>
+        </CardActionArea>
+      </Card>
+    </>
+  )
+}
