5db1b85d20dea2cc32870c3d42623b7cbcbdc228
Ziwei Liu <ziwei.liu.23@gmail.com>
2023-03-21 02:11:20 +0000

    fix: add placeholder test file

---
+++ apps/journeys-admin/src/components/OnboardingPanelContent/OnboardingPanelContent.spec.tsx
@@ -0,0 +1,22 @@
+export {}
+describe.skip('OnboardingPanelContent', () => {
+  it('should add a new journey on custom journey button click', () => {
+    // test that the useMutation call result is correct and cache is updated
+  })
+
+  it('should display onboarding templates', () => {
+    // expect those 5 templates
+  })
+
+  it('should redirect to template details page onClick', () => {
+    // test onClick for a template card
+  })
+
+  it('should redirect on See all link', () => {
+    // test router on See all link click
+  })
+
+  it('should redirect on See all templates button', () => {
+    // test router on See all templates button click
+  })
+})
