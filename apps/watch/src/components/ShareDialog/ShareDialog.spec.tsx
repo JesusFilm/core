fe6f4af00d0c2063359c99e6eb452116088077f2
Gavin Walsh <walsh.gavin@gmail.com>
2023-03-25 15:20:32 +0000

    fix: embed code respects language

--- apps/watch/src/components/ShareDialog/ShareDialog.spec.tsx
+++ apps/watch/src/components/ShareDialog/ShareDialog.spec.tsx
@@ -249,7 +249,7 @@ describe('ShareDialog', () => {
     })
 
     it('should copy embed code', async () => {
-      const code = `<div class="arc-cont"><iframe src="https://api.arclight.org/videoPlayerUrl?refId=1_529-jf-0-0&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><style>.arc-cont{position:relative;display:block;margin:10px auto;width:100%}.arc-cont:after{padding-top:59%;display:block;content:""}.arc-cont>iframe{position:absolute;top:0;bottom:0;right:0;left:0;width:98%;height:98%;border:0}</style></div>`
+      const code = `<div class="arc-cont"><iframe src="https://api.arclight.org/videoPlayerUrl?refId=videoVariantId&playerStyle=default" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe><style>.arc-cont{position:relative;display:block;margin:10px auto;width:100%}.arc-cont:after{padding-top:59%;display:block;content:""}.arc-cont>iframe{position:absolute;top:0;bottom:0;right:0;left:0;width:98%;height:98%;border:0}</style></div>`
 
       const { getByRole, getByText } = render(
         <SnackbarProvider>
