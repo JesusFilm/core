5ee780dd4e19cbbf078657d572466c836381aa64
tanflem <tanner.fleming@gmail.com>
2023-03-28 16:54:59 +0000

    refactor: tests use noop and renamed onClick to handleClick

--- apps/journeys-admin/src/components/ContainedIconButton/ContainedIconButton.spec.tsx
+++ apps/journeys-admin/src/components/ContainedIconButton/ContainedIconButton.spec.tsx
@@ -1,16 +1,16 @@
 import { render, fireEvent } from '@testing-library/react'
 import NoteAddIcon from '@mui/icons-material/NoteAdd'
 import AddIcon from '@mui/icons-material/Add'
+import { noop } from 'lodash'
 import { ContainedIconButton } from './ContainedIconButton'
 
 describe('ContainedIconButton', () => {
-  const onClick = jest.fn()
   it('should render button', async () => {
     const { getByRole } = render(
       <ContainedIconButton
         thumbnailIcon={<NoteAddIcon />}
         label="Label"
-        onClick={onClick}
+        handleClick={noop}
         actionIcon={<AddIcon />}
       />
     )
@@ -18,32 +18,17 @@ describe('ContainedIconButton', () => {
     expect(getByRole('button')).toHaveTextContent('Label')
   })
 
-  it('should call onClick on button click', () => {
+  it('should call handleClick on button click', () => {
     const { getByRole } = render(
       <ContainedIconButton
         thumbnailIcon={<NoteAddIcon />}
         label="Label"
-        onClick={onClick}
+        handleClick={noop}
         actionIcon={<AddIcon />}
       />
     )
     fireEvent.click(getByRole('button'))
 
-    expect(onClick).toHaveBeenCalled()
+    expect(noop).toHaveBeenCalled()
   })
-
-  //   it('should render imageSrc when given', () => {
-  //     const { getByRole } = render(
-  //       <ContainedIconButton
-  //         imageSrc={testImageSrc}
-  //         thumbnailIcon={<NoteAddIcon />}
-  //         label="Label"
-  //         onClick={onClick}
-  //         actionIcon={<AddIcon />}
-  //       />
-  //     )
-  //     expect(getByRole('imageSrc')).toHaveTextContent(
-  //       'https://d1wl257kev7hsz.cloudfront.net/cinematics/2_Acts7302-0-0.mobileCinematicHigh.jpg'
-  //     )
-  //   })
 })
