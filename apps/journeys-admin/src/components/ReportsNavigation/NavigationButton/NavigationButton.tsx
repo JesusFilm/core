7e1e674de0d638fe7e649fb6b0efd4b439fd35b6
Siyang <csiyang@yahoo.co.nz>
2023-03-31 01:13:26 +0000

    feat: button styling

---
+++ apps/journeys-admin/src/components/ReportsNavigation/NavigationButton/NavigationButton.tsx
@@ -0,0 +1,33 @@
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import NextLink from 'next/link'
+import { ReactElement } from 'react'
+
+interface Props {
+  selected: boolean
+  value: string
+  link: string
+}
+
+export function NavigationButton({
+  selected,
+  value,
+  link
+}: Props): ReactElement {
+  return (
+    <NextLink href={link} passHref>
+      <Button
+        aria-selected={selected}
+        variant={selected ? 'contained' : 'outlined'}
+        sx={{
+          p: 4,
+          minWidth: '250px',
+          borderRadius: 1,
+          backgroundColor: selected ? undefined : 'background.paper'
+        }}
+      >
+        <Typography variant="subtitle1">{value}</Typography>
+      </Button>
+    </NextLink>
+  )
+}
--- apps/journeys-admin/src/components/ReportsNavigation/ReportsNavigation.tsx
+++ apps/journeys-admin/src/components/ReportsNavigation/ReportsNavigation.tsx
@@ -1,7 +1,6 @@
 import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
-import NextLink from 'next/link'
 import { ReactElement } from 'react'
+import { NavigationButton } from './NavigationButton'
 
 interface Props {
   selected: 'journeys' | 'visitors'
@@ -10,16 +9,16 @@ interface Props {
 export function ReportsNavigation({ selected }: Props): ReactElement {
   return (
     <Stack direction="row" spacing={4} sx={{ p: 4 }}>
-      <NextLink href="/reports/journeys" passHref>
-        <Button variant={selected === 'journeys' ? 'contained' : 'outlined'}>
-          Journeys
-        </Button>
-      </NextLink>
-      <NextLink href="/reports/visitors" passHref>
-        <Button variant={selected === 'visitors' ? 'contained' : 'outlined'}>
-          Visitors
-        </Button>
-      </NextLink>
+      <NavigationButton
+        selected={selected === 'journeys'}
+        value="Journeys"
+        link="/reports/journeys"
+      />
+      <NavigationButton
+        selected={selected === 'visitors'}
+        value="Visitors"
+        link="/reports/visitors"
+      />
     </Stack>
   )
 }
