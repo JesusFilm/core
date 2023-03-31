61199c1b11187e8fbb4468a9fd3674167f1376e7
Siyang <csiyang@yahoo.co.nz>
2023-03-31 01:48:05 +0000

    test: reports navigation

---
+++ apps/journeys-admin/src/components/ReportsNavigation/ReportsNavigation.spec.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react'
+import { ReportsNavigation } from './ReportsNavigation'
+
+describe('ReportsNavigation', () => {
+  it('should should select journeys', () => {
+    const { getByRole } = render(<ReportsNavigation selected="journeys" />)
+    expect(getByRole('link', { name: 'Journeys' })).toHaveAttribute(
+      'aria-selected',
+      'true'
+    )
+    expect(getByRole('link', { name: 'Visitors' })).toHaveAttribute(
+      'aria-selected',
+      'false'
+    )
+  })
+
+  it('should should select visitors', () => {
+    const { getByRole } = render(<ReportsNavigation selected="visitors" />)
+    expect(getByRole('link', { name: 'Visitors' })).toHaveAttribute(
+      'aria-selected',
+      'true'
+    )
+    expect(getByRole('link', { name: 'Journeys' })).toHaveAttribute(
+      'aria-selected',
+      'false'
+    )
+  })
+})
