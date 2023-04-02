733e2fb54b21bc820a70f60774cf11b512c327f2
John Geronimo <johnageronimo1@gmail.com>
2023-04-02 22:42:48 +0000

    fix: goal navigation story

--- apps/journeys-admin/src/components/CardPreview/NavigationCard/NavigationCard.stories.tsx
+++ apps/journeys-admin/src/components/CardPreview/NavigationCard/NavigationCard.stories.tsx
@@ -1,6 +1,8 @@
 import { ActiveJourneyEditContent } from '@core/journeys/ui/EditorProvider'
 import { Story, Meta } from '@storybook/react'
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt'
+import Box from '@mui/material/Box'
+import { CustomIcon } from '@core/shared/ui/CustomIcon'
 import { simpleComponentConfig } from '../../../libs/storybook'
 import { NavigationCard } from '.'
 
@@ -31,12 +33,32 @@ Default.args = {
 
 export const Outlined = Template.bind({})
 Outlined.args = {
-  outlined: true
+  outlined: true,
+  title: 'Outlined'
 }
 
 export const Loading = Template.bind({})
 Loading.args = {
   loading: true
+}
+
+export const Goals = Template.bind({})
+Goals.args = {
+  id: 'goals',
+  title: 'Goals',
+  header: (
+    <Box
+      bgcolor={(theme) => theme.palette.background.paper}
+      borderRadius={1}
+      width={72}
+      height={72}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <CustomIcon name="Target" color="error" />
+    </Box>
+  )
 }
 
 export default NavigationCardStory as Meta
