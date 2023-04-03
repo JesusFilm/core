17f38a1e0efed03314274efe6fdbdeb7631007d8
Mike Allison <dj.mikeallison@gmail.com>
2023-04-03 20:52:53 +0000

    feat: fix-nx-console

--- tools/scripts/fix-nx-console.sh
+++ tools/scripts/fix-nx-console.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script fixes the nx console vscode extension issue with parcel for M1 macs
+
 cd ~/.vscode-server/extensions/nrwl.angular-console-17.*/node_modules/@parcel/watcher
 npm install
 npm run prebuild --openssl_fips=''
