7f464437d994c9a41fc1dc887462575dc0e6c170
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-04-07 11:12:04 +1200

    fix: increase max sockets when connecting to arango (#1469)

--- libs/nest/database/src/lib/DatabaseModule.ts
+++ libs/nest/database/src/lib/DatabaseModule.ts
@@ -1,17 +1,24 @@
 import { Global, Module } from '@nestjs/common'
 import { Database } from 'arangojs'
+import type { Config } from 'arangojs/connection'
 
 const databaseProviders = [
   {
     provide: 'DATABASE',
     useFactory: () => {
       let db: Database
+      const config: Config = {
+        url: process.env.DATABASE_URL,
+        databaseName: process.env.DATABASE_DB,
+        agentOptions: { maxSockets: 50 }
+      }
+
       if (process.env.DATABASE_DB != null)
         db = new Database({
-          url: process.env.DATABASE_URL,
+          ...config,
           databaseName: process.env.DATABASE_DB
         })
-      else db = new Database({ url: process.env.DATABASE_URL })
+      else db = new Database(config)
       db.useBasicAuth(process.env.DATABASE_USER, process.env.DATABASE_PASS)
       return db
     }
