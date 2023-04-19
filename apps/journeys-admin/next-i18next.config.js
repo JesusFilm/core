358c8df4f381b6ec0a86c230c860f36596c26eae
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-04-19 02:16:05 +0000

    ci: add logging

--- apps/journeys-admin/next-i18next.config.js
+++ apps/journeys-admin/next-i18next.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+console.log('NEXT_PUBLIC_VERCEL_ENV', process.env.NEXT_PUBLIC_VERCEL_ENV)
+console.log('CI', process.env.CI)
+console.log(
+  'localePath test',
+  process.env.NEXT_PUBLIC_VERCEL_ENV == null || process.env.CI != null
+)
+
 /**
  * @type {import('next-i18next').UserConfig}
  **/
