c2eae95d343da46a9d505627cca64c11c3c10b16
Mike Allison <dj.mikeallison@gmail.com>
2023-04-20 00:15:48 +0000

    feat: remove pg etl

---
+++ apps/api-users/src/app/__generated__/graphql.ts
@@ -0,0 +1,37 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export class User {
+    __typename?: 'User';
+    id: string;
+    firstName: string;
+    lastName?: Nullable<string>;
+    email: string;
+    imageUrl?: Nullable<string>;
+}
+
+export abstract class IQuery {
+    __typename?: 'IQuery';
+
+    abstract me(): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export class Translation {
+    __typename?: 'Translation';
+    value: string;
+    language: Language;
+    primary: boolean;
+}
+
+export class Language {
+    id: string;
+}
+
+type Nullable<T> = T | null;
