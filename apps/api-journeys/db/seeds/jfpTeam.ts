b39b65bf79fd8d3cbdcc4070e48711c433548b1e
Mike Allison <dj.mikeallison@gmail.com>
2023-04-24 21:02:28 +0000

    feat: seed update

--- apps/api-journeys/db/seeds/jfpTeam.ts
+++ apps/api-journeys/db/seeds/jfpTeam.ts
@@ -1,25 +1,37 @@
 import { aql } from 'arangojs'
+import { omit } from 'lodash'
+import { PrismaClient } from '.prisma/api-journeys-client'
 import { ArangoDB } from '../db'
 
+const prisma = new PrismaClient()
 const db = ArangoDB()
 
 // this should be removed when the UI can support team management
 export async function jfpTeam(): Promise<void> {
+  await prisma.$connect()
+  const data = {
+    title: 'Jesus Film Project',
+    contactEmail: 'sway.ciaramello@jesusfilm.org'
+  }
   // create JFP team (teams)
-  const team = await db.collection('teams').save(
-    {
-      _key: 'jfp-team',
-      title: 'Jesus Film Project',
-      contactEmail: 'sway.ciaramello@jesusfilm.org',
-      createdAt: new Date().toISOString()
+  const team = await prisma.team.upsert({
+    where: {
+      id: 'jfp-team'
     },
-    { overwriteMode: 'ignore' }
-  )
+    create: {
+      ...data,
+      id: 'jfp-team',
+      createdAt: new Date()
+    },
+    update: {
+      ...data
+    }
+  })
 
   // update all journeys to belong to JFP team (journeys)
   await db.query(aql`
     FOR journey IN journeys
-      UPDATE journey._key WITH { teamId: ${team._key} }
+      UPDATE journey._key WITH { teamId: ${team.id} }
       IN journeys
   `)
 
@@ -27,23 +39,75 @@ export async function jfpTeam(): Promise<void> {
   await db.query(aql`
     FOR userJourney IN userJourneys
       INSERT {
-        _key: CONCAT(userJourney.userId, ":", ${team._key}),
-        teamId: ${team._key}, 
+        _key: CONCAT(userJourney.userId, ":", ${team.id}),
+        teamId: ${team.id}, 
         userId: userJourney.userId, 
         createdAt: DATE_ISO8601(DATE_NOW())
       }
       INTO members OPTIONS { ignoreErrors: true }
   `)
 
-  // add all visitors to JFP team (visitors)
-  await db.query(aql`
-    FOR event IN events
-      INSERT {
-        _key: CONCAT(event.userId, ":", ${team._key}),
-        teamId: ${team._key}, 
-        userId: event.userId,
-        createdAt: DATE_ISO8601(DATE_NOW())
-      }
-      IN visitors OPTIONS { ignoreErrors: true }
+  // TODO: REMOVE once converted to postgresql
+  // import events from arangodb
+  let offset = 0
+  let end = true
+  do {
+    const events = await (
+      await db.query(aql`
+        FOR event IN events
+        LIMIT ${offset}, 50
+        RETURN event
+      `)
+    ).all()
+    for (let i = 0; i < events.length; i++) {
+      const event = events[i]
+      await prisma.event.create({
+        data: {
+          id: event._key,
+          journeyId: event.journeyId,
+          createdAt: new Date(event.createdAt),
+          label: event.label,
+          value: event.value,
+          visitorId: event.visitorId,
+          extra: omit(event, [
+            '_key',
+            'journeyId',
+            'createdAt',
+            'label',
+            'value',
+            'visitorId'
+          ])
+        }
+      })
+    }
+    offset += 50
+    end = events.length > 49
+  } while (end)
+
+  // TODO: REMOVE once converted to postgresql
+  // import visitors from arangodb
+  offset = 0
+  end = true
+  do {
+    const visitors = await (
+      await db.query(aql`
+      FOR visitor IN visitors
+      LIMIT ${offset}, 50
+      RETURN visitor
   `)
+    ).all()
+    for (let i = 0; i < visitors.length; i++) {
+      const visitor = visitors[i]
+      await prisma.visitor.create({
+        data: {
+          id: visitor._key,
+          teamId: visitor.teamId,
+          userId: visitor.userId,
+          createdAt: new Date(visitor.createdAt)
+        }
+      })
+    }
+    offset += 50
+    end = visitors.length > 49
+  } while (end)
 }
