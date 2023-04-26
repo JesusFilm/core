a746bf88bf55de400d4cfbea082779f5a03c77cb
Siyang <csiyang@yahoo.co.nz>
2023-04-26 04:49:58 +0000

    refactor: utils folder

--- apps/journeys-admin/src/components/VisitorInfo/JourneyList/TimelineEvent/TimelineEvent.tsx
+++ apps/journeys-admin/src/components/VisitorInfo/JourneyList/TimelineEvent/TimelineEvent.tsx
@@ -8,9 +8,11 @@ import MovieIcon from '@mui/icons-material/MovieRounded'
 import AnnouncementIcon from '@mui/icons-material/AnnouncementRounded'
 import EmojiFlagsRoundedIcon from '@mui/icons-material/EmojiFlagsRounded'
 import { useTranslation } from 'react-i18next'
-import { videoBlockSourceToLabel } from '../../videoBlockSourceToLabel' // duplicate into utils
-import { messagePlatformToLabel } from '../../messagePlatformToLabel' // duplicate into utils
-import { TimelineItem } from '../utils/transformEvents/transformEvents'
+import {
+  videoBlockSourceToLabel,
+  messagePlatformToLabel,
+  TimelineItem
+} from '../utils'
 import { GenericEvent } from './GenericEvent'
 
 interface Props {
