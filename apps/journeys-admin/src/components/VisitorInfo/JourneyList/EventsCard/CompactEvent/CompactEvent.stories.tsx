2a83d444ea341b66e84fc472ee425108d2f28c02
Siyang <csiyang@yahoo.co.nz>
2023-04-27 23:05:28 +0000

    test: compact events

---
+++ apps/journeys-admin/src/components/VisitorInfo/JourneyList/EventsCard/CompactEvent/CompactEvent.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react'
+import { ComponentProps } from 'react'
+import { noop } from 'lodash'
+import { simpleComponentConfig } from '../../../../../libs/storybook'
+import { CompactEvent } from './CompactEvent'
+
+const CompactEventStory = {
+  ...simpleComponentConfig,
+  title: 'Journeys-Admin/VisitorInfo/JourneyList/EventsCard/CompactEvent'
+}
+
+const Template: Story<ComponentProps<typeof CompactEvent>> = ({ ...args }) => (
+  <CompactEvent {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  value: '3 more cards',
+  onClick: noop
+}
+
+export default CompactEventStory as Meta
