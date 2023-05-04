eb85c0b9c0e9ae3e88a95c640de467203b0b4ea3
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-05-04 14:47:38 +1200

    feat: add journey visitors page (#1498)

---
+++ apps/journeys-admin/pages/journeys/[journeyId]/reports/visitors.tsx
@@ -0,0 +1,101 @@
+import {
+  useAuthUser,
+  AuthAction,
+  withAuthUser,
+  withAuthUserTokenSSR
+} from 'next-firebase-auth'
+import { useRouter } from 'next/router'
+import { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
+import { NextSeo } from 'next-seo'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
+import { PageWrapper } from '../../../../src/components/NewPageWrapper'
+import { useTermsRedirect } from '../../../../src/libs/useTermsRedirect'
+import { GetJourney } from '../../../../__generated__/GetJourney'
+import { GET_JOURNEY, USER_JOURNEY_OPEN } from '../../[journeyId]'
+import { UserInviteAcceptAll } from '../../../../__generated__/UserInviteAcceptAll'
+import i18nConfig from '../../../../next-i18next.config'
+import { createApolloClient } from '../../../../src/libs/apolloClient'
+import { ACCEPT_USER_INVITE } from '../../..'
+import { UserJourneyOpen } from '../../../../__generated__/UserJourneyOpen'
+
+function JourneyVisitorsPage(): ReactElement {
+  const { t } = useTranslation('apps-journeys-admin')
+  const AuthUser = useAuthUser()
+  const router = useRouter()
+
+  const journeyId = router.query.journeyId as string
+
+  useTermsRedirect()
+
+  return (
+    <>
+      <NextSeo title={t('Visitors')} />
+      <PageWrapper
+        title={t('Visitors')}
+        authUser={AuthUser}
+        backHref={`/journeys/${journeyId}/reports`}
+      >
+        Content
+      </PageWrapper>
+    </>
+  )
+}
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
+})(async ({ AuthUser, locale, query }) => {
+  const ldUser = {
+    key: AuthUser.id as string,
+    firstName: AuthUser.displayName ?? undefined,
+    email: AuthUser.email ?? undefined
+  }
+  const launchDarklyClient = await getLaunchDarklyClient(ldUser)
+  const flags = (await launchDarklyClient.allFlagsState(ldUser)).toJSON() as {
+    [key: string]: boolean | undefined
+  }
+
+  const token = await AuthUser.getIdToken()
+  const apolloClient = createApolloClient(token != null ? token : '')
+
+  await apolloClient.mutate<UserInviteAcceptAll>({
+    mutation: ACCEPT_USER_INVITE
+  })
+
+  try {
+    await apolloClient.query<GetJourney>({
+      query: GET_JOURNEY,
+      variables: {
+        id: query?.journeyId
+      }
+    })
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/journeys/${query?.journeyId as string}`
+      }
+    }
+  }
+
+  await apolloClient.mutate<UserJourneyOpen>({
+    mutation: USER_JOURNEY_OPEN,
+    variables: { id: query?.journeyId }
+  })
+
+  return {
+    props: {
+      flags,
+      ...(await serverSideTranslations(
+        locale ?? 'en',
+        ['apps-journeys-admin', 'libs-journeys-ui'],
+        i18nConfig
+      ))
+    }
+  }
+})
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN
+})(JourneyVisitorsPage)
