addc405fcb7c0a23b48c10f860f6991c9f1f4975
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-05-04 15:11:17 +1200

    refactor: navigation drawer use router (#1490)

--- apps/journeys-admin/pages/reports/visitors/[visitorId].tsx
+++ apps/journeys-admin/pages/reports/visitors/[visitorId].tsx
@@ -29,7 +29,6 @@ function SingleVisitorReportsPage(): ReactElement {
         title={t('Visitor Info')}
         backHref="/reports/visitors"
         authUser={AuthUser}
-        router={router}
       >
         <VisitorInfo id={router.query.visitorId as string} />
       </PageWrapper>
