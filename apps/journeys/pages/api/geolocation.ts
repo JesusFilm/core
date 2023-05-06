11e4c44e7ba542d11cf4b3787babf4f41335b850
Mike Allison <dj.mikeallison@gmail.com>
2023-05-06 02:05:15 +0000

    feat: prettier

--- apps/journeys/pages/api/geolocation.ts
+++ apps/journeys/pages/api/geolocation.ts
@@ -4,11 +4,9 @@ export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  res
-    .status(200)
-    .send({
-      country: req.headers['x-vercel-ip-country'],
-      region: req.headers['x-vercel-ip-country-region'],
-      city: req.headers['x-vercel-ip-city']
-    })
+  res.status(200).send({
+    country: req.headers['x-vercel-ip-country'],
+    region: req.headers['x-vercel-ip-country-region'],
+    city: req.headers['x-vercel-ip-city']
+  })
 }
