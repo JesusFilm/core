1587e5d5d8691eda64b6b2ff4db87d52dc6ce457
Siyang <csiyang@yahoo.co.nz>
2023-05-11 05:18:54 +0000

    feat: add drawer

--- apps/journeys-admin/src/components/JourneyVisitorsList/FilterDrawer/FilterDrawer.tsx
+++ apps/journeys-admin/src/components/JourneyVisitorsList/FilterDrawer/FilterDrawer.tsx
@@ -2,32 +2,88 @@ import Divider from '@mui/material/Divider'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { ReactElement } from 'react'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
 
 export function FilterDrawer(): ReactElement {
+  const locationOptions = [
+    'Dnipro, Ukraine',
+    'Halifax, Canada',
+    'Auckland, New Zealand'
+  ]
+
+  const sourceOptions = ['Facebook', 'Youtube', 'whatsApp']
+
   return (
     <>
       <Box sx={{ px: 6, py: 5 }}>
-        <Typography>Location</Typography>
-        <Typography>Source</Typography>
+        <Autocomplete
+          disablePortal
+          id="location-autocomplete"
+          options={locationOptions}
+          sx={{ width: '100%', mb: 5 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              aria-label="location"
+              variant="filled"
+              label="Location"
+            />
+          )}
+        />
+        <Autocomplete
+          disablePortal
+          id="source-autocomplete"
+          options={sourceOptions}
+          sx={{ width: '100%' }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              aria-label="source"
+              variant="filled"
+              label="Source"
+            />
+          )}
+        />
       </Box>
 
       <Divider />
 
       <Box sx={{ px: 6, py: 5 }}>
         <Typography variant="subtitle2">Categories</Typography>
-        <Typography>Chat Started</Typography>
-        <Typography>With Poll Answers</Typography>
-        <Typography>With Submitted Text</Typography>
-        <Typography>With Icon</Typography>
-        <Typography>Hide Inactive</Typography>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Chat Started" />
+          <FormControlLabel control={<Checkbox />} label="With Poll Answers" />
+          <FormControlLabel
+            control={<Checkbox />}
+            label="With Submitted Text"
+          />
+          <FormControlLabel control={<Checkbox />} label="With Icon" />
+          <FormControlLabel control={<Checkbox />} label="Hide Inactive" />
+        </FormGroup>
       </Box>
 
       <Divider />
 
       <Box sx={{ px: 6, py: 5 }}>
         <Typography variant="subtitle2">Sort By</Typography>
-        <Typography>Date</Typography>
-        <Typography>Duration</Typography>
+        <RadioGroup
+          aria-labelledby="journeys-sort-radio-buttons-group"
+          defaultValue="date"
+          name="journeys-sort-radio-group"
+        >
+          <FormControlLabel value="date" control={<Radio />} label="Date" />
+          <FormControlLabel
+            value="duration"
+            control={<Radio />}
+            label="Duration"
+          />
+        </RadioGroup>
       </Box>
     </>
   )
