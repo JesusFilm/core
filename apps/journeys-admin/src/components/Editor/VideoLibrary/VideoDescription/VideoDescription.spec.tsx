115cbc2aaa64c87e29db8dc38ac526e2445aa2e9
Kneesal <nisalcottingham@gmail.com>
2023-05-15 22:10:00 +0000

    fix: remove unused waitFor

--- apps/journeys-admin/src/components/Editor/VideoLibrary/VideoDescription/VideoDescription.spec.tsx
+++ apps/journeys-admin/src/components/Editor/VideoLibrary/VideoDescription/VideoDescription.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, waitFor } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { VideoDescription } from './VideoDescription'
 
 describe('VideoDescription', () => {
