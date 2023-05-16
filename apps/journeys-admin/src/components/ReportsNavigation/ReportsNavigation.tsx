e413a40a3e39eece7cb8b11ddd552cb2ed6b6389
Edmond Shen <egsmond@gmail.com>
2023-05-16 23:23:49 +0000

    feat: add report navigation

--- apps/journeys-admin/src/components/ReportsNavigation/ReportsNavigation.tsx
+++ apps/journeys-admin/src/components/ReportsNavigation/ReportsNavigation.tsx
@@ -1,25 +1,42 @@
 import Stack from '@mui/material/Stack'
 import { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
+import { JourneysReportType } from '../../../__generated__/globalTypes'
 import { NavigationButton } from './NavigationButton'
 
 interface Props {
+  reportType: JourneysReportType
+  journeyId?: string
   selected: 'journeys' | 'visitors'
 }
 
-export function ReportsNavigation({ selected }: Props): ReactElement {
+export function ReportsNavigation({
+  reportType,
+  journeyId,
+  selected
+}: Props): ReactElement {
   const { t } = useTranslation('journeys-admin')
   return (
     <Stack direction="row" spacing={4} sx={{ pb: 8 }}>
       <NavigationButton
         selected={selected === 'journeys'}
         value={t('Journeys')}
-        link="/reports/journeys"
+        link={
+          reportType === JourneysReportType.singleFull &&
+          journeyId !== undefined
+            ? `/journeys/${journeyId}/reports`
+            : '/reports/journeys'
+        }
       />
       <NavigationButton
         selected={selected === 'visitors'}
         value={t('Visitors')}
-        link="/reports/visitors"
+        link={
+          reportType === JourneysReportType.singleFull &&
+          journeyId !== undefined
+            ? `/journeys/${journeyId}/reports/visitors`
+            : '/reports/visitors'
+        }
       />
     </Stack>
   )
