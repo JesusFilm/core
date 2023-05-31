46ef1640ad093bc3c75596499893abe7b0192c77
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-05-31 14:05:15 +1200

    feat: journey visitors list (#1573)
    Co-authored-by: Mike Allison <dj.mikeallison@gmail.com>
    Co-authored-by: Mike Allison <mike@captainofbass.com>
    Co-authored-by: Edmond Shen <72168666+edmonday@users.noreply.github.com>
    Co-authored-by: Kneesal <nisalcottingham@gmail.com>
    Co-authored-by: Nisal Cottingham <114973713+Kneesal@users.noreply.github.com>

---
+++ apps/journeys-admin/src/components/JourneyVisitorsList/FilterDrawer/FilterDrawer.spec.tsx
@@ -0,0 +1,42 @@
+import { fireEvent, render } from '@testing-library/react'
+import { FilterDrawer } from './FilterDrawer'
+
+const props = {
+  chatStarted: false,
+  withPollAnswers: false,
+  withSubmittedText: false,
+  withIcon: false,
+  hideInteractive: false,
+  handleClearAll: jest.fn(),
+  handleChange: jest.fn((e) => e.target.value)
+}
+
+describe('FilterDrawer', () => {
+  it('calls handleClearAll when the clear all button is clicked', async () => {
+    const { getByText } = render(<FilterDrawer {...props} />)
+
+    fireEvent.click(getByText('Clear all'))
+    expect(props.handleClearAll).toHaveBeenCalled()
+  })
+  it('calls handleChange when checkboxes and radio buttons are selected', async () => {
+    const { handleChange } = props
+    const { getByText, getByRole } = render(<FilterDrawer {...props} />)
+
+    fireEvent.click(getByText('Chat Started'))
+    expect(handleChange).toReturnWith('Chat Started')
+    fireEvent.click(getByText('With Poll Answers'))
+    expect(handleChange).toReturnWith('With Poll Answers')
+    fireEvent.click(getByText('With Submitted Text'))
+    expect(handleChange).toReturnWith('With Submitted Text')
+    fireEvent.click(getByText('With Icon'))
+    expect(handleChange).toReturnWith('With Icon')
+    fireEvent.click(getByText('Hide Inactive'))
+    expect(handleChange).toReturnWith('Hide Inactive')
+    fireEvent.click(getByText('Duration'))
+    expect(handleChange).toReturnWith('duration')
+    expect(getByRole('radio', { name: 'Date' })).not.toBeChecked()
+    fireEvent.click(getByText('Date'))
+    expect(handleChange).toReturnWith('date')
+    expect(getByRole('radio', { name: 'Duration' })).not.toBeChecked()
+  })
+})
