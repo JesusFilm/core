c35e126a4aebdda0be1d92eb8c5cbeccd64a9067
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2023-06-02 15:35:24 +1200

    fix: journeys card layout (#1582)

--- libs/journeys/ui/src/components/Card/ExpandedCover/ExpandedCover.tsx
+++ libs/journeys/ui/src/components/Card/ExpandedCover/ExpandedCover.tsx
@@ -1,64 +1,86 @@
 import { ReactElement, ReactNode } from 'react'
-import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import { NextImage } from '@core/shared/ui/NextImage'
+import type { TreeBlock } from '../../../libs/block'
+import { ImageFields } from '../../Image/__generated__/ImageFields'
+import { OverlayContent } from '../OverlayContent'
 
 interface ExpandedCoverProps {
   children: ReactNode
+  imageBlock?: TreeBlock<ImageFields>
+  backgroundColor?: string
   backgroundBlur?: string
+  isVideoOnlyCard?: boolean
 }
 
 export function ExpandedCover({
   children,
-  backgroundBlur
+  imageBlock,
+  backgroundColor,
+  backgroundBlur,
+  isVideoOnlyCard = false
 }: ExpandedCoverProps): ReactElement {
+  const enableVerticalScroll = {
+    overflowY: 'scroll',
+    // Hide on Firefox https://caniuse.com/?search=scrollbar-width
+    scrollbarWidth: 'none',
+    // Hide on all others https://caniuse.com/?search=webkit-scrollbar
+    '&::-webkit-scrollbar': {
+      display: 'none'
+    }
+  }
+
   return (
-    <Box
-      data-testid="ExpandedCover"
-      sx={{
-        flexGrow: 1,
-        overflow: 'auto',
-        display: 'flex',
-        padding: (theme) => ({
-          xs: theme.spacing(7),
-          sm: theme.spacing(7, 10),
-          md: theme.spacing(10)
-        }),
-        borderRadius: (theme) => theme.spacing(4),
-        justifyContent: 'center'
-      }}
-    >
-      <Box
-        sx={{
-          margin: 'auto',
-          width: '100%',
-          maxWidth: 500,
-          zIndex: 1,
-          '& > *': {
-            '&:first-child': { mt: 0 },
-            '&:last-child': { mb: 0 }
-          }
-        }}
-      >
-        {children}
-      </Box>
-      {backgroundBlur != null && (
-        <Box
-          data-testid="expandedBlurBackground"
-          sx={{
-            position: 'absolute',
-            width: '100%',
-            height: '110%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '100% 100%',
-            backgroundPosition: '0% 0%',
-            left: 0,
-            top: '-10%',
-            zIndex: -1,
-            transform: 'scaleY(-1)',
-            backgroundBlendMode: 'hard-light',
-            backgroundImage: `url(${backgroundBlur}), url(${backgroundBlur})`
-          }}
+    <>
+      {/* Background Image */}
+      {backgroundBlur != null && imageBlock != null && (
+        <NextImage
+          data-testid="ExpandedImageCover"
+          src={imageBlock.src ?? backgroundBlur}
+          alt={imageBlock.alt}
+          placeholder="blur"
+          blurDataURL={backgroundBlur}
+          layout="fill"
+          objectFit="cover"
         />
       )}
-    </Box>
+      <Stack
+        data-testid="ExpandedCover"
+        sx={{
+          height: '100%',
+          WebkitBackdropFilter: 'blur(20px)',
+          backdropFilter: 'blur(20px)',
+          background:
+            backgroundColor != null ? `${backgroundColor}4d` : 'unset',
+          borderRadius: 'inherit',
+          overflow: 'hidden'
+        }}
+      >
+        <Stack
+          data-testid="overlay-content-container"
+          justifyContent="center"
+          sx={{
+            flexGrow: 1,
+            py: isVideoOnlyCard ? 0 : { xs: 9, lg: 8 },
+            ...enableVerticalScroll
+          }}
+        >
+          {isVideoOnlyCard ? (
+            children
+          ) : (
+            <OverlayContent
+              sx={{
+                margin: 'auto',
+                width: '100%',
+                maxWidth: { xs: 'calc(100% - 48px)', lg: 500 },
+                p: { xs: 2, lg: 'auto' }
+              }}
+            >
+              {children}
+            </OverlayContent>
+          )}
+        </Stack>
+      </Stack>
+    </>
   )
 }
