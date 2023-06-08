dda5f139e77939315fdb65edb24735b8994e58b5
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-06-08 22:36:59 +0000

    fix: add prisma service to req modules

--- apps/api-journeys/src/app/modules/userJourney/userJourney.module.ts
+++ apps/api-journeys/src/app/modules/userJourney/userJourney.module.ts
@@ -1,13 +1,14 @@
 import { Global, Module } from '@nestjs/common'
 import { DatabaseModule } from '@core/nest/database/DatabaseModule'
 import { JourneyService } from '../journey/journey.service'
+import { PrismaService } from '../../lib/prisma.service'
 import { UserJourneyService } from './userJourney.service'
 import { UserJourneyResolver } from './userJourney.resolver'
 
 @Global()
 @Module({
   imports: [DatabaseModule],
-  providers: [UserJourneyService, UserJourneyResolver, JourneyService],
+  providers: [UserJourneyService, UserJourneyResolver, JourneyService, PrismaService],
   exports: [UserJourneyService]
 })
 export class UserJourneyModule {}
