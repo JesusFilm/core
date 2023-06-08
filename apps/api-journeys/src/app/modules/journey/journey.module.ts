f4cce3169057e727ef8dc3b5ef7b96065283fa8f
Kneesal <nisalcottingham@gmail.com>
2023-06-08 23:08:27 +0000

    fix: resolve tatais comments

--- apps/api-journeys/src/app/modules/journey/journey.module.ts
+++ apps/api-journeys/src/app/modules/journey/journey.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '@core/nest/database/DatabaseModule'
 import { BlockService } from '../block/block.service'
 import { DateTimeScalar } from '../../lib/dateTime/dateTime.provider'
+import { PrismaService } from '../../lib/prisma.service'
 import { JourneyService } from './journey.service'
 import { JourneyResolver } from './journey.resolver'
 
 @Module({
   imports: [DatabaseModule],
-  providers: [JourneyService, JourneyResolver, BlockService, DateTimeScalar],
+  providers: [
+    JourneyService,
+    JourneyResolver,
+    BlockService,
+    DateTimeScalar,
+    PrismaService
+  ],
   exports: [JourneyService]
 })
 export class JourneyModule {}
