7d135d32fe02492206d11a6bf4f897436e10e205
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-06-09 12:09:04 +1200

    refactor: migrate user team to postgres (#1608)

--- apps/api-journeys/src/app/modules/block/textResponse/textResponse.resolver.spec.ts
+++ apps/api-journeys/src/app/modules/block/textResponse/textResponse.resolver.spec.ts
@@ -7,7 +7,7 @@ import {
   TextResponseBlockCreateInput
 } from '../../../__generated__/graphql'
 import { JourneyService } from '../../journey/journey.service'
-import { MemberService } from '../../member/member.service'
+import { PrismaService } from '../../../lib/prisma.service'
 import { UserJourneyService } from '../../userJourney/userJourney.service'
 import { UserRoleService } from '../../userRole/userRole.service'
 import { BlockResolver } from '../block.resolver'
@@ -82,7 +82,7 @@ describe('TextResponseBlockResolver', () => {
         UserJourneyService,
         UserRoleService,
         JourneyService,
-        MemberService,
+        PrismaService,
         {
           provide: 'DATABASE',
           useFactory: () => mockDeep<Database>()
