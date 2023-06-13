abb8e050d92e19a713e1b4ca4ac8880d8cdf6d92
John Geronimo <johnageronimo1@gmail.com>
2023-06-13 23:46:52 +0000

    feat: handle click

--- libs/journeys/ui/src/components/StepFooter/ChatButtons/ChatButtons.tsx
+++ libs/journeys/ui/src/components/StepFooter/ChatButtons/ChatButtons.tsx
@@ -3,9 +3,9 @@ import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
 import MessageTyping from '@core/shared/ui/icons/MessageTyping'
 import AddSquare4 from '@core/shared/ui/icons/AddSquare4'
-import { styled } from '@mui/material/styles'
 import { gql, useMutation } from '@apollo/client'
 import { useJourney } from '../../../libs/JourneyProvider'
+import { useBlocks } from '../../../libs/block'
 import { ChatButtonEventCreate } from './__generated__/ChatButtonEventCreate'
 
 export const CHAT_BUTTON_EVENT_CREATE = gql`
@@ -23,46 +23,67 @@ interface ChatButton {
   platform: string
 }
 
-const StyledIconButton = styled(IconButton)(({ theme }) => ({
-  backgroundColor: theme.palette.grey[100],
-  '&:hover': {
-    backgroundColor: theme.palette.grey[100]
-  }
-}))
-
 export function ChatButtons(): ReactElement {
   const { admin } = useJourney()
+  const { activeBlock } = useBlocks()
 
-  // const [chatButtonEventCreate] = useMutation<ChatButtonEventCreate>(
-  //   CHAT_BUTTON_EVENT_CREATE
-  // )
+  const [chatButtonEventCreate] = useMutation<ChatButtonEventCreate>(
+    CHAT_BUTTON_EVENT_CREATE
+  )
 
   // update to be using journeys.chatButtons
   const chatButtons: ChatButton[] = [
-    { id: '1', link: 'm.me/user', platform: 'facebook' }
+    { id: '1', link: 'https://m.me/', platform: 'facebook' },
+    { id: '2', link: 'https://google.com', platform: 'facebook' }
   ]
 
   const handleClick = (chatButton: ChatButton): void => {
     if (!admin) {
       window.open(chatButton.link, '_blank')
-      // trigger chat event and gtm event
+      void chatButtonEventCreate({
+        variables: {
+          input: {
+            id: chatButton?.id,
+            blockId: activeBlock?.id,
+            stepId: activeBlock?.id,
+            value: chatButton?.platform
+          }
+        }
+      })
     }
   }
 
+  // TODO:
+  // add unit tests
+  // fix chat going out of card issue
+
   return (
-    <Stack direction="row" gap={3}>
-      {chatButtons.map((chatButton) => (
-        <StyledIconButton
+    <Stack direction="row-reverse" gap={3}>
+      {chatButtons.map((chatButton, index) => (
+        <IconButton
           key={chatButton?.id}
           onClick={() => handleClick(chatButton)}
+          sx={{
+            backgroundColor: (theme) =>
+              index === 0 ? theme.palette.grey[100] : theme.palette.grey[700],
+            '&:hover': {
+              backgroundColor: (theme) =>
+                index === 0 ? theme.palette.grey[100] : theme.palette.grey[700]
+            }
+          }}
         >
-          <MessageTyping sx={{ color: 'secondary.dark' }} />
-        </StyledIconButton>
+          <MessageTyping
+            sx={{
+              color: (theme) =>
+                index === 0 ? theme.palette.grey[900] : theme.palette.grey[100]
+            }}
+          />
+        </IconButton>
       ))}
       {admin && chatButtons.length === 0 && (
-        <StyledIconButton key="default">
+        <IconButton key="default">
           <AddSquare4 sx={{ color: 'secondary.dark' }} />
-        </StyledIconButton>
+        </IconButton>
       )}
     </Stack>
   )
