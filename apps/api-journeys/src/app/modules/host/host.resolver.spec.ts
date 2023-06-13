b6e2f9fd37fcd4e8d971c68857a6d6398df93c12
Kneesal <nisalcottingham@gmail.com>
2023-06-13 03:08:46 +0000

    fix: update tests and group by resolvers

--- apps/api-journeys/src/app/modules/host/host.resolver.spec.ts
+++ apps/api-journeys/src/app/modules/host/host.resolver.spec.ts
@@ -28,120 +28,133 @@ describe('HostResolver', () => {
     prismaService = module.get<PrismaService>(PrismaService)
   })
 
-  it('should return an array of hosts', async () => {
-    const userId = 'user-1'
-    const teamId = 'edmondshen'
-    const mockHosts = [
-      {
+  describe('hostCreate', () => {
+    it('should create a new host', async () => {
+      const teamId = 'team-id'
+      const input = {
+        title: 'New Host',
+        location: 'Location',
+        avatar1Id: 'avatar1',
+        avatar2Id: 'avatar2'
+      }
+      const mockHost = { teamId, ...input, id: 'hostid' }
+      jest.spyOn(prismaService.host, 'create').mockResolvedValue(mockHost)
+
+      const result = await hostResolver.hostCreate(teamId, input)
+
+      expect(result).toEqual(mockHost)
+      expect(prismaService.host.create).toHaveBeenCalledWith({
+        data: { teamId, ...input }
+      })
+    })
+  })
+
+  describe('hostResolver', () => {
+    it('should return an array of hosts', async () => {
+      const userId = 'user-1'
+      const teamId = 'edmondshen'
+      const mockHosts = [
+        {
+          id: 'host-id',
+          teamId: 'edmond-shen-fans',
+          title: 'Edmond Shen & Nisal Cottingham',
+          location: 'New Zealand',
+          avatar1Id: 'avatar1-id',
+          avatar2Id: 'vatar2-id'
+        },
+        {
+          id: 'host-id2',
+          teamId: 'best-juniors-engineers-gang',
+          title: 'Edmond Shen & Nisal Cottingham',
+          location: 'New Zealand',
+          avatar1Id: 'avatar1-id',
+          avatar2Id: 'avatar2-id'
+        }
+      ]
+      jest.spyOn(prismaService.host, 'findMany').mockResolvedValue(mockHosts)
+
+      const result = await hostResolver.hosts(userId, teamId)
+
+      expect(result).toEqual(mockHosts)
+      expect(prismaService.host.findMany).toHaveBeenCalledWith({
+        where: { teamId, team: { userTeams: { some: { userId } } } }
+      })
+    })
+  })
+
+  describe('hostUpdate', () => {
+    it('should update an existing host', async () => {
+      const id = 'host-id'
+      const input = {
+        title: 'Edmond Shen',
+        location: 'National Team Staff',
+        avatar1Id: 'new-profile-pic-who-thos',
+        avatar2Id: 'new-avatar2'
+      }
+      const mockHost = {
         id: 'host-id',
-        teamId: 'edmond-shen-fans',
-        title: 'Edmond Shen & Nisal Cottingham',
-        location: 'New Zealand',
-        avatar1Id: 'avatar1-id',
-        avatar2Id: 'vatar2-id'
-      },
-      {
-        id: 'host-id2',
         teamId: 'best-juniors-engineers-gang',
         title: 'Edmond Shen & Nisal Cottingham',
-        location: 'New Zealand',
+        location: 'JFP Staff',
         avatar1Id: 'avatar1-id',
         avatar2Id: 'avatar2-id'
       }
-    ]
-    jest.spyOn(prismaService.host, 'findMany').mockResolvedValue(mockHosts)
+      const mockUpdatedHost = { ...mockHost, ...input }
+      jest.spyOn(prismaService.host, 'findUnique').mockResolvedValue(mockHost)
+      jest
+        .spyOn(prismaService.host, 'update')
+        .mockResolvedValue(mockUpdatedHost)
 
-    const result = await hostResolver.hosts(userId, teamId)
+      const result = await hostResolver.hostUpdate(id, input)
 
-    expect(result).toEqual(mockHosts)
-    expect(prismaService.host.findMany).toHaveBeenCalledWith({
-      where: { teamId, team: { userTeams: { some: { userId } } } }
+      expect(result).toEqual(mockUpdatedHost)
+
+      expect(prismaService.host.update).toHaveBeenCalledWith({
+        where: { id },
+        data: {
+          title: input.title,
+          location: input.location,
+          avatar1Id: input.avatar1Id,
+          avatar2Id: input.avatar2Id
+        }
+      })
     })
-  })
 
-  it('should create a new host', async () => {
-    const teamId = 'team-id'
-    const input = {
-      title: 'New Host',
-      location: 'Location',
-      avatar1Id: 'avatar1',
-      avatar2Id: 'avatar2'
-    }
-    const mockHost = { teamId, ...input, id: 'hostid' }
-    jest.spyOn(prismaService.host, 'create').mockResolvedValue(mockHost)
-
-    const result = await hostResolver.hostCreate(teamId, input)
-
-    expect(result).toEqual(mockHost)
-    expect(prismaService.host.create).toHaveBeenCalledWith({
-      data: { teamId, ...input }
-    })
-  })
-
-  it('should update an existing host', async () => {
-    const id = 'host-id'
-    const input = {
-      title: 'Edmond Shen',
-      location: 'National Team Staff',
-      avatar1Id: 'new-profile-pic-who-thos',
-      avatar2Id: 'new-avatar2'
-    }
-    const mockHost = {
-      id: 'host-id',
-      teamId: 'best-juniors-engineers-gang',
-      title: 'Edmond Shen & Nisal Cottingham',
-      location: 'JFP Staff',
-      avatar1Id: 'avatar1-id',
-      avatar2Id: 'avatar2-id'
-    }
-    const mockUpdatedHost = { ...mockHost, ...input }
-    jest.spyOn(prismaService.host, 'findUnique').mockResolvedValue(mockHost)
-    jest.spyOn(prismaService.host, 'update').mockResolvedValue(mockUpdatedHost)
-
-    const result = await hostResolver.hostUpdate(id, input)
-
-    expect(result).toEqual(mockUpdatedHost)
-
-    expect(prismaService.host.update).toHaveBeenCalledWith({
-      where: { id },
-      data: {
-        title: input.title,
-        location: input.location,
-        avatar1Id: input.avatar1Id,
-        avatar2Id: input.avatar2Id
+    it('should throw UserInputError when host title is undefined', async () => {
+      const hostId = 'host-id'
+      const input = {
+        title: undefined as unknown as string,
+        location: 'National Team Staff',
+        avatar1Id: 'new-profile-pic-who-thos',
+        avatar2Id: 'new-avatar2'
       }
+      await hostResolver
+        .hostUpdate(hostId, input)
+        .catch((e) =>
+          expect(e.message).toEqual('host title cannot be undefined or null')
+        )
     })
   })
-  it('should delete an existing host', async () => {
-    const id = 'host-id'
-    const mockDeletedHost = {
-      id: 'host-id',
-      teamId: 'best-juniors-engineers-gang',
-      title: 'Edmond Shen & Nisal Cottingham',
-      location: 'JFP Staff',
-      avatar1Id: 'avatar1-id',
-      avatar2Id: 'avatar2-id'
-    }
-    jest.spyOn(prismaService.host, 'delete').mockResolvedValue(mockDeletedHost)
 
-    const result = await hostResolver.hostDelete(id)
+  describe('hostDelete', () => {
+    it('should delete an existing host', async () => {
+      const id = 'host-id'
+      const mockDeletedHost = {
+        id: 'host-id',
+        teamId: 'best-juniors-engineers-gang',
+        title: 'Edmond Shen & Nisal Cottingham',
+        location: 'JFP Staff',
+        avatar1Id: 'avatar1-id',
+        avatar2Id: 'avatar2-id'
+      }
+      jest
+        .spyOn(prismaService.host, 'delete')
+        .mockResolvedValue(mockDeletedHost)
 
-    expect(result).toEqual(mockDeletedHost)
-    expect(prismaService.host.delete).toHaveBeenCalledWith({ where: { id } })
-  })
+      const result = await hostResolver.hostDelete(id)
 
-  it('should throw UserInputError when host title is undefined', async () => {
-    const hostId = 'host-id'
-    const input = {
-      title: undefined as unknown as string,
-      location: 'National Team Staff',
-      avatar1Id: 'new-profile-pic-who-thos',
-      avatar2Id: 'new-avatar2'
-    }
-    await hostResolver
-      .hostUpdate(hostId, input)
-      .catch((e) =>
-        expect(e.message).toEqual('host title cannot be undefined or null')
-      )
+      expect(result).toEqual(mockDeletedHost)
+      expect(prismaService.host.delete).toHaveBeenCalledWith({ where: { id } })
+    })
   })
 })
