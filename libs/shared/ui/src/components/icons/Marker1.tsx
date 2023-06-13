6c5d0ac3be2048b136fbb8192fb9653089992987
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2023-06-13 14:16:21 +1200

    feat: stratis icons (#1593)

---
+++ libs/shared/ui/src/components/icons/Marker1.tsx
@@ -0,0 +1,10 @@
+import { createSvgIcon } from '@mui/material/utils'
+
+export default createSvgIcon(
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M4.322 2.4a1 1 0 0 1 1-1h15.86a1 1 0 0 1 .895 1.448l-2.28 4.561 2.28 4.562a1 1 0 0 1-.894 1.447H6.322V20.6h1.504a1 1 0 1 1 0 2H2.817a1 1 0 1 1 0-2h1.505V2.4Zm2 9.018h13.243l-1.781-3.562a1 1 0 0 1 0-.894l1.78-3.562H6.323v8.018Z"
+  />,
+  'Marker1'
+)
