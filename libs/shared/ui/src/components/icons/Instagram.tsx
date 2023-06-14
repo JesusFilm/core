8f99a34fc84c601659c2bb7511ffc48253fbc69a
John Geronimo <johnageronimo1@gmail.com>
2023-06-14 00:58:20 +0000

    feat: initial chat button icons commit

---
+++ libs/shared/ui/src/components/icons/Instagram.tsx
@@ -0,0 +1,18 @@
+import { createSvgIcon } from '@mui/material/utils'
+
+export default createSvgIcon(
+  <>
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M18 2.4A3.6 3.6 0 0 1 21.6 6v12a3.6 3.6 0 0 1-3.6 3.6H6A3.6 3.6 0 0 1 2.4 18V6A3.6 3.6 0 0 1 6 2.4h12Z"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"
+    />
+    <circle cx="18" cy="7" r="1" fill="#C52D3A" />
+  </>,
+  'Instagram'
+)
