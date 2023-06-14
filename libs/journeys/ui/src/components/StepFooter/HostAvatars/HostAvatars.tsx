42840adce729220d7b093136a76296141df8883a
Kneesal <nisalcottingham@gmail..com>
2023-06-14 20:21:58 +0000

    fix: add correct icons

--- libs/journeys/ui/src/components/StepFooter/HostAvatars/HostAvatars.tsx
+++ libs/journeys/ui/src/components/StepFooter/HostAvatars/HostAvatars.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 import Avatar from '@mui/material/Avatar'
 import AvatarGroup from '@mui/material/AvatarGroup'
-import UserProfileCircle from '@core/shared/ui/icons/UserProfileCircle'
+import UserProfile3 from '@core/shared/ui/icons/UserProfile3'
 
 interface HostAvatarsProps {
   src1?: string
@@ -23,28 +23,57 @@ export const HostAvatars = ({
         </AvatarGroup>
       )}
       {src1 == null && src2 == null && admin ? (
-        <UserProfileCircle
-          data-testid="account-circled-out-icon"
+        <Avatar
           sx={{
             height: '44px',
             width: '44px',
-            color: 'secondary.light'
+            color: 'secondary.light',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            outline: 'none',
+            border: '3px dashed #6D6D7D'
           }}
-        />
+        >
+          <UserProfile3
+            data-testid="account-circled-out-icon"
+            sx={{
+              pl: '6px',
+              pt: '6px',
+              height: '34px',
+              width: '34px',
+              color: 'secondary.light'
+            }}
+          />
+        </Avatar>
       ) : (src1 == null || src2 == null) && admin ? (
         <AvatarGroup
           spacing="small"
           data-testid="journeys-admin-render-one-avatar"
         >
           <Avatar src={src1 == null && src2 != null ? src2 : src1} />
-          <UserProfileCircle
-            data-testid="account-circled-out-icon"
+          <Avatar
             sx={{
-              height: '44px',
-              width: '44px',
-              color: 'secondary.light'
+              color: 'secondary.light',
+              backgroundColor: 'rgba(0, 0, 0, 0)',
+              outline: 'none',
+              border: '3px dashed #6D6D7D',
+              '&.MuiAvatar-root': {
+                border: '3px dashed #6D6D7D',
+                height: '36px',
+                width: '36px'
+              }
             }}
-          />
+          >
+            <UserProfile3
+              data-testid="account-circled-out-icon"
+              sx={{
+                pl: '6px',
+                pt: '6px',
+                height: '34px',
+                width: '34px',
+                color: 'secondary.light'
+              }}
+            />
+          </Avatar>
         </AvatarGroup>
       ) : (
         admin && (
