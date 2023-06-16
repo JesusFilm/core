c83b9f5c263caa207171bf3dfed3d6a46b5c6e06
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-06-16 04:12:31 +0000

    feat: add user team endpoints

---
+++ apps/api-journeys/src/app/modules/userTeam/userTeam.resolver.ts
@@ -0,0 +1,109 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent
+} from '@nestjs/graphql'
+import { UseGuards } from '@nestjs/common'
+import { GqlAuthGuard } from '@core/nest/gqlAuthGuard/GqlAuthGuard'
+import { UserTeam, Prisma } from '.prisma/api-journeys-client'
+import { subject } from '@casl/ability'
+import { ForbiddenError } from 'apollo-server-errors'
+import { GraphQLError } from 'graphql'
+import {
+  CaslAbility,
+  CaslAccessible,
+  CaslGuard
+} from '@core/nest/common/CaslAuthModule'
+import { PrismaService } from '../../lib/prisma.service'
+import { Action, AppAbility } from '../../lib/casl/caslFactory'
+import { UserTeamUpdateInput } from '../../__generated__/graphql'
+
+@Resolver('UserTeam')
+export class UserTeamResolver {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Query()
+  @UseGuards(GqlAuthGuard, CaslGuard)
+  async userTeams(
+    @CaslAccessible('UserTeam') accessibleUserTeams: Prisma.TeamWhereInput,
+    @Args('teamId') teamId: string
+  ): Promise<UserTeam[]> {
+    return await this.prismaService.userTeam.findMany({
+      where: {
+        AND: [accessibleUserTeams, { teamId }]
+      }
+    })
+  }
+
+  @Query()
+  @UseGuards(GqlAuthGuard, CaslGuard)
+  async userTeam(
+    @CaslAbility() ability: AppAbility,
+    @Args('id') id: string
+  ): Promise<UserTeam> {
+    const userTeam = await this.prismaService.userTeam.findFirst({
+      where: { id },
+      include: { team: { include: { userTeams: true } } }
+    })
+    if (userTeam == null)
+      throw new GraphQLError('userTeam not found', {
+        extensions: { code: 'NOT_FOUND' }
+      })
+    if (ability.can(Action.Read, subject('UserTeam', userTeam))) return userTeam
+    throw new ForbiddenError('user is not allowed to view userTeam')
+  }
+
+  @Mutation()
+  @UseGuards(GqlAuthGuard, CaslGuard)
+  async userTeamUpdate(
+    @CaslAbility() ability: AppAbility,
+    @Args('id') id: string,
+    @Args('input') data: UserTeamUpdateInput
+  ): Promise<UserTeam> {
+    const userTeam = await this.prismaService.userTeam.findFirst({
+      where: { id },
+      include: { team: { include: { userTeams: true } } }
+    })
+    if (userTeam == null)
+      throw new GraphQLError('userTeam not found', {
+        extensions: { code: 'NOT_FOUND' }
+      })
+    if (ability.can(Action.Update, subject('UserTeam', userTeam)))
+      return await this.prismaService.userTeam.update({
+        where: { id },
+        data
+      })
+    throw new ForbiddenError('user is not allowed to update userTeam')
+  }
+
+  @Mutation()
+  @UseGuards(GqlAuthGuard, CaslGuard)
+  async userTeamDelete(
+    @CaslAbility() ability: AppAbility,
+    @Args('id') id: string
+  ): Promise<UserTeam> {
+    const userTeam = await this.prismaService.userTeam.findFirst({
+      where: { id },
+      include: { team: { include: { userTeams: true } } }
+    })
+    if (userTeam == null)
+      throw new GraphQLError('userTeam not found', {
+        extensions: { code: 'NOT_FOUND' }
+      })
+    if (ability.can(Action.Delete, subject('UserTeam', userTeam)))
+      return await this.prismaService.userTeam.delete({
+        where: { id }
+      })
+    throw new ForbiddenError('user is not allowed to delete userTeam')
+  }
+
+  @ResolveField('user')
+  async user(
+    @Parent() userTeam: UserTeam
+  ): Promise<{ __typename: string; id: string }> {
+    return { __typename: 'User', id: userTeam.userId }
+  }
+}
