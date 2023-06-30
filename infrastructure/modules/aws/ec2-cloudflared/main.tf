2726dd58792ecc6172775e420c97c65589585a9a
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-06-30 04:31:06 +0000

    fix: use value for ssm param

--- infrastructure/modules/aws/ec2-cloudflared/main.tf
+++ infrastructure/modules/aws/ec2-cloudflared/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "ec2_cloudflared" {
   ami                         = "ami-02af4904e34687a9e"
   instance_type               = "t2.nano"
   associate_public_ip_address = true
@@ -6,6 +6,6 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids      = var.security_group_ids
   user_data = templatefile("${path.module}/startup.sh", {
     cloudflared_token = var.cloudflared_token
-    datadog_api_key   = data.aws_ssm_parameter.datadog_api_key
+    datadog_api_key   = data.aws_ssm_parameter.datadog_api_key.value
   })
 }
