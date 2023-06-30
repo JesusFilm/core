12644b41962e686d37f9a0c1e50fe44c4acc33cc
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-06-30 16:37:17 +1200

    feat: cloudflared logging to datadog (#1667)

--- infrastructure/modules/aws/ec2-cloudflared/startup.sh
+++ infrastructure/modules/aws/ec2-cloudflared/startup.sh
@@ -3,6 +3,9 @@
 # install curl
 apt-get update && apt-get install -y curl
 
+# install datadog agent
+DD_API_KEY="${datadog_api_key}" DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
+
 # Add cloudflare gpg key
 mkdir -p --mode=0755 /usr/share/keyrings
 curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
