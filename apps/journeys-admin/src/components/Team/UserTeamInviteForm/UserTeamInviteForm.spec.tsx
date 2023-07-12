6b341ff16ebd49ad0f84b56fb703e70570056793
Kneesal <nisalcottingham@gmail.com>
2023-07-12 23:51:14 +0000

    fix: adjust tests and lint

--- apps/journeys-admin/src/components/Team/UserTeamInviteForm/UserTeamInviteForm.spec.tsx
+++ apps/journeys-admin/src/components/Team/UserTeamInviteForm/UserTeamInviteForm.spec.tsx
@@ -19,6 +19,20 @@ jest.mock('react-i18next', () => ({
     }
   }
 }))
+
+// jest.mock('../../../libs/useCurrentUser', () => ({
+//   __esModule: true,
+//   useCurrentUser: jest.fn().mockReturnValue({
+//     loadUser: jest.fn(),
+//     data: {
+//       __typename: 'User',
+//       ...user1
+//     }
+//   })
+// }))
+
+// const user1 = { id: 'userId', email: 'siyangguccigang@example.com' }
+
 describe('UserTeamInviteForm', () => {
   const getTeams: MockedResponse<GetTeams> = {
     request: {
@@ -195,18 +209,18 @@ describe('UserTeamInviteForm', () => {
       >
         <TeamProvider>
           <TeamManageWrapper>
-            {({ UserTeamList, UserTeamInviteList, UserTeamInviteForm }) => (
+            {({ userTeamList, userTeamInviteList, userTeamInviteForm }) => (
               <>
-                {UserTeamList}
-                {UserTeamInviteList}
-                {UserTeamInviteForm}
+                {userTeamList}
+                {userTeamInviteList}
+                {userTeamInviteForm}
               </>
             )}
           </TeamManageWrapper>
         </TeamProvider>
       </MockedProvider>
     )
-    const email = getByRole('textbox', { name: 'Email' })
+    getByRole('textbox', { name: 'Email' })
     await waitFor(() => {
       fireEvent.change(email, {
         target: { value: 'johnTHEgeronimo@example.com' }
