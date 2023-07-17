2189bab937da1b8429727d6d77c251d9e76051e6
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-07-17 20:59:44 +0000

    fix: change path matching logic

--- apps/journeys-admin/middleware.ts
+++ apps/journeys-admin/middleware.ts
@@ -4,13 +4,14 @@ import { get } from '@vercel/edge-config'
 export const config = {
   matcher: [
     /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (Next static content)
+     * 5. all root files inside /public (e.g. /favicon.ico)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|site.webmanifest|favicon-16x16.png).*)',
+    '/((?!api/|_next/|_static/|[\\w-]+\\.\\w+).*)',
+    // Ensure the root path is matched.
     '/'
   ]
 }
@@ -18,7 +19,7 @@ export const config = {
 export async function middleware(
   req: NextRequest
 ): Promise<NextResponse | undefined> {
-  let maintenanceMode = false
+  let maintenanceMode = true
   try {
     maintenanceMode = (await get('maintenanceMode')) ?? maintenanceMode
   } catch {}
