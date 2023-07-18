eb91bb6c257decd8fee6c4d93111b526c0a69809
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-18 14:56:01 +1200

    test: sample test

--- apps/journeys-e2e/tests/example.spec.ts
+++ apps/journeys-e2e/tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:4100/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:4100/');
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
