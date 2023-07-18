1529630bbb8c6ed9536bb1b53d20f7bf7728ea87
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-07-18 21:09:55 +0000

    fix: remove migration

--- apps/api-journeys/db/migrations/20230711035550_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `UserRole` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "UserRole_userId_key" ON "UserRole"("userId");
