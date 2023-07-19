9bb3d0eaf5962eb9353b376ef2b7036dc0aa9937
John Geronimo <johnageronimo1@gmail.com>
2023-07-19 03:22:09 +0000

    fix: getting admin to run

--- apps/journeys-admin/src/components/Editor/Canvas/VideoWrapper/VideoWrapper.tsx
+++ apps/journeys-admin/src/components/Editor/Canvas/VideoWrapper/VideoWrapper.tsx
@@ -3,6 +3,7 @@ import { Video } from '@core/journeys/ui/Video'
 import type { WrapperProps } from '@core/journeys/ui/BlockRenderer'
 
 export function VideoWrapper({ block }: WrapperProps): ReactElement {
+  // getting admin to run
   return block.__typename === 'VideoBlock' ? (
     <Video
       {...{
