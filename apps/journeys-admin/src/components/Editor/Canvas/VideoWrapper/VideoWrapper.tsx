1c8f77bd6c1541cceb3563239935abb51fd2ad9b
John Geronimo <johnageronimo1@gmail.com>
2023-07-19 03:28:45 +0000

    fix: remove comment

--- apps/journeys-admin/src/components/Editor/Canvas/VideoWrapper/VideoWrapper.tsx
+++ apps/journeys-admin/src/components/Editor/Canvas/VideoWrapper/VideoWrapper.tsx
@@ -3,7 +3,6 @@ import { Video } from '@core/journeys/ui/Video'
 import type { WrapperProps } from '@core/journeys/ui/BlockRenderer'
 
 export function VideoWrapper({ block }: WrapperProps): ReactElement {
-  // getting admin to run
   return block.__typename === 'VideoBlock' ? (
     <Video
       {...{
