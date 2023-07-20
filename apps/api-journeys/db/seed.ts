912eee900f1f059a4f8861ca1e0b7b63b6929acd
John Geronimo <johnageronimo1@gmail.com>
2023-07-20 23:32:45 +0000

    feat: discovery journeys seed data

--- apps/api-journeys/db/seed.ts
+++ apps/api-journeys/db/seed.ts
@@ -8,6 +8,9 @@ import { nua9 } from './seeds/nua9'
 import { jfpTeam } from './seeds/jfpTeam'
 import { onboarding } from './seeds/onboarding'
 import { onboardingTemplates } from './seeds/onboardingTemplates'
+import { vision } from './seeds/vision'
+import { howTo } from './seeds/howTo'
+import { feedback } from './seeds/feedback'
 
 async function main(): Promise<void> {
   // this should be removed when the UI can support team management
@@ -19,6 +22,9 @@ async function main(): Promise<void> {
   await nua9()
   await onboarding()
   await onboardingTemplates()
+  await vision()
+  await howTo()
+  await feedback()
 }
 main().catch((e) => {
   console.error(e)
