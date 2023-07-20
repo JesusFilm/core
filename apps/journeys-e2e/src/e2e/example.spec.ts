494be339eed1aa6d8cf2ade2fdb621b247b6e0a0
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-20 03:21:03 +0000

    test: delete watch & journeys admin and watch as they got cypress

--- apps/journeys-e2e/src/e2e/example.spec.ts
+++ apps/journeys-e2e/src/e2e/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+  await page.goto('/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/)
 })
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+  await page.goto('/')
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click()
