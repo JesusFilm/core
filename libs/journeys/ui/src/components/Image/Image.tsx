23a482be436fef8dd740d79061c919e01695d182
John Geronimo <johnageronimo1@gmail.com>
2023-07-21 00:15:05 +0000

    fix: testing lazy loading image

--- libs/journeys/ui/src/components/Image/Image.tsx
+++ libs/journeys/ui/src/components/Image/Image.tsx
@@ -40,6 +40,7 @@ export function Image({
           blurDataURL={placeholderSrc ?? src}
           layout="responsive"
           objectFit="cover"
+          loading="eager"
         />
       ) : (
         <Paper
