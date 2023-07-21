e406283e810cd31b5727f2edf3fbc43f632a7d45
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-07-21 21:06:05 +0000

    fix: visitor endpoints to use acls

--- apps/api-journeys/src/app/lib/casl/caslFactory/caslFactory.ts
+++ apps/api-journeys/src/app/lib/casl/caslFactory/caslFactory.ts
@@ -9,6 +9,7 @@ import { userTeamAcl } from '../../../modules/userTeam/userTeam.acl'
 import { journeyAcl } from '../../../modules/journey/journey.acl'
 import { hostAcl } from '../../../modules/host/host.acl'
 import { userTeamInviteAcl } from '../../../modules/userTeamInvite/userTeamInvite.acl'
+import { visitorAcl } from '../../../modules/visitor/visitor.acl'
 
 export enum Action {
   Manage = 'manage',
@@ -40,7 +41,14 @@ export class AppCaslFactory extends CaslFactory<Role> {
     const { can, cannot, build } = new AbilityBuilder<AppAbility>(
       createPrismaAbility
     )
-    const acls = [hostAcl, journeyAcl, teamAcl, userTeamAcl, userTeamInviteAcl]
+    const acls = [
+      hostAcl,
+      journeyAcl,
+      teamAcl,
+      userTeamAcl,
+      userTeamInviteAcl,
+      visitorAcl
+    ]
     acls.forEach((acl) => acl({ can, cannot, user }))
 
     return build()
