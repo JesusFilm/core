4218f4edac1254af95ab768d3c05a6fc5dae0c05
John Geronimo <johnageronimo1@gmail.com>
2023-07-25 03:08:20 +0000

    fix: updated urls

--- apps/journeys-admin/lighthouserc.js
+++ apps/journeys-admin/lighthouserc.js
@@ -6,7 +6,9 @@ module.exports = {
       url: [
         DEPLOYMENT_URL,
         `${DEPLOYMENT_URL}/journeys/e08ef53a-be61-4758-8d1f-a461d6e34e7a`,
-        `'${DEPLOYMENT_URL}/journeys/e08ef53a-be61-4758-8d1f-a461d6e34e7a/edit?stepId=728f91b2-57a0-4e0e-b753-a5bc07af02b3`
+        `${DEPLOYMENT_URL}/journeys/e08ef53a-be61-4758-8d1f-a461d6e34e7a/edit?stepId=728f91b2-57a0-4e0e-b753-a5bc07af02b3`,
+        `${DEPLOYMENT_URL}/templates`,
+        `${DEPLOYMENT_URL}/reports`
       ],
       numberOfRuns: 1,
       startServerCommand: 'nx serve journeys-admin'
--- apps/journeys/lighthouserc.js
+++ apps/journeys/lighthouserc.js
@@ -3,7 +3,11 @@ const DEPLOYMENT_URL = process.env.DEPLOYMENT_URL || 'http://localhost:4100/'
 module.exports = {
   ci: {
     collect: {
-      url: [DEPLOYMENT_URL, `${DEPLOYMENT_URL}/fact-or-fiction`],
+      url: [
+        DEPLOYMENT_URL,
+        `${DEPLOYMENT_URL}/fact-or-fiction`,
+        `${DEPLOYMENT_URL}/embed/fact-or-fiction`
+      ],
       numberOfRuns: 1,
       startServerCommand: 'nx serve journeys'
     },
