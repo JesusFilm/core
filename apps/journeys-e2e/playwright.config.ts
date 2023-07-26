7bfaf13d94fd06fb6f9ee1d7311c444c3b843490
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-26 05:18:24 +0000

    test: set correct env

--- apps/journeys-e2e/playwright.config.ts
+++ apps/journeys-e2e/playwright.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'http://localhost:4100',
+    /* Use BASE_URL from env if exists */
+    baseURL: process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:4100',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
