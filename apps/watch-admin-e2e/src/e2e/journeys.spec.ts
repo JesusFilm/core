a14bb91db247242d90d049df6fea6d141b6ef890
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-28 03:48:25 +0000

    test: add sample test in each e2e folder

--- apps/watch-admin-e2e/src/e2e/journeys.spec.ts
+++ apps/watch-admin-e2e/src/e2e/journeys.spec.ts
@@ -1,19 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('journeys e2e test', async ({ page }) => {
-  // 10 mins timeout for this test as it got videos (later we can use 10 seconds videos)
-  test.setTimeout(600000)
+test('sample watch-admin e2e test', async ({ page }) => {
   await page.goto('/')
 
-  await page.click('a[href="/fact-or-fiction"]')
+  // Get and log the current URL
+  const url = await page.url();
+  console.log('Current URL:', url);
 
-  // Test that user actually navigated to the choosen journey
-  await expect(page).toHaveURL(/.*fact-or-fiction/)
-  await page.getByRole('button', { name: 'Explore Now' }).click()
-  await page.getByText('Yes, it’s a true story', { exact: false }).click()
-  await page.getByText('One question remains', { exact: false }).click()
-  await page.getByText('The Son of God').click()
-
-  // Test that app automatically navigated user to second journey upon completion of first journey
-  await expect(page).toHaveURL(/.*what-about-the-resurrection/)
+  // Test the URL
+  await expect(page).toHaveURL('watch-admin')
 })
