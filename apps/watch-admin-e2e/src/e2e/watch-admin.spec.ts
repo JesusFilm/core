cc392c39ef2ff7d933f67567a43c0c1e9d0c990b
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-28 04:39:45 +0000

    test: add sample playwright test

--- apps/watch-admin-e2e/src/e2e/watch-admin.spec.ts
+++ apps/watch-admin-e2e/src/e2e/watch-admin.spec.ts
@@ -1,12 +1,9 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test('sample watch-admin e2e test', async ({ page }) => {
-  await page.goto('/')
-
-  // Get and log the current URL
-  const url = await page.url();
-  console.log('Current URL:', url);
-
-  // Test the URL
-  await expect(page).toHaveURL(/.*watch-admin/);
-})
+// Sample playwright test for now
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveURL(/.*play/);
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
