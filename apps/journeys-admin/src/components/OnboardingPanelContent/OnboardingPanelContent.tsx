d6d82c78e00ebcc5b8e99b16f32f7a324f92d0cf
John Geronimo <johnageronimo1@gmail.com>
2023-07-31 03:22:50 +0000

    fix: dependency cyle issues

--- apps/journeys-admin/src/components/OnboardingPanelContent/OnboardingPanelContent.tsx
+++ apps/journeys-admin/src/components/OnboardingPanelContent/OnboardingPanelContent.tsx
@@ -16,7 +16,14 @@ import { ContainedIconButton } from '../ContainedIconButton'
 import { useJourneyCreate } from '../../libs/useJourneyCreate/useJourneyCreate'
 import { useTeam } from '../Team/TeamProvider'
 import { GetOnboardingJourneys_onboardingJourneys as OnboardingJourneys } from '../../../__generated__/GetOnboardingJourneys'
-import { onboardingIds } from '../../../pages'
+
+const onboardingIds = [
+  '014c7add-288b-4f84-ac85-ccefef7a07d3',
+  'c4889bb1-49ac-41c9-8fdb-0297afb32cd9',
+  'e978adb4-e4d8-42ef-89a9-79811f10b7e9',
+  '178c01bd-371c-4e73-a9b8-e2bb95215fd8',
+  '13317d05-a805-4b3c-b362-9018971d9b57'
+]
 
 interface OnboardingPanelContentProps {
   onboardingJourneys: OnboardingJourneys[]
