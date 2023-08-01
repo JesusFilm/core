97e4a411e4e89fa258125293f412c3d9c05ff268
Siyang <csiyang@yahoo.co.nz>
2023-08-01 10:55:04 +0000

    fix: add invites accept

--- apps/journeys-admin/pages/users/terms-and-conditions.tsx
+++ apps/journeys-admin/pages/users/terms-and-conditions.tsx
@@ -7,9 +7,12 @@ import {
 import { NextSeo } from 'next-seo'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'react-i18next'
+import { initAndAuthApp } from '../../src/libs/initAndAuthApp'
 
 import { TermsAndConditions } from '../../src/components/TermsAndConditions'
 import i18nConfig from '../../next-i18next.config'
+import { AcceptAllInvites } from '../../__generated__/AcceptAllInvites'
+import { ACCEPT_ALL_INVITES } from '..'
 
 function TermsAndConditionsPage(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
@@ -23,7 +26,16 @@ function TermsAndConditionsPage(): ReactElement {
 
 export const getServerSideProps = withAuthUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
-})(async ({ locale }) => {
+})(async ({ AuthUser, locale }) => {
+  const { apolloClient } = await initAndAuthApp({
+    AuthUser,
+    locale
+  })
+
+  await apolloClient.mutate<AcceptAllInvites>({
+    mutation: ACCEPT_ALL_INVITES
+  })
+
   return {
     props: {
       ...(await serverSideTranslations(
