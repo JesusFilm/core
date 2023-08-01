0afef7202639a0a151f17da204b23cbb2651ce05
Siyang <csiyang@yahoo.co.nz>
2023-08-01 09:48:33 +0000

    chore: add i18n module mock

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/Action/Action.spec.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/Action/Action.spec.tsx
@@ -11,6 +11,15 @@ import {
 import { Action, NAVIGATE_ACTION_UPDATE, ACTION_DELETE } from './Action'
 import { steps } from './data'
 
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str
+    }
+  }
+}))
+
 describe('Action', () => {
   const selectedStep = steps[1]
   const selectedBlock = selectedStep?.children[0].children[3]
