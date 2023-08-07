43bc17bf36d5ea7e668212dd7aa16d28553ee383
Siyang <csiyang@yahoo.co.nz>
2023-08-07 21:38:46 +0000

    chore: remove redundant comment

--- apps/journeys/pages/_document.tsx
+++ apps/journeys/pages/_document.tsx
@@ -56,10 +56,6 @@ export default class MyDocument extends Document<{
           <link rel="manifest" href="/site.webmanifest" />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
-          {/* <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
-          /> */}
         </Head>
         <body>
           <Main />
