4c5de8aef5e80f89374f7e32c8b42112eb521c6d
Siyang <csiyang@yahoo.co.nz>
2023-08-07 01:50:32 +0000

    refactor: blocks into own page

--- apps/journeys/pages/404.tsx
+++ apps/journeys/pages/404.tsx
@@ -3,15 +3,100 @@ import { ReactElement } from 'react'
 import { transformer } from '@core/journeys/ui/transformer'
 import { useTranslation } from 'react-i18next'
 import { Conductor } from '../src/components/Conductor'
-import { ThemeName, ThemeMode } from '../__generated__/globalTypes'
-import { getErrorJourneyBlocks } from '../libs/getErrorJourneyBlocks'
+import { GetJourney_journey_blocks as Block } from '../__generated__/GetJourney'
+import {
+  ThemeName,
+  ThemeMode,
+  TypographyAlign,
+  TypographyVariant,
+  ButtonColor,
+  ButtonVariant,
+  ButtonSize,
+  IconName
+} from '../__generated__/globalTypes'
 
 export function Custom404(): ReactElement {
   const { t } = useTranslation('apps-journeys')
-  const blocks = getErrorJourneyBlocks(
-    t('404'),
-    t('This Journey is not available.')
-  )
+  const blocks: Block[] = [
+    {
+      __typename: 'StepBlock',
+      id: '404-stepBlock-id',
+      locked: false,
+      nextBlockId: null,
+      parentBlockId: null,
+      parentOrder: 0
+    },
+    {
+      __typename: 'CardBlock',
+      backgroundColor: '#30313D',
+      coverBlockId: '404-imageBlock-id',
+      fullscreen: false,
+      id: '404-cardBlock-id',
+      parentBlockId: '404-stepBlock-id',
+      parentOrder: 0,
+      themeMode: null,
+      themeName: null
+    },
+    {
+      __typename: 'ImageBlock',
+      alt: 'error-404-image',
+      blurhash: 'U05OKJ0300sq5O?Y~VM|0M-.%1%K~o9HEKxu',
+      height: 2230,
+      id: '404-imageBlock-id',
+      parentBlockId: '404-cardBlock-id',
+      parentOrder: null,
+      src: 'https://images.unsplash.com/photo-1522030865324-4062412a2023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3244&q=80',
+      width: 3244
+    },
+    {
+      __typename: 'TypographyBlock',
+      align: TypographyAlign.center,
+      color: null,
+      content: t('404'),
+      id: '404-typog-id',
+      parentBlockId: '404-cardBlock-id',
+      parentOrder: 0,
+      variant: TypographyVariant.h2
+    },
+    {
+      __typename: 'TypographyBlock',
+      align: TypographyAlign.center,
+      color: null,
+      content: t('This Journey is not available.'),
+      id: 'typog2-id',
+      parentBlockId: '404-cardBlock-id',
+      parentOrder: 1,
+      variant: TypographyVariant.h5
+    },
+    {
+      __typename: 'ButtonBlock',
+      buttonColor: ButtonColor.primary,
+      buttonVariant: ButtonVariant.contained,
+      endIconId: null,
+      id: '404-button-id',
+      label: 'See Other Journeys',
+      parentBlockId: '404-cardBlock-id',
+      parentOrder: 2,
+      size: ButtonSize.large,
+      startIconId: '404-icon-id',
+      action: {
+        gtmEventName: null,
+        parentBlockId: '404-button-id',
+        url: '/',
+        __typename: 'LinkAction'
+      }
+    },
+    {
+      __typename: 'IconBlock',
+      iconColor: null,
+      iconName: IconName.SubscriptionsRounded,
+      iconSize: null,
+      id: '404-icon-id',
+      parentBlockId: '404-button-id',
+      parentOrder: null
+    }
+  ]
+
   return (
     <ThemeProvider themeName={ThemeName.base} themeMode={ThemeMode.dark}>
       <Conductor blocks={transformer(blocks)} />
--- apps/journeys/pages/500.tsx
+++ apps/journeys/pages/500.tsx
@@ -3,15 +3,101 @@ import { transformer } from '@core/journeys/ui/transformer'
 import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 import { useTranslation } from 'react-i18next'
 import { Conductor } from '../src/components/Conductor'
-import { ThemeName, ThemeMode } from '../__generated__/globalTypes'
-import { getErrorJourneyBlocks } from '../libs/getErrorJourneyBlocks'
+import { GetJourney_journey_blocks as Block } from '../__generated__/GetJourney'
+import {
+  ThemeName,
+  ThemeMode,
+  TypographyAlign,
+  TypographyVariant,
+  ButtonColor,
+  ButtonVariant,
+  ButtonSize,
+  IconName
+} from '../__generated__/globalTypes'
 
 export function Custom500(): ReactElement {
   const { t } = useTranslation('apps-journeys')
-  const blocks = getErrorJourneyBlocks(
-    t('500'),
-    t('Oops! Something went wrong.')
-  )
+
+  const blocks: Block[] = [
+    {
+      __typename: 'StepBlock',
+      id: '500-stepBlock-id',
+      locked: false,
+      nextBlockId: null,
+      parentBlockId: null,
+      parentOrder: 0
+    },
+    {
+      __typename: 'CardBlock',
+      backgroundColor: '#30313D',
+      coverBlockId: '500-imageBlock-id',
+      fullscreen: false,
+      id: '500-cardBlock-id',
+      parentBlockId: '500-stepBlock-id',
+      parentOrder: 0,
+      themeMode: null,
+      themeName: null
+    },
+    {
+      __typename: 'ImageBlock',
+      alt: 'error-500-image',
+      blurhash: 'U05OKJ0300sq5O?Y~VM|0M-.%1%K~o9HEKxu',
+      height: 2230,
+      id: '500-imageBlock-id',
+      parentBlockId: '500-cardBlock-id',
+      parentOrder: null,
+      src: 'https://images.unsplash.com/photo-1522030865324-4062412a2023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3244&q=80',
+      width: 3244
+    },
+    {
+      __typename: 'TypographyBlock',
+      align: TypographyAlign.center,
+      color: null,
+      content: t('500'),
+      id: '500-typog-id',
+      parentBlockId: '500-cardBlock-id',
+      parentOrder: 0,
+      variant: TypographyVariant.h2
+    },
+    {
+      __typename: 'TypographyBlock',
+      align: TypographyAlign.center,
+      color: null,
+      content: t('Oops! Something went wrong.'),
+      id: 'typog2-id',
+      parentBlockId: '500-cardBlock-id',
+      parentOrder: 1,
+      variant: TypographyVariant.h5
+    },
+    {
+      __typename: 'ButtonBlock',
+      buttonColor: ButtonColor.primary,
+      buttonVariant: ButtonVariant.contained,
+      endIconId: null,
+      id: '500-button-id',
+      label: 'See Other Journeys',
+      parentBlockId: '500-cardBlock-id',
+      parentOrder: 2,
+      size: ButtonSize.large,
+      startIconId: '500-icon-id',
+      action: {
+        gtmEventName: null,
+        parentBlockId: '500-button-id',
+        url: '/',
+        __typename: 'LinkAction'
+      }
+    },
+    {
+      __typename: 'IconBlock',
+      iconColor: null,
+      iconName: IconName.SubscriptionsRounded,
+      iconSize: null,
+      id: '500-icon-id',
+      parentBlockId: '500-button-id',
+      parentOrder: null
+    }
+  ]
+
   return (
     <ThemeProvider themeName={ThemeName.base} themeMode={ThemeMode.dark}>
       <Conductor blocks={transformer(blocks)} />
