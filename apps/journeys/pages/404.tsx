d7857fd7b9a2e85e17a5bc7d56a8587c377146e9
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-08-07 17:04:40 +1200

    fix: add 404 page (#1781)

--- apps/journeys/pages/404.tsx
+++ apps/journeys/pages/404.tsx
@@ -1,6 +1,6 @@
+import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 import { ReactElement } from 'react'
 import { transformer } from '@core/journeys/ui/transformer'
-import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 import { useTranslation } from 'react-i18next'
 import { Conductor } from '../src/components/Conductor'
 import { GetJourney_journey_blocks as Block } from '../__generated__/GetJourney'
@@ -15,13 +15,12 @@ import {
   IconName
 } from '../__generated__/globalTypes'
 
-export function Custom500(): ReactElement {
+export function Custom404(): ReactElement {
   const { t } = useTranslation('apps-journeys')
-
   const blocks: Block[] = [
     {
       __typename: 'StepBlock',
-      id: '500-stepBlock-id',
+      id: '404-stepBlock-id',
       locked: false,
       nextBlockId: null,
       parentBlockId: null,
@@ -30,21 +29,21 @@ export function Custom500(): ReactElement {
     {
       __typename: 'CardBlock',
       backgroundColor: '#30313D',
-      coverBlockId: '500-imageBlock-id',
+      coverBlockId: '404-imageBlock-id',
       fullscreen: false,
-      id: '500-cardBlock-id',
-      parentBlockId: '500-stepBlock-id',
+      id: '404-cardBlock-id',
+      parentBlockId: '404-stepBlock-id',
       parentOrder: 0,
       themeMode: null,
       themeName: null
     },
     {
       __typename: 'ImageBlock',
-      alt: 'error-500-image',
+      alt: 'error-404-image',
       blurhash: 'U05OKJ0300sq5O?Y~VM|0M-.%1%K~o9HEKxu',
       height: 2230,
-      id: '500-imageBlock-id',
-      parentBlockId: '500-cardBlock-id',
+      id: '404-imageBlock-id',
+      parentBlockId: '404-cardBlock-id',
       parentOrder: null,
       src: 'https://images.unsplash.com/photo-1522030865324-4062412a2023?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3244&q=80',
       width: 3244
@@ -53,9 +52,9 @@ export function Custom500(): ReactElement {
       __typename: 'TypographyBlock',
       align: TypographyAlign.center,
       color: null,
-      content: t('500'),
-      id: '500-typog-id',
-      parentBlockId: '500-cardBlock-id',
+      content: t('404'),
+      id: '404-typog-id',
+      parentBlockId: '404-cardBlock-id',
       parentOrder: 0,
       variant: TypographyVariant.h2
     },
@@ -63,9 +62,9 @@ export function Custom500(): ReactElement {
       __typename: 'TypographyBlock',
       align: TypographyAlign.center,
       color: null,
-      content: t('Oops! Something went wrong.'),
+      content: t('This Journey is not available.'),
       id: 'typog2-id',
-      parentBlockId: '500-cardBlock-id',
+      parentBlockId: '404-cardBlock-id',
       parentOrder: 1,
       variant: TypographyVariant.h5
     },
@@ -74,15 +73,15 @@ export function Custom500(): ReactElement {
       buttonColor: ButtonColor.primary,
       buttonVariant: ButtonVariant.contained,
       endIconId: null,
-      id: '500-button-id',
+      id: '404-button-id',
       label: 'See Other Journeys',
-      parentBlockId: '500-cardBlock-id',
+      parentBlockId: '404-cardBlock-id',
       parentOrder: 2,
       size: ButtonSize.large,
-      startIconId: '500-icon-id',
+      startIconId: '404-icon-id',
       action: {
         gtmEventName: null,
-        parentBlockId: '500-button-id',
+        parentBlockId: '404-button-id',
         url: '/',
         __typename: 'LinkAction'
       }
@@ -92,8 +91,8 @@ export function Custom500(): ReactElement {
       iconColor: null,
       iconName: IconName.SubscriptionsRounded,
       iconSize: null,
-      id: '500-icon-id',
-      parentBlockId: '500-button-id',
+      id: '404-icon-id',
+      parentBlockId: '404-button-id',
       parentOrder: null
     }
   ]
@@ -105,4 +104,4 @@ export function Custom500(): ReactElement {
   )
 }
 
-export default Custom500
+export default Custom404
