e01015a22370f589b40ccb2c53e3e320fbe2862e
Mike Allison <dj.mikeallison@gmail.com>
2023-08-18 20:14:34 +0000

    feat: react 18 round 2

--- apps/api-videos/src/app/modules/video/video.resolver.spec.ts
+++ apps/api-videos/src/app/modules/video/video.resolver.spec.ts
@@ -1,4 +1,3 @@
-import {} from '@nestjs/common'
 import { CacheModule } from '@nestjs/cache-manager'
 import { Test, TestingModule } from '@nestjs/testing'
 import { GraphQLResolveInfo, Kind } from 'graphql'
