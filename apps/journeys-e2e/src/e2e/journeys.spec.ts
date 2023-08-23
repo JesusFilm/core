4c29b1c218cc735361b321da0719495e2d62f462
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-08-23 07:39:30 +0000

    test: fix linting

--- apps/journeys-e2e/src/e2e/journeys.spec.ts
+++ apps/journeys-e2e/src/e2e/journeys.spec.ts
@@ -8,33 +8,50 @@ test('journeys', async ({ page }) => {
 
   await page.goto('/')
   // disable annimation before taking screenshot
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
 
   // fact or fiction page - click on on fact or fiction
   await page.click('a[href="/fact-or-fiction"]')
   // test that user actually navigated to the choosen journey
   await expect(page).toHaveURL(/.*fact-or-fiction/)
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
   await page.getByRole('button', { name: 'Explore Now' }).click()
 
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
   await page.getByText('Yes, it’s a true story', { exact: false }).click()
 
   await page.getByText('One question remains', { exact: false }).click()
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
 
   await page.getByText('The Son of God').click()
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
 
   // Test that app automatically navigated user to second journey upon completion of first journey
   await expect(page).toHaveURL(/.*what-about-the-resurrection/)
-  await expect(page).toHaveScreenshot({animations: "disabled", fullPage: true });
+  await expect(page).toHaveScreenshot({
+    animations: 'disabled',
+    fullPage: true
+  })
 })
 
 /* 
 Test a journey by following right arrow
 */
-
 
 /* 
 Test a journey by following left arrow
