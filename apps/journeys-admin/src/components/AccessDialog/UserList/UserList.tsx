8481c955b97c118ec5a41a315cae142c94f04094
Nisal Cottingham <114973713+Kneesal@users.noreply.github.com>
2023-08-25 11:09:42 +1200

    feat: show team members in access dialog (#1822)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

--- apps/journeys-admin/src/components/AccessDialog/UserList/UserList.tsx
+++ apps/journeys-admin/src/components/AccessDialog/UserList/UserList.tsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography'
 import sortBy from 'lodash/sortBy'
 import { ReactElement, useMemo } from 'react'
 
-import { GetJourneyWithUserJourneys_journey_userJourneys as UserJourney } from '../../../../__generated__/GetJourneyWithUserJourneys'
+import { GetJourneyWithPermissions_journey_userJourneys as UserJourney } from '../../../../__generated__/GetJourneyWithPermissions'
 import { GetUserInvites_userInvites as UserInvite } from '../../../../__generated__/GetUserInvites'
 import { UserJourneyRole } from '../../../../__generated__/globalTypes'
 
