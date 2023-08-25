1b2de517bf3ed2d4a4263fe4e3b0aad71e94cfb9
DanChae03 <danchae03@gmail.com>
2023-08-25 04:27:41 +0000

    test: add test id to all videos button

--- apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.tsx
+++ apps/watch/src/components/HomePage/SeeAllVideos/SeeAllVideos.tsx
@@ -5,13 +5,12 @@ import { ReactElement } from 'react'
 
 export function SeeAllVideos(): ReactElement {
   return (
-    <Stack sx={{ pt: '54px', alignItems: 'center' }}>
+    <Stack sx={{ pt: '54px', alignItems: 'center' }} data-testid = "AllVideosButton">
       <Link href="/videos" passHref>
         <Button
           size="small"
           color="secondary"
           variant="outlined"
-          aria-label="all-videos-button"
           sx={{
             width: '15%',
             whiteSpace: 'nowrap'
