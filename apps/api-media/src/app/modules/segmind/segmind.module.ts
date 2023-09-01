1fcefc0c351ec4d92f766f53369b140f6f3a87bb
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-09-01 03:07:53 +0000

    fix: upload image fix

--- apps/api-media/src/app/modules/segmind/segmind.module.ts
+++ apps/api-media/src/app/modules/segmind/segmind.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 
 import { PrismaService } from '../../lib/prisma.service'
+import { ImageService } from '../cloudflare/image/image.service'
 
 import { SegmindResolver } from './segmind.resolver'
 import { SegmindService } from './segmind.service'
 
 @Module({
   imports: [],
-  providers: [SegmindResolver, PrismaService, SegmindService],
+  providers: [SegmindResolver, ImageService, PrismaService, SegmindService],
   exports: [SegmindService]
 })
 export class SegmindModule {}
