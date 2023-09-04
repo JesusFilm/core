8421039d55b60706af1fd04305cbd32218ebd38e
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-09-04 17:37:21 +1200

    test: wait 30 secs

--- apps/watch-e2e/src/e2e/watch.spec.ts
+++ apps/watch-e2e/src/e2e/watch.spec.ts
@@ -15,13 +15,13 @@ test('Test single video', async ({ page }) => {
   // 3 mins timeout for this test as it got videos (later we can use 10 seconds videos)
   test.setTimeout(3 * 60 * 1000)
 
-  await page.goto('/')
+  await page.goto('https://watch-1841-jesusfilm.vercel.app/watch')
 
   // Get and log the current URL
   const url = page.url()
   console.log('Current URL:', url)
 
-  // Wait for 30 seconds before taking screenshot without this all the video tiles are not fully loading
+  // Wait for 30 seconds before taking screenshot to load the page fully
   // eslint-disable-next-line
   await page.waitForTimeout(30 * 1000)
   await expect(page).toHaveScreenshot('home-page.png', {
