8926cb9d5bfd35ed95c29e2dbd897735544bea2f
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-09-05 03:11:33 +0000

    fix: echo affected array

--- tools/scripts/affected-frontend-apps.ts
+++ tools/scripts/affected-frontend-apps.ts
@@ -3,9 +3,8 @@ process.stdin.setEncoding('utf8')
 process.stdin.on('data', (data: string) => {
   const services = data
     .split('\n')
-    .filter((service) => !service.startsWith('api-'))
+    .filter((value) => value != null && value !== '')
     .join('","')
-    .replace(/\n/g, '')
   if (services === '') {
     console.log('matrix=[]')
   } else {
