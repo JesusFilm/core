d6ae79f6038fa3ecbf7d41d10303cd6d30429c03
Kneesal <nisalcottingham@gmail.com>
2023-09-05 03:47:58 +0000

    fix: add pagination dots to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 const { initialize: mswInitialize, mswLoader } = require('msw-storybook-addon')
 const { MockedProvider } = require('@apollo/client/testing')
 require('swiper/swiper.min.css')
+require('swiper/components/pagination/pagination.min.css')
 
 const customViewports = {
   mobileMin: {
