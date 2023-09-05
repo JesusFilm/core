2594c04ba488aaf41b9c8e3351668ffc225085bc
Kneesal <nisalcottingham@gmail.com>
2023-09-05 21:29:23 +0000

    fix: center journeys index logo

--- apps/journeys/pages/index.tsx
+++ apps/journeys/pages/index.tsx
@@ -48,7 +48,8 @@ function JourneysPage({ journeys }: JourneysPageProps): ReactElement {
               width={152}
               style={{
                 maxWidth: '100%',
-                height: 'auto'
+                height: 'auto',
+                alignSelf: 'center'
               }}
             />
             <Box>
