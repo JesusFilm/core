72e2dae98d82adb0d5675c3a329b7568e61d02ef
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-09-06 06:57:31 +0000

    test: add direct url to test the end 2 end tests

--- apps/docs-e2e/src/e2e/docs.spec.ts
+++ apps/docs-e2e/src/e2e/docs.spec.ts
@@ -5,7 +5,7 @@ This is just a sample test
 Test that 'docs' part of the URL
 */
 test('sample docs e2e test', async ({ page }) => {
-  await page.goto('/')
+  await page.goto('https://docs.core.jesusfilm.org/')
 
   // Get and log the current URL
   const url = page.url()
