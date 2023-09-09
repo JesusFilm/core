cfa1be4f6eef0b375d338b04943744b44a6a0f1f
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-09-09 17:44:27 +1200

    test: try to generate watch snaps

--- apps/watch-e2e/src/e2e/watch.spec.ts
+++ apps/watch-e2e/src/e2e/watch.spec.ts
@@ -12,8 +12,8 @@ Wait for two minutes as this video is for 1.59 minutes
 Take screenshot - so it will be tested all the times
 */
 test('Test single video', async ({ page }) => {
-  // 3 mins timeout for this test as it got videos (later we can use 10 seconds videos)
-  test.setTimeout(3 * 60 * 1000)
+  // 8 mins timeout for this test as it got videos (later we can use 10 seconds videos)
+  test.setTimeout(8 * 60 * 1000)
 
   await page.goto('https://www.jesusfilm.org/watch')
 
@@ -21,6 +21,9 @@ test('Test single video', async ({ page }) => {
   const url = page.url()
   console.log('Current URL:', url)
 
+  // Screenshot test are not stable - so give a hard wait and try
+  // eslint-disable-next-line
+  await page.waitForTimeout(1 * 60 * 1000)  
   // Note: all video tiles are not fully loading
   await expect(page).toHaveScreenshot('home-page.png', {
     animations: 'disabled',
@@ -34,6 +37,9 @@ test('Test single video', async ({ page }) => {
     })
     .click()
 
+  // Screenshot test are not stable - so give a hard wait and try
+  // eslint-disable-next-line
+  await page.waitForTimeout(1 * 60 * 1000)    
   await expect(page).toHaveScreenshot('before-video.png', {
     animations: 'disabled',
     fullPage: true
@@ -41,10 +47,10 @@ test('Test single video', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Play' }).click()
 
-  // wait for 2 minutes to see if the video is complete - a quick way of finding without writing much code for now
+  // wait for 3 minutes to see if the video is complete - a quick way of finding without writing much code for now
   // later find a way to check if the video is complete and check if the video is playing. Also use 10 seconds video
   // eslint-disable-next-line
-  await page.waitForTimeout(2 * 60 * 1000)
+  await page.waitForTimeout(3 * 60 * 1000)
 
   // Take screenshot once video is played and test it is same all the times
   await expect(page).toHaveScreenshot('after-video.png', {
