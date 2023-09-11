4e1fa49a8e54fe06c9ef0ee93039b796546cd892
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-09-11 09:37:41 +0000

    test: use local host

--- apps/watch-e2e/playwright.config.ts
+++ apps/watch-e2e/playwright.config.ts
@@ -24,9 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Use URL that has been set part of app-deploy.yml */
-    baseURL:
-      process.env.DEPLOYMENT_URL ??
-      'https://watch-1846-jesusfilm.vercel.app/watch',
+    baseURL: process.env.DEPLOYMENT_URL ?? 'http://localhost:4100',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
