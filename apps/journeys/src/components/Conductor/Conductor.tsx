dec61812aae0edfb96512a8a4ac86af2d7d36990
Siyang <csiyang@yahoo.co.nz>
2023-09-14 00:43:38 +0000

    test: manually add space

--- apps/journeys/src/components/Conductor/Conductor.tsx
+++ apps/journeys/src/components/Conductor/Conductor.tsx
@@ -112,7 +112,7 @@ export function Conductor({ blocks }: ConductorProps): ReactElement {
               (data: { city?: string; country?: string; region?: string }) => {
                 const countryCodes: string[] = []
                 if (data.city != null)
-                  countryCodes.push(decodeURIComponent(data.city))
+                  countryCodes.push(data.city.replace(/%20/g, ' '))
                 if (data.region != null) countryCodes.push(data.region)
                 if (data.country != null) countryCodes.push(data.country)
 
