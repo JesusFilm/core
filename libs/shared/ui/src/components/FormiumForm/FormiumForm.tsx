a34b88934790fb305d853b348b9a87ae56e3488b
Siyang <csiyang@yahoo.co.nz>
2023-09-18 09:21:17 +0000

    feat: custom components

--- libs/shared/ui/src/components/FormiumForm/FormiumForm.tsx
+++ libs/shared/ui/src/components/FormiumForm/FormiumForm.tsx
@@ -5,13 +5,57 @@ import {
   FormiumComponents,
   defaultComponents
 } from '@formium/react'
-import { TextInputProps } from '@formium/react/dist/inputs'
+import {
+  CheckboxProps,
+  RadioGroupProps,
+  TextInputProps,
+  TextareaProps
+} from '@formium/react/dist/inputs'
 import { Form } from '@formium/types'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import MuiCheckbox from '@mui/material/Checkbox'
+import MuiFormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormLabel from '@mui/material/FormLabel'
+import Paper from '@mui/material/Paper'
+import Radio from '@mui/material/Radio'
+import MuiRadioGroup from '@mui/material/RadioGroup'
+import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
 import { ReactElement } from 'react'
 
 function TextInput(props: TextInputProps): ReactElement {
-  return <TextField label="Outlined" variant="outlined" {...props} />
+  return <TextField variant="outlined" fullWidth {...props} />
+}
+
+function Textarea(props: TextareaProps): ReactElement {
+  return <TextField variant="outlined" fullWidth multiline {...props} />
+}
+
+function Checkbox(props: CheckboxProps): ReactElement {
+  return <MuiCheckbox {...props} />
+}
+
+function RadioGroup({
+  disabled,
+  id,
+  name,
+  onChange,
+  onBlur,
+  options
+}: RadioGroupProps): ReactElement {
+  return (
+    <MuiFormControl disabled={disabled}>
+      <FormLabel id={id}>{name}</FormLabel>
+      <MuiRadioGroup name={name} onChange={onChange} onBlur={onBlur}>
+        {options.map((option) => (
+          <FormControlLabel {...option} control={<Radio />} />
+        ))}
+      </MuiRadioGroup>
+    </MuiFormControl>
+  )
 }
 
 function FormControl({
@@ -23,18 +67,85 @@ function FormControl({
 }: FormControlProps): ReactElement {
   return (
     <div>
-      {/* {label && <label htmlFor={labelFor}>{label}</label>} */}
-      {/* {description && <div>{description}</div>} */}
+      {label != null && <Typography variant="subtitle1">{label}</Typography>}
+      {description != null && (
+        <Typography variant="body1">{description}</Typography>
+      )}
       {children}
-      {/* {error && <div>{error}</div>} */}
+      {error != null && (
+        <Typography variant="body1" color="error">
+          {error}
+        </Typography>
+      )}
     </div>
+  )
+}
+
+function PageWrapper({ children }: { children: ReactElement }): ReactElement {
+  return <Paper sx={{ minWidth: '600px' }}>{children}</Paper>
+}
+
+function ElementsWrapper({
+  children
+}: {
+  children: ReactElement
+}): ReactElement {
+  return (
+    <Stack
+      spacing={10}
+      sx={{
+        m: 4
+      }}
+    >
+      {children}
+    </Stack>
+  )
+}
+
+function FooterWrapper({ children }: { children: ReactElement }): ReactElement {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>{children}</Box>
+  )
+}
+
+function Header(props: any): ReactElement {
+  const title = props.page.title ?? 'default'
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        minHeight: '100px'
+      }}
+    >
+      <Typography variant="h2" sx={{ minHeight: '100px' }}>
+        {title}
+      </Typography>
+    </Box>
+  )
+}
+
+function SubmitButton(props: JSX.IntrinsicElements['button']): ReactElement {
+  console.log(props)
+  return (
+    <Button variant="contained" {...props}>
+      Submit
+    </Button>
   )
 }
 
 const myComponents: FormiumComponents = {
   ...defaultComponents,
   TextInput,
-  FormControl
+  Textarea,
+  Checkbox,
+  RadioGroup,
+  FormControl,
+  ElementsWrapper,
+  PageWrapper,
+  FooterWrapper,
+  Header,
+  SubmitButton
 }
 
 interface Props {
