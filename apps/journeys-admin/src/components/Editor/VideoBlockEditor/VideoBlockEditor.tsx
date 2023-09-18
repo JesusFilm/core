9afa2e341dcb2d2498a53eb26dcec44241fc5602
DanChae03 <dan.chae@tandem.org.nz>
2023-09-18 23:04:50 +0000

    fix: consistent padding for video and image select cards

--- apps/journeys-admin/src/components/Editor/VideoBlockEditor/VideoBlockEditor.tsx
+++ apps/journeys-admin/src/components/Editor/VideoBlockEditor/VideoBlockEditor.tsx
@@ -29,7 +29,7 @@ export function VideoBlockEditor({
 
   return (
     <>
-      <Box sx={{ px: 6, pb: 6 }}>
+      <Box sx={{ p: 6 }}>
         <Source selectedBlock={selectedBlock} onChange={onChange} />
       </Box>
       {videoBlock?.videoId != null && (
