588fd584caeda7a923fc299da841406c63034d4b
Kneesal <nisalcottingham@gmail.com>
2023-09-20 21:17:21 +0000

    fix: featuerd checkbox

---
+++ apps/journeys-admin/src/components/TemplateView/FeaturedCheckbox/FeaturedCheckbox.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box'
+import Checkbox from '@mui/material/Checkbox'
+import { ReactElement } from 'react'
+
+export function FeaturedCheckbox(): ReactElement {
+  return (
+    <Box>
+      <Checkbox />
+    </Box>
+  )
+}
