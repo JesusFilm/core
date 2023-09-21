df08cbf54b133754f8a8d5aace6750279a87a038
Tanner Fleming <tanner.fleming@gmail.com>
2023-09-21 14:00:50 +0000

    fix: revert commit af42b26 template gallery

--- apps/journeys-admin/pages/templates/index.tsx
+++ apps/journeys-admin/pages/templates/index.tsx
@@ -8,23 +8,19 @@ import { NextSeo } from 'next-seo'
 import { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { useFlags } from '@core/shared/ui/FlagsProvider'
-
 import { PageWrapper } from '../../src/components/NewPageWrapper'
-import { TemplateGallery } from '../../src/components/TemplateGallery'
 import { TemplateLibrary } from '../../src/components/TemplateLibrary'
 import { initAndAuthApp } from '../../src/libs/initAndAuthApp'
 
 function LibraryIndex(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const AuthUser = useAuthUser()
-  const { templates } = useFlags()
 
   return (
     <>
       <NextSeo title={t('Journey Templates')} />
       <PageWrapper title={t('Journey Templates')} authUser={AuthUser}>
-        {templates ? <TemplateGallery /> : <TemplateLibrary />}
+        <TemplateLibrary />
       </PageWrapper>
     </>
   )
--- apps/journeys-admin/src/components/TemplateGallery/index.ts
+++
@@ -1,1 +0,0 @@
-export { TemplateGallery } from './TemplateGallery'
