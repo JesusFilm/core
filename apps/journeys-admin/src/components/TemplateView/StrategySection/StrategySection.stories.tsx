d2544a1720182ff72c945c172d3f1565fdc006ed
Kneesal <nisalcottingham@gmail.com>
2023-09-22 00:48:30 +0000

    fix: story

---
+++ apps/journeys-admin/src/components/TemplateView/StrategySection/StrategySection.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { simpleComponentConfig } from '../../../libs/storybook'
+
+import { StrategySection } from './StrategySection'
+
+const StrategySectionStory: Meta<typeof StrategySection> = {
+  ...simpleComponentConfig,
+  component: StrategySection,
+  title: 'Journeys-Admin/StrategySection'
+}
+
+const Template: StoryObj<typeof StrategySection> = {
+  render: ({ ...args }) => <StrategySection strategySlug={args.strategySlug} />
+}
+
+export const Canva = {
+  ...Template,
+  args: {
+    strategySlug: 'https://www.canva.com/design/DAFvDBw1z1A/view'
+  }
+}
+
+export const CanvaMovie = {
+  ...Template,
+  args: {
+    strategySlug: 'https://www.canva.com/design/DAFvCrg6dMw/watch'
+  }
+}
+
+export const GoogleSlides = {
+  ...Template,
+  args: {
+    strategySlug:
+      'https://docs.google.com/presentation/d/e/2PACX-1vR9RRy1myecVCtOG06olCS7M4h2eEsVDrNdp_17Z1KjRpY0HieSnK5SFEWjDaE6LZR9kBbVm4hQOsr7/pub?start=false&loop=false&delayms=3000'
+  }
+}
+
+export default StrategySectionStory
