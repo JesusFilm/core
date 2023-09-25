824d7be81d6d19975c7b4649951dfd9b371e0c81
DanChae03 <dan.chae@tandem.org.nz>
2023-09-25 22:48:40 +0000

    fix: trim input email string on add user

--- apps/journeys-admin/src/components/AccessDialog/AddUserSection/EmailInviteForm/EmailInviteForm.tsx
+++ apps/journeys-admin/src/components/AccessDialog/AddUserSection/EmailInviteForm/EmailInviteForm.tsx
@@ -43,7 +43,7 @@ export function EmailInviteForm({
         variables: {
           journeyId,
           input: {
-            email: values.email.toLowerCase()
+            email: values.email.trim().toLowerCase()
           }
         },
         update(cache, { data }) {
