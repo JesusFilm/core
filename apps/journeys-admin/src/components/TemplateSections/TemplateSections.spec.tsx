9233fefc019f74600cfac61dbaf35b61cf2d19d9
John Geronimo <johnageronimo1@gmail.com>
2023-09-25 03:38:41 +0000

    feat: featured and new templates

--- apps/journeys-admin/src/components/TemplateSections/TemplateSections.spec.tsx
+++ apps/journeys-admin/src/components/TemplateSections/TemplateSections.spec.tsx
@@ -1,123 +1,15 @@
 import { MockedProvider } from '@apollo/client/testing'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
-import { GET_JOURNEYS } from '../../libs/useJourneysQuery/useJourneysQuery'
-
-import { TemplateSections } from './TemplateSections'
-
-jest.mock('react-i18next', () => ({
-  __esModule: true,
-  useTranslation: () => {
-    return {
-      t: (str: string) => str
-    }
-  }
-}))
+import { FeaturedAndNewTemplates } from './FeaturedAndNewTemplates'
 
 describe('TemplateSections', () => {
-  describe('Featured And New', () => {
-    const newTemplate = {
-      id: 'nw',
-      title: 'New Template',
-      publishedAt: '2023-09-05T23:27:45.596Z',
-      featuredAt: null,
-      template: true
-    }
-
-    it('should render Featured & New Section', async () => {
-      const { getByRole, getAllByTestId } = render(
-        <MockedProvider
-          mocks={[
-            {
-              request: {
-                query: GET_JOURNEYS,
-                variables: {
-                  where: {
-                    featured: true,
-                    template: true,
-                    orderByRecent: true
-                  }
-                }
-              },
-              result: {
-                data: {
-                  journeys: [
-                    {
-                      title: 'Featured Template 1',
-                      id: 1,
-                      publishedAt: '2023-08-14T04:24:24.392Z',
-                      featuredAt: '2023-08-14T04:24:24.392Z',
-                      template: true
-                    },
-                    {
-                      title: 'Featured Template 2',
-                      id: 2,
-                      publishedAt: '2023-08-14T04:24:24.292Z',
-                      featuredAt: '2023-08-14T04:24:24.292Z',
-                      template: true
-                    }
-                  ]
-                }
-              }
-            },
-            {
-              request: {
-                query: GET_JOURNEYS,
-                variables: {
-                  where: {
-                    featured: false,
-                    template: true,
-                    limit: 10,
-                    orderByRecent: true
-                  }
-                }
-              },
-              result: {
-                data: {
-                  journeys: [
-                    {
-                      ...newTemplate,
-                      id: 'nw1',
-                      title: 'New Template 1',
-                      publishedAt: '2023-09-05T23:27:45.796Z'
-                    },
-                    {
-                      ...newTemplate,
-                      id: 'nw2',
-                      title: 'New Template 2',
-                      publishedAt: '2023-09-05T23:27:45.696Z'
-                    },
-                    newTemplate,
-                    newTemplate,
-                    newTemplate,
-                    newTemplate,
-                    newTemplate,
-                    newTemplate,
-                    newTemplate,
-                    newTemplate
-                  ]
-                }
-              }
-            }
-          ]}
-        >
-          <TemplateSections />
-        </MockedProvider>
-      )
-      expect(
-        getByRole('heading', { name: 'Featured & New' })
-      ).toBeInTheDocument()
-
-      await waitFor(() => {
-        const cards = getAllByTestId(/journey-/)
-        // featured templates
-        expect(cards[0]).toHaveTextContent('Featured Template 1')
-        expect(cards[1]).toHaveTextContent('Featured Template 2')
-        // new templates
-        expect(cards[2]).toHaveTextContent('New Template 1')
-        expect(cards[3]).toHaveTextContent('New Template 2')
-        expect(cards[11]).toHaveTextContent('New Template')
-      })
-    })
+  it('should render Featured and New templates', () => {
+    const { getByRole } = render(
+      <MockedProvider>
+        <FeaturedAndNewTemplates />
+      </MockedProvider>
+    )
+    expect(getByRole('heading', { name: 'Featured & New' })).toBeInTheDocument()
   })
 })
--- apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
+++ apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
@@ -1,32 +1,12 @@
 import Stack from '@mui/material/Stack'
 import { ReactElement } from 'react'
-import { useTranslation } from 'react-i18next'
 
-import { useJourneysQuery } from '../../libs/useJourneysQuery'
-
-import { TemplateSection } from './TemplateSection'
+import { FeaturedAndNewTemplates } from './FeaturedAndNewTemplates'
 
 export function TemplateSections(): ReactElement {
-  const { t } = useTranslation('apps-journeys-admin')
-
-  const { data: featuredData } = useJourneysQuery({
-    where: { featured: true, template: true, orderByRecent: true }
-  })
-  const { data: newData } = useJourneysQuery({
-    where: { featured: false, template: true, limit: 10, orderByRecent: true }
-  })
-
-  const featuredAndNewTemplates = [
-    ...(featuredData?.journeys ?? []),
-    ...(newData?.journeys ?? [])
-  ]
-
   return (
     <Stack spacing={8}>
-      <TemplateSection
-        category={t('Featured & New')}
-        journeys={featuredAndNewTemplates}
-      />
+      <FeaturedAndNewTemplates />
     </Stack>
   )
 }
