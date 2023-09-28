faa529581f7e7e8173ebb805ccafbcb36997ce73
Mike Allison <dj.mikeallison@gmail.com>
2023-09-28 01:32:15 +0000

    fix: video initial resolution

--- libs/shared/ui/src/libs/videoJsOptions/videoJsOptions.ts
+++ libs/shared/ui/src/libs/videoJsOptions/videoJsOptions.ts
@@ -1,14 +1,10 @@
 // note: all video.js players import these options, so take care with what is added here
 
 export const videoJsOptions = {
-  // overrideNative: true,
-  // html5: {
-  //   nativeAudioTracks: false,
-  //   nativeVideoTracks: false
-  // }
-  hls: {
-    //   // limitRenditionByPlayerDimensions: true,
-    enableLowInitialPlaylist: false
-    // capLevelToPlayerSize: false
+  html5: {
+    hls: {
+      useDevicePixelRatio: true,
+      bandwidth: 1e9 // used to set initial bandwidth before calculation for abr video
+    }
   }
 }
