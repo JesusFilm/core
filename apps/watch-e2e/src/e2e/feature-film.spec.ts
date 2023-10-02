d57aea9606cc3cb388abeefb9f6f25854ac3533e
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-10-02 09:35:14 +0000

    test: use firefox for watch tests and add extra test

--- apps/watch-e2e/src/e2e/feature-film.spec.ts
+++ apps/watch-e2e/src/e2e/feature-film.spec.ts
@@ -16,67 +16,76 @@ Click on the Play button
 Wait for 20 seconds as this video is 19 seconds
 Take screenshot (after-video)
 */
-test('Feature film', async ({ page }) => {
-  // Set test time out as it has video
-  test.setTimeout(5 * 60 * 1000)
 
-  await page.goto('/')
-
-  // Get and log the current URL
-  const url = page.url()
-  console.log('Current URL:', url)
-
-  await page
-    .getByRole('button', { name: 'JESUS JESUS Feature Film 61 chapters' })
-    .click()
-
-  // video tiles aren't loading upon right away and there is no event to say they are loaded. So the only option is to hard wait
-  //   eslint-disable-next-line
-  await page.waitForTimeout(6 * 1000)
-  await expect(page).toHaveScreenshot('chapters-landing-page.png', {
-    animations: 'disabled',
-    fullPage: true
-  })
-
-  // check it's navigated to the correct URL
-  await expect(page).toHaveURL('watch/jesus.html/english.html')
-  await page.getByTestId('NavigateNextIcon').click()
-  await page.getByTestId('NavigateNextIcon').click()
-  await page.getByTestId('NavigateBeforeIcon').click()
-
-  //   eslint-disable-next-line
-  await page.waitForTimeout(6 * 1000)
-  await expect(page).toHaveScreenshot('chapters-click-page.png', {
-    animations: 'disabled',
-    fullPage: true
-  })
-
-  await page
-    .getByRole('button', {
-      name: 'Blessed are those Who Hear and Obey'
-    })
-    .click()
-
-  await expect(page).toHaveURL(
-    '/watch/jesus.html/blessed-are-those-who-hear-and-obey/english.html'
-  )
-
-  //   eslint-disable-next-line
-  await page.waitForTimeout(6 * 1000)
-  await expect(page).toHaveScreenshot('before-video.png', {
-    animations: 'disabled',
-    fullPage: true
-  })
-
-  await page.getByRole('button', { name: 'Play' }).click()
-
-  // wait for 60 seconds to see if the video is complete. Until there are some events in the code to figure this out
+test.describe('firefox only', () => {
+  
+  // skip the test if mobile as no.of video clips differs based on the device size
   // eslint-disable-next-line
-  await page.waitForTimeout(45 * 1000)
+  test.skip(({ browserName }) => browserName !== 'firefox', 'firefox only!')
 
-  // Take screenshot once video is played and test it is same all the times
-  await expect(page).toHaveScreenshot('after-video.png', {
-    animations: 'disabled',
-    fullPage: true
+  test('Feature film', async ({ page, browser }) => {
+    // Set test time out as it has video
+    test.setTimeout(5 * 60 * 1000)
+
+    await page.goto('/')
+
+    // Get and log the current URL
+    console.log('Current URL:', page.url())
+
+    await page
+      .getByRole('button', { name: 'JESUS JESUS Feature Film 61 chapters' })
+      .click()
+
+    // video tiles aren't loading upon right away and there is no event to say they are loaded. So the only option is to hard wait
+    //   eslint-disable-next-line
+    await page.waitForTimeout(6 * 1000)
+    await expect(page).toHaveScreenshot('ff-landing-page.png', {
+      animations: 'disabled',
+      fullPage: true
+    })
+
+    // check it's navigated to the correct URL
+    await expect(page).toHaveURL('watch/jesus.html/english.html')
+
+    // check navigation buttons are working
+    await page.getByTestId('NavigateNextIcon').click()
+    await page.getByTestId('NavigateNextIcon').click()
+    await page.getByTestId('NavigateBeforeIcon').click()
+
+    //   eslint-disable-next-line
+    await page.waitForTimeout(6 * 1000)
+    await expect(page).toHaveScreenshot('ff-navigated-page.png', {
+      animations: 'disabled',
+      fullPage: true
+    })
+
+    await page
+      .getByRole('button', {
+        name: 'Blessed are those Who Hear and Obey'
+      })
+      .click()
+
+    await expect(page).toHaveURL(
+      '/watch/jesus.html/blessed-are-those-who-hear-and-obey/english.html'
+    )
+
+    //   eslint-disable-next-line
+    await page.waitForTimeout(6 * 1000)
+    await expect(page).toHaveScreenshot('before-video.png', {
+      animations: 'disabled',
+      fullPage: true
+    })
+
+    await page.getByRole('button', { name: 'Play' }).click()
+
+    // wait for 60 seconds to see if the video is complete. Until there are some events in the code to figure this out
+    // eslint-disable-next-line
+    await page.waitForTimeout(45 * 1000)
+
+    // Take screenshot once video is played and test it is same all the times
+    await expect(page).toHaveScreenshot('after-video.png', {
+      animations: 'disabled',
+      fullPage: true
+    })
   })
 })
