ae15f455cd7ca95058f5209377cf8e6a49eec04d
John Geronimo <johnageronimo1@gmail.com>
2023-10-02 05:04:30 +0000

    fix: partial code review changes

--- apps/journeys-admin/src/components/TemplateSections/CategoryTemplates/CategoryTemplates.tsx
+++ apps/journeys-admin/src/components/TemplateSections/CategoryTemplates/CategoryTemplates.tsx
@@ -1,78 +1,33 @@
-import { gql, useQuery } from '@apollo/client'
-import { useRouter } from 'next/router'
-import { ReactElement, useMemo } from 'react'
+import { ReactElement } from 'react'
 
-import { GetTags } from '../../../../__generated__/GetTags'
 import { useJourneysQuery } from '../../../libs/useJourneysQuery'
 import { TemplateSection } from '../TemplateSection'
 
-export const GET_TAGS = gql`
-  query GetTags {
-    tags {
-      id
-      name {
-        value
-        primary
-      }
-    }
-  }
-`
+interface CategoryTemplateProps {
+  id: string
+  name?: string
+}
 
-type TagDetailsProps = Array<{ id: string; name?: string }> | undefined
-
-export function CategoryTemplates(): ReactElement {
-  const router = useRouter()
-  const queryTags = router.query.tags as string[]
-  const { data: serverTags } = useQuery<GetTags>(GET_TAGS)
-
-  function transformTagsValue(queryTags, serverTags): TagDetailsProps {
-    if (queryTags != null) {
-      const tagsArray = Array.isArray(queryTags) ? queryTags : [queryTags]
-      return tagsArray.map((tag) => ({ id: tag }))
-    }
-
-    return serverTags?.tags.map((tag) => ({
-      id: tag.id,
-      name: tag.name.find(({ primary }) => primary)?.value
-    }))
-  }
-
-  const tagDetails = useMemo(
-    () => transformTagsValue(queryTags, serverTags),
-    [queryTags, serverTags]
-  )
-
+export function CategoryTemplates({
+  id,
+  name
+}: CategoryTemplateProps): ReactElement {
   const { data } = useJourneysQuery({
     where: {
       template: true,
       orderByRecent: true,
-      tagIds: tagDetails?.map((tag) => tag.id)
+      tagIds: [id]
     }
   })
 
-  function CategoryTemplate({ tagId, serverTagName, data }): ReactElement {
-    const queryTagName = data?.journeys[0]?.tags
-      .find((tag) => tag.id === tagId)
-      ?.name.find(({ primary }) => primary)?.value
-
-    return (
-      <TemplateSection
-        category={queryTags != null ? queryTagName : serverTagName}
-        journeys={data?.journeys}
-      />
-    )
-  }
+  const paramName = data?.journeys[0]?.tags
+    .find((tag) => tag.id === id)
+    ?.name.find(({ primary }) => primary)?.value
 
   return (
-    <>
-      {tagDetails?.map(({ name, id }) => (
-        <CategoryTemplate
-          key={`category-${name as string}`}
-          serverTagName={name}
-          tagId={id}
-          data={data}
-        />
-      ))}
-    </>
+    <TemplateSection
+      category={name != null ? name : paramName ?? ''}
+      journeys={data?.journeys}
+    />
   )
 }
--- apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
+++ apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
@@ -1,23 +1,45 @@
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { useRouter } from 'next/router'
-import { ReactElement } from 'react'
+import { ReactElement, useMemo } from 'react'
+
+import { useTagsQuery } from '../../libs/useTagsQuery/useTagsQuery'
 
 import { CategoryTemplates } from './CategoryTemplates'
 import { FeaturedAndNewTemplates } from './FeaturedAndNewTemplates'
 
+export type TagDetailsProps = Array<{ id: string; name?: string }> | undefined
+
 export function TemplateSections(): ReactElement {
   const router = useRouter()
-  const tags = router.query.tags as string[]
+  const paramTags = router.query.tags as string[]
+  const { data: serverTags } = useTagsQuery()
+
+  function transformTagsValue(paramTags, serverTags): TagDetailsProps {
+    if (paramTags != null) {
+      const tagsArray = Array.isArray(paramTags) ? paramTags : [paramTags]
+      return tagsArray.map((tag) => ({ id: tag }))
+    }
+
+    return serverTags?.tags.map((tag) => ({
+      id: tag.id,
+      name: tag.name.find(({ primary }) => primary)?.value
+    }))
+  }
+
+  const tags = useMemo(
+    () => transformTagsValue(paramTags, serverTags),
+    [paramTags, serverTags]
+  )
 
   return (
     <Stack spacing={8}>
-      {tags != null ? (
+      {paramTags != null ? (
         // replace with MostRelevantTemplates Component
         <>
           <Typography variant="h3">Tag Ids</Typography>
-          {Array.isArray(tags) &&
-            tags.map((tag, i) => (
+          {Array.isArray(paramTags) &&
+            paramTags.map((tag, i) => (
               <Typography key={i} variant="h4">
                 {tag}
               </Typography>
@@ -26,7 +48,9 @@ export function TemplateSections(): ReactElement {
       ) : (
         <FeaturedAndNewTemplates />
       )}
-      <CategoryTemplates />
+      {tags?.map(({ id, name }) => (
+        <CategoryTemplates key={`category-${id}`} id={id} name={name} />
+      ))}
     </Stack>
   )
 }
