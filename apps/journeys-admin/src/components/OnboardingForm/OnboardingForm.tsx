098328c693ccd6d640ad9003878906734116f6be
Siyang <csiyang@yahoo.co.nz>
2023-10-05 22:11:54 +0000

    feat: form completed at

--- apps/journeys-admin/src/components/OnboardingForm/OnboardingForm.tsx
+++ apps/journeys-admin/src/components/OnboardingForm/OnboardingForm.tsx
@@ -1,9 +1,21 @@
+import { gql, useMutation } from '@apollo/client'
 import { Form } from '@formium/client'
 import Box from '@mui/material/Box'
+import { useRouter } from 'next/router'
 import { AuthUser } from 'next-firebase-auth'
 import { ReactElement } from 'react'
 
 import { FormiumForm } from '@core/shared/ui/FormiumForm'
+
+import { JourneyProfileOnboardingFormComplete } from '../../../__generated__/JourneyProfileOnboardingFormComplete'
+
+export const JOURNEY_PROFILE_ONBOARDING_FORM_COMPLETE = gql`
+  mutation JourneyProfileOnboardingFormComplete {
+    journeyProfileOnboardingFormComplete {
+      id
+    }
+  }
+`
 
 interface OnboardingFormProps {
   form: Form
@@ -14,6 +26,18 @@ export function OnboardingForm({
   form,
   authUser
 }: OnboardingFormProps): ReactElement {
+  const router = useRouter()
+
+  const [journeyProfileOnboardingFormComplete] =
+    useMutation<JourneyProfileOnboardingFormComplete>(
+      JOURNEY_PROFILE_ONBOARDING_FORM_COMPLETE
+    )
+
+  async function handleClick(): Promise<void> {
+    await journeyProfileOnboardingFormComplete()
+    await router?.push('/?onboarding=true')
+  }
+
   return (
     <Box
       sx={{
