a5665b96825c4a5081e8a2af48c801aa909b3f94
John Geronimo <johnageronimo1@gmail.com>
2023-10-06 01:27:09 +0000

    feat: template carousel

--- apps/journeys-admin/src/components/TemplateSections/TemplateSection/TemplateSection.tsx
+++ apps/journeys-admin/src/components/TemplateSections/TemplateSection/TemplateSection.tsx
@@ -1,10 +1,19 @@
-import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/system/Stack'
-import { ReactElement } from 'react'
+import { ReactElement, forwardRef, useRef } from 'react'
+import SwiperCore, { A11y, Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import ChevronLeft from '@core/shared/ui/icons/ChevronLeft'
+import ChevronRight from '@core/shared/ui/icons/ChevronRight'
 
 import { GetJourneys_journeys as Journeys } from '../../../../__generated__/GetJourneys'
 import { TemplateGalleryCard } from '../../TemplateGalleryCard'
+
+import 'swiper/swiper.min.css'
+
+SwiperCore.use([Navigation, A11y])
 
 interface TemplateSectionProps {
   journeys?: Journeys[]
@@ -15,16 +24,66 @@ export function TemplateSection({
   journeys,
   category
 }: TemplateSectionProps): ReactElement {
+  const nextRef = useRef<HTMLButtonElement>(null)
+  const prevRef = useRef<HTMLButtonElement>(null)
+
   return (
-    <Stack spacing={4}>
+    <Stack spacing={4} justifyContent="center" sx={{ position: 'relative' }}>
       <Typography variant="h2">{category}</Typography>
-      <Stack direction="row" spacing={16} sx={{ overflowX: 'auto' }}>
+      <Swiper
+        autoHeight
+        watchOverflow
+        slidesPerView="auto"
+        spaceBetween={36}
+        navigation={{
+          nextEl: nextRef.current,
+          prevEl: prevRef.current
+        }}
+      >
         {journeys?.map((journey) => (
-          <Box key={journey?.id} data-testId={`journey-${journey.id}`}>
+          <SwiperSlide
+            key={journey?.id}
+            data-testId={`journey-${journey.id}`}
+            style={{
+              width: 184
+            }}
+          >
             <TemplateGalleryCard journey={journey} />
-          </Box>
+          </SwiperSlide>
         ))}
-      </Stack>
+      </Swiper>
+      <NavButton variant="prev" ref={prevRef} disabled={journeys == null} />
+      <NavButton variant="next" ref={nextRef} disabled={journeys == null} />
     </Stack>
   )
 }
+
+interface NavButtonProps {
+  variant: 'prev' | 'next'
+  disabled?: boolean
+}
+
+const NavButton = forwardRef<HTMLButtonElement, NavButtonProps>(
+  function NavButton({ variant, disabled = false }, ref): ReactElement {
+    return (
+      <IconButton
+        ref={ref}
+        sx={{
+          position: 'absolute',
+          zIndex: 2,
+          backgroundColor: 'background.paper',
+          opacity: { xs: 0, lg: disabled ? 0 : 1 },
+          boxShadow: (theme) => theme.shadows[2],
+          cursor: 'pointer',
+          left: variant === 'prev' ? 0 : undefined,
+          right: variant === 'next' ? 0 : undefined,
+          '&:hover': {
+            backgroundColor: 'background.paper'
+          }
+        }}
+      >
+        {variant === 'prev' ? <ChevronLeft /> : <ChevronRight />}
+      </IconButton>
+    )
+  }
+)
