9e434766b360c21818c7a6184085444c46e4a91a
Siyang <csiyang@yahoo.co.nz>
2023-10-11 04:07:15 +0000

    fix: page redirects

--- apps/journeys-admin/pages/onboarding-form.tsx
+++ apps/journeys-admin/pages/onboarding-form.tsx
@@ -5,19 +5,15 @@ import {
   withUser,
   withUserTokenSSR
 } from 'next-firebase-auth'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
 import { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { formiumClient } from '@core/shared/ui/formiumClient'
-import { getLaunchDarklyClient } from '@core/shared/ui/getLaunchDarklyClient'
 
-import i18nConfig from '../next-i18next.config'
 import { OnboardingForm } from '../src/components/OnboardingForm'
 import { OnboardingPageWrapper } from '../src/components/OnboardingPageWrapper'
-import { createApolloClient } from '../src/libs/apolloClient'
-import { checkConditionalRedirect } from '../src/libs/checkConditionalRedirect'
+import { initAndAuthApp } from '../src/libs/initAndAuthApp'
 
 interface OnboardingFormPageProps {
   form: Form
@@ -46,28 +42,16 @@ function OnboardingFormPage({ form }: OnboardingFormPageProps): ReactElement {
 
 export const getServerSideProps = withUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
-})(async ({ user, locale }) => {
+})(async ({ user, locale, resolvedUrl }) => {
   if (user == null)
     return { redirect: { permanent: false, destination: '/users/sign-in' } }
 
-  const ldUser = {
-    key: user.id as string,
-    firstName: user.displayName ?? undefined,
-    email: user.email ?? undefined
-  }
-  const launchDarklyClient = await getLaunchDarklyClient(ldUser)
-  const flags = (await launchDarklyClient.allFlagsState(ldUser)).toJSON() as {
-    [key: string]: boolean | undefined
-  }
-
-  const token = await user.getIdToken()
-  const apolloClient = createApolloClient(token != null ? token : '')
-
-  const redirect = await checkConditionalRedirect(apolloClient, {
-    ...flags,
-    onboardingForm: false,
-    teams: false
+  const { flags, redirect, translations } = await initAndAuthApp({
+    user,
+    locale,
+    resolvedUrl
   })
+
   if (redirect != null) return { redirect }
 
   const form = await formiumClient.getFormBySlug(
@@ -78,11 +62,7 @@ export const getServerSideProps = withUserTokenSSR({
     props: {
       form,
       flags,
-      ...(await serverSideTranslations(
-        locale ?? 'en',
-        ['apps-journeys-admin', 'libs-journeys-ui'],
-        i18nConfig
-      ))
+      ...translations
     }
   }
 })
