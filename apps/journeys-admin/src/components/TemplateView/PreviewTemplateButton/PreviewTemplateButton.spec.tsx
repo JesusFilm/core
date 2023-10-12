ebf07f60f1c10787fca12d945f7127c3bfeea91e
Siyang <csiyang@yahoo.co.nz>
2023-10-12 00:59:29 +0000

    test: preview button

--- apps/journeys-admin/src/components/TemplateView/PreviewTemplateButton/PreviewTemplateButton.spec.tsx
+++ apps/journeys-admin/src/components/TemplateView/PreviewTemplateButton/PreviewTemplateButton.spec.tsx
@@ -1,9 +1,45 @@
+import { render } from '@testing-library/react'
+
+import { PreviewTemplateButton } from './PreviewTemplateButton'
+
+jest.mock('react-i18next', () => ({
+  __esModule: true,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str
+    }
+  }
+}))
+
 describe('PreviewTemplateButton', () => {
+  const originalEnv = process.env
+  jest.resetModules()
+  process.env = {
+    ...originalEnv,
+    NEXT_PUBLIC_JOURNEYS_URL: 'http://localhost:4100'
+  }
+
   it('should link to api', () => {
-    expect(true).toBe(true)
+    const { getByRole } = render(
+      <PreviewTemplateButton slug="template-slug" isPublisher />
+    )
+    expect(getByRole('link')).toHaveAttribute(
+      'href',
+      '/api/preview?slug=template-slug'
+    )
   })
 
   it('should link journey', () => {
-    expect(true).toBe(true)
+    const { getByRole } = render(<PreviewTemplateButton slug="template-slug" />)
+    expect(getByRole('link')).toHaveAttribute(
+      'href',
+      'http://localhost:4100/template-slug'
+    )
+  })
+
+  it('should be disabled', () => {
+    const { getByRole } = render(<PreviewTemplateButton />)
+
+    expect(getByRole('link')).toHaveAttribute('aria-disabled', 'true')
   })
 })
