1694970812854597d3527295ceae813fab41f648
Kneesal <nisalcottingham@gmail.com>
2023-10-12 04:27:13 +0000

    chore: story

--- apps/journeys-admin/src/components/TemplateView/TemplateViewHeader/TemplateViewHeader.stories.tsx
+++ apps/journeys-admin/src/components/TemplateView/TemplateViewHeader/TemplateViewHeader.stories.tsx
@@ -1,7 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { ComponentProps } from 'react'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 
+import {
+  JourneyFields as Journey,
+  JourneyFields_primaryImageBlock as PrimaryImageBlock
+} from '../../../../__generated__/JourneyFields'
 import { journeysAdminConfig } from '../../../libs/storybook'
 import { journey } from '../../Editor/ActionDetails/data'
 
@@ -13,11 +18,25 @@ const TemplateViewHeaderStory: Meta<typeof TemplateViewHeader> = {
   component: TemplateViewHeader
 }
 
-const Template: StoryObj<typeof TemplateViewHeaderStory> = {
+const primaryImageBlock: PrimaryImageBlock = {
+  id: 'image1.id',
+  __typename: 'ImageBlock',
+  parentBlockId: null,
+  parentOrder: 0,
+  src: 'https://images.unsplash.com/photo-1508363778367-af363f107cbb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=chester-wade-hLP7lVm4KUE-unsplash.jpg&w=1920',
+  alt: 'image.jpg',
+  width: 1920,
+  height: 1080,
+  blurhash: 'L9AS}j^-0dVC4Tq[=~PATeXSV?aL'
+} as unknown as PrimaryImageBlock
+
+const Template: StoryObj<
+  ComponentProps<typeof TemplateViewHeader> & { journey: Journey }
+> = {
   render: ({ ...args }) => (
     <JourneyProvider
       value={{
-        journey
+        journey: args.journey
       }}
     >
       <TemplateViewHeader
@@ -31,6 +50,13 @@ const Template: StoryObj<typeof TemplateViewHeaderStory> = {
 export const Default = {
   ...Template,
   args: {
+    journey: {
+      ...journey,
+      primaryImageBlock,
+      title: 'My Cool Journey',
+      description:
+        'some description with some words in it... cool journey, huh?'
+    },
     isPublisher: false,
     authUser: { id: 'userId' }
   }
@@ -39,16 +65,16 @@ export const Default = {
 export const Publisher = {
   ...Template,
   args: {
-    isPublisher: true,
-    authUser: { id: 'userId' }
+    ...Default.args,
+    isPublisher: true
   }
 }
 
-export const Public = {
+export const EmptyImage = {
   ...Template,
   args: {
-    isPublisher: false,
-    authUser: {}
+    ...Publisher.args,
+    journey
   }
 }
 
