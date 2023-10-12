9c74b43ec3d763a035b5eaa0554748616a79bc21
Mike Allison <dj.mikeallison@gmail.com>
2023-10-12 01:35:06 +0000

    feat: api-videos seed update

--- apps/api-videos/db/seed.ts
+++ apps/api-videos/db/seed.ts
@@ -17,7 +17,6 @@ import {
 import {
   getVideoIdsAndSlugs,
   handleVideo,
-  handleVideoChildren,
   updateParentChild
 } from '../src/libs/postgresql'
 
@@ -26,123 +25,17 @@ const prisma = new PrismaClient()
 let mode = process.argv[2] ?? null
 
 if (mode == null || mode === 'undefined' || mode === '') {
-  console.log('No mode argument provided. Using default missing mode.')
+  console.log('No mode argument provided. Using default complete mode.')
   console.log(
-    'usage: nx prisma-seed api-videos --mode={mode} --target={target arclight id for replace/update}'
+    'usage: nx prisma-seed api-videos --mode={mode} --target={target arclight id for replace/update/resume}'
   )
   console.log('valid modes: missing, replace, update, complete, resume')
-  mode = 'missing'
+  mode = 'complete'
 }
 
 let target: string | undefined = process.argv[3]
 if (target === 'undefined' || target === '') {
   target = undefined
-}
-
-async function importMediaComponents(
-  mode: string,
-  target?: string
-): Promise<void> {
-  const { slugs: usedVideoSlugs, ids: existingVideoIds } =
-    await getVideoIdsAndSlugs()
-
-  const languages = await fetchMediaLanguagesAndTransformToLanguages()
-
-  let count = 0
-  const importedVideos: string[] = []
-
-  const startTime = new Date().getTime()
-  if (mode === 'complete' || (mode === 'missing' && target == null)) {
-    let page = 1
-    const errors: Record<string, Error> = {}
-    do {
-      const videos = await getArclightMediaComponents(page)
-      count = videos.length
-      for (const arclightVideo of videos) {
-        try {
-          if (
-            mode === 'missing' &&
-            target == null &&
-            existingVideoIds.includes(arclightVideo.mediaComponentId)
-          )
-            continue
-
-          if (mode === 'missing' && target !== arclightVideo.mediaComponentId)
-            continue
-
-          if (!importedVideos.includes(arclightVideo.mediaComponentId)) {
-            const video = await transformMediaComponentToVideo(
-              arclightVideo,
-              languages,
-              usedVideoSlugs
-            )
-
-            await handleVideo(video, importedVideos)
-          } else {
-            console.log('already imported')
-          }
-
-          const childIds = await getArclightMediaComponentLinks(
-            arclightVideo.mediaComponentId
-          )
-          await handleVideoChildren(
-            arclightVideo.mediaComponentId,
-            childIds,
-            importedVideos,
-            languages,
-            usedVideoSlugs
-          )
-        } catch (e) {
-          console.error(e)
-          errors[arclightVideo.mediaComponentId] = e
-        }
-      }
-      const duration = new Date().getTime() - startTime
-      console.log('importMediaComponents duration(s):', duration * 0.001)
-      console.log('importMediaComponents page:', page)
-      page++
-    } while (count > 0)
-    for (const [key, value] of Object.entries(errors)) {
-      console.log('error:', key, value)
-    }
-  } else if (mode === 'replace' || mode === 'update' || mode === 'missing') {
-    if (target == null) {
-      console.log('no target arclight id provided')
-      return
-    }
-    const arclightVideo = await getArclightMediaComponent(target)
-
-    if (arclightVideo == null) {
-      console.log('no arclight video found for id:', target)
-      return
-    }
-
-    const video = await transformMediaComponentToVideo(
-      arclightVideo,
-      languages,
-      usedVideoSlugs
-    )
-
-    if (mode === 'replace') {
-      await prisma.video.delete({
-        where: { id: video.id }
-      })
-    }
-    await handleVideo(video, importedVideos)
-
-    const childIds = await getArclightMediaComponentLinks(
-      arclightVideo.mediaComponentId
-    )
-    await handleVideoChildren(
-      arclightVideo.mediaComponentId,
-      childIds,
-      importedVideos,
-      languages,
-      usedVideoSlugs
-    )
-    const duration = new Date().getTime() - startTime
-    console.log('importMediaComponents duration(s):', duration * 0.001)
-  }
 }
 
 async function main(): Promise<void> {
@@ -250,8 +143,6 @@ async function main(): Promise<void> {
       target
     ))
   }
-  console.log('importing mediaComponents as videos...')
-  await importMediaComponents(mode, target)
   console.log('mediaComponents imported')
   for (const [key, value] of Object.entries(errors)) {
     console.log('error:', key, value)
