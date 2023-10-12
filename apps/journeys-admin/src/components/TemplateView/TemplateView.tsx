ad02999c6aba9c10c7e50b5c763941aac633e9b4
Kneesal <nisalcottingham@gmail.com>
2023-10-12 21:06:26 +0000

    fix: add containter

--- apps/journeys-admin/src/components/TemplateView/TemplateView.tsx
+++ apps/journeys-admin/src/components/TemplateView/TemplateView.tsx
@@ -1,4 +1,5 @@
 import Stack from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import { User } from 'next-firebase-auth'
 import { ReactElement } from 'react'
@@ -19,22 +20,24 @@ export function TemplateView({ authUser }: TemplateViewProps): ReactElement {
   const { journey } = useJourney()
 
   return (
-    <Stack gap={4}>
-      <Typography variant="h1">{journey?.title}</Typography>
-      <Typography variant="body1">{journey?.description}</Typography>
-      <CreateJourneyButton signedIn={authUser?.id != null} />
-      <Stack sx={{ pt: 4 }}>
-        <TemplatePreviewTabs />
-      </Stack>
-      {journey?.strategySlug != null && (
-        <Stack sx={{ pt: { xs: 0, sm: 4 } }}>
-          <StrategySection
-            strategySlug={journey?.strategySlug}
-            variant="full"
-          />
+    <Container disableGutters>
+      <Stack gap={4}>
+        <Typography variant="h1">{journey?.title}</Typography>
+        <Typography variant="body1">{journey?.description}</Typography>
+        <CreateJourneyButton signedIn={authUser?.id != null} />
+        <Stack sx={{ pt: 4 }}>
+          <TemplatePreviewTabs />
         </Stack>
-      )}
-      <TemplateFooter signedIn={authUser?.id != null} />
-    </Stack>
+        {journey?.strategySlug != null && (
+          <Stack sx={{ pt: { xs: 0, sm: 4 } }}>
+            <StrategySection
+              strategySlug={journey?.strategySlug}
+              variant="full"
+            />
+          </Stack>
+        )}
+        <TemplateFooter signedIn={authUser?.id != null} />
+      </Stack>
+    </Container>
   )
 }
