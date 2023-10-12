6fa0a66dbc74187e689b45fe855da2bd494cbd95
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-10-12 14:21:55 +1300

    feat: template sections (#1950)

--- apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
+++ apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
@@ -1,12 +1,17 @@
 import Box from '@mui/material/Box'
+import castArray from 'lodash/castArray'
+import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
 
 import { TemplateSections } from '../TemplateSections'
 
 export function TemplateGallery(): ReactElement {
+  const { query } = useRouter()
   return (
     <Box>
-      <TemplateSections />
+      <TemplateSections
+        tagIds={query.tagIds != null ? castArray(query.tagIds) : undefined}
+      />
     </Box>
   )
 }
