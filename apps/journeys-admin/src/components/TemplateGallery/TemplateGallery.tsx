befeb62433ca68b2f31f0a442e09eb6e38159d96
John Geronimo <johnageronimo1@gmail.com>
2023-10-16 20:03:51 +0000

    fix: template carousel

--- apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
+++ apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import castArray from 'lodash/castArray'
 import { useRouter } from 'next/router'
 import { ReactElement } from 'react'
@@ -8,10 +9,12 @@ import { TemplateSections } from '../TemplateSections'
 export function TemplateGallery(): ReactElement {
   const { query } = useRouter()
   return (
-    <Box>
-      <TemplateSections
-        tagIds={query.tagIds != null ? castArray(query.tagIds) : undefined}
-      />
-    </Box>
+    <Container disableGutters sx={{ px: { xs: 6, lg: 0 } }}>
+      <Box>
+        <TemplateSections
+          tagIds={query.tagIds != null ? castArray(query.tagIds) : undefined}
+        />
+      </Box>
+    </Container>
   )
 }
