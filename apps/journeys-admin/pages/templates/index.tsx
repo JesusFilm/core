f39b617a9889befd1216b5666bee85b4e7711267
John Geronimo <johnageronimo1@gmail.com>
2023-10-18 01:42:49 +0000

    fix: code review

--- apps/journeys-admin/pages/templates/index.tsx
+++ apps/journeys-admin/pages/templates/index.tsx
@@ -21,7 +21,10 @@ function LibraryIndex(): ReactElement {
       <PageWrapper
         title={t('Journey Templates')}
         user={user}
-        mainPanelSx={{ backgroundColor: 'background.paper' }}
+        mainPanelSx={{
+          backgroundColor: 'background.paper',
+          overflowX: 'hidden'
+        }}
       >
         {templates ? <TemplateGallery /> : <TemplateLibrary />}
       </PageWrapper>
