2675f64216d43b7567d8f18218efa181469f23a3
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2023-10-18 15:29:28 +1300

    feat: templates tag filtering (#1968)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

--- apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
+++ apps/journeys-admin/src/components/TemplateGallery/TemplateGallery.tsx
@@ -1,16 +1,71 @@
 import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
 import castArray from 'lodash/castArray'
+import difference from 'lodash/difference'
 import { useRouter } from 'next/router'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { TemplateSections } from '../TemplateSections'
 
+import { TagsFilter } from './TagsFilter'
+
 export function TemplateGallery(): ReactElement {
-  const { query } = useRouter()
+  const { t } = useTranslation('apps-journeys-admin')
+  const router = useRouter()
+  const [selectedTagIds, setSelectedTagIds] = useState<string[]>(
+    router.query.tagIds != null ? castArray(router.query.tagIds) : []
+  )
+
+  function handleChange(
+    newSelectedTagIds: string[],
+    filteredTagIds: string[]
+  ): void {
+    const tagIds = [
+      ...difference(selectedTagIds, filteredTagIds),
+      ...newSelectedTagIds
+    ]
+    setSelectedTagIds(tagIds)
+    router.query.tagIds = tagIds
+    void router.push(router)
+  }
+
   return (
     <Container disableGutters>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          pb: { xs: 6, md: 9 }
+        }}
+      >
+        <Grid item xs={12} md={8}>
+          <TagsFilter
+            label={t('Topics, holidays, felt needs, collections')}
+            tagNames={['Topics', 'Holidays', 'Felt Needs', 'Collections']}
+            onChange={handleChange}
+            selectedTagIds={selectedTagIds}
+          />
+        </Grid>
+        <Grid item xs={6} md={2}>
+          <TagsFilter
+            label={t('Audience')}
+            tagNames={['Audience']}
+            onChange={handleChange}
+            selectedTagIds={selectedTagIds}
+          />
+        </Grid>
+        <Grid item xs={6} md={2}>
+          <TagsFilter
+            label={t('Genre')}
+            tagNames={['Genre']}
+            onChange={handleChange}
+            selectedTagIds={selectedTagIds}
+          />
+        </Grid>
+      </Grid>
       <TemplateSections
-        tagIds={query.tagIds != null ? castArray(query.tagIds) : undefined}
+        tagIds={selectedTagIds.length > 0 ? selectedTagIds : undefined}
       />
     </Container>
   )
