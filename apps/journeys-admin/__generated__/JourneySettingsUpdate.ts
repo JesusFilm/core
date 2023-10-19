f29f805d62f74757e5db6c378d93dcf960835b5d
Ziwei Liu <10802634+storyworks@users.noreply.github.com>
2023-10-20 10:37:06 +1300

    feat: single tag group autocomplete component (#1893)
    Co-authored-by: John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>

--- apps/journeys-admin/__generated__/JourneySettingsUpdate.ts
+++ apps/journeys-admin/__generated__/JourneySettingsUpdate.ts
@@ -9,12 +9,18 @@ import { JourneyUpdateInput } from "./globalTypes";
 // GraphQL mutation operation: JourneySettingsUpdate
 // ====================================================
 
+export interface JourneySettingsUpdate_journeyUpdate_tags {
+  __typename: "Tag";
+  id: string;
+}
+
 export interface JourneySettingsUpdate_journeyUpdate {
   __typename: "Journey";
   id: string;
   title: string;
   description: string | null;
   strategySlug: string | null;
+  tags: JourneySettingsUpdate_journeyUpdate_tags[];
 }
 
 export interface JourneySettingsUpdate {
