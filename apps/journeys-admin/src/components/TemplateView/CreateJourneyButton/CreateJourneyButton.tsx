dbd9eebbe97aa2c2af96554b4d93ac0eb9d2cedb
John Geronimo <johnageronimo1@gmail.com>
2023-10-26 21:05:44 +0000

    fix: use template check

--- apps/journeys-admin/src/components/TemplateView/CreateJourneyButton/CreateJourneyButton.tsx
+++ apps/journeys-admin/src/components/TemplateView/CreateJourneyButton/CreateJourneyButton.tsx
@@ -39,7 +39,7 @@ export function CreateJourneyButton({
       setLoadingJourney(true)
 
       const { data } = await journeyDuplicate({
-        variables: { id: journey.id, teamId }
+        variables: { id: journey?.id, teamId }
       })
 
       if (data != null) {
