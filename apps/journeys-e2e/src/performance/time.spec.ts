14e03674317d77c30fabf8be27934a7c63134202
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-10-27 01:07:16 +0000

    test: add assertions

--- apps/journeys-e2e/src/performance/time.spec.ts
+++ apps/journeys-e2e/src/performance/time.spec.ts
@@ -3,40 +3,36 @@ import { expect, test } from '@playwright/test';
 test("Capture performance traces by marking actions using Performance API", async ({ page, browser }) => {
     await page.goto("https://your.nextstep.is/")
 
-    // fact or fiction page - click on on fact or fiction
+    // Click on on fact or fiction
     await page.click('a[href="/fact-or-fiction"]')
 
     console.log("========== Start Tracing Perf ===========")
+    await browser.startTracing()
     // Using Performanc.mark API
-    await page.evaluate(() => (window.performance.mark('Perf:Started')))
+    await page.evaluate(() => (window.performance.mark('Start:FoF')))
         
     // test that user actually navigated to the choosen journey
     await expect(page).toHaveURL(/.*fact-or-fiction/)
     await expect(page).toHaveScreenshot('fact-or-fiction.png', {
-        animations: 'disabled',
         fullPage: true
       })
 
     // Using performance.mark API
-    await page.evaluate(() => (window.performance.mark('Perf:Ended')))
+    await page.evaluate(() => (window.performance.mark('End:FoF')))
 
-    // Performance measure
-    await page.evaluate(() => (window.performance.measure("overall", "Perf:Started", "Perf:Ended")))
+    console.log("======= Stop Tracing ============")
+    await browser.stopTracing()    
 
     // To get all performance marks
     const getAllMarksJson = await page.evaluate(() => (JSON.stringify(window.performance.getEntriesByType("mark"))))
     const getAllMarks = await JSON.parse(getAllMarksJson)
     console.log('window.performance.getEntriesByType("mark")', getAllMarks)
+    expect(getAllMarks.length).toBeGreaterThan(0)
 
-    // To get all performance measures of Google
-    const getAllMeasuresJson = await page.evaluate(() => (JSON.stringify(window.performance.getEntriesByType("measure"))))
-    const getAllMeasures = await JSON.parse(getAllMeasuresJson)
-    console.log('window.performance.getEntriesByType("measure")', getAllMeasures)
-    await expect(page).toHaveURL(/.*fact-or-fiction/)
-    console.log("======= Stop Tracing ============")
-    await browser.stopTracing()
-    expect(getAllMarks).not.toBeNull()
-
-    // expect overall duration is less than or equals to 700 ms
-
+    // Test that time taken between Start:FoF and End:FoF is less than acceptable threshold
+    const start = getAllMarks.find((obj) => obj.name === "Start:FoF")?.startTime;
+    const end = getAllMarks.find((obj) => obj.name === "End:FoF")?.startTime;
+    const diff = (end) - (start);
+    console.log("diff: ", diff);
+    expect(diff).toBeLessThan(1000);
 })
