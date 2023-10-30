352bd3fa065df470d82135d6b06b738016d0c341
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-10-30 03:18:09 +0000

    test: remove unwanted files fix lint

--- apps/journeys-e2e/src/performance/lighthouse.spec.ts
+++ apps/journeys-e2e/src/performance/lighthouse.spec.ts
@@ -1,12 +1,12 @@
 import { test } from '@playwright/test'
-import { chromium } from 'playwright-core';
-import { playAudit } from 'playwright-lighthouse';
+import { chromium } from 'playwright-core'
+import { playAudit } from 'playwright-lighthouse'
 
 const lighthouseConfiguration = async (): Promise<void> => {
   const browser = await chromium.launch({
-    args: ['--remote-debugging-port=9222', '--start-maximized'],
-  });
-  const page = await browser.newPage();
+    args: ['--remote-debugging-port=9222', '--start-maximized']
+  })
+  const page = await browser.newPage()
   const config = {
     extends: 'lighthouse:default',
     settings: {
@@ -16,30 +16,33 @@ const lighthouseConfiguration = async (): Promise<void> => {
         width: 1350,
         height: 940,
         deviceScaleFactor: 1,
-        disabled: false,
-      },
-    },
-  };
-  await page.goto('https://playwright.dev/');
+        disabled: false
+      }
+    }
+  }
+  await page.goto('https://playwright.dev/')
   await playAudit({
     page,
     config,
     thresholds: {
       performance: 50,
-      accessibility: 50,
+      accessibility: 50
     },
     reports: {
-      "formats": { html: true },
-      name: "lighthouse-report",
-      directory: "lighthouse-report" + Date.now().toString()
+      formats: { html: true },
+      name: 'lighthouse-report',
+      directory: 'lighthouse-report' + Date.now().toString()
     },
     port: 9222
-  });
-  await browser.close();
-};
+  })
+  await browser.close()
+}
 
 test.describe('Perform lighthouse audit', () => {
-  test.use({ viewport: null });
+  test.use({ viewport: null })
 
-  test("Configure lighthouse for performance and accessibility audit", lighthouseConfiguration);
-});
+  test(
+    'Configure lighthouse for performance and accessibility audit',
+    lighthouseConfiguration
+  )
+})
