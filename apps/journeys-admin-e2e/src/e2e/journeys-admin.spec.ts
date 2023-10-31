860ce6cc0552ebb6d3da8a82015ebf570e172b97
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-10-31 08:29:49 +0000

    test: revert admin file

--- apps/journeys-admin-e2e/src/e2e/journeys-admin.spec.ts
+++ apps/journeys-admin-e2e/src/e2e/journeys-admin.spec.ts
@@ -15,6 +15,7 @@ test('sample journeys-admin e2e test', async ({ page }) => {
   // await expect(page).toHaveURL(/.*journeys-admin/)
 
   await expect(page).toHaveScreenshot('home-page.png', {
+    animations: 'disabled',
     fullPage: true
   })
 })
