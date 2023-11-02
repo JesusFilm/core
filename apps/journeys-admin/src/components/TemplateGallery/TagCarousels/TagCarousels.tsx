aad7a68eee8fedf8e9d5746d8fc7d512a88d31c9
Ziwei Liu <ziwei.liu.23@gmail.com>
2023-11-02 04:02:26 +0000

    fix: add low res background, use basic carousel

--- apps/journeys-admin/src/components/TemplateGallery/TagCarousels/TagCarousels.tsx
+++ apps/journeys-admin/src/components/TemplateGallery/TagCarousels/TagCarousels.tsx
@@ -1,23 +1,71 @@
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
+import ButtonBase from '@mui/material/ButtonBase'
 import Stack from '@mui/material/Stack'
+import { styled, useTheme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
-import { ReactElement, useMemo } from 'react'
+import { ReactElement, useCallback, useMemo } from 'react'
+import { SwiperOptions } from 'swiper'
+
+import { NextImage } from '@core/shared/ui/NextImage'
 
 import { useTagsQuery } from '../../../libs/useTagsQuery'
+import { TemplateGalleryCarousel } from '../TemplateGalleryCarousel'
+
+import acceptanceImage from './assets/acceptance.png'
+import depressionImage from './assets/depression.png'
+import fearAnxietyImage from './assets/fearAnxiety.png'
+
+const StyledFeltNeedsButton = styled(ButtonBase)(({ theme }) => ({
+  backgroundColor: 'grey',
+  padding: '32px 28px',
+  color: 'white',
+  borderRadius: '8px'
+}))
 
 export function TagCarousels(): ReactElement {
   const { parentTags, childTags } = useTagsQuery()
+  const { breakpoints } = useTheme()
 
-  const featureTagId = useMemo(() => {
+  const swiperBreakpoints: SwiperOptions['breakpoints'] = {
+    [breakpoints.values.xs]: {
+      slidesPerGroup: 1,
+      slidesPerView: 'auto',
+      spaceBetween: 20
+    },
+    [breakpoints.values.md]: {
+      slidesPerGroup: 3.8,
+      slidesPerView: 3.8,
+      spaceBetween: 20
+    },
+    [breakpoints.values.lg]: {
+      slidesPerGroup: 5,
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  }
+
+  const feltNeedsTagId = useMemo(() => {
     return parentTags.find((tag) => tag.name[0].value === 'Felt Needs')?.id
   }, [parentTags])
 
-  const featureTags = useMemo(() => {
-    return featureTagId != null
-      ? childTags.filter((tag) => tag.parentId === featureTagId)
+  const feltNeedsTags = useMemo(() => {
+    return feltNeedsTagId != null
+      ? childTags.filter((tag) => tag.parentId === feltNeedsTagId)
       : []
-  }, [childTags, featureTagId])
+  }, [childTags, feltNeedsTagId])
+
+  const tagImage = useCallback((tagLabel: string) => {
+    switch (tagLabel) {
+      case 'Acceptance':
+        return acceptanceImage
+      case 'Depression':
+        return depressionImage
+      case 'Fear/Anxiety':
+        return fearAnxietyImage
+      default:
+        return undefined
+    }
+  }, [])
 
   const collectionTagId = useMemo(() => {
     return parentTags.find((tag) => tag.name[0].value === 'Collections')?.id
@@ -29,39 +77,45 @@ export function TagCarousels(): ReactElement {
       : []
   }, [childTags, collectionTagId])
 
-  console.log('tags', parentTags)
+  const FeltNeedsButton = ({ item: tag, index }): ReactElement => {
+    const tagLabel: string =
+      tag.name[0].value ?? `felt-needs-${index as string}`
+    const image = tagImage(tagLabel)
+
+    return (
+      <StyledFeltNeedsButton
+        key={`${tagLabel}-button}`}
+        sx={{
+          width: { xs: '150px', md: '100%' },
+          height: { xs: '56px', md: '110px' }
+        }}
+      >
+        {image != null && (
+          <NextImage src={image.src} layout="fill" sx={{ borderRadius: 2 }} />
+        )}
+        <Typography
+          variant="h3"
+          sx={{ zIndex: 1, display: { xs: 'none', md: 'flex' } }}
+        >
+          {tagLabel}
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          sx={{ zIndex: 1, display: { md: 'none' } }}
+        >
+          {tagLabel}
+        </Typography>
+      </StyledFeltNeedsButton>
+    )
+  }
 
   return (
     <Stack gap={7} sx={{ mb: { xs: 10, md: 16 } }}>
-      <Stack
-        direction="row"
-        gap={4}
-        sx={{
-          overflowX: 'scroll',
-          // Hide scrollbar on Chrome, Safari
-          '&::-webkit-scrollbar': {
-            display: 'none'
-          },
-          // Hide scrollbar on Firefox
-          scrollbarWidth: 'none'
-        }}
-      >
-        {featureTags.map((tag) => (
-          <Paper
-            key={`${tag.name[0].value}-button`}
-            elevation={0}
-            sx={{
-              backgroundColor: 'grey',
-              px: 8,
-              py: 7,
-              color: 'white',
-              borderRadius: 2
-            }}
-          >
-            <Typography variant="h3">{tag.name[0].value}</Typography>
-          </Paper>
-        ))}
-      </Stack>
+      <TemplateGalleryCarousel
+        items={feltNeedsTags}
+        renderItem={(itemProps) => <FeltNeedsButton {...itemProps} />}
+        breakpoints={swiperBreakpoints}
+      />
       <Stack direction="row" gap={10}>
         {collectionTags.map((tag) => (
           <Stack
@@ -79,10 +133,7 @@ export function TagCarousels(): ReactElement {
                 borderRadius: 8
               }}
             />
-            <Stack>
-              <Typography variant="subtitle2">{tag.name[0].value}</Typography>
-              <Typography variant="caption">{tag.name[0].value}</Typography>
-            </Stack>
+            <Typography variant="subtitle2">{tag.name[0].value}</Typography>
           </Stack>
         ))}
       </Stack>
