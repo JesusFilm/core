d9940a20a4751c5eddaa6e9fd655c67534d050a9
Nisal Cottingham <114973713+Kneesal@users.noreply.github.com>
2023-11-09 14:20:36 +1300

    feat: template dialog settings forms (#2021)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

--- apps/journeys-admin/src/components/Editor/EditToolbar/Menu/TemplateSettingsDialog/TemplateSettingsDialog.stories.tsx
+++ apps/journeys-admin/src/components/Editor/EditToolbar/Menu/TemplateSettingsDialog/TemplateSettingsDialog.stories.tsx
@@ -287,7 +287,8 @@ const Template: StoryObj<typeof TemplateSettingsDialog> = {
               ...feltNeeds,
               audience[1],
               genre[1]
-            ] as unknown as Tag[]
+            ] as unknown as Tag[],
+            publishedAt: '2021-12-19T12:34:56.647Z'
           },
           variant: 'admin'
         }}
