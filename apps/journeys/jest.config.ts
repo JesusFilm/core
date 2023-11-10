d97e38350f30db5ef724b802f91588bfb6ee2e4f
Mike Allison <dj.mikeallison@gmail.com>
2023-11-10 23:27:17 +0000

    chore: swiper bump

--- apps/journeys/jest.config.ts
+++ apps/journeys/jest.config.ts
@@ -1,17 +1,32 @@
 import type { Config } from 'jest'
 
+const esmModules = ['swiper', 'ssr-window', 'dom7']
+
 const config: Config = {
   displayName: 'journeys',
   transform: {
+    // '(?!.*\\.mjs$|swiper|ssr-window|dom7)': 'babel-jest',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }]
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        presets: [
+          // ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@nx/next/babel'
+        ],
+        plugins: ['@babel/plugin-proposal-private-methods']
+      }
+    ]
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/journeys',
   setupFilesAfterEnv: ['<rootDir>setupTests.ts'],
   collectCoverage: true,
   coverageReporters: ['cobertura'],
-  preset: '../../jest.preset.js'
+  preset: '../../jest.preset.js',
+  transformIgnorePatterns: [
+    `<rootDir>/node_modules/(?!(${esmModules.join('|')}))`
+  ]
 }
 
 export default config
