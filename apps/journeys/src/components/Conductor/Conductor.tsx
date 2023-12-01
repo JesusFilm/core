d6b15c016457bc6ea3f5420d261bb95d01112a38
Ziwei Liu <ziwei.liu.23@gmail.com>
2023-12-01 01:11:02 +0000

    fix: cleanup dom

--- apps/journeys/src/components/Conductor/Conductor.tsx
+++ apps/journeys/src/components/Conductor/Conductor.tsx
@@ -1,10 +1,9 @@
 import { gql, useMutation } from '@apollo/client'
-import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import { SxProps, useTheme } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import { useRouter } from 'next/router'
-import { ReactElement, useEffect, useRef } from 'react'
+import { ReactElement, useEffect } from 'react'
 import { use100vh } from 'react-div-100vh'
 import TagManager from 'react-gtm-module'
 import { v4 as uuidv4 } from 'uuid'
@@ -97,29 +96,6 @@ export function Conductor({ blocks }: ConductorProps): ReactElement {
     }
   }, [activeBlock, enableTouchMoveNext])
 
-  // ScrollBlock: https:// gist.github.com/reecelucas/2f510e6b8504008deaaa52732202d2da
-  // const scroll = useRef(false)
-  // if (typeof document !== 'undefined') {
-  //   const html = document.documentElement
-  //   const body = document.body
-
-  //   if (body != null || !scroll.current) {
-  //     const scrollBarWidth = window.innerWidth - html.clientWidth
-  //     const bodyPaddingRight =
-  //       parseInt(
-  //         window.getComputedStyle(body).getPropertyValue('padding-right')
-  //       ) ?? 0
-
-  //     html.style.position = 'relative' /* [1] */
-  //     body.style.position = 'relative' /* [1] */
-  //     html.style.overflow = 'hidden' /* [2] */
-  //     body.style.overflow = 'hidden' /* [2] */
-  //     body.style.paddingRight = `${bodyPaddingRight + scrollBarWidth}px`
-
-  //     scroll.current = true
-  //   }
-  // }
-
   useEffect(() => {
     if ((variant === 'default' || variant === 'embed') && journey != null) {
       const id = uuidv4()
@@ -192,72 +168,22 @@ export function Conductor({ blocks }: ConductorProps): ReactElement {
   }
 
   return (
-    <Box
+    <Stack
+      data-testid="Conductor"
       sx={{
+        justifyContent: 'center',
         height: viewportHeight ?? '100vh',
-        minHeight: '-webkit-fill-available',
-        [theme.breakpoints.down('md')]: { overflowY: 'auto' },
+        background: theme.palette.grey[900],
+        p: { lg: 6 },
         overflow: 'hidden'
       }}
     >
-      <Stack
-        sx={{
-          justifyContent: 'center',
-          height: '100%',
-          background: theme.palette.grey[900]
-        }}
-        data-testid="Conductor"
-      >
-        <Box sx={{ height: { xs: '100%', lg: 'unset' } }}>
-          <ThemeProvider {...stepTheme} locale={locale} rtl={rtl} nested>
-            <Stack
-              justifyContent="center"
-              sx={{
-                maxHeight: {
-                  xs: '100vh',
-                  lg: 'calc(100vh - 80px)'
-                },
-                height: {
-                  xs: 'inherit',
-                  lg: 'calc(54.25vw + 102px)'
-                },
-                px: { lg: 6 }
-              }}
-            >
-              <Stack
-                sx={{
-                  position: 'absolute',
-                  mt: 1,
-                  lg: 0,
-                  zIndex: 10,
-                  top: 1,
-                  left: 1,
-                  backgroundColor: 'rgba(76, 175, 80, 0.3)'
-                }}
-              >
-                <Typography color="secondary">
-                  {`Unlocked: ${!activeBlock?.locked ? 'true' : 'false'}`}
-                </Typography>
-                <Typography color="secondary">
-                  {`Enable touch move: ${
-                    enableTouchMoveNext ? 'true' : 'false'
-                  }`}
-                </Typography>
-              </Stack>
-              {showHeaderFooter && router.query.noi == null && (
-                <StepHeader sx={{ ...mobileNotchStyling }} />
-              )}
-
-              <BlockRenderer block={activeBlock} />
-
-              <StepFooter
-                sx={{
-                  visibility: showHeaderFooter ? 'visible' : 'hidden',
-                  ...mobileNotchStyling
-                }}
-              />
-            </Stack>
-          </ThemeProvider>
+      <ThemeProvider {...stepTheme} locale={locale} rtl={rtl} nested>
+        {showHeaderFooter && router.query.noi == null && (
+          <StepHeader sx={{ ...mobileNotchStyling }} />
+        )}
+        <Stack sx={{ height: 'inherit' }}>
+          <BlockRenderer block={activeBlock} />
 
           <NavigationButton
             variant={rtl ? 'next' : 'previous'}
@@ -267,8 +193,14 @@ export function Conductor({ blocks }: ConductorProps): ReactElement {
             variant={rtl ? 'previous' : 'next'}
             alignment="right"
           />
-        </Box>
-      </Stack>
-    </Box>
+        </Stack>
+        <StepFooter
+          sx={{
+            visibility: showHeaderFooter ? 'visible' : 'hidden',
+            ...mobileNotchStyling
+          }}
+        />
+      </ThemeProvider>
+    </Stack>
   )
 }
