9f0ec6f078e990322931bfb47947149f658a317c
Siyang <csiyang@yahoo.co.nz>
2023-12-04 02:35:26 +0000

    chore: codegen

--- apps/journeys-admin/__generated__/BlockOrderUpdate.ts
+++ apps/journeys-admin/__generated__/BlockOrderUpdate.ts
@@ -8,7 +8,7 @@
 // ====================================================
 
 export interface BlockOrderUpdate_blockOrderUpdate {
-  __typename: "ImageBlock" | "ButtonBlock" | "CardBlock" | "IconBlock" | "RadioOptionBlock" | "RadioQuestionBlock" | "SignUpBlock" | "StepBlock" | "TextResponseBlock" | "TypographyBlock" | "VideoBlock" | "GridContainerBlock" | "GridItemBlock" | "VideoTriggerBlock";
+  __typename: "ImageBlock" | "ButtonBlock" | "CardBlock" | "FormBlock" | "IconBlock" | "RadioOptionBlock" | "RadioQuestionBlock" | "SignUpBlock" | "StepBlock" | "TextResponseBlock" | "TypographyBlock" | "VideoBlock" | "GridContainerBlock" | "GridItemBlock" | "VideoTriggerBlock";
   id: string;
   parentOrder: number | null;
 }
