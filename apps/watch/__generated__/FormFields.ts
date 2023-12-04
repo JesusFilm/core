97dbceac73544774b5087ab1824a09fa455dca18
Siyang <csiyang@yahoo.co.nz>
2023-12-04 03:15:58 +0000

    chore: codegen

--- apps/watch/__generated__/FormFields.ts
+++ apps/watch/__generated__/FormFields.ts
@@ -7,9 +7,59 @@
 // GraphQL fragment: FormFields
 // ====================================================
 
+export interface FormFields_action_NavigateAction {
+  __typename: "NavigateAction";
+  parentBlockId: string;
+  gtmEventName: string | null;
+}
+
+export interface FormFields_action_NavigateToBlockAction {
+  __typename: "NavigateToBlockAction";
+  parentBlockId: string;
+  gtmEventName: string | null;
+  blockId: string;
+}
+
+export interface FormFields_action_NavigateToJourneyAction_journey_language {
+  __typename: "Language";
+  bcp47: string | null;
+}
+
+export interface FormFields_action_NavigateToJourneyAction_journey {
+  __typename: "Journey";
+  id: string;
+  slug: string;
+  language: FormFields_action_NavigateToJourneyAction_journey_language;
+}
+
+export interface FormFields_action_NavigateToJourneyAction {
+  __typename: "NavigateToJourneyAction";
+  parentBlockId: string;
+  gtmEventName: string | null;
+  journey: FormFields_action_NavigateToJourneyAction_journey | null;
+}
+
+export interface FormFields_action_LinkAction {
+  __typename: "LinkAction";
+  parentBlockId: string;
+  gtmEventName: string | null;
+  url: string;
+}
+
+export interface FormFields_action_EmailAction {
+  __typename: "EmailAction";
+  parentBlockId: string;
+  gtmEventName: string | null;
+  email: string;
+}
+
+export type FormFields_action = FormFields_action_NavigateAction | FormFields_action_NavigateToBlockAction | FormFields_action_NavigateToJourneyAction | FormFields_action_LinkAction | FormFields_action_EmailAction;
+
 export interface FormFields {
   __typename: "FormBlock";
   id: string;
   parentBlockId: string | null;
   parentOrder: number | null;
+  form: any | null;
+  action: FormFields_action | null;
 }
