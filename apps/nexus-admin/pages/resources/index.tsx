f701b3c6750deacf07729b50d4d56175e045cc4f
Nahom Bekele <nahom.fix@gmail.com>
2023-12-07 22:51:09 +0000

    feat: add google drive picker

--- apps/nexus-admin/pages/resources/index.tsx
+++ apps/nexus-admin/pages/resources/index.tsx
@@ -1,11 +1,31 @@
 import { Button, Stack } from '@mui/material'
 import { useState } from 'react'
+import useDrivePicker from 'react-google-drive-picker'
 import { DeleteModal } from '../../src/components/DeleteModal'
 import { MainLayout } from '../../src/components/MainLayout'
 import { ResourcesTable } from '../../src/components/ResourcesTable'
 
 const ResourcesPage = () => {
   const [deleteResourceModal, setDeleteResourceModal] = useState<boolean>(false)
+  const [openPicker, authResponse] = useDrivePicker()
+
+  const openGooglePicker = () => {
+    openPicker({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
+      developerKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? '',
+      viewId: 'DOCS_VIDEOS',
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      callbackFunction: (data) => {
+        if (data.action === 'cancel') {
+          console.log('User clicked cancel/close button')
+        }
+        console.log(data)
+        console.log(authResponse)
+      }
+    })
+  }
 
   return (
     <MainLayout title="Resources">
@@ -16,7 +36,9 @@ const ResourcesPage = () => {
             pt: 4
           }}
         >
-          <Button variant="contained">Load from Google drive</Button>
+          <Button variant="contained" onClick={() => openGooglePicker()}>
+            Load from Google drive
+          </Button>
         </Stack>
         <ResourcesTable />
       </Stack>
