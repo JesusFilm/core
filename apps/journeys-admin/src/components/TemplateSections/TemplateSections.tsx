11c50768cdfe6d6e08235f07ea37d2025aa8410e
Kneesal <nisalcottingham@gmail.com>
2023-12-08 01:23:30 +0000

    fix: remove cache only

--- apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
+++ apps/journeys-admin/src/components/TemplateSections/TemplateSections.tsx
@@ -33,8 +33,6 @@ export function TemplateSections({
   const [showNoResults, setShowNoResults] = useState<boolean>(false)
 
   const { loading: apolloLoading } = useJourneysQuery({
-    // todo remove once cache working for this page - Nisal
-    fetchPolicy: 'cache-only',
     variables: {
       where: {
         template: true,
