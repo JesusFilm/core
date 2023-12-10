de253b707c6480e53bb19e955be1a77ec8b7a78e
Biniam Kassahun <george.beng@gmail.com>
2023-12-10 10:09:11 +0000

    feat: refactor channels resolvers and schema

---
+++ apps/api-nexus/src/app/lib/googleAPI/googleYoutubeService.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@nestjs/common/decorators/core'
+
+interface ChannelsRequest {
+  accessToken: string
+}
+
+interface ChannelsResponse {
+  kind: string
+  etag: string
+  items: Array<{
+    kind: string
+    etag: string
+    id: string
+    snippet: {
+      title: string
+      description: string
+      customUrl: string
+      publishedAt: string
+      thumbnails: {
+        default: {
+          url: string
+          width: number
+          height: number
+        }
+        medium: {
+          url: string
+          width: number
+          height: number
+        }
+        high: {
+          url: string
+          width: number
+          height: number
+        }
+      }
+    }
+    localized: {
+      title: string
+      description: string
+    }
+  }>
+  grant_type: 'authorization_code' | 'refresh_token'
+}
+
+@Injectable()
+export class GoogleYoutubeService {
+  rootUrl: string
+  constructor() {
+    this.rootUrl = 'https://www.googleapis.com/youtube/v3'
+  }
+
+  async getChannels(req: ChannelsRequest): Promise<ChannelsResponse> {
+    const channelsParam = {
+      part: ['snippet'],
+      mine: true
+    }
+    const params = new URLSearchParams()
+    Object.entries(channelsParam).forEach(([key, value]) => {
+      params.append(key, value.toString())
+    })
+
+    const response = await fetch(`${this.rootUrl}/${params.toString()}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${req.accessToken}`
+      }
+    })
+
+    return await response.json()
+  }
+}
--- apps/api-nexus/src/app/modules/channel/channel.resolver.ts
+++ apps/api-nexus/src/app/modules/channel/channel.resolver.ts
@@ -1,10 +1,9 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
-import FormData from 'form-data'
 import { GraphQLError } from 'graphql'
-import fetch, { Response } from 'node-fetch'
 import { v4 as uuidv4 } from 'uuid'
 
 import { Channel, Prisma } from '.prisma/api-nexus-client'
+import { CurrentUserId } from '@core/nest/decorators/CurrentUserId/CurrentUserId'
 
 import {
   ChannelCreateInput,
@@ -12,230 +11,161 @@ import {
   ChannelUpdateInput,
   ConnectYoutubeChannelInput
 } from '../../__generated__/graphql'
+import { GoogleYoutubeService } from '../../lib/googleAPI/googleYoutubeService'
+import { GoogleOAuthService } from '../../lib/googleOAuth/googleOAuth'
 import { PrismaService } from '../../lib/prisma.service'
 
 @Resolver('Channel')
 export class ChannelResolver {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly googleOAuth: GoogleOAuthService,
+    private readonly googleYoutube: GoogleYoutubeService
+  ) {}
 
   @Query()
-  async channels(@Args('where') where?: ChannelFilter): Promise<Channel[]> {
+  async channels(
+    @CurrentUserId() userId: string,
+    @Args('where') where?: ChannelFilter
+  ): Promise<Channel[]> {
     const filter: Prisma.ChannelWhereInput = {}
     if (where?.ids != null) filter.id = { in: where?.ids }
-
-    // return []
-
     const channels = await this.prismaService.channel.findMany({
-      where: filter,
+      where: {
+        AND: [filter, { nexus: { userNexuses: { every: { userId } } } }]
+      },
       take: where?.limit ?? undefined,
       include: {
-        channelYoutubeCredential: true
+        youtube: true
       }
     })
-
-    console.log('channels', channels)
-
-    // if(channels == null) {
-    //   return []
-    // }
-
     return channels
   }
 
   @Query()
-  async channel(@Args('id') id: string): Promise<Channel | null> {
+  async channel(
+    @CurrentUserId() userId: string,
+    @Args('id') id: string
+  ): Promise<Channel | null> {
     const filter: Prisma.ChannelWhereUniqueInput = { id }
     const channel = await this.prismaService.channel.findUnique({
-      where: filter,
+      where: {
+        id,
+        AND: [filter, { nexus: { userNexuses: { every: { userId } } } }]
+      },
       include: {
-        channelYoutubeCredential: true
+        youtube: true
       }
     })
-    if (channel == null)
-      throw new GraphQLError('channel not found', {
-        extensions: { code: 'NOT_FOUND' }
-      })
-
     return channel
   }
 
   @Mutation()
   async channelCreate(
+    @CurrentUserId() userId: string,
     @Args('input') input: ChannelCreateInput
   ): Promise<Channel | undefined> {
-    // eslint-disable-next-line no-useless-catch
-    try {
-      return await this.prismaService.$transaction(async (tx) => {
-        const channel = await tx.channel.create({
-          data: {
-            id: uuidv4(),
-            ...input
-          }
-        })
-        return channel
-      })
-    } catch (err) {
-      throw err
+    const nexus = await this.prismaService.nexus.findUnique({
+      where: { id: input.nexusId, userNexuses: { every: { userId } } }
+    })
+    if (nexus == null) {
+      throw new GraphQLError('Nexus not found.')
     }
+    const channel = await this.prismaService.channel.create({
+      data: {
+        id: uuidv4(),
+        ...input,
+        nexusId: nexus.id
+      },
+      include: {
+        youtube: true
+      }
+    })
+    return channel
   }
 
   @Mutation()
   async channelUpdate(
+    @CurrentUserId() userId: string,
     @Args('id') id: string,
     @Args('input') input: ChannelUpdateInput
   ): Promise<Channel> {
-    const channel = await this.prismaService.channel.findUnique({
-      where: { id }
+    const channel = await this.prismaService.channel.update({
+      where: {
+        id,
+        nexus: { userNexuses: { every: { userId } } }
+      },
+      data: {
+        name: input.name ?? undefined,
+        nexusId: input.nexusId ?? undefined,
+        platform: input.platform ?? undefined
+      },
+      include: {
+        youtube: true
+      }
     })
-    if (channel == null)
-      throw new GraphQLError('channel not found', {
-        extensions: { code: 'NOT_FOUND' }
-      })
-    // eslint-disable-next-line no-useless-catch
-    try {
-      return await this.prismaService.$transaction(async (tx) => {
-        const updatedChannel = await tx.channel.update({
-          where: { id },
-          data: {
-            name: input.name ?? undefined,
-            nexusId: input.nexusId ?? undefined,
-            platform: input.platform ?? undefined
-          }
-        })
-        return updatedChannel
-      })
-    } catch (err) {
-      throw err
-    }
+    return channel
   }
 
   @Mutation()
-  async channelDelete(@Args('id') id: string): Promise<Channel> {
-    const channel = await this.prismaService.channel.findUnique({
-      where: { id }
+  async channelDelete(
+    @CurrentUserId() userId: string,
+    @Args('id') id: string
+  ): Promise<Channel> {
+    const channel = await this.prismaService.channel.update({
+      where: {
+        id,
+        nexus: { userNexuses: { every: { userId } } }
+      },
+      data: {
+        name: 'dd'
+      }
     })
-
-    if (channel == null)
-      throw new GraphQLError('channel not found', {
-        extensions: { code: 'NOT_FOUND' }
-      })
-
-    // eslint-disable-next-line no-useless-catch
-    try {
-      return await this.prismaService.channel.delete({
-        where: { id }
-      })
-    } catch (err) {
-      throw err
-    }
+    return channel
   }
 
   @Mutation()
   async connectYoutubeChannel(
+    @CurrentUserId() userId: string,
     @Args('input') input: ConnectYoutubeChannelInput
   ): Promise<Channel | null> {
-    try {
-      const tokenResponse = await getRefreshToken(
-        input.authCode,
-        input.redirectUri
-      )
-
-      const tokenData: { refresh_token: string; access_token: string } =
-        await tokenResponse.json()
-
-      const youtubeChannel: {
-        id: string
-        snippet: { thumbnails: { high: { url: string } } }
-      } = await getYouTubeChannel(tokenData.access_token)
-
-      await this.prismaService.channelYoutubeCredential.create({
-        data: {
-          channelId: input.channelId,
-          youtubeId: youtubeChannel.id,
-          imageUrl: youtubeChannel.snippet.thumbnails.high.url,
-          redirectUrl: input.redirectUri,
-          refreshToken: tokenData.refresh_token
-        }
-      })
-    } catch (error) {
-      console.error('Error getting token:', error.message)
-    }
-
-    return await this.prismaService.channel.findUnique({
-      where: { id: input.channelId }
+    const authResponse = await this.googleOAuth.getAccessToken({
+      code: input.authCode,
+      grant_type: 'authorization_code',
+      redirect_uri: input.redirectUri
     })
-  }
-}
-
-async function getRefreshToken(
-  authCode: string,
-  redirectUri: string
-): Promise<Response> {
-  const body = new FormData()
-  body.append('code', authCode)
-  body.append('client_id', process.env.GOOGLE_CLIENT_ID ?? '')
-  body.append('client_secret', process.env.GOOGLE_CLIENT_SECRET ?? '')
-  body.append('redirect_uri', redirectUri)
-  body.append('grant_type', 'authorization_code')
-
-  try {
-    const response = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      body
+    const youtubeChannels = await this.googleYoutube.getChannels({
+      accessToken: authResponse.access_token
     })
 
-    return response
-  } catch (error) {
-    console.error('Error fetching refresh token:', error.message)
-    throw error
-  }
-}
-
-async function getAccessToken(refreshToken: string): Promise<Response> {
-  const body = new FormData()
-  body.append('client_id', process.env.GOOGLE_CLIENT_ID ?? '')
-  body.append('client_secret', process.env.GOOGLE_CLIENT_SECRET ?? '')
-  body.append('refresh_token', refreshToken)
-  body.append('grant_type', 'refresh_token')
-
-  try {
-    const response = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      body
-    })
-
-    return response
-  } catch (error) {
-    console.error('Error fetching access token:', error.message)
-    throw error
-  }
-}
-
-async function getYouTubeChannel(accessToken: string): Promise<{
-  id: string
-  snippet: { thumbnails: { high: { url: string } } }
-}> {
-  try {
-    const response = await fetch(
-      'https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true',
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
+    const channel = await this.prismaService.channel.findUnique({
+      where: {
+        id: input.channelId,
+        nexus: { userNexuses: { every: { userId } } }
       }
-    )
-
-    const responseData = await response.json()
-
-    if (responseData.items.length === 0) {
-      throw new Error('No channel data found.')
+    })
+    if (channel == null) {
+      throw new GraphQLError('Channel not found.')
     }
-
-    // returns the first channel
-    return responseData.items[0]
-  } catch (error) {
-    console.error('Error fetching YouTube channel:', error.message)
-    throw error
+    await this.prismaService.channelYoutube.create({
+      data: {
+        channelId: channel?.id,
+        title: youtubeChannels.items[0].snippet.title,
+        description: youtubeChannels.items[0].snippet.description,
+        youtubeId: youtubeChannels.items[0].id,
+        imageUrl: youtubeChannels.items[0].snippet.thumbnails.high.url,
+        refreshToken: authResponse.refresh_token
+      }
+    })
+    const _channel = await this.prismaService.channel.findUnique({
+      where: {
+        id: input.channelId,
+        nexus: { userNexuses: { every: { userId } } }
+      },
+      include: {
+        youtube: true
+      }
+    })
+    return _channel
   }
 }
