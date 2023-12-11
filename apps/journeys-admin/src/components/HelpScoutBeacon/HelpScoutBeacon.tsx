afb682c0589a3b18517e7a4b3d2a1a9e68ebcc69
Tanner <tanner.fleming@gmail.com>
2023-12-11 16:27:47 +0000

    fix: lazy on load beacon to avoid any performance effects

--- apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
+++ apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
@@ -10,12 +10,12 @@ export function HelpScoutBeacon(): ReactElement {
       <Script
         id="help-scout"
         className="help-scout"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       >{`!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});`}</Script>
       <Script
         id="help-scout-init"
         className="help-scout-init"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       >{`
         window.Beacon('init', '4f0abc47-b29c-454a-b618-39b34fd116b8')`}</Script>
       <style>{`
