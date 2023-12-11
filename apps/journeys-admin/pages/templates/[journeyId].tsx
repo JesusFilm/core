b27bdfcfd1fa7476e971e006159a78b8278d8a7a
Tataihono Nikora <tataihono.nikora@gmail.com>
2023-12-11 04:44:35 +0000

    fix: load more template details via ssr

--- apps/journeys-admin/pages/templates/[journeyId].tsx
+++ apps/journeys-admin/pages/templates/[journeyId].tsx
@@ -7,6 +7,12 @@ import { useTranslation } from 'react-i18next'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 
+import { GetJourney, GetJourneyVariables } from '../../__generated__/GetJourney'
+import {
+  GetJourneys,
+  GetJourneysVariables
+} from '../../__generated__/GetJourneys'
+import { GetTags } from '../../__generated__/GetTags'
 import { ONBOARDING_IDS } from '../../src/components/OnboardingPanel/OnboardingList/OnboardingList'
 import { PageWrapper } from '../../src/components/PageWrapper'
 import { TemplateView } from '../../src/components/TemplateView'
@@ -15,8 +21,10 @@ import {
   GET_JOURNEY,
   useJourneyQuery
 } from '../../src/libs/useJourneyQuery/useJourneyQuery'
+import { GET_JOURNEYS } from '../../src/libs/useJourneysQuery/useJourneysQuery'
+import { GET_TAGS } from '../../src/libs/useTagsQuery/useTagsQuery'
 
-function TemplateDetails(): ReactElement {
+function TemplateDetailsPage(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const router = useRouter()
   const user = useUser()
@@ -55,12 +63,38 @@ export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
     locale
   })
 
+  if (params?.journeyId == null) {
+    return {
+      redirect: {
+        destination: '/templates',
+        permanent: false
+      }
+    }
+  }
+
   try {
-    await apolloClient.query({
+    // TemplateDetailsPage
+    const { data } = await apolloClient.query<GetJourney, GetJourneyVariables>({
       query: GET_JOURNEY,
       variables: {
-        id: params?.journeyId
+        id: params.journeyId.toString()
       }
+    })
+    const tagIds = data.journey.tags.map((tag) => tag.id)
+    // src/components/TemplateView/TemplateView.tsx useJourneysQuery
+    await apolloClient.query<GetJourneys, GetJourneysVariables>({
+      query: GET_JOURNEYS,
+      variables: {
+        where: {
+          template: true,
+          orderByRecent: true,
+          tagIds
+        }
+      }
+    })
+    // src/components/TemplateView/TemplateTags/TemplateTags.tsx useTagsQuery
+    await apolloClient.query<GetTags>({
+      query: GET_TAGS
     })
   } catch (error) {
     if (error.message === 'journey not found') {
@@ -94,4 +128,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }
 }
 
-export default withUser()(TemplateDetails)
+export default withUser()(TemplateDetailsPage)
