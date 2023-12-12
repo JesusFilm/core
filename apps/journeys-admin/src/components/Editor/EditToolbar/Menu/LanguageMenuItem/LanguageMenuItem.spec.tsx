f9f4d0a15398ce7fdc58a66d015885b10f53bd6c
John Geronimo <johnageronimo1@gmail.com>
2023-12-12 01:37:54 +0000

    test: update language menu item

--- apps/journeys-admin/src/components/Editor/EditToolbar/Menu/LanguageMenuItem/LanguageMenuItem.spec.tsx
+++ apps/journeys-admin/src/components/Editor/EditToolbar/Menu/LanguageMenuItem/LanguageMenuItem.spec.tsx
@@ -1,5 +1,6 @@
 import { MockedProvider } from '@apollo/client/testing'
 import { fireEvent, render, waitFor } from '@testing-library/react'
+import { NextRouter, useRouter } from 'next/router'
 import { SnackbarProvider } from 'notistack'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
@@ -9,8 +10,25 @@ import { defaultJourney } from '../../../data'
 
 import { LanguageMenuItem } from './LanguageMenuItem'
 
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn(() => ({ query: { tab: 'active' } }))
+}))
+
+const mockedUseRouter = useRouter as jest.MockedFunction<typeof useRouter>
+
 describe('LanguageMenuItem', () => {
   it('should open edit journey language dialog', async () => {
+    const push = jest.fn()
+    const on = jest.fn()
+    mockedUseRouter.mockReturnValue({
+      query: { param: null },
+      push,
+      events: {
+        on
+      }
+    } as unknown as NextRouter)
+
     const onClose = jest.fn()
     const { getByRole } = render(
       <SnackbarProvider>
@@ -33,5 +51,15 @@ describe('LanguageMenuItem', () => {
     await waitFor(() => expect(getByRole('dialog')).toBeInTheDocument())
     fireEvent.click(getByRole('button', { name: 'Cancel' }))
     await waitFor(() => expect(onClose).toHaveBeenCalled())
+
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith({
+        query: { param: 'languages' },
+        push,
+        events: {
+          on
+        }
+      })
+    })
   })
 })
