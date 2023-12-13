91c2f8ea93fb779d51dd3bda55317f1ba0448c3c
John Geronimo <johnageronimo1@gmail.com>
2023-12-13 00:57:31 +0000

    feat: prefill help scout search

--- apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
+++ apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@mui/material/styles'
 import Script from 'next/script'
-import { ReactElement, useEffect } from 'react'
+import { ReactElement } from 'react'
 
 interface EventObject {
   type: string
@@ -30,25 +30,6 @@ declare global {
 
 export function HelpScoutBeacon(): ReactElement {
   const { breakpoints, zIndex } = useTheme()
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (window.Beacon != null) {
-        clearInterval(intervalId)
-        window.Beacon('on', 'open', () => {
-          console.log('I have been clicked')
-          const urlParam = new URLSearchParams(window.location.search)
-          const param = urlParam.get('param')
-          console.log(param)
-
-          if (urlParam.has('param') && param === 'social') {
-            window.Beacon?.('search', 'social')
-          }
-        })
-      }
-    }, 1000)
-  }, [])
-
   return (
     <>
       <Script
--- apps/journeys-admin/src/libs/setBeaconPageViewed/setBeaconPageViewed.ts
+++ apps/journeys-admin/src/libs/setBeaconPageViewed/setBeaconPageViewed.ts
@@ -1,10 +1,17 @@
+import camelCase from 'lodash/camelCase'
+import startCase from 'lodash/startCase'
+
 export function setBeaconPageViewed(route: string): void {
-  console.log(document.location.href)
   if (window.Beacon != null) {
     window.Beacon('event', {
       type: 'page-viewed',
       url: document.location.href,
       title: `Clicked on ${route}`
+    })
+
+    window.Beacon('on', 'open', () => {
+      const query = startCase(camelCase(route))
+      window.Beacon?.('search', query)
     })
   }
 }
