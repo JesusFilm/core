45e5ec15fe2e74a677a16f895676a80c88e4bd91
Siyang <csiyang@yahoo.co.nz>
2023-12-14 21:44:08 +0000

    feat: add token field locking

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Form/Credentials/ApiTokenTextField/ApiTokenTextField.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Form/Credentials/ApiTokenTextField/ApiTokenTextField.tsx
@@ -1,7 +1,12 @@
 import { gql, useMutation } from '@apollo/client'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
 import { useSnackbar } from 'notistack'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+
+import Lock1Icon from '@core/shared/ui/icons/Lock1'
+import LockOpen1Icon from '@core/shared/ui/icons/LockOpen1'
 
 import { FormBlockUpdateCredentials } from '../../../../../../../../../__generated__/FormBlockUpdateCredentials'
 import { TextFieldForm } from '../../../../../../../TextFieldForm'
@@ -39,6 +44,22 @@ export function ApiTokenTextField({
     useMutation<FormBlockUpdateCredentials>(FORM_BLOCK_UPDATE)
   const { enqueueSnackbar } = useSnackbar()
   const { t } = useTranslation('apps-journeys-admin')
+  const [lockTextField, setLockTextField] = useState(true)
+  const apiTokenTextFieldId = 'apiTokenTextFieldId'
+
+  useEffect(() => {
+    if (!lockTextField) {
+      const input = document.getElementById(
+        apiTokenTextFieldId
+      ) as HTMLInputElement
+      input?.focus()
+      input?.select()
+    }
+  }, [lockTextField])
+
+  const handleToggleLock = (): void => {
+    setLockTextField((lock) => !lock)
+  }
 
   async function handleSubmitApiToken(apiToken: string): Promise<void> {
     if (id == null || apiToken === placeHolderToken) return
@@ -65,11 +86,24 @@ export function ApiTokenTextField({
 
   return (
     <TextFieldForm
-      id="apiToken"
+      id={apiTokenTextFieldId}
       label={t('Api Token')}
+      disabled={lockTextField}
       type="password"
       initialValue={placeHolderToken}
       onSubmit={handleSubmitApiToken}
+      startIcon={
+        <InputAdornment position="start">
+          <IconButton
+            aria-label="toggle field lock"
+            onClick={handleToggleLock}
+            onMouseDown={(e) => e.preventDefault()}
+            edge="start"
+          >
+            {lockTextField ? <Lock1Icon /> : <LockOpen1Icon />}
+          </IconButton>
+        </InputAdornment>
+      }
     />
   )
 }
