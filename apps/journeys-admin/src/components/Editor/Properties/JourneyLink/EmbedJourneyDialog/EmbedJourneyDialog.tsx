3faef3e2d9f2d544d55560c5fe1611fa6bb77b83
Tanner <tanner.fleming@gmail.com>
2023-12-14 22:26:36 -0600

    fix: add autoplay into embed iframe (#2049)

--- apps/journeys-admin/src/components/Editor/Properties/JourneyLink/EmbedJourneyDialog/EmbedJourneyDialog.tsx
+++ apps/journeys-admin/src/components/Editor/Properties/JourneyLink/EmbedJourneyDialog/EmbedJourneyDialog.tsx
@@ -38,7 +38,7 @@ export function EmbedJourneyDialog({
     process.env.NEXT_PUBLIC_JOURNEYS_URL ?? 'your.nextstep.is'
   }/embed/${
     journey?.slug as string
-  }" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%; border: none;" allow="fullscreen"></iframe></div><script>window.addEventListener('message', event => { if(event.origin==='https://your.nextstep.is'){ const iframe=document.getElementById('jfm-iframe')
+  }" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%; border: none;" allow="fullscreen; autoplay"></iframe></div><script>window.addEventListener('message', event => { if(event.origin==='https://your.nextstep.is'){ const iframe=document.getElementById('jfm-iframe')
 if(event.data === true){ 
 iframe.style.position="fixed"
 iframe.style.zIndex="999999999999999999999"
