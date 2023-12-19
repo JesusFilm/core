6e81771bd8120ab668c304c125ae377d51770599
Mike Allison <dj.mikeallison@gmail.com>
2023-12-19 21:45:28 +0000

    feat: message queue

--- apps/api-journeys/src/app/modules/email/email.consumer.spec.ts
+++ apps/api-journeys/src/app/modules/email/email.consumer.spec.ts
@@ -1,49 +1,49 @@
-// import { Job } from 'bullmq'
+import { Job } from 'bullmq'
 
-// import { EmailConsumer, EmailJob } from './email.consumer'
+import { EmailConsumer, EmailJob } from './email.consumer'
 
-// describe('EmailConsumer', () => {
-//   let emailConsumer: EmailConsumer
+describe('EmailConsumer', () => {
+  let emailConsumer: EmailConsumer
 
-//   beforeEach(() => {
-//     emailConsumer = new EmailConsumer()
-//   })
+  beforeEach(() => {
+    emailConsumer = new EmailConsumer()
+  })
 
-//   it('should process the email job', async () => {
-//     const job: Job<EmailJob, any, string> = {
-//       name: 'emailJob',
-//       data: {
-//         email: 'test@example.com',
-//         subject: 'Test Subject',
-//         body: 'Test Body'
-//       }
-//     } as unknown as Job<EmailJob, any, string>
+  it.skip('should process the email job', async () => {
+    const job: Job<EmailJob, any, string> = {
+      name: 'emailJob',
+      data: {
+        email: 'test@example.com',
+        subject: 'Test Subject',
+        body: 'Test Body'
+      }
+    } as unknown as Job<EmailJob, any, string>
 
-//     // Mock the SES sendEmail method
-//     const sendEmailMock = jest.fn().mockReturnValue({ promise: jest.fn() })
-//     jest.mock('aws-sdk', () => ({
-//       SES: jest.fn().mockImplementation(() => ({
-//         sendEmail: sendEmailMock
-//       }))
-//     }))
+    // Mock the SES sendEmail method
+    const sendEmailMock = jest.fn().mockReturnValue({ promise: jest.fn() })
+    jest.mock('aws-sdk', () => ({
+      SES: jest.fn().mockImplementation(() => ({
+        sendEmail: sendEmailMock
+      }))
+    }))
 
-//     await emailConsumer.process(job)
+    await emailConsumer.process(job)
 
-//     expect(sendEmailMock).toHaveBeenCalledWith({
-//       Source: 'support@nextstep.is',
-//       Destination: { ToAddresses: [job.data.email] },
-//       Message: {
-//         Subject: {
-//           Charset: 'UTF-8',
-//           Data: job.data.subject
-//         },
-//         Body: {
-//           Html: {
-//             Charset: 'UTF-8',
-//             Data: job.data.body
-//           }
-//         }
-//       }
-//     })
-//   })
-// })
+    expect(sendEmailMock).toHaveBeenCalledWith({
+      Source: 'support@nextstep.is',
+      Destination: { ToAddresses: [job.data.email] },
+      Message: {
+        Subject: {
+          Charset: 'UTF-8',
+          Data: job.data.subject
+        },
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: job.data.body
+          }
+        }
+      }
+    })
+  })
+})
