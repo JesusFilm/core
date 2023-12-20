58dfdb1254b51e385771069da6a27373b21487e0
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2023-12-21 08:55:46 +1300

    feat: formblock api token field improvements (#2146)
    Co-authored-by: Nicholas Yao <nicholasyaoyao@gmail.com>
    Co-authored-by: Nicholas Yao <72791134+nicholasyao974@users.noreply.github.com>

--- apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Form/Credentials/ApiTokenTextField/ApiTokenTextField.tsx
+++ apps/journeys-admin/src/components/Editor/ControlPanel/Attributes/blocks/Form/Credentials/ApiTokenTextField/ApiTokenTextField.tsx
@@ -1,7 +1,12 @@
 import { gql, useMutation } from '@apollo/client'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
 import { useSnackbar } from 'notistack'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+
+import Lock1Icon from '@core/shared/ui/icons/Lock1'
+import LockOpen1Icon from '@core/shared/ui/icons/LockOpen1'
 
 import { FormBlockUpdateCredentials } from '../../../../../../../../../__generated__/FormBlockUpdateCredentials'
 import { TextFieldForm } from '../../../../../../../TextFieldForm'
@@ -21,24 +26,49 @@ export const FORM_BLOCK_UPDATE = gql`
       }
       projectId
       formSlug
+      apiTokenExists
     }
   }
 `
 
 interface ApiTokenTextFieldProps {
   id?: string
+  apiTokenExists: boolean
+  loading: boolean
 }
 
 export const placeHolderToken =
   'thisIsAFakeApiTokenTheReaOneIsNeverShowAgainInTheFrontEnd!!!'
 
 export function ApiTokenTextField({
-  id
+  id,
+  apiTokenExists,
+  loading
 }: ApiTokenTextFieldProps): ReactElement {
   const [formBlockUpdateCredentials] =
     useMutation<FormBlockUpdateCredentials>(FORM_BLOCK_UPDATE)
   const { enqueueSnackbar } = useSnackbar()
   const { t } = useTranslation('apps-journeys-admin')
+  const [lockTextField, setLockTextField] = useState(true)
+  const apiTokenTextFieldId = 'apiTokenTextFieldId'
+
+  useEffect(() => {
+    if (!lockTextField) {
+      const input = document.getElementById(
+        apiTokenTextFieldId
+      ) as HTMLInputElement
+      input?.focus()
+      input?.select()
+    }
+  }, [lockTextField])
+
+  useEffect(() => {
+    if (!loading && !apiTokenExists) setLockTextField(false)
+  }, [loading, apiTokenExists])
+
+  const handleToggleLock = (): void => {
+    setLockTextField((lock) => !lock)
+  }
 
   async function handleSubmitApiToken(apiToken: string): Promise<void> {
     if (id == null || apiToken === placeHolderToken) return
@@ -51,6 +81,7 @@ export function ApiTokenTextField({
           }
         }
       })
+      if (apiToken !== '') setLockTextField(true)
       enqueueSnackbar(t('API Token updated'), {
         variant: 'success',
         preventDuplicate: true
@@ -65,11 +96,23 @@ export function ApiTokenTextField({
 
   return (
     <TextFieldForm
-      id="apiToken"
+      id={apiTokenTextFieldId}
       label={t('Api Token')}
+      disabled={lockTextField}
       type="password"
-      initialValue={placeHolderToken}
+      initialValue={loading || !apiTokenExists ? '' : placeHolderToken}
       onSubmit={handleSubmitApiToken}
+      startIcon={
+        <InputAdornment position="start">
+          <IconButton
+            aria-label="toggle field lock"
+            onClick={handleToggleLock}
+            edge="start"
+          >
+            {lockTextField ? <Lock1Icon /> : <LockOpen1Icon />}
+          </IconButton>
+        </InputAdornment>
+      }
     />
   )
 }
