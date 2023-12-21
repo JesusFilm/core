d87186c306e826d2c91c226451405b5085b54633
Mike Allison <dj.mikeallison@gmail.com>
2023-12-21 04:14:09 +0000

    feat: separate

--- apps/api-languages/infrastructure/main.tf
+++ apps/api-languages/infrastructure/main.tf
@@ -17,18 +17,20 @@ module "seed" {
     "DATABASE_PASS",
     "DATABASE_URL",
     "DATABASE_USER",
-    "PG_DATABASE_URL"
+    "PG_DATABASE_URL",
+    "PG_DATABASE_URL_LANGUAGES",
   ]
   task_execution_role_arn = var.ecs_config.task_execution_role_arn
   env                     = var.env
 }
 
 module "database" {
-  source                = "../../../infrastructure/modules/aws/aurora"
-  name                  = local.service_config.name
-  env                   = var.env
-  doppler_token         = var.doppler_token
-  doppler_project       = local.service_config.name
-  subnet_group_name     = var.subnet_group_name
-  vpc_security_group_id = var.vpc_security_group_id
+  source                  = "../../../infrastructure/modules/aws/aurora"
+  name                    = local.service_config.name
+  env                     = var.env
+  doppler_token           = var.doppler_token
+  doppler_project         = local.service_config.name
+  subnet_group_name       = var.subnet_group_name
+  vpc_security_group_id   = var.vpc_security_group_id
+  PG_DATABASE_URL_ENV_VAR = "PG_DATABASE_URL_LANGUAGES"
 }
--- infrastructure/modules/aws/aurora/main.tf
+++ infrastructure/modules/aws/aurora/main.tf
@@ -41,12 +41,12 @@ resource "aws_rds_cluster_instance" "default" {
 }
 
 resource "aws_ssm_parameter" "parameter" {
-  name      = "/ecs/${var.name}/${var.env}/PG_DATABASE_URL"
+  name      = "/ecs/${var.name}/${var.env}/${var.PG_DATABASE_URL_ENV_VAR}"
   type      = "SecureString"
   value     = "postgresql://${aws_rds_cluster.default.master_username}:${urlencode(random_password.password.result)}@${aws_rds_cluster.default.endpoint}:${aws_rds_cluster.default.port}/${var.env}?schema=public"
   overwrite = true
   tags = {
-    name = "PG_DATABASE_URL"
+    name = var.PG_DATABASE_URL_ENV_VAR
   }
 }
 
@@ -58,7 +58,7 @@ resource "doppler_secret" "rds_password" {
 }
 
 resource "doppler_secret" "rds_url" {
-  name    = "PG_DATABASE_URL"
+  name    = var.PG_DATABASE_URL_ENV_VAR
   config  = var.env == "prod" ? "prd" : "stg"
   project = var.doppler_project
   value   = "postgresql://${aws_rds_cluster.default.master_username}:${urlencode(random_password.password.result)}@${aws_rds_cluster.default.endpoint}:${aws_rds_cluster.default.port}/${var.env}?schema=public"
