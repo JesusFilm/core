2291be757daf65cde36dc48af958341df705ba79
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-12-21 05:38:47 +0000

    test: upload test-results

--- apps/journeys-admin-e2e/src/e2e/journeys-admin.spec.ts
+++ apps/journeys-admin-e2e/src/e2e/journeys-admin.spec.ts
@@ -1,18 +1,18 @@
 import { expect, test } from '@playwright/test'
 
+import { LandingPage } from '../pages/landing-page'
+
 /* 
 This is just a sample test
 Test that 'journeys-admin' part of the URL
 */
 test('sample journeys-admin e2e test', async ({ page }) => {
-  await page.goto('/')
+  const landingPage = new LandingPage(page)
+  await landingPage.goToAdminUrl()
 
   // Get and log the current URL
   const url = page.url()
   console.log('Current URL:', url)
-
-  // Test the URL
-  // await expect(page).toHaveURL(/.*journeys-admin/)
 
   await expect(page).toHaveScreenshot('home-page.png', {
     animations: 'disabled',
