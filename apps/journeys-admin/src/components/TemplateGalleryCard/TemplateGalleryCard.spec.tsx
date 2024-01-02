4a119e97153e02b2ed46558b6ee8741379cfed3d
Mike Allison <dj.mikeallison@gmail.com>
2024-01-02 21:18:51 +0000

    fix: broken test year

--- apps/journeys-admin/src/components/TemplateGalleryCard/TemplateGalleryCard.spec.tsx
+++ apps/journeys-admin/src/components/TemplateGalleryCard/TemplateGalleryCard.spec.tsx
@@ -59,8 +59,7 @@ describe('TemplateGalleryCard', () => {
     expect(getByRole('img').attributes.getNamedItem('src')?.value).toBe(
       'https://images.unsplash.com/photo-1508363778367-af363f107cbb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=chester-wade-hLP7lVm4KUE-unsplash.jpg&w=1920'
     )
-    const year = new Date().getFullYear()
-    expect(getByText(`Aug, ${year} ● English`)).toBeInTheDocument()
+    expect(getByText('Aug, 2023 ● English')).toBeInTheDocument()
     expect(
       getByRole('heading', { name: 'A Template Heading' })
     ).toBeInTheDocument()
@@ -90,7 +89,8 @@ describe('TemplateGalleryCard', () => {
         }}
       />
     )
-    expect(getByText('Aug, 2023 ● Kalagan (TK)')).toBeInTheDocument()
+    const year = new Date().getFullYear()
+    expect(getByText(`Aug, ${year} ● Kalagan (TK)`)).toBeInTheDocument()
   })
 
   it('should link to template details', () => {
