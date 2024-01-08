604326888c13258a7ad488c4d011a00e973f8b66
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-01-08 02:06:20 +0000

    test: enable to run locally

--- apps/journeys-admin-e2e/src/framework/helpers.ts
+++ apps/journeys-admin-e2e/src/framework/helpers.ts
@@ -1,5 +1,7 @@
+import user from '../../user.json'
+
 export async function getEmail(): Promise<string> {
-  const email = process.env.PLAYWRIGHT_EMAIL?.toString()
+  const email = process.env.PLAYWRIGHT_EMAIL?.toString() ?? user.email
 
   if (!email || email === '') {
     throw new Error('Email was not provided via environment variable')
@@ -8,7 +10,7 @@ export async function getEmail(): Promise<string> {
 }
 
 export async function getPassword(): Promise<string> {
-  const password = process.env.PLAYWRIGHT_PASSWORD?.toString()
+  const password = process.env.PLAYWRIGHT_PASSWORD?.toString() ?? user.password
 
   if (!password || password === '') {
     throw new Error('Password was not provided via environment variable')
@@ -17,7 +19,7 @@ export async function getPassword(): Promise<string> {
 }
 
 export async function getUser(): Promise<string> {
-  const firstAndLastName = process.env.PLAYWRIGHT_USER?.toString()
+  const firstAndLastName = process.env.PLAYWRIGHT_USER?.toString() ?? user.first_last_name
 
   if (!firstAndLastName || firstAndLastName === '') {
     throw new Error(
@@ -28,7 +30,7 @@ export async function getUser(): Promise<string> {
 }
 
 export async function getTeamName(): Promise<string> {
-  const teamName = process.env.PLAYWRIGHT_TEAM_NAME?.toString()
+  const teamName = process.env.PLAYWRIGHT_TEAM_NAME?.toString() ?? user.team_name
 
   if (!teamName || teamName === '') {
     throw new Error('Team name was not provided via environment variable')
