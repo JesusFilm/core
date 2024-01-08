2746f9749e89cb623bc26071b057432c21230b3c
Tanner <tanner.fleming@gmail.com>
2024-01-08 21:57:57 +0000

    chore: prettier

--- apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
+++ apps/journeys-admin/src/components/HelpScoutBeacon/HelpScoutBeacon.tsx
@@ -118,7 +118,7 @@ export function HelpScoutBeacon({
           position: 'fixed',
           top: { xs: 11.5, md: 11.5 },
           zIndex: zIndex.modal + 3,
-          right: { xs: 10, md: 15},
+          right: { xs: 10, md: 15 },
           width: 24,
           height: 24,
           color: matches ? 'secondary.dark' : 'background.paper'
