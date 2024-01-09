1ad14b4917d37530855339b193dca290067569f9
Nicholas Yao <nicholasyaoyao@gmail.com>
2024-01-09 01:32:22 +0000

    test: home page test cases

---
+++ apps/journeys-admin/src/components/SignIn/Home/Home.spec.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, waitFor } from '@testing-library/react'
+
+import { Home } from './Home'
+
+describe('Home', () => {
+  it('should render home page', () => {
+    const { getByText } = render(
+      <Home setActivePage={jest.fn()} setEmail={jest.fn()} />
+    )
+    expect(getByText('Log in or Sign up')).toBeInTheDocument()
+    expect(
+      getByText("No account? We'll create one for you automatically.")
+    ).toBeInTheDocument()
+  })
+
+  it('should require user to enter an email', async () => {
+    const { getByRole, getByText } = render(
+      <Home setActivePage={jest.fn()} setEmail={jest.fn()} />
+    )
+
+    fireEvent.focus(getByRole('textbox'))
+    fireEvent.blur(getByRole('textbox'))
+    await waitFor(() => expect(getByText('Required')).toBeInTheDocument())
+  })
+
+  it('should validate user email', async () => {
+    const { getByRole, getByText } = render(
+      <Home setActivePage={jest.fn()} setEmail={jest.fn()} />
+    )
+
+    fireEvent.change(getByRole('textbox'), {
+      target: { value: 'Invalid Email Address' }
+    })
+    fireEvent.blur(getByRole('textbox'))
+    await waitFor(() =>
+      expect(
+        getByText('Please enter a valid email address')
+      ).toBeInTheDocument()
+    )
+  })
+})
