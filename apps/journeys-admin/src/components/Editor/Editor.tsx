d707208fc9598c8a98bcffed97b38b914314180d
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-01-09 20:34:17 +0000

    refactor: merge journey edit with editor

--- apps/journeys-admin/src/components/Editor/Editor.tsx
+++ apps/journeys-admin/src/components/Editor/Editor.tsx
@@ -1,4 +1,7 @@
-import { ReactElement, ReactNode } from 'react'
+import Stack from '@mui/material/Stack'
+import { Theme } from '@mui/system/createTheme'
+import dynamic from 'next/dynamic'
+import { ComponentProps, ReactElement, useState } from 'react'
 
 import type { TreeBlock } from '@core/journeys/ui/block'
 import {
@@ -10,21 +13,54 @@ import { transformer } from '@core/journeys/ui/transformer'
 
 import { BlockFields_StepBlock as StepBlock } from '../../../__generated__/BlockFields'
 import { GetJourney_journey as Journey } from '../../../__generated__/GetJourney'
+import { PageWrapper } from '../PageWrapper'
 
+import { Canvas } from './Canvas'
 import { Properties } from './Properties'
+
+const ActionsTable = dynamic(
+  async () =>
+    await import(
+      /* webpackChunkName: "Editor/ActionsTable" */
+      './ActionsTable'
+    ).then((mod) => mod.ActionsTable),
+  { ssr: false }
+)
+const SocialPreview = dynamic(
+  async () =>
+    await import(
+      /* webpackChunkName: "Editor/SocialPreview" */
+      './SocialPreview'
+    ).then((mod) => mod.SocialPreview),
+  { ssr: false }
+)
 
 interface EditorProps {
   journey?: Journey
   selectedStepId?: string
-  children: ReactNode
   view?: ActiveJourneyEditContent
+  PageWrapperProps?: ComponentProps<typeof PageWrapper>
+}
+
+function bgColor(
+  theme: Theme,
+  view: ActiveJourneyEditContent,
+  hasAction: boolean
+): string {
+  if (
+    view === ActiveJourneyEditContent.SocialPreview ||
+    (hasAction && view === ActiveJourneyEditContent.Action)
+  ) {
+    return theme.palette.background.default
+  }
+  return theme.palette.background.paper
 }
 
 export function Editor({
   journey,
   selectedStepId,
-  children,
-  view
+  view,
+  PageWrapperProps
 }: EditorProps): ReactElement {
   const steps =
     journey != null
@@ -34,6 +70,7 @@ export function Editor({
     selectedStepId != null && steps != null
       ? steps.find(({ id }) => id === selectedStepId)
       : undefined
+  const [hasAction, setHasAction] = useState(false)
 
   return (
     <JourneyProvider value={{ journey, variant: 'admin' }}>
@@ -46,7 +83,31 @@ export function Editor({
           journeyEditContentComponent: view ?? ActiveJourneyEditContent.Canvas
         }}
       >
-        {children}
+        {({ journeyEditContentComponent }) => (
+          <PageWrapper {...PageWrapperProps}>
+            <Stack
+              data-testid="journey-edit-content"
+              flexGrow={1}
+              justifyContent="center"
+              sx={{
+                backgroundColor: (theme) =>
+                  bgColor(theme, journeyEditContentComponent, hasAction)
+              }}
+            >
+              {
+                {
+                  [ActiveJourneyEditContent.Canvas]: <Canvas />,
+                  [ActiveJourneyEditContent.Action]: (
+                    <ActionsTable
+                      hasAction={(action) => setHasAction(action)}
+                    />
+                  ),
+                  [ActiveJourneyEditContent.SocialPreview]: <SocialPreview />
+                }[journeyEditContentComponent]
+              }
+            </Stack>
+          </PageWrapper>
+        )}
       </EditorProvider>
     </JourneyProvider>
   )
