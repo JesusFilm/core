04e428b87be7af05f43a5072b1feea5dda5537b7
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-01-10 03:35:20 +0000

    test: fix lint

--- apps/journeys-admin-e2e/src/e2e/response-time.spec.ts
+++ apps/journeys-admin-e2e/src/e2e/response-time.spec.ts
@@ -6,7 +6,7 @@ import { LandingPage } from '../pages/landing-page'
 import { LoginPage } from '../pages/login-page'
 import { TopNav } from '../pages/top-nav'
 
-test('Home page - response time test', async ({ page, browser}) => {
+test('Home page - response time test', async ({ page, browser }) => {
   const landingPage = new LandingPage(page)
   const loginPage = new LoginPage(page)
   const topNav = new TopNav(page)
@@ -17,27 +17,29 @@ test('Home page - response time test', async ({ page, browser}) => {
   await loginPage.login()
 
   // Using Performanc.mark API
-  await page.evaluate(() => (window.performance.mark('Start:FoF')))    
-    
+  await page.evaluate(() => window.performance.mark('Start:FoF'))
+
   // Get team name from env vars compare it with actual team name in the app
   const teamName = await getTeamName()
   await topNav.clickTeamName(teamName)
   expect(await topNav.getTeamName()).toContain(teamName)
 
   // Using performance.mark API
-  await page.evaluate(() => (window.performance.mark('End:FoF')))
+  await page.evaluate(() => window.performance.mark('End:FoF'))
 
   // Get all performance marks
-  const getAllMarksJson = await page.evaluate(() => (JSON.stringify(window.performance.getEntriesByType("mark"))))
+  const getAllMarksJson = await page.evaluate(() =>
+    JSON.stringify(window.performance.getEntriesByType('mark'))
+  )
   const getAllMarks = await JSON.parse(getAllMarksJson)
   console.log('window.performance.getEntriesByType("mark")', getAllMarks)
 
   await browser.stopTracing()
 
   // Test that time taken between Start:FoF and End:FoF is less than 1700ms
-  const start = getAllMarks.find((obj) => obj.name === "Start:FoF")?.startTime;
-  const end = getAllMarks.find((obj) => obj.name === "End:FoF")?.startTime;
-  const diff = (end) - (start);
-  console.log("diff", diff);
-  expect(diff).toBeLessThan(7000);
+  const start = getAllMarks.find((obj) => obj.name === 'Start:FoF')?.startTime
+  const end = getAllMarks.find((obj) => obj.name === 'End:FoF')?.startTime
+  const diff = end - start
+  console.log('diff', diff)
+  expect(diff).toBeLessThan(7000)
 })
