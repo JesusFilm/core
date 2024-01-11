4f5a386564e4803d3eb580e44c845ddd9b7aaf06
Siyang <csiyang@yahoo.co.nz>
2024-01-11 01:16:02 +0000

    fix: enable all pointer events

--- apps/journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
+++ apps/journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
@@ -224,35 +224,33 @@ export function NavigationButton({
           sm: 'calc(100vh - 238px)',
           lg: 'calc(100% - 105px)'
         },
-        alignItems: 'center',
-        pointerEvents: 'none'
+        alignItems: 'center'
       }}
     >
-      {/* <Fade
+      <Fade
         in={canNavigate && showNavigation && !disabled}
         timeout={{ appear: 300, exit: 1000 }}
-      > */}
-      <IconButton
-        data-testid={`ConductorNavigationButton${capitalize(variant)}`}
-        size="small"
-        onClick={() => handleNav(variant)}
-        disableRipple
-        sx={{
-          pointerEvents: 'all',
-          mx: { xs: 2, lg: 8 },
-          mt: { xs: 17, md: 7, lg: 4 },
-          p: 2,
-          color: (theme) => theme.palette.common.white,
-          backgroundColor: (theme) => `${theme.palette.grey[700]}33`,
-          '&:hover': {
-            color: (theme) => theme.palette.common.white,
-            backgroundColor: (theme) => `${theme.palette.grey[700]}4d`
-          }
-        }}
       >
-        {alignment === 'left' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-      </IconButton>
-      {/* </Fade> */}
+        <IconButton
+          data-testid={`ConductorNavigationButton${capitalize(variant)}`}
+          size="small"
+          onClick={() => handleNav(variant)}
+          disableRipple
+          sx={{
+            mx: { xs: 2, lg: 8 },
+            mt: { xs: 17, md: 7, lg: 4 },
+            p: 2,
+            color: (theme) => theme.palette.common.white,
+            backgroundColor: (theme) => `${theme.palette.grey[700]}33`,
+            '&:hover': {
+              color: (theme) => theme.palette.common.white,
+              backgroundColor: (theme) => `${theme.palette.grey[700]}4d`
+            }
+          }}
+        >
+          {alignment === 'left' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </Fade>
     </NavigationContainer>
   )
 }
