988a2ed4b2fc32f42e27142abcccbdb65b373fd2
Kneesal <nisalcottingham@gmail.com>
2024-01-16 20:40:37 +0000

    fix: open fullscreen

--- apps/journeys/pages/api/oembed.ts
+++ apps/journeys/pages/api/oembed.ts
@@ -29,7 +29,25 @@ export default async function Handler(
       type: 'rich',
       version: '1.0',
       // oembed rich type required fields
-      html: `<div style="position:relative;width:100%;overflow:hidden;padding-top:150%"><iframe id="ns-iframe" src="${embedUrl}" style="position:absolute;top:0;left:0;bottom:0;right:0;width:100%;height:100%;border:none" allow="fullscreen; autoplay" allowfullscreen="true"></iframe></div><script>window.addEventListener("message",e=>{if("${providerUrl}"===e.origin){let t=document.getElementById("ns-iframe");!0===e.data?(t.style.position="fixed",t.style.zIndex="999999999999999999999"):(t.style.position="absolute",t.style.zIndex="auto")}});</script>`,
+      html: `<div style="position:relative;width:100%;overflow:hidden;padding-top:150%">
+      <iframe id="ns-iframe" src="${embedUrl}" style="position:absolute;top:0;left:0;bottom:0;right:0;width:100%;height:100%;border:none" allow="fullscreen; autoplay" allowfullscreen="true" onclick="openFullscreen();">
+      </iframe>
+      </div>
+      <script>window.addEventListener("message",e=>{if("${providerUrl}"===e.origin)
+      {let t=document.getElementById("ns-iframe");
+      !0===e.data?(t.style.position="fixed",t.style.zIndex="999999999999999999999"):(t.style.position="absolute",t.style.zIndex="auto")}});</script>
+      <script>
+      var t = document.getElementById("ns-iframe");
+      function openFullscreen() {
+        if (t.requestFullscreen) {
+          t.requestFullscreen();
+        } else if (t.webkitRequestFullscreen) { /* Safari */
+          t.webkitRequestFullscreen();
+        } else if (t.msRequestFullscreen) { /* IE11 */
+          t.msRequestFullscreen();
+        }
+      }
+      </script>`,
       width: 375,
       height: 500,
       // oembed optional fields
