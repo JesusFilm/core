fa780f074423ca5d54b26a9b22ea3ca9893fbf2a
Kneesal <nisalcottingham@gmail.com>
2024-01-17 20:29:31 +0000

    fix: revert on-boarding page changes

--- apps/journeys-admin/pages/onboarding-form.tsx
+++ apps/journeys-admin/pages/onboarding-form.tsx
@@ -13,8 +13,6 @@ import { formiumClient } from '@core/shared/ui/formiumClient'
 
 import { OnboardingForm } from '../src/components/OnboardingForm'
 import { OnboardingPageWrapper } from '../src/components/OnboardingPageWrapper'
-import { GET_LAST_ACTIVE_TEAM_ID_AND_TEAMS } from '../src/components/Team/TeamProvider'
-import { UPDATE_LAST_ACTIVE_TEAM_ID } from '../src/components/Team/TeamSelect/TeamSelect'
 import { initAndAuthApp } from '../src/libs/initAndAuthApp'
 
 interface OnboardingFormPageProps {
@@ -43,7 +41,7 @@ export const getServerSideProps = withUserTokenSSR({
   if (user == null)
     return { redirect: { permanent: false, destination: '/users/sign-in' } }
 
-  const { redirect, translations, apolloClient } = await initAndAuthApp({
+  const { redirect, translations } = await initAndAuthApp({
     user,
     locale,
     resolvedUrl
@@ -55,25 +53,9 @@ export const getServerSideProps = withUserTokenSSR({
     process.env.NEXT_PUBLIC_FORMIUM_PROJECT_SLUG ?? ''
   )
 
-  const query = await apolloClient.query({
-    query: GET_LAST_ACTIVE_TEAM_ID_AND_TEAMS
-  })
-
-  if (query.data.teams[0].id != null) {
-    await apolloClient.mutate({
-      mutation: UPDATE_LAST_ACTIVE_TEAM_ID,
-      variables: {
-        input: {
-          lastActiveTeamId: query.data.teams[0].id
-        }
-      }
-    })
-  }
-
   return {
     props: {
       form,
-      initialApolloState: apolloClient.cache.extract(),
       ...translations
     }
   }
