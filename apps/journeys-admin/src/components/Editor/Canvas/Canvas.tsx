4d64415302660fe316e94480b313f28f4fe53c9a
Zachary Annabell <zwsaqx@gmail.com>
2024-01-18 21:09:45 +0000

    refactor: remove unused imports

--- apps/journeys-admin/src/components/Editor/Canvas/Canvas.tsx
+++ apps/journeys-admin/src/components/Editor/Canvas/Canvas.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { BlockRenderer } from '@core/journeys/ui/BlockRenderer'
