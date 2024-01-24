17ca8cecf8430619c225ceb1be0a9f23b6d5445d
Zachary Annabell <zwsaqx@gmail.com>
2024-01-24 03:02:51 +0000

    fix: added button styling

--- apps/journeys-admin/src/components/JourneyFlow/nodes/BaseNode.tsx
+++ apps/journeys-admin/src/components/JourneyFlow/nodes/BaseNode.tsx
@@ -1,3 +1,4 @@
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
@@ -24,7 +25,7 @@ export function BaseNode({
   title,
   selected = false
 }: BaseNodeProps): ReactElement {
-  return (
+  return isTargetConnectable !== false ? (
     <Card
       sx={{
         borderRadius: 1,
@@ -60,16 +61,76 @@ export function BaseNode({
           {title}
         </Typography>
       </CardContent>
-      {isTargetConnectable !== false && (
-        <Handle type="target" position={Position.Left} />
-      )}
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{
+          width: 15,
+          height: 15,
+          background: '#3b3b3b',
+          borderRadius: '20%'
+        }}
+      />
       {isSourceConnectable !== false && (
         <Handle
           type="source"
           position={Position.Right}
           onConnect={onSourceConnect}
+          style={{
+            width: '15px',
+            height: '15px',
+            borderRadius: '20%',
+            background: '#3b3b3b'
+          }}
         />
       )}
     </Card>
+  ) : (
+    <Box
+      sx={{
+        borderRadius: 1,
+        outline: '2px solid',
+        outlineColor: (theme) =>
+          selected === true
+            ? theme.palette.primary.main
+            : selected === 'descendant'
+            ? theme.palette.divider
+            : 'transparent',
+        outlineOffset: '5px'
+      }}
+    >
+      <CardContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          width: NODE_WIDTH,
+          height: NODE_HEIGHT,
+          gap: 2
+        }}
+      >
+        {icon}
+        <Typography
+          sx={{
+            display: '-webkit-box',
+            '-webkit-box-orient': 'vertical',
+            '-webkit-line-clamp': '2',
+            overflow: 'hidden'
+          }}
+        >
+          {title}
+        </Typography>
+      </CardContent>
+      <Handle
+        type="source"
+        position={Position.Right}
+        onConnect={onSourceConnect}
+        style={{
+          width: '15px',
+          height: '15px',
+          background: '#3b3b3b'
+        }}
+      />
+    </Box>
   )
 }
