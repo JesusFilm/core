6e832b712445be35cb92807e473eea6d8e3da81e
JayChen2004 <ijaygt@gmail.com>
2024-01-24 00:19:01 +0000

    chore: adding extract translations

---
+++ apps/watch/i18next-parser.config.js
@@ -0,0 +1,8 @@
+const i18nextParserConfigBase = require('../../i18next-parser.config.base')
+
+module.exports = {
+  ...i18nextParserConfigBase,
+  input: ['src/**/*.{js,jsx,ts,tsx}', 'pages/**/*.{js,jsx,ts,tsx}']
+}
+
+
