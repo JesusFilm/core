4a9eeeb2d8ddf1cd022efb0b2a04a1363d48f4f6
Nicholas Yao <nicholasyaoyao@gmail.com>
2024-01-24 00:10:24 +0000

    feat: add other langauges

--- apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.tsx
+++ apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.tsx
@@ -20,8 +20,19 @@ export function LanguageSelector(): ReactElement {
         '496', // Français, French
         '584', // Português, Portuguese, Brazil
         '21028', // Español, Spanish, Latin American
-        '20615', // 普通話, Chinese, Mandarin
-        '3934' // Русский, Russian
+        '21754', // 中文, Chinese, Simplified
+        '3934', // Русский, Russian
+        '1444', // Afrikaans
+        '6788', // Farsi, Western
+        '6930', // Hebrew
+        '6464', // हिन्दी, Hindi
+        '16639', // Bahasa Indonesia, Indonesian (Yesus)
+        '7083', // 日本語, Japanese
+        '3804', // 한국어, Korean
+        '9198', // te Reo Māori, Maori
+        '1942', // Türkçe, Turkish
+        '407', // اُردُو, Urdu
+        '3887' // Tiếng Việt, Vietnamese
       ]
     }
   })
@@ -48,11 +59,44 @@ export function LanguageSelector(): ReactElement {
         case '21028':
           locale = 'es'
           break
-        case '20615':
+        case '21754':
           locale = 'zh'
           break
         case '3934':
           locale = 'ru'
+          break
+        case '1444':
+          locale = 'af'
+          break
+        case '6788':
+          locale = 'fa'
+          break
+        case '6930':
+          locale = 'he'
+          break
+        case '6464':
+          locale = 'hi'
+          break
+        case '16639':
+          locale = 'id'
+          break
+        case '7083':
+          locale = 'ja'
+          break
+        case '3804':
+          locale = 'ko'
+          break
+        case '9198':
+          locale = 'mi'
+          break
+        case '1942':
+          locale = 'tr'
+          break
+        case '407':
+          locale = 'ur'
+          break
+        case '3887':
+          locale = 'vi'
           break
         default:
           locale = 'en'
