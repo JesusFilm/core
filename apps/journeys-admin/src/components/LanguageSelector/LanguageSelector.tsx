66601a09882979ad97da2f23179f0a9b2089a002
Nicholas Yao <nicholasyaoyao@gmail.com>
2024-01-25 21:31:27 +0000

    refactor: map language ids via switch case

--- apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.tsx
+++ apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.tsx
@@ -31,19 +31,79 @@ export function LanguageSelector({
     languageId: '529'
   })
 
+  function getCrowdinId(languageId: string): string {
+    let crowdinId = ''
+    switch (languageId) {
+      case '1106':
+        crowdinId = 'de'
+        break
+      case '16639':
+        crowdinId = 'id'
+        break
+      case '1942':
+        crowdinId = 'tr'
+        break
+      case '21028':
+        crowdinId = 'es-ES'
+        break
+      case '21753':
+        crowdinId = 'zh-TW'
+        break
+      case '21754':
+        crowdinId = 'zh-CN'
+        break
+      case '22658':
+        crowdinId = 'ar'
+        break
+      case '3804':
+        crowdinId = 'ko'
+        break
+      case '3887':
+        crowdinId = 'vi'
+        break
+      case '3934':
+        crowdinId = 'ru'
+        break
+      case '407':
+        crowdinId = 'ur-PK'
+        break
+      case '496':
+        crowdinId = 'fr'
+        break
+      case '584':
+        crowdinId = 'pt-PT'
+        break
+      case '6464':
+        crowdinId = 'hi'
+        break
+      case '6788':
+        crowdinId = 'fa'
+        break
+      case '6930':
+        crowdinId = 'he'
+        break
+      case '7083':
+        crowdinId = 'ja'
+        break
+    }
+    return crowdinId
+  }
+
   useEffect(() => {
     const translationStatus = new TranslationStatus(credentials)
 
     if (data !== undefined) {
       translationStatus
         .getProjectProgress(518286)
-        .then((r) => {
+        .then((crowdinData) => {
+          console.log(crowdinData)
           const availableLanguages = data.languages.filter((l) => {
-            const crowdinLanguageData = r.data.find(
-              // need a better way to do this check - not sure it's always the same.
-              (cl) => cl.data.language.osxLocale.toLowerCase() === l.bcp47
+            const crowdinId = getCrowdinId(l.id)
+            const crowdinLanguageData = crowdinData.data.find(
+              (cl) => cl.data.languageId === crowdinId
             )
             return (
+              // always display English
               l.id === '529' ||
               crowdinLanguageData?.data.approvalProgress === 100
             )
