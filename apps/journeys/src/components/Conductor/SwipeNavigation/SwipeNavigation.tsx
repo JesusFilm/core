5721faea7811bf327d58ececfc8b918ea97814b6
Siyang <csiyang@yahoo.co.nz>
2024-01-31 22:49:28 +0000

    refactor: handle nav to useCallback

--- apps/journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
+++ apps/journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from '@apollo/client'
 import last from 'lodash/last'
-import { ReactElement, useEffect } from 'react'
+import { ReactElement, useCallback, useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import { useTranslation } from 'react-i18next'
 import { v4 as uuidv4 } from 'uuid'
@@ -43,102 +43,120 @@ export function SwipeNavigation({
   const onFirstStep = activeBlock === treeBlocks[0]
   const onLastStep = activeBlock === last(treeBlocks)
 
-  // should always be called with nextActiveBlock()
-  // should match with other handleNextNavigationEventCreate functions
-  // places used:
-  // libs/journeys/ui/src/components/Card/Card.tsx
-  // journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
-  // journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
-  function handleNextNavigationEventCreate(): void {
-    const id = uuidv4()
-    const stepName = getStepHeading(
-      activeBlock.id,
-      activeBlock.children,
+  const handleNav = useCallback(
+    (direction: 'next' | 'previous'): void => {
+      if (variant === 'admin') return
+
+      // should always be called with nextActiveBlock()
+      // should match with other handleNextNavigationEventCreate functions
+      // places used:
+      // libs/journeys/ui/src/components/Card/Card.tsx
+      // journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
+      // journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
+      function handleNextNavigationEventCreate(): void {
+        const id = uuidv4()
+        const stepName = getStepHeading(
+          activeBlock.id,
+          activeBlock.children,
+          treeBlocks,
+          t
+        )
+        const targetBlock = getNextBlock({ id: undefined, activeBlock })
+        const targetStepName =
+          targetBlock != null &&
+          getStepHeading(targetBlock.id, targetBlock.children, treeBlocks, t)
+
+        void stepNextEventCreate({
+          variables: {
+            input: {
+              id,
+              blockId: activeBlock.id,
+              label: stepName,
+              value: targetStepName,
+              nextStepId: targetBlock?.id
+            }
+          }
+        })
+
+        TagManager.dataLayer({
+          dataLayer: {
+            event: 'step_next',
+            eventId: id,
+            blockId: activeBlock.id,
+            stepName,
+            targetStepId: targetBlock?.id,
+            targetStepName
+          }
+        })
+      }
+      // should always be called with previousActiveBlock()
+      // should match with other handlePreviousNavigationEventCreate functions
+      // places used:
+      // libs/journeys/ui/src/components/Card/Card.tsx
+      // journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
+      // journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
+      function handlePreviousNavigationEventCreate(): void {
+        const id = uuidv4()
+        const stepName = getStepHeading(
+          activeBlock.id,
+          activeBlock.children,
+          treeBlocks,
+          t
+        )
+        const targetBlock = blockHistory[
+          blockHistory.length - 2
+        ] as TreeBlock<StepFields>
+        const targetStepName =
+          targetBlock != null &&
+          getStepHeading(targetBlock.id, targetBlock.children, treeBlocks, t)
+
+        void stepPreviousEventCreate({
+          variables: {
+            input: {
+              id,
+              blockId: activeBlock.id,
+              label: stepName,
+              value: targetStepName,
+              previousStepId: targetBlock?.id
+            }
+          }
+        })
+
+        TagManager.dataLayer({
+          dataLayer: {
+            event: 'step_prev',
+            eventId: id,
+            blockId: activeBlock.id,
+            stepName,
+            targetStepId: targetBlock?.id,
+            targetStepName
+          }
+        })
+      }
+
+      if (direction === 'next' && !onLastStep && !activeBlock.locked) {
+        handleNextNavigationEventCreate()
+        nextActiveBlock()
+      } else if (direction === 'previous' && !onFirstStep) {
+        handlePreviousNavigationEventCreate()
+        previousActiveBlock()
+      }
+    },
+    [
+      activeBlock,
+      nextActiveBlock,
+      previousActiveBlock,
+      onFirstStep,
+      onLastStep,
+      variant,
+      stepNextEventCreate,
+      stepPreviousEventCreate,
       treeBlocks,
-      t
-    )
-    const targetBlock = getNextBlock({ id: undefined, activeBlock })
-    const targetStepName =
-      targetBlock != null &&
-      getStepHeading(targetBlock.id, targetBlock.children, treeBlocks, t)
-
-    void stepNextEventCreate({
-      variables: {
-        input: {
-          id,
-          blockId: activeBlock.id,
-          label: stepName,
-          value: targetStepName,
-          nextStepId: targetBlock?.id
-        }
-      }
-    })
-
-    TagManager.dataLayer({
-      dataLayer: {
-        event: 'step_next',
-        eventId: id,
-        blockId: activeBlock.id,
-        stepName,
-        targetStepId: targetBlock?.id,
-        targetStepName
-      }
-    })
-  }
-  // should always be called with previousActiveBlock()
-  // should match with other handlePreviousNavigationEventCreate functions
-  // places used:
-  // libs/journeys/ui/src/components/Card/Card.tsx
-  // journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
-  // journeys/src/components/Conductor/SwipeNavigation/SwipeNavigation.tsx
-  function handlePreviousNavigationEventCreate(): void {
-    const id = uuidv4()
-    const stepName = getStepHeading(
-      activeBlock.id,
-      activeBlock.children,
-      treeBlocks,
-      t
-    )
-    const targetBlock = blockHistory[
-      blockHistory.length - 2
-    ] as TreeBlock<StepFields>
-    const targetStepName =
-      targetBlock != null &&
-      getStepHeading(targetBlock.id, targetBlock.children, treeBlocks, t)
-
-    void stepPreviousEventCreate({
-      variables: {
-        input: {
-          id,
-          blockId: activeBlock.id,
-          label: stepName,
-          value: targetStepName,
-          previousStepId: targetBlock?.id
-        }
-      }
-    })
-
-    TagManager.dataLayer({
-      dataLayer: {
-        event: 'step_prev',
-        eventId: id,
-        blockId: activeBlock.id,
-        stepName,
-        targetStepId: targetBlock?.id,
-        targetStepName
-      }
-    })
-  }
-  function handleNav(direction: 'next' | 'previous'): void {
-    if (variant === 'admin') return
-    if (direction === 'next' && !onLastStep && !activeBlock.locked) {
-      handleNextNavigationEventCreate()
-      nextActiveBlock()
-    } else if (direction === 'previous' && !onFirstStep) {
-      handlePreviousNavigationEventCreate()
-      previousActiveBlock()
-    }
-  }
+      blockHistory,
+      t,
+      getNextBlock
+    ]
+  )
 
   useEffect(() => {
     let touchstartX = 0
@@ -167,7 +185,7 @@ export function SwipeNavigation({
       document.removeEventListener('touchstart', touchStart)
       document.removeEventListener('touchend', touchEnd)
     }
-  }, [activeBlock, enableTouchMoveNext])
+  }, [activeBlock, enableTouchMoveNext, handleNav])
 
   return <></>
 }
