a9f0c1dbf81861a29f1806fb8bbc10b2500c7746
Nicholas Yao <nicholasyaoyao@gmail.com>
2024-01-31 22:46:33 +0000

    test: langauge change confirmation

--- apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.spec.tsx
+++ apps/journeys-admin/src/components/LanguageSelector/LanguageSelector.spec.tsx
@@ -2,6 +2,7 @@ import { MockedProvider } from '@apollo/client/testing'
 import { TranslationStatus } from '@crowdin/crowdin-api-client'
 import { fireEvent, render, waitFor } from '@testing-library/react'
 import { NextRouter, useRouter } from 'next/router'
+import { i18n } from 'next-i18next'
 
 import { GET_LANGUAGES } from '../../libs/useLanguagesQuery'
 
@@ -10,6 +11,13 @@ import { LanguageSelector } from './LanguageSelector'
 jest.mock('next/router', () => ({
   __esModule: true,
   useRouter: jest.fn()
+}))
+
+jest.mock('next-i18next', () => ({
+  __esModule: true,
+  i18n: {
+    language: 'en'
+  }
 }))
 
 const mockUseRouter = useRouter as jest.MockedFunction<typeof useRouter>
@@ -309,6 +317,121 @@ describe('LanguageSelector', () => {
     fireEvent.click(getByRole('button', { name: 'Open' }))
     await waitFor(() => fireEvent.click(getByText('Arabic, Modern Standard')))
     expect(push).toHaveBeenCalledWith('/', '/', { locale: 'ar' })
+    expect(getByText('Confirm Language Change')).toBeInTheDocument()
+    fireEvent.click(getByRole('button', { name: 'Yes' }))
     expect(mockHandleClose).toHaveBeenCalled()
+  })
+
+  it('should revert back to previous language', async () => {
+    const push = jest.fn()
+    mockUseRouter.mockReturnValue({
+      push,
+      asPath: '/'
+    } as unknown as NextRouter)
+    jest
+      .spyOn(TranslationStatus.prototype, 'getFileProgress')
+      .mockImplementation(async () => {
+        return await Promise.resolve({
+          pagination: {
+            offset: 0,
+            limit: 25
+          },
+          data: [
+            {
+              data: {
+                approvalProgress: 100,
+                words: {
+                  approved: 1,
+                  preTranslateAppliedTo: 0,
+                  total: 1,
+                  translated: 1
+                },
+                phrases: {
+                  approved: 1,
+                  preTranslateAppliedTo: 0,
+                  total: 1,
+                  translated: 1
+                },
+                translationProgress: 100,
+                languageId: 'ar',
+                eTag: '',
+                language: {
+                  twoLettersCode: '',
+                  androidCode: '',
+                  dialectOf: '',
+                  editorCode: '',
+                  id: '',
+                  locale: '',
+                  name: 'Arabic',
+                  osxCode: '',
+                  osxLocale: 'ar',
+                  pluralCategoryNames: [],
+                  pluralExamples: [],
+                  pluralRules: '',
+                  textDirection: 'rtl',
+                  threeLettersCode: ''
+                }
+              }
+            }
+          ]
+        })
+      })
+
+    const mockHandleClose = jest.fn()
+
+    const { getByText, getByRole } = render(
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: GET_LANGUAGES,
+              variables: {
+                languageId: '529'
+              }
+            },
+            result: {
+              data: {
+                languages: [
+                  {
+                    id: '22658',
+                    bcp47: 'ar',
+                    name: [
+                      {
+                        primary: true,
+                        value: ' اللغة العربية'
+                      },
+                      {
+                        primary: false,
+                        value: 'Arabic, Modern Standard'
+                      }
+                    ]
+                  },
+                  {
+                    id: '529',
+                    bcp47: 'en',
+                    name: [
+                      {
+                        primary: true,
+                        value: 'English'
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        ]}
+      >
+        <LanguageSelector open handleClose={mockHandleClose} />
+      </MockedProvider>
+    )
+
+    fireEvent.click(getByRole('button', { name: 'Open' }))
+    await waitFor(() => fireEvent.click(getByText('Arabic, Modern Standard')))
+    expect(push).toHaveBeenCalledWith('/', '/', { locale: 'ar' })
+    expect(getByText('Confirm Language Change')).toBeInTheDocument()
+    fireEvent.click(getByRole('button', { name: 'No' }))
+    expect(push).toHaveBeenCalledWith('/', '/', { locale: 'en' })
+    await waitFor(() => expect(mockHandleClose).toHaveBeenCalled())
   })
 })
