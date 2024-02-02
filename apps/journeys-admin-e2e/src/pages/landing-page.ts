183f6e6a717589d9fd46e700e817ce5d2cbaac73
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-02-02 02:31:32 +0000

    test: fix login flaky test and load env vars

--- apps/journeys-admin-e2e/src/pages/landing-page.ts
+++ apps/journeys-admin-e2e/src/pages/landing-page.ts
@@ -2,6 +2,7 @@ import { expect } from '@playwright/test'
 import { Page } from 'playwright-core'
 
 import { isVisible } from '../framework/actions'
+import { time } from 'console'
 
 export class LandingPage {
   readonly page: Page
@@ -12,10 +13,29 @@ export class LandingPage {
 
   async goToAdminUrl(): Promise<void> {
     await this.page.goto('/')
+
+    // Wait for the landing page to load fully otherwise 'Sign in with email' button coming twice
+    await this.page.waitForResponse((response) => {
+      return (
+        /api-gateway.*.jesusfilm.org/.test(response.url()) &&
+        response.status() === 200
+      )
+    })
   }
 
   async clickSignInWithEmail(): Promise<void> {
     await this.page.locator('button[data-provider-id="password"]').click()
+
+    // If the email input is not visible, click the 'Sign in with email' button again
+    // as sometimes landing page load was still in flight even after clicking the button
+    // and landing page is loading again even after clicking the button
+    if (
+      !(await this.page
+        .locator('input[name="email"]')
+        .isVisible({ timeout: 8000 }))
+    ) {
+      await this.page.locator('button[data-provider-id="password"]').click()
+    }
   }
 
   async signInWithEmailVisible(): Promise<void> {
