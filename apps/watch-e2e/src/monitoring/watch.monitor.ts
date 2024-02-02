7004b2e1d2c7150aebbf3dbcc72ea5ebc4189537
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-02-02 04:39:58 +0000

    test: add redirection test to checkly

--- apps/watch-e2e/src/monitoring/watch.monitor.ts
+++ apps/watch-e2e/src/monitoring/watch.monitor.ts
@@ -16,3 +16,16 @@ test('Homepage checks', async ({ page }) => {
     .textContent()
   expect(videoTitle).toEqual('Jesus Calms the StormChapter1:59')
 })
+
+/*  
+Check https://arc.gt/s/1_jf-0-0/529 is redirected to
+https://www.jesusfilm.org/watch/jesus.html/english.html 
+*/
+
+test('Check redirect', async ({ page }) => {
+  const response = await page.goto('https://arc.gt/s/1_jf-0-0/529')
+  expect(response?.status()).toEqual(200)
+  expect(response?.url()).toEqual(
+    'https://www.jesusfilm.org/watch/jesus.html/english.html'
+  )
+})
