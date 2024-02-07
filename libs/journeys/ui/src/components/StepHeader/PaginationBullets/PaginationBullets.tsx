056dcbce69e9a21542ea3a0b5b4ca2a9da0e36e6
Edmond Shen <egsmond@gmail.com>
2024-02-07 22:52:09 +0000

    fix: color lg breakpoint

--- libs/journeys/ui/src/components/StepHeader/PaginationBullets/PaginationBullets.tsx
+++ libs/journeys/ui/src/components/StepHeader/PaginationBullets/PaginationBullets.tsx
@@ -1,74 +1,46 @@
-import CircleIcon from '@mui/icons-material/Circle'
+import Circle from '../../../../../../shared/ui/src/components/icons/Circle'
+
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
-import { ReactElement, useEffect, useRef, useState } from 'react'
+import { ReactElement } from 'react'
 
 import { TreeBlock, useBlocks } from '@core/journeys/ui/block'
 import { StepFields } from '../../Step/__generated__/StepFields'
 
 export function PaginationBullets(): ReactElement {
-  const { treeBlocks, blockHistory, previousActiveBlock } = useBlocks()
-  const gap = 16
-  // const [left, setLeft] = useState(32)
+  const { treeBlocks, blockHistory } = useBlocks()
+  const gap = 17
   const activeBlock = blockHistory[
     blockHistory.length - 1
   ] as TreeBlock<StepFields>
 
-  const activeCardRef = useRef<SVGSVGElement>(null)
-  //apply left (usestate) on all bullets
-  // increment and decrement depending on nav left or right, need to consider the difference between active and prevActive
-  // compare previousBlock with activeBlock
-  // blockHistory.slice(-1)
   function isAdjacentBlock(block: TreeBlock): boolean {
-    if (activeBlock.parentOrder == null || block.parentOrder == null)
-      return false
-    if (
-      block.parentOrder - activeBlock.parentOrder === -1 ||
-      activeBlock.parentOrder - block.parentOrder === -1
-    ) {
-      return true
-    }
+    if (activeBlock.parentOrder !== null && block.parentOrder !== null)
+      if (
+        block.parentOrder - activeBlock.parentOrder === -1 ||
+        activeBlock.parentOrder - block.parentOrder === -1
+      ) {
+        return true
+      }
     return false
   }
 
   function isSecondBlock(block: TreeBlock): boolean {
-    if (activeBlock.parentOrder == null || block.parentOrder == null)
-      return false
-    if (
-      block.parentOrder - activeBlock.parentOrder === 2 ||
-      activeBlock.parentOrder - block.parentOrder === 2
-    ) {
-      return true
-    }
+    if (activeBlock.parentOrder !== null && block.parentOrder !== null)
+      if (
+        block.parentOrder - activeBlock.parentOrder === 2 ||
+        activeBlock.parentOrder - block.parentOrder === 2
+      ) {
+        return true
+      }
     return false
   }
-
-  function updateLeft() {
-    if (activeBlock.parentOrder !== null) {
-    }
-  }
-
-  // useEffect(() => {
-  //   activeCardRef.current?.scrollIntoView({
-  //     behavior: 'smooth',
-  //     block: 'center',
-  //     inline: 'center'
-  //   })
-  // }, [activeBlock])
-
-  console.log('blocks', treeBlocks)
-  console.log('blockHist', blockHistory)
-
-  const prevActiveBlock = blockHistory[blockHistory.length - 2]
-  console.log('prevActiveBlock', prevActiveBlock)
-  console.log('activeBlock', activeBlock)
-  // console.log('left:', left)
 
   return (
     <Box
       sx={{
         position: 'absolute',
-        top: 15,
+        top: 13,
         width: '100%',
         zIndex: 1,
         display: 'flex'
@@ -77,10 +49,9 @@ export function PaginationBullets(): ReactElement {
       <Stack
         direction="row"
         sx={{
-          border: 'red solid 2px',
           position: 'absolute',
-          transform: 'translateX(-50%)',
-          width: '80px',
+          transform: 'translateX(-40%)',
+          width: '84px',
           height: '16px',
           alignItems: 'center',
           left: '50%',
@@ -88,52 +59,51 @@ export function PaginationBullets(): ReactElement {
         }}
       >
         {treeBlocks.map((block) => {
+          if (block.parentOrder === null || activeBlock.parentOrder === null)
+            return <></>
           if (block.id === activeBlock.id) {
             return (
-              <CircleIcon
+              <Circle
                 key={block.id}
+                data-testid="activeBlock"
                 sx={{
-                  fontSize: '10px',
+                  fontSize: '8px',
                   scale: '1',
-                  color: 'primary.main',
+                  color: { xs: 'primary.main', lg: 'white' },
+                  bgcolor: { xs: 'primary.main', lg: 'white' },
+                  borderRadius: '100%',
                   left: `${
-                    32 +
-                    gap * (block.parentOrder !== null ? block.parentOrder : 1) -
-                    (activeBlock.parentOrder !== null
-                      ? activeBlock.parentOrder * gap
-                      : 0)
+                    32 + gap * block.parentOrder - activeBlock.parentOrder * gap
                   }px`,
                   position: 'absolute',
-                  transition: 'transform .2s, left .2s'
+                  transition: 'scale .2s, left .2s'
                 }}
-                ref={activeCardRef}
+              />
+            )
+          } else {
+            return (
+              <Circle
+                key={block.id}
+                sx={{
+                  fontSize: '8px',
+                  color: { xs: 'primary.main', lg: 'white' },
+                  bgcolor: { xs: 'primary.main', lg: 'white' },
+                  borderRadius: '100%',
+                  scale: isAdjacentBlock(block)
+                    ? '.66'
+                    : isSecondBlock(block)
+                    ? '.33'
+                    : '.33',
+                  opacity: '60%',
+                  left: `${
+                    32 + gap * block.parentOrder - activeBlock.parentOrder * gap
+                  }px`,
+                  position: 'absolute',
+                  transition: 'scale .2s, left .2s'
+                }}
               />
             )
           }
-          return (
-            <CircleIcon
-              key={block.id}
-              sx={{
-                fontSize: '10px',
-                color: 'primary.dark',
-                scale: isAdjacentBlock(block)
-                  ? '.66'
-                  : isSecondBlock(block)
-                  ? '.33'
-                  : '.33',
-                opacity: '60%',
-                left: `${
-                  32 +
-                  gap * (block.parentOrder !== null ? block.parentOrder : 1) -
-                  (activeBlock.parentOrder !== null
-                    ? activeBlock.parentOrder * gap
-                    : 0)
-                }px`,
-                position: 'absolute',
-                transition: 'transform .2s, left .2s'
-              }}
-            />
-          )
         })}
       </Stack>
     </Box>
