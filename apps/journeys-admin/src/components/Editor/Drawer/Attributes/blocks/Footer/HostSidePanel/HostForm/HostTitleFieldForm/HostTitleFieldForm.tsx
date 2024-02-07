824a282c006a6e6ee6a27785bce2fd97eb271c4d
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-02-07 14:31:42 +1300

    refactor: mobile styles and drawer (#2284)

---
+++ apps/journeys-admin/src/components/Editor/Drawer/Attributes/blocks/Footer/HostSidePanel/HostForm/HostTitleFieldForm/HostTitleFieldForm.tsx
@@ -0,0 +1,89 @@
+import { gql, useMutation } from '@apollo/client'
+import { ReactElement } from 'react'
+import { object, string } from 'yup'
+
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
+
+import { CreateHost } from '../../../../../../../../../../__generated__/CreateHost'
+import { UpdateJourneyHost } from '../../../../../../../../../../__generated__/UpdateJourneyHost'
+import { useHostUpdateMutation } from '../../../../../../../../../libs/useHostUpdateMutation/useHostUpdateMutation'
+import { TextFieldForm } from '../../../../../../../../TextFieldForm'
+
+export const CREATE_HOST = gql`
+  mutation CreateHost($teamId: ID!, $input: HostCreateInput!) {
+    hostCreate(teamId: $teamId, input: $input) {
+      id
+      title
+    }
+  }
+`
+
+export const UPDATE_JOURNEY_HOST = gql`
+  mutation UpdateJourneyHost($id: ID!, $input: JourneyUpdateInput!) {
+    journeyUpdate(id: $id, input: $input) {
+      id
+      host {
+        id
+      }
+    }
+  }
+`
+
+export function HostTitleFieldForm(): ReactElement {
+  const [hostCreate] = useMutation<CreateHost>(CREATE_HOST)
+  const [journeyHostUpdate] =
+    useMutation<UpdateJourneyHost>(UPDATE_JOURNEY_HOST)
+
+  const { updateHost } = useHostUpdateMutation()
+  const { journey } = useJourney()
+
+  const titleSchema = object({
+    hostTitle: string().required('Please enter a host name')
+  })
+
+  async function handleSubmit(value: string): Promise<void> {
+    if (journey?.host != null) {
+      const { id, teamId } = journey.host
+      await updateHost({ id, teamId, input: { title: value } })
+    } else if (journey?.team != null) {
+      const { data } = await hostCreate({
+        variables: { teamId: journey.team.id, input: { title: value } },
+        update(cache, { data }) {
+          if (data?.hostCreate != null) {
+            cache.modify({
+              fields: {
+                hosts(existingTeamHosts = []) {
+                  const newHostRef = cache.writeFragment({
+                    data: data.hostCreate,
+                    fragment: gql`
+                      fragment NewHost on Host {
+                        id
+                      }
+                    `
+                  })
+                  return [...existingTeamHosts, newHostRef]
+                }
+              }
+            })
+          }
+        }
+      })
+      if (data?.hostCreate.id != null) {
+        await journeyHostUpdate({
+          variables: { id: journey?.id, input: { hostId: data.hostCreate.id } }
+        })
+      }
+    }
+  }
+
+  return (
+    <TextFieldForm
+      id="hostTitle"
+      label="Host Name"
+      initialValue={journey?.host == null ? '' : journey.host.title}
+      validationSchema={titleSchema}
+      onSubmit={handleSubmit}
+      data-testid="HostTitleFieldForm"
+    />
+  )
+}
--- apps/journeys-admin/src/components/Editor/Drawer/Drawer.tsx
+++ apps/journeys-admin/src/components/Editor/Drawer/Drawer.tsx
@@ -1,34 +1,21 @@
 import AppBar from '@mui/material/AppBar'
 import MuiDrawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
 import Paper from '@mui/material/Paper'
-import { Theme, useTheme } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import useMediaQuery from '@mui/material/useMediaQuery'
 import { ReactElement, ReactNode } from 'react'
-import { useTranslation } from 'react-i18next'
-
-import {
-  ActiveJourneyEditContent,
-  useEditor
-} from '@core/journeys/ui/EditorProvider'
 import X2Icon from '@core/shared/ui/icons/X2'
+import IconButton from '@mui/material/IconButton'
+import { Theme, useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { DRAWER_WIDTH, EDIT_TOOLBAR_HEIGHT } from '../constants'
 
-import { CardTemplateDrawer } from '../CardTemplateDrawer'
-import { Attributes } from '../ControlPanel/Attributes'
-
-interface DrawerContentProps {
+interface DrawerTitleProps {
   title?: string
-  children?: ReactNode
-  handleDrawerToggle: () => void
+  onClose?: () => void
 }
 
-function DrawerContent({
-  title,
-  children,
-  handleDrawerToggle
-}: DrawerContentProps): ReactElement {
+function DrawerTitle({ title, onClose }: DrawerTitleProps): ReactElement {
   return (
     <>
       <AppBar position="static" color="default">
@@ -38,149 +25,73 @@ function DrawerContent({
             noWrap
             component="div"
             sx={{ flexGrow: 1 }}
-            data-testid="drawer-title"
           >
             {title}
           </Typography>
-          <IconButton
-            onClick={handleDrawerToggle}
-            sx={{ display: { xs: 'inline-flex', sm: 'none' } }}
-            edge="end"
-          >
-            <X2Icon />
-          </IconButton>
+          {onClose != null && (
+            <IconButton
+              aria-label="close-image-library"
+              onClick={onClose}
+              sx={{ display: 'inline-flex' }}
+              edge="end"
+            >
+              <X2Icon />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
-      {children}
     </>
   )
 }
 
-export function Drawer(): ReactElement {
+interface DrawerProps {
+  title?: string
+  children?: ReactNode
+  open?: boolean
+  onClose?: () => void
+}
+
+export function Drawer({
+  title,
+  children,
+  open,
+  onClose
+}: DrawerProps): ReactElement {
   const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
-  const {
-    state: {
-      drawerTitle: title,
-      drawerChildren: children,
-      drawerMobileOpen: mobileOpen,
-      selectedComponent,
-      selectedBlock,
-      selectedStep,
-      journeyEditContentComponent
-    },
-    dispatch
-  } = useEditor()
-  const { t } = useTranslation('apps-journeys-admin')
-  const selected = selectedComponent ?? selectedBlock ?? 'none'
-  let blockTitle: string | undefined
-  switch (selectedBlock?.__typename) {
-    case 'ButtonBlock':
-      blockTitle = t('Button Properties')
-      break
-    case 'FormBlock':
-      blockTitle = t('Form Properties')
-      break
-    case 'ImageBlock':
-      blockTitle = t('Image Properties')
-      break
-    case 'RadioQuestionBlock':
-      blockTitle = t('Poll Properties')
-      break
-    case 'RadioOptionBlock':
-      blockTitle = t('Poll Option Properties')
-      break
-    case 'SignUpBlock':
-      blockTitle = t('Subscribe Properties')
-      break
-    case 'StepBlock':
-      if (selectedBlock.children[0]?.children.length > 0) {
-        blockTitle = t('Card Properties')
-      } else {
-        blockTitle = t('Card Templates')
-      }
-      break
-    case 'TextResponseBlock':
-      blockTitle = t('Feedback Properties')
-      break
-    case 'TypographyBlock':
-      blockTitle = t('Typography Properties')
-      break
-    case 'VideoBlock':
-      blockTitle = t('Video Properties')
-      break
-    default:
-      blockTitle = title
-  }
-  switch (selectedComponent) {
-    case 'Footer':
-      blockTitle = t('Footer Properties')
-      break
-  }
-  switch (journeyEditContentComponent) {
-    case ActiveJourneyEditContent.SocialPreview:
-      blockTitle = t('Social Share Preview')
-      break
-    case ActiveJourneyEditContent.Action:
-      blockTitle = t('Information')
-      break
-    case ActiveJourneyEditContent.JourneyFlow:
-      blockTitle = t('Properties')
-      break
-  }
-  const { zIndex } = useTheme()
 
-  const handleDrawerToggle = (): void => {
-    dispatch({
-      type: 'SetDrawerMobileOpenAction',
-      mobileOpen: !mobileOpen
-    })
-  }
-
-  return smUp ? (
-    <Paper
+  return (
+    <MuiDrawer
+      anchor={smUp ? 'right' : 'bottom'}
+      variant={open != null ? 'temporary' : 'permanent'}
+      SlideProps={{ appear: true }}
+      open={open}
       elevation={0}
+      hideBackdrop
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        border: 1,
-        borderColor: 'divider',
-        borderRadius: 0,
-        overflow: 'hidden',
-        height: '100%',
-        width: '100%',
-        minWidth: 0
+        '& .MuiDrawer-paper':
+          open != null
+            ? {
+                borderRadius: 4,
+                width: smUp ? DRAWER_WIDTH : 'auto',
+                top: EDIT_TOOLBAR_HEIGHT + 20,
+                left: { xs: 20, sm: 'auto' },
+                right: 20,
+                bottom: 20,
+                height: 'auto'
+              }
+            : {
+                borderRadius: 4,
+                width: smUp ? DRAWER_WIDTH : 'auto',
+                left: { xs: 20, sm: 'auto' },
+                top: 20,
+                right: 20,
+                bottom: 20,
+                height: 'auto'
+              }
       }}
-      data-testid="EditorDrawer"
     >
-      <DrawerContent title={blockTitle} handleDrawerToggle={handleDrawerToggle}>
-        {journeyEditContentComponent === ActiveJourneyEditContent.Canvas
-          ? selected !== 'none' &&
-            selectedStep !== undefined &&
-            (selectedStep.children[0]?.children.length > 0 ? (
-              <Attributes selected={selected} step={selectedStep} />
-            ) : (
-              <CardTemplateDrawer />
-            ))
-          : children}
-      </DrawerContent>
-    </Paper>
-  ) : (
-    <>
-      <MuiDrawer
-        anchor="bottom"
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        sx={{
-          display: { xs: 'block', md: 'none' },
-          zIndex: zIndex.modal
-        }}
-        data-testid="EditorDrawer"
-      >
-        <DrawerContent title={title} handleDrawerToggle={handleDrawerToggle}>
-          {children}
-        </DrawerContent>
-      </MuiDrawer>
-    </>
+      <DrawerTitle title={title} onClose={onClose} />
+      {children}
+    </MuiDrawer>
   )
 }
