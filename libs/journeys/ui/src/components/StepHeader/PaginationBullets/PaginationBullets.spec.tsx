3c87be0b593ad07b56f2c9bbf354cedbc9d2ffcb
Edmond Shen <egsmond@gmail.com>
2024-02-12 23:56:48 +0000

    test: add test

---
+++ libs/journeys/ui/src/components/StepHeader/PaginationBullets/PaginationBullets.spec.tsx
@@ -0,0 +1,43 @@
+import { render } from '@testing-library/react'
+
+import { TreeBlock, blockHistoryVar, treeBlocksVar } from '../../../libs/block'
+import { BlockFields_StepBlock as StepBlock } from '../../../libs/block/__generated__/BlockFields'
+
+import { PaginationBullets } from './PaginationBullets'
+
+describe('PaginationBullets', () => {
+  const step1: TreeBlock<StepBlock> = {
+    id: 'step1.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 0,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+  const step2: TreeBlock<StepBlock> = {
+    id: 'step2.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 1,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+  const step3: TreeBlock<StepBlock> = {
+    id: 'step3.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 2,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+
+  it('should display activeBlock', () => {
+    treeBlocksVar([step1, step2, step3])
+    blockHistoryVar([step1])
+    const { getByTestId } = render(<PaginationBullets />)
+    expect(getByTestId('activeBlock')).toBeInTheDocument()
+  })
+})
