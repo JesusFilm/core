2e545aaa89af5aa57ed9831a6cc839c0df21c25a
Zachary Annabell <zwsaqx@gmail.com>
2024-02-14 03:12:37 +0000

    fix: run codegen locally

---
+++ libs/journeys/ui/src/components/Card/--nocache/StepNextEventCreate.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+import { StepNextEventCreateInput } from "./../../../../__generated__/globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: StepNextEventCreate
+// ====================================================
+
+export interface StepNextEventCreate_stepNextEventCreate {
+  __typename: "StepNextEvent";
+  id: string;
+}
+
+export interface StepNextEventCreate {
+  stepNextEventCreate: StepNextEventCreate_stepNextEventCreate;
+}
+
+export interface StepNextEventCreateVariables {
+  input: StepNextEventCreateInput;
+}
