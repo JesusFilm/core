394652b965e8902d06d4ac2ea61dff1ef5f184e5
Edmond Shen <egsmond@gmail.com>
2024-02-14 01:39:53 +0000

    test: add test

--- libs/journeys/ui/src/components/StepHeader/PaginationBullets/PaginationBullets.spec.tsx
+++ libs/journeys/ui/src/components/StepHeader/PaginationBullets/PaginationBullets.spec.tsx
@@ -33,11 +33,56 @@ describe('PaginationBullets', () => {
     nextBlockId: null,
     children: []
   }
+  const step4: TreeBlock<StepBlock> = {
+    id: 'step4.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 3,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+  const step5: TreeBlock<StepBlock> = {
+    id: 'step5.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 4,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+  const step6: TreeBlock<StepBlock> = {
+    id: 'step6.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 5,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
+  const step7: TreeBlock<StepBlock> = {
+    id: 'step7.id',
+    __typename: 'StepBlock',
+    parentBlockId: null,
+    parentOrder: 6,
+    locked: false,
+    nextBlockId: null,
+    children: []
+  }
 
   it('should display the active bullet', () => {
-    treeBlocksVar([step1, step2, step3])
+    treeBlocksVar([step1, step2, step3, step4, step5, step6, step7])
     blockHistoryVar([step1])
     const { getByTestId } = render(<PaginationBullets />)
-    expect(getByTestId('activeBullet')).toBeInTheDocument()
+    expect(getByTestId('bullet-active')).toBeInTheDocument()
+  })
+
+  it('should render the correct sized bullets', () => {
+    treeBlocksVar([step1, step2, step3, step4, step5, step6, step7])
+    blockHistoryVar([step1, step2, step3, step4])
+    const { getAllByTestId } = render(<PaginationBullets />)
+    expect(getAllByTestId('bullet-active')).toHaveLength(1)
+    expect(getAllByTestId('bullet-adjacent')).toHaveLength(2)
+    expect(getAllByTestId('bullet-default')).toHaveLength(4)
   })
 })
