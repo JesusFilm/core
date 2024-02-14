877598557ebd118d9dfba92cf3819f8e8702a4aa
Nisal Cottingham <114973713+Kneesal@users.noreply.github.com>
2024-02-14 13:10:43 +1300

    chore: host logo image (#2303)

--- libs/nest/common/src/lib/email/components/EmailLogo/EmailLogo.tsx
+++ libs/nest/common/src/lib/email/components/EmailLogo/EmailLogo.tsx
@@ -2,7 +2,7 @@ import { Img } from '@react-email/components'
 import { ReactElement } from 'react'
 
 export function EmailLogo(): ReactElement {
-  return <Img src="https://i.postimg.cc/vmb9xQPk/Logo-Horizontal.png" />
+  return <Img src="https://your.nextstep.is/LogoHorizontal.png" />
 }
 
 export default EmailLogo
