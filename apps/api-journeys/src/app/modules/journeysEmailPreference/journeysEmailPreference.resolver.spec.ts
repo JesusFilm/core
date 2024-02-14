07d9887bfaa79b05e7fb7a803722b732d07c819c
Tanner <tanner.fleming@gmail.com>
2024-02-14 21:54:58 +0000

    refactor: rename email prefs to journey email prefs

---
+++ apps/api-journeys/src/app/modules/journeysEmailPreference/journeysEmailPreference.resolver.spec.ts
@@ -0,0 +1,117 @@
+import { Test } from '@nestjs/testing'
+
+import { PrismaService } from '../../lib/prisma.service'
+
+import { JourneysEmailPreferenceResolver } from './journeysEmailPreference.resolver'
+
+describe('JourneysEmailPreferenceResolver', () => {
+  let resolver: JourneysEmailPreferenceResolver
+  let prismaService: PrismaService
+
+  const journeysEmailPreference = {
+    email: 'test@example.com',
+    unsubscribeAll: false,
+    teamInvite: true,
+    teamRemoved: true,
+    teamInviteAccepted: true,
+    journeyEditInvite: true,
+    journeyRequestApproved: true,
+    journeyAccessRequest: true
+  }
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        JourneysEmailPreferenceResolver,
+        {
+          provide: PrismaService,
+          useValue: {
+            journeysEmailPreference: {
+              findMany: jest.fn(),
+              findFirst: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              create: jest.fn()
+            }
+          }
+        }
+      ]
+    }).compile()
+
+    resolver = moduleRef.get<JourneysEmailPreferenceResolver>(JourneysEmailPreferenceResolver)
+    prismaService = moduleRef.get<PrismaService>(PrismaService)
+  })
+
+  describe('updateJourneysEmailPreference', () => {
+    it('should update email preference', async () => {
+      const updatedJourneysEmailPreference = { teaminvite: true, ...journeysEmailPreference }
+      jest
+        .spyOn(prismaService.journeysEmailPreference, 'findUnique')
+        .mockResolvedValue(journeysEmailPreference)
+      jest
+        .spyOn(prismaService.journeysEmailPreference, 'update')
+        .mockResolvedValue(updatedJourneysEmailPreference)
+
+      const result = await resolver.updateJourneysEmailPreference(
+        updatedJourneysEmailPreference
+      )
+
+      expect(result).toEqual(updatedJourneysEmailPreference)
+      expect(prismaService.journeysEmailPreference.findUnique).toHaveBeenCalledWith({
+        where: { email: 'test@example.com' }
+      })
+      expect(prismaService.journeysEmailPreference.update).toHaveBeenCalledWith({
+        where: { email: 'test@example.com' },
+        data: updatedJourneysEmailPreference
+      })
+    })
+  })
+
+  describe('findOrCreateJourneysEmailPreference', () => {
+    it('should find an existing email preference', async () => {
+      const email = journeysEmailPreference.email
+      const existingJourneysEmailPreference = journeysEmailPreference
+      jest
+        .spyOn(prismaService.journeysEmailPreference, 'findUnique')
+        .mockResolvedValue(existingJourneysEmailPreference)
+
+      const result = await resolver.findOrCreateJourneysEmailPreference(email)
+
+      expect(result).toEqual(existingJourneysEmailPreference)
+      expect(prismaService.journeysEmailPreference.findUnique).toHaveBeenCalledWith({
+        where: { email }
+      })
+      expect(prismaService.journeysEmailPreference.create).not.toHaveBeenCalled()
+    })
+
+    it('should create a new email preference if it does not exist', async () => {
+      const newJourneysEmailPreference = journeysEmailPreference
+      const email = journeysEmailPreference.email
+      jest
+        .spyOn(prismaService.journeysEmailPreference, 'findUnique')
+        .mockResolvedValue(null)
+      jest
+        .spyOn(prismaService.journeysEmailPreference, 'create')
+        .mockResolvedValue(newJourneysEmailPreference)
+
+      const result = await resolver.findOrCreateJourneysEmailPreference(email)
+
+      expect(result).toEqual(newJourneysEmailPreference)
+      expect(prismaService.journeysEmailPreference.findUnique).toHaveBeenCalledWith({
+        where: { email }
+      })
+      expect(prismaService.journeysEmailPreference.create).toHaveBeenCalledWith({
+        data: {
+          email,
+          unsubscribeAll: false,
+          teamInvite: true,
+          teamRemoved: true,
+          teamInviteAccepted: true,
+          journeyEditInvite: true,
+          journeyRequestApproved: true,
+          journeyAccessRequest: true
+        }
+      })
+    })
+  })
+})
---
+++ apps/api-journeys/src/app/modules/journeysEmailPreference/journeysEmailPreference.resolver.ts
@@ -0,0 +1,70 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { GraphQLError } from 'graphql'
+
+import { JourneysEmailPreference } from '.prisma/api-journeys-client'
+
+import { JourneysEmailPreferenceUpdateInput } from '../../__generated__/graphql'
+import { PrismaService } from '../../lib/prisma.service'
+
+@Resolver('JourneysEmailPreference')
+export class JourneysEmailPreferenceResolver {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Mutation()
+  async updateJourneysEmailPreference(
+    @Args('input') input: JourneysEmailPreferenceUpdateInput
+  ): Promise<JourneysEmailPreference> {
+    const { email, ...rest } = input
+    const journeysEmailPreference = await this.prismaService.journeysEmailPreference.findUnique(
+      {
+        where: { email }
+      }
+    )
+
+    console.log('journeysEmailPreference', journeysEmailPreference)
+    console.log('rest', rest)
+
+    const updatedJourneysEmailPreference =
+      await this.prismaService.journeysEmailPreference.update({
+        where: { email },
+        data: input
+      })
+
+    console.log('updatedJourneysEmailPreference', updatedJourneysEmailPreference)
+
+    return updatedJourneysEmailPreference
+  }
+
+  @Mutation()
+  async findOrCreateJourneysEmailPreference(
+    @Args('email') email: string
+  ): Promise<JourneysEmailPreference> {
+    let result: JourneysEmailPreference | null = null
+
+    try {
+      result = await this.prismaService.journeysEmailPreference.findUnique({
+        where: { email }
+      })
+
+      if (result === null) {
+        result = await this.prismaService.journeysEmailPreference.create({
+          data: {
+            email,
+            unsubscribeAll: false,
+            teamInvite: true,
+            teamRemoved: true,
+            teamInviteAccepted: true,
+            journeyEditInvite: true,
+            journeyRequestApproved: true,
+            journeyAccessRequest: true
+          }
+        })
+      }
+    } catch (error) {
+      console.error('Error in findOrCreateJourneysEmailPreference:', error)
+      throw new GraphQLError('Error in findOrCreateJourneysEmailPreference')
+    }
+
+    return result
+  }
+}
