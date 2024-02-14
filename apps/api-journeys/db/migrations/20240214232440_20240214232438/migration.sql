3b8b36540449b1eb91519063cbca941ad19a9c88
Mike Allison <dj.mikeallison@gmail.com>
2024-02-14 23:25:44 +0000

    feat: email unsubscribe

--- apps/api-journeys/db/migrations/20240214232440_20240214232438/migration.sql
+++ apps/api-journeys/db/migrations/20240214232440_20240214232438/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `EmailPreference` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "EmailPreference";
-
 -- CreateTable
 CREATE TABLE "JourneysEmailPreference" (
     "email" TEXT NOT NULL,
