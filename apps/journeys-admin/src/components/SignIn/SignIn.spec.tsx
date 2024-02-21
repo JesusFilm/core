3baaeadf513f444efa1b18cef07cacb80d63552f
Dan <dan.chae@tandem.org.nz>
2024-02-21 03:31:02 +0000

    fix: failing tests

--- apps/journeys-admin/src/components/SignIn/SignIn.spec.tsx
+++ apps/journeys-admin/src/components/SignIn/SignIn.spec.tsx
@@ -5,8 +5,10 @@ import { SignIn } from './SignIn'
 describe('SignIn', () => {
   it('should render sign in page', () => {
     const { getByRole } = render(<SignIn />)
-    expect(getByRole('link', { name: 'Need help?' })).toBeInTheDocument()
-    expect(getByRole('link', { name: 'Need help?' })).toHaveAttribute(
+    expect(
+      getByRole('link', { name: 'Feedback & Support' })
+    ).toBeInTheDocument()
+    expect(getByRole('link', { name: 'Feedback & Support' })).toHaveAttribute(
       'href',
       'mailto:support@nextstep.is?Subject=Support%2FFeedback%20Request'
     )
