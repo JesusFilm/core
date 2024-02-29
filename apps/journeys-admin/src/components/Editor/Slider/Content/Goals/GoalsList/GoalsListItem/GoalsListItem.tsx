e5987981911f5cc330a82c85018edc4f816d0c4c
Dan <dan.chae@tandem.org.nz>
2024-02-29 03:48:08 +0000

    fix: usetranslation to follow documented standards

--- apps/journeys-admin/src/components/Editor/Slider/Content/Goals/GoalsList/GoalsListItem/GoalsListItem.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Goals/GoalsList/GoalsListItem/GoalsListItem.tsx
@@ -149,11 +149,11 @@ export function GoalsListItem({
               {goalLabel}
             </Typography>
           </Stack>
-          <Typography variant="body2" color="secondary.light">
-            <Trans t={t} count={count}>
-              Appears on <strong>{{ count }}</strong> card
-            </Trans>
-          </Typography>
+          <Trans t={t} count={count}>
+            <Typography variant="body2" color="secondary.light">
+              Appears on <strong>{'{{ count }}'}</strong> card
+            </Typography>
+          </Trans>
         </TableCell>
         <TableCell
           align="center"
