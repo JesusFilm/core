8bb4e6c279525010bc72e452f7d8141e3952ce87
John Geronimo <johnageronimo1@gmail.com>
2024-03-04 01:23:17 +0000

    fix: hide navbar on templates

--- apps/journeys-admin/pages/templates/index.tsx
+++ apps/journeys-admin/pages/templates/index.tsx
@@ -39,6 +39,8 @@ function TemplateIndexPage(): ReactElement {
         user={user}
         mainBodyPadding={false}
         showMainHeader={false}
+        showAppHeader={user?.id != null}
+        showNavBar={user?.id != null}
       >
         <TemplateGallery />
       </PageWrapper>
