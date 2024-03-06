d2469ebb316aa413775b160574be323532932562
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2024-03-07 12:59:44 +1300

    feat: onboarding side bar - desktop view (#2428)

--- apps/journeys-admin/src/components/OnboardingPageWrapper/OnboardingPageWrapper.spec.tsx
+++ apps/journeys-admin/src/components/OnboardingPageWrapper/OnboardingPageWrapper.spec.tsx
@@ -4,16 +4,6 @@ import { fireEvent, render, waitFor } from '@testing-library/react'
 import { OnboardingPageWrapper } from './OnboardingPageWrapper'
 
 describe('OnboardingPageWrapper', () => {
-  it('should show logo', () => {
-    const { getByRole } = render(
-      <OnboardingPageWrapper emailSubject="a question about onboarding">
-        <div>Child</div>
-      </OnboardingPageWrapper>
-    )
-
-    expect(getByRole('img', { name: 'Next Steps' })).toBeInTheDocument()
-  })
-
   it('should render children', () => {
     const { getByText } = render(
       <OnboardingPageWrapper emailSubject="a question about onboarding">
