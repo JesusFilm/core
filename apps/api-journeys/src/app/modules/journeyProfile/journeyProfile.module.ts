7083e3f18a9f16b054a3b59d5622cd970e73cebd
Kneesal <nisalcottingham@gmail.com>
2024-03-11 02:15:59 +0000

    fix: mailchimp service

--- apps/api-journeys/src/app/modules/journeyProfile/journeyProfile.module.ts
+++ apps/api-journeys/src/app/modules/journeyProfile/journeyProfile.module.ts
@@ -6,11 +6,12 @@ import { AppCaslFactory } from '../../lib/casl/caslFactory'
 import { PrismaService } from '../../lib/prisma.service'
 
 import { JourneyProfileResolver } from './journeyProfile.resolver'
+import { MailChimpService } from '../mailChimp/mailChimp.service'
 
 @Global()
 @Module({
   imports: [CaslAuthModule.register(AppCaslFactory)],
-  providers: [JourneyProfileResolver, PrismaService],
+  providers: [JourneyProfileResolver, MailChimpService, PrismaService],
   exports: []
 })
 export class JourneyProfileModule {}
