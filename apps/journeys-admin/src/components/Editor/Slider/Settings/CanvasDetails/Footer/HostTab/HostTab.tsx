b13750a95dd09e39fb5075db35b457bd61c74864
jianwei1 <jianweichong08@gmail.com>
2024-03-12 22:49:04 +0000

    refactor: extracted hostselection out of hosttab

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostTab.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostTab.tsx
@@ -1,14 +1,8 @@
 import { gql, useLazyQuery } from '@apollo/client'
-import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
 import dynamic from 'next/dynamic'
-import { useTranslation } from 'next-i18next'
 import { ReactElement, useEffect, useState } from 'react'
 
 import { useJourney } from '@core/journeys/ui/JourneyProvider'
-import { HostAvatars } from '@core/journeys/ui/StepFooter/HostAvatars'
-import AlertCircleIcon from '@core/shared/ui/icons/AlertCircle'
-import Edit2Icon from '@core/shared/ui/icons/Edit2'
 
 import {
   GetAllTeamHosts,
@@ -18,7 +12,8 @@ import { UserTeamRole } from '../../../../../../../../__generated__/globalTypes'
 import { useCurrentUserLazyQuery } from '../../../../../../../libs/useCurrentUserLazyQuery'
 import { useUpdateJourneyHostMutation } from '../../../../../../../libs/useUpdateJourneyHostMutation'
 import { useUserTeamsAndInvitesQuery } from '../../../../../../../libs/useUserTeamsAndInvitesQuery'
-import { ContainedIconButton } from '../../../../../../ContainedIconButton'
+
+import { HostSelection } from './HostSelection'
 
 export const GET_ALL_TEAM_HOSTS = gql`
   query GetAllTeamHosts($teamId: ID!) {
@@ -61,7 +56,6 @@ export function HostTab(): ReactElement {
   const [openHostList, setOpenHostList] = useState(false)
   const [openHostForm, setOpenHostForm] = useState(false)
   const [openHostInfo, setOpenHostInfo] = useState(false)
-  const { t } = useTranslation('apps-journeys-admin')
   const { journey } = useJourney()
   // Get all team members of journey team, check if user in team
   // TODO: Replace with CASL authorisation check
@@ -120,59 +114,13 @@ export function HostTab(): ReactElement {
   return (
     <>
       {selectHostBox && (
-        <>
-          {journey?.host != null ? (
-            <Stack sx={{ p: 4 }}>
-              <ContainedIconButton
-                label={journey.host.title}
-                description={journey.host.location ?? ''}
-                disabled={!userInTeam}
-                slots={{
-                  ImageThumbnail: (
-                    <HostAvatars
-                      size="small"
-                      avatarSrc1={journey.host.src1}
-                      avatarSrc2={journey.host.src2}
-                      hasPlaceholder
-                    />
-                  )
-                }}
-                onClick={() => {
-                  setSelectHostBox(false)
-                  setOpenHostForm(true)
-                }}
-                actionIcon={<Edit2Icon />}
-                // data-testid="host-select-icon-button"
-              />
-            </Stack>
-          ) : (
-            <Stack sx={{ p: 4 }}>
-              <ContainedIconButton
-                label={t('Select a Host')}
-                disabled={!userInTeam}
-                slots={{
-                  ImageThumbnail: <HostAvatars size="large" hasPlaceholder />
-                }}
-                onClick={() => {
-                  setSelectHostBox(false)
-                  setOpenHostList(true)
-                }}
-              />
-            </Stack>
-          )}
-          {!userInTeam && journey?.team != null && (
-            <Stack direction="row" alignItems="center" gap={3}>
-              <AlertCircleIcon />
-              <Typography variant="subtitle2">
-                {data?.userTeams.length === 0
-                  ? t('Cannot edit hosts for this old journey')
-                  : t('Only {{ teamName }} members can edit this', {
-                      teamName: journey.team.title
-                    })}
-              </Typography>
-            </Stack>
-          )}
-        </>
+        <HostSelection
+          data={data}
+          userInTeam={userInTeam}
+          setSelectHostBox={setSelectHostBox}
+          setOpenHostForm={setOpenHostForm}
+          setOpenHostList={setOpenHostList}
+        />
       )}
       <HostList
         openHostList={openHostList}
