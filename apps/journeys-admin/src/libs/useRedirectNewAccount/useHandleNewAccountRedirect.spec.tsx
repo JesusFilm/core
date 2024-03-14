dca5530ba94a3344740d244b8a9ce94e9c2357a5
Edmond Shen <72168666+edmonday@users.noreply.github.com>
2024-03-14 16:59:27 +1300

    test: redirect handle new user (#2465)

---
+++ apps/journeys-admin/src/libs/useRedirectNewAccount/useHandleNewAccountRedirect.spec.tsx
@@ -0,0 +1,62 @@
+import { renderHook } from '@testing-library/react'
+import { NextRouter, useRouter } from 'next/router'
+
+import { useHandleNewAccountRedirect } from './useHandleNewAccountRedirect'
+
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn()
+}))
+
+const mockUseRouter = useRouter as jest.MockedFunction<typeof useRouter>
+
+describe('HandleNewAccountRedirect', () => {
+  const push = jest.fn()
+  Object.defineProperty(window, 'location', {
+    configurable: true,
+    enumerable: true,
+    value: { origin: 'http://localhost:4200' }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should append newAccount to redirect', () => {
+    mockUseRouter.mockReturnValue({
+      push,
+      pathname: '/users/sign-in',
+      query: {
+        redirect: 'http://localhost:4200/'
+      },
+      asPath: '/users/sign-in'
+    } as unknown as NextRouter)
+    renderHook(() => useHandleNewAccountRedirect())
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/users/sign-in',
+      query: {
+        redirect: 'http://localhost:4200?newAccount=true'
+      }
+    })
+  })
+
+  it('should append newAccount at the end of redirect if createNew exists', () => {
+    mockUseRouter.mockReturnValue({
+      push,
+      pathname: '/users/sign-in',
+      query: {
+        redirect: 'http://localhost:4200?createNew=true'
+      },
+      asPath: '/users/sign-in'
+    } as unknown as NextRouter)
+    renderHook(() => useHandleNewAccountRedirect())
+
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/users/sign-in',
+      query: {
+        redirect: 'http://localhost:4200?createNew=true&newAccount=true'
+      }
+    })
+  })
+})
