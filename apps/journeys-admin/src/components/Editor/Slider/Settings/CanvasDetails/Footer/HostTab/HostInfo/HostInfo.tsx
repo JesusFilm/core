111163b9b155a8380b1d4c30192183e50efb1875
jianwei1 <jianweichong08@gmail.com>
2024-03-14 01:53:26 +0000

    fix: info back button styling

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostInfo/HostInfo.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostInfo/HostInfo.tsx
@@ -13,33 +13,31 @@ interface HostInfoTabProps {
 export function HostInfo({ handleSelection }: HostInfoTabProps): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   return (
-    <>
-      <Stack sx={{ p: 4 }}>
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => handleSelection('list')}
-          sx={{ maxWidth: 45, mb: 4 }}
-        >
-          {t('Back')}
-        </Button>
-        <Stack direction="row" alignItems="center" gap={3} sx={{ mb: 4 }}>
-          <UserProfileCircleIcon />
-          <Typography variant="subtitle2">
-            {t('Why does your journey need a host?')}
-          </Typography>
-        </Stack>
-        <Typography gutterBottom color="secondary.light">
-          {t(
-            'A great way to add personality to your content is to include both male and female journey creators. Diverse creators, especially with a local feel, are more likely to engage users in conversation.'
-          )}
-        </Typography>
-        <Typography color="secondary.light">
-          {t(
-            'In countries with security concerns, it is advisable to create fake personas for your own safety.'
-          )}
+    <Stack sx={{ p: 4 }}>
+      <Button
+        variant="outlined"
+        size="small"
+        onClick={() => handleSelection('list')}
+        sx={{ display: 'flex', mb: 4 }}
+      >
+        {t('Back')}
+      </Button>
+      <Stack direction="row" alignItems="center" gap={3} sx={{ mb: 4 }}>
+        <UserProfileCircleIcon />
+        <Typography variant="subtitle2">
+          {t('Why does your journey need a host?')}
         </Typography>
       </Stack>
-    </>
+      <Typography gutterBottom color="secondary.light">
+        {t(
+          'A great way to add personality to your content is to include both male and female journey creators. Diverse creators, especially with a local feel, are more likely to engage users in conversation.'
+        )}
+      </Typography>
+      <Typography color="secondary.light">
+        {t(
+          'In countries with security concerns, it is advisable to create fake personas for your own safety.'
+        )}
+      </Typography>
+    </Stack>
   )
 }
