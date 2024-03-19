67003b90aca9ae4a225546d1fdebcfff0359335d
Tanner <tanner.fleming@gmail.com>
2024-03-19 21:10:58 +0000

    fix: remove mocked provider

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/AddBlock.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/AddBlock.spec.tsx
@@ -1,70 +1,18 @@
-import { MockedProvider, MockedResponse } from '@apollo/client/testing'
-import { render } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import { render, screen } from '@testing-library/react'
 
 import { FlagsProvider } from '@core/shared/ui/FlagsProvider'
+import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 
-import {
-  ThemeMode,
-  ThemeName
-} from '../../../../../../../__generated__/globalTypes'
-import {
-  StepAndCardBlockCreate,
-  StepAndCardBlockCreateVariables
-} from '../../../../../../../__generated__/StepAndCardBlockCreate'
-import { STEP_AND_CARD_BLOCK_CREATE } from '../../../../../../libs/useStepAndCardBlockCreateMutation/useStepAndCardBlockCreateMutation'
 import { ThemeProvider } from '../../../../../ThemeProvider'
 
-import { AddBlock } from './AddBlock'
+import { AddBlock } from '.'
 
 describe('AddNewBlock', () => {
-  const stepAndCardBlockCreateMock: MockedResponse<
-    StepAndCardBlockCreate,
-    StepAndCardBlockCreateVariables
-  > = {
-    request: {
-      query: STEP_AND_CARD_BLOCK_CREATE,
-      variables: {
-        stepBlockCreateInput: {
-          id: 'stepId',
-          journeyId: 'journeyId'
-        },
-        cardBlockCreateInput: {
-          id: 'cardId',
-          journeyId: 'journeyId',
-          parentBlockId: 'stepId',
-          themeMode: ThemeMode.dark,
-          themeName: ThemeName.base
-        }
-      }
-    },
-    result: {
-      data: {
-        stepBlockCreate: {
-          id: 'stepId',
-          parentBlockId: null,
-          parentOrder: 0,
-          locked: false,
-          nextBlockId: null,
-          __typename: 'StepBlock'
-        },
-        cardBlockCreate: {
-          id: 'cardId',
-          parentBlockId: 'stepId',
-          parentOrder: 0,
-          backgroundColor: null,
-          coverBlockId: null,
-          themeMode: null,
-          themeName: null,
-          fullscreen: false,
-          __typename: 'CardBlock'
-        }
-      }
-    }
-  }
 
   it('renders add blocks toolbar properly', () => {
     const { getByTestId } = render(
-      <MockedProvider mocks={[stepAndCardBlockCreateMock]}>
+      <MockedProvider>
         <ThemeProvider>
           <FlagsProvider flags={{ formiumForm: true }}>
             <AddBlock />
@@ -93,9 +41,9 @@ describe('AddNewBlock', () => {
     ).toBeInTheDocument()
   })
 
-  it('does not render FormiumForm button when flag is falsh', () => {
+  it('does not render FormiumForm button when flag is false', () => {
     const { queryByTestId } = render(
-      <MockedProvider mocks={[stepAndCardBlockCreateMock]}>
+      <MockedProvider>
         <ThemeProvider>
           <FlagsProvider flags={{ formiumForm: false }}>
             <AddBlock />
