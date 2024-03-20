7741e17ca5104801290917e75e9d876794735de3
Edmond Shen <egsmond@gmail.com>
2024-03-20 21:22:26 +0000

    fix: remove filter accordions

--- apps/watch/src/components/VideosPage/FilterList/FilterList.tsx
+++ apps/watch/src/components/VideosPage/FilterList/FilterList.tsx
@@ -1,19 +1,14 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import SubtitlesIcon from '@mui/icons-material/Subtitles'
 import TitleIcon from '@mui/icons-material/Title'
 import VolumeUpIcon from '@mui/icons-material/VolumeUp'
-import MuiAccordion from '@mui/material/Accordion'
-import AccordianDetails from '@mui/material/AccordionDetails'
-import AccordionSummary from '@mui/material/AccordionSummary'
-import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import { Formik } from 'formik'
-import { ReactElement, ReactNode, SyntheticEvent, useState } from 'react'
+import { useTranslation } from 'next-i18next'
+import { ReactElement } from 'react'
 
 import { LanguageOption } from '@core/shared/ui/LanguageAutocomplete'
-import { SubmitListener } from '@core/shared/ui/SubmitListener'
 
 import { GetLanguages } from '../../../../__generated__/GetLanguages'
 import type { VideoPageFilter } from '../VideosPage'
@@ -76,47 +71,6 @@ const subtitleLanguageIds = [
   '184528'
 ]
 
-interface AccordianProps {
-  children: ReactNode
-  heading: string
-  headingIcon: ReactNode
-  expanded: boolean
-  onChange: (event: SyntheticEvent, isExpanded: boolean) => void
-}
-
-function Accordion({
-  children,
-  heading,
-  headingIcon,
-  expanded,
-  onChange
-}: AccordianProps): ReactElement {
-  return (
-    <MuiAccordion
-      expanded={expanded}
-      onChange={onChange}
-      variant="outlined"
-      disableGutters
-      sx={{ '&:before': { opacity: 0 } }}
-      TransitionProps={{ unmountOnExit: true }}
-    >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        sx={{ py: 2, px: 4 }}
-        data-testid={`filter-item-${heading.toLowerCase()}`}
-      >
-        <Stack direction="row" spacing={2}>
-          {headingIcon}
-          <Typography>{heading}</Typography>
-        </Stack>
-      </AccordionSummary>
-      <AccordianDetails sx={{ p: 4, backgroundColor: '#DCDDE5' }}>
-        {children}
-      </AccordianDetails>
-    </MuiAccordion>
-  )
-}
-
 interface FilterListProps {
   filter: VideoPageFilter
   onChange: (filter: VideoPageFilter) => void
@@ -130,12 +84,7 @@ export function FilterList({
   languagesData,
   languagesLoading
 }: FilterListProps): ReactElement {
-  const [expanded, setExpanded] = useState<string | false>('panel1')
-  const handleExpandChange =
-    (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
-      setExpanded(isExpanded ? panel : false)
-    }
-
+  const { t } = useTranslation()
   const subtitleLanguages = languagesData?.languages.filter((language) =>
     subtitleLanguageIds.includes(language.id)
   )
@@ -186,13 +135,12 @@ export function FilterList({
       enableReinitialize
     >
       {({ values, setFieldValue, handleChange, handleBlur }) => (
-        <Box data-testid="FilterList">
-          <Accordion
-            expanded={expanded === 'panel1'}
-            onChange={handleExpandChange('panel1')}
-            headingIcon={<VolumeUpIcon />}
-            heading="Languages"
-          >
+        <Stack data-testid="FilterList" gap={4}>
+          <Stack spacing={2}>
+            <Stack direction="row" spacing={2}>
+              <VolumeUpIcon />
+              <Typography>{t('Languages')}</Typography>
+            </Stack>
             <LanguagesFilter
               onChange={async (language) =>
                 await setFieldValue('language', language)
@@ -201,13 +149,12 @@ export function FilterList({
               languages={languagesData?.languages}
               loading={languagesLoading}
             />
-          </Accordion>
-          <Accordion
-            expanded={expanded === 'panel2'}
-            onChange={handleExpandChange('panel2')}
-            headingIcon={<SubtitlesIcon />}
-            heading="Subtitles"
-          >
+          </Stack>
+          <Stack spacing={2}>
+            <Stack direction="row" spacing={2}>
+              <SubtitlesIcon />
+              <Typography>{t('Subtitles')}</Typography>
+            </Stack>
             <LanguagesFilter
               onChange={async (subtitleLanguage) =>
                 await setFieldValue('subtitleLanguage', subtitleLanguage)
@@ -217,13 +164,12 @@ export function FilterList({
               loading={languagesLoading}
               helperText="54 languages"
             />
-          </Accordion>
-          <Accordion
-            expanded={expanded === 'panel3'}
-            onChange={handleExpandChange('panel3')}
-            headingIcon={<TitleIcon />}
-            heading="Title"
-          >
+          </Stack>
+          <Stack spacing={2}>
+            <Stack direction="row" spacing={2}>
+              <TitleIcon />
+              <Typography>{t('Title')}</Typography>
+            </Stack>
             <TextField
               value={values.title}
               name="title"
@@ -234,9 +180,8 @@ export function FilterList({
               helperText="724+ titles"
               fullWidth
             />
-          </Accordion>
-          <SubmitListener />
-        </Box>
+          </Stack>
+        </Stack>
       )}
     </Formik>
   )
