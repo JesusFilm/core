7c014cf65b111bf77e8825d4e905cf1ec2ba0a24
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2024-03-21 14:06:43 +1300

    fix: template gallery (#2501)
    Co-authored-by: jianwei1 <jianweichong08@gmail.com>

--- apps/journeys-admin/src/components/TemplateGallery/HeaderAndLanguageFilter/HeaderAndLanguageFilter.spec.tsx
+++ apps/journeys-admin/src/components/TemplateGallery/HeaderAndLanguageFilter/HeaderAndLanguageFilter.spec.tsx
@@ -6,8 +6,6 @@ import { GET_LANGUAGES } from '../../../libs/useLanguagesQuery/useLanguagesQuery
 
 import { HeaderAndLanguageFilter } from '.'
 
-import '../../../../test/i18n'
-
 jest.mock('@mui/material/useMediaQuery', () => ({
   __esModule: true,
   default: () => true
@@ -119,8 +117,11 @@ describe('HeaderAndLanguageFilter', () => {
     )
     await waitFor(() => {
       expect(getAllByText('Journey Templates')[0]).toBeInTheDocument()
-      fireEvent.click(getAllByRole('heading', { name: 'All Languages' })[0])
     })
+    // "All Languages" text is handled by the translation file /locale/en/journeys-admin.json
+    // Manually add the below line to the translation file if can't find the text
+    // "<0>Journey Templates</0><1>in</1><2>{{firstLanguage}}</2>_zero": "<0>Journey Templates</0><1>in</1><2>All Languages</2>"
+    fireEvent.click(getAllByRole('heading', { name: 'All Languages' })[0])
     fireEvent.click(getByRole('button', { name: 'German, Standard Deutsch' }))
     await waitFor(() => expect(onChange).toHaveBeenCalledTimes(1))
     fireEvent.click(getByRole('button', { name: 'French Français' }))
