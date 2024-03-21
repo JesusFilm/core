a22e55e420e112ec1e97f5528dbd543559a5e5a4
Mike Allison <dj.mikeallison@gmail.com>
2024-03-21 09:34:18 +0000

    feat: api-nexus

--- apps/api-nexus/src/app/app.module.ts
+++ apps/api-nexus/src/app/app.module.ts
@@ -31,6 +31,9 @@ import { ResourceModule } from './modules/resource/resource.module';
     }),
     LoggerModule.forRoot({
       pinoHttp: {
+        formatters: {
+          level: (label, _number) => ({ level: label }),
+        },
         redact: ['req.headers.authorization'],
         autoLogging: {
           ignore: (req) => req.url === '/.well-known/apollo/server-health',
