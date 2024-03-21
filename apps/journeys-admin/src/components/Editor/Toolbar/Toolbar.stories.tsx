87d49aa739397f0703b150aef86bb5334ab39f0f
jianwei1 <jianweichong08@gmail.com>
2024-03-21 01:28:54 +0000

    feat: toolbar stories

--- apps/journeys-admin/src/components/Editor/Toolbar/Toolbar.stories.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/Toolbar.stories.tsx
@@ -1,50 +1,55 @@
 import { MockedProvider } from '@apollo/client/testing'
 import Stack from '@mui/material/Stack'
 import { Meta, StoryObj } from '@storybook/react'
+import { ComponentProps } from 'react'
 
-import type { TreeBlock } from '@core/journeys/ui/block'
 import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 
-import { BlockFields_TypographyBlock as TypographyBlock } from '../../../../__generated__/BlockFields'
+import { BlockFields_ImageBlock as ImageBlock } from '../../../../__generated__/BlockFields'
 import { GetJourney_journey as Journey } from '../../../../__generated__/GetJourney'
 import { JourneyStatus } from '../../../../__generated__/globalTypes'
 import { simpleComponentConfig } from '../../../libs/storybook'
 
 import { Toolbar } from '.'
 
-const ToolbarStory: Meta<typeof Toolbar> = {
+const Demo: Meta<typeof Toolbar> = {
   ...simpleComponentConfig,
   component: Toolbar,
   title: 'Journeys-Admin/Editor/Toolbar'
 }
 
-export const Default: StoryObj<typeof Toolbar> = {
-  render: () => {
-    const selectedBlock: TreeBlock<TypographyBlock> = {
-      id: 'typography0.id',
-      __typename: 'TypographyBlock',
-      parentBlockId: 'card1.id',
-      parentOrder: 0,
-      content: 'Title',
-      variant: null,
-      color: null,
-      align: null,
-      children: []
-    }
+const image: ImageBlock = {
+  id: 'image1.id',
+  __typename: 'ImageBlock',
+  parentBlockId: null,
+  parentOrder: 0,
+  src: 'https://images.unsplash.com/photo-1649866725673-16dc15de5c29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1009&q=80',
+  alt: 'image.jpg',
+  width: 1920,
+  height: 1080,
+  blurhash: ''
+}
 
+const Template: StoryObj<
+  ComponentProps<typeof Toolbar> & { primaryImageBlock: ImageBlock }
+> = {
+  render: ({ primaryImageBlock }) => {
     return (
       <MockedProvider>
         <JourneyProvider
           value={{
             journey: {
+              title: 'My awesome journey',
+              description: 'My awesome journey description',
               status: JourneyStatus.published,
+              primaryImageBlock,
               tags: []
             } as unknown as Journey,
             variant: 'admin'
           }}
         >
-          <EditorProvider initialState={{ selectedBlock }}>
+          <EditorProvider>
             <Stack direction="row">
               <Toolbar />
             </Stack>
@@ -55,4 +60,15 @@ export const Default: StoryObj<typeof Toolbar> = {
   }
 }
 
-export default ToolbarStory
+export const Default = {
+  ...Template
+}
+
+export const WithJourneyImage = {
+  ...Template,
+  args: {
+    primaryImageBlock: image
+  }
+}
+
+export default Demo
