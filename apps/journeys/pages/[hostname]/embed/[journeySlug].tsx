61f29383e4f481cbd6e9df1da3858ebdd837ca1e
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-03-25 22:33:05 +0000

    fix: import issues

--- apps/journeys/pages/[hostname]/embed/[journeySlug].tsx
+++ apps/journeys/pages/[hostname]/embed/[journeySlug].tsx
@@ -1,8 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
-import { ReactElement } from 'react'
+import { ReactElement, useMemo } from 'react'
 
+import { TreeBlock } from '@core/journeys/ui/block'
+import { getStepTheme } from '@core/journeys/ui/getStepTheme'
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import { getJourneyRTL } from '@core/journeys/ui/rtl'
 import { transformer } from '@core/journeys/ui/transformer'
@@ -13,6 +15,7 @@ import {
   GetJourneyVariables,
   GetJourney_journey as Journey
 } from '../../../__generated__/GetJourney'
+import { StepFields } from '../../../__generated__/StepFields'
 import i18nConfig from '../../../next-i18next.config'
 import { EmbeddedPreview } from '../../../src/components/EmbeddedPreview'
 import { createApolloClient } from '../../../src/libs/apolloClient'
