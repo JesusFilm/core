c797fddf02a92d4122560b89337f990ba9ea3494
Siyang <csiyang@yahoo.co.nz>
2024-03-25 23:04:56 +0000

    fix: lint

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/Button/Button.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/Button/Button.tsx
@@ -6,8 +6,8 @@ import Stack from '@mui/material/Stack'
 import { styled } from '@mui/material/styles'
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
+import { useTranslation } from 'next-i18next'
 import { ReactElement } from 'react'
-import { useTranslation } from 'react-i18next'
 
 import Plus2 from '@core/shared/ui/icons/Plus2'
 
