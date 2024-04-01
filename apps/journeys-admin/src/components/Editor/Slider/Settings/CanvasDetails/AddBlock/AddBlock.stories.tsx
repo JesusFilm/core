3debf08babf3d66dc250c2b8d0dab2a99d2ff3e4
Tanner <tanner.fleming@gmail.com>
2024-04-01 19:05:44 +0000

    test: add video disabled to story

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/AddBlock.stories.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/AddBlock.stories.tsx
@@ -3,39 +3,61 @@ import type { Meta, StoryObj } from '@storybook/react'
 import type { TreeBlock } from '@core/journeys/ui/block'
 import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 
-import type { BlockFields_StepBlock } from '../../../../../../../__generated__/BlockFields'
-import { journeysAdminConfig } from '../../../../../../libs/storybook'
+import type { BlockFields_StepBlock as StepBlock } from '../../../../../../../__generated__/BlockFields'
+import {
+  TypographyAlign,
+  TypographyColor,
+  TypographyVariant
+} from '../../../../../../../__generated__/globalTypes'
+import { simpleComponentConfig } from '../../../../../../libs/storybook'
 
 import { AddBlock } from './AddBlock'
 
 const AddBlockStory: Meta<typeof AddBlock> = {
-  ...journeysAdminConfig,
+  ...simpleComponentConfig,
   component: AddBlock,
   title: 'Journeys-Admin/Editor/Slider/Settings/CanvasDetails/AddBlock'
 }
 
 export const Default: StoryObj<typeof AddBlock> = {
   render: () => {
-    const selectedStep: TreeBlock<BlockFields_StepBlock> = {
-      // Fix: Change the type to BlockFields_StepBlock
-      id: 'step1.id',
+    return <AddBlock />
+  }
+}
+
+export const DisabledNewVideo: StoryObj<typeof AddBlock> = {
+  render: () => {
+    const selectedStep: TreeBlock<StepBlock> = {
       __typename: 'StepBlock',
-      parentBlockId: null,
+      id: 'stepId',
+      parentBlockId: 'journeyId',
       parentOrder: 0,
-      locked: false,
+      locked: true,
       nextBlockId: null,
       children: [
         {
           id: 'card1.id',
           __typename: 'CardBlock',
-          parentBlockId: 'step1.id',
+          parentBlockId: 'stepId',
           parentOrder: 0,
           coverBlockId: null,
           backgroundColor: null,
-          themeMode: null, // Fix: Add the missing properties
+          themeMode: null,
           themeName: null,
-          fullscreen: true,
-          children: []
+          fullscreen: false,
+          children: [
+            {
+              id: 'typography0.id',
+              __typename: 'TypographyBlock',
+              parentBlockId: 'card1.id',
+              parentOrder: 0,
+              content: 'Title',
+              variant: TypographyVariant.h1,
+              color: TypographyColor.primary,
+              align: TypographyAlign.center,
+              children: []
+            }
+          ]
         }
       ]
     }
