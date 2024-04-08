cbe19808877dc1bbf09671b2316c9b4ae3345b9e
Kneesal <nisalcottingham@gmail.com>
2024-04-08 02:26:59 +0000

    fix: journey flow story

---
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/JourneyFlow.stories.tsx
@@ -0,0 +1,59 @@
+import Box from '@mui/material/Box'
+import { Meta, StoryObj } from '@storybook/react'
+import { ComponentProps } from 'react'
+
+import { TreeBlock } from '@core/journeys/ui/block'
+import { BlockFields_StepBlock as StepBlock } from '@core/journeys/ui/block/__generated__/BlockFields'
+import { EditorProvider } from '@core/journeys/ui/EditorProvider'
+import { transformer } from '@core/journeys/ui/transformer'
+
+import { journeysAdminConfig } from '../../../../libs/storybook'
+import { blocks, defaultJourney } from '../../data'
+
+import { JourneyFlow } from './JourneyFlow'
+
+const JourneyFlowStory: Meta<typeof JourneyFlow> = {
+  ...journeysAdminConfig,
+  component: JourneyFlow,
+  title: 'Journeys-Admin/Editor/Slider/JourneyFlow',
+  parameters: {
+    ...journeysAdminConfig.parameters,
+    layout: 'fullscreen'
+  }
+}
+
+const journey = { defaultJourney, blocks }
+
+const steps = transformer(journey?.blocks ?? []) as Array<TreeBlock<StepBlock>>
+
+const Template: StoryObj<
+  ComponentProps<typeof JourneyFlow> & {
+    steps: Array<TreeBlock<StepBlock>>
+  }
+> = {
+  render: (args) => {
+    return (
+      <EditorProvider
+        initialState={{
+          steps: args.steps
+        }}
+      >
+        <Box sx={{ width: '100vw', height: '100vh' }}>
+          <JourneyFlow />
+        </Box>
+      </EditorProvider>
+    )
+  }
+}
+
+export const Default = {
+  ...Template,
+  args: { steps }
+}
+
+export const Loading = {
+  ...Template,
+  args: { steps: undefined }
+}
+
+export default JourneyFlowStory
