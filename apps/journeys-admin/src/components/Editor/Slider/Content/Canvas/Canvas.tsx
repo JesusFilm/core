d26dcee279844d4ecccc95a18ffa257bf63a30c6
Kneesal <nisalcottingham@gmail.com>
2024-04-09 02:45:32 +0000

    fix: add frameRef that was not carried over properly

--- apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/Canvas.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/Canvas.tsx
@@ -179,7 +179,12 @@ export function Canvas(): ReactElement {
                     : `2px solid ${theme.palette.background.default}`
               }}
             >
-              <FramePortal width="100%" height="100%" dir={rtl ? 'rtl' : 'ltr'}>
+              <FramePortal
+                width="100%"
+                height="100%"
+                dir={rtl ? 'rtl' : 'ltr'}
+                ref={frameRef}
+              >
                 <ThemeProvider {...theme} rtl={rtl} locale={locale}>
                   <TransitionGroup
                     component={Box}
