7f91134280d8355eab90480cd1aeafe7b2a7d95d
Edmond Shen <egsmond@gmail.com>
2024-04-11 01:52:16 +0000

    fix: env name

--- apps/watch/src/components/VideosPage/utils/useVideoSearch/useVideoSearch.ts
+++ apps/watch/src/components/VideosPage/utils/useVideoSearch/useVideoSearch.ts
@@ -9,7 +9,7 @@ import { VideoPageFilter } from '../getQueryParameters'
 import { transformAlgoliaVideos } from '../transformAlgoliaVideos'
 
 const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_KEY ?? '',
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID ?? '',
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY ?? ''
 )
 const index = searchClient.initIndex(
