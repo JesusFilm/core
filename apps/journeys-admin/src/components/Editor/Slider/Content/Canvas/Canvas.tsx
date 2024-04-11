a6416d8f69c2129329f370c3d709ee1e6e627103
Kneesal <nisalcottingham@gmail.com>
2024-04-11 03:37:57 +0000

    fix: readd frame ref

--- apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/Canvas.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/Canvas.tsx
@@ -180,7 +180,13 @@ export function Canvas(): ReactElement {
                     : `2px solid ${theme.palette.background.default}`
               }}
             >
-              <FramePortal width="100%" height="100%" dir={rtl ? 'rtl' : 'ltr'}>
+              <FramePortal
+                width="100%"
+                height="100%"
+                dir={rtl ? 'rtl' : 'ltr'}
+                // ref added to assist typography block drag selection logic
+                ref={frameRef}
+              >
                 <ThemeProvider {...theme} rtl={rtl} locale={locale}>
                   <TransitionGroup
                     component={Box}
