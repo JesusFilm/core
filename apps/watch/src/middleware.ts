7d18ecb9b59dcae7dfc0a475e842b4dd357db374
Mike Allison <dj.mikeallison@gmail.com>
2024-04-13 01:10:36 +0000

    feat: app router watch

--- apps/watch/src/middleware.ts
+++ apps/watch/src/middleware.ts
@@ -9,7 +9,7 @@ acceptLanguage.languages(languages)
 export const config = {
   // matcher: '/:lng*'
   matcher: [
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)',
+    '/((?!api|fonts|locales|_next/static|_next/image|assets|favicon.ico|site.webmanifest|sw.js").*)',
     '/'
   ]
 }
