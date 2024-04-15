886e67f8ee545f4ccd23d8a5b8e0c17b4f1e57fd
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-04-15 02:28:17 +0000

    fix: add logging

--- dangerfile.ts
+++ dangerfile.ts
@@ -54,6 +54,8 @@ export default async () => {
     join(__dirname, '/.github/PULL_REQUEST_TEMPLATE/pull_request_template.md'),
     'utf8'
   )
+  console.log('pullRequestTemplate', pullRequestTemplate)
+  console.log('danger.github.pr.body', danger.github.pr.body)
   // check PR has description
   if (
     danger.github.pr.body.length < 10 ||
