bcabeb5caa7fb7ff567d240b20d448f9bb152f66
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-04-17 01:09:19 +0000

    fix: remove top level mocks folder

--- apps/__mocks__/swiper/modules.tsx
+++
@@ -1,4 +0,0 @@
-export const Navigation = (props) => null
-export const Pagination = (props) => null
-export const Scrollbar = (props) => null
-export const A11y = (props) => null
--- apps/__mocks__/swiper/react.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Swiper = ({ children, 'data-testid': dataTestId }) => (
-  <div data-testid={dataTestId}>{children}</div>
-)
-export const SwiperSlide = ({ children, 'data-testid': dataTestId }) => (
-  <div data-testid={dataTestId}>{children}</div>
-)
-export const SwiperClass = {}
