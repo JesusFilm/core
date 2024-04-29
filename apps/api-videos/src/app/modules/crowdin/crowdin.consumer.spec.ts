f58df1557e40f4364485ee081f47350273a10ca5
Edmond Shen <egsmond@gmail.com>
2024-04-29 03:34:52 +0000

    test: crowdin consumer test

---
+++ apps/api-videos/src/app/modules/crowdin/crowdin.consumer.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { Job } from 'bullmq'
+
+import { PrismaService } from '../../lib/prisma.service'
+
+import { CrowdinConsumer } from './crowdin.consumer'
+import { CrowdinService } from './crowdin.service'
+
+describe('CrowdinConsumer', () => {
+  let consumer: CrowdinConsumer
+  let crowdinService: CrowdinService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CrowdinConsumer, CrowdinService, PrismaService]
+    }).compile()
+
+    consumer = module.get<CrowdinConsumer>(CrowdinConsumer)
+    crowdinService = module.get<CrowdinService>(CrowdinService)
+  })
+
+  describe('process', () => {
+    it('should call syncVideosToAlgolia', async () => {
+      crowdinService.getCrowdinTranslations = jest.fn()
+
+      await consumer.process({} as unknown as Job)
+
+      expect(crowdinService.getCrowdinTranslations).toHaveBeenCalled()
+    })
+  })
+})
