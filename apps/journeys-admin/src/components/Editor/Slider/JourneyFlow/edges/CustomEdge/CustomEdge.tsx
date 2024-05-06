e742ef77f29fa279d5674565683d12937734f9cd
Siyang <csiyang@yahoo.co.nz>
2024-05-06 17:02:07 +0000

    feat: delete edge

--- apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/edges/CustomEdge/CustomEdge.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/edges/CustomEdge/CustomEdge.tsx
@@ -1,13 +1,18 @@
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
 import { ReactElement, useState } from 'react'
 import {
   BaseEdge,
   EdgeLabelRenderer,
   EdgeProps,
   getBezierPath,
-  useOnSelectionChange,
-  useStore
+  useOnSelectionChange
 } from 'reactflow'
+
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import X3Icon from '@core/shared/ui/icons/X3'
+
+import { useStepBlockNextBlockUpdateMutation } from '../../../../../../libs/useStepBlockNextBlockUpdateMutation'
 
 export function CustomEdge({
   id,
@@ -17,11 +22,15 @@ export function CustomEdge({
   targetY,
   sourcePosition,
   targetPosition,
-  style = {},
-  markerEnd
+  style = {}
 }: EdgeProps): ReactElement {
-  const [edgeColor, setEdgeColor] = useState('#0000001A')
-  const [markerEndColor, setMarkerEndColor] = useState('lightGrey')
+  const { journey } = useJourney()
+  const [stepBlockNextBlockUpdate] = useStepBlockNextBlockUpdateMutation()
+
+  const [sourceNodeId, setSourceNodeId] = useState<string | undefined>(
+    undefined
+  )
+  // const [edgeSelected, setEdgeSelected] = useState(false)
   const [edgePath, labelX, labelY] = getBezierPath({
     sourceX,
     sourceY,
@@ -33,44 +42,77 @@ export function CustomEdge({
 
   useOnSelectionChange({
     onChange: (selected) => {
-      if (selected.edges.find((edge) => edge.id === id) != null) {
-        setEdgeColor('#C52D3A')
-        setMarkerEndColor('#C52D3A')
+      const selectedEdge = selected.edges.find((edge) => edge.id === id)
+      console.log(selected)
+      if (selectedEdge != null) {
+        setSourceNodeId(selectedEdge.source)
       } else {
-        setEdgeColor('#0000001A')
-        setMarkerEndColor('lightGrey')
+        setSourceNodeId(undefined)
       }
     }
   })
 
-  const onEdgeClick = (params): void => {
-    console.log('button clicked! ' + params)
+  const onEdgeClick = (): void => {
+    if (journey == null || sourceNodeId == null) return
+    void stepBlockNextBlockUpdate({
+      variables: {
+        id: sourceNodeId,
+        journeyId: journey.id,
+        input: {
+          nextBlockId: null
+        }
+      },
+      optimisticResponse: {
+        stepBlockUpdate: {
+          id: sourceNodeId,
+          __typename: 'StepBlock',
+          nextBlockId: null
+        }
+      }
+    })
   }
 
   return (
     <>
       <BaseEdge
         path={edgePath}
-        markerEnd={`url(#1__color=${markerEndColor}&height=10&type=arrowclosed&width=10)`}
-        style={{ strokeWidth: 2, stroke: edgeColor, ...style }}
+        markerEnd={`url(#1__color=${
+          sourceNodeId != null ? '#C52D3A' : 'lightGrey'
+        }&height=10&type=arrowclosed&width=10)`}
+        style={{
+          strokeWidth: 2,
+          stroke: sourceNodeId != null ? '#C52D3A' : '#0000001A',
+          ...style
+        }}
       />
-      {/* <EdgeLabelRenderer>
-        <div
-          style={{
-            position: 'absolute',
-            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
-            fontSize: 12,
-            // everything inside EdgeLabelRenderer has no pointer events by default
-            // if you have an interactive element, set pointer-events: all
-            pointerEvents: 'all'
-          }}
-          className="nodrag nopan"
-        >
-          <button className="edgebutton" onClick={onEdgeClick}>
-            ×
-          </button>
-        </div>
-      </EdgeLabelRenderer> */}
+      {sourceNodeId != null && (
+        <EdgeLabelRenderer>
+          <Box
+            style={{
+              position: 'absolute',
+              transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+              pointerEvents: 'all'
+            }}
+          >
+            <IconButton
+              onClick={onEdgeClick}
+              sx={{
+                borderRadius: '100%',
+                backgroundColor: '#C52D3A',
+                height: '16px',
+                width: '16px',
+                display: 'flex',
+                '&:hover': {
+                  backgroundColor: '#C52D3A',
+                  opacity: 0.8
+                }
+              }}
+            >
+              <X3Icon sx={{ height: '12px', width: '12px', color: 'white' }} />
+            </IconButton>
+          </Box>
+        </EdgeLabelRenderer>
+      )}
     </>
   )
 }
