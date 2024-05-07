d355f981f155830d35487e2d26e373932c6faff6
Edmond Shen <egsmond@gmail.com>
2024-05-07 03:46:27 +0000

    chore: journeys-admin ui setup

---
+++ libs/journeys-admin/ui/i18next-parser.config.js
@@ -0,0 +1,7 @@
+const i18nextParserConfigBase = require('../../../i18next-parser.config.base')
+
+module.exports = {
+  ...i18nextParserConfigBase,
+  defaultNamespace: 'libs-journeys-admin-ui',
+  input: ['src/**/*.{js,jsx,ts,tsx}']
+}
