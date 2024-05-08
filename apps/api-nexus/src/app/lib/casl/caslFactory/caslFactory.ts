538d2a7904d3f673e6ea8e9235580fad8f65a4f2
Biniam Kassahun <bengeos@Biniams-MacBook-Pro.local>
2024-05-08 10:01:43 +0000

    feat: restore castl load roles

--- apps/api-nexus/src/app/lib/casl/caslFactory/caslFactory.ts
+++ apps/api-nexus/src/app/lib/casl/caslFactory/caslFactory.ts
@@ -1,7 +1,7 @@
 import { AbilityBuilder, PureAbility } from '@casl/ability'
 import { Injectable } from '@nestjs/common'
 
-import { Role } from '.prisma/api-journeys-client'
+import { Role } from '.prisma/api-nexus-client'
 import { CaslFactory } from '@core/nest/common/CaslAuthModule'
 
 import { channelAcl } from '../../../modules/channel/channel.acl'
