1a2b3dba15ce2bee9fbf8159a0c8e23f8541b78f
John Geronimo <johnageronimo1@gmail.com>
2024-05-15 23:59:47 +0000

    fix: remove nextblockid advance to next block

--- apps/journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
+++ apps/journeys/src/components/Conductor/NavigationButton/NavigationButton.tsx
@@ -108,28 +108,30 @@ export function NavigationButton({
       targetBlock != null &&
       getStepHeading(targetBlock.id, targetBlock.children, treeBlocks, t)
 
-    void stepNextEventCreate({
-      variables: {
-        input: {
-          id,
-          blockId: activeBlock.id,
-          label: stepName,
-          value: targetStepName,
-          nextStepId: targetBlock?.id
+    if (targetBlock != null) {
+      void stepNextEventCreate({
+        variables: {
+          input: {
+            id,
+            blockId: activeBlock.id,
+            label: stepName,
+            value: targetStepName,
+            nextStepId: targetBlock?.id
+          }
         }
-      }
-    })
+      })
 
-    TagManager.dataLayer({
-      dataLayer: {
-        event: 'step_next',
-        eventId: id,
-        blockId: activeBlock.id,
-        stepName,
-        targetStepId: targetBlock?.id,
-        targetStepName
-      }
-    })
+      TagManager.dataLayer({
+        dataLayer: {
+          event: 'step_next',
+          eventId: id,
+          blockId: activeBlock.id,
+          stepName,
+          targetStepId: targetBlock?.id,
+          targetStepName
+        }
+      })
+    }
   }
   // should always be called with previousActiveBlock()
   // should match with other handlePreviousNavigationEventCreate functions
