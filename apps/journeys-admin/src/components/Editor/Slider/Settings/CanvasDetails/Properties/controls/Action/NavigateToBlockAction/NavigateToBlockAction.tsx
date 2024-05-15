bd5c17f37cafc807662390fae5527d1264d0dcb9
John Geronimo <johnageronimo1@gmail.com>
2024-05-15 04:29:20 +0000

    refactor: show connected card

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/NavigateToBlockAction/NavigateToBlockAction.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/NavigateToBlockAction/NavigateToBlockAction.tsx
@@ -1,15 +1,27 @@
+import Box from '@mui/material/Box'
+import { Theme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { ReactElement } from 'react'
 
 import type { TreeBlock } from '@core/journeys/ui/block'
+import { BlockRenderer } from '@core/journeys/ui/BlockRenderer'
 import { useEditor } from '@core/journeys/ui/EditorProvider'
 import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import { getJourneyRTL } from '@core/journeys/ui/rtl'
+import { transformer } from '@core/journeys/ui/transformer'
+import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 
 import {
   BlockFields_ButtonBlock as ButtonBlock,
   BlockFields_VideoBlock as VideoBlock
 } from '../../../../../../../../../../__generated__/BlockFields'
-import { useNavigateToBlockActionUpdateMutation } from '../../../../../../../../../libs/useNavigateToBlockActionUpdateMutation'
-import { CardPreview, OnSelectProps } from '../../../../../../../../CardPreview'
+import {
+  ThemeMode,
+  ThemeName
+} from '../../../../../../../../../../__generated__/globalTypes'
+import { FramePortal } from '../../../../../../../../FramePortal'
+
+const CARD_WIDTH = 340
 
 export function NavigateToBlockAction(): ReactElement {
   const {
@@ -21,8 +33,6 @@ export function NavigateToBlockAction(): ReactElement {
     | TreeBlock<VideoBlock>
     | undefined
 
-  const [navigateToBlockActionUpdate] = useNavigateToBlockActionUpdateMutation()
-
   const currentActionStep =
     steps?.find(
       ({ id }) =>
@@ -30,18 +40,45 @@ export function NavigateToBlockAction(): ReactElement {
         id === currentBlock?.action?.blockId
     ) ?? undefined
 
-  async function handleSelectStep({ step }: OnSelectProps): Promise<void> {
-    if (currentBlock != null && journey != null && step != null) {
-      await navigateToBlockActionUpdate(currentBlock, step.id)
-    }
-  }
-
   return (
-    <CardPreview
-      selected={currentActionStep}
-      steps={steps}
-      onSelect={handleSelectStep}
-      testId="NavigationToBlockAction"
-    />
+    <Box
+      sx={{
+        transform: 'scale(0.25)',
+        transformOrigin: 'top left'
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          display: 'block',
+          width: 380,
+          height: 560,
+          zIndex: 2,
+          cursor: 'pointer'
+        }}
+      />
+      <FramePortal width={380} height={560}>
+        <ThemeProvider
+          themeName={journey?.themeName ?? ThemeName.base}
+          themeMode={journey?.themeMode ?? ThemeMode.dark}
+          // rtl={rtl}
+          // locale={locale}
+        >
+          <Box sx={{ p: 4, height: '100%', borderRadius: 4 }}>
+            <BlockRenderer
+              block={selectedBlock}
+              wrappers={{
+                ImageWrapper: NullWrapper,
+                VideoWrapper: NullWrapper
+              }}
+            />
+          </Box>
+        </ThemeProvider>
+      </FramePortal>
+    </Box>
   )
+}
+
+function NullWrapper({ children }): ReactElement {
+  return <fieldset disabled>{children}</fieldset>
 }
