7ee5a658a09d3e1ffcdcffafccbb111226527656
jianwei1 <jianweichong08@gmail.com>
2024-05-16 02:14:04 +0000

    feat: began work on tests and stories

---
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/edges/CustomEdge/CustomEdge.spec.tsx
@@ -0,0 +1,33 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { render } from '@testing-library/react'
+import { ReactFlowProvider } from 'reactflow'
+
+import { CustomEdge } from './CustomEdge'
+
+describe('CustomEdge', () => {
+  beforeEach(() => {
+    mockReactFlow()
+  })
+
+  // source/target x and y are numbers
+
+  // source/target positions are Position enum
+
+  // could maybe render edge without creating nodes ? and just using source/target
+
+  it('should render customEdge', () => {
+    render(
+      <ReactFlowProvider>
+        <MockedProvider>
+          <CustomEdge
+            id="id"
+            style={{}}
+            edgePath="M-230.5,38 C-145.75,38 -145.75,48 -61,48"
+          >
+            <Typography>Children</Typography>
+          </CustomEdge>
+        </MockedProvider>
+      </ReactFlowProvider>
+    )
+  })
+})
