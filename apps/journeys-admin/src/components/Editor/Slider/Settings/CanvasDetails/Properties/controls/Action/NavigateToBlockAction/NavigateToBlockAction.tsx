74b303649616564b881ad824f69521195d1e08e4
Kneesal <nisalcottingham@gmail.com>
2024-05-16 03:00:54 +0000

    refactor: selected card

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/NavigateToBlockAction/NavigateToBlockAction.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/NavigateToBlockAction/NavigateToBlockAction.tsx
@@ -1,27 +1,28 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { useTranslation } from 'next-i18next'
 import { ReactElement } from 'react'
 
-import type { TreeBlock } from '@core/journeys/ui/block'
-import { useEditor } from '@core/journeys/ui/EditorProvider'
-import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import { type TreeBlock } from '@core/journeys/ui/block'
+import { ActiveSlide, useEditor } from '@core/journeys/ui/EditorProvider'
 
 import {
   BlockFields_ButtonBlock as ButtonBlock,
   BlockFields_VideoBlock as VideoBlock
 } from '../../../../../../../../../../__generated__/BlockFields'
-import { useNavigateToBlockActionUpdateMutation } from '../../../../../../../../../libs/useNavigateToBlockActionUpdateMutation'
-import { CardPreview, OnSelectProps } from '../../../../../../../../CardPreview'
+
+import { CardItem } from './CardItem/CardItem'
 
 export function NavigateToBlockAction(): ReactElement {
+  const { t } = useTranslation('apps-journeys-admin')
   const {
-    state: { steps, selectedBlock }
+    state: { steps, selectedBlock },
+    dispatch
   } = useEditor()
-  const { journey } = useJourney()
   const currentBlock = selectedBlock as
     | TreeBlock<ButtonBlock>
     | TreeBlock<VideoBlock>
     | undefined
-
-  const [navigateToBlockActionUpdate] = useNavigateToBlockActionUpdateMutation()
 
   const currentActionStep =
     steps?.find(
@@ -30,18 +31,25 @@ export function NavigateToBlockAction(): ReactElement {
         id === currentBlock?.action?.blockId
     ) ?? undefined
 
-  async function handleSelectStep({ step }: OnSelectProps): Promise<void> {
-    if (currentBlock != null && journey != null && step != null) {
-      await navigateToBlockActionUpdate(currentBlock, step.id)
-    }
+  function handleButtonClick(): void {
+    dispatch({
+      type: 'SetActiveSlideAction',
+      activeSlide: ActiveSlide.JourneyFlow
+    })
   }
-
   return (
-    <CardPreview
-      selected={currentActionStep}
-      steps={steps}
-      onSelect={handleSelectStep}
-      testId="NavigationToBlockAction"
-    />
+    <Box sx={{ p: 4 }}>
+      {currentActionStep == null ? (
+        <Button
+          variant="outlined"
+          onClick={handleButtonClick}
+          sx={{ height: 140 }}
+        >
+          {t('Connect this block to a card in the Journey Flow')}
+        </Button>
+      ) : (
+        <CardItem step={currentActionStep} id={currentActionStep.id} />
+      )}
+    </Box>
   )
 }
