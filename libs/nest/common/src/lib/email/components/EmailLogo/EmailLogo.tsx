ac5abd0628d748d97fbb529174713280294fd181
Kneesal <nisalcottingham@gmail.com>
2024-05-22 01:36:03 +0000

    fix: url

--- libs/nest/common/src/lib/email/components/EmailLogo/EmailLogo.tsx
+++ libs/nest/common/src/lib/email/components/EmailLogo/EmailLogo.tsx
@@ -20,7 +20,7 @@ export function EmailLogo(): ReactElement {
           lineHeight: '0px',
           visibility: 'hidden'
         }}
-        src="https://your.nextstep.is/LogoHorizontalDark.png"
+        src="https://your.nextstep.is/LogoHoriztonalDark.png"
       />
     </>
   )
