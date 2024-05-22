d3cfcb9a79a82ef4a0010a96b0422552d356456a
Kneesal <nisalcottingham@gmail.com>
2024-05-22 02:53:41 +0000

    fix: add important override

--- libs/nest/common/src/lib/email/components/Head/Head.tsx
+++ libs/nest/common/src/lib/email/components/Head/Head.tsx
@@ -8,14 +8,13 @@ export function Head(): ReactElement {
       <meta name="supported-color-schemes" content="light dark" />
       <style>
         {`
-          @media (prefers-color-scheme: dark) {
-            .light-img {
-              display: none;
+          @media (prefers-color-scheme: dark ) {
+            /* Shows Dark Mode-Only Content, Like Images */
+            .dark-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; }
+              
+            /* Hides Light Mode-Only Content, Like Images */
+            .light-img { display:none; display:none !important; }
             }
-            .dark-img {
-              display: block;
-            }
-          }
 
           /* Shows Dark Mode-Only Content, Like Images */
           [data-ogsc] .dark-img { display:block !important; width: auto !important; overflow: visible !important; float: none !important; max-height:inherit !important; max-width:inherit !important; line-height: auto !important; margin-top:0px !important; visibility:inherit !important; } 
