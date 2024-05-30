c931a07980ddd3740572aa6cc6dc354ba2f94577
Mike Allison <dj.mikeallison@gmail.com>
2024-05-30 03:55:41 +0000

    feat: bun

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -41,6 +41,7 @@ curl -sSL https://router.apollo.dev/download/nix/v1.43.1 | sh
 mv router apps/api-gateway/
 
 # install doppler
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
 echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
 sudo apt-get update && sudo apt-get install doppler
