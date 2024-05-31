be4f1e08f539a34a1262268b11cb44f67d8772e1
jianwei1 <jianweichong08@gmail.com>
2024-05-31 04:36:24 +0000

    refactor: used map on nextlinkbuttons, moved to separate component

---
+++ apps/watch/src/components/ResourcePageHeader/ResourceNextLinkButtons/ResourceNextLinkButtons.tsx
@@ -0,0 +1,71 @@
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import NextLink from 'next/link'
+import { useTranslation } from 'next-i18next'
+import { ReactElement, useState } from 'react'
+
+import BulbIcon from '@core/shared/ui/icons/Bulb'
+import Calendar1Icon from '@core/shared/ui/icons/Calendar1'
+import Grid1Icon from '@core/shared/ui/icons/Grid1'
+import JourneysIcon from '@core/shared/ui/icons/Journeys'
+import Play1Icon from '@core/shared/ui/icons/Play1'
+import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
+import { ThemeMode, ThemeName } from '@core/shared/ui/themes'
+
+interface ResourceNextLinkButtonsProps {
+  selectedTab: string
+  setSelectedTab: (string) => void
+}
+
+const ResourceNextLinkButtonsData = [
+  { label: 'Journeys', icon: <JourneysIcon />, href: '/journeys' },
+  { label: 'Videos', icon: <Play1Icon />, href: '/videos' },
+  { label: 'Strategies', icon: <BulbIcon />, href: '/strategies' },
+  { label: 'Calendar', icon: <Calendar1Icon />, href: '/calendar' },
+  { label: 'Apps', icon: <Grid1Icon />, href: '/apps' }
+]
+
+export function ResourceNextLinkButtons({
+  selectedTab,
+  setSelectedTab
+}: ResourceNextLinkButtonsProps): ReactElement {
+  const { t } = useTranslation('apps-watch')
+  return (
+    <>
+      {/* <ThemeProvider themeName={ThemeName.website} themeMode={ThemeMode.dark}> */}
+      <Container
+        maxWidth="xxl"
+        disableGutters
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'space-between'
+        }}
+      >
+        {ResourceNextLinkButtonsData.map(({ label, icon, href }) => (
+          <NextLink href={href} passHref legacyBehavior key={label}>
+            <Button
+              component="a"
+              color="secondary"
+              sx={{
+                flexGrow: 1,
+                textAlign: 'center',
+                borderRadius: '40px !important',
+                backgroundColor:
+                  selectedTab === label.toLowerCase()
+                    ? 'lightgrey'
+                    : 'transparent'
+                // color: (theme) => theme.palette.secondary.main
+              }}
+              onClick={() => setSelectedTab(label.toLowerCase())}
+              startIcon={icon}
+            >
+              {t(label)}
+            </Button>
+          </NextLink>
+        ))}
+      </Container>
+      {/* </ThemeProvider> */}
+    </>
+  )
+}
--- apps/watch/src/components/ResourcePageHeader/ResourcePageHeader.tsx
+++ apps/watch/src/components/ResourcePageHeader/ResourcePageHeader.tsx
@@ -1,22 +1,12 @@
-import { useTheme } from '@mui/material'
 import Button from '@mui/material/Button'
-import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { ReactElement, useState } from 'react'
 
-import BulbIcon from '@core/shared/ui/icons/Bulb'
-import Calendar1Icon from '@core/shared/ui/icons/Calendar1'
-import Grid1Icon from '@core/shared/ui/icons/Grid1'
-import JourneysIcon from '@core/shared/ui/icons/Journeys'
-import Play1Icon from '@core/shared/ui/icons/Play1'
-import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
-import { ThemeMode, ThemeName } from '@core/shared/ui/themes'
-
 import { ResourceAppBar } from './ResourceAppBar'
+import { ResourceNextLinkButtons } from './ResourceNextLinkButtons'
 import { ResourceTabButtons } from './ResourceTabButtons'
 
 interface TestComponentProps {
@@ -66,128 +56,6 @@ function TestComponent({
   )
 }
 
-interface NextLinkButtonsProps {
-  selectedTab: string
-  setSelectedTab: (string) => void
-}
-
-function NextLinkButtons({
-  selectedTab,
-  setSelectedTab
-}: NextLinkButtonsProps): ReactElement {
-  const { t } = useTranslation('apps-watch')
-  //  const theme = useTheme()
-  // console.log(theme.palette.primary.main)
-  return (
-    <>
-      {/* <Stack flexDirection="row"> */}
-      {/* <ThemeProvider themeName={ThemeName.website} themeMode={ThemeMode.dark}> */}
-      <Container
-        maxWidth="xxl"
-        disableGutters
-        sx={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: 'space-between'
-        }}
-      >
-        <NextLink href="/journeys" passHref legacyBehavior>
-          <Button
-            component="a"
-            color="secondary"
-            sx={{
-              flexGrow: 1,
-              textAlign: 'center',
-              borderRadius: '40px !important',
-              backgroundColor:
-                selectedTab === 'journeys' ? 'lightgrey' : 'transparent'
-              // color: (theme) => theme.palette.secondary.main
-            }}
-            onClick={() => setSelectedTab('journeys')}
-            startIcon={<JourneysIcon />}
-          >
-            {t('Journeys')}
-          </Button>
-        </NextLink>
-        <NextLink href="/videos" passHref legacyBehavior>
-          <Button
-            component="a"
-            color="secondary"
-            sx={{
-              flexGrow: 1,
-              textAlign: 'center',
-              borderRadius: '40px !important',
-              backgroundColor:
-                selectedTab === 'videos' ? 'lightgrey' : 'transparent'
-              // color: (theme) => theme.palette.secondary.main
-            }}
-            onClick={() => setSelectedTab('videos')}
-            startIcon={<Play1Icon />}
-          >
-            {t('Videos')}
-          </Button>
-        </NextLink>
-        <NextLink href="/strategies" passHref legacyBehavior>
-          <Button
-            component="a"
-            color="secondary"
-            sx={{
-              flexGrow: 1,
-              textAlign: 'center',
-              borderRadius: '40px !important',
-              backgroundColor:
-                selectedTab === 'strategies' ? 'lightgrey' : 'transparent'
-              // color: (theme) => theme.palette.secondary.main
-            }}
-            onClick={() => setSelectedTab('strategies')}
-            startIcon={<BulbIcon />}
-          >
-            {t('Strategies')}
-          </Button>
-        </NextLink>
-        <NextLink href="/calendar" passHref legacyBehavior>
-          <Button
-            component="a"
-            color="secondary"
-            sx={{
-              flexGrow: 1,
-              textAlign: 'center',
-              borderRadius: '40px !important',
-              backgroundColor:
-                selectedTab === 'calendar' ? 'lightgrey' : 'transparent'
-              // color: (theme) => theme.palette.secondary.main
-            }}
-            onClick={() => setSelectedTab('calendar')}
-            startIcon={<Calendar1Icon />}
-          >
-            {t('Calendar')}
-          </Button>
-        </NextLink>
-        <NextLink href="/apps" passHref legacyBehavior>
-          <Button
-            component="a"
-            color="secondary"
-            sx={{
-              flexGrow: 1,
-              textAlign: 'center',
-              borderRadius: '40px !important',
-              backgroundColor:
-                selectedTab === 'apps' ? 'lightgrey' : 'transparent'
-              // color: (theme) => theme.palette.secondary.main
-            }}
-            onClick={() => setSelectedTab('apps')}
-            startIcon={<Grid1Icon />}
-          >
-            {t('Apps')}
-          </Button>
-        </NextLink>
-      </Container>
-      {/* </ThemeProvider> */}
-      {/* </Stack> */}
-    </>
-  )
-}
-
 // TODO:
 // Refactor out app bar and tab buttons x
 // add button/component below tab buttons for testing x
@@ -198,9 +66,9 @@ function NextLinkButtons({
 
 // change toggle buttons into nextLinks(which have a button with 'a' component inside) x
 // remove router logic from handleTabChange x
-// create page for resourcepageheader, to deploy on watch
+// refactor nextLink buttons x
+// create page for resourcepageheader, to deploy on watch x
 // use current pathname to highlight the selected button (eg '/journeys' pathname should highlight journeys button)
-// refactor nextLink buttons
 
 // add nextLinks to toggle buttons
 // try to make buttons go full width of screen
@@ -254,7 +122,7 @@ export function ResourcePageHeader(): ReactElement {
           selectedTab={selectedTab}
           handleTabChange={handleTabChange}
         /> */}
-        <NextLinkButtons
+        <ResourceNextLinkButtons
           selectedTab={selectedTab}
           setSelectedTab={setSelectedTab}
         />
