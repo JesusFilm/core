5f60fe3d080019c7f942202fa2d05275b164b9f5
Mike Allison <dj.mikeallison@gmail.com>
2024-06-06 18:40:51 +0000

    feat: plausible

--- infrastructure/kube/alb/deploy.sh
+++ infrastructure/kube/alb/deploy.sh
@@ -14,4 +14,6 @@ helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
   --set clusterName=jfp-eks-prod \
   --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller 
+  --set serviceAccount.name=aws-load-balancer-controller \
+  --set region=us-east-2 \
+  --set vpcId=vpc-0b722b0a1f7789afd 
--- infrastructure/kube/ssl/deploy.sh
+++ infrastructure/kube/ssl/deploy.sh
@@ -1,3 +1,1 @@
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.crds.yaml
-helm repo add jetstack https://charts.jetstack.io --force-update
-helm install cert-manager --namespace cert-manager --version v1.15.0 jetstack/cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.0/cert-manager.yaml
