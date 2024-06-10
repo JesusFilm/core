80ad713273814cfaaf7b8971b48a269e3152b69c
jianwei1 <jianweichong08@gmail.com>
2024-06-10 05:46:20 +0000

    feat: changed icons on nextlink buttons

--- apps/watch/src/components/ResourcePageHeader/ResourceNextLinkButtons/ResourceNextLinkButtons.tsx
+++ apps/watch/src/components/ResourcePageHeader/ResourceNextLinkButtons/ResourceNextLinkButtons.tsx
@@ -7,9 +7,11 @@ import { ReactElement, useState } from 'react'
 
 import BulbIcon from '@core/shared/ui/icons/Bulb'
 import Calendar1Icon from '@core/shared/ui/icons/Calendar1'
+import Calendar2Icon from '@core/shared/ui/icons/Calendar2'
 import Grid1Icon from '@core/shared/ui/icons/Grid1'
 import JourneysIcon from '@core/shared/ui/icons/Journeys'
 import Play1Icon from '@core/shared/ui/icons/Play1'
+import TerminalIcon from '@core/shared/ui/icons/Terminal'
 import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 import { ThemeMode, ThemeName } from '@core/shared/ui/themes'
 
@@ -19,17 +21,18 @@ interface ResourceNextLinkButtonsProps {
 }
 
 const ResourceNextLinkButtonsData = [
+  { label: 'Strategies', icon: <TerminalIcon />, href: '/strategies' },
   { label: 'Journeys', icon: <JourneysIcon />, href: '/journeys' },
   { label: 'Videos', icon: <Play1Icon />, href: '/watch' },
-  { label: 'Strategies', icon: <BulbIcon />, href: '/strategies' },
-  { label: 'Calendar', icon: <Calendar1Icon />, href: '/calendar' },
+  { label: 'Calendar', icon: <Calendar2Icon />, href: '/calendar' },
   { label: 'Products', icon: <Grid1Icon />, href: '/products' }
 ]
 
-export function ResourceNextLinkButtons({
-  selectedTab,
-  setSelectedTab
-}: ResourceNextLinkButtonsProps): ReactElement {
+export function ResourceNextLinkButtons(): ReactElement {
+  //   {
+  //   selectedTab,
+  //   setSelectedTab
+  // }: ResourceNextLinkButtonsProps
   const { t } = useTranslation('apps-watch')
   const router = useRouter()
   return (
@@ -56,9 +59,10 @@ export function ResourceNextLinkButtons({
                 backgroundColor: router.pathname.startsWith(href)
                   ? 'lightgrey'
                   : 'transparent'
+
                 // color: (theme) => theme.palette.secondary.main
               }}
-              onClick={() => setSelectedTab(label.toLowerCase())}
+              // onClick={() => setSelectedTab(label.toLowerCase())}
               startIcon={icon}
             >
               {t(label)}
