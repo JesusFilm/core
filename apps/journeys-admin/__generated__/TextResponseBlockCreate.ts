fd8128320276d9025aa7aaa374f081751ed21f5d
John Geronimo <johnageronimo1@gmail.com>
2024-06-11 03:14:07 +0000

    fix: adding a new text response

--- apps/journeys-admin/__generated__/TextResponseBlockCreate.ts
+++ apps/journeys-admin/__generated__/TextResponseBlockCreate.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { TextResponseBlockCreateInput, IconBlockCreateInput, TextResponseBlockUpdateInput, IconName, IconSize, IconColor } from "./globalTypes";
+import { TextResponseBlockCreateInput } from "./globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: TextResponseBlockCreate
@@ -19,36 +19,10 @@ export interface TextResponseBlockCreate_textResponseBlockCreate {
   minRows: number | null;
 }
 
-export interface TextResponseBlockCreate_submitIcon {
-  __typename: "IconBlock";
-  id: string;
-  parentBlockId: string | null;
-  parentOrder: number | null;
-  iconName: IconName | null;
-  iconSize: IconSize | null;
-  iconColor: IconColor | null;
-}
-
-export interface TextResponseBlockCreate_textResponseBlockUpdate {
-  __typename: "TextResponseBlock";
-  id: string;
-  parentBlockId: string | null;
-  parentOrder: number | null;
-  label: string;
-  hint: string | null;
-  minRows: number | null;
-}
-
 export interface TextResponseBlockCreate {
   textResponseBlockCreate: TextResponseBlockCreate_textResponseBlockCreate;
-  submitIcon: TextResponseBlockCreate_submitIcon;
-  textResponseBlockUpdate: TextResponseBlockCreate_textResponseBlockUpdate | null;
 }
 
 export interface TextResponseBlockCreateVariables {
   input: TextResponseBlockCreateInput;
-  iconBlockCreateInput: IconBlockCreateInput;
-  id: string;
-  journeyId: string;
-  updateInput: TextResponseBlockUpdateInput;
 }
