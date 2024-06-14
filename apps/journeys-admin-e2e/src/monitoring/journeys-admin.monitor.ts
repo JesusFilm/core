4a43eff0ecaa8fbeab1ac985771867af2fb8c256
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-06-14 14:15:57 +1200

    test: check for env vars before using

--- apps/journeys-admin-e2e/src/monitoring/journeys-admin.monitor.ts
+++ apps/journeys-admin-e2e/src/monitoring/journeys-admin.monitor.ts
@@ -6,17 +6,24 @@ NS Admin: Monitoring
 test('NS Admin Monitoring: Check user can login and logout successfuly', async ({
   page
 }) => {
+  if (!process.env.PLAYWRIGHT_EMAIL || !process.env.PLAYWRIGHT_PASSWORD) {
+    throw new Error('Email & password environment variables are not set in checkly.')
+  }
+  
+  const email = process.env.PLAYWRIGHT_EMAIL as string
+  const password = process.env.PLAYWRIGHT_PASSWORD as string
+
   await page.goto('https://admin.nextstep.is/')
   await page.getByPlaceholder('Enter your email address here').click()
   await page
     .getByPlaceholder('Enter your email address here')
-    .fill(process.env.PLAYWRIGHT_EMAIL)
+    .fill(email)
   await page.getByRole('button', { name: 'Continue with email' }).click()
   await page.getByPlaceholder('Enter Password').click()
-  await page.getByPlaceholder('Enter Password').fill('pwu@ec')
+  await page.getByPlaceholder('Enter Password').fill(password)
   await page.getByRole('button', { name: 'Sign In' }).click()
   await page.getByTestId('NavigationListItemProfile').click()
-  await expect(page.getByText(process.env.PLAYWRIGHT_EMAIL)).toBeVisible()
+  await expect(page.getByText(email)).toBeVisible()
   await page.getByText('Logout').click()
   await expect(
     page.getByRole('button', { name: 'Continue with email' })
