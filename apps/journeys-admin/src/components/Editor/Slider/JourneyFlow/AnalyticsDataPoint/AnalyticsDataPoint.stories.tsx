c9e34571521ee357c7ec547659a4094e9a51017b
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-06-14 18:55:48 +0000

    feat: add AnalyticsDataPoint stories

---
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/AnalyticsDataPoint/AnalyticsDataPoint.stories.tsx
@@ -0,0 +1,36 @@
+import { StoryObj } from '@storybook/react'
+import { ComponentPropsWithoutRef } from 'react'
+
+import PointerClick from '@core/shared/ui/icons/PointerClick'
+
+import { simpleComponentConfig } from '../../../../../libs/storybook'
+
+import { AnalyticsDataPoint } from './AnalyticsDataPoint'
+
+const AnalyticsDataPointDemo = {
+  ...simpleComponentConfig,
+  component: AnalyticsDataPoint,
+  title: 'Journeys-Admin/Editor/Slider/JourneyFlow/AnalyticsDataPoint'
+}
+
+const Template: StoryObj<ComponentPropsWithoutRef<typeof AnalyticsDataPoint>> =
+  {
+    render: (args) => <AnalyticsDataPoint {...args} />
+  }
+
+export const Default = {
+  ...Template,
+  args: {
+    children: 10,
+    Icon: PointerClick
+  }
+}
+
+export const WithoutIcon = {
+  ...Template,
+  args: {
+    children: '10'
+  }
+}
+
+export default AnalyticsDataPointDemo
