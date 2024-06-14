ad302374a9dc8ee64fb9d8ddd11dcc2f009e3203
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-06-14 03:09:44 +0000

    test: linknodeanalytics

---
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/nodes/LinkNode/LinkNodeAnalytics/LinkNodeAnalytics.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+
+import { LinkNodeAnalytics } from '.'
+
+describe('LinkNodeAnalytics', () => {
+  it('should render with count', () => {
+    render(<LinkNodeAnalytics>{10}</LinkNodeAnalytics>)
+
+    const icon = screen.getByTestId('PointerClickIcon')
+    const clickCount = screen.getByText('10')
+
+    expect(icon).toBeInTheDocument()
+    expect(clickCount).toBeInTheDocument()
+  })
+
+  it('should render default value', () => {
+    render(<LinkNodeAnalytics />)
+
+    // should show the tilde
+    const clickCount = screen.getByText('~')
+    expect(clickCount).toBeInTheDocument()
+  })
+})
