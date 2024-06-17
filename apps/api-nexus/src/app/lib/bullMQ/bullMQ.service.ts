bcf4314db5437211a3d7147eec0fcc107db85ce2
Biniam Kassahun <bengeos@Biniams-MacBook-Pro.local>
2024-06-17 08:24:03 +0000

    feat: fix prettier

--- apps/api-nexus/src/app/lib/bullMQ/bullMQ.service.ts
+++ apps/api-nexus/src/app/lib/bullMQ/bullMQ.service.ts
@@ -227,7 +227,7 @@ export class BullMQService {
       })
       const job: UpdateVideoLocalizationJob = {
         batchId: batch.id,
-        batchTaskId: batchTask?.id ?? '',∏
+        batchTaskId: batchTask?.id ?? '',
         accessToken,
         channel: {
           id: channel?.id ?? '',
