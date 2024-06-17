6eec7deb977dc49ff06127f8e53436f0d3ac43ef
Siyang <csiyang@yahoo.co.nz>
2024-06-17 01:37:36 +0000

    feat: add plausible client

--- apps/api-journeys/src/app/modules/plausible/plausible.service.ts
+++ apps/api-journeys/src/app/modules/plausible/plausible.service.ts
@@ -6,6 +6,7 @@ import {
 } from '@apollo/client'
 import { InjectQueue } from '@nestjs/bullmq'
 import { Injectable, OnModuleInit } from '@nestjs/common'
+import axios, { AxiosInstance } from 'axios'
 import { Queue } from 'bullmq'
 import { GraphQLError } from 'graphql'
 import camelCase from 'lodash/camelCase'
@@ -64,6 +65,7 @@ interface PlausibleAPIStatsAggregateResponse {
 @Injectable()
 export class PlausibleService implements OnModuleInit {
   client: ApolloClient<NormalizedCacheObject>
+  plausibleClient: AxiosInstance
 
   constructor(
     @InjectQueue('api-journeys-plausible')
@@ -81,6 +83,12 @@ export class PlausibleService implements OnModuleInit {
         }
       }),
       cache: new InMemoryCache()
+    })
+    this.plausibleClient = axios.create({
+      baseURL: process.env.PLAUSIBLE_URL,
+      headers: {
+        Authorization: `Bearer ${process.env.PLAUSIBLE_API_KEY}`
+      }
     })
     await this.plausibleQueue.add('plausibleCreateSites', {
       __typename: 'plausibleCreateSites'
@@ -140,145 +148,6 @@ export class PlausibleService implements OnModuleInit {
         plausibleToken: site.data.sharedLinks[0].slug
       }
     })
-  }
-
-  async getStatsRealtimeVisitors(
-    id: string,
-    type: 'journey' | 'team'
-  ): Promise<number> {
-    const domain =
-      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
-    const response = await this.client.get<number>(
-      `/api/v1/stats/realtime/visitors`,
-      {
-        params: {
-          site_id: domain
-        }
-      }
-    )
-    return response.data
-  }
-
-  async getStatsAggregate(
-    id: string,
-    type: 'journey' | 'team',
-    params: {
-      metrics: string
-    } & PlausibleStatsAggregateFilter
-  ): Promise<PlausibleStatsAggregateResponse> {
-    const domain =
-      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
-    try {
-      const response = await this.client.get<{
-        results: PlausibleAPIStatsAggregateResponse
-      }>(`/api/v1/stats/aggregate`, {
-        params: {
-          site_id: domain,
-          ...params
-        }
-      })
-      const accumulator: PlausibleStatsAggregateResponse = {}
-      return reduce(
-        response.data.results,
-        (acc, value, key) => {
-          if (value != null) acc[camelCase(key)] = value
-          return acc
-        },
-        accumulator
-      )
-    } catch (error) {
-      const message = get(error, 'response.data.error')
-      if (typeof message === 'string')
-        throw new GraphQLError(message, {
-          extensions: { code: 'BAD_USER_INPUT' }
-        })
-      throw error
-    }
-  }
-
-  async getStatsBreakdown(
-    id: string,
-    type: 'journey' | 'team',
-    params: {
-      metrics: string
-    } & PlausibleStatsBreakdownFilter
-  ): Promise<PlausibleStatsResponse[]> {
-    const domain =
-      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
-    try {
-      const response = await this.client.get<{
-        results: PlausibleAPIStatsBreakdownResponse[]
-      }>(`/api/v1/stats/breakdown`, {
-        params: {
-          site_id: domain,
-          ...params
-        }
-      })
-
-      const propertyKey = last(params.property.split(':') ?? []) ?? ''
-      return response.data.results.map((result) => {
-        const accumulator: PlausibleStatsResponse = {
-          property: result[propertyKey] as string
-        }
-        return reduce(
-          result,
-          (acc, value, key) => {
-            if (key !== propertyKey) acc[camelCase(key)] = value
-            return acc
-          },
-          accumulator
-        )
-      })
-    } catch (error) {
-      const message = get(error, 'response.data.error')
-      if (typeof message === 'string')
-        throw new GraphQLError(message, {
-          extensions: { code: 'BAD_USER_INPUT' }
-        })
-      throw error
-    }
-  }
-
-  async getStatsTimeseries(
-    id: string,
-    type: 'journey' | 'team',
-    params: {
-      metrics: string
-    } & PlausibleStatsTimeseriesFilter
-  ): Promise<PlausibleStatsResponse[]> {
-    const domain =
-      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
-    try {
-      const response = await this.client.get<{
-        results: PlausibleAPIStatsTimeseriesResponse[]
-      }>(`/api/v1/stats/timeseries`, {
-        params: {
-          site_id: domain,
-          ...params
-        }
-      })
-
-      return response.data.results.map((result) => {
-        const accumulator: PlausibleStatsResponse = {
-          property: result.date
-        }
-        return reduce(
-          result,
-          (acc, value, key) => {
-            if (key !== 'date') acc[camelCase(key)] = value ?? 0
-            return acc
-          },
-          accumulator
-        )
-      })
-    } catch (error) {
-      const message = get(error, 'response.data.error')
-      if (typeof message === 'string')
-        throw new GraphQLError(message, {
-          extensions: { code: 'BAD_USER_INPUT' }
-        })
-      throw error
-    }
   }
 
   async createSite(
@@ -350,6 +219,145 @@ export class PlausibleService implements OnModuleInit {
       }
     })
     return data?.siteCreate
+  }
+
+  async getStatsRealtimeVisitors(
+    id: string,
+    type: 'journey' | 'team'
+  ): Promise<number> {
+    const domain =
+      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
+    const response = await this.plausibleClient.get<number>(
+      `/api/v1/stats/realtime/visitors`,
+      {
+        params: {
+          site_id: domain
+        }
+      }
+    )
+    return response.data
+  }
+
+  async getStatsAggregate(
+    id: string,
+    type: 'journey' | 'team',
+    params: {
+      metrics: string
+    } & PlausibleStatsAggregateFilter
+  ): Promise<PlausibleStatsAggregateResponse> {
+    const domain =
+      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
+    try {
+      const response = await this.plausibleClient.get<{
+        results: PlausibleAPIStatsAggregateResponse
+      }>(`/api/v1/stats/aggregate`, {
+        params: {
+          site_id: domain,
+          ...params
+        }
+      })
+      const accumulator: PlausibleStatsAggregateResponse = {}
+      return reduce(
+        response.data.results,
+        (acc, value, key) => {
+          if (value != null) acc[camelCase(key)] = value
+          return acc
+        },
+        accumulator
+      )
+    } catch (error) {
+      const message = get(error, 'response.data.error')
+      if (typeof message === 'string')
+        throw new GraphQLError(message, {
+          extensions: { code: 'BAD_USER_INPUT' }
+        })
+      throw error
+    }
+  }
+
+  async getStatsBreakdown(
+    id: string,
+    type: 'journey' | 'team',
+    params: {
+      metrics: string
+    } & PlausibleStatsBreakdownFilter
+  ): Promise<PlausibleStatsResponse[]> {
+    const domain =
+      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
+    try {
+      const response = await this.plausibleClient.get<{
+        results: PlausibleAPIStatsBreakdownResponse[]
+      }>(`/api/v1/stats/breakdown`, {
+        params: {
+          site_id: domain,
+          ...params
+        }
+      })
+
+      const propertyKey = last(params.property.split(':') ?? []) ?? ''
+      return response.data.results.map((result) => {
+        const accumulator: PlausibleStatsResponse = {
+          property: result[propertyKey] as string
+        }
+        return reduce(
+          result,
+          (acc, value, key) => {
+            if (key !== propertyKey) acc[camelCase(key)] = value
+            return acc
+          },
+          accumulator
+        )
+      })
+    } catch (error) {
+      const message = get(error, 'response.data.error')
+      if (typeof message === 'string')
+        throw new GraphQLError(message, {
+          extensions: { code: 'BAD_USER_INPUT' }
+        })
+      throw error
+    }
+  }
+
+  async getStatsTimeseries(
+    id: string,
+    type: 'journey' | 'team',
+    params: {
+      metrics: string
+    } & PlausibleStatsTimeseriesFilter
+  ): Promise<PlausibleStatsResponse[]> {
+    const domain =
+      type === 'journey' ? this.journeySiteId(id) : this.teamSiteId(id)
+    try {
+      const response = await this.plausibleClient.get<{
+        results: PlausibleAPIStatsTimeseriesResponse[]
+      }>(`/api/v1/stats/timeseries`, {
+        params: {
+          site_id: domain,
+          ...params
+        }
+      })
+
+      return response.data.results.map((result) => {
+        const accumulator: PlausibleStatsResponse = {
+          property: result.date
+        }
+        return reduce(
+          result,
+          (acc, value, key) => {
+            if (key !== 'date') acc[camelCase(key)] = value ?? 0
+            return acc
+          },
+          accumulator
+        )
+      })
+    } catch (error) {
+      const message = get(error, 'response.data.error')
+      if (typeof message === 'string')
+        throw new GraphQLError(message, {
+          extensions: { code: 'BAD_USER_INPUT' }
+        })
+      throw error
+    }
   }
 
   private journeySiteId(journeyId: string): string {
