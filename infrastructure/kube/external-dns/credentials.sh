be59ada7ede695fe2aaae6a7ea383bce177f707d
Mike Allison <dj.mikeallison@gmail.com>
2024-06-19 21:12:50 +0000

    feat: stage eks and postgres

--- infrastructure/kube/external-dns/credentials.sh
+++
@@ -1,9 +0,0 @@
-SECRET_ACCESS_KEY=$(aws iam create-access-key --user-name "externaldns")
-ACCESS_KEY_ID=$(echo $SECRET_ACCESS_KEY | jq -r '.AccessKey.AccessKeyId')
-
-cat <<-EOF > credentials
-
-[default]
-aws_access_key_id = $(echo $ACCESS_KEY_ID)
-aws_secret_access_key = $(echo $SECRET_ACCESS_KEY | jq -r '.AccessKey.SecretAccessKey')
-EOF
