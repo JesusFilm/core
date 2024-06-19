9ba23e82e392f1e229f9f6a7be088189ce00d38c
Dan <dan.chae@tandem.org.nz>
2024-06-19 23:27:29 +0000

    feat: use useuser hook in root new page

--- apps/journeys-admin/src/components/Team/TeamCreateDialog/TeamCreateDialog.spec.tsx
+++ apps/journeys-admin/src/components/Team/TeamCreateDialog/TeamCreateDialog.spec.tsx
@@ -22,11 +22,6 @@ jest.mock('@mui/material/useMediaQuery', () => ({
   default: jest.fn()
 }))
 
-jest.mock('next-firebase-auth', () => ({
-  __esModule: true,
-  useUser: jest.fn(() => ({ id: 'userId', name: 'userName' }))
-}))
-
 describe('TeamCreateDialog', () => {
   beforeEach(() => {
     ;(useMediaQuery as jest.Mock).mockImplementation(() => true)
