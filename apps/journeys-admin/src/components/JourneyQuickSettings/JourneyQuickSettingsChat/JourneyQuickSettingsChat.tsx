e1644fa40a467bc45c72b5fdde931cc9a8745db4
jianwei1 <jianweichong08@gmail.com>
2024-06-22 13:05:48 +0000

    feat: used hostitlefieldform props on journeyquicksettingschat

--- apps/journeys-admin/src/components/JourneyQuickSettings/JourneyQuickSettingsChat/JourneyQuickSettingsChat.tsx
+++ apps/journeys-admin/src/components/JourneyQuickSettings/JourneyQuickSettingsChat/JourneyQuickSettingsChat.tsx
@@ -1,14 +1,21 @@
 import Box from '@mui/material/Box'
+import { useUser } from 'next-firebase-auth'
+import { useTranslation } from 'next-i18next'
 import { ReactElement } from 'react'
-
 import { Chat } from '../../Editor/Slider/Settings/CanvasDetails/Footer/Chat'
 import { HostAvatarsButton } from '../../Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostForm/HostAvatarsButton'
 import { HostTitleFieldForm } from '../../Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostForm/HostTitleFieldForm'
 
 export function JourneyQuickSettingsChat(): ReactElement {
+  const { displayName } = useUser()
+  const { t } = useTranslation('apps-journeys-admin')
   return (
     <>
-      <HostTitleFieldForm />
+      <HostTitleFieldForm
+        defaultTitle={displayName ?? undefined}
+        label={t('Your Name')}
+        hostTitleRequiredErrorMessage={t('Please Enter Your Name')}
+      />
       <HostAvatarsButton />
       <Box sx={{ mx: -8 }}>
         <Chat />
