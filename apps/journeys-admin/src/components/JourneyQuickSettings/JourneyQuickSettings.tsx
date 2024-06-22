9ee55368584f68e71b766f807ea9122f8014a0c0
jianwei1 <jianweichong08@gmail.com>
2024-06-22 08:44:22 +0000

    feat: added sharedialog and share button

--- apps/journeys-admin/src/components/JourneyQuickSettings/JourneyQuickSettings.tsx
+++ apps/journeys-admin/src/components/JourneyQuickSettings/JourneyQuickSettings.tsx
@@ -1,15 +1,27 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import { ReactElement } from 'react'
+import { useTranslation } from 'next-i18next'
+import { ReactElement, useState } from 'react'
 
 import { Chat } from '../Editor/Slider/Settings/CanvasDetails/Footer/Chat'
 
 import { HostTitleFieldForm } from '../Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostForm/HostTitleFieldForm'
 import { PreviewItem } from '../Editor/Toolbar/Items/PreviewItem'
 
-import { ShareButton } from 'libs/journeys/ui/src/components/StepFooter/FooterButtonList/ShareButton'
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import { ShareDialog } from '@core/journeys/ui/StepFooter/FooterButtonList/ShareButton/ShareDialog'
+import ShareIcon from '@core/shared/ui/icons/Share'
 import { HostAvatarsButton } from '../Editor/Slider/Settings/CanvasDetails/Footer/HostTab/HostForm/HostAvatarsButton'
+import { Item } from '../Editor/Toolbar/Items/Item'
 
 export function JourneyQuickSettings(): ReactElement {
+  const { t } = useTranslation('apps-journeys-admin')
+  const { journey } = useJourney()
+  const [shareDialogOpen, setShareDialogOpen] = useState(false)
+
+  const url =
+    typeof window !== 'undefined' && journey?.slug != null
+      ? window.location.href
+      : undefined
   return (
     <Box sx={{ p: 4 }}>
       <Typography variant="h3"> Journeys Form Page</Typography>
@@ -26,7 +38,20 @@ export function JourneyQuickSettings(): ReactElement {
 
       <Stack sx={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <PreviewItem variant="button" />
-        <ShareButton />
+        <Item
+          variant="button"
+          label={t('Share')}
+          icon={<ShareIcon />}
+          onClick={() => setShareDialogOpen(true)}
+          ButtonProps={{
+            variant: 'contained'
+          }}
+        />
+        <ShareDialog
+          url={url}
+          open={shareDialogOpen}
+          closeDialog={() => setShareDialogOpen(false)}
+        />
       </Stack>
     </Box>
   )
