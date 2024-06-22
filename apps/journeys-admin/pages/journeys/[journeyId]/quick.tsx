0268cc67d22195e05d8f903f3a7b475516eeb447
Edmond Shen <egsmond@gmail.com>
2024-06-22 05:11:16 +0000

    feat: redirect if no journey or access

--- apps/journeys-admin/pages/journeys/[journeyId]/quick.tsx
+++ apps/journeys-admin/pages/journeys/[journeyId]/quick.tsx
@@ -1,7 +1,9 @@
 import { AuthAction, withUser, withUserTokenSSR } from 'next-firebase-auth'
 import { ReactElement } from 'react'
 
+import { GetAdminJourney } from '../../../__generated__/GetAdminJourney'
 import { initAndAuthApp } from '../../../src/libs/initAndAuthApp'
+import { GET_ADMIN_JOURNEY } from '../[journeyId]'
 
 function JourneyQuickPage(): ReactElement {
   return <>Quick</>
@@ -9,14 +11,52 @@ function JourneyQuickPage(): ReactElement {
 
 export const getServerSideProps = withUserTokenSSR({
   whenUnauthed: AuthAction.REDIRECT_TO_LOGIN
-})(async ({ user, locale, resolvedUrl }) => {
-  const { redirect } = await initAndAuthApp({
+})(async ({ user, locale, query, resolvedUrl }) => {
+  const { apolloClient, flags, redirect, translations } = await initAndAuthApp({
     user,
     locale,
     resolvedUrl
   })
 
   if (redirect != null) return { redirect }
+
+  try {
+    const { data } = await apolloClient.query<GetAdminJourney>({
+      query: GET_ADMIN_JOURNEY,
+      variables: {
+        id: query?.journeyId
+      }
+    })
+
+    if (data.journey?.template === true) {
+      return {
+        redirect: {
+          permanent: false,
+          destination: `/publisher/${data.journey?.id}`
+        }
+      }
+    }
+  } catch (error) {
+    if (error.message === 'journey not found') {
+      return {
+        redirect: {
+          permanent: false,
+          destination: `/`
+        }
+      }
+    }
+    if (error.message === 'user is not allowed to view journey') {
+      return {
+        props: {
+          status: 'noAccess',
+          ...translations,
+          flags,
+          initialApolloState: apolloClient.cache.extract()
+        }
+      }
+    }
+    throw error
+  }
 
   return {
     props: {}
