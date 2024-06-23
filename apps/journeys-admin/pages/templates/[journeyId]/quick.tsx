290306d051e21592a3cbba154b3073124046e435
scottkelso <scottkelso07@hotmail.com>
2024-06-23 22:40:06 +0000

    fix: imports from merge to main

--- apps/journeys-admin/pages/templates/[journeyId]/quick.tsx
+++ apps/journeys-admin/pages/templates/[journeyId]/quick.tsx
@@ -2,15 +2,14 @@ import { AuthAction, withUser, withUserTokenSSR } from 'next-firebase-auth'
 import { ReactElement, useEffect } from 'react'
 
 import { gql } from '@apollo/client'
+import { JOURNEY_DUPLICATE } from '@core/journeys/ui/useJourneyDuplicateMutation'
 import { useRouter } from 'next/router'
 import { GetTeams } from '../../../__generated__/GetTeams'
 import {
   JourneyDuplicate,
   JourneyDuplicateVariables
 } from '../../../__generated__/JourneyDuplicate'
-import { UserTeamRole } from '../../../__generated__/globalTypes'
 import { initAndAuthApp } from '../../../src/libs/initAndAuthApp'
-import { JOURNEY_DUPLICATE } from '../../../src/libs/useJourneyDuplicateMutation'
 
 export const GET_TEAMS = gql`
   query GetTeams {
