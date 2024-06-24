0ef663e5a089c7f3f002af8f8a1cfbc4f07ee0dd
Mike Allison <dj.mikeallison@gmail.com>
2024-06-24 20:54:48 +0000

    feat: plausible persistent storage

--- infrastructure/kube/storage/deploy-prod.sh
+++ infrastructure/kube/storage/deploy-prod.sh
@@ -10,4 +10,10 @@ eksctl create iamserviceaccount \
 
 eksctl create addon --name aws-ebs-csi-driver --cluster jfp-eks-prod --service-account-role-arn arn:aws:iam::410965620680:role/AmazonEKS_EBS_CSI_DriverRole_prod --force
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/master/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml
+
 kubectl apply -f deploy.yaml
