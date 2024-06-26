1cbd7683c2e84a918e7709b2714a442777591772
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-06-26 02:11:47 +0000

    fix: add deploy apps

--- tools/scripts/affected-apps.ts
+++ tools/scripts/affected-apps.ts
@@ -1,21 +1,29 @@
-import { exec } from 'child_process'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 
-const args = process.argv
-// remove args:
-// '/workspaces/core/node_modules/.bin/ts-node',
-// '/workspaces/core/tools/scripts/affected-apps.ts',
-args.splice(0, 2)
-exec(
-  `npx nx show projects --affected ${args.join(' ')}`,
-  (_error, stdout, _stderr) => {
-    const services = stdout
-      .split('\n')
-      .filter((value) => value != null && value !== '')
-      .join('","')
-    if (services === '') {
-      console.log('[]')
-    } else {
-      console.log(`["${services}"]`)
-    }
+const execAsync = promisify(exec)
+
+async function main() {
+  const args = process.argv
+  // remove args:
+  // '/workspaces/core/node_modules/.bin/ts-node',
+  // '/workspaces/core/tools/scripts/affected-apps.ts',
+  args.splice(0, 2)
+
+  const { stdout: affected } = await execAsync(
+    `npx nx show projects --affected ${args.join(' ')}`
+  )
+
+  const services = affected
+    .split('\n')
+    .filter((value) => value != null && value !== '')
+    .join('","')
+
+  if (services === '') {
+    console.log('[]')
+  } else {
+    console.log(`["${services}"]`)
   }
-)
+}
+
+void main()
