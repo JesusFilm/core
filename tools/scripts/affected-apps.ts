a222786f30d6f5760750ee98afa5a2bd725edf00
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-06-26 02:08:49 +0000

    test: check running just e2es

--- tools/scripts/affected-apps.ts
+++ tools/scripts/affected-apps.ts
@@ -1,21 +1,28 @@
 import { exec } from 'child_process'
 
 const args = process.argv
-// remove args:
-// '/workspaces/core/node_modules/.bin/ts-node',
-// '/workspaces/core/tools/scripts/affected-apps.ts',
 args.splice(0, 2)
 exec(
   `npx nx show projects --affected ${args.join(' ')}`,
   (_error, stdout, _stderr) => {
-    const services = stdout
+    let services = stdout
       .split('\n')
       .filter((value) => value != null && value !== '')
-      .join('","')
-    if (services === '') {
+
+    // Filter out "-e2e" projects if their counterpart is also in the list
+    services = services.filter((service) => {
+      if (service.endsWith('-e2e')) {
+        const baseService = service.replace('-e2e', '')
+        return !services.includes(baseService)
+      }
+      return true
+    })
+
+    const output = services.join('","')
+    if (output === '') {
       console.log('[]')
     } else {
-      console.log(`["${services}"]`)
+      console.log(`["${output}"]`)
     }
   }
 )
