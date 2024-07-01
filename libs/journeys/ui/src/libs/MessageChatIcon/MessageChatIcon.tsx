94fe7b5fd505c33ac50c27ad0ba92520c4ba8091
Edmond Shen <egsmond@gmail.com>
2024-07-01 04:21:59 +0000

    feat: add new icons to message chat icon

--- libs/journeys/ui/src/libs/MessageChatIcon/MessageChatIcon.tsx
+++ libs/journeys/ui/src/libs/MessageChatIcon/MessageChatIcon.tsx
@@ -1,7 +1,29 @@
+import CheckBroken from '@core/shared/ui/icons/CheckBroken'
+import CheckContained from '@core/shared/ui/icons/CheckContained'
 import Facebook from '@core/shared/ui/icons/Facebook'
+import Globe2 from '@core/shared/ui/icons/Globe2'
+import Globe3 from '@core/shared/ui/icons/Globe3'
+import HelpCircle from '@core/shared/ui/icons/HelpCircleContained'
+import HelpSquare from '@core/shared/ui/icons/HelpSquareContained'
+import Home3 from '@core/shared/ui/icons/Home3'
+import Home4 from '@core/shared/ui/icons/Home4'
 import Instagram from '@core/shared/ui/icons/Instagram'
 import Line from '@core/shared/ui/icons/Line'
+import LinkExternal from '@core/shared/ui/icons/LinkExternal'
+import Mail1 from '@core/shared/ui/icons/Mail1'
+import Menu from '@core/shared/ui/icons/Menu1'
+import MessageChat2 from '@core/shared/ui/icons/MessageChat2'
+import MessageCircle from '@core/shared/ui/icons/MessageCircle'
+import MessageNotifyCircle from '@core/shared/ui/icons/MessageNotifyCircle'
+import MessageNotifySquare from '@core/shared/ui/icons/MessageNotifySquare'
+import MessageSquare from '@core/shared/ui/icons/MessageSquare'
+import MessageText1 from '@core/shared/ui/icons/MessageText1'
+import MessageText2 from '@core/shared/ui/icons/MessageText2'
 import MessageTyping from '@core/shared/ui/icons/MessageTyping'
+import Send1 from '@core/shared/ui/icons/Send1'
+import Send2 from '@core/shared/ui/icons/Send2'
+import Settings from '@core/shared/ui/icons/Settings'
+import ShieldCheck from '@core/shared/ui/icons/ShieldCheck'
 import Skype from '@core/shared/ui/icons/Skype'
 import Snapchat from '@core/shared/ui/icons/Snapchat'
 import Telegram from '@core/shared/ui/icons/Telegram'
@@ -31,7 +53,29 @@ export function MessageChatIcon({ platform, sx }: ChatIconProps): ReactElement {
     skype: Skype,
     line: Line,
     tikTok: Tiktok,
-    custom: MessageTyping
+    custom: MessageTyping,
+    globe2: Globe2,
+    globe3: Globe3,
+    messageText2: MessageText2,
+    messageText1: MessageText1,
+    send1: Send1,
+    send2: Send2,
+    messageChat2: MessageChat2,
+    messageCircle: MessageCircle,
+    messageNotifyCircle: MessageNotifyCircle,
+    messageNotifySquare: MessageNotifySquare,
+    messageSquare: MessageSquare,
+    mail1: Mail1,
+    linkExternal: LinkExternal,
+    home4: Home4,
+    home3: Home3,
+    helpCircle: HelpCircle,
+    helpSquare: HelpSquare,
+    shieldCheck: ShieldCheck,
+    menu: Menu,
+    checkBroken: CheckBroken,
+    checkContained: CheckContained,
+    settings: Settings
   }
 
   const IconComponent = platformComponents[platform]
