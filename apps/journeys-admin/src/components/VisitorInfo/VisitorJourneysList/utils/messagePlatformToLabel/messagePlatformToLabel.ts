32e8f9111c9f3ff65ff38523f42a5ce63d3fbc5f
Edmond Shen <egsmond@gmail.com>
2024-07-01 04:41:17 +0000

    feat: update message platform to label function

--- apps/journeys-admin/src/components/VisitorInfo/VisitorJourneysList/utils/messagePlatformToLabel/messagePlatformToLabel.ts
+++ apps/journeys-admin/src/components/VisitorInfo/VisitorJourneysList/utils/messagePlatformToLabel/messagePlatformToLabel.ts
@@ -29,5 +29,49 @@ export function messagePlatformToLabel(
       return t('WhatsApp')
     case MessagePlatform.custom:
       return t('Custom')
+    case MessagePlatform.globe2:
+      return t('Globe 2')
+    case MessagePlatform.globe3:
+      return t('Globe 3')
+    case MessagePlatform.messageText1:
+      return t('Message Text 1')
+    case MessagePlatform.messageText2:
+      return t('Message Text 2')
+    case MessagePlatform.send1:
+      return t('Send 1')
+    case MessagePlatform.send2:
+      return t('Send 2')
+    case MessagePlatform.messageChat2:
+      return t('Message Chat 2')
+    case MessagePlatform.messageCircle:
+      return t('Message Circle')
+    case MessagePlatform.messageNotifyCircle:
+      return t('Message Notify Circle')
+    case MessagePlatform.messageNotifySquare:
+      return t('Message Notify Square')
+    case MessagePlatform.messageSquare:
+      return t('Message Sqaure')
+    case MessagePlatform.mail1:
+      return t('Mail 1')
+    case MessagePlatform.linkExternal:
+      return t('Link External')
+    case MessagePlatform.home3:
+      return t('Home 3')
+    case MessagePlatform.home4:
+      return t('Home 4')
+    case MessagePlatform.helpCircle:
+      return t('Help Circle')
+    case MessagePlatform.helpSquare:
+      return t('Help Square')
+    case MessagePlatform.shieldCheck:
+      return t('Shield Check')
+    case MessagePlatform.menu:
+      return t('Menu')
+    case MessagePlatform.checkBroken:
+      return t('Check Broken')
+    case MessagePlatform.checkContained:
+      return t('Check Contained')
+    case MessagePlatform.settings:
+      return t('Settings')
   }
 }
