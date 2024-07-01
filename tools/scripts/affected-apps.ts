0c282fdbb70c70a33745ca7803438d536d22645c
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-07-01 09:21:06 +0000

    test: revert app

--- tools/scripts/affected-apps.ts
+++ tools/scripts/affected-apps.ts
@@ -1,25 +1,21 @@
 import { exec } from 'child_process'
 
 const args = process.argv
+// remove args:
+// '/workspaces/core/node_modules/.bin/ts-node',
+// '/workspaces/core/tools/scripts/affected-apps.ts',
 args.splice(0, 2)
 exec(
   `npx nx show projects --affected ${args.join(' ')}`,
   (_error, stdout, _stderr) => {
-    let services = stdout
+    const services = stdout
       .split('\n')
       .filter((value) => value != null && value !== '')
-
-    let appsProjects = services.filter(service => !service.endsWith('-e2e'))
-    const e2eProjects = services.filter(service => service.endsWith('-e2e'))
-
-    // If each e2e projects (after removing the word '-e2e') is not already included in apps project, add it
-    appsProjects = appsProjects.concat(e2eProjects.map(each => each.replace('-e2e', ''))).filter((value, index, self) => self.indexOf(value) === index)
-
-    const output = appsProjects.join('","')
-    if (output === '') {
+      .join('","')
+    if (services === '') {
       console.log('[]')
     } else {
-      console.log(`["${output}"]`)
+      console.log(`["${services}"]`)
     }
   }
 )
