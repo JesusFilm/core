8180e7b0992236d2d18091e3636e4d5fa3461aa7
jianwei1 <jianweichong08@gmail.com>
2024-07-03 02:02:47 +0000

    test: added imageblock test

--- apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/nodes/StepBlockNode/libs/getCardHeadings/getCardHeadings.spec.ts
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/nodes/StepBlockNode/libs/getCardHeadings/getCardHeadings.spec.ts
@@ -2,11 +2,26 @@ import { TreeBlock } from '@core/journeys/ui/block/TreeBlock'
 
 import {
   BlockFields,
+  BlockFields_CardBlock as CardBlock,
+  BlockFields_ImageBlock as ImageBlock,
   BlockFields_TypographyBlock as TypographyBlock
 } from '../../../../../../../../../__generated__/BlockFields'
 import { TypographyVariant } from '../../../../../../../../../__generated__/globalTypes'
 
 import { getCardHeadings } from '.'
+
+const card: TreeBlock<CardBlock> = {
+  id: 'cardId',
+  __typename: 'CardBlock',
+  parentBlockId: 'step1.id',
+  parentOrder: 0,
+  coverBlockId: null,
+  backgroundColor: null,
+  themeMode: null,
+  themeName: null,
+  fullscreen: false,
+  children: []
+}
 
 const typography1: TreeBlock<TypographyBlock> = {
   __typename: 'TypographyBlock',
@@ -32,24 +47,45 @@ const typography2: TreeBlock<TypographyBlock> = {
   children: []
 }
 
+const image: TreeBlock<ImageBlock> = {
+  __typename: 'ImageBlock',
+  id: 'image0.id',
+  src: 'https://url.com',
+  width: 1600,
+  height: 1067,
+  alt: 'random image from unsplash',
+  parentBlockId: 'Image1',
+  parentOrder: 0,
+  blurhash: 'L9AS}j^-0dVC4Tq[=~PATeXSV?aL',
+  children: []
+}
+
 const children: Array<TreeBlock<BlockFields>> = [typography1]
 
 describe('getCardHeading', () => {
   it('should return title content from children', () => {
-    const cardHeadings = getCardHeadings(children)
+    const cardHeadings = getCardHeadings({ ...card, children })
     expect(cardHeadings).toEqual(['title'])
   })
 
   it('should return both title and subtitle content according to typography order', () => {
-    const cardHeadings = getCardHeadings([...children, typography2])
+    const cardHeadings = getCardHeadings({
+      ...card,
+      children: [...children, typography2]
+    })
     expect(cardHeadings).toEqual(['subtitle content', 'title'])
   })
 
   it('should return typography without a variant', () => {
-    const cardHeadings = getCardHeadings([
-      ...children,
-      { ...typography2, variant: null }
-    ])
+    const cardHeadings = getCardHeadings({
+      ...card,
+      children: [...children, { ...typography2, variant: null }]
+    })
     expect(cardHeadings).toEqual(['title', 'subtitle content'])
+  })
+
+  it('should return image details for imageblock', () => {
+    const cardHeadings = getCardHeadings({ ...card, children: [image] })
+    expect(cardHeadings).toEqual(['Image', '1600 x 1067 pixels'])
   })
 })
