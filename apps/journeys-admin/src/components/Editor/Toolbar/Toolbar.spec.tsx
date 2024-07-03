d3a9e7a4848acd13d525cafcf368dd9bfb99ab94
Zachary Annabell <77838223+zwsaqx@users.noreply.github.com>
2024-07-03 02:51:48 +0000

    refactor: test

--- apps/journeys-admin/src/components/Editor/Toolbar/Toolbar.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/Toolbar.spec.tsx
@@ -19,36 +19,60 @@ jest.mock('@mui/material/useMediaQuery', () => ({
 }))
 
 describe('Toolbar', () => {
+  const defaultJourney = {
+    title: 'My Awesome Journey Title',
+    description: 'My Awesome Journey Description',
+    primaryImageBlock: null,
+    status: JourneyStatus.draft
+  } as unknown as Journey
+
+  const socialImageJourney = {
+    title: 'My Awesome Journey Title',
+    description: 'My Awesome Journey Description',
+    primaryImageBlock: {
+      id: 'image1.id',
+      __typeame: 'ImageBlock',
+      parentBlockId: null,
+      parentOrder: 0,
+      src: 'https://images.unsplash.com/photo-1508363778367-af363f107cbb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=chester-wade-hLP7lVm4KUE-unsplash.jpg&w=1920',
+      alt: 'random image from unsplash',
+      width: 1920,
+      height: 1080,
+      blurhash: 'L9AS}j^-0dVC4Tq[=~PATeXSV?aL'
+    },
+    status: JourneyStatus.draft
+  } as unknown as Journey
+
   it('should render NextSteps logo on Toolbar', () => {
-    const { getByAltText } = render(toolbar())
+    const { getByAltText } = render(toolbar(defaultJourney))
     expect(getByAltText('Next Steps')).toBeInTheDocument() // NextSteps logo
   })
 
   it('should render title & description on Toolbar', () => {
-    const { getByText } = render(toolbar())
+    const { getByText } = render(toolbar(defaultJourney))
     expect(getByText('My Awesome Journey Title')).toBeInTheDocument()
     expect(getByText('My Awesome Journey Description')).toBeInTheDocument()
   })
 
   it('should render items stack on Toolbar', () => {
-    const { getByTestId } = render(toolbar())
+    const { getByTestId } = render(toolbar(defaultJourney))
     expect(getByTestId('ItemsStack')).toBeInTheDocument()
   })
 
   it('should render menu button on Toolbar', () => {
-    const { getByTestId } = render(toolbar())
+    const { getByTestId } = render(toolbar(defaultJourney))
     expect(getByTestId('ToolbarMenuButton')).toBeInTheDocument()
     expect(getByTestId('MoreIcon')).toBeInTheDocument()
   })
 
   it('should render all journeys button', () => {
-    const { getByTestId } = render(toolbar())
+    const { getByTestId } = render(toolbar(defaultJourney))
     expect(getByTestId('ToolbarBackButton')).toHaveAttribute('href', '/')
     expect(getByTestId('FormatListBulletedIcon')).toBeInTheDocument()
   })
 
   it('should render journeys tooltip on hover', async () => {
-    const { getByTestId, getByText } = render(toolbar())
+    const { getByTestId, getByText } = render(toolbar(defaultJourney))
     fireEvent.mouseOver(getByTestId('ToolbarBackButton'))
     await waitFor(() => {
       expect(getByText('See all journeys')).toBeInTheDocument()
@@ -56,50 +80,30 @@ describe('Toolbar', () => {
   })
 
   it('should render journey image', () => {
-    const { getByAltText, queryByTestId } = render(
-      <SnackbarProvider>
-        <MockedProvider>
-          <JourneyProvider
-            value={{
-              journey: {
-                title: 'My Awesome Journey Title',
-                description: 'My Awesome Journey Description',
-                primaryImageBlock: {
-                  id: 'image1.id',
-                  __typename: 'ImageBlock',
-                  parentBlockId: null,
-                  parentOrder: 0,
-                  src: 'https://images.unsplash.com/photo-1508363778367-af363f107cbb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=chester-wade-hLP7lVm4KUE-unsplash.jpg&w=1920',
-                  alt: 'random image from unsplash',
-                  width: 1920,
-                  height: 1080,
-                  blurhash: 'L9AS}j^-0dVC4Tq[=~PATeXSV?aL'
-                },
-                status: JourneyStatus.draft
-              } as unknown as Journey,
-              variant: 'admin'
-            }}
-          >
-            <Toolbar />
-          </JourneyProvider>
-        </MockedProvider>
-      </SnackbarProvider>
-    )
+    const { getByAltText, queryByTestId } = render(toolbar(socialImageJourney))
 
     expect(getByAltText('random image from unsplash')).toBeInTheDocument()
     expect(queryByTestId('ThumbsUpIcon')).not.toBeInTheDocument()
   })
 
   it('should open the tooltip when the image is hovered over', async () => {
-    const { getByTestId, getByText } = render(toolbar())
+    const { getByTestId, getByText } = render(toolbar(defaultJourney))
     fireEvent.mouseOver(getByTestId('ToolbarSocialImage'))
     await waitFor(() => {
       expect(getByText('Social Image')).toBeInTheDocument()
     })
   })
 
-  it('should open the social preview when the image is clicked', async () => {
-    const { getByText, getByTestId } = render(
+  it('should open the social preview when the image is clicked', () => {
+    const { getByText, getByTestId } = render(toolbar(socialImageJourney))
+
+    expect(getByText('activeSlide: 0')).toBeInTheDocument()
+    fireEvent.click(getByTestId('ToolbarSocialImage'))
+    expect(getByText('activeSlide: 1')).toBeInTheDocument()
+  })
+
+  function toolbar(defaultJourney): ReactElement {
+    return (
       <MockedProvider>
         <SnackbarProvider>
           <EditorProvider>
@@ -131,32 +135,6 @@ describe('Toolbar', () => {
           </EditorProvider>
         </SnackbarProvider>
       </MockedProvider>
-    )
-
-    expect(getByText('activeSlide: 0')).toBeInTheDocument()
-    fireEvent.click(getByTestId('ToolbarSocialImage'))
-    expect(getByText('activeSlide: 1')).toBeInTheDocument()
-  })
-
-  function toolbar(): ReactElement {
-    return (
-      <SnackbarProvider>
-        <MockedProvider>
-          <JourneyProvider
-            value={{
-              journey: {
-                title: 'My Awesome Journey Title',
-                description: 'My Awesome Journey Description',
-                primaryImageBlock: null,
-                status: JourneyStatus.draft
-              } as unknown as Journey,
-              variant: 'admin'
-            }}
-          >
-            <Toolbar />
-          </JourneyProvider>
-        </MockedProvider>
-      </SnackbarProvider>
     )
   }
 })
