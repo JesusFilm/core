5ba3b2a52af9f6582fc0d26e5b762a4dcec09085
Nahom Bekele <nahom.fix@gmail.com>
2024-07-04 08:38:05 +0000

    chore: revert e2e spec file

--- apps/nexus-admin-e2e/src/e2e/nexus-admin.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('has title on login page', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.getByText('Welcome to Nexus')).toHaveText(
-    'Welcome to Nexus'
-  )
-})
