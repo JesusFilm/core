41ea5aa1decfff53bf66b3d7d6949f48bcc06916
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-07-05 03:06:03 +0000

    test: monitoring

--- apps/journeys-admin-e2e/src/monitoring/journeys-admin.monitor.ts
+++ apps/journeys-admin-e2e/src/monitoring/journeys-admin.monitor.ts
@@ -16,12 +16,20 @@ test('NS Admin Monitoring: Check user can login and logout successfuly', async (
   const password = process.env.PLAYWRIGHT_PASSWORD as string
 
   await page.goto('https://admin.nextstep.is/')
-  await page.getByPlaceholder('Enter your email address here').click()
+
+  // Wait for all network requests to finish and page to load fully
+  await page.waitForLoadState('networkidle')
+
   await page.getByPlaceholder('Enter your email address here').fill(email)
-  await page.getByRole('button', { name: 'Continue with email' }).click()
-  await page.getByPlaceholder('Enter Password').click()
+  await expect(
+    page.getByPlaceholder('Enter your email address here').textContent()
+  ).toContain(email)
+
+  await page.locator('button[type="submit"]').click()
+
   await page.getByPlaceholder('Enter Password').fill(password)
   await page.getByRole('button', { name: 'Sign In' }).click()
+
   await page.getByTestId('NavigationListItemProfile').click()
   await expect(page.getByText(email)).toBeVisible()
   await page.getByText('Logout').click()
