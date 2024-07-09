6259893bccadabd338c3d05fc76ba5e7886436d1
jianwei1 <jianweichong08@gmail.com>
2024-07-09 00:03:12 +0000

    chore: added comments

--- apps/watch/src/components/VideosPage/utils/useJourneySearch/useJourneySearch.ts
+++ apps/watch/src/components/VideosPage/utils/useJourneySearch/useJourneySearch.ts
@@ -3,3 +3,16 @@
 export function useJourneySearch(): string {
   return 'dummy function'
 }
+
+/* 
+ it should return data from algolia when it gets passed a title
+ it should return data from algolia when it gets passed a languageId or languagesIds
+ it should return data from algolia when it gets passed a category or categories
+
+
+ Potential steps:
+Need to mimic current useVideoSearch:
+ - some version of getQueryParameters ? 
+ - some version of checkfilterapplied? 
+ - some version of transformAlgoliaVideos? 
+*/
