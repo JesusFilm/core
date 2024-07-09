97e4f5ab2f5131af3e333a3b1edd7c1b0b7e7222
scottkelso <scottkelso07@hotmail.com>
2024-07-09 04:13:47 +0000

    feat: add coloured border

--- apps/watch/src/components/SearchBar/SearchBar.tsx
+++ apps/watch/src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,22 @@
 import Search1Icon from '@core/shared/ui/icons/Search1'
-import { InputAdornment, TextField } from '@mui/material'
+import { InputAdornment, TextField, styled } from '@mui/material'
 import { ReactElement } from 'react'
 
 export function SearchBar(): ReactElement {
+  const CssTextField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset, fieldset': {
+        border: '4px solid',
+        borderImageSource:
+          'linear-gradient(90deg, #0C79B3 0%, #0FDABC 51%, #E72DBB 100%)',
+        borderImageSlice: 1,
+        borderRadius: '12px' // not working!
+      }
+    }
+  })
+
   return (
-    <TextField
+    <CssTextField
       placeholder="Search by topic, occasion, or audience ..."
       InputProps={{
         startAdornment: (
