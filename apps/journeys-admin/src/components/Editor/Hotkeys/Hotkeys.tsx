eb3732a87febd339de65553a9a1ae6df5d0d7717
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-07-12 09:19:27 +0000

    fix: undo inside iframe

--- apps/journeys-admin/src/components/Editor/Hotkeys/Hotkeys.tsx
+++ apps/journeys-admin/src/components/Editor/Hotkeys/Hotkeys.tsx
@@ -2,10 +2,14 @@ import { ReactElement } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import { useCommand } from '../../../../../../libs/journeys/ui/src/libs/CommandProvider'
 
-export function Hotkeys(): ReactElement {
+interface HotkeysProps {
+  document?: Document
+}
+
+export function Hotkeys({ document }: HotkeysProps): ReactElement {
   const { undo, redo } = useCommand()
-  useHotkeys('mod+z', undo, { preventDefault: true })
-  useHotkeys('mod+shift+z', redo, { preventDefault: true })
+  useHotkeys('mod+z', undo, { preventDefault: true, document })
+  useHotkeys('mod+shift+z', redo, { preventDefault: true, document })
 
   return <></>
 }
--- libs/journeys/ui/src/components/FramePortal/FramePortal.tsx
+++ libs/journeys/ui/src/components/FramePortal/FramePortal.tsx
@@ -1,33 +1,28 @@
 import createCache from '@emotion/cache'
 import { CacheProvider } from '@emotion/react'
+import Box from '@mui/material/Box'
 import { styled } from '@mui/material/styles'
+import isFunction from 'lodash/isFunction'
 import {
   ComponentProps,
   ReactElement,
   ReactNode,
-  forwardRef,
-  memo,
   useEffect,
-  useImperativeHandle,
-  useMemo,
-  useReducer,
-  useRef
+  useMemo
 } from 'react'
-import { createPortal } from 'react-dom'
+import Frame, { FrameContextConsumer } from 'react-frame-component'
 import { prefixer } from 'stylis'
 import rtlPlugin from 'stylis-plugin-rtl'
 
 interface ContentProps {
   children: ReactNode
+  dir: string | undefined
   document: Document
 }
 
-function Content({ children, document }: ContentProps): ReactElement {
+function Cache({ children, dir, document }: ContentProps): ReactElement {
   const cache = useMemo(() => {
-    document.head.innerHTML =
-      window.document.head.innerHTML +
-      '<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;800&family=Open+Sans&family=El+Messiri:wght@400;600;700&display=swap" rel="stylesheet" />, '
-
+    document.head.innerHTML = `<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;800&family=Open+Sans&family=El+Messiri:wght@400;600;700&display=swap" rel="stylesheet" />`
     return createCache({
       key: 'iframe',
       container: document.head,
@@ -37,62 +32,47 @@ function Content({ children, document }: ContentProps): ReactElement {
   }, [document])
 
   useEffect(() => {
+    document.dir = dir ?? 'ltr'
     document.body.style.backgroundColor = 'transparent'
-  }, [document])
+  }, [document, dir])
 
-  return <CacheProvider value={cache}>{children}</CacheProvider>
+  return (
+    <CacheProvider value={cache}>
+      <Box sx={{ position: 'fixed', top: 0, right: 0, bottom: 0, left: 0 }}>
+        {children}
+      </Box>
+    </CacheProvider>
+  )
 }
 
-const StyledFrame = styled('iframe')(() => ({
+const StyledFrame = styled(Frame)(() => ({
   border: 0
 }))
 
-interface FrameProps extends ComponentProps<typeof StyledFrame> {
-  children: ReactNode
+interface FrameProps
+  extends Omit<ComponentProps<typeof StyledFrame>, 'children'> {
+  children:
+    | ((props: { window: Window; document: Document }) => ReactNode)
+    | ReactNode
 }
 
-export const FramePortal = memo(
-  forwardRef<HTMLIFrameElement, FrameProps>(function FramePortal(
-    { children, dir, ...rest },
-    ref
-  ): ReactElement {
-    const frameRef = useRef<HTMLIFrameElement>(null)
-    useImperativeHandle(ref, () => frameRef.current as HTMLIFrameElement, [])
-    // If we portal content into the iframe before the load event then that
-    // content is dropped in firefox.
-    const [iframeLoaded, dispatch] = useReducer(() => true, false)
-    const document = frameRef.current?.contentDocument
-    if (document != null) {
-      document.dir = dir ?? ''
-    }
-
-    useEffect(() => {
-      // When we hydrate the iframe then the load event is already dispatched
-      // once the iframe markup is parsed (maybe later but the important part is
-      // that it happens before React can attach event listeners).
-      // We need to check the readyState of the document once the iframe is
-      // mounted and "replay" the missed load event.
-      // See https://github.com/facebook/react/pull/13862 for ongoing effort in
-      // React (though not with iframes in mind).
-      if (
-        frameRef?.current?.contentDocument != null &&
-        frameRef.current.contentDocument.readyState === 'complete' &&
-        !iframeLoaded
-      ) {
-        dispatch()
-      }
-    }, [iframeLoaded])
-
-    return (
-      <>
-        <StyledFrame onLoad={dispatch} ref={frameRef} {...rest} />
-        {iframeLoaded &&
-          document != null &&
-          createPortal(
-            <Content document={document}>{children}</Content>,
-            document.body
-          )}
-      </>
-    )
-  })
-)
+export function FramePortal({
+  children,
+  dir,
+  ...rest
+}: FrameProps): ReactElement {
+  return (
+    <StyledFrame {...rest}>
+      <FrameContextConsumer>
+        {({ window, document }) =>
+          window != null &&
+          document != null && (
+            <Cache dir={dir} document={document}>
+              {isFunction(children) ? children({ window, document }) : children}
+            </Cache>
+          )
+        }
+      </FrameContextConsumer>
+    </StyledFrame>
+  )
+}
