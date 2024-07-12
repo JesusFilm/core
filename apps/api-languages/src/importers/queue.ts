3b41c7981554fde6e07b9c0d1d49565970a80725
Mike Allison <dj.mikeallison@gmail.com>
2024-07-12 22:23:01 +0000

    feat: languages audio preview import

--- apps/api-languages/src/importers/queue.ts
+++ apps/api-languages/src/importers/queue.ts
@@ -1,8 +1,10 @@
 import { Queue } from 'bullmq'
 
-new Queue('api-languages-importer', {
-  connection: {
-    host: process.env.REDIS_URL ?? 'redis',
-    port: 6379
-  }
+export const bullConnection = {
+  host: process.env.REDIS_URL ?? 'redis',
+  port: 6379
+}
+
+export const importLanguagesQueue = new Queue('api-languages-importer', {
+  connection: bullConnection
 })
--- apps/api-languages/src/importers/worker.ts
+++ apps/api-languages/src/importers/worker.ts
@@ -2,10 +2,17 @@ import { Job, Worker } from 'bullmq'
 
 import { importAudioPreview } from './audioPreviews/audioPreviews'
 import { getExistingPrismaLanguageIds } from './languages/languages'
+import { bullConnection, importLanguagesQueue } from './queue'
 
-new Worker('api-languages-importer', async (job: Job) => {
-  if (job.name !== 'import-languages') return
+new Worker(
+  'api-languages-importer',
+  async (job: Job) => {
+    if (job.name !== 'import-languages') return
 
-  const languageIds = await getExistingPrismaLanguageIds()
-  await importAudioPreview(languageIds)
-})
+    const languageIds = await getExistingPrismaLanguageIds()
+    await importAudioPreview(languageIds)
+  },
+  {
+    connection: bullConnection
+  }
+)
