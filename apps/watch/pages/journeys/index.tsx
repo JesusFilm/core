73e1752d9063db2e8aaab3a9cef0c9f779624774
scottkelso <scottkelso07@hotmail.com>
2024-07-15 05:13:33 +0000

    feat: remove popper changes

--- apps/watch/pages/journeys/index.tsx
+++ apps/watch/pages/journeys/index.tsx
@@ -17,33 +17,24 @@ import { SearchBar } from '../../src/components/SearchBar/SearchBar'
 import Stack from '@mui/material/Stack'
 import { Configure, Highlight, Hits, Index } from 'react-instantsearch'
 
-function Hit({ hit }) {
-  return (
-    <article>
-      <img src={hit.image.src} alt={hit.image.alt} width={50} />
-      <h1>
-        <Highlight attribute="title" hit={hit} />
-      </h1>
-      <p>{hit.date}</p>
-      <p>{hit.language}</p>
-      <p>{hit.tags.toString()}</p>
-      <p>{hit.objectID}</p>
-    </article>
-  )
-}
-
-function StratHit({ hit }) {
-  return (
-    <article>
-      <h1>
-        <Highlight attribute="post_title" hit={hit} />
-      </h1>
-      <p>{hit.objectID}</p>
-      <p>{hit.content}</p>
-      <p>{hit.post_type_label}</p>
-      <p>{hit.post_date_formatted}</p>
-    </article>
-  )
+// TODO(jk): see useVideoSearch interfaces
+type AlgoliaJourney = {
+  objectID: string
+  title: string
+  description: string
+  tags: {
+    Topics: string[]
+    Audience: string[]
+    Holidays: string[]
+    Collections: string[]
+  }
+  image: {
+    src: string
+    alt: string
+  }
+  language: string
+  date: Date
+  featuredAt?: string
 }
 
 function JourneysPage(): ReactElement {
@@ -55,23 +46,8 @@ function JourneysPage(): ReactElement {
       >
         <Container maxWidth="xxl">
           <Stack gap={10}>
-            {/* There needs to be a default index for all algolia widgets
-            This is not set at the _app level because we want to default
-            to different indexes on each page of the app */}
             <Index indexName="api-journeys-journeys-dev">
-              {/* Searchbar should also search below indexes */}
-              <SearchBar />
-
-              <Index indexName="api-journeys-journeys-dev">
-                <Configure hitsPerPage={10} />
-                <Hits hitComponent={Hit} />
-              </Index>
-
-              <Index indexName="wp_dev_posts_mission-trip">
-                <Configure hitsPerPage={10} />
-                <Hits hitComponent={StratHit} />
-              </Index>
-
+              {/* <SearchBar /> */}
               <ThemeProvider
                 themeName={ThemeName.journeysAdmin}
                 themeMode={ThemeMode.light}
