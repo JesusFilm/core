39553aecd2abf912000f669d16b7d0c982580efc
Edmond Shen <egsmond@gmail.com>
2024-07-16 04:48:14 +0000

    feat: undo hint

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/TextResponse/TextResponseFields/Hint/Hint.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/TextResponse/TextResponseFields/Hint/Hint.tsx
@@ -10,7 +10,11 @@ import { useEditor } from '@core/journeys/ui/EditorProvider'
 import { useJourney } from '@core/journeys/ui/JourneyProvider'
 import type { TreeBlock } from '@core/journeys/ui/block'
 
-import { BlockFields_TextResponseBlock as TextResponseBlock } from '../../../../../../../../../../../__generated__/BlockFields'
+import { useCommand } from '@core/journeys/ui/CommandProvider'
+import {
+  BlockFields_StepBlock,
+  BlockFields_TextResponseBlock as TextResponseBlock
+} from '../../../../../../../../../../../__generated__/BlockFields'
 import { TextResponseHintUpdate } from '../../../../../../../../../../../__generated__/TextResponseHintUpdate'
 
 export const TEXT_RESPONSE_HINT_UPDATE = gql`
@@ -32,30 +36,82 @@ export function Hint(): ReactElement {
     TEXT_RESPONSE_HINT_UPDATE
   )
   const { journey } = useJourney()
-  const { state } = useEditor()
+  const { state, dispatch } = useEditor()
+  const { add } = useCommand()
   const selectedBlock = state.selectedBlock as
     | TreeBlock<TextResponseBlock>
     | undefined
 
-  async function handleSubmit(e: FocusEvent): Promise<void> {
-    if (journey == null || selectedBlock == null) return
-    const target = e.target as HTMLInputElement
-    await textResponseHintUpdate({
-      variables: {
-        id: selectedBlock?.id,
-        journeyId: journey.id,
-        input: {
-          hint: target.value
-        }
-      },
-      optimisticResponse: {
-        textResponseBlockUpdate: {
-          id: selectedBlock?.id,
-          __typename: 'TextResponseBlock',
-          hint: target.value
-        }
-      }
+  function updateEditorState(
+    step: TreeBlock<BlockFields_StepBlock> | undefined,
+    block: TreeBlock
+  ): void {
+    dispatch({
+      type: 'SetSelectedStepAction',
+      selectedStep: step
     })
+
+    dispatch({
+      type: 'SetSelectedBlockAction',
+      selectedBlock: block
+    })
+  }
+
+  async function handleSubmit(e: FocusEvent): Promise<void> {
+    if (selectedBlock != null && journey != null) {
+      const target = e.target as HTMLInputElement
+      const hint = target.value
+
+      await add({
+        parameters: {
+          execute: { id: selectedBlock.id, journeyId: journey.id, hint },
+          undo: {
+            id: selectedBlock.id,
+            journeyId: journey.id,
+            hint: selectedBlock.hint
+          }
+        },
+        async execute({ id, journeyId, hint }) {
+          updateEditorState(state.selectedStep, selectedBlock)
+
+          await textResponseHintUpdate({
+            variables: {
+              id,
+              journeyId,
+              input: {
+                hint
+              }
+            },
+            optimisticResponse: {
+              textResponseBlockUpdate: {
+                id,
+                hint,
+                __typename: 'TextResponseBlock'
+              }
+            }
+          })
+        },
+        async undo({ id, journeyId, hint }) {
+          await textResponseHintUpdate({
+            variables: {
+              id,
+              journeyId,
+              input: {
+                hint
+              }
+            },
+            optimisticResponse: {
+              textResponseBlockUpdate: {
+                id,
+                hint,
+                __typename: 'TextResponseBlock'
+              }
+            }
+          })
+          updateEditorState(state.selectedStep, selectedBlock)
+        }
+      })
+    }
   }
 
   const initialValues =
