555295e481191457b8e100c9b717d11d312190c7
jianwei1 <jianweichong08@gmail.com>
2024-07-17 23:13:48 +0000

    test: added testwrapper to strategysections

--- apps/watch/src/components/StrategySections/StrategySections.spec.tsx
+++ apps/watch/src/components/StrategySections/StrategySections.spec.tsx
@@ -1,9 +1,14 @@
+import { InstantSearchTestWrapper } from '@core/journeys/ui/mocks/InstantSearchWrapper'
 import { render, screen } from '@testing-library/react'
 import { StrategySections } from './StrategySections'
 
 describe('StrategySections', () => {
   it('should render strategysections', () => {
-    render(<StrategySections />)
+    render(
+      <InstantSearchTestWrapper>
+        <StrategySections />
+      </InstantSearchTestWrapper>
+    )
 
     expect(screen.getByTestId('StrategySections')).toBeInTheDocument()
   })
