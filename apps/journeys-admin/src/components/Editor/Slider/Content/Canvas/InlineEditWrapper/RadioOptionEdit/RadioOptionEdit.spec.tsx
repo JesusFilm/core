bbc23478a2daec5bc2ea38d3662fc42a11e85347
Edmond Shen <egsmond@gmail.com>
2024-07-17 21:41:16 +0000

    test: test for radio option undo

--- apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/InlineEditWrapper/RadioOptionEdit/RadioOptionEdit.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/InlineEditWrapper/RadioOptionEdit/RadioOptionEdit.spec.tsx
@@ -1,10 +1,12 @@
 import { MockedProvider } from '@apollo/client/testing'
-import { fireEvent, render, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 
 import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 import type { TreeBlock } from '@core/journeys/ui/block'
 
 import { RadioOptionFields } from '../../../../../../../../__generated__/RadioOptionFields'
+import { CommandRedoItem } from '../../../../../Toolbar/Items/CommandRedoItem'
+import { CommandUndoItem } from '../../../../../Toolbar/Items/CommandUndoItem'
 
 import { RADIO_OPTION_BLOCK_UPDATE_CONTENT, RadioOptionEdit } from '.'
 
@@ -179,5 +181,150 @@ describe('RadioOptionEdit', () => {
       </MockedProvider>
     )
     expect(getByRole('button', { name: '' })).toBeInTheDocument()
+  })
+
+  it('should undo the label change', async () => {
+    const result1 = jest.fn(() => ({
+      data: {
+        radioOptionBlockUpdate: [
+          {
+            __typename: 'RadioOptionBlock',
+            id: 'option.id',
+            label: 'updated label more'
+          }
+        ]
+      }
+    }))
+
+    const result2 = jest.fn(() => ({
+      data: {
+        radioOptionBlockUpdate: [
+          {
+            __typename: 'RadioOptionBlock',
+            id: 'option.id',
+            label: 'test label'
+          }
+        ]
+      }
+    }))
+
+    const mockUpdateSuccess1 = {
+      request: {
+        query: RADIO_OPTION_BLOCK_UPDATE_CONTENT,
+        variables: {
+          id: 'option.id',
+          input: {
+            label: 'updated label more'
+          }
+        }
+      },
+      result: result1
+    }
+
+    const mockUpdateSuccess2 = {
+      request: {
+        query: RADIO_OPTION_BLOCK_UPDATE_CONTENT,
+        variables: {
+          id: 'option.id',
+          input: {
+            label: 'test label'
+          }
+        }
+      },
+      result: result2
+    }
+
+    render(
+      <MockedProvider mocks={[mockUpdateSuccess1, mockUpdateSuccess2]}>
+        <EditorProvider>
+          <CommandUndoItem variant="button" />
+          <RadioOptionEdit {...props} />
+        </EditorProvider>
+      </MockedProvider>
+    )
+
+    const input = screen.getByRole('textbox')
+    fireEvent.click(input)
+    fireEvent.change(input, { target: { value: 'updated label more' } })
+    fireEvent.blur(input)
+    await waitFor(() => expect(result1).toHaveBeenCalled())
+
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    await waitFor(() => expect(result2).toHaveBeenCalled())
+  })
+
+  it('should redo the change to label that was undone', async () => {
+    const result1 = jest.fn(() => ({
+      data: {
+        radioOptionBlockUpdate: [
+          {
+            __typename: 'RadioOptionBlock',
+            id: 'option.id',
+            label: 'updated label more'
+          }
+        ]
+      }
+    }))
+
+    const result2 = jest.fn(() => ({
+      data: {
+        radioOptionBlockUpdate: [
+          {
+            __typename: 'RadioOptionBlock',
+            id: 'option.id',
+            label: 'test label'
+          }
+        ]
+      }
+    }))
+
+    const mockUpdateSuccess1 = {
+      request: {
+        query: RADIO_OPTION_BLOCK_UPDATE_CONTENT,
+        variables: {
+          id: 'option.id',
+          input: {
+            label: 'updated label more'
+          }
+        }
+      },
+      result: result1,
+      maxUsageCount: 2
+    }
+
+    const mockUpdateSuccess2 = {
+      request: {
+        query: RADIO_OPTION_BLOCK_UPDATE_CONTENT,
+        variables: {
+          id: 'option.id',
+          input: {
+            label: 'test label'
+          }
+        }
+      },
+      result: result2
+    }
+
+    render(
+      <MockedProvider mocks={[mockUpdateSuccess1, mockUpdateSuccess2]}>
+        <EditorProvider>
+          <CommandUndoItem variant="button" />
+          <CommandRedoItem variant="button" />
+          <RadioOptionEdit {...props} />
+        </EditorProvider>
+      </MockedProvider>
+    )
+
+    const input = screen.getByRole('textbox')
+    fireEvent.click(input)
+    fireEvent.change(input, { target: { value: 'updated label more' } })
+    fireEvent.blur(input)
+    await waitFor(() => expect(result1).toHaveBeenCalled())
+
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    await waitFor(() => expect(result2).toHaveBeenCalled())
+
+    fireEvent.click(screen.getByRole('button', { name: 'Redo' }))
+    await waitFor(() => expect(result1).toHaveBeenCalled())
   })
 })
