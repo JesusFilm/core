70ca05f692d24d73197b19edc1860f65fa040fcc
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-07-18 20:05:14 +0000

    feat: use color picker for typography blocks

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Typography/Color/Color.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Typography/Color/Color.tsx
@@ -1,19 +1,21 @@
 import { gql, useMutation } from '@apollo/client'
-import { useTranslation } from 'next-i18next'
-import { ReactElement } from 'react'
+import { type ReactElement, useRef, useState } from 'react'
 
 import type { TreeBlock } from '@core/journeys/ui/block'
 import { useCommand } from '@core/journeys/ui/CommandProvider'
-import { useEditor } from '@core/journeys/ui/EditorProvider'
-
 import { BlockFields_TypographyBlock as TypographyBlock } from '../../../../../../../../../../__generated__/BlockFields'
 import { TypographyColor } from '../../../../../../../../../../__generated__/globalTypes'
+import { ColorDisplayIcon } from '../../../controls/ColorDisplayIcon'
+import { ToggleButtonGroup } from '../../../controls/ToggleButtonGroup'
+import { Box } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { debounce } from 'lodash'
+import { HexColorPicker } from 'react-colorful'
+import { useEditor } from '@core/journeys/ui/EditorProvider'
 import {
   TypographyBlockUpdateColor,
   TypographyBlockUpdateColorVariables
 } from '../../../../../../../../../../__generated__/TypographyBlockUpdateColor'
-import { ColorDisplayIcon } from '../../../controls/ColorDisplayIcon'
-import { ToggleButtonGroup } from '../../../controls/ToggleButtonGroup'
 
 export const TYPOGRAPHY_BLOCK_UPDATE_COLOR = gql`
   mutation TypographyBlockUpdateColor($id: ID!, $color: TypographyColor!) {
@@ -25,7 +27,7 @@ export const TYPOGRAPHY_BLOCK_UPDATE_COLOR = gql`
 `
 
 export function Color(): ReactElement {
-  const { t } = useTranslation('apps-journeys-admin')
+  const theme = useTheme()
   const [typographyBlockUpdate] = useMutation<
     TypographyBlockUpdateColor,
     TypographyBlockUpdateColorVariables
@@ -39,63 +41,62 @@ export function Color(): ReactElement {
     | TreeBlock<TypographyBlock>
     | undefined
 
-  function handleChange(color: TypographyColor): void {
-    if (selectedBlock == null || color == null) return
+  const [selectedColor, setSelectedColor] = useState(
+    selectedBlock?.customColor ?? theme.palette.primary.main
+  )
 
-    add({
-      parameters: {
-        execute: { color },
-        undo: {
-          color: selectedBlock.color
-        }
-      },
-      execute({ color }) {
-        dispatch({
-          type: 'SetEditorFocusAction',
-          selectedStep,
-          selectedBlock
-        })
-        void typographyBlockUpdate({
-          variables: {
-            id: selectedBlock.id,
-            color
-          },
-          optimisticResponse: {
-            typographyBlockUpdate: {
-              id: selectedBlock.id,
-              color,
-              __typename: 'TypographyBlock'
-            }
-          }
-        })
-      }
-    })
+  const handleColorChange = async (color: string): Promise<void> => {
+    void debouncedColorChange(color.toUpperCase())
   }
 
-  const options = [
-    {
-      value: TypographyColor.primary,
-      label: t('Primary'),
-      icon: <ColorDisplayIcon color={TypographyColor.primary} />
-    },
-    {
-      value: TypographyColor.secondary,
-      label: t('Secondary'),
-      icon: <ColorDisplayIcon color={TypographyColor.secondary} />
-    },
-    {
-      value: TypographyColor.error,
-      label: t('Error'),
-      icon: <ColorDisplayIcon color={TypographyColor.error} />
-    }
-  ]
+  const debouncedColorChange = useRef(
+    debounce(async (color: string) => {
+      void changeTypographyColor(color)
+      setSelectedColor(color)
+    }, 100)
+  ).current
+
+  function changeTypographyColor(color: string): void {
+    if (selectedBlock == null || color == null) return
+
+      add({
+        parameters: {
+          execute: { color },
+          undo: {
+            color: selectedBlock.color
+          }
+        },
+        execute({ color }) {
+          dispatch({
+            type: 'SetEditorFocusAction',
+            selectedStep,
+            selectedBlock
+          })
+          void typographyBlockUpdate({
+            variables: {
+              id: selectedBlock.id,
+              customColor: color
+            },
+            optimisticResponse: {
+              typographyBlockUpdate: {
+                id: selectedBlock.id,
+                customColor: color,
+                __typename: 'TypographyBlock'
+              }
+            }
+          })
+        }
+      })
+  }
 
   return (
-    <ToggleButtonGroup
-      value={selectedBlock?.color ?? TypographyColor.primary}
-      onChange={handleChange}
-      options={options}
-      testId="Color"
-    />
+    <Box sx={{ px: 4, pb: 4 }}>
+      <HexColorPicker
+        data-testid="TextColorPicker"
+        color={selectedColor}
+        onChange={handleColorChange}
+        style={{ width: '100%', height: 125 }}
+      />
+    </Box>
   )
 }
