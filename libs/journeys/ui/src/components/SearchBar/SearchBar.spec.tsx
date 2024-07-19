87513d9e2b1db1b6031c9cb3fe3bbef1f65d0ae1
scottkelso <scottkelso07@hotmail.com>
2024-07-19 02:04:11 +0000

    test: add test for searchbar refine call

--- libs/journeys/ui/src/components/SearchBar/SearchBar.spec.tsx
+++ libs/journeys/ui/src/components/SearchBar/SearchBar.spec.tsx
@@ -1,22 +1,24 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { SearchBoxRenderState } from 'instantsearch.js/es/connectors/search-box/connectSearchBox'
-import {
-  InstantSearchApi,
-  useInstantSearch,
-  useSearchBox
-} from 'react-instantsearch'
+import { useSearchBox } from 'react-instantsearch'
 import '../../../test/i18n'
 import { SearchBar } from './SearchBar'
 
 jest.mock('react-instantsearch')
 
+function mockUseSearchBox() {
+  const refine = jest.fn()
+  const useSearchBoxMocked = jest.mocked(useSearchBox)
+  useSearchBoxMocked.mockReturnValue({
+    query: 'Hello World!',
+    refine: refine
+  } as unknown as SearchBoxRenderState)
+  return refine
+}
+
 describe('SearchBar', () => {
   beforeEach(() => {
-    const useSearchBoxMocked = jest.mocked(useSearchBox)
-    useSearchBoxMocked.mockReturnValue({
-      query: 'Hello World!',
-      refine: jest.fn()
-    } as unknown as SearchBoxRenderState)
+    mockUseSearchBox()
   })
 
   it('should render input field', async () => {
@@ -36,5 +38,13 @@ describe('SearchBar', () => {
     render(<SearchBar />)
     const searchIcon = screen.getByTestId('Search1Icon')
     expect(searchIcon).toBeInTheDocument()
+  })
+
+  it('should refine when text typed', async () => {
+    const refine = mockUseSearchBox()
+    render(<SearchBar />)
+    const input = screen.getByDisplayValue('Hello World!')
+    fireEvent.change(input, { target: { value: 'Hello' } })
+    expect(refine).toHaveBeenCalled()
   })
 })
