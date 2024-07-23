d743beec5efc2bb4f46ac79f91f353f49e952e6f
Kneesal <nisalcottingham@gmail.com>
2024-07-23 00:57:34 +0000

    feat: init

--- apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/NewStepButton/NewStepButton.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/NewStepButton/NewStepButton.spec.tsx
@@ -6,8 +6,13 @@ import { v4 as uuidv4 } from 'uuid'
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import { defaultJourney } from '@core/journeys/ui/TemplateView/data'
 
+import { deleteStepMock as deleteStep } from '../../../../../libs/useBlockDeleteMutation/useBlockDeleteMutation.mock'
+import { useBlockRestoreMutationMock } from '../../../../../libs/useBlockRestoreMutation/useBlockRestoreMutation.mock'
 import { stepAndCardBlockCreateMock } from '../../../../../libs/useStepAndCardBlockCreateMutation/useStepAndCardBlockCreateMutation.mock'
+import { CommandRedoItem } from '../../../Toolbar/Items/CommandRedoItem'
+import { CommandUndoItem } from '../../../Toolbar/Items/CommandUndoItem'
 
+import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 import { NewStepButton } from '.'
 
 jest.mock('uuid', () => ({
@@ -39,9 +44,11 @@ describe('NewStepButton', () => {
     render(
       <MockedProvider mocks={[{ ...stepAndCardBlockCreateMock, result }]}>
         <JourneyProvider value={{ journey: defaultJourney }}>
-          <ReactFlowProvider>
-            <NewStepButton />
-          </ReactFlowProvider>
+          <EditorProvider>
+            <ReactFlowProvider>
+              <NewStepButton />
+            </ReactFlowProvider>
+          </EditorProvider>
         </JourneyProvider>
       </MockedProvider>
     )
@@ -49,5 +56,104 @@ describe('NewStepButton', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Add Step' }))
 
     await waitFor(() => expect(result).toHaveBeenCalled())
+  })
+  it('should remove a step on undo click', async () => {
+    mockUuidv4.mockReturnValueOnce('newStep.id')
+    mockUuidv4.mockReturnValueOnce('newCard.id')
+
+    const result = jest.fn().mockReturnValue(stepAndCardBlockCreateMock.result)
+    const deletStepMock = {
+      ...deleteStep,
+      request: {
+        ...deleteStep.request,
+        variables: {
+          id: 'newStep.id',
+          journeyId: 'journey-id',
+          parentBlockId: null
+        }
+      }
+    }
+    const deleteResult = jest.fn().mockReturnValue(deletStepMock.result)
+
+    render(
+      <MockedProvider
+        mocks={[
+          { ...stepAndCardBlockCreateMock, result },
+          { ...deletStepMock, result: deleteResult }
+        ]}
+      >
+        <JourneyProvider value={{ journey: defaultJourney }}>
+          <EditorProvider>
+            <ReactFlowProvider>
+              <CommandUndoItem variant="button" />
+              <NewStepButton />
+            </ReactFlowProvider>
+          </EditorProvider>
+        </JourneyProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: 'Add Step' }))
+    await waitFor(() => expect(result).toHaveBeenCalled())
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    await waitFor(() => expect(deleteResult).toHaveBeenCalled())
+  })
+
+  it('should redo a step on redo click', async () => {
+    mockUuidv4.mockReturnValueOnce('newStep.id')
+    mockUuidv4.mockReturnValueOnce('newCard.id')
+
+    const result = jest.fn().mockReturnValue(stepAndCardBlockCreateMock.result)
+    const deletStepMock = {
+      ...deleteStep,
+      request: {
+        ...deleteStep.request,
+        variables: {
+          id: 'newStep.id',
+          journeyId: 'journey-id',
+          parentBlockId: null
+        }
+      }
+    }
+    const deleteResult = jest.fn().mockReturnValue(deletStepMock.result)
+
+    const blockRestoreMock = {
+      ...useBlockRestoreMutationMock,
+      request: {
+        ...useBlockRestoreMutationMock.request,
+        variables: { blockRestoreId: 'newStep.id' }
+      }
+    }
+
+    const blockRestoreResult = jest
+      .fn()
+      .mockReturnValue(blockRestoreMock.result)
+
+    render(
+      <MockedProvider
+        mocks={[
+          { ...stepAndCardBlockCreateMock, result },
+          { ...deletStepMock, result: deleteResult },
+          { ...blockRestoreMock, result: blockRestoreResult }
+        ]}
+      >
+        <JourneyProvider value={{ journey: defaultJourney }}>
+          <EditorProvider>
+            <ReactFlowProvider>
+              <CommandUndoItem variant="button" />
+              <CommandRedoItem variant="button" />
+              <NewStepButton />
+            </ReactFlowProvider>
+          </EditorProvider>
+        </JourneyProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: 'Add Step' }))
+    await waitFor(() => expect(result).toHaveBeenCalled())
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    await waitFor(() => expect(deleteResult).toHaveBeenCalled())
+    fireEvent.click(screen.getByRole('button', { name: 'Redo' }))
+    await waitFor(() => expect(blockRestoreResult).toHaveBeenCalled())
   })
 })
