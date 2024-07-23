c872ef14255b6517aaba9cf4abea38f8cc8c50cb
Edmond Shen <egsmond@gmail.com>
2024-07-23 22:09:04 +0000

    test: add test for undo type

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/TextResponse/TextResponseFields/Type/Type.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/TextResponse/TextResponseFields/Type/Type.spec.tsx
@@ -3,10 +3,13 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 
 import { EditorProvider } from '@core/journeys/ui/EditorProvider'
 import { TreeBlock } from '@core/journeys/ui/block'
+
 import { BlockFields_TextResponseBlock as TextResponseBlock } from '../../../../../../../../../../../__generated__/BlockFields'
 import { TextResponseLabelUpdate } from '../../../../../../../../../../../__generated__/TextResponseLabelUpdate'
 import { TextResponseTypeUpdate } from '../../../../../../../../../../../__generated__/TextResponseTypeUpdate'
 import { TextResponseType } from '../../../../../../../../../../../__generated__/globalTypes'
+
+import { CommandUndoItem } from '../../../../../../../../Toolbar/Items/CommandUndoItem'
 import { TEXT_RESPONSE_LABEL_UPDATE } from '../Label/Label'
 import { TEXT_RESPONSE_TYPE_UPDATE, Type } from './Type'
 
@@ -163,4 +166,106 @@ describe('Type', () => {
       expect(freeFormTypeUpdateMock.result).toHaveBeenCalled()
     )
   })
+
+  it('should undo the change to type', async () => {
+    const emailLabelUpdateMock: MockedResponse<TextResponseLabelUpdate> = {
+      ...textResponseLabelUpdateMock,
+      request: {
+        query: TEXT_RESPONSE_LABEL_UPDATE,
+        variables: {
+          id: selectedBlock.id,
+          input: {
+            label: 'Email'
+          }
+        }
+      }
+    }
+
+    const emailTextResponseTypeUpdateMock = {
+      ...textResponseTypeUpdateMock,
+      request: {
+        query: TEXT_RESPONSE_TYPE_UPDATE,
+        variables: {
+          id: selectedBlock.id,
+          input: {
+            type: TextResponseType.email
+          }
+        }
+      }
+    }
+
+    const undoLabelUpdateMock: MockedResponse<TextResponseLabelUpdate> = {
+      ...textResponseLabelUpdateMock,
+      request: {
+        query: TEXT_RESPONSE_LABEL_UPDATE,
+        variables: {
+          id: selectedBlock.id,
+          input: {
+            label: 'Your answer here'
+          }
+        }
+      },
+      result: jest.fn(() => ({
+        data: {
+          textResponseBlockUpdate: {
+            __typename: 'TextResponseBlock',
+            id: selectedBlock.id,
+            label: 'Updated label'
+          }
+        }
+      }))
+    }
+
+    const undoTextResponseTypeUpdateMock = {
+      ...textResponseTypeUpdateMock,
+      request: {
+        query: TEXT_RESPONSE_TYPE_UPDATE,
+        variables: {
+          id: selectedBlock.id,
+          input: {
+            type: TextResponseType.freeForm
+          }
+        }
+      },
+      result: jest.fn(() => ({
+        data: {
+          textResponseBlockUpdate: {
+            id: selectedBlock.id,
+            __typename: 'TextResponseBlock',
+            type: TextResponseType.freeForm
+          }
+        }
+      }))
+    }
+
+    render(
+      <MockedProvider
+        mocks={[
+          emailLabelUpdateMock,
+          emailTextResponseTypeUpdateMock,
+          undoLabelUpdateMock,
+          undoTextResponseTypeUpdateMock
+        ]}
+      >
+        <EditorProvider initialState={{ selectedBlock }}>
+          <CommandUndoItem variant="button" />
+          <Type />
+        </EditorProvider>
+      </MockedProvider>
+    )
+
+    fireEvent.click(screen.getByRole('button', { name: 'Email' }))
+    await waitFor(() =>
+      expect(emailTextResponseTypeUpdateMock.result).toHaveBeenCalled()
+    )
+    await waitFor(() => expect(emailLabelUpdateMock.result).toHaveBeenCalled())
+
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    await waitFor(() =>
+      expect(undoTextResponseTypeUpdateMock.result).toHaveBeenCalled()
+    )
+    await waitFor(() => expect(undoLabelUpdateMock.result).toHaveBeenCalled())
+  })
+
+  //redo test, also check with John about expected behaviour
 })
