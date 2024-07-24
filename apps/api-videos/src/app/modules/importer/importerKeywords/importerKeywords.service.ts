b60dfdc9fb3227cee67b2337d537c23428805d45
Tanner <tanner.fleming@gmail.com>
2024-07-24 13:51:59 +0000

    fix: filter videos instead of failing

--- apps/api-videos/src/app/modules/importer/importerKeywords/importerKeywords.service.ts
+++ apps/api-videos/src/app/modules/importer/importerKeywords/importerKeywords.service.ts
@@ -36,33 +36,34 @@ export class ImporterKeywordsService extends ImporterService<Keyword> {
     super()
   }
 
-  protected async checkVideos(keyword: Keyword): Promise<void> {
+  protected async filterValidVideos(keyword: Keyword): Promise<Keyword> {
     if (keyword.videos && keyword.videos.connect) {
-      for (const video of keyword.videos.connect) {
-        if (!this.importerVideosService.ids.includes(video.id)) {
-          throw new Error(`Video ${video.id} not found`)
-        }
-      }
+      const validVideoIds = this.importerVideosService.ids
+
+      keyword.videos.connect = keyword.videos.connect.filter((v) =>
+        validVideoIds.some((id) => id === v.id)
+      )
     }
+    return keyword
   }
 
   protected async save(keyword: Keyword): Promise<void> {
-    this.checkVideos(keyword)
+    const filteredKeyword = await this.filterValidVideos(keyword)
     await this.prismaService.keyword.upsert({
       where: {
         value_languageId: {
-          value: keyword.value,
-          languageId: keyword.languageId
+          value: filteredKeyword.value,
+          languageId: filteredKeyword.languageId
         }
       },
-      update: keyword,
-      create: keyword
+      update: filteredKeyword,
+      create: filteredKeyword
     })
   }
 
   protected async saveMany(keywords: Keyword[]): Promise<void> {
     await this.prismaService.keyword.createMany({
-      data: keywords.filter((keyword) => this.checkVideos(keyword)),
+      data: keywords.filter((keyword) => this.filterValidVideos(keyword)),
       skipDuplicates: true
     })
   }
