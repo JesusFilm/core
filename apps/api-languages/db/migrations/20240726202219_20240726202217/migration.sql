7f40b284de767660118fd06852c5f4d4618de08a
Mike Allison <dj.mikeallison@gmail.com>
2024-07-26 20:25:08 +0000

    feat: language bq import

--- apps/api-languages/db/migrations/20240726202219_20240726202217/migration.sql
+++ apps/api-languages/db/migrations/20240726202219_20240726202217/migration.sql
@@ -20,6 +20,17 @@ ALTER TABLE "Language" ADD COLUMN     "hasVideos" BOOLEAN NOT NULL DEFAULT true;
 DROP TABLE "CountryContinent";
 
 -- CreateTable
+CREATE TABLE "CountryLanguage" (
+    "id" TEXT NOT NULL,
+    "languageId" TEXT NOT NULL,
+    "countryId" TEXT NOT NULL,
+    "speakers" INTEGER NOT NULL,
+    "displaySpeakers" INTEGER,
+
+    CONSTRAINT "CountryLanguage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Continent" (
     "id" TEXT NOT NULL,
 
@@ -38,6 +49,15 @@ CREATE TABLE "ContinentName" (
 );
 
 -- CreateIndex
+CREATE INDEX "CountryLanguage_languageId_idx" ON "CountryLanguage"("languageId");
+
+-- CreateIndex
+CREATE INDEX "CountryLanguage_countryId_idx" ON "CountryLanguage"("countryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CountryLanguage_languageId_countryId_key" ON "CountryLanguage"("languageId", "countryId");
+
+-- CreateIndex
 CREATE INDEX "ContinentName_languageId_idx" ON "ContinentName"("languageId");
 
 -- CreateIndex
@@ -54,6 +74,12 @@ CREATE INDEX "Language_hasVideos_idx" ON "Language"("hasVideos");
 
 -- AddForeignKey
 ALTER TABLE "Country" ADD CONSTRAINT "Country_continentId_fkey" FOREIGN KEY ("continentId") REFERENCES "Continent"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CountryLanguage" ADD CONSTRAINT "CountryLanguage_languageId_fkey" FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CountryLanguage" ADD CONSTRAINT "CountryLanguage_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ContinentName" ADD CONSTRAINT "ContinentName_continentId_fkey" FOREIGN KEY ("continentId") REFERENCES "Continent"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
