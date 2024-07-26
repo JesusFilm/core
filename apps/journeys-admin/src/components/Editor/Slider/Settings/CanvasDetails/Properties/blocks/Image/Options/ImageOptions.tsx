e5c3f910bb4a7243c8fbc187842377b21b3fb5e3
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-07-26 02:00:39 +0000

    feat: image options

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Image/Options/ImageOptions.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Image/Options/ImageOptions.tsx
@@ -1,30 +1,27 @@
 import { gql, useMutation } from '@apollo/client'
 import Box from '@mui/material/Box'
-import { useSnackbar } from 'notistack'
+import pick from 'lodash/pick'
 import { ReactElement } from 'react'
 
 import { useEditor } from '@core/journeys/ui/EditorProvider'
-import { useJourney } from '@core/journeys/ui/JourneyProvider'
 import type { TreeBlock } from '@core/journeys/ui/block'
 
+import { useCommand } from '@core/journeys/ui/CommandProvider'
+import { IMAGE_FIELDS } from '@core/journeys/ui/Image/imageFields'
 import { BlockFields_ImageBlock as ImageBlock } from '../../../../../../../../../../__generated__/BlockFields'
 import { ImageBlockUpdate } from '../../../../../../../../../../__generated__/ImageBlockUpdate'
+import { ImageBlockUpdateInput } from '../../../../../../../../../../__generated__/globalTypes'
 import { ImageSource } from '../../../../../Drawer/ImageSource'
 
 export const IMAGE_BLOCK_UPDATE = gql`
+  ${IMAGE_FIELDS}
   mutation ImageBlockUpdate(
     $id: ID!
-    $journeyId: ID!
     $input: ImageBlockUpdateInput!
   ) {
-    imageBlockUpdate(id: $id, journeyId: $journeyId, input: $input) {
+    imageBlockUpdate(id: $id, input: $input) {
       id
-      src
-      alt
-      width
-      height
-      parentOrder
-      blurhash
+      ...ImageFields
     }
   }
 `
@@ -33,59 +30,42 @@ export function ImageOptions(): ReactElement {
   const {
     state: { selectedBlock }
   } = useEditor()
-  const { journey } = useJourney()
-  const { enqueueSnackbar } = useSnackbar()
-  const [imageBlockUpdate, { loading, error }] =
-    useMutation<ImageBlockUpdate>(IMAGE_BLOCK_UPDATE)
+  const [imageBlockUpdate] = useMutation<ImageBlockUpdate>(IMAGE_BLOCK_UPDATE)
+  const { add } = useCommand()
 
   const imageBlock = selectedBlock as TreeBlock<ImageBlock>
 
-  const handleImageDelete = async (): Promise<void> => {
-    if (journey == null) return
-
-    try {
-      await imageBlockUpdate({
-        variables: {
-          id: imageBlock.id,
-          journeyId: journey.id,
-          input: {
-            src: null,
-            alt: ''
-          }
-        }
-      })
-    } catch (e) {
-      if (e instanceof Error) {
-        enqueueSnackbar(e.message, {
-          variant: 'error',
-          preventDuplicate: true
-        })
-      }
-    }
+  async function deleteImageBlock(): Promise<void> {
+    await updateImageBlock({ src: null, alt: '' })
   }
 
-  const updateImageBlock = async (block: ImageBlock): Promise<void> => {
-    if (journey == null) return
+  async function updateImageBlock(input: ImageBlockUpdateInput): Promise<void> {
+    const block: ImageBlock = {
+      ...imageBlock,
+      ...input,
+      alt: input.alt ?? imageBlock.alt,
+      blurhash: input.blurhash ?? imageBlock.blurhash,
+      height: input.height ?? imageBlock.height,
+      width: input.width ?? imageBlock.width
+    }
 
-    try {
-      await imageBlockUpdate({
-        variables: {
-          id: imageBlock.id,
-          journeyId: journey.id,
-          input: {
-            src: block.src,
-            alt: block.alt
+    await add({
+      parameters: {
+        execute: block,
+        undo: imageBlock
+      },
+      async execute(block) {
+        await imageBlockUpdate({
+          variables: {
+            id: imageBlock.id,
+            input: pick(block, Object.keys(input))
+          },
+          optimisticResponse: {
+            imageBlockUpdate: block
           }
-        }
-      })
-    } catch (e) {
-      if (e instanceof Error) {
-        enqueueSnackbar(e.message, {
-          variant: 'error',
-          preventDuplicate: true
         })
       }
-    }
+    })
   }
 
   return (
@@ -93,9 +73,7 @@ export function ImageOptions(): ReactElement {
       <ImageSource
         selectedBlock={imageBlock}
         onChange={updateImageBlock}
-        onDelete={handleImageDelete}
-        loading={loading}
-        error={error != null}
+        onDelete={deleteImageBlock}
       />
     </Box>
   )
