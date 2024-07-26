acfcf061f517068ee3af9ff423912ce8148a0e1c
scottkelso <scottkelso07@hotmail.com>
2024-07-26 02:38:02 +0000

    test: remove unused mock

--- libs/journeys/ui/src/components/TemplateGallery/TemplateGallery.spec.tsx
+++ libs/journeys/ui/src/components/TemplateGallery/TemplateGallery.spec.tsx
@@ -8,14 +8,8 @@ import { SearchBoxRenderState } from 'instantsearch.js/es/connectors/search-box/
 import { useRefinementList, useSearchBox } from 'react-instantsearch'
 import { TemplateGallery } from '.'
 import '../../../test/i18n'
-import { QueryResult } from '@apollo/client'
 import { useAlgoliaJourneys } from '../../libs/algolia/useAlgoliaJourneys'
 import { algoliaJourneys } from '../../libs/algolia/useAlgoliaJourneys/useAlgoliaJourneys.mock'
-import { useLanguagesQuery } from '../../libs/useLanguagesQuery'
-import {
-  GetLanguages,
-  GetLanguagesVariables
-} from '../../libs/useLanguagesQuery/__generated__/GetLanguages'
 
 jest.mock('@mui/material/useMediaQuery', () => ({
   __esModule: true,
@@ -32,7 +26,6 @@ jest.mock('next/router', () => ({
 
 jest.mock('react-instantsearch')
 jest.mock('../../libs/algolia/useAlgoliaJourneys')
-jest.mock('../../libs/useLanguagesQuery')
 
 describe('TemplateGallery', () => {
   beforeEach(() => {
@@ -53,11 +46,6 @@ describe('TemplateGallery', () => {
     useRefinementListMocked.mockReturnValue({
       refine: jest.fn()
     } as unknown as RefinementListRenderState)
-
-    const useLanguagesQueryMocked = jest.mocked(useLanguagesQuery)
-    useLanguagesQueryMocked.mockReturnValue({
-      data: []
-    } as unknown as QueryResult<GetLanguages, GetLanguagesVariables>)
   })
 
   it('should render searchbar', async () => {
