914d2893e99d13bf7a17c729dee5051cf69a3a62
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-08-05 09:52:14 +0000

    fix: failing test

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Icon/Color/Color.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Icon/Color/Color.spec.tsx
@@ -20,34 +20,41 @@ jest.mock('@mui/material/useMediaQuery', () => ({
 }))
 
 describe('Color', () => {
-  it('should change the icon color', async () => {
-    const selectedBlock: TreeBlock<ButtonBlock> = {
-      __typename: 'ButtonBlock',
-      id: 'id',
-      parentBlockId: 'parentBlockId',
-      parentOrder: 0,
-      label: 'test button',
-      buttonVariant: null,
-      buttonColor: null,
-      size: null,
-      startIconId: null,
-      endIconId: null,
-      action: null,
-      children: [
-        {
-          __typename: 'IconBlock',
-          id: 'iconBlock.id',
-          parentBlockId: null,
-          parentOrder: null,
-          iconName: IconName.ArrowForwardRounded,
-          iconSize: null,
-          iconColor: null,
-          children: []
-        }
-      ]
-    }
+  const selectedBlock: TreeBlock<ButtonBlock> = {
+    __typename: 'ButtonBlock',
+    id: 'id',
+    parentBlockId: 'parentBlockId',
+    parentOrder: 0,
+    label: 'test button',
+    buttonVariant: null,
+    buttonColor: null,
+    size: null,
+    startIconId: null,
+    endIconId: null,
+    action: null,
+    children: [
+      {
+        __typename: 'IconBlock',
+        id: 'iconBlock.id',
+        parentBlockId: null,
+        parentOrder: null,
+        iconName: IconName.ArrowForwardRounded,
+        iconSize: null,
+        iconColor: null,
+        children: []
+      }
+    ]
+  }
 
-    const result = jest.fn(() => ({
+  const mockIconColorUpdate1 = {
+    request: {
+      query: ICON_BLOCK_COLOR_UPDATE,
+      variables: {
+        id: 'iconBlock.id',
+        color: IconColor.secondary
+      }
+    },
+    result: jest.fn(() => ({
       data: {
         iconBlockUpdate: {
           id: 'iconBlock.id',
@@ -142,150 +149,5 @@ describe('Color', () => {
 
     fireEvent.click(screen.getByRole('button', { name: 'Redo' }))
     await waitFor(() => expect(mockFirstUpdate.result).toHaveBeenCalled())
-  })
-
-  it('should undo the icon color change', async () => {
-    const result1 = jest.fn(() => ({
-      data: {
-        iconBlockUpdate: {
-          id: 'iconBlock.id',
-          parentBlockId: 'buttonBlockId',
-          name: IconName.ArrowForwardRounded,
-          color: IconColor.secondary,
-          size: null
-        }
-      }
-    }))
-
-    const mockUpdateSuccess1 = {
-      request: {
-        query: ICON_BLOCK_COLOR_UPDATE,
-        variables: {
-          id: 'iconBlock.id',
-          input: {
-            color: IconColor.secondary
-          }
-        }
-      },
-      result: result1
-    }
-
-    const result2 = jest.fn(() => ({
-      data: {
-        iconBlockUpdate: {
-          id: 'iconBlock.id',
-          parentBlockId: 'buttonBlockId',
-          name: IconName.ArrowForwardRounded,
-          color: IconColor.inherit,
-          size: null
-        }
-      }
-    }))
-
-    const mockUpdateSuccess2 = {
-      request: {
-        query: ICON_BLOCK_COLOR_UPDATE,
-        variables: {
-          id: 'iconBlock.id',
-          input: {
-            color: IconColor.inherit
-          }
-        }
-      },
-      result: result2
-    }
-
-    render(
-      <MockedProvider mocks={[mockUpdateSuccess1, mockUpdateSuccess2]}>
-        <EditorProvider initialState={{ selectedBlock }}>
-          <CommandUndoItem variant="button" />
-          <Color id="iconBlock.id" iconColor={IconColor.inherit} />
-        </EditorProvider>
-      </MockedProvider>
-    )
-
-    expect(screen.getByRole('button', { name: 'Default' })).toHaveClass(
-      'Mui-selected'
-    )
-    fireEvent.click(screen.getByRole('button', { name: 'Secondary' }))
-    await waitFor(() => expect(result1).toHaveBeenCalled())
-
-    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
-    await waitFor(() => expect(result2).toHaveBeenCalled())
-  })
-
-  it('should redo the icon color change that was undone', async () => {
-    const result1 = jest.fn(() => ({
-      data: {
-        iconBlockUpdate: {
-          id: 'iconBlock.id',
-          parentBlockId: 'buttonBlockId',
-          name: IconName.ArrowForwardRounded,
-          color: IconColor.secondary,
-          size: null
-        }
-      }
-    }))
-
-    const mockUpdateSuccess1 = {
-      request: {
-        query: ICON_BLOCK_COLOR_UPDATE,
-        variables: {
-          id: 'iconBlock.id',
-          input: {
-            color: IconColor.secondary
-          }
-        }
-      },
-      result: result1,
-      maxUsageCount: 2
-    }
-
-    const result2 = jest.fn(() => ({
-      data: {
-        iconBlockUpdate: {
-          id: 'iconBlock.id',
-          parentBlockId: 'buttonBlockId',
-          name: IconName.ArrowForwardRounded,
-          color: IconColor.inherit,
-          size: null
-        }
-      }
-    }))
-
-    const mockUpdateSuccess2 = {
-      request: {
-        query: ICON_BLOCK_COLOR_UPDATE,
-        variables: {
-          id: 'iconBlock.id',
-          input: {
-            color: IconColor.inherit
-          }
-        }
-      },
-      result: result2
-    }
-
-    render(
-      <MockedProvider mocks={[mockUpdateSuccess1, mockUpdateSuccess2]}>
-        <EditorProvider initialState={{ selectedBlock }}>
-          <CommandUndoItem variant="button" />
-          <CommandRedoItem variant="button" />
-          <Color id="iconBlock.id" iconColor={IconColor.inherit} />
-        </EditorProvider>
-      </MockedProvider>
-    )
-
-    expect(screen.getByRole('button', { name: 'Default' })).toHaveClass(
-      'Mui-selected'
-    )
-    fireEvent.click(screen.getByRole('button', { name: 'Secondary' }))
-    await waitFor(() => expect(result1).toHaveBeenCalled())
-
-    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
-    await waitFor(() => expect(result2).toHaveBeenCalled())
-
-    fireEvent.click(screen.getByRole('button', { name: 'Redo' }))
-    await waitFor(() => expect(result1).toHaveBeenCalled())
   })
 })
