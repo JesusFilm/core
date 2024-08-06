2bd55c74765f4a5625dffb1fb0b3b531d6a14491
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-08-06 22:56:59 +0000

    fix: lint

--- apps/journeys-admin/src/components/Editor/Slider/Content/Social/Message/Message.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Social/Message/Message.spec.tsx
@@ -1,11 +1,12 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { render, screen, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 
 import { JourneyFields as Journey } from '../../../../../../../__generated__/JourneyFields'
 
-import { MockedProvider } from '@apollo/client/testing'
-import userEvent from '@testing-library/user-event'
+
 import { Message } from '.'
 
 describe('Message', () => {
