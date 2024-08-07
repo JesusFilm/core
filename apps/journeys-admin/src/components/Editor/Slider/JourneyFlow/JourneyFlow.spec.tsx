07245d878f6a3ba47dd1787f61ff911b12a9bd16
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-08-07 01:35:11 +0000

    fix: update journey flow

--- apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/JourneyFlow.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/JourneyFlow.spec.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 
 import { TreeBlock } from '@core/journeys/ui/block'
+import { CommandProvider } from '@core/journeys/ui/CommandProvider'
 import { ActiveSlide, EditorProvider } from '@core/journeys/ui/EditorProvider'
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import {
@@ -23,6 +24,8 @@ import {
 import { StepFields as StepBlock } from '../../../../../__generated__/StepFields'
 import { mockReactFlow } from '../../../../../test/mockReactFlow'
 import { useStepBlockPositionUpdateMutation } from '../../../../libs/useStepBlockPositionUpdateMutation'
+import { CommandRedoItem } from '../../Toolbar/Items/CommandRedoItem'
+import { CommandUndoItem } from '../../Toolbar/Items/CommandUndoItem'
 
 import { GET_STEP_BLOCKS_WITH_POSITION } from './JourneyFlow'
 import { transformSteps } from './libs/transformSteps'
@@ -130,7 +133,10 @@ describe('JourneyFlow', () => {
             }}
           >
             <Box sx={{ width: '100vw', height: '100vh' }}>
-              <JourneyFlow />
+              <CommandProvider>
+                <CommandUndoItem variant="button" />
+                <JourneyFlow />
+              </CommandProvider>
             </Box>
           </EditorProvider>
         </JourneyProvider>
@@ -138,7 +144,31 @@ describe('JourneyFlow', () => {
     )
 
     await waitFor(() => expect(result).toHaveBeenCalled())
-    expect(mockUpdate).toHaveBeenCalledTimes(7)
+    expect(mockUpdate).toHaveBeenCalledWith({
+      optimisticResponse: {
+        stepBlockPositionUpdate: [
+          { __typename: 'StepBlock', id: 'step0.id', x: 0, y: -8 },
+          { __typename: 'StepBlock', id: 'step1.id', x: 600, y: -8 },
+          { __typename: 'StepBlock', id: 'step2.id', x: 1200, y: -8 },
+          { __typename: 'StepBlock', id: 'step3.id', x: 1800, y: -8 },
+          { __typename: 'StepBlock', id: 'step4.id', x: 2400, y: -8 },
+          { __typename: 'StepBlock', id: 'step5.id', x: 2500, y: -8 },
+          { __typename: 'StepBlock', id: 'step6.id', x: 3600, y: -8 }
+        ]
+      },
+      variables: {
+        input: [
+          { id: 'step0.id', x: 0, y: -8 },
+          { id: 'step1.id', x: 600, y: -8 },
+          { id: 'step2.id', x: 1200, y: -8 },
+          { id: 'step3.id', x: 1800, y: -8 },
+          { id: 'step4.id', x: 2400, y: -8 },
+          { id: 'step5.id', x: 2500, y: -8 },
+          { id: 'step6.id', x: 3600, y: -8 }
+        ]
+      }
+    })
+    expect(screen.getByRole('button', { name: 'Undo' })).toBeDisabled()
   })
 
   it('should reorganize graph', async () => {
@@ -159,16 +189,76 @@ describe('JourneyFlow', () => {
             initialState={{ steps, activeSlide: ActiveSlide.JourneyFlow }}
           >
             <Box sx={{ width: '100vw', height: '100vh' }}>
-              <JourneyFlow />
+              <CommandProvider>
+                <CommandUndoItem variant="button" />
+                <CommandRedoItem variant="button" />
+                <JourneyFlow />
+              </CommandProvider>
             </Box>
           </EditorProvider>
         </JourneyProvider>
       </MockedProvider>
     )
 
+    const mockUpdateExecute = {
+      optimisticResponse: {
+        stepBlockPositionUpdate: [
+          { __typename: 'StepBlock', id: 'step0.id', x: 0, y: -8 },
+          { __typename: 'StepBlock', id: 'step1.id', x: 600, y: -8 },
+          { __typename: 'StepBlock', id: 'step2.id', x: 1200, y: -8 },
+          { __typename: 'StepBlock', id: 'step3.id', x: 1800, y: -8 },
+          { __typename: 'StepBlock', id: 'step4.id', x: 2400, y: -8 },
+          { __typename: 'StepBlock', id: 'step5.id', x: 2500, y: -8 },
+          { __typename: 'StepBlock', id: 'step6.id', x: 3600, y: -8 }
+        ]
+      },
+      variables: {
+        input: [
+          { id: 'step0.id', x: 0, y: -8 },
+          { id: 'step1.id', x: 600, y: -8 },
+          { id: 'step2.id', x: 1200, y: -8 },
+          { id: 'step3.id', x: 1800, y: -8 },
+          { id: 'step4.id', x: 2400, y: -8 },
+          { id: 'step5.id', x: 2500, y: -8 },
+          { id: 'step6.id', x: 3600, y: -8 }
+        ]
+      }
+    }
+
+    const mockUpdateUndo = {
+      optimisticResponse: {
+        stepBlockPositionUpdate: [
+          { __typename: 'StepBlock', id: 'step0.id', x: 0, y: 1 },
+          { __typename: 'StepBlock', id: 'step1.id', x: 300, y: 1 },
+          { __typename: 'StepBlock', id: 'step2.id', x: 600, y: 1 },
+          { __typename: 'StepBlock', id: 'step3.id', x: 900, y: 1 },
+          { __typename: 'StepBlock', id: 'step4.id', x: 1200, y: 1 },
+          { __typename: 'StepBlock', id: 'step5.id', x: 1500, y: 1 },
+          { __typename: 'StepBlock', id: 'step6.id', x: 1800, y: 1 }
+        ]
+      },
+      variables: {
+        input: [
+          { id: 'step0.id', x: 0, y: 1 },
+          { id: 'step1.id', x: 300, y: 1 },
+          { id: 'step2.id', x: 600, y: 1 },
+          { id: 'step3.id', x: 900, y: 1 },
+          { id: 'step4.id', x: 1200, y: 1 },
+          { id: 'step5.id', x: 1500, y: 1 },
+          { id: 'step6.id', x: 1800, y: 1 }
+        ]
+      }
+    }
+
     await waitFor(() => expect(result).toHaveBeenCalled())
     fireEvent.click(screen.getByTestId('ArrowRefresh6Icon'))
-    expect(mockUpdate).toHaveBeenCalled()
+    expect(mockUpdate).toHaveBeenCalledWith(mockUpdateExecute)
+    mockUpdate.mockClear()
+    fireEvent.click(screen.getByRole('button', { name: 'Undo' }))
+    expect(mockUpdate).toHaveBeenCalledWith(mockUpdateUndo)
+    mockUpdate.mockClear()
+    fireEvent.click(screen.getByRole('button', { name: 'Redo' }))
+    expect(mockUpdate).toHaveBeenCalledWith(mockUpdateExecute)
   })
 
   it('should hide new step button if in analytics mode', async () => {
