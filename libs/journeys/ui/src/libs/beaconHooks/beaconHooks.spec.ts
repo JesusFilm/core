a59c08da161fecf62fca2a4c3561a897994bf2b1
Siyang <csiyang@yahoo.co.nz>
2024-08-08 03:43:27 +0000

    test: update beaconhooks tests

--- libs/journeys/ui/src/libs/beaconHooks/beaconHooks.spec.ts
+++ libs/journeys/ui/src/libs/beaconHooks/beaconHooks.spec.ts
@@ -1,9 +1,9 @@
 import camelCase from 'lodash/camelCase'
 import startCase from 'lodash/startCase'
 
-import { setBeaconPageViewed } from '.'
+import { openBeacon, setBeaconPageViewed, setBeaconRoute } from '.'
 
-describe('setBeaconPageViewed', () => {
+describe('beaconHooks', () => {
   beforeEach(() => {
     window.Beacon = jest.fn()
   })
@@ -12,43 +12,63 @@ describe('setBeaconPageViewed', () => {
     jest.resetAllMocks()
   })
 
-  it('should call window.Beacon with page-viewed event', () => {
-    const route = 'testRoute'
-    const expectedUrl = document.location.href
-    const expectedTitle = `Clicked on ${route}`
+  describe('setBeaconPageViewed', () => {
+    it('should call window.Beacon with page-viewed event', () => {
+      const route = 'testRoute'
+      const expectedUrl = document.location.href
+      const expectedTitle = `Clicked on ${route}`
 
-    setBeaconPageViewed(route)
+      setBeaconPageViewed(route)
 
-    expect(window.Beacon).toHaveBeenCalledWith('event', {
-      type: 'page-viewed',
-      url: expectedUrl,
-      title: expectedTitle
+      expect(window.Beacon).toHaveBeenCalledWith('event', {
+        type: 'page-viewed',
+        url: expectedUrl,
+        title: expectedTitle
+      })
+    })
+
+    it('should set up window.Beacon on open event with search query', () => {
+      const route = 'testRoute'
+      const query = startCase(camelCase(route))
+
+      setBeaconPageViewed(route)
+
+      // Simulate the 'on' event callback
+      const onOpenCallback = (window.Beacon as jest.Mock).mock.calls.find(
+        (call) => call[0] === 'on' && call[1] === 'open'
+      )?.[2]
+      if (onOpenCallback != null) {
+        onOpenCallback()
+      }
+
+      expect(window.Beacon).toHaveBeenCalledWith('search', query)
+    })
+
+    it('should not call window.Beacon if it is not defined', () => {
+      window.Beacon = undefined
+      const route = 'testRoute'
+
+      setBeaconPageViewed(route)
+
+      expect(window.Beacon).toBeUndefined()
     })
   })
 
-  it('should set up window.Beacon on open event with search query', () => {
-    const route = 'testRoute'
-    const query = startCase(camelCase(route))
+  describe('setBeaconRoute', () => {
+    it('should set beacon route', () => {
+      const route = 'testRoute'
 
-    setBeaconPageViewed(route)
+      setBeaconRoute(route)
 
-    // Simulate the 'on' event callback
-    const onOpenCallback = (window.Beacon as jest.Mock).mock.calls.find(
-      (call) => call[0] === 'on' && call[1] === 'open'
-    )?.[2]
-    if (onOpenCallback != null) {
-      onOpenCallback()
-    }
-
-    expect(window.Beacon).toHaveBeenCalledWith('search', query)
+      expect(window.Beacon).toHaveBeenCalledWith('navigate', route)
+    })
   })
 
-  it('should not call window.Beacon if it is not defined', () => {
-    window.Beacon = undefined
-    const route = 'testRoute'
+  describe('openBeacon', () => {
+    it('should open the beacon', () => {
+      openBeacon()
 
-    setBeaconPageViewed(route)
-
-    expect(window.Beacon).toBeUndefined()
+      expect(window.Beacon).toHaveBeenCalledWith('open')
+    })
   })
 })
--- libs/journeys/ui/src/libs/beaconHooks/index.ts
+++ libs/journeys/ui/src/libs/beaconHooks/index.ts
@@ -1,7 +1,2 @@
-export {
-  setBeaconPageViewed,
-  setBeaconRoute,
-  openBeacon,
-  isBeaconOpen
-} from './beaconHooks'
+export { setBeaconPageViewed, setBeaconRoute, openBeacon } from './beaconHooks'
 export type { FormObject } from './beaconHooks'
