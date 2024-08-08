f96a2fab5d52df5639bf0298eaa18b4cd2e8adf8
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-08-08 10:32:49 +0000

    chore: add otlp

---
+++ apps/api-analytics/src/tracer.ts
@@ -0,0 +1,57 @@
+import { trace } from '@opentelemetry/api'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { Resource } from '@opentelemetry/resources'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { AttributeNames, SpanNames } from '@pothos/tracing-opentelemetry'
+import { ASTNode, print } from 'graphql'
+import { Plugin } from 'graphql-yoga'
+
+export const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'api-analytics'
+  })
+})
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({})))
+
+provider.register()
+
+registerInstrumentations({
+  instrumentations: [new HttpInstrumentation({})]
+})
+
+export const tracer = trace.getTracer('graphql')
+
+export const tracingPlugin: Plugin = {
+  onExecute: ({ setExecuteFn, executeFn }) => {
+    setExecuteFn(
+      async (options) =>
+        await tracer.startActiveSpan(
+          SpanNames.EXECUTE,
+          {
+            attributes: {
+              [AttributeNames.OPERATION_NAME]:
+                options.operationName ?? undefined,
+              [AttributeNames.SOURCE]: print(options.document as ASTNode)
+            }
+          },
+          async (span) => {
+            try {
+              const result = await executeFn(options)
+
+              return result
+            } catch (error) {
+              span.recordException(error as Error)
+              throw error
+            } finally {
+              span.end()
+            }
+          }
+        )
+    )
+  }
+}
--- apps/api-languages/src/schema/builder.ts
+++ apps/api-languages/src/schema/builder.ts
@@ -1,13 +1,19 @@
 import SchemaBuilder from '@pothos/core'
 import DirectivesPlugin from '@pothos/plugin-directives'
 import FederationPlugin from '@pothos/plugin-federation'
-// eslint-disable-next-line import/no-named-as-default
-import PrismaPlugin from '@pothos/plugin-prisma'
+import pluginName from '@pothos/plugin-prisma'
+import TracingPlugin, { isRootField } from '@pothos/plugin-tracing'
+import { createOpenTelemetryWrapper } from '@pothos/tracing-opentelemetry'
 
 import { Prisma } from '.prisma/api-languages-client'
 
 import type PrismaTypes from '../__generated__/pothos-types'
 import { prisma } from '../lib/prisma'
+import { tracer } from '../tracer'
+
+const createSpan = createOpenTelemetryWrapper(tracer, {
+  includeSource: true
+})
 
 export const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes
@@ -15,11 +21,15 @@ export const builder = new SchemaBuilder<{
     ID: { Input: string; Output: number | string }
   }
 }>({
-  plugins: [PrismaPlugin, DirectivesPlugin, FederationPlugin],
+  plugins: [TracingPlugin, pluginName, DirectivesPlugin, FederationPlugin],
   prisma: {
     client: prisma,
     dmmf: Prisma.dmmf,
     onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn'
+  },
+  tracing: {
+    default: (config) => isRootField(config),
+    wrap: (resolver, options) => createSpan(resolver, options)
   }
 })
 
