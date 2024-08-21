a6d53da379009a12157de77aa20f9fed23356d8b
John Geronimo <53248335+GeronimoJohn@users.noreply.github.com>
2024-08-21 14:41:28 +1200

    feat: use languages continents query (#3280)
    Co-authored-by: scottkelso <scottkelso07@hotmail.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

---
+++ libs/journeys/ui/src/libs/useLanguagesContinentsQuery/useLanguagesContinentsQuery.ts
@@ -0,0 +1,25 @@
+import { QueryResult, gql, useQuery } from '@apollo/client'
+
+import { GetLanguagesContinents } from './__generated__/GetLanguagesContinents'
+
+export const GET_LANGUAGES_CONTINENTS = gql`
+  query GetLanguagesContinents {
+    languages {
+      id
+      name {
+        value
+      }
+      countryLanguages {
+        country {
+          continent {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+export function useLanguagesContinentsQuery(): QueryResult<GetLanguagesContinents> {
+  return useQuery<GetLanguagesContinents>(GET_LANGUAGES_CONTINENTS)
+}
