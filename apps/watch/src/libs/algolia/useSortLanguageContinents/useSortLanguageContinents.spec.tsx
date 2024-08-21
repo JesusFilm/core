28cba5cfa36b3b8e70c67d7fb3fd9f27f98c2de0
John Geronimo <johnageronimo1@gmail.com>
2024-08-21 22:49:00 +0000

    test: add more cases

--- apps/watch/src/libs/algolia/useSortLanguageContinents/useSortLanguageContinents.spec.tsx
+++ apps/watch/src/libs/algolia/useSortLanguageContinents/useSortLanguageContinents.spec.tsx
@@ -1,5 +1,10 @@
 import { languagesContinents } from '@core/journeys/ui/useLanguagesContinentsQuery/data'
 
+import {
+  GetLanguagesContinents_languages_countryLanguages as CountryLanguage,
+  GetLanguagesContinents_languages as Languages
+} from '../../../../__generated__/GetLanguagesContinents'
+
 import { useSortLanguageContinents } from './useSortLanguageContinents'
 
 describe('useSortLanguageContinents', () => {
@@ -13,5 +18,76 @@ describe('useSortLanguageContinents', () => {
       NorthAmerica: ['English'],
       SouthAmerica: ['Português']
     })
+  })
+
+  it('should handle duplicate languages', () => {
+    const northAmericaCountryLanguage: CountryLanguage = {
+      __typename: 'CountryLanguage',
+      country: {
+        __typename: 'Country',
+        continent: {
+          __typename: 'Continent',
+          id: 'North America'
+        }
+      }
+    }
+
+    const asiaCountryLanguage: CountryLanguage = {
+      __typename: 'CountryLanguage',
+      country: {
+        __typename: 'Country',
+        continent: {
+          __typename: 'Continent',
+          id: 'Asia'
+        }
+      }
+    }
+
+    const languageWithMultipleCountries: Languages[] = [
+      {
+        id: '1',
+        __typename: 'Language',
+        name: [{ __typename: 'LanguageName', value: 'English' }],
+        countryLanguages: [
+          northAmericaCountryLanguage,
+          northAmericaCountryLanguage
+        ]
+      },
+      {
+        id: '2',
+        __typename: 'Language',
+        name: [{ __typename: 'LanguageName', value: 'Mandarin' }],
+        countryLanguages: [asiaCountryLanguage, asiaCountryLanguage]
+      }
+    ]
+
+    const result = useSortLanguageContinents({
+      languages: languageWithMultipleCountries
+    })
+    expect(result).toEqual({
+      'North America': ['English'],
+      Asia: ['Mandarin']
+    })
+  })
+
+  it('should return an empty record if no languages are provided', () => {
+    const result = useSortLanguageContinents({ languages: [] })
+    expect(result).toEqual({})
+  })
+
+  it('should handle languages with no associated countries or continents', () => {
+    const languageWithNoCountries: Languages[] = [
+      {
+        id: '1',
+        __typename: 'Language',
+        name: [{ __typename: 'LanguageName', value: 'English' }],
+        countryLanguages: []
+      }
+    ]
+
+    const result = useSortLanguageContinents({
+      languages: languageWithNoCountries
+    })
+    expect(result).toEqual({})
   })
 })
