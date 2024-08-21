017ae024dc479fdae8107c663b0ecdd67eabbfe9
Mike Allison <dj.mikeallison@gmail.com>
2024-08-21 22:58:59 +0000

    feat: nexus-admin base

--- apps/videos-admin-e2e/src/e2e/example.spec.ts
+++ apps/videos-admin-e2e/src/e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('Authentication', async ({ page }) => {
   await page.goto('/')
 
   // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Nexus Admin')
+  expect(await page.locator('label').innerText()).toContain('Email')
 })
