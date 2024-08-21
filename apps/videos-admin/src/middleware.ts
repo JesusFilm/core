7f9c4d784540a55275643349ee6a80b8fd29ec53
Mike Allison <mike@captainofbass.com>
2024-08-21 16:50:08 -0700

    feat: nexus admin base (#3279)
    Co-authored-by: Kneesal <nisalcottingham@gmail.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ apps/videos-admin/src/middleware.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from 'next/server'
+import createMiddleware from 'next-intl/middleware'
+
+import { auth } from './auth'
+
+const locales = ['en']
+
+const testPathnameRegex = (pages: string[], pathName: string): boolean => {
+  return RegExp(
+    `^(/(${locales.join('|')}))?(${pages
+      .flatMap((p) => (p === '/' ? ['', '/'] : p))
+      .join('|')})/?$`,
+    'i'
+  ).test(pathName)
+}
+
+const intlMiddleware = createMiddleware({
+  locales,
+  defaultLocale: 'en'
+})
+
+const authPage = '/api/auth/signin'
+const unAuthenticatedPages = [authPage]
+
+export default async function middleware(
+  req: NextRequest
+): Promise<NextResponse<unknown>> {
+  if (req.nextUrl.pathname === '/api/auth/signIn') {
+    const signinUrl = req.nextUrl.clone()
+    signinUrl.pathname = '/api/auth/signin'
+    return NextResponse.redirect(signinUrl, { status: 301 })
+  }
+
+  if (req.nextUrl.pathname.startsWith('/api/')) return NextResponse.next()
+
+  const intlResponse = intlMiddleware(req)
+
+  const session = await auth()
+  if (
+    session !== null ||
+    testPathnameRegex(unAuthenticatedPages, req.nextUrl.pathname)
+  ) {
+    return intlResponse
+  }
+
+  const nextUrl = req.nextUrl.clone()
+  nextUrl.pathname = authPage
+
+  return NextResponse.redirect(nextUrl, {
+    // ...intlResponse,
+    status: 301
+  })
+}
+
+export const config = { matcher: ['/((?!_next.*\\..*).*)'] }
