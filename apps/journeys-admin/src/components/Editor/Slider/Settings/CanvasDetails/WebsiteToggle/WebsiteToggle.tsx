e237d0d97f2e44636b482b11612a5fc5c26d4ae1
Siyang <csiyang@yahoo.co.nz>
2024-08-26 04:18:14 +0000

    feat: add hook to update website

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/WebsiteToggle/WebsiteToggle.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/WebsiteToggle/WebsiteToggle.tsx
@@ -4,13 +4,35 @@ import { useTranslation } from 'next-i18next'
 import { ReactElement } from 'react'
 
 import { useJourney } from '@core/journeys/ui/JourneyProvider'
+import { useJourneyUpdateMutation } from '../../../../../../libs/useJourneyUpdateMutation'
 
 export function WebsiteToggle(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const { journey } = useJourney()
+  const [journeyUpdate] = useJourneyUpdateMutation()
 
-  function handleChange(): void {
-    console.log('handleChange')
+  async function handleChange(): Promise<void> {
+    if (journey == null) return
+    const currentMode = journey.website ?? false
+
+    await journeyUpdate({
+      variables: {
+        id: journey.id,
+        input: { website: !currentMode }
+      },
+      optimisticResponse: {
+        journeyUpdate: {
+          id: journey.id,
+          __typename: 'Journey',
+          title: journey.title,
+          description: journey.description,
+          strategySlug: journey.strategySlug,
+          language: journey.language,
+          tags: [],
+          website: !currentMode
+        }
+      }
+    })
   }
 
   return (
