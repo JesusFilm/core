bf8d9aa0df71f462ca44c8797403e1bc1f6daf3c
Kneesal <nisalcottingham@gmail.com>
2024-08-26 01:21:14 +0000

    fix: server handled video list

--- apps/videos-admin/src/components/VideoList/VideoList.tsx
+++ apps/videos-admin/src/components/VideoList/VideoList.tsx
@@ -73,7 +73,7 @@ export function VideoList(): ReactElement {
       showTitle: columnVisibilityModel.title ?? true,
       showSnippet: columnVisibilityModel.description ?? true,
       where: getVideosWhereArgs
-    }
+    },
     fetchPolicy: 'no-cache'
   })
 
