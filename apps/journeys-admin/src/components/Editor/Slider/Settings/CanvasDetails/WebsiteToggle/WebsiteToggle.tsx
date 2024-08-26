54255ffefec69c97ba03fea8ae9b413e4916bd59
Siyang <csiyang@yahoo.co.nz>
2024-08-26 22:52:06 +0000

    feat: command  website update

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/WebsiteToggle/WebsiteToggle.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/WebsiteToggle/WebsiteToggle.tsx
@@ -6,34 +6,63 @@ import { ReactElement } from 'react'
 import { useJourney } from '@core/journeys/ui/JourneyProvider'
 
 import { useJourneyUpdateMutation } from '../../../../../../libs/useJourneyUpdateMutation'
+import { useCommand } from '@core/journeys/ui/CommandProvider'
 
 export function WebsiteToggle(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const { journey } = useJourney()
   const [journeyUpdate] = useJourneyUpdateMutation()
+  const { add } = useCommand()
 
   async function handleChange(): Promise<void> {
     if (journey == null) return
-    const currentMode = journey.website ?? false
 
-    await journeyUpdate({
-      variables: {
-        id: journey.id,
-        input: { website: !currentMode }
+    const currentMode = journey.website ?? false
+    add({
+      parameters: {
+        execute: { currentMode },
+        undo: { currentMode: !currentMode }
       },
-      optimisticResponse: {
-        journeyUpdate: {
-          id: journey.id,
-          __typename: 'Journey',
-          title: journey.title,
-          description: journey.description,
-          strategySlug: journey.strategySlug,
-          language: journey.language,
-          tags: [],
-          website: !currentMode
-        }
+      execute({ currentMode }) {
+        void journeyUpdate({
+          variables: {
+            id: journey.id,
+            input: { website: !currentMode }
+          },
+          optimisticResponse: {
+            journeyUpdate: {
+              id: journey.id,
+              __typename: 'Journey',
+              title: journey.title,
+              description: journey.description,
+              strategySlug: journey.strategySlug,
+              language: journey.language,
+              tags: [],
+              website: !currentMode
+            }
+          }
+        })
       }
     })
+
+    // await journeyUpdate({
+    //   variables: {
+    //     id: journey.id,
+    //     input: { website: !currentMode }
+    //   },
+    //   optimisticResponse: {
+    //     journeyUpdate: {
+    //       id: journey.id,
+    //       __typename: 'Journey',
+    //       title: journey.title,
+    //       description: journey.description,
+    //       strategySlug: journey.strategySlug,
+    //       language: journey.language,
+    //       tags: [],
+    //       website: !currentMode
+    //     }
+    //   }
+    // })
   }
 
   return (
