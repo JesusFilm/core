20f7e7a0205b5d5b062d10b1abb09e818fe2a0cf
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-08-27 16:06:59 +0000

    feat: conditionally render footer buttons

--- libs/journeys/ui/src/components/StepFooter/FooterButtonList/FooterButtonList.tsx
+++ libs/journeys/ui/src/components/StepFooter/FooterButtonList/FooterButtonList.tsx
@@ -1,15 +1,23 @@
 import Stack from '@mui/material/Stack'
 import { ReactElement } from 'react'
 
+import { useJourney } from '../../../libs/JourneyProvider'
+
 import { ReactionButton } from './ReactionButton'
 import { ShareButton } from './ShareButton'
 
 export function FooterButtonList(): ReactElement {
+  const { journey } = useJourney();
+
+  const showShareButton = journey?.showShareButton ?? true
+  const showLikeButton = journey?.showLikeButton ?? true
+  const showDislikeButton = journey?.showDislikeButton ?? true
+
   return (
     <Stack direction="row" gap={2.5} data-testid="StepFooterButtonList">
-      <ShareButton />
-      <ReactionButton variant="thumbsup" />
-      <ReactionButton variant="thumbsdown" />
+      {showShareButton && <ShareButton />}
+      {showLikeButton && <ReactionButton variant="thumbsup" />}
+      {showDislikeButton && <ReactionButton variant="thumbsdown" />}
     </Stack>
   )
 }
