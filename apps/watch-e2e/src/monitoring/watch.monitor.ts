1cf7f3f1c4d83d0f4b6cd038ea50a872a944b0f2
Kiran Chilakamarri <kiran.redhat@gmail.com>
2024-08-28 12:59:35 +1200

    test: update watch monitoring script

--- apps/watch-e2e/src/monitoring/watch.monitor.ts
+++ apps/watch-e2e/src/monitoring/watch.monitor.ts
@@ -3,17 +3,23 @@ import { expect, test } from '@playwright/test'
 /*  
 Check homepage is accessible
 Returns with 200 response
-Get text of a locator and assert it's correct 
+Assert that search bar and see all videos button are visible
 */
 test('Homepage checks', async ({ page }) => {
   const response = await page.goto('https://www.jesusfilm.org/watch')
   expect(response?.status()).toEqual(200)
   await expect(page).toHaveTitle(/Watch | Jesus Film Project/)
-  const videoTitle = page.getByRole('button', {
-    name: 'Jesus Calms the Storm Jesus Calms the Storm Chapter 1:59'
-  })
-
-  await expect(videoTitle).toHaveText('Jesus Calms the StormChapter1:59')
+  await expect(
+    page.getByRole('heading', { name: 'Free Gospel Video Streaming' })
+  ).toBeVisible()
+  await expect(
+    page.getByRole('heading', { name: 'Watch, learn and share the' })
+  ).toBeVisible()
+  await expect(page.getByTestId('SearchBar')).toBeVisible()
+  await expect(page.getByTestId('SeeAllVideos')).toBeVisible()
+  await expect(page.getByTestId('WatchHomePage')).toContainText(
+    'About Our ProjectWith 70% of the world not being able to speak English, there is a huge opportunity for the gospel to spread to unreached places. We have a vision to make it easier to watch, download and share Christian videos with people in their native heart language.Jesus Film Project is a Christian ministry with a vision to reach the world with the gospel of Jesus Christ through evangelistic videos. Watch from over 2000 languages on any device and share it with others.'
+  )
 })
 
 /*  
