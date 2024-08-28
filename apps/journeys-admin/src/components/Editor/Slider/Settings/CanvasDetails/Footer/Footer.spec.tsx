07a293cea135e7bd78847fbdf3efeb2e6e16ae83
Levi Zustiak <levizustiak@gmail.com>
2024-08-28 20:02:56 +0000

    test: footer tests for website mode

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/Footer.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Footer/Footer.spec.tsx
@@ -16,6 +16,9 @@ import {
 import { TestEditorState } from '../../../../../../libs/TestEditorState'
 
 import { Footer } from './Footer'
+import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
+import { defaultJourney } from '@core/journeys/ui/TemplateView/data'
+import { FlagsProvider } from '@core/shared/ui/FlagsProvider'
 
 jest.mock('@mui/material/useMediaQuery', () => ({
   __esModule: true,
@@ -34,24 +37,26 @@ describe('Footer', () => {
     jest.resetAllMocks()
   })
 
-  it('should display Footer attributes', async () => {
+  it('should display Footer attributes for Journey mode', async () => {
     render(
       <MockedProvider>
         <SnackbarProvider>
-          <EditorProvider initialState={state}>
-            <Footer />
-          </EditorProvider>
+          <FlagsProvider flags={{ websiteMode: true }}>
+            <JourneyProvider value={{ journey: defaultJourney }}>
+              <EditorProvider initialState={state}>
+                <Footer />
+              </EditorProvider>
+            </JourneyProvider>
+          </FlagsProvider>
         </SnackbarProvider>
       </MockedProvider>
     )
 
     expect(screen.getByText('Journey Appearance')).toBeInTheDocument()
 
-    await waitFor(() =>
-      expect(
-        screen.getByRole('button', { name: 'Hosted By' })
-      ).toBeInTheDocument()
-    )
+    const toggleBtn = screen.getByRole('button', { name: 'Journey' })
+    expect(toggleBtn).toBeInTheDocument()
+    expect(toggleBtn).toHaveAttribute('aria-pressed', 'true')
 
     const reactions = await waitFor(() =>
       screen.getByRole('button', { name: 'Reactions' })
@@ -66,6 +71,36 @@ describe('Footer', () => {
 
     expect(reactions).toBeInTheDocument()
     expect(details).toBeInTheDocument()
+    expect(chat).toBeInTheDocument()
+  })
+
+  it('should display Footer attributes for Website mode', async () => {
+    render(
+      <MockedProvider>
+        <SnackbarProvider>
+          <FlagsProvider flags={{ websiteMode: true }}>
+            <JourneyProvider
+              value={{ journey: { ...defaultJourney, website: true } }}
+            >
+              <EditorProvider initialState={state}>
+                <Footer />
+              </EditorProvider>
+            </JourneyProvider>
+          </FlagsProvider>
+        </SnackbarProvider>
+      </MockedProvider>
+    )
+
+    expect(screen.getByText('Journey Appearance')).toBeInTheDocument()
+
+    const toggleBtn = screen.getByRole('button', { name: 'Website' })
+    expect(toggleBtn).toBeInTheDocument()
+    expect(toggleBtn).toHaveAttribute('aria-pressed', 'true')
+
+    const chat = await waitFor(() =>
+      screen.getByRole('button', { name: 'Chat Widget' })
+    )
+
     expect(chat).toBeInTheDocument()
   })
 
