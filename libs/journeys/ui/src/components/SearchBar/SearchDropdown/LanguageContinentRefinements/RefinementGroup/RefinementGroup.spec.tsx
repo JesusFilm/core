69ed3277c1600243be33d9cd6bff649673010a4a
John Geronimo <johnageronimo1@gmail.com>
2024-08-30 05:44:40 +0000

    test: update tests

--- libs/journeys/ui/src/components/SearchBar/SearchDropdown/LanguageContinentRefinements/RefinementGroup/RefinementGroup.spec.tsx
+++ libs/journeys/ui/src/components/SearchBar/SearchDropdown/LanguageContinentRefinements/RefinementGroup/RefinementGroup.spec.tsx
@@ -14,19 +14,37 @@ describe('RefinementGroup', () => {
   } as unknown as RefinementListRenderState
 
   it('should have languages header', () => {
-    render(<RefinementGroup title="Languages" refinement={useRefinementList} />)
+    render(
+      <RefinementGroup
+        title="Languages"
+        refinement={useRefinementList}
+        handleSelectedContinent={jest.fn()}
+      />
+    )
     expect(screen.getByText('Languages')).toBeInTheDocument()
   })
 
   it('should have languages listed', () => {
-    render(<RefinementGroup title="Languages" refinement={useRefinementList} />)
+    render(
+      <RefinementGroup
+        title="Languages"
+        refinement={useRefinementList}
+        handleSelectedContinent={jest.fn()}
+      />
+    )
     expect(screen.getByText('English')).toBeInTheDocument()
     expect(screen.getByText('Spanish, Latin American')).toBeInTheDocument()
     expect(screen.getByText('Chinese, Mandarin')).toBeInTheDocument()
   })
 
   it('should refine when langauge selected', () => {
-    render(<RefinementGroup title="Languages" refinement={useRefinementList} />)
+    render(
+      <RefinementGroup
+        title="Languages"
+        refinement={useRefinementList}
+        handleSelectedContinent={jest.fn()}
+      />
+    )
     fireEvent.click(screen.getByText('Cantonese'))
     expect(refine).toHaveBeenCalled()
   })
@@ -37,9 +55,72 @@ describe('RefinementGroup', () => {
       refine
     } as unknown as RefinementListRenderState
     render(
-      <RefinementGroup title="Languages" refinement={emptyRefinementList} />
+      <RefinementGroup
+        title="Languages"
+        refinement={emptyRefinementList}
+        handleSelectedContinent={jest.fn()}
+      />
     )
     expect(screen.getByText('Languages')).toBeInTheDocument()
     expect(screen.queryByRole('label')).not.toBeInTheDocument()
+  })
+
+  it('should handle selected continent on click', () => {
+    const handleSelectedContinent = jest.fn()
+    render(
+      <RefinementGroup
+        title="Languages"
+        refinement={useRefinementList}
+        handleSelectedContinent={handleSelectedContinent}
+      />
+    )
+    fireEvent.click(screen.getByText('Cantonese'))
+    expect(handleSelectedContinent).toHaveBeenCalled()
+  })
+
+  it('should check the checkbox if title, continent, and refinement match', () => {
+    const useRefinementListWithRefinedValue = {
+      items: [
+        {
+          label: 'Cantonese',
+          value: 'Cantonese',
+          isRefined: true
+        }
+      ],
+      refine
+    } as unknown as RefinementListRenderState
+
+    render(
+      <RefinementGroup
+        title="Asia"
+        refinement={useRefinementListWithRefinedValue}
+        selectedContinent="Asia"
+        handleSelectedContinent={jest.fn()}
+      />
+    )
+    expect(screen.getByRole('checkbox', { name: 'Cantonese' })).toBeChecked()
+  })
+
+  it('should disable the checkbox if title and continent match but refinement does not', () => {
+    const useRefinementListWithRefinedValue = {
+      items: [
+        {
+          label: 'Cantonese',
+          value: 'Cantonese',
+          isRefined: true
+        }
+      ],
+      refine
+    } as unknown as RefinementListRenderState
+
+    render(
+      <RefinementGroup
+        title="Asia"
+        refinement={useRefinementListWithRefinedValue}
+        selectedContinent="Europe"
+        handleSelectedContinent={jest.fn()}
+      />
+    )
+    expect(screen.getByRole('checkbox', { name: 'Cantonese' })).toBeDisabled()
   })
 })
