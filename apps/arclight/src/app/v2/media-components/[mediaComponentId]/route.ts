f12080e5e438f483337c70cb96bd96a26832305b
Tanner <tanner.fleming@gmail.com>
2024-09-03 19:33:27 +0000

    feat: add bible citations

--- apps/arclight/src/app/v2/media-components/[mediaComponentId]/route.ts
+++ apps/arclight/src/app/v2/media-components/[mediaComponentId]/route.ts
@@ -31,6 +31,13 @@ const GET_VIDEO = graphql(`
       studyQuestions {
         value
       }
+      bibleCitations {
+        osisId
+        chapterStart
+        verseStart
+        chapterEnd
+        verseEnd
+      }
       childrenCount
       variant {
         duration
@@ -96,9 +103,14 @@ export async function GET(
         video.variant?.downloads?.find(({ quality }) => quality === 'high')
           ?.size ?? 0
     },
-    // TODO: Needs new field in the schema
-    bibleCitations: [],
-    primaryLanguageId: video.primaryLanguageId,
+    bibleCitations: video.bibleCitations.map((citation) => ({
+      osisBibleBook: citation.osisId,
+      chapterStart: citation.chapterStart,
+      verseStart: citation.verseStart,
+      chapterEnd: citation.chapterEnd,
+      verseEnd: citation.verseEnd
+    })),
+    primaryLanguageId: Number(video.primaryLanguageId),
     title: video.title[0].value,
     shortDescription: video.snippet[0].value,
     longDescription: video.description[0].value,
--- apps/arclight/src/app/v2/media-components/route.ts
+++ apps/arclight/src/app/v2/media-components/route.ts
@@ -38,6 +38,13 @@ const GET_VIDEOS = graphql(`
       studyQuestions {
         value
       }
+      bibleCitations {
+        osisId
+        chapterStart
+        verseStart
+        chapterEnd
+        verseEnd
+      }
       childrenCount
       variant {
         duration
@@ -95,9 +102,14 @@ export async function GET(request: NextRequest): Promise<Response> {
         video.variant?.downloads?.find(({ quality }) => quality === 'high')
           ?.size ?? 0
     },
-    // TODO: Needs new field in the schema
-    bibleCitations: [],
-    primaryLanguageId: video.primaryLanguageId,
+    bibleCitations: video.bibleCitations.map((citation) => ({
+      osisBibleBook: citation.osisId,
+      chapterStart: citation.chapterStart,
+      verseStart: citation.verseStart,
+      chapterEnd: citation.chapterEnd,
+      verseEnd: citation.verseEnd
+    })),
+    primaryLanguageId: Number(video.primaryLanguageId),
     title: video.title[0].value,
     shortDescription: video.snippet[0].value,
     longDescription: video.description[0].value,
