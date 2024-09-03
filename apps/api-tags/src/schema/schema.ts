4adcb3a5b6cdc629203caf6768ce81f21e56e8c0
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-09-03 14:58:04 +1200

    chore: migrate tags gql into api-media (#3362)

--- apps/api-tags/src/schema/schema.ts
+++ apps/api-tags/src/schema/schema.ts
@@ -1,9 +1,6 @@
 // Developers must add an import for every resolver
 // and object type in the schema
 
-import './language'
-import './tag'
-
 import { builder } from './builder'
 
 export const schema = builder.toSubGraphSchema({
