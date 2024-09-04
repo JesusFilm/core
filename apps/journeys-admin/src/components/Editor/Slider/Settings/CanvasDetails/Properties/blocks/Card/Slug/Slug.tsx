d10b5d50f948b95da9646f2f40bfe7a49be84c39
Siyang <csiyang@yahoo.co.nz>
2024-09-04 03:57:14 +0000

    feat: add copy button

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Card/Slug/Slug.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Card/Slug/Slug.tsx
@@ -1,12 +1,16 @@
 import { gql, useMutation } from '@apollo/client'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 import { useTranslation } from 'next-i18next'
 import { useSnackbar } from 'notistack'
 import { ReactElement } from 'react'
 
 import { useCommand } from '@core/journeys/ui/CommandProvider'
 import { useEditor } from '@core/journeys/ui/EditorProvider'
+import { useJourney } from '@core/journeys/ui/JourneyProvider'
 
 import { StepBlockSlugUpdate } from '../../../../../../../../../../__generated__/StepBlockSlugUpdate'
+import { useCustomDomainsQuery } from '../../../../../../../../../libs/useCustomDomainsQuery'
 import { TextFieldForm } from '../../../../../../../../TextFieldForm'
 
 export const STEP_BLOCK_SLUG_UPDATE = gql`
@@ -21,10 +25,16 @@ export const STEP_BLOCK_SLUG_UPDATE = gql`
 export function Slug(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const { add } = useCommand()
+  const { journey } = useJourney()
   const { enqueueSnackbar } = useSnackbar()
   const {
     state: { selectedStep }
   } = useEditor()
+  const { hostname } = useCustomDomainsQuery({
+    variables: { teamId: journey?.team?.id ?? '' },
+    skip: journey?.team?.id == null
+  })
+
   const [stepBlockUpdate] = useMutation<StepBlockSlugUpdate>(
     STEP_BLOCK_SLUG_UPDATE
   )
@@ -71,12 +81,32 @@ export function Slug(): ReactElement {
     })
   }
 
+  async function handleCopyClick(): Promise<void> {
+    if (slug == null || journey == null) return
+    const cardUrl = `${
+      hostname != null
+        ? `https://${hostname}`
+        : process.env.NEXT_PUBLIC_JOURNEYS_URL ?? 'https://your.nextstep.is'
+    }/${journey.slug}/${slug}`
+
+    await navigator.clipboard.writeText(cardUrl)
+    enqueueSnackbar(`${t('Copied to clipboard:')} ${cardUrl}`, {
+      variant: 'success',
+      preventDuplicate: true
+    })
+  }
+
   return (
-    <TextFieldForm
-      id="StepSlugEdit"
-      label={t('Card URL edit')}
-      initialValue={slug}
-      onSubmit={async (value) => handleUpdate(value)}
-    />
+    <Stack spacing={4} sx={{ px: 4 }}>
+      <TextFieldForm
+        id="StepSlugEdit"
+        label={t('Card URL edit')}
+        initialValue={slug}
+        onSubmit={async (value) => handleUpdate(value)}
+      />
+      <Button variant="contained" onClick={handleCopyClick}>
+        {t('Copy Card URL')}
+      </Button>
+    </Stack>
   )
 }
