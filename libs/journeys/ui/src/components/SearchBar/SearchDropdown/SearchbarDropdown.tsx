d651806c1f6588d6f4460c674185e719a4951f11
scottkelso <scottkelso07@hotmail.com>
2024-09-04 21:59:06 +0000

    feat: add icons to tabs

--- libs/journeys/ui/src/components/SearchBar/SearchDropdown/SearchbarDropdown.tsx
+++ libs/journeys/ui/src/components/SearchBar/SearchDropdown/SearchbarDropdown.tsx
@@ -5,6 +5,8 @@ import Tabs from '@mui/material/Tabs'
 import { RefinementListRenderState } from 'instantsearch.js/es/connectors/refinement-list/connectRefinementList'
 import { ReactElement, useState } from 'react'
 
+import Globe1 from '@core/shared/ui/icons/Globe1'
+import Search1 from '@core/shared/ui/icons/Search1'
 import { TabPanel } from '@core/shared/ui/TabPanel'
 
 import { useLanguagesContinentsQuery } from '../../../libs/useLanguagesContinentsQuery'
@@ -60,7 +62,7 @@ export function SearchbarDropdown({
       <Box
         borderRadius={3}
         boxShadow="0px 4px 4px 0px #00000040"
-        sx={{ p: 8, bgcolor: 'background.paper', mt: 3 }}
+        sx={{ px: 8, pb: 8, bgcolor: 'background.paper', mt: 3 }}
         color="text.primary"
       >
         <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 5 }}>
@@ -69,8 +71,16 @@ export function SearchbarDropdown({
             onChange={handleTabChange}
             aria-label="Searchbar dropdown"
           >
-            <Tab label="Search Suggestions" />
-            <Tab label="Languages" />
+            <Tab
+              icon={<Search1 sx={{ marginRight: 5 }} />}
+              iconPosition="start"
+              label="Search Suggestions"
+            />
+            <Tab
+              icon={<Globe1 sx={{ marginRight: 5 }} />}
+              iconPosition="start"
+              label="Languages"
+            />
           </Tabs>
         </Box>
         <TabPanel name="suggestions-tab" value={tabValue} index={0}>
