ea457fc20985294fbf521a30865b0142bb965a8d
Dan <dan.chae@tandem.org.nz>
2024-09-05 03:45:44 +0000

    test: cases for responsesitem

--- apps/journeys-admin/src/components/Editor/Toolbar/Items/ResponsesItem/ResponsesItem.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/Items/ResponsesItem/ResponsesItem.spec.tsx
@@ -1,15 +1,30 @@
-import { MockedProvider } from '@apollo/client/testing'
-import { render } from '@testing-library/react'
+import { MockedProvider, MockedResponse } from '@apollo/client/testing'
+import { render, screen, waitFor } from '@testing-library/react'
 import { SnackbarProvider } from 'notistack'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import { TeamProvider } from '@core/journeys/ui/TeamProvider'
 import { defaultJourney } from '@core/journeys/ui/TemplateView/data'
+
+import { GetJourneyVisitorsCount } from '../../../../../../__generated__/GetJourneyVisitorsCount'
+import { GET_JOURNEY_VISITORS_COUNT } from '../../../../../../pages/journeys/[journeyId]/reports/visitors'
+
 import { ResponsesItem } from '.'
+import { NextRouter, useRouter } from 'next/router'
+
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn()
+}))
+
+const mockedUseRouter = useRouter as jest.MockedFunction<typeof useRouter>
 
 describe('ResponsesItem', () => {
-  it('should link to journey reports page as a list item', async () => {
-    const { getByRole } = render(
+  it('should link to journey visitors page with correct params', async () => {
+    mockedUseRouter.mockReturnValue({
+      query: { journeyId: 'journey-id' }
+    } as unknown as NextRouter)
+    render(
       <SnackbarProvider>
         <MockedProvider mocks={[]}>
           <TeamProvider>
@@ -25,15 +40,17 @@ describe('ResponsesItem', () => {
         </MockedProvider>
       </SnackbarProvider>
     )
-    expect(getByRole('menuitem', { name: 'Analytics' })).toBeInTheDocument()
-    expect(getByRole('menuitem', { name: 'Analytics' })).toHaveAttribute(
+    expect(screen.getByRole('menuitem', { name: 'Responses' })).toHaveAttribute(
       'href',
-      '/journeys/journey-id/reports'
+      '/journeys/journey-id/reports/visitors?withSubmittedText=true'
     )
   })
 
-  it('should link to journey reports page as an icon button', async () => {
-    const { getByRole } = render(
+  it('should link to journey visitors page as an icon button', async () => {
+    mockedUseRouter.mockReturnValue({
+      query: { journeyId: 'journey-id' }
+    } as unknown as NextRouter)
+    render(
       <SnackbarProvider>
         <MockedProvider mocks={[]}>
           <TeamProvider>
@@ -49,10 +66,48 @@ describe('ResponsesItem', () => {
         </MockedProvider>
       </SnackbarProvider>
     )
-    expect(getByRole('link', { name: 'Analytics' })).toBeInTheDocument()
-    expect(getByRole('link', { name: 'Analytics' })).toHaveAttribute(
+    expect(screen.getByRole('link', { name: 'Responses' })).toHaveAttribute(
       'href',
-      '/journeys/journey-id/reports'
+      '/journeys/journey-id/reports/visitors?withSubmittedText=true'
     )
+  })
+
+  it('should display visitor count next to button', async () => {
+    mockedUseRouter.mockReturnValue({
+      query: { journeyId: 'journey-id' }
+    } as unknown as NextRouter)
+
+    const getVisitorCountMock: MockedResponse<GetJourneyVisitorsCount> = {
+      request: {
+        query: GET_JOURNEY_VISITORS_COUNT,
+        variables: {
+          filter: { journeyId: 'journey-id', hasTextResponse: true }
+        }
+      },
+      result: {
+        data: { journeyVisitorCount: 153 }
+      }
+    }
+    render(
+      <SnackbarProvider>
+        <MockedProvider mocks={[getVisitorCountMock]}>
+          <TeamProvider>
+            <JourneyProvider
+              value={{
+                journey: defaultJourney,
+                variant: 'admin'
+              }}
+            >
+              <ResponsesItem variant="icon-button" />
+            </JourneyProvider>
+          </TeamProvider>
+        </MockedProvider>
+      </SnackbarProvider>
+    )
+    expect(screen.getByRole('link', { name: 'Responses' })).toHaveAttribute(
+      'href',
+      '/journeys/journey-id/reports/visitors?withSubmittedText=true'
+    )
+    await waitFor(() => expect(screen.getByText('153')).toBeInTheDocument())
   })
 })
