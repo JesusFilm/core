7c94729411e3b71f994e10584087162efd48c135
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-09-05 00:37:15 +0000

    chore: add icons

--- apps/videos-admin/src/components/NavbarBreadcrumbs/NavbarBreadcrumbs.tsx
+++ apps/videos-admin/src/components/NavbarBreadcrumbs/NavbarBreadcrumbs.tsx
@@ -1,4 +1,7 @@
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded'
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded'
+import VideoLibraryRoundedIcon from '@mui/icons-material/VideoLibraryRounded'
 import Breadcrumbs, { breadcrumbsClasses } from '@mui/material/Breadcrumbs'
 import MuiLink from '@mui/material/Link'
 import { styled } from '@mui/material/styles'
@@ -6,7 +9,7 @@ import Typography from '@mui/material/Typography'
 import Link from 'next/link'
 import { useParams, usePathname } from 'next/navigation'
 import { useTranslations } from 'next-intl'
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode } from 'react'
 
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   margin: theme.spacing(1, 0),
@@ -19,16 +22,30 @@ const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   }
 }))
 
+interface Label {
+  value: string
+  icon?: ReactNode
+}
+
 export function NavbarBreadcrumbs(): ReactElement {
   const t = useTranslations()
   const paths = usePathname()
   const pathNames = paths?.split('/').filter((path) => path) ?? []
   const params = useParams()
 
-  const labels = {
-    [params?.locale.toString() ?? 'en']: t('Dashboard'),
-    videos: t('Video Library'),
-    settings: t('Settings')
+  const labels: { [key: string]: Label } = {
+    [params?.locale.toString() ?? 'en']: {
+      icon: <HomeRoundedIcon fontSize="inherit" />,
+      value: t('Dashboard')
+    },
+    videos: {
+      icon: <VideoLibraryRoundedIcon fontSize="inherit" />,
+      value: t('Video Library')
+    },
+    settings: {
+      icon: <SettingsRoundedIcon fontSize="inherit" />,
+      value: t('Settings')
+    }
   }
 
   return (
@@ -38,18 +55,32 @@ export function NavbarBreadcrumbs(): ReactElement {
     >
       {pathNames.map((link, index) => {
         const href = `/${pathNames.slice(0, index + 1).join('/')}`
-        const itemLink =
-          labels[link] ?? link[0].toUpperCase() + link.slice(1, link.length)
+        const itemLink = labels[link] ?? {
+          value: link[0].toUpperCase() + link.slice(1, link.length)
+        }
         return index + 1 < pathNames.length ? (
-          <MuiLink component={Link} href={href} key={index}>
-            {itemLink}
+          <MuiLink
+            component={Link}
+            href={href}
+            key={index}
+            sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}
+          >
+            {itemLink.icon}
+            {itemLink.value}
           </MuiLink>
         ) : (
           <Typography
             variant="body1"
-            sx={{ color: 'text.primary', fontWeight: 600 }}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'text.primary',
+              fontWeight: 600,
+              gap: 0.5
+            }}
           >
-            {itemLink}
+            {itemLink.icon}
+            {itemLink.value}
           </Typography>
         )
       })}
