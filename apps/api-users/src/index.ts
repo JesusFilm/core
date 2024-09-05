4bb48eb37cf2934d91bfbefcca913a0f18fa6c83
Mike Allison <dj.mikeallison@gmail.com>
2024-09-05 20:06:54 +0000

    feat: api-users yoga

--- apps/api-users/src/index.ts
+++ apps/api-users/src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'node:http'
 
 import { yoga } from './yoga'
+import './workers/server'
 
 const port = 4002
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
--- apps/api-users/src/workers/server.ts
+++ apps/api-users/src/workers/server.ts
@@ -1,22 +1,15 @@
-import { Job, Queue, Worker } from 'bullmq'
+import { Job, Worker } from 'bullmq'
 import { Logger } from 'pino'
 
 import { connection } from './lib/connection'
 import { logger } from './lib/logger'
 
-const ONE_HOUR = 3600
-const ONE_DAY = 86_400
-
 function run({
   service,
-  queueName,
-  jobName,
-  repeat
+  queueName
 }: {
   service: (job: Job, logger?: Logger) => Promise<void>
   queueName: string
-  jobName: string
-  repeat?: string
 }): void {
   // eslint-disable-next-line no-new
   new Worker(queueName, job, {
@@ -24,8 +17,6 @@ function run({
   })
 
   async function job(job: Job): Promise<void> {
-    if (job.name !== jobName) return
-
     const childLogger = logger.child({
       queue: queueName,
       jobId: job.id
