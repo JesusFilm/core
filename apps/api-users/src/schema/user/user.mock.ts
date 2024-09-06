2b66275362de3fc9c3c601518dde7e61a6cec0ed
Mike Allison <mike@captainofbass.com>
2024-09-05 17:19:44 -0700

    feat: api users yoga (#3315)
    Co-authored-by: Tataihono Nikora <tataihono.nikora@gmail.com>

---
+++ apps/api-users/src/schema/user/user.mock.ts
@@ -0,0 +1,11 @@
+export const user = {
+  id: '1',
+  userId: '1',
+  createdAt: new Date('2021-01-01T00:00:00.000Z'),
+  firstName: 'Amin',
+  lastName: 'One',
+  email: 'amin@email.com',
+  imageUrl: 'https://bit.ly/3Gth4',
+  emailVerified: false,
+  superAdmin: false
+}
