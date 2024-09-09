0fbcaf4d6ac317bbaf26de5cbae117540e9c9c48
scottkelso <scottkelso07@hotmail.com>
2024-09-09 21:42:22 +0000

    feat: set suggestions to most relevant languages

--- libs/journeys/ui/src/components/SearchBar/SearchDropdown/Suggestions/Suggestions.tsx
+++ libs/journeys/ui/src/components/SearchBar/SearchDropdown/Suggestions/Suggestions.tsx
@@ -1,8 +1,13 @@
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 import { RefinementListRenderState } from 'instantsearch.js/es/connectors/refinement-list/connectRefinementList'
+import { useTranslation } from 'next-i18next'
 import { ReactElement } from 'react'
 import { useSearchBox } from 'react-instantsearch'
+
+import { normalizeLanguage } from '../../../../libs/algolia/normalizeLanguage'
+import { capitalizeFirstLetter } from '../../../../libs/algolia/normalizeLanguage/normalizeLanguage'
 
 import { Suggestion } from './Suggestion'
 
@@ -10,8 +15,12 @@ interface SuggestionsProps {
   refinements: RefinementListRenderState
 }
 
+const MAX_QUERY_SUGGESTIONS = 2
+
 export function Suggestions({ refinements }: SuggestionsProps): ReactElement {
-  const { query } = useSearchBox()
+  const { t } = useTranslation('apps-watch')
+
+  const { query, refine: refineQuery } = useSearchBox()
   const { items, refine: refineLanguage } = refinements
 
   function refineLanguages(languagesToRefine: string[]): void {
@@ -28,22 +37,52 @@ export function Suggestions({ refinements }: SuggestionsProps): ReactElement {
     refineLanguages(suggestionParts.slice(1))
   }
 
+  const unrefinedLanguages = items
+    .filter((item) => !item.isRefined)
+    .slice(0, MAX_QUERY_SUGGESTIONS)
+
+  const firstLanguage = capitalizeFirstLetter(
+    normalizeLanguage(unrefinedLanguages[0].value)
+  )
+  const secondLanguage = capitalizeFirstLetter(
+    normalizeLanguage(unrefinedLanguages[1].value)
+  )
+
   return (
     <Box width="100%">
       <Box color="text.primary">
         <Stack spacing={1}>
-          <Suggestion
-            query={query !== '' ? query : 'Jesus'}
-            filters={['English']}
-            handleClick={() => selectSuggestion('Jesus in English')}
-          />
-          <Suggestion
-            query={query !== '' ? query : 'Jesus'}
-            filters={['English', 'Spanish']}
-            handleClick={() =>
-              selectSuggestion('Jesus in English and Spanish, Latin American')
-            }
-          />
+          {unrefinedLanguages.length === 0 && (
+            <Typography>
+              {t('There are no suggestions for this search')}
+            </Typography>
+          )}
+          {unrefinedLanguages.length > 0 && (
+            <Suggestion
+              query={query !== '' ? query : 'Jesus'}
+              filters={[firstLanguage]}
+              handleClick={() => {
+                if (query === '') {
+                  refineQuery('Jesus')
+                }
+                selectSuggestion(`Jesus in ${firstLanguage}`)
+              }}
+            />
+          )}
+          {unrefinedLanguages.length > 1 && (
+            <Suggestion
+              query={query !== '' ? query : 'Jesus'}
+              filters={[firstLanguage, secondLanguage]}
+              handleClick={() => {
+                if (query === '') {
+                  refineQuery('Jesus')
+                }
+                selectSuggestion(
+                  `Jesus in ${firstLanguage} and ${secondLanguage}`
+                )
+              }}
+            />
+          )}
         </Stack>
       </Box>
     </Box>
--- libs/journeys/ui/src/libs/algolia/normalizeLanguage/normalizeLanguage.ts
+++ libs/journeys/ui/src/libs/algolia/normalizeLanguage/normalizeLanguage.ts
@@ -1,3 +1,7 @@
 export function normalizeLanguage(language: string): string {
   return language.toLowerCase().split(',')[0].trim()
 }
+
+export const capitalizeFirstLetter = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
