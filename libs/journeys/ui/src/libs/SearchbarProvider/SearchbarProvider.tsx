0a21dc2f7ab9e620c31ecd3f681ed9edadf26b4f
John Geronimo <johnageronimo1@gmail.com>
2024-09-09 00:02:40 +0000

    fix: clean up reducer

--- libs/journeys/ui/src/libs/SearchbarProvider/SearchbarProvider.tsx
+++ libs/journeys/ui/src/libs/SearchbarProvider/SearchbarProvider.tsx
@@ -42,67 +42,87 @@ type SearchbarAction =
   | SetDefaultLanguageContinentAction
   | RemoveLanguageContinentsAction
 
+function handleSelectLanguageContinent(
+  state: SearchBarState,
+  action: SelectLanguageContinentAction
+): SearchBarState {
+  const { continent, language, isSelected } = action
+  const currentLanguages = state.continentLanguages[continent] ?? []
+  const updatedLanguages = isSelected
+    ? [...currentLanguages, language]
+    : currentLanguages.filter((lang) => lang !== language)
+  return {
+    ...state,
+    continentLanguages: {
+      ...state.continentLanguages,
+      [continent]: updatedLanguages
+    }
+  }
+}
+
+function handleSetDefaultLanguageContinent(
+  state: SearchBarState,
+  action: SetDefaultLanguageContinentAction
+): SearchBarState {
+  const { continents, refinedItems } = action
+
+  const updatedContinentLanguages = refinedItems.reduce(
+    (acc, item) => {
+      const continent = Object.keys(continents).find((continent) =>
+        continents[continent].includes(item)
+      )
+
+      if (continent != null) {
+        if (!(continent in acc)) {
+          acc[continent] = []
+        }
+        if (!acc[continent].includes(item)) {
+          acc[continent] = [...acc[continent], item]
+        }
+      }
+
+      return acc
+    },
+    { ...state.continentLanguages }
+  )
+
+  if (Object.keys(updatedContinentLanguages).length > 0) {
+    return {
+      ...state,
+      continentLanguages: updatedContinentLanguages
+    }
+  }
+
+  return state
+}
+
+function handleRemoveLanguageContinents(
+  state: SearchBarState,
+  action: RemoveLanguageContinentsAction
+): SearchBarState {
+  const { language } = action
+  const updatedLanguages = mapValues(state.continentLanguages, (languages) =>
+    filter(languages, (lang) => lang !== language)
+  )
+  return {
+    ...state,
+    continentLanguages: updatedLanguages
+  }
+}
+
 const searchbarReducer = (
   state: SearchBarState,
   action: SearchbarAction
 ): SearchBarState => {
   switch (action.type) {
     case 'SelectLanguageContinent': {
-      const { continent, language, isSelected } = action
-      const currentLanguages = state.continentLanguages[continent] ?? []
-      const updatedLanguages = isSelected
-        ? [...currentLanguages, language]
-        : currentLanguages.filter((lang) => lang !== language)
-      return {
-        ...state,
-        continentLanguages: {
-          ...state.continentLanguages,
-          [continent]: updatedLanguages
-        }
-      }
+      return handleSelectLanguageContinent(state, action)
     }
     case 'SetDefaultLanguageContinent': {
-      const { continents, refinedItems } = action
-
-      const updatedContinentLanguages = refinedItems.reduce(
-        (acc, item) => {
-          const continent = Object.keys(continents).find((continent) =>
-            continents[continent].includes(item)
-          )
-
-          if (continent != null) {
-            if (!(continent in acc)) {
-              acc[continent] = []
-            }
-            if (!acc[continent].includes(item)) {
-              acc[continent] = [...acc[continent], item]
-            }
-          }
-
-          return acc
-        },
-        { ...state.continentLanguages }
-      )
-
-      if (Object.keys(updatedContinentLanguages).length > 0) {
-        return {
-          ...state,
-          continentLanguages: updatedContinentLanguages
-        }
-      }
-
-      return state
+      return handleSetDefaultLanguageContinent(state, action)
     }
     case 'RemoveLanguageContinents': {
-      const { language } = action
-      const updatedLanguages = mapValues(
-        state.continentLanguages,
-        (languages) => filter(languages, (lang) => lang !== language)
-      )
-      return {
-        ...state,
-        continentLanguages: updatedLanguages
-      }
+      return handleRemoveLanguageContinents(state, action)
     }
     default:
       return state
