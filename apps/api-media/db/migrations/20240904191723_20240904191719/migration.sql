bf7a04523ad937f256ef3724762afb756049f38b
Tanner <tanner.fleming@gmail.com>
2024-09-10 18:16:13 +0000

    chore: consolidate migratiens

--- apps/api-media/db/migrations/20240904191723_20240904191719/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "VideoVariant" ADD COLUMN     "dash" TEXT;
-
--- AlterTable
-ALTER TABLE "VideoVariantDownload" ADD COLUMN     "height" INTEGER,
-ADD COLUMN     "width" INTEGER;
--- apps/api-media/db/migrations/20240910181459_20240910181456/migration.sql
+++ apps/api-media/db/migrations/20240910181459_20240910181456/migration.sql
@@ -4,3 +4,11 @@ ADD COLUMN     "mobileCinematicLow" TEXT,
 ADD COLUMN     "mobileCinematicVeryLow" TEXT,
 ADD COLUMN     "thumbnail" TEXT,
 ADD COLUMN     "videoStill" TEXT;
+
+-- AlterTable
+ALTER TABLE "VideoVariant" ADD COLUMN     "dash" TEXT,
+ADD COLUMN     "share" TEXT;
+
+-- AlterTable
+ALTER TABLE "VideoVariantDownload" ADD COLUMN     "height" INTEGER,
+ADD COLUMN     "width" INTEGER;
