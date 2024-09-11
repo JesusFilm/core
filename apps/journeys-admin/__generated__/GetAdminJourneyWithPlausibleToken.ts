8ad6be56b03946cddce9ea327e9acb781f73fa2a
Levi Zustiak <levizustiak@gmail.com>
2024-09-11 17:49:48 +0000

    chore: generate types

--- apps/journeys-admin/__generated__/GetAdminJourneyWithPlausibleToken.ts
+++ apps/journeys-admin/__generated__/GetAdminJourneyWithPlausibleToken.ts
@@ -545,6 +545,31 @@ export interface GetAdminJourneyWithPlausibleToken_journey_tags {
   name: GetAdminJourneyWithPlausibleToken_journey_tags_name[];
 }
 
+export interface GetAdminJourneyWithPlausibleToken_journey_menuStepBlock {
+  __typename: "StepBlock";
+  id: string;
+  parentBlockId: string | null;
+  parentOrder: number | null;
+  /**
+   * locked will be set to true if the user should not be able to manually
+   * advance to the next step.
+   */
+  locked: boolean;
+  /**
+   * nextBlockId contains the preferred block to navigate to, users will have to
+   * manually set the next block they want to card to navigate to
+   */
+  nextBlockId: string | null;
+  /**
+   * Slug should be unique amongst all blocks
+   * (server will throw BAD_USER_INPUT error if not)
+   * If not required will use the current block id
+   * If the generated slug is not unique the uuid will be placed
+   * at the end of the slug guaranteeing uniqueness
+   */
+  slug: string | null;
+}
+
 export interface GetAdminJourneyWithPlausibleToken_journey {
   __typename: "Journey";
   id: string;
@@ -586,6 +611,7 @@ export interface GetAdminJourneyWithPlausibleToken_journey {
    */
   displayTitle: string | null;
   menuButtonIcon: JourneyMenuButtonIcon | null;
+  menuStepBlock: GetAdminJourneyWithPlausibleToken_journey_menuStepBlock | null;
   /**
    * used in a plausible share link to embed report
    */
--- libs/journeys/ui/src/libs/JourneyProvider/__generated__/JourneyFields.ts
+++ libs/journeys/ui/src/libs/JourneyProvider/__generated__/JourneyFields.ts
@@ -545,6 +545,31 @@ export interface JourneyFields_tags {
   name: JourneyFields_tags_name[];
 }
 
+export interface JourneyFields_menuStepBlock {
+  __typename: "StepBlock";
+  id: string;
+  parentBlockId: string | null;
+  parentOrder: number | null;
+  /**
+   * locked will be set to true if the user should not be able to manually
+   * advance to the next step.
+   */
+  locked: boolean;
+  /**
+   * nextBlockId contains the preferred block to navigate to, users will have to
+   * manually set the next block they want to card to navigate to
+   */
+  nextBlockId: string | null;
+  /**
+   * Slug should be unique amongst all blocks
+   * (server will throw BAD_USER_INPUT error if not)
+   * If not required will use the current block id
+   * If the generated slug is not unique the uuid will be placed
+   * at the end of the slug guaranteeing uniqueness
+   */
+  slug: string | null;
+}
+
 export interface JourneyFields {
   __typename: "Journey";
   id: string;
@@ -586,4 +611,5 @@ export interface JourneyFields {
    */
   displayTitle: string | null;
   menuButtonIcon: JourneyMenuButtonIcon | null;
+  menuStepBlock: JourneyFields_menuStepBlock | null;
 }
