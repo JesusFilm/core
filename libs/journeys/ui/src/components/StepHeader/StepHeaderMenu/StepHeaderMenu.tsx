a308d0c5a22ba570881e307ec158a9e37b6b55dd
Levi Zustiak <levizustiak@gmail.com>
2024-09-13 00:29:18 +0000

    feat: handle admin menu step preview

--- libs/journeys/ui/src/components/StepHeader/StepHeaderMenu/StepHeaderMenu.tsx
+++ libs/journeys/ui/src/components/StepHeader/StepHeaderMenu/StepHeaderMenu.tsx
@@ -7,12 +7,17 @@ import { ReactElement } from 'react'
 import X2 from '@core/shared/ui/icons/X2'
 
 import { JourneyMenuButtonIcon } from '../../../../__generated__/globalTypes'
+import { useEditor } from '../../../libs/EditorProvider'
 import { useJourney } from '../../../libs/JourneyProvider'
 import { transformer } from '../../../libs/transformer'
 import { getMenuIcon } from '../utils/getMenuIcon'
 
 export function StepHeaderMenu(): ReactElement {
   const { journey, variant } = useJourney()
+  const {
+    state: { selectedStep }
+  } = useEditor()
+  const { menuStepBlock, menuButtonIcon } = journey ?? {}
 
   const router = useRouter()
 
@@ -26,27 +31,28 @@ export function StepHeaderMenu(): ReactElement {
   )
 
   const isMenu =
-    journey?.menuStepBlock != null &&
-    stepBlock?.id === journey?.menuStepBlock.id
+    menuStepBlock != null &&
+    (selectedStep?.id === menuStepBlock.id ||
+      stepBlock?.id === menuStepBlock.id)
 
   let Icon: typeof SvgIcon | null =
     variant === 'admin' ? getMenuIcon(JourneyMenuButtonIcon.menu1) : null
 
-  if (journey != null && journey.menuButtonIcon != null) {
+  if (menuButtonIcon != null) {
     if (isMenu) {
       Icon = X2
     } else {
-      Icon = getMenuIcon(journey.menuButtonIcon)
+      Icon = getMenuIcon(menuButtonIcon)
     }
   }
 
   const handleClick = (): void => {
-    if (journey == null || journey.menuStepBlock == null) return
+    if (journey == null || menuStepBlock == null) return
 
     if (isMenu) {
       void router.back()
     } else {
-      const { slug, id } = journey.menuStepBlock
+      const { slug, id } = menuStepBlock
 
       void router.push(`/${journey?.slug}/${slug ?? id}`)
     }
@@ -56,22 +62,19 @@ export function StepHeaderMenu(): ReactElement {
     <Box
       sx={{
         borderRadius: 100,
-        border:
-          journey?.menuButtonIcon == null && variant === 'admin'
-            ? 'dashed'
-            : null,
+        border: menuButtonIcon == null && variant === 'admin' ? 'dashed' : null,
         minHeight: 48,
         minWidth: 48,
         display: 'grid',
         placeItems: 'center',
         maxWidth: 'min-content',
-        opacity: journey?.menuButtonIcon == null ? 0.6 : 1
+        opacity: menuButtonIcon == null ? 0.6 : 1
       }}
       data-testid="StepHeaderMenu"
     >
       {Icon != null && (
         <>
-          {variant === 'default' && journey?.menuStepBlock != null ? (
+          {variant === 'default' && menuStepBlock != null ? (
             <IconButton onClick={handleClick}>
               <Icon />
             </IconButton>
