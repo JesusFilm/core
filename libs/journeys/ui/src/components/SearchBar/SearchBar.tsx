10480d279faa78534d4e7d51f56eb0fbe18c49cd
scottkelso <scottkelso07@hotmail.com>
2024-09-16 00:58:56 +0000

    Revert "test: remove conditional render for dropdown"
    This reverts commit 63566f27dbd471ccc4c30309cec6d32936fb21f1.

--- libs/journeys/ui/src/components/SearchBar/SearchBar.tsx
+++ libs/journeys/ui/src/components/SearchBar/SearchBar.tsx
@@ -5,8 +5,8 @@ import InputAdornment from '@mui/material/InputAdornment'
 import { styled, useTheme } from '@mui/material/styles'
 import TextField, { TextFieldProps } from '@mui/material/TextField'
 import { Formik } from 'formik'
-import dynamic from 'next/dynamic'
 import { useTranslation } from 'next-i18next'
+import dynamic from 'next/dynamic'
 import { type ReactElement, useEffect, useRef, useState } from 'react'
 import { useRefinementList, useSearchBox } from 'react-instantsearch'
 
@@ -193,14 +193,16 @@ export function SearchBar({
               />
             </Box>
           </Box>
-          <DynamicSearchbarDropdown
-            open={open}
-            refinements={refinements}
-            id={open ? 'simple-popper' : undefined}
-            anchorEl={anchorEl}
-            tabIndex={tabValue}
-            handleTabValueChange={setTabValue}
-          />
+          {open && (
+            <DynamicSearchbarDropdown
+              open={open}
+              refinements={refinements}
+              id={open ? 'simple-popper' : undefined}
+              anchorEl={anchorEl}
+              tabIndex={tabValue}
+              handleTabValueChange={setTabValue}
+            />
+          )}
         </Box>
       </ClickAwayListener>
     </SearchBarProvider>
