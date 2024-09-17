8d80af645618965b33d15fe1512b7186fc7fef04
Kneesal <nisalcottingham@gmail.com>
2024-09-17 22:51:13 +0000

    fix: add use effect

--- apps/journeys/pages/[hostname]/[journeySlug].tsx
+++ apps/journeys/pages/[hostname]/[journeySlug].tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect } from 'react'
 
 import { getJourneyRTL } from '@core/journeys/ui/rtl'
 import { GET_JOURNEY } from '@core/journeys/ui/useJourneyQuery'
@@ -41,13 +41,13 @@ function HostJourneyPage({
   }
   console.log(router.query)
   console.log(router?.query?.defaultJourney === 'true')
-  // useEffect(() => {
-  //   if (router?.query?.defaultJourney === 'true') {
-  //     window.history.pushState(null, `${journey.title}`, hostname)
-  //   }
-  // }, [router, journey, hostname])
+  useEffect(() => {
+    if (router?.query?.defaultJourney === 'true') {
+      window.history.pushState(null, `${journey.title}`, '')
+    }
+  }, [router, journey, hostname])
   if (router?.query?.defaultJourney === 'true') {
-    window.history.pushState(null, `${journey.title}`, '/')
+    window.history.pushState(null, `${journey.title}`, '')
   }
   return (
     <>
