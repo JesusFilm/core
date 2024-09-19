eecb62cfa847fdb0c72c33d7e02808cce0a4db72
Levi Zustiak <levizustiak@gmail.com>
2024-09-19 23:01:44 +0000

    refactor: logic for displaying chat buttons

--- libs/journeys/ui/src/components/StepFooter/ChatButtons/ChatButtons.tsx
+++ libs/journeys/ui/src/components/StepFooter/ChatButtons/ChatButtons.tsx
@@ -1,10 +1,11 @@
 import { gql, useMutation } from '@apollo/client'
 import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
-import { useTheme } from '@mui/material/styles'
+import { alpha, useTheme } from '@mui/material/styles'
 import { usePlausible } from 'next-plausible'
 import { ReactElement } from 'react'
 
+import MessageTyping from '@core/shared/ui/icons/MessageTyping'
 import Plus2 from '@core/shared/ui/icons/Plus2'
 
 import {
@@ -31,15 +32,54 @@ export const CHAT_BUTTON_EVENT_CREATE = gql`
   }
 `
 
-export function ChatButtons(): ReactElement {
+function Fallback(): ReactElement {
+  return (
+    <IconButton
+      key="default"
+      disabled
+      sx={{
+        height: 44,
+        width: 44,
+        outline: 'none',
+        border: '3px dashed ',
+        opacity: 0.5,
+        borderColor: (theme) => theme.palette.grey[700]
+      }}
+    >
+      <Plus2 sx={{ color: (theme) => theme.palette.grey[700] }} />
+    </IconButton>
+  )
+}
+
+function Empty(): ReactElement {
+  return (
+    <IconButton
+      disabled
+      sx={{
+        height: 44,
+        width: 44,
+        outline: 'none',
+        border: '3px dashed ',
+        opacity: 0.5,
+        borderColor: 'common.white',
+        '&.Mui-disabled': {
+          backgroundColor: ({ palette }) => alpha(palette.common.white, 0.7)
+        }
+      }}
+    >
+      <MessageTyping sx={{ color: 'white' }} />
+    </IconButton>
+  )
+}
+
+function Filled(): ReactElement[] {
   const plausible = usePlausible<JourneyPlausibleEvents>()
-  const { variant, journey } = useJourney()
+  const { journey, variant } = useJourney()
   const { blockHistory } = useBlocks()
   const theme = useTheme()
-  const { rtl } = getJourneyRTL(journey)
+  const chatButtons = journey?.chatButtons ?? []
 
   const activeBlock = blockHistory[blockHistory.length - 1]
-  const chatButtons = journey?.chatButtons
   const isWebsite = journey?.website === true
   const showDefault = variant === 'admin' && chatButtons?.length === 0
 
@@ -105,56 +145,58 @@ export function ChatButtons(): ReactElement {
     }
   }
 
+  return chatButtons?.map((chatButton, index) => (
+    <IconButton
+      key={chatButton?.id}
+      onClick={() => handleClick(chatButton)}
+      sx={{
+        height: 44,
+        width: 44,
+        backgroundColor: getColor(
+          index === 0,
+          isWebsite ? 'website' : 'background'
+        ),
+        '&:hover': {
+          backgroundColor: getColor(
+            index === 0,
+            isWebsite ? 'website' : 'background'
+          )
+        },
+        backdropFilter: 'blur(5px)',
+        boxShadow: (theme) =>
+          isWebsite ? `0px 6px 6px ${theme.palette.grey[900]}60` : undefined
+      }}
+    >
+      <MessageChatIcon
+        platform={chatButton.platform ?? MessagePlatform.custom}
+        sx={{ color: getColor(index === 0, 'main') }}
+      />
+    </IconButton>
+  ))
+}
+
+export function ChatButtons(): ReactElement | null {
+  const { variant, journey } = useJourney()
+  const { rtl } = getJourneyRTL(journey)
+  const chatButtons = journey?.chatButtons
+
   return (
     <Stack
       data-testid="StepFooterChatButtons"
       direction={rtl ? 'row' : 'row-reverse'}
       gap={2}
     >
-      {chatButtons?.map((chatButton, index) => (
-        <IconButton
-          key={chatButton?.id}
-          onClick={() => handleClick(chatButton)}
-          sx={{
-            height: 44,
-            width: 44,
-            backgroundColor: getColor(
-              index === 0,
-              isWebsite ? 'website' : 'background'
-            ),
-            '&:hover': {
-              backgroundColor: getColor(
-                index === 0,
-                isWebsite ? 'website' : 'background'
-              )
-            },
-            backdropFilter: 'blur(5px)',
-            boxShadow: (theme) =>
-              isWebsite ? `0px 6px 6px ${theme.palette.grey[900]}60` : undefined
-          }}
-        >
-          <MessageChatIcon
-            platform={chatButton.platform ?? MessagePlatform.custom}
-            sx={{ color: getColor(index === 0, 'main') }}
-          />
-        </IconButton>
-      ))}
-      {showDefault && (
-        <IconButton
-          key="default"
-          disabled
-          sx={{
-            height: 44,
-            width: 44,
-            outline: 'none',
-            border: '3px dashed ',
-            opacity: 0.5,
-            borderColor: (theme) => theme.palette.grey[700]
-          }}
-        >
-          <Plus2 sx={{ color: (theme) => theme.palette.grey[700] }} />
-        </IconButton>
-      )}
+      {(() => {
+        if (journey?.showChatButtons !== true) {
+          return variant === 'admin' ? <Fallback /> : null
+        }
+
+        if (chatButtons == null || chatButtons.length === 0) {
+          return variant === 'admin' ? <Empty /> : null
+        }
+
+        return <Filled />
+      })()}
     </Stack>
   )
 }
