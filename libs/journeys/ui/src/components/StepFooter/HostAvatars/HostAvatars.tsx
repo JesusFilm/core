6d14582ff3787fc1bfca21f9c183b9de6f02a923
Levi Zustiak <levizustiak@gmail.com>
2024-09-19 23:03:13 +0000

    refactor: logic for displaying host avatars

--- libs/journeys/ui/src/components/StepFooter/HostAvatars/HostAvatars.tsx
+++ libs/journeys/ui/src/components/StepFooter/HostAvatars/HostAvatars.tsx
@@ -7,21 +7,75 @@ import UserProfile3Icon from '@core/shared/ui/icons/UserProfile3'
 import { useJourney } from '../../../libs/JourneyProvider'
 import { getJourneyRTL } from '../../../libs/rtl'
 
+function Placeholder(): ReactElement {
+  return (
+    <Avatar
+      sx={{
+        background: 'background.paper',
+        opacity: 0.6,
+        '&.MuiAvatar-root': {
+          border: 'none',
+          height: '32px',
+          width: '32px'
+        }
+      }}
+    >
+      <UserProfile3Icon
+        data-testid="host-avatar-active"
+        sx={{ color: (theme) => theme.palette.secondary.contrastText }}
+      />
+    </Avatar>
+  )
+}
+
 interface HostAvatarsProps {
   hasPlaceholder?: boolean
   size?: 'small' | 'large'
   avatarSrc1?: string | null
   avatarSrc2?: string | null
+  live?: boolean
 }
 
 export function HostAvatars({
   hasPlaceholder,
   size = 'small',
   avatarSrc1,
-  avatarSrc2
+  avatarSrc2,
+  live
 }: HostAvatarsProps): ReactElement {
   const { journey } = useJourney()
   const { rtl } = getJourneyRTL(journey)
+
+  const isEmpty = avatarSrc1 == null && avatarSrc2 == null
+
+  function render(): ReactElement | null {
+    if (isEmpty && size === 'large') return null
+
+    return journey?.showHosts !== true && live !== true ? (
+      <Placeholder />
+    ) : (
+      <>
+        {avatarSrc1 != null && (
+          <Avatar
+            src={avatarSrc1}
+            sx={{
+              height: size === 'small' ? '30px' : '48px',
+              width: size === 'small' ? '30px' : '48px'
+            }}
+          />
+        )}
+        {avatarSrc2 != null && (
+          <Avatar
+            src={avatarSrc2}
+            sx={{
+              height: size === 'small' ? '30px' : '48px',
+              width: size === 'small' ? '30px' : '48px'
+            }}
+          />
+        )}
+      </>
+    )
+  }
 
   return (
     <AvatarGroup
@@ -37,24 +91,7 @@ export function HostAvatars({
         }
       }}
     >
-      {avatarSrc1 != null && (
-        <Avatar
-          src={avatarSrc1}
-          sx={{
-            height: size === 'small' ? '30px' : '48px',
-            width: size === 'small' ? '30px' : '48px'
-          }}
-        />
-      )}
-      {avatarSrc2 != null && (
-        <Avatar
-          src={avatarSrc2}
-          sx={{
-            height: size === 'small' ? '30px' : '48px',
-            width: size === 'small' ? '30px' : '48px'
-          }}
-        />
-      )}
+      {render()}
       {(avatarSrc1 == null || avatarSrc2 == null) &&
         hasPlaceholder === true &&
         size === 'small' && (
