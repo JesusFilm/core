56ed681c9209b2fbafe3ce79563d6c3fe48a6a44
John Geronimo <johnageronimo1@gmail.com>
2024-09-22 23:15:26 +0000

    fix: clean up

--- apps/watch/pages/api/geolocation.ts
+++ apps/watch/pages/api/geolocation.ts
@@ -4,10 +4,7 @@ export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const country =
-    req.headers['cf-ipcountry'] ?? req.headers['x-vercel-ip-country']
-
   res.status(200).send({
-    country
+    country: req.headers['cf-ipcountry'] ?? req.headers['x-vercel-ip-country']
   })
 }
