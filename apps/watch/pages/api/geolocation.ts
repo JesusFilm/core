9269f45eb58ebd9bbc207049f970b53dfe9fa3ed
John Geronimo <johnageronimo1@gmail.com>
2024-09-22 23:14:11 +0000

    fix: code review

--- apps/watch/pages/api/geolocation.ts
+++ apps/watch/pages/api/geolocation.ts
@@ -4,17 +4,8 @@ export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const cloudflareIPCountry = req.headers['cf-ipcountry']
-  const vercelIPCountry = req.headers['x-vercel-ip-country']
-
-  console.log(
-    'Using:',
-    cloudflareIPCountry != null
-      ? `Cloudflare Country: ${cloudflareIPCountry as string}`
-      : `Vercel Country: ${vercelIPCountry as string}`
-  )
-
-  const country = cloudflareIPCountry ?? vercelIPCountry
+  const country =
+    req.headers['cf-ipcountry'] ?? req.headers['x-vercel-ip-country']
 
   res.status(200).send({
     country
