81bcd08a7320ec646674d90d8ff040b530477d3e
John Geronimo <johnageronimo1@gmail.com>
2024-09-22 22:21:33 +0000

    fix: log headers

--- apps/watch/pages/api/geolocation.ts
+++ apps/watch/pages/api/geolocation.ts
@@ -1,18 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import pino from 'pino'
-
-const logger = pino({ level: 'info' })
 
 export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   // Log all headers
-  logger.info(
-    { headers: Object.fromEntries(Object.entries(req.headers)) },
-    'Request Headers'
-  )
-
+  console.log('headers', {
+    headers: Object.fromEntries(Object.entries(req.headers))
+  })
   res.status(200).send({
     country: req.headers['x-vercel-ip-country'],
     region: req.headers['x-vercel-ip-country-region'],
