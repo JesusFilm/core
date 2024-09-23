efba532343968bb3266f28649041d522e74ceeee
Kneesal <nisalcottingham@gmail.com>
2024-09-23 23:05:37 +0000

    fix: drop discriminated union in favour of journeys array

--- apps/journeys/pages/[hostname]/index.tsx
+++ apps/journeys/pages/[hostname]/index.tsx
@@ -1,4 +1,3 @@
-import { gql } from '@apollo/client'
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
@@ -9,67 +8,57 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
 import { ReactElement } from 'react'
 
-import { JOURNEY_FIELDS } from '@core/journeys/ui/JourneyProvider/journeyFields'
 import { getJourneyRTL } from '@core/journeys/ui/rtl'
+import { GET_JOURNEY } from '@core/journeys/ui/useJourneyQuery'
 import { ThemeProvider } from '@core/shared/ui/ThemeProvider'
 
 import {
-  GetJourneysFields,
-  GetJourneysFieldsVariables,
-  GetJourneysFields_journeys as Journey
-} from '../../__generated__/GetJourneysFields'
-import { ThemeMode, ThemeName } from '../../__generated__/globalTypes'
+  GetJourney,
+  GetJourneyVariables,
+  GetJourney_journey as JourneyFields
+} from '../../__generated__/GetJourney'
+import {
+  GetJourneysSummary,
+  GetJourneysSummaryVariables,
+  GetJourneysSummary_journeys as Journey
+} from '../../__generated__/GetJourneysSummary'
+import { IdType, ThemeMode, ThemeName } from '../../__generated__/globalTypes'
 import i18nConfig from '../../next-i18next.config'
 import logo from '../../public/logo.svg'
 import { createApolloClient } from '../../src/libs/apolloClient'
-import JourneysPage from '../home'
+import JourneysPage, { GET_JOURNEYS } from '../home'
 
 import HostJourneyPage from './[journeySlug]'
 
-export enum PageType {
-  DefaultJourney = 'DefaultJourney',
-  JourneyCollection = 'JourneyCollection',
-  NotFound = 'NotFound'
-}
-
-interface NotFoundPageProps {
-  pageType: PageType.NotFound
-}
-
 interface HostJourneysPageProps {
-  pageType: PageType.JourneyCollection
-  journeys: Journey[]
+  host?: string
+  journeys?: Journey[]
 }
 
-interface DefaulJourneytHostPageProps {
-  pageType: PageType.DefaultJourney
-  host: string
-  journey: Journey
-  locale: string
-  rtl: boolean
-}
-
-export const GET_JOURNEYS_FIELDS = gql`
-  ${JOURNEY_FIELDS}
-  query GetJourneysFields($featured: Boolean, $options: JourneysQueryOptions) {
-    journeys(
-      where: { featured: $featured, template: false }
-      options: $options
-    ) {
-      id
-      ...JourneyFields
-    }
-  }
-`
-
-function HostJourneysPage(
-  props: HostJourneysPageProps | NotFoundPageProps | DefaulJourneytHostPageProps
+function getHostJourneyPage(
+  journey: JourneyFields,
+  host: string | undefined
 ): ReactElement {
+  const { rtl, locale } = getJourneyRTL(journey)
+  return (
+    <HostJourneyPage
+      journey={journey}
+      host={host ?? ''}
+      locale={locale}
+      rtl={rtl}
+    />
+  )
+}
+
+function HostJourneysPage({
+  journeys,
+  host
+}: HostJourneysPageProps): ReactElement {
   const { t } = useTranslation('apps-journeys')
 
   return (
     <>
-      {props.pageType === PageType.NotFound && (
+      {journeys == null ? (
         <>
           <NextSeo nofollow noindex />
           <ThemeProvider themeName={ThemeName.base} themeMode={ThemeMode.light}>
@@ -93,30 +82,24 @@ function HostJourneysPage(
             </Container>
           </ThemeProvider>
         </>
-      )}
-      {props.pageType === PageType.JourneyCollection && (
-        <>
-          <JourneysPage {...props} />
-        </>
-      )}
-      {props.pageType === PageType.DefaultJourney && (
-        <>
-          <HostJourneyPage {...props} />
-        </>
+      ) : journeys.length === 1 ? (
+        getHostJourneyPage(journeys[0] as JourneyFields, host)
+      ) : (
+        <JourneysPage journeys={journeys} />
       )}
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps<
-  HostJourneysPageProps | NotFoundPageProps | DefaulJourneytHostPageProps
-> = async (context) => {
+export const getStaticProps: GetStaticProps<HostJourneysPageProps> = async (
+  context
+) => {
   const apolloClient = createApolloClient()
   const { data } = await apolloClient.query<
-    GetJourneysFields,
-    GetJourneysFieldsVariables
+    GetJourneysSummary,
+    GetJourneysSummaryVariables
   >({
-    query: GET_JOURNEYS_FIELDS,
+    query: GET_JOURNEYS,
     variables: {
       options: {
         hostname: context.params?.hostname as string,
@@ -132,28 +115,52 @@ export const getStaticProps: GetStaticProps<
           context.locale ?? 'en',
           ['apps-journeys', 'libs-journeys-ui'],
           i18nConfig
-        )),
-        pageType: PageType.NotFound
+        ))
       },
+      journeys: data.journeys,
       notFound: true,
       revalidate: 1
     }
   } else if (data.journeys.length === 1) {
-    const { rtl, locale } = getJourneyRTL(data.journeys[0])
-    return {
-      props: {
-        host: context.params?.host?.toString() ?? '',
-        ...(await serverSideTranslations(
-          context.locale ?? 'en',
-          ['apps-journeys', 'libs-journeys-ui'],
-          i18nConfig
-        )),
-        journey: data.journeys[0],
-        locale,
-        rtl,
-        pageType: PageType.DefaultJourney
-      },
-      revalidate: 60
+    try {
+      const { data: journeyData } = await apolloClient.query<
+        GetJourney,
+        GetJourneyVariables
+      >({
+        query: GET_JOURNEY,
+        variables: {
+          id: data.journeys[0].slug.toString(),
+          idType: IdType.slug,
+          options: {
+            hostname: context.params?.hostname?.toString() ?? ''
+          }
+        }
+      })
+      return {
+        props: {
+          host: context.params?.host?.toString() ?? '',
+          ...(await serverSideTranslations(
+            context.locale ?? 'en',
+            ['apps-journeys', 'libs-journeys-ui'],
+            i18nConfig
+          )),
+          journeys: [{ ...journeyData.journey }]
+        },
+        revalidate: 60
+      }
+    } catch {
+      return {
+        props: {
+          ...(await serverSideTranslations(
+            context.locale ?? 'en',
+            ['apps-journeys', 'libs-journeys-ui'],
+            i18nConfig
+          ))
+        },
+        journeys: data.journeys,
+        notFound: true,
+        revalidate: 1
+      }
     }
   } else {
     return {
@@ -163,8 +170,7 @@ export const getStaticProps: GetStaticProps<
           ['apps-journeys', 'libs-journeys-ui'],
           i18nConfig
         )),
-        journeys: data.journeys,
-        pageType: PageType.JourneyCollection
+        journeys: data.journeys
       },
       revalidate: 60
     }
