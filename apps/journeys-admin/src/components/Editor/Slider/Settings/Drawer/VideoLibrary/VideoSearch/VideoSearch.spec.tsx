c9ff6aada6e45e41d8fe94c9462b8eee39c1feae
John Geronimo <johnageronimo1@gmail.com>
2024-09-23 05:24:33 +0000

    test: update case

--- apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoLibrary/VideoSearch/VideoSearch.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoLibrary/VideoSearch/VideoSearch.spec.tsx
@@ -1,24 +1,8 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
-import { SearchBoxRenderState } from 'instantsearch.js/es/connectors/search-box/connectSearchBox'
-import { useSearchBox } from 'react-instantsearch'
 
 import { VideoSearch } from './VideoSearch'
 
-jest.mock('react-instantsearch')
-
-const mockUseSearchBox = useSearchBox as jest.MockedFunction<
-  typeof useSearchBox
->
-
 describe('VideoSearch', () => {
-  const refine = jest.fn()
-
-  beforeEach(() => {
-    mockUseSearchBox.mockReturnValue({
-      refine
-    } as unknown as SearchBoxRenderState)
-  })
-
   it('has value in textbox', () => {
     render(<VideoSearch value="Jesus" onChange={jest.fn()} />)
     expect(screen.getByRole('searchbox', { name: 'Search' })).toHaveValue(
@@ -35,7 +19,7 @@ describe('VideoSearch', () => {
         target: { value: 'Jesus' }
       })
     )
-    await waitFor(() => expect(refine).toHaveBeenCalled())
+    await waitFor(() => expect(onChange).toHaveBeenCalled())
   })
 
   it('uses the search icon if tab is internal', () => {
