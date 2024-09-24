65070c8e4fc8e2b1888b7a23a65b2fc2d0a76c54
Siyang <csiyang@yahoo.co.nz>
2024-09-24 02:03:51 +0000

    feat: add  formblock deletes to seed

---
+++ apps/api-journeys/db/seeds/formBlocksDelete.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '.prisma/api-journeys-client'
+
+const prisma = new PrismaClient()
+
+export async function formBlocksDelete(): Promise<void> {
+  await prisma.block.deleteMany({
+    where: { typename: 'FormBlock' }
+  })
+}
