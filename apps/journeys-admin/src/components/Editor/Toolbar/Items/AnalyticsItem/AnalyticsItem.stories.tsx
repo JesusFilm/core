57ebe86344516e53451e9044790a96367508be73
Kneesal <nisalcottingham@gmail.com>
2024-09-26 02:59:28 +0000

    fix: init

--- apps/journeys-admin/src/components/Editor/Toolbar/Items/AnalyticsItem/AnalyticsItem.stories.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/Items/AnalyticsItem/AnalyticsItem.stories.tsx
@@ -1,39 +1,42 @@
-import { MockedProvider } from '@apollo/client/testing'
 import Box from '@mui/material/Box'
 import { Meta, StoryObj } from '@storybook/react'
+import { fn } from '@storybook/test'
+import { formatISO } from 'date-fns'
 
 import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
 import { publishedJourney } from '@core/journeys/ui/TemplateView/data'
 import { simpleComponentConfig } from '@core/shared/ui/storybook'
 
-import { ApolloLoadingProvider } from '../../../../../../test/ApolloLoadingProvider'
-
 import { AnalyticsItem } from './AnalyticsItem'
+import { getJourneyPlausibleVisitorsMock } from './AnalyticsItem.spec'
+
+const mockFormatISO = fn(formatISO)
 
 const Demo: Meta<typeof AnalyticsItem> = {
   ...simpleComponentConfig,
   component: AnalyticsItem,
+  async beforeEach() {
+    mockFormatISO.mockReturnValue('2024-09-26')
+    // 👇 Reset the mock after each story
+    return () => {
+      mockFormatISO.mockClear()
+    }
+  },
   title: 'Journeys-Admin/Editor/Toolbar/Items/AnalyticsItem'
 }
 
 const Template: StoryObj<typeof AnalyticsItem> = {
   render: ({ ...args }) => (
-    <ApolloLoadingProvider>
-      <MockedProvider>
-        <JourneyProvider
-          value={{ journey: publishedJourney, variant: 'admin' }}
-        >
-          <Box
-            sx={{
-              p: 6,
-              backgroundColor: 'background.paper'
-            }}
-          >
-            <AnalyticsItem {...args} />
-          </Box>
-        </JourneyProvider>
-      </MockedProvider>
-    </ApolloLoadingProvider>
+    <JourneyProvider value={{ journey: publishedJourney, variant: 'admin' }}>
+      <Box
+        sx={{
+          p: 6,
+          backgroundColor: 'background.paper'
+        }}
+      >
+        <AnalyticsItem {...args} />
+      </Box>
+    </JourneyProvider>
   )
 }
 
@@ -41,6 +44,23 @@ export const Default = {
   ...Template,
   args: {
     variant: 'icon-button'
+  },
+  parameters: {
+    apolloClient: {
+      mocks: []
+    }
+  }
+}
+
+export const WithVisitors = {
+  ...Template,
+  args: {
+    variant: 'icon-button'
+  },
+  parameters: {
+    apolloClient: {
+      mocks: [getJourneyPlausibleVisitorsMock]
+    }
   }
 }
 
--- apps/journeys-admin/src/components/Editor/Toolbar/Items/AnalyticsItem/AnalyticsItem.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/Items/AnalyticsItem/AnalyticsItem.tsx
@@ -1,4 +1,4 @@
-import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
 import NextLink from 'next/link'
 import { useTranslation } from 'next-i18next'
 import { ComponentProps, ReactElement } from 'react'
@@ -7,16 +7,55 @@ import { useJourney } from '@core/journeys/ui/JourneyProvider'
 import BarChartSquare3Icon from '@core/shared/ui/icons/BarChartSquare3'
 
 import { Item } from '../Item/Item'
+import { gql, useQuery } from '@apollo/client'
+import { formatISO } from 'date-fns'
+import { earliestStatsCollected } from '../../../Slider/JourneyFlow/AnalyticsOverlaySwitch'
+import Typography from '@mui/material/Typography'
+import {
+  GetJourneyPlausibleVisitors,
+  GetJourneyPlausibleVisitorsVariables
+} from '../../../../../../__generated__/GetJourneyPlausibleVisitors'
 
 interface AnalyticsItemProps {
   variant: ComponentProps<typeof Item>['variant']
 }
 
+export const GET_JOURNEY_PLAUSIBLE_VISITORS = gql`
+  query GetJourneyPlausibleVisitors($id: ID!, $date: String) {
+    journeyAggregateVisitors: journeysPlausibleStatsAggregate(
+      id: $id
+      idType: databaseId
+      where: { period: "custom", date: $date }
+    ) {
+      visitors {
+        value
+      }
+    }
+  }
+`
+
 export function AnalyticsItem({ variant }: AnalyticsItemProps): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const { journey } = useJourney()
+  const currentDate = formatISO(new Date(), { representation: 'date' })
+
+  const { data } = useQuery<
+    GetJourneyPlausibleVisitors,
+    GetJourneyPlausibleVisitorsVariables
+  >(GET_JOURNEY_PLAUSIBLE_VISITORS, {
+    variables: {
+      date: `${earliestStatsCollected},${currentDate}`,
+      id: journey?.id ?? ''
+    }
+  })
+
   return (
-    <Box data-testid="AnalyticsItem">
+    <Stack
+      data-testid="AnalyticsItem"
+      direction="row"
+      gap={1}
+      alignItems="center"
+    >
       <NextLink
         href={`/journeys/${journey?.id}/reports`}
         passHref
@@ -29,6 +68,9 @@ export function AnalyticsItem({ variant }: AnalyticsItemProps): ReactElement {
           icon={<BarChartSquare3Icon />}
         />
       </NextLink>
-    </Box>
+      <Typography variant="body2">
+        {data?.journeyAggregateVisitors?.visitors?.value ?? ''}
+      </Typography>
+    </Stack>
   )
 }
