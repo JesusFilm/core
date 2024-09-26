15cffc34d1fb8e873bc78cd4a82f350e4fbe29b3
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-09-26 21:50:02 +0000

    chore: init api journeys modern

--- Procfile
+++ Procfile
@@ -1,6 +1,7 @@
 analytics: nx serve api-analytics
 journeys: nx serve api-journeys
 journeys-watcher: nx watcher api-journeys
+journeys-modern: nx serve api-journeys-modern
 languages: nx serve api-languages
 media: nx serve api-media
 users: nx serve api-users
---
+++ apps/api-journeys-modern/src/schema/builder.ts
@@ -0,0 +1,26 @@
+import SchemaBuilder from '@pothos/core'
+import DirectivesPlugin from '@pothos/plugin-directives'
+import FederationPlugin from '@pothos/plugin-federation'
+// eslint-disable-next-line import/no-named-as-default
+import PrismaPlugin from '@pothos/plugin-prisma'
+
+import { Prisma } from '.prisma/api-journeys-modern-client'
+
+import type PrismaTypes from '../__generated__/pothos-types'
+import { prisma } from '../lib/prisma'
+
+export const builder = new SchemaBuilder<{
+  PrismaTypes: PrismaTypes
+  Scalars: {
+    ID: { Input: string; Output: number | string }
+  }
+}>({
+  plugins: [PrismaPlugin, DirectivesPlugin, FederationPlugin],
+  prisma: {
+    client: prisma,
+    dmmf: Prisma.dmmf,
+    onUnusedQuery: process.env.NODE_ENV === 'production' ? null : 'warn'
+  }
+})
+
+builder.queryType({})
