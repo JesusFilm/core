24bab8f103588055aac182ee4bebb3c8cdeec559
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-09-27 13:34:04 +1300

    chore: standardise logging and generate in yoga (#3576)
    Co-authored-by: Mike Allison <mike@captainofbass.com>

--- apps/api-analytics/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'node:http'
-
-import { yoga } from './yoga'
-
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-createServer(yoga).listen(4008, () => {
-  console.info('Server is running on http://localhost:4008/graphql')
-})
