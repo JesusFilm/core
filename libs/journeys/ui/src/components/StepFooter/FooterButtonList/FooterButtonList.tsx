f0ba87c940148134b44617e7cd89d4ba6f8681a2
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-09-27 16:15:01 +0000

    refactor: update render logic for footer button list

--- libs/journeys/ui/src/components/StepFooter/FooterButtonList/FooterButtonList.tsx
+++ libs/journeys/ui/src/components/StepFooter/FooterButtonList/FooterButtonList.tsx
@@ -1,4 +1,5 @@
 import Stack from '@mui/material/Stack'
+import { SxProps } from '@mui/material/styles'
 import { ReactElement } from 'react'
 
 import { useJourney } from '../../../libs/JourneyProvider'
@@ -6,15 +7,23 @@ import { useJourney } from '../../../libs/JourneyProvider'
 import { ReactionButton } from './ReactionButton'
 import { ShareButton } from './ShareButton'
 
-export function FooterButtonList(): ReactElement | null {
+interface FooterButtonListProps {
+  sx?: SxProps
+}
+
+export function FooterButtonList({
+  sx
+}: FooterButtonListProps): ReactElement | null {
   const { journey } = useJourney()
 
   const showShareButton = journey?.showShareButton ?? true
   const showLikeButton = journey?.showLikeButton ?? true
   const showDislikeButton = journey?.showDislikeButton ?? true
 
+  if (!showShareButton && !showLikeButton && !showDislikeButton) return null
+
   return (
-    <Stack direction="row" gap={2.5} data-testid="StepFooterButtonList">
+    <Stack direction="row" gap={2.5} data-testid="StepFooterButtonList" sx={sx}>
       {showShareButton && <ShareButton />}
       {showLikeButton && <ReactionButton variant="thumbsup" />}
       {showDislikeButton && <ReactionButton variant="thumbsdown" />}
