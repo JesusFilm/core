9186cb0d7d7b50068597f9dd72a3bd9567a37c65
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-10-03 21:46:36 +0000

    feat: add useAdminVideo hook

---
+++ apps/videos-admin/src/libs/useAdminVideo/useAdminVideo.spec.tsx
@@ -0,0 +1,39 @@
+import { MockedProvider } from '@apollo/client/testing'
+import { act, renderHook, waitFor } from '@testing-library/react'
+
+import { GET_ADMIN_VIDEO } from './useAdminVideo'
+import { useAdminVideoMock } from './useAdminVideo.mock'
+
+import { useAdminVideo } from '.'
+
+describe('useAdminVideo', () => {
+  it('should get custom domain for a team', async () => {
+    const result = jest.fn().mockReturnValue(useAdminVideoMock.result)
+
+    renderHook(
+      () =>
+        useAdminVideo({
+          variables: { videoId: 'someId' }
+        }),
+      {
+        wrapper: ({ children }) => (
+          <MockedProvider
+            mocks={[
+              {
+                request: {
+                  query: GET_ADMIN_VIDEO,
+                  variables: { videoId: 'someId' }
+                },
+                result
+              }
+            ]}
+          >
+            {children}
+          </MockedProvider>
+        )
+      }
+    )
+
+    await waitFor(() => expect(result).toHaveBeenCalled())
+  })
+})
