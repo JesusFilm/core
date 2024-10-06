b5068e653d107e6d4b596528e415a408e07cb35e
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-10-07 09:44:39 +1300

    feat: add jwt validation to gateway (#3605)

--- libs/nest/common/src/lib/firebaseClient/firebaseClient.ts
+++ libs/nest/common/src/lib/firebaseClient/firebaseClient.ts
@@ -3,6 +3,7 @@ import { GqlExecutionContext } from '@nestjs/graphql'
 import { ServiceAccount, cert, initializeApp } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import get from 'lodash/get'
+import { z } from 'zod'
 
 export interface User {
   id: string
@@ -26,14 +27,38 @@ export const firebaseClient = initializeApp(
 
 export const auth = getAuth(firebaseClient)
 
+const payloadSchema = z.object({
+  name: z.string(),
+  picture: z.string().nullable(),
+  // iss: z.string(),
+  // aud: z.string(),
+  // auth_time: z.number(),
+  user_id: z.string(),
+  // sub: z.string(),
+  // iat: z.number(),
+  // exp: z.number(),
+  email: z.string(),
+  email_verified: z.boolean()
+  // firebase: z.object({
+  //   identities: z.object({
+  //     email: z.array(z.string())
+  //   }),
+  //   sign_in_provider: z.string()
+  // })
+})
+
 export async function contextToUserId(
   context: ExecutionContext
 ): Promise<string | null> {
   const ctx = GqlExecutionContext.create(context).getContext()
+  const payload = get(ctx, 'req.body.extensions.jwt.payload')
+  const result = payloadSchema.safeParse(payload)
+  if (result.success) return result.data.user_id
+
   const token: string = get(ctx.headers, 'authorization')
   if (token == null || token === '') return null
   try {
-    const { uid } = await auth.verifyIdToken(token)
+    const { uid } = await auth.verifyIdToken(token.replace(/^JWT /g, ''))
     return uid
   } catch (err) {
     if (
@@ -50,6 +75,32 @@ export async function contextToUserId(
 export async function contextToUser(
   context: ExecutionContext
 ): Promise<User | null> {
+  const ctx = GqlExecutionContext.create(context).getContext()
+  const payload = get(ctx, 'req.body.extensions.jwt.payload')
+  const result = payloadSchema.safeParse(payload)
+
+  if (result.success) {
+    const {
+      user_id: id,
+      name,
+      email,
+      picture: imageUrl,
+      email_verified: emailVerified
+    } = result.data
+
+    const firstName = name?.split(' ')?.slice(0, -1)?.join(' ') ?? ''
+    const lastName = name?.split(' ')?.slice(-1)?.join(' ') ?? ''
+
+    return {
+      id,
+      firstName,
+      lastName,
+      email,
+      imageUrl,
+      emailVerified
+    }
+  }
+
   const userId = await contextToUserId(context)
 
   if (userId != null) {
