e15c5e9067b34d2688908fc3aa41198a7ea1a1bd
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-10-09 22:54:21 +0000

    fix: remove email verified requirement

--- libs/nest/common/src/lib/firebaseClient/firebaseClient.ts
+++ libs/nest/common/src/lib/firebaseClient/firebaseClient.ts
@@ -33,7 +33,7 @@ const payloadSchema = z
     picture: z.string().nullish(),
     user_id: z.string(),
     email: z.string(),
-    email_verified: z.boolean()
+    email_verified: z.boolean().nullish()
   })
   .transform((data) => ({
     id: data.user_id,
@@ -41,7 +41,7 @@ const payloadSchema = z
     lastName: data.name?.split(' ').slice(-1).join(' '),
     email: data.email,
     imageUrl: data.picture,
-    emailVerified: data.email_verified
+    emailVerified: data.email_verified ?? false
   }))
 
 export function contextToUserId(context: ExecutionContext): string | null {
