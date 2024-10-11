ab48dd185218112c13b493e7f92dc3489dca5428
Mike Allison <mike@captainofbass.com>
2024-10-11 11:47:26 -0700

    feat: deprecate image fields on video  (#3639)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/api-journeys/src/__generated__/graphql.ts
+++ apps/api-journeys/src/__generated__/graphql.ts
@@ -3366,13 +3366,17 @@ export type Video = {
   childrenCount: Scalars['Int']['output'];
   description: Array<VideoDescription>;
   id: Scalars['ID']['output'];
+  /** @deprecated use images.mobileCinematicHigh */
   image?: Maybe<Scalars['String']['output']>;
   imageAlt: Array<VideoImageAlt>;
   images: Array<CloudflareImage>;
   keywords: Array<Keyword>;
   label: VideoLabel;
+  /** @deprecated use images.mobileCinematicHigh */
   mobileCinematicHigh?: Maybe<Scalars['String']['output']>;
+  /** @deprecated use images.mobileCinematicLow */
   mobileCinematicLow?: Maybe<Scalars['String']['output']>;
+  /** @deprecated use images.mobileCinematicVeryLow */
   mobileCinematicVeryLow?: Maybe<Scalars['String']['output']>;
   noIndex?: Maybe<Scalars['Boolean']['output']>;
   parents: Array<Video>;
@@ -3383,6 +3387,7 @@ export type Video = {
   snippet: Array<VideoSnippet>;
   studyQuestions: Array<VideoStudyQuestion>;
   subtitles: Array<VideoSubtitle>;
+  /** @deprecated use images.thumbnail */
   thumbnail?: Maybe<Scalars['String']['output']>;
   title: Array<VideoTitle>;
   variant?: Maybe<VideoVariant>;
@@ -3390,6 +3395,7 @@ export type Video = {
   variantLanguagesCount: Scalars['Int']['output'];
   variantLanguagesWithSlug: Array<LanguageWithSlug>;
   variants: Array<VideoVariant>;
+  /** @deprecated use images.videoStill */
   videoStill?: Maybe<Scalars['String']['output']>;
 };
 
