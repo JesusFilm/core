f8f3da59e5f84db22b901dc05bec1646e8dbfb6a
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-10-13 22:24:58 +0000

    chore: add tabs

--- apps/disciple/app/_layout.tsx
+++ apps/disciple/app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Tabs } from 'expo-router'
+import { Stack } from 'expo-router'
 import { ReactElement } from 'react'
 
 export default function Layout(): ReactElement {
-  return <Tabs />
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
--- apps/disciple/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactElement } from 'react'
-import { Text } from 'react-native'
-
-export default function Page(): ReactElement {
-  return <Text>Top-level page</Text>
-}
