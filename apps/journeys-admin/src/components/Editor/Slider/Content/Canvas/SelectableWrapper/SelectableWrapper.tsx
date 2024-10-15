1299eb2dbf011eee1af1536fd0968a65bb035b73
Kneesal <nisalcottingham@gmail.com>
2024-10-15 22:47:12 +0000

    fix: code comment

--- apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/SelectableWrapper/SelectableWrapper.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/SelectableWrapper/SelectableWrapper.tsx
@@ -40,7 +40,7 @@ export function SelectableWrapper({
   }
 
   // TODO: Test dispatch via E2E
-  // if changing the event handlers or their functions, please check RadioOptionBlock events are being propogated properly i.e - can be re-ordered
+  // please check RadioOptionBlock events are being propogated properly i.e - can be re-ordered
   const handleSelectBlock = (e: MouseEvent<HTMLElement>): void => {
     // Allow RadioQuestion select event to be overridden by RadioOption select/edit events (no e.stopPropogation)
     if (block.__typename === 'RadioQuestionBlock') {
