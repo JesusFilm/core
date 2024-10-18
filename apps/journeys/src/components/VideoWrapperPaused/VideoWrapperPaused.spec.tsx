c67af4a0c18449a0255438be5cefd265ce031002
John Geronimo <johnageronimo1@gmail.com>
2024-10-18 02:33:26 +0000

    fix: add missing fields journeys

--- apps/journeys/src/components/VideoWrapperPaused/VideoWrapperPaused.spec.tsx
+++ apps/journeys/src/components/VideoWrapperPaused/VideoWrapperPaused.spec.tsx
@@ -47,7 +47,9 @@ describe('VideoWrapper', () => {
           parentOrder: 0,
           children: [],
           blurhash: 'LFALX]%g4Tf+?^jEMxo#00Mx%gjZ',
-          scale: null
+          scale: null,
+          focalLeft: 50,
+          focalTop: 50
         }
       ]
     }
@@ -132,7 +134,9 @@ describe('VideoWrapper', () => {
           parentOrder: 0,
           children: [],
           blurhash: 'LFALX]%g4Tf+?^jEMxo#00Mx%gjZ',
-          scale: null
+          scale: null,
+          focalLeft: 50,
+          focalTop: 50
         }
       ]
     }
