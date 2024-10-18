47131393f4014cf9428051d7fce5346102e76b4b
John Geronimo <johnageronimo1@gmail.com>
2024-10-18 02:23:59 +0000

    chore: codegen

--- apps/journeys-admin/__generated__/CardPollRestore.ts
+++ apps/journeys-admin/__generated__/CardPollRestore.ts
@@ -111,6 +111,8 @@ export interface CardPollRestore_imageRestore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_imageRestore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -513,6 +515,8 @@ export interface CardPollRestore_subtitleRestore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_subtitleRestore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -915,6 +919,8 @@ export interface CardPollRestore_titleRestore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_titleRestore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -1317,6 +1323,8 @@ export interface CardPollRestore_radioQuestionRestore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_radioQuestionRestore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -1719,6 +1727,8 @@ export interface CardPollRestore_radioOption1Restore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_radioOption1Restore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -2121,6 +2131,8 @@ export interface CardPollRestore_radioOption2Restore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_radioOption2Restore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -2523,6 +2535,8 @@ export interface CardPollRestore_radioOption3Restore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_radioOption3Restore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -2925,6 +2939,8 @@ export interface CardPollRestore_radioOption4Restore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_radioOption4Restore_RadioOptionBlock_action_NavigateToBlockAction {
@@ -3327,6 +3343,8 @@ export interface CardPollRestore_bodyRestore_ImageBlock {
    */
   blurhash: string;
   scale: number | null;
+  focalTop: number | null;
+  focalLeft: number | null;
 }
 
 export interface CardPollRestore_bodyRestore_RadioOptionBlock_action_NavigateToBlockAction {
