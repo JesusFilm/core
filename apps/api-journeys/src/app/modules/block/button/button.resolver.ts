d17325347d7b6ec9fa8d357ea4b3dfbae37e65d2
scottkelso <scottkelso07@hotmail.com>
2024-10-23 16:23:45 +1300

    refactor: include action and journey (#3785)

--- apps/api-journeys/src/app/modules/block/button/button.resolver.ts
+++ apps/api-journeys/src/app/modules/block/button/button.resolver.ts
@@ -14,6 +14,7 @@ import {
 import { Action, AppAbility } from '../../../lib/casl/caslFactory'
 import { AppCaslGuard } from '../../../lib/casl/caslGuard'
 import { PrismaService } from '../../../lib/prisma.service'
+import { INCLUDE_JOURNEY_ACL } from '../../journey/journey.acl'
 import { BlockService } from '../block.service'
 
 @Resolver('ButtonBlock')
@@ -44,12 +45,7 @@ export class ButtonBlockResolver {
         },
         include: {
           action: true,
-          journey: {
-            include: {
-              team: { include: { userTeams: true } },
-              userJourneys: true
-            }
-          }
+          ...INCLUDE_JOURNEY_ACL
         }
       })
       if (!ability.can(Action.Update, subject('Journey', block.journey)))
