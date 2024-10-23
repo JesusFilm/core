4cb5d2cc837aed411e97e4579d954800081d26e2
Kneesal <nisalcottingham@gmail.com>
2024-10-23 02:03:25 +0000

    fix: full screen modal

--- apps/journeys-admin/src/components/Editor/Toolbar/JourneyDetails/JourneyDetailsDialog/JourneyDetailsDialog.tsx
+++ apps/journeys-admin/src/components/Editor/Toolbar/JourneyDetails/JourneyDetailsDialog/JourneyDetailsDialog.tsx
@@ -1,6 +1,8 @@
 import { ApolloError } from '@apollo/client'
 import Stack from '@mui/material/Stack'
+import { Theme } from '@mui/material/styles'
 import TextField from '@mui/material/TextField'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import { Form, Formik, FormikValues } from 'formik'
 import { useTranslation } from 'next-i18next'
 import { useSnackbar } from 'notistack'
@@ -30,6 +32,7 @@ export function JourneyDetailsDialog({
   onClose
 }: JourneyDetailsDialogProps): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
+  const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('sm'))
   const [journeyUpdate] = useJourneyUpdateMutation()
   const { journey } = useJourney()
   const { enqueueSnackbar } = useSnackbar()
@@ -150,6 +153,7 @@ export function JourneyDetailsDialog({
                 closeLabel: t('Cancel')
               }}
               testId="JourneyDetailsDialog"
+              fullscreen={!smUp}
             >
               <Form>
                 <Stack spacing={6}>
@@ -193,16 +197,15 @@ export function JourneyDetailsDialog({
                       />
                     )}
                     popper={{
-                      modifiers: [
-                        {
-                          name: 'flip',
-                          enabled: true
-                        },
-                        {
-                          name: 'preventOverflow',
-                          enabled: true
+                      placement: smUp ? 'bottom' : 'top',
+                      sx: {
+                        '&>.MuiAutocomplete-paper': {
+                          height: !smUp ? '200px' : 'auto',
+                          '&>.MuiAutocomplete-listbox': {
+                            height: !smUp ? '200px' : 'auto'
+                          }
                         }
-                      ]
+                      }
                     }}
                   />
                 </Stack>
