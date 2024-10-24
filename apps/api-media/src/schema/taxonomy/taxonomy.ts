5bd41557aa6f10cdda17d0d695bbc10b1b9d02d4
Tanner <tanner.fleming@gmail.com>
2024-10-24 15:00:24 +0000

    fix: taxonomy calls

--- apps/api-media/src/schema/taxonomy/taxonomy.ts
+++ apps/api-media/src/schema/taxonomy/taxonomy.ts
@@ -3,15 +3,6 @@ import { Prisma } from '.prisma/api-media-client'
 import { prisma } from '../../lib/prisma'
 import { builder } from '../builder'
 
-const TaxonomiesFilter = builder.inputType('TaxonomiesFilter', {
-  fields: (t) => ({
-    ids: t.field({
-      type: ['ID']
-    }),
-    category: t.string({ required: false })
-  })
-})
-
 builder.prismaObject('TaxonomyName', {
   fields: (t) => ({
     id: t.exposeID('id'),
@@ -36,13 +27,7 @@ export const Taxonomy = builder.prismaObject('Taxonomy', {
       query: ({ languageId, languageCodes }) => {
         const where: Prisma.TaxonomyNameWhereInput = {}
         if (languageId !== null) where.languageId = languageId
-        if (
-          languageCodes !== undefined &&
-          languageCodes !== null &&
-          languageCodes.length > 0
-        ) {
-          where.languageCode = { in: languageCodes }
-        }
+        if (languageCodes !== null) where.languageCode = { in: languageCodes }
         return { where }
       }
     })
@@ -64,25 +49,13 @@ builder.queryFields((t) => ({
       return taxonomy ?? null
     }
   }),
+
   taxonomies: t.prismaField({
     type: ['Taxonomy'],
     nullable: false,
-    args: {
-      offset: t.arg.int({ required: false }),
-      limit: t.arg.int({ required: false }),
-      where: t.arg({ type: TaxonomiesFilter, required: false })
-    },
-    resolve: async (query, _parent, { offset, limit, where }) => {
-      const filter: Prisma.TaxonomyWhereInput = {}
-      if (where !== null && where !== undefined) {
-        if (where?.ids !== null) filter.id = { in: where.ids }
-        if (where?.category !== null) filter.category = where.category
-      }
+    resolve: async (query, _parent) => {
       const taxonomies = await prisma.taxonomy.findMany({
-        ...query,
-        where: filter,
-        skip: offset ?? undefined,
-        take: limit ?? undefined
+        ...query
       })
       return taxonomies
     }
