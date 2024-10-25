beebedfb149f629ca5a62d446d64bc879c60f03d
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-10-25 19:01:26 +0000

    feat: add Section component

---
+++ apps/videos-admin/src/components/Section/Section.tsx
@@ -0,0 +1,77 @@
+import Box from '@mui/material/Box'
+import Button, { ButtonProps as MuiButtonProps } from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import { MouseEvent, ReactElement, ReactNode } from 'react'
+
+function SectionFallback({ children }: { children: string }): ReactElement {
+  return (
+    <Box sx={{ display: 'grid', placeItems: 'center', padding: 2 }}>
+      <Typography variant="subtitle2" fontWeight={500}>
+        {children}
+      </Typography>
+    </Box>
+  )
+}
+
+interface SectionProps {
+  title: string
+  action?: {
+    label: string
+    startIcon?: MuiButtonProps['startIcon']
+    onClick: (e: MouseEvent<HTMLButtonElement>) => void
+  }
+  children: ReactNode
+  gridColumn?: number
+}
+
+export function Section({
+  title,
+  action,
+  children,
+  gridColumn
+}: SectionProps): ReactElement {
+  return (
+    <Stack
+      sx={{
+        border: '1px solid',
+        borderColor: 'divider',
+        overflow: 'hidden',
+        borderRadius: 1,
+        width: '100%',
+        gridColumn: gridColumn ?? 'auto'
+      }}
+      data-testid={`${title}-section`}
+    >
+      <Stack
+        sx={{
+          borderBottom: '1px solid',
+          borderColor: 'divider',
+          backgroundColor: 'background.paper',
+          px: 2,
+          py: 1
+        }}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Typography variant="h6" lineHeight={2}>
+          {title}
+        </Typography>
+        {action != null && (
+          <Button
+            size="small"
+            variant="outlined"
+            onClick={action.onClick}
+            startIcon={action.startIcon}
+          >
+            {action.label}
+          </Button>
+        )}
+      </Stack>
+      <Box sx={{ padding: 2 }}>{children}</Box>
+    </Stack>
+  )
+}
+
+Section.Fallback = SectionFallback
