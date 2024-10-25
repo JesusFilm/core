15f838cf288d038096c71d4e89ad6a86973265a6
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2024-10-25 18:52:03 +0000

    feat: add TabLabel

---
+++ apps/videos-admin/src/components/Tabs/TabLabel/TabLabel.tsx
@@ -0,0 +1,36 @@
+import Box from '@mui/material/Box'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import { ReactElement } from 'react'
+
+interface TabLabelProps {
+  label: string
+  count?: number
+}
+
+export function TabLabel({ label, count }: TabLabelProps): ReactElement {
+  return (
+    <Stack direction="row" gap={1} alignItems="center">
+      <Typography variant="subtitle2">{label}</Typography>
+      {count != null && (
+        <Box
+          sx={{
+            borderRadius: '6px',
+            height: 24,
+            minWidth: 24,
+            px: '2px',
+            display: 'grid',
+            placeItems: 'center',
+            borderWidth: '2px',
+            borderStyle: 'solid',
+            borderColor: 'divider'
+          }}
+        >
+          <Typography color="primary.main" fontWeight={600}>
+            {count}
+          </Typography>
+        </Box>
+      )}
+    </Stack>
+  )
+}
