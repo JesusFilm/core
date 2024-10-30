9c296115b06560c56456aa44fe136493f6608fb6
scottkelso <scottkelso07@hotmail.com>
2024-10-30 21:41:23 +0000

    refactor: data object journey provider

--- libs/journeys/ui/src/libs/JourneyProvider/JourneyProvider.spec.tsx
+++ libs/journeys/ui/src/libs/JourneyProvider/JourneyProvider.spec.tsx
@@ -2,14 +2,7 @@ import Button from '@mui/material/Button'
 import { fireEvent, render } from '@testing-library/react'
 import { ReactElement } from 'react'
 
-import {
-  JourneyStatus,
-  ThemeMode,
-  ThemeName
-} from '../../../__generated__/globalTypes'
-import type { TreeBlock } from '../block'
-
-import { JourneyFields as Journey } from './__generated__/JourneyFields'
+import { journey } from './JourneyProvider.data'
 
 import { JourneyProvider, useJourney } from '.'
 
@@ -19,62 +12,6 @@ const TestComponent = (): ReactElement => {
   const { journey } = useJourney()
 
   return <Button onClick={checkJourney(journey)}>Test</Button>
-}
-
-const journey: Journey = {
-  __typename: 'Journey',
-  id: 'journeyId',
-  themeName: ThemeName.base,
-  themeMode: ThemeMode.light,
-  featuredAt: null,
-  title: 'my journey',
-  strategySlug: null,
-  slug: 'my-journey',
-  language: {
-    __typename: 'Language',
-    id: '529',
-    bcp47: 'en',
-    iso3: 'eng',
-    name: [
-      {
-        __typename: 'LanguageName',
-        value: 'English',
-        primary: true
-      }
-    ]
-  },
-  description: 'my cool journey',
-  status: JourneyStatus.draft,
-  createdAt: '2021-11-19T12:34:56.647Z',
-  publishedAt: null,
-  blocks: [
-    {
-      id: 'step0.id',
-      __typename: 'StepBlock',
-      parentBlockId: null,
-      locked: false,
-      nextBlockId: 'step1.id'
-    }
-  ] as TreeBlock[],
-  primaryImageBlock: null,
-  creatorDescription: null,
-  creatorImageBlock: null,
-  userJourneys: [],
-  template: null,
-  seoTitle: null,
-  seoDescription: null,
-  chatButtons: [],
-  host: null,
-  team: null,
-  tags: [],
-  website: null,
-  showShareButton: null,
-  showLikeButton: null,
-  showDislikeButton: null,
-  displayTitle: null,
-  logoImageBlock: null,
-  menuButtonIcon: null,
-  menuStepBlock: null
 }
 
 describe('JourneyContext', () => {
@@ -87,60 +24,6 @@ describe('JourneyContext', () => {
 
     fireEvent.click(getByRole('button'))
 
-    expect(checkJourney).toHaveBeenCalledWith({
-      __typename: 'Journey',
-      id: 'journeyId',
-      themeName: ThemeName.base,
-      themeMode: ThemeMode.light,
-      featuredAt: null,
-      title: 'my journey',
-      strategySlug: null,
-      slug: 'my-journey',
-      language: {
-        __typename: 'Language',
-        id: '529',
-        bcp47: 'en',
-        iso3: 'eng',
-        name: [
-          {
-            __typename: 'LanguageName',
-            value: 'English',
-            primary: true
-          }
-        ]
-      },
-      description: 'my cool journey',
-      status: JourneyStatus.draft,
-      template: null,
-      createdAt: '2021-11-19T12:34:56.647Z',
-      publishedAt: null,
-      blocks: [
-        {
-          id: 'step0.id',
-          __typename: 'StepBlock',
-          parentBlockId: null,
-          locked: false,
-          nextBlockId: 'step1.id'
-        }
-      ] as TreeBlock[],
-      primaryImageBlock: null,
-      creatorDescription: null,
-      creatorImageBlock: null,
-      userJourneys: [],
-      seoTitle: null,
-      seoDescription: null,
-      chatButtons: [],
-      host: null,
-      team: null,
-      tags: [],
-      website: null,
-      showShareButton: null,
-      showLikeButton: null,
-      showDislikeButton: null,
-      displayTitle: null,
-      logoImageBlock: null,
-      menuButtonIcon: null,
-      menuStepBlock: null
-    })
+    expect(checkJourney).toHaveBeenCalledWith(journey)
   })
 })
