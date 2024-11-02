dcab0d8877adbc78b5dece22c99e8813fa02f701
Mike Allison <dj.mikeallison@gmail.com>
2024-11-02 00:28:12 +0000

    fix: nullable flip

--- apps/api-users/src/schema/user/objects/user.ts
+++ apps/api-users/src/schema/user/objects/user.ts
@@ -2,12 +2,12 @@ import { builder } from '../../builder'
 
 export const User = builder.prismaObject('User', {
   fields: (t) => ({
-    id: t.exposeID('id'),
-    firstName: t.exposeString('firstName'),
-    lastName: t.exposeString('lastName', { nullable: true }),
-    email: t.exposeString('email'),
-    imageUrl: t.exposeString('imageUrl', { nullable: true }),
-    superAdmin: t.exposeBoolean('superAdmin', { nullable: true }),
-    emailVerified: t.exposeBoolean('emailVerified')
+    id: t.exposeID('id', { nullable: false }),
+    firstName: t.exposeString('firstName', { nullable: false }),
+    lastName: t.exposeString('lastName'),
+    email: t.exposeString('email', { nullable: false }),
+    imageUrl: t.exposeString('imageUrl'),
+    superAdmin: t.exposeBoolean('superAdmin'),
+    emailVerified: t.exposeBoolean('emailVerified', { nullable: false })
   })
 })
