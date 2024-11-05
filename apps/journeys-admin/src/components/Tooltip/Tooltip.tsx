5923b72ff9aa22f48fa65cb1f39995458b5ee8f8
Levi Zustiak <levizustiak@gmail.com>
2024-11-05 18:17:23 +0000

    style: use theme tokens

--- apps/journeys-admin/src/components/Tooltip/Tooltip.tsx
+++ apps/journeys-admin/src/components/Tooltip/Tooltip.tsx
@@ -7,17 +7,17 @@ import { ReactElement } from 'react'
 
 const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
   <MuiTooltip {...props} classes={{ popper: className }} />
-))(() => ({
+))(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     padding: '8px',
-    backgroundColor: '#26262e',
+    backgroundColor: theme.palette.secondary.dark,
     fontSize: '12px',
     fontWeight: 400,
     lineHeight: '12px',
     textAlign: 'center'
   },
   [`& .${tooltipClasses.arrow}`]: {
-    color: '#26262e'
+    color: theme.palette.secondary.dark
   }
 }))
 
