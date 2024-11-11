5684dc58a25d37854516774afcd81f4b6adb5dde
Mike Allison <dj.mikeallison@gmail.com>
2024-11-11 23:34:14 +0000

    feat: 3 individual videos & mux analytics

--- apps/journeys/pages/home/3-video-test-b.tsx
+++ apps/journeys/pages/home/3-video-test-b.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable i18next/no-literal-string */
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Link from 'next/link'
 import { ReactElement, useEffect, useRef } from 'react'
 import videojs from 'video.js'
 import 'videojs-mux'
@@ -29,7 +31,8 @@ export default function ThreeVideoTest(): ReactElement {
           debug: false,
           data: {
             env_key: 'e2thjm49ulacc6tgf56laoeak',
-            player_name: 'test player'
+            player_name: 'test player',
+            video_title: 'mux'
           }
         }
       }
@@ -44,6 +47,17 @@ export default function ThreeVideoTest(): ReactElement {
   }, [muxPlayerRef])
   return (
     <Box flexGrow={1}>
+      <Box>
+        <Link href="/3-video-test-a" passHref legacyBehavior>
+          <Chip label="A" clickable />
+        </Link>
+        <Link href="/3-video-test-b" passHref legacyBehavior>
+          <Chip label="B" clickable color="primary" />
+        </Link>
+        <Link href="/3-video-test-c" passHref legacyBehavior>
+          <Chip label="C" clickable />
+        </Link>
+      </Box>
       <video
         className="video-js vjs-fluid"
         id="mux-player"
--- apps/journeys/pages/home/3-video-test-c.tsx
+++ apps/journeys/pages/home/3-video-test-c.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable i18next/no-literal-string */
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import Link from 'next/link'
 import { ReactElement, useEffect, useRef } from 'react'
 import videojs from 'video.js'
+import 'videojs-mux'
 
 import 'video.js/dist/video-js.css'
 
@@ -22,6 +25,16 @@ export default function ThreeVideoTest(): ReactElement {
           useNetworkInformationApi: true,
           useDevicePixelRatio: true
         }
+      },
+      plugins: {
+        mux: {
+          debug: false,
+          data: {
+            env_key: 'e2thjm49ulacc6tgf56laoeak',
+            player_name: 'test player',
+            video_title: 'brightcove'
+          }
+        }
       }
     })
   }
@@ -34,6 +47,17 @@ export default function ThreeVideoTest(): ReactElement {
   }, [brightcovePlayerRef])
   return (
     <Box flexGrow={1}>
+      <Box>
+        <Link href="/3-video-test-a" passHref legacyBehavior>
+          <Chip label="A" clickable />
+        </Link>
+        <Link href="/3-video-test-b" passHref legacyBehavior>
+          <Chip label="B" clickable />
+        </Link>
+        <Link href="/3-video-test-c" passHref legacyBehavior>
+          <Chip label="C" clickable color="primary" />
+        </Link>
+      </Box>
       <video
         ref={brightcovePlayerRef}
         className="video-js vjs-fluid"
