cf62be6561eb644958171b0ad894e658e9384e94
Dan <dan.chae@tandem.org.nz>
2024-11-12 03:26:46 +0000

    refactor:  extract controlitem component

--- apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/Controls/Controls.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/JourneyFlow/Controls/Controls.tsx
@@ -1,8 +1,12 @@
 import Box from '@mui/material/Box'
 import Tooltip from '@mui/material/Tooltip'
 import { useTranslation } from 'next-i18next'
-import { ReactElement } from 'react'
-import { Controls as Control, ControlButton, useReactFlow } from 'reactflow'
+import { ReactElement, ReactNode } from 'react'
+import {
+  ControlButton,
+  Controls as ControlItems,
+  useReactFlow
+} from 'reactflow'
 
 import ArrowRefresh6Icon from '@core/shared/ui/icons/ArrowRefresh6'
 import Dash from '@core/shared/ui/icons/Dash'
@@ -13,40 +17,34 @@ interface ControlsProps {
   handleReset: (boolean?) => Promise<void>
 }
 
+function ControlItem({ onClick, title, children }): ReactNode {
+  return (
+    <Tooltip title={title} arrow placement="right">
+      <Box>
+        <ControlButton onClick={onClick}>{children}</ControlButton>
+      </Box>
+    </Tooltip>
+  )
+}
+
 export function Controls({ handleReset }: ControlsProps): ReactElement {
   const { zoomIn, zoomOut, fitView } = useReactFlow()
   const { t } = useTranslation('apps-journeys-admin')
 
   return (
-    <Control showInteractive={false} showFitView={false} showZoom={false}>
-      <Tooltip title={t('Zoom in')} arrow placement="right">
-        <Box>
-          <ControlButton onClick={() => zoomIn()}>
-            <Plus1 />
-          </ControlButton>
-        </Box>
-      </Tooltip>
-      <Tooltip title={t('Zoom out')} arrow placement="right">
-        <Box>
-          <ControlButton onClick={() => zoomOut()}>
-            <Dash />
-          </ControlButton>
-        </Box>
-      </Tooltip>
-      <Tooltip title={t('Recenter')} arrow placement="right">
-        <Box>
-          <ControlButton onClick={() => fitView()}>
-            <Maximise2 />
-          </ControlButton>
-        </Box>
-      </Tooltip>
-      <Tooltip title={t('Reset layout')} arrow placement="right">
-        <Box>
-          <ControlButton onClick={async () => await handleReset()}>
-            <ArrowRefresh6Icon />
-          </ControlButton>
-        </Box>
-      </Tooltip>
-    </Control>
+    <ControlItems showInteractive={false} showFitView={false} showZoom={false}>
+      <ControlItem onClick={zoomIn} title={t('Zoom in')}>
+        <Plus1 />
+      </ControlItem>
+      <ControlItem onClick={zoomOut} title={t('Zoom out')}>
+        <Dash />
+      </ControlItem>
+      <ControlItem onClick={fitView} title={t('Recenter')}>
+        <Maximise2 />
+      </ControlItem>
+      <ControlItem onClick={handleReset} title={t('Reset layout')}>
+        <ArrowRefresh6Icon />
+      </ControlItem>
+    </ControlItems>
   )
 }
