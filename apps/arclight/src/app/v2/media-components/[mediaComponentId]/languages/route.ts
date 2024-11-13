87dd42a40ddb9c263aebc0deacd44b081519bc1f
Tanner <tanner.fleming@gmail.com>
2024-11-13 20:19:05 +0000

    feat: add lanugageids param

--- apps/arclight/src/app/v2/media-components/[mediaComponentId]/languages/route.ts
+++ apps/arclight/src/app/v2/media-components/[mediaComponentId]/languages/route.ts
@@ -54,6 +54,8 @@ export async function GET(
   const platform = query.get('platform') ?? 'ios'
   const apiSessionId = '6622f10d2260a8.05128925'
 
+  const languageIds = query.get('languageIds')?.split(',') ?? []
+
   const { data } = await getApolloClient().query<
     ResultOf<typeof GET_VIDEO_LANGUAGES>
   >({
@@ -62,171 +64,177 @@ export async function GET(
       id: mediaComponentId
     }
   })
+  const video = data.video
 
   const mediaComponentLanguage =
-    data.video?.variants == null
+    video?.variants == null
       ? []
-      : data.video.variants.map((variant) => {
-          const downloadLow = variant.downloads?.find(
-            (download) => download.quality === 'low'
+      : video.variants
+          .filter((variant) =>
+            languageIds.length > 0
+              ? languageIds.includes(variant.language?.id)
+              : true
           )
-          const downloadHigh = variant.downloads?.find(
-            (download) => download.quality === 'high'
-          )
+          .map((variant) => {
+            const downloadLow = variant.downloads?.find(
+              (download) => download.quality === 'low'
+            )
+            const downloadHigh = variant.downloads?.find(
+              (download) => download.quality === 'high'
+            )
 
-          const subtitleUrls =
-            platform === 'android'
-              ? {
-                  vtt:
-                    variant.subtitle?.map((subtitle) => ({
-                      languageId: Number(subtitle.language?.id),
-                      languageName: subtitle.language?.name[0].value,
-                      languageTag: subtitle.language?.bcp47,
-                      url: subtitle.vttSrc
-                    })) ?? [],
-                  srt:
-                    variant.subtitle?.map((subtitle) => ({
-                      languageId: Number(subtitle.language?.id),
-                      languageName: subtitle.language?.name[0].value,
-                      languageTag: subtitle.language?.bcp47,
-                      url: subtitle.srtSrc
-                    })) ?? []
-                }
-              : {
-                  vtt:
-                    variant.subtitle?.map((subtitle) => ({
-                      languageId: Number(subtitle.language?.id),
-                      languageName: subtitle.language?.name[0].value,
-                      languageTag: subtitle.language?.bcp47,
-                      url: subtitle.vttSrc
-                    })) ?? []
-                }
-
-          const streamingUrls =
-            platform === 'web'
-              ? {}
-              : platform === 'android'
+            const subtitleUrls =
+              platform === 'android'
                 ? {
-                    dash: [{ videoBitrate: 0, url: variant.dash }],
-                    hls: [{ videoBitrate: 0, url: variant.hls }],
-                    http: []
+                    vtt:
+                      variant.subtitle?.map((subtitle) => ({
+                        languageId: Number(subtitle.language?.id),
+                        languageName: subtitle.language?.name[0].value,
+                        languageTag: subtitle.language?.bcp47,
+                        url: subtitle.vttSrc
+                      })) ?? [],
+                    srt:
+                      variant.subtitle?.map((subtitle) => ({
+                        languageId: Number(subtitle.language?.id),
+                        languageName: subtitle.language?.name[0].value,
+                        languageTag: subtitle.language?.bcp47,
+                        url: subtitle.srtSrc
+                      })) ?? []
                   }
                 : {
-                    m3u8: [{ videoBitrate: 0, url: variant.hls }],
-                    http: []
+                    vtt:
+                      variant.subtitle?.map((subtitle) => ({
+                        languageId: Number(subtitle.language?.id),
+                        languageName: subtitle.language?.name[0].value,
+                        languageTag: subtitle.language?.bcp47,
+                        url: subtitle.vttSrc
+                      })) ?? []
                   }
 
-          const url = `https://api.arclight.org/videoPlayerUrl?refId=${variant.id}&apiSessionId=${apiSessionId}&player=bc.vanilla6&dtm=0&playerStyle=vanilla`
-
-          const cleanString = (str: string): string =>
-            str.replace(/\s+/g, ' ').trim()
-
-          const webEmbedPlayer =
-            platform === 'web'
-              ? cleanString(`
-            <div class="arc-cont">
-              <iframe src="${url}" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
-              <style>
-                .arc-cont {
-                  position: relative;
-                  display: block;
-                  margin: 10px auto;
-                  width: 100%;
-                }
-                .arc-cont:after {
-                  padding-top: 59%;
-                  display: block;
-                  content: "";
-                }
-                .arc-cont > iframe {
-                  position: absolute;
-                  top: 0;
-                  bottom: 0;
-                  right: 0;
-                  left: 0;
-                  width: 98%;
-                  height: 98%;
-                  border: 0;
-                }
-              </style>
-            </div>`)
-              : undefined
-
-          const webEmbedSharePlayer =
-            platform === 'web'
-              ? cleanString(`
-            <div class="arc-cont">
-              <iframe src="${url}" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
-              <style>
-                .arc-cont {
-                  position: relative;
-                  display: block;
-                  margin: 10px auto;
-                  width: 100%;
-                }
-                .arc-cont:after {
-                  padding-top: 59%;
-                  display: block;
-                  content: "";
-                }
-                .arc-cont > iframe {
-                  position: absolute;
-                  top: 0;
-                  bottom: 0;
-                  right: 0;
-                  left: 0;
-                  width: 98%;
-                  height: 98%;
-                  border: 0;
-                }
-              </style>
-            </div>`)
-              : undefined
-
-          return {
-            mediaComponentId,
-            languageId: Number(variant.language?.id),
-            refId: variant.id,
-            lengthInMilliseconds: variant.duration,
-            subtitleUrls,
-            downloadUrls: {
-              low:
-                downloadLow == null
-                  ? undefined
-                  : {
-                      url: downloadLow.url,
-                      sizeInBytes: downloadLow.size
-                    },
-              high:
-                downloadHigh == null
-                  ? undefined
-                  : {
-                      url: downloadHigh.url,
-                      sizeInBytes: downloadHigh.size
+            const streamingUrls =
+              platform === 'web'
+                ? {}
+                : platform === 'android'
+                  ? {
+                      dash: [{ videoBitrate: 0, url: variant.dash }],
+                      hls: [{ videoBitrate: 0, url: variant.hls }],
+                      http: []
                     }
-            },
-            streamingUrls,
-            shareUrl: variant.share ?? '',
-            socialMediaUrls: {},
-            ...(platform === 'web' && {
-              webEmbedPlayer,
-              webEmbedSharePlayer
-            }),
-            // TODO: implement
-            openGraphVideoPlayer: 'https://jesusfilm.org/',
-            _links: {
-              self: {
-                href: `https://api.arclight.com/v2/media-components/${mediaComponentId}/languages/${variant.language?.id}?platform=${platform}&apiKey=${apiKey}`
+                  : {
+                      m3u8: [{ videoBitrate: 0, url: variant.hls }],
+                      http: []
+                    }
+
+            const url = `https://api.arclight.org/videoPlayerUrl?refId=${variant.id}&apiSessionId=${apiSessionId}&player=bc.vanilla6&dtm=0&playerStyle=vanilla`
+
+            const cleanString = (str: string): string =>
+              str.replace(/\s+/g, ' ').trim()
+
+            const webEmbedPlayer =
+              platform === 'web'
+                ? cleanString(`
+            <div class="arc-cont">
+              <iframe src="${url}" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
+              <style>
+                .arc-cont {
+                  position: relative;
+                  display: block;
+                  margin: 10px auto;
+                  width: 100%;
+                }
+                .arc-cont:after {
+                  padding-top: 59%;
+                  display: block;
+                  content: "";
+                }
+                .arc-cont > iframe {
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  right: 0;
+                  left: 0;
+                  width: 98%;
+                  height: 98%;
+                  border: 0;
+                }
+              </style>
+            </div>`)
+                : undefined
+
+            const webEmbedSharePlayer =
+              platform === 'web'
+                ? cleanString(`
+            <div class="arc-cont">
+              <iframe src="${url}" allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>
+              <style>
+                .arc-cont {
+                  position: relative;
+                  display: block;
+                  margin: 10px auto;
+                  width: 100%;
+                }
+                .arc-cont:after {
+                  padding-top: 59%;
+                  display: block;
+                  content: "";
+                }
+                .arc-cont > iframe {
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  right: 0;
+                  left: 0;
+                  width: 98%;
+                  height: 98%;
+                  border: 0;
+                }
+              </style>
+            </div>`)
+                : undefined
+
+            return {
+              mediaComponentId,
+              languageId: Number(variant.language?.id),
+              refId: variant.id,
+              lengthInMilliseconds: variant.duration,
+              subtitleUrls,
+              downloadUrls: {
+                low:
+                  downloadLow == null
+                    ? undefined
+                    : {
+                        url: downloadLow.url,
+                        sizeInBytes: downloadLow.size
+                      },
+                high:
+                  downloadHigh == null
+                    ? undefined
+                    : {
+                        url: downloadHigh.url,
+                        sizeInBytes: downloadHigh.size
+                      }
               },
-              mediaComponent: {
-                href: `https://api.arclight.com/v2/media-components/${mediaComponentId}?apiKey=${apiKey}`
-              },
-              mediaLanguage: {
-                href: `https://api.arclight.com/v2/media-languages/${variant.language?.id}/?apiKey=${apiKey}`
+              streamingUrls,
+              shareUrl: variant.share ?? '',
+              socialMediaUrls: {},
+              ...(platform === 'web' && {
+                webEmbedPlayer,
+                webEmbedSharePlayer
+              }),
+              openGraphVideoPlayer: 'https://jesusfilm.org/',
+              _links: {
+                self: {
+                  href: `https://api.arclight.com/v2/media-components/${mediaComponentId}/languages/${variant.language?.id}?platform=${platform}&apiKey=${apiKey}`
+                },
+                mediaComponent: {
+                  href: `https://api.arclight.com/v2/media-components/${mediaComponentId}?apiKey=${apiKey}`
+                },
+                mediaLanguage: {
+                  href: `https://api.arclight.com/v2/media-languages/${variant.language?.id}/?apiKey=${apiKey}`
+                }
               }
             }
-          }
-        })
+          })
 
   const queryObject: Record<string, string> = {
     ...paramsToRecord(query.entries())
