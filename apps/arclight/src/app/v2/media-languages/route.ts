7304d689452cf9c548608dc6495a3c0eba0daad6
Tanner <tanner.fleming@gmail.com>
2024-11-14 16:26:03 +0000

    feat: add parameters to media languages

--- apps/arclight/src/app/v2/media-languages/route.ts
+++ apps/arclight/src/app/v2/media-languages/route.ts
@@ -4,28 +4,44 @@ import { NextRequest } from 'next/server'
 import { getApolloClient } from '../../../lib/apolloClient'
 import { paramsToRecord } from '../../../lib/paramsToRecord'
 
-/* TODO: 
-querystring:
-  apiKey
-  term
-    iso3
-    bcp47
-    ids
-    countryIds
-type
-    subTypes
-    contentTypes
-    expand
-    filter
-*/
 const GET_LANGUAGES = graphql(`
-  query GetLanguagesWithTags {
-    languages {
+  query GetLanguagesWithTags(
+    $limit: Int!
+    $offset: Int!
+    $ids: [ID!]
+    $bcp47: [String!]
+    $iso3: [String!]
+    $languageId: ID
+    $fallbackLanguageId: ID
+    $term: String
+  ) {
+    languagesCount(
+      where: { ids: $ids, bcp47: $bcp47, iso3: $iso3 }
+      term: $term
+    )
+    languages(
+      limit: $limit
+      offset: $offset
+      where: { ids: $ids, bcp47: $bcp47, iso3: $iso3 }
+      term: $term
+    ) {
       id
       iso3
       bcp47
-      name {
+      name(languageId: $languageId) {
         value
+        primary
+        language {
+          bcp47
+        }
+      }
+      fallbackName: name(languageId: $fallbackLanguageId) {
+        value
+        primary
+      }
+      nameNative: name {
+        value
+        primary
       }
       audioPreview {
         size
@@ -41,7 +57,14 @@ const GET_LANGUAGES = graphql(`
       featureFilmCount
       shortFilmCount
     }
-    languagesCount
+  }
+`)
+
+const GET_LANGUAGE_ID_FROM_BCP47 = graphql(`
+  query GetLanguageIdFromBCP47($bcp47: ID!) {
+    language(id: $bcp47, idType: bcp47) {
+      id
+    }
   }
 `)
 
@@ -51,6 +74,44 @@ export async function GET(request: NextRequest): Promise<Response> {
   const page = Number(query.get('page') ?? 1)
   const limit = Number(query.get('limit') ?? 10)
   const offset = (page - 1) * limit
+  const bcp47 = query.get('bcp47')?.split(',')
+  const ids = query.get('ids')?.split(',')
+  const iso3 = query.get('iso3')?.split(',')
+  const metadataLanguageTags =
+    query.get('metadataLanguageTags')?.split(',') ?? []
+  const term = query.get('term')
+
+  let languageId = '529'
+  let fallbackLanguageId = ''
+  if (metadataLanguageTags.length > 0) {
+    const { data } = await getApolloClient().query<
+      ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
+    >({
+      query: GET_LANGUAGE_ID_FROM_BCP47,
+      variables: { bcp47: metadataLanguageTags[0] }
+    })
+
+    if (data.language == null) {
+      return new Response(
+        JSON.stringify({
+          message: `Parameter "metadataLanguageTags" of value "${metadataLanguageTags}" violated a constraint "Not acceptable metadata language tag(s): ${metadataLanguageTags}"`,
+          logref: 400
+        }),
+        { status: 400 }
+      )
+    }
+
+    languageId = data.language?.id
+  }
+  if (metadataLanguageTags.length > 1) {
+    const { data } = await getApolloClient().query<
+      ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
+    >({
+      query: GET_LANGUAGE_ID_FROM_BCP47,
+      variables: { bcp47: metadataLanguageTags[1] }
+    })
+    fallbackLanguageId = data.language?.id ?? ''
+  }
 
   const { data } = await getApolloClient().query<
     ResultOf<typeof GET_LANGUAGES>
@@ -58,9 +119,16 @@ export async function GET(request: NextRequest): Promise<Response> {
     query: GET_LANGUAGES,
     variables: {
       limit,
-      offset
+      offset,
+      ids,
+      bcp47,
+      iso3,
+      languageId,
+      fallbackLanguageId,
+      term
     }
   })
+  const languages = data.languages
 
   const queryObject: Record<string, string> = {
     ...paramsToRecord(query.entries()),
@@ -68,22 +136,26 @@ export async function GET(request: NextRequest): Promise<Response> {
     limit: limit.toString()
   }
 
+  const totalPages = Math.ceil(Number(data.languagesCount) / limit)
   const queryString = new URLSearchParams(queryObject).toString()
   const firstQueryString = new URLSearchParams({
     ...queryObject,
     page: '1'
   }).toString()
-  // TODO: Needs new query in gql
   const lastQueryString = new URLSearchParams({
     ...queryObject,
-    page: '1192'
+    page: totalPages.toString()
   }).toString()
   const nextQueryString = new URLSearchParams({
     ...queryObject,
     page: (page + 1).toString()
   }).toString()
+  const previousQueryString = new URLSearchParams({
+    ...queryObject,
+    page: (page - 1).toString()
+  }).toString()
 
-  const mediaLanguages = data.languages.map((language) => ({
+  const mediaLanguages = languages.map((language) => ({
     languageId: Number(language.id),
     iso3: language.iso3,
     bcp47: language.bcp47,
@@ -119,14 +191,13 @@ export async function GET(request: NextRequest): Promise<Response> {
           }
         : null,
     primaryCountryId: language.primaryCountryId ?? '',
-    name: language.name[0]?.value,
-    nameNative: language.name[1]?.value,
+    name: language.name[0]?.value ?? language.fallbackName[0]?.value ?? '',
+    nameNative: language.nameNative.find(({ primary }) => primary)?.value,
     alternateLanguageName: '',
     alternateLanguageNameNative: '',
-    metadataLanguageTag: 'en', // TODO: Get from parameters
+    metadataLanguageTag: language.name[0]?.language.bcp47 ?? '',
     _links: {
       self: {
-        // TODO queerystring
         href: `/v2/media-languages/${language.id}`
       }
     }
@@ -135,7 +206,7 @@ export async function GET(request: NextRequest): Promise<Response> {
   const response = {
     page,
     limit,
-    pages: Math.ceil(Number(data.languagesCount) / limit),
+    pages: totalPages,
     total: data.languagesCount,
     _links: {
       self: {
@@ -147,9 +218,20 @@ export async function GET(request: NextRequest): Promise<Response> {
       last: {
         href: `http://api.arclight.org/v2/media-languages?${lastQueryString}`
       },
-      next: {
-        href: `http://api.arclight.org/v2/media-languages?${nextQueryString}`
-      }
+      ...(page < totalPages
+        ? {
+            next: {
+              href: `http://api.arclight.org/v2/media-languages?${nextQueryString}`
+            }
+          }
+        : {}),
+      ...(page > 1
+        ? {
+            previous: {
+              href: `http://api.arclight.org/v2/media-languages?${previousQueryString}`
+            }
+          }
+        : {})
     },
     _embedded: {
       mediaLanguages
