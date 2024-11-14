e44c58b68d8598fe102afba6c63d54142bfccf67
Tanner <tanner.fleming@gmail.com>
2024-11-14 16:36:04 +0000

    feat: add metadata language tags to media languages id

--- apps/arclight/src/app/v2/media-languages/[languageId]/route.ts
+++ apps/arclight/src/app/v2/media-languages/[languageId]/route.ts
@@ -4,22 +4,26 @@ import { NextRequest } from 'next/server'
 import { getApolloClient } from '../../../../lib/apolloClient'
 import { paramsToRecord } from '../../../../lib/paramsToRecord'
 
-/* TODO: 
-  querystring:
-    apiKey
-    expand
-    metadataLanguageTags
-    filter
-*/
-
 const GET_LANGUAGE = graphql(`
-  query GetLanguage($id: ID!) {
+  query GetLanguage($id: ID!, $languageId: ID, $fallbackLanguageId: ID) {
     language(id: $id, idType: databaseId) {
       id
       iso3
       bcp47
-      name {
+      name(languageId: $languageId) {
         value
+        primary
+        language {
+          bcp47
+        }
+      }
+      fallbackName: name(languageId: $fallbackLanguageId) {
+        value
+        primary
+      }
+      nameNative: name {
+        value
+        primary
       }
       audioPreview {
         size
@@ -38,6 +42,14 @@ const GET_LANGUAGE = graphql(`
   }
 `)
 
+const GET_LANGUAGE_ID_FROM_BCP47 = graphql(`
+  query GetLanguageIdFromBCP47($bcp47: ID!) {
+    language(id: $bcp47, idType: bcp47) {
+      id
+    }
+  }
+`)
+
 interface GetParams {
   params: { languageId: string }
 }
@@ -48,6 +60,41 @@ export async function GET(
 ): Promise<Response> {
   const query = request.nextUrl.searchParams
   const { languageId } = params
+  const metadataLanguageTags =
+    query.get('metadataLanguageTags')?.split(',') ?? []
+
+  let metadataLanguageId = '529'
+  let fallbackLanguageId = ''
+
+  if (metadataLanguageTags.length > 0) {
+    const { data } = await getApolloClient().query<
+      ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
+    >({
+      query: GET_LANGUAGE_ID_FROM_BCP47,
+      variables: { bcp47: metadataLanguageTags[0] }
+    })
+
+    if (data.language == null) {
+      return new Response(
+        JSON.stringify({
+          message: `Parameter "metadataLanguageTags" of value "${metadataLanguageTags}" violated a constraint "Not acceptable metadata language tag(s): ${metadataLanguageTags}"`,
+          logref: 400
+        }),
+        { status: 400 }
+      )
+    }
+
+    metadataLanguageId = data.language?.id
+  }
+  if (metadataLanguageTags.length > 1) {
+    const { data } = await getApolloClient().query<
+      ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
+    >({
+      query: GET_LANGUAGE_ID_FROM_BCP47,
+      variables: { bcp47: metadataLanguageTags[1] }
+    })
+    fallbackLanguageId = data.language?.id ?? ''
+  }
 
   const { data } = await getApolloClient().query<ResultOf<typeof GET_LANGUAGE>>(
     {
@@ -57,12 +104,13 @@ export async function GET(
       }
     }
   )
+  const language = data.language
 
   const queryObject: Record<string, string> = {
     ...paramsToRecord(query.entries())
   }
 
-  if (data.language == null)
+  if (language == null)
     return new Response(
       JSON.stringify({
         message: `${languageId}:\n  The requested language ID '${languageId}'not found.\n`,
@@ -74,43 +122,43 @@ export async function GET(
   const queryString = new URLSearchParams(queryObject).toString()
 
   const response = {
-    languageId: Number(data.language.id),
-    iso3: data.language.iso3,
-    bcp47: data.language.bcp47,
+    languageId: Number(language.id),
+    iso3: language.iso3,
+    bcp47: language.bcp47,
     counts: {
       speakerCount: {
-        value: data.language.speakerCount,
+        value: language.speakerCount,
         description: 'Number of speakers'
       },
       countriesCount: {
-        value: data.language.countriesCount,
+        value: language.countriesCount,
         description: 'Number of countries'
       },
       series: {
-        value: data.language.seriesCount,
+        value: language.seriesCount,
         description: 'Series'
       },
       featureFilm: {
-        value: data.language.featureFilmCount,
+        value: language.featureFilmCount,
         description: 'Feature Film'
       },
       shortFilm: {
-        value: data.language.shortFilmCount,
+        value: language.shortFilmCount,
         description: 'Short Film'
       }
     },
     audioPreview:
-      data.language.audioPreview != null
+      language.audioPreview != null
         ? {
-            url: data.language.audioPreview.value,
-            audioBitrate: data.language.audioPreview.bitrate,
-            audioContainer: data.language.audioPreview.codec,
-            sizeInBytes: data.language.audioPreview.size
+            url: language.audioPreview.value,
+            audioBitrate: language.audioPreview.bitrate,
+            audioContainer: language.audioPreview.codec,
+            sizeInBytes: language.audioPreview.size
           }
         : null,
-    primaryCountryId: data.language.primaryCountryId ?? '',
-    name: data.language.name[0].value,
-    nameNative: data.language.name[1]?.value ?? data.language.name[0].value,
+    primaryCountryId: language.primaryCountryId ?? '',
+    name: language.name[0]?.value ?? language.fallbackName[0]?.value ?? '',
+    nameNative: language.nameNative.find(({ primary }) => primary)?.value,
     alternateLanguageName: '',
     alternateLanguageNameNative: '',
     metadataLanguageTag: 'en',
