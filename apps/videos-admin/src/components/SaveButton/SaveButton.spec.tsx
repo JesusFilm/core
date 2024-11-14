858a2ba9abddbb8dc2839da645c9a1e44cb5b8ff
Edmond Shen <egsmond@gmail.com>
2024-11-14 03:22:29 +0000

    test: tests for save button

---
+++ apps/videos-admin/src/components/SaveButton/SaveButton.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import { NextIntlClientProvider } from 'next-intl'
+
+import { SaveButton } from '.'
+
+describe('SaveButton', () => {
+  it('should render save button as disabled', () => {
+    render(
+      <NextIntlClientProvider locale="en">
+        <SaveButton disabled />
+      </NextIntlClientProvider>
+    )
+
+    expect(screen.getByRole('button', { name: 'Save' })).toBeDisabled()
+  })
+
+  it('should render save button as not disabled', () => {
+    render(
+      <NextIntlClientProvider locale="en">
+        <SaveButton disabled={false} />
+      </NextIntlClientProvider>
+    )
+
+    expect(screen.getByRole('button', { name: 'Save' })).not.toBeDisabled()
+  })
+})
