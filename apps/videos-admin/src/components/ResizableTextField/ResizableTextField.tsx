f0b9b6dc8760ab293c75f31072e4cd7e222237bc
Edmond Shen <egsmond@gmail.com>
2024-11-18 01:31:48 +0000

    fix: resizable text field

---
+++ apps/videos-admin/src/components/ResizableTextField/ResizableTextField.tsx
@@ -0,0 +1,52 @@
+import { StyledEngineProvider, useTheme } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+import { ReactElement } from 'react'
+
+interface ResizableTextFieldProps {
+  id: string
+  name: string
+  value: string
+  handleChange: (e: React.ChangeEvent) => void
+  error: boolean
+  helperText: string
+  disabled: boolean
+}
+
+export function ResizableTextField({
+  id,
+  name,
+  value,
+  handleChange,
+  error,
+  helperText,
+  disabled
+}: ResizableTextFieldProps): ReactElement {
+  const theme = useTheme()
+  return (
+    <StyledEngineProvider injectFirst>
+      <TextField
+        hiddenLabel
+        id={id}
+        name={name}
+        variant="filled"
+        value={value}
+        onChange={handleChange}
+        error={error}
+        helperText={helperText}
+        multiline
+        minRows={6}
+        maxRows={6}
+        fullWidth
+        disabled={disabled}
+        sx={{
+          '.MuiFilledInput-root': {
+            backgroundColor: theme.palette.background.paper
+          },
+          '& .MuiFilledInput-input': {
+            resize: 'vertical'
+          }
+        }}
+      />
+    </StyledEngineProvider>
+  )
+}
