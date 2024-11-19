f7ad55baf2b412110a93c6a69fcd3eb309f1c788
Edmond Shen <egsmond@gmail.com>
2024-11-19 00:57:08 +0000

    fix: remove styled engine provider

--- apps/videos-admin/src/components/ResizableTextField/ResizableTextField.tsx
+++ apps/videos-admin/src/components/ResizableTextField/ResizableTextField.tsx
@@ -1,4 +1,3 @@
-import { StyledEngineProvider } from '@mui/material/styles'
 import { ReactElement } from 'react'
 import { TextField, TextFieldProps } from '@mui/material'
 
@@ -6,21 +5,19 @@ export function ResizableTextField(
   props: Omit<TextFieldProps, 'variant | multiline | sx'>
 ): ReactElement {
   return (
-    <StyledEngineProvider injectFirst>
-      <TextField
-        {...props}
-        variant="outlined"
-        multiline
-        hiddenLabel
-        sx={{
-          '.MuiOutlinedInput-root': {
-            height: 'fit-content'
-          },
-          '& .MuiOutlinedInput-input': {
-            resize: 'vertical'
-          }
-        }}
-      />
-    </StyledEngineProvider>
+    <TextField
+      {...props}
+      variant="outlined"
+      multiline
+      hiddenLabel
+      sx={{
+        '.MuiOutlinedInput-root': {
+          height: 'fit-content'
+        },
+        '& .MuiOutlinedInput-input': {
+          resize: 'vertical'
+        }
+      }}
+    />
   )
 }
