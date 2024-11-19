d7cac3a28172063f6b4b3d00df910cc203030b4c
Edmond Shen <egsmond@gmail.com>
2024-11-19 01:15:19 +0000

    fix: add tests for video description

--- apps/videos-admin/src/libs/useAdminVideo/useAdminVideo.mock.ts
+++ apps/videos-admin/src/libs/useAdminVideo/useAdminVideo.mock.ts
@@ -39,9 +39,8 @@ export const useAdminVideoMock: MockedResponse<
         ],
         description: [
           {
-            value:
-              "This film is a perfect introduction to Jesus through the Gospel of Luke. Jesus constantly surprises and confounds people, from His miraculous birth to His rise from the grave. Follow His life through excerpts from the Book of Luke, all the miracles, the teachings, and the passion. &#13;&#13;God creates everything and loves mankind. But mankind disobeys God. God and mankind are separated, but God loves mankind so much, He arranges redemption for mankind. He sends his Son Jesus to be a perfect sacrifice to make amends for us.&#13;&#13;Before Jesus arrives, God prepares mankind. Prophets speak of the birth, the life, and the death of Jesus.&#13;&#13;Jesus attracts attention. He teaches in parables no one really understands, gives sight to the blind, and helps those who no one sees as worth helping.&#13;&#13;He scares the Jewish leaders, they see him as a threat. So they arrange, through Judas the traitor and their Roman oppressors, for the crucifixion of Jesus. They think the matter is settled. But the women who serve Jesus discover an empty tomb. The disciples panic. When Jesus appears, they doubt He's real. But it's what He proclaimed all along: He is their perfect sacrifice, their Savior, victor over death. He ascends to heaven, telling His followers to tell others about Him and His teachings.",
-            id: '41f0dad1-f2b4-4850-8ebe-c60e3ca1e64f'
+            id: 'c1afff2e-057e-4e4a-a48c-11f005ffbb06',
+            value: 'Jesus description text'
           }
         ],
         snippet: [
