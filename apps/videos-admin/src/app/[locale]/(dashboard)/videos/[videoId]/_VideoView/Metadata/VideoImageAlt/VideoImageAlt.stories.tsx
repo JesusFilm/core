d874b897931e53c5331ee272c1104f733ff20829
Edmond Shen <egsmond@gmail.com>
2024-11-19 02:31:40 +0000

    fix: add story for image alt

---
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/VideoImageAlt/VideoImageAlt.stories.tsx
@@ -0,0 +1,110 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { screen } from '@testing-library/react'
+import { NextIntlClientProvider } from 'next-intl'
+import { ComponentProps } from 'react'
+
+import { videosAdminConfig } from '../../../../../../../../libs/storybookConfig'
+import { useAdminVideoMock } from '../../../../../../../../libs/useAdminVideo/useAdminVideo.mock'
+import userEvent from '@testing-library/user-event'
+import { EditProvider } from '../../../_EditProvider'
+import { VideoImageAlt } from './VideoImageAlt'
+
+const meta: Meta<typeof VideoImageAlt> = {
+  ...videosAdminConfig,
+  component: VideoImageAlt,
+  title: 'Videos-Admin/VideoImageAlt',
+  parameters: {
+    ...videosAdminConfig.parameters,
+    tags: ['!autodocs']
+  }
+}
+
+type Story = StoryObj<ComponentProps<typeof VideoImageAlt>>
+
+const Template: Story = {
+  render: ({ ...args }) => (
+    <NextIntlClientProvider locale="en">
+      <EditProvider initialState={args.state}>
+        <VideoImageAlt />
+      </EditProvider>
+    </NextIntlClientProvider>
+  )
+}
+
+export const Default = {
+  ...Template,
+  args: {
+    state: {
+      isEdit: true
+    }
+  },
+  parameters: {
+    apolloClient: {
+      mocks: [useAdminVideoMock]
+    },
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        segments: [
+          ['videoId', 'someId'],
+          ['locale', 'en']
+        ]
+      }
+    }
+  }
+}
+
+export const Disabled = {
+  ...Template,
+  args: {
+    state: {
+      isEdit: false
+    }
+  },
+  parameters: {
+    apolloClient: {
+      mocks: [useAdminVideoMock]
+    },
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        segments: [
+          ['videoId', 'someId'],
+          ['locale', 'en']
+        ]
+      }
+    }
+  }
+}
+
+export const Required = {
+  ...Template,
+  args: {
+    state: {
+      isEdit: true
+    }
+  },
+  parameters: {
+    apolloClient: {
+      mocks: [useAdminVideoMock]
+    },
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        segments: [
+          ['videoId', 'someId'],
+          ['locale', 'en']
+        ]
+      }
+    }
+  },
+  play: async () => {
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Image Alt' }),
+      'a'
+    )
+    await userEvent.clear(screen.getByRole('textbox', { name: 'Image Alt' }))
+  }
+}
+
+export default meta
