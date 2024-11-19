38b9d9b3d494bfca20c666e3a83dd911166c65d7
Tanner <tanner.fleming@gmail.com>
2024-11-19 16:55:41 +0000

    feat: add bulk param

--- apps/arclight/src/app/v2/resources/route.ts
+++ apps/arclight/src/app/v2/resources/route.ts
@@ -4,10 +4,8 @@ import { NextRequest } from 'next/server'
 import { getApolloClient } from '../../../lib/apolloClient'
 
 /*
- * TODO: return values
- * TODO: query params
- * metadataLanguageTags,
- * bulk
+ * TODO
+ * metadataLanguageTags
  */
 
 const GET_VIDEO = graphql(`
@@ -146,85 +144,108 @@ export async function GET(request: NextRequest): Promise<Response> {
   })
   const countries = countriesData.countries
 
-  const transformedResponse = {
-    _links: {
-      self: {
-        href: `http://api.arclight.org/v2/resources?term=${term}&bulk=false&apiKey=${apiKey}`
+  let transformedResponse
+  if (bulk === 'true') {
+    transformedResponse = {
+      _links: {
+        self: {
+          href: `http://api.arclight.org/v2/resources?term=${term}&bulk=false&apiKey=${apiKey}`
+        }
+      },
+      _embedded: {
+        resources: {
+          resourceCount: videos.length + countries.length + languages.length,
+          mediaCountries: countries.map((country) => country.id),
+          mediaLanguages: languages.map((language) => Number(language.id)),
+          alternateLanguages: [],
+          mediaComponents: videos.map((video) => video.id)
+        }
       }
-    },
-    _embedded: {
-      resources: {
-        resourceCount: videos.length + countries.length + languages.length,
-        mediaCountries: countries.map((country) => ({
-          countryId: country.id,
-          name: country.name[0]?.value ?? '',
-          continentName: country.continent.name[0]?.value ?? '',
-          metadataLanguageTag: 'en',
-          longitude: country.longitude,
-          latitude: country.latitude,
-          _links: {
-            self: {
-              href: `http://api.arclight.org/v2/media-countries/${country.id}?apiKey=${apiKey}`
+    }
+  } else {
+    transformedResponse = {
+      _links: {
+        self: {
+          href: `http://api.arclight.org/v2/resources?term=${term}&bulk=false&apiKey=${apiKey}`
+        }
+      },
+      _embedded: {
+        resources: {
+          resourceCount: videos.length + countries.length + languages.length,
+          mediaCountries: countries.map((country) => ({
+            countryId: country.id,
+            name: country.name[0]?.value ?? '',
+            continentName: country.continent.name[0]?.value ?? '',
+            metadataLanguageTag: 'en',
+            longitude: country.longitude,
+            latitude: country.latitude,
+            _links: {
+              self: {
+                href: `http://api.arclight.org/v2/media-countries/${country.id}?apiKey=${apiKey}`
+              }
             }
-          }
-        })),
-        mediaLanguages: languages.map((language) => ({
-          languageId: Number(language.id),
-          iso3: language.iso3,
-          bcp47: language.bcp47,
-          primaryCountryId: language.primaryCountryId,
-          name: language.name.find(({ primary }) => !primary)?.value ?? '',
-          nameNative: language.name.find(({ primary }) => primary)?.value ?? '',
-          metadataLanguageTag: 'en',
-          _links: {
-            self: {
-              href: `http://api.arclight.org/v2/media-languages/${language.id}?apiKey=${apiKey}`
-            }
-          }
-        })),
-        alternateLanguages: [],
-        mediaComponents: videos.map((video) => ({
-          mediaComponentId: video.id,
-          componentType: video.variant?.hls != null ? 'content' : 'collection',
-          contentType: 'video',
-          subType: video.label,
-          imageUrls: {
-            thumbnail:
-              video.images.find((image) => image.thumbnail != null)
-                ?.thumbnail ?? '',
-            videoStill:
-              video.images.find((image) => image.videoStill != null)
-                ?.videoStill ?? '',
-            mobileCinematicHigh:
-              video.images.find((image) => image.mobileCinematicHigh != null)
-                ?.mobileCinematicHigh ?? '',
-            mobileCinematicLow:
-              video.images.find((image) => image.mobileCinematicLow != null)
-                ?.mobileCinematicLow ?? '',
-            mobileCinematicVeryLow:
-              video.images.find((image) => image.mobileCinematicVeryLow != null)
-                ?.mobileCinematicVeryLow ?? ''
-          },
-          lengthInMilliseconds: (video?.variant?.duration ?? 0) * 1000,
-          containsCount: video.childrenCount ?? 0,
-          isDownloadable: true,
-          downloadSizes: {},
-          bibleCitations: video.bibleCitations.map((citation) => ({
-            osisBibleBook: citation.osisId,
-            chapterStart: citation.chapterStart,
-            verseStart: citation.verseStart,
-            chapterEnd: citation.chapterEnd,
-            verseEnd: citation.verseEnd
           })),
-          primaryLanguageId: Number(video.primaryLanguageId),
-          title: video.title[0]?.value ?? '',
-          shortDescription: video.snippet[0]?.value ?? '',
-          longDescription: video.description[0]?.value ?? '',
-          studyQuestions: video.studyQuestions.map(
-            (question) => question.value
-          ),
-          metadataLanguageTag: 'en'
-        }))
+          mediaLanguages: languages.map((language) => ({
+            languageId: Number(language.id),
+            iso3: language.iso3,
+            bcp47: language.bcp47,
+            primaryCountryId: language.primaryCountryId,
+            name: language.name.find(({ primary }) => !primary)?.value ?? '',
+            nameNative:
+              language.name.find(({ primary }) => primary)?.value ?? '',
+            metadataLanguageTag: 'en',
+            _links: {
+              self: {
+                href: `http://api.arclight.org/v2/media-languages/${language.id}?apiKey=${apiKey}`
+              }
+            }
+          })),
+          alternateLanguages: [],
+          mediaComponents: videos.map((video) => ({
+            mediaComponentId: video.id,
+            componentType:
+              video.variant?.hls != null ? 'content' : 'collection',
+            contentType: 'video',
+            subType: video.label,
+            imageUrls: {
+              thumbnail:
+                video.images.find((image) => image.thumbnail != null)
+                  ?.thumbnail ?? '',
+              videoStill:
+                video.images.find((image) => image.videoStill != null)
+                  ?.videoStill ?? '',
+              mobileCinematicHigh:
+                video.images.find((image) => image.mobileCinematicHigh != null)
+                  ?.mobileCinematicHigh ?? '',
+              mobileCinematicLow:
+                video.images.find((image) => image.mobileCinematicLow != null)
+                  ?.mobileCinematicLow ?? '',
+              mobileCinematicVeryLow:
+                video.images.find(
+                  (image) => image.mobileCinematicVeryLow != null
+                )?.mobileCinematicVeryLow ?? ''
+            },
+            lengthInMilliseconds: (video?.variant?.duration ?? 0) * 1000,
+            containsCount: video.childrenCount ?? 0,
+            isDownloadable: true,
+            downloadSizes: {},
+            bibleCitations: video.bibleCitations.map((citation) => ({
+              osisBibleBook: citation.osisId,
+              chapterStart: citation.chapterStart,
+              verseStart: citation.verseStart,
+              chapterEnd: citation.chapterEnd,
+              verseEnd: citation.verseEnd
+            })),
+            primaryLanguageId: Number(video.primaryLanguageId),
+            title: video.title[0]?.value ?? '',
+            shortDescription: video.snippet[0]?.value ?? '',
+            longDescription: video.description[0]?.value ?? '',
+            studyQuestions: video.studyQuestions.map(
+              (question) => question.value
+            ),
+            metadataLanguageTag: 'en'
+          }))
+        }
       }
     }
   }
