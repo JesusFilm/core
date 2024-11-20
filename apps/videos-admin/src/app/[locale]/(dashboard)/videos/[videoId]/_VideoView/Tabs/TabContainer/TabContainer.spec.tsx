2ed14fc787050446b68a76615c7e6c470a3b6abf
Edmond Shen <egsmond@gmail.com>
2024-11-20 01:37:57 +0000

    test: add tests for no children

--- apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Tabs/TabContainer/TabContainer.spec.tsx
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Tabs/TabContainer/TabContainer.spec.tsx
@@ -13,4 +13,14 @@ describe('TabContainer', () => {
 
     expect(screen.getByText('Hello')).toBeInTheDocument()
   })
+
+  it('should not render children when value does not equal index', () => {
+    render(
+      <TabContainer value={0} index={1}>
+        <Typography>Hello</Typography>
+      </TabContainer>
+    )
+
+    expect(screen.queryByText('Hello')).not.toBeInTheDocument()
+  })
 })
