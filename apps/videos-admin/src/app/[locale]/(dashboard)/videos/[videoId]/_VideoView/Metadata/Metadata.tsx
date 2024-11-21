00c073a6d97cfe5992621a12eae24eb81e886c4a
Edmond Shen <egsmond@gmail.com>
2024-11-21 21:20:56 +0000

    fix: video for video information

--- apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/Metadata.tsx
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/Metadata.tsx
@@ -35,21 +35,21 @@ export function Metadata({ video, loading }: MetadataProps): ReactElement {
       ) : (
         <>
           <Section title={t('Information')}>
-            <VideoInformation />
+            <VideoInformation video={video} />
           </Section>
           <Section title={t('Image')}>
             <Stack gap={2}>
-              <VideoImageAlt videoImageAlts={video?.imageAlt} />
+              <VideoImageAlt videoImageAlts={video.imageAlt} />
               <VideoImage video={video} isEdit={isEdit} />
             </Stack>
           </Section>
           <Section title={t('Snippet')}>
-            <VideoSnippet videoSnippets={video?.snippet} />
+            <VideoSnippet videoSnippets={video.snippet} />
           </Section>
           <Section title={t('Description')}>
-            <VideoDescription videoDescriptions={video?.description} />
+            <VideoDescription videoDescriptions={video.description} />
           </Section>
-          <StudyQuestionsList studyQuestions={video?.studyQuestions} />
+          <StudyQuestionsList studyQuestions={video.studyQuestions} />
         </>
       )}
     </Stack>
