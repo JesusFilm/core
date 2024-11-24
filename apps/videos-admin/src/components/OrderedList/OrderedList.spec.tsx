66b7b9a097159cad2a0592aee27c002297fb80e9
Kneesal <nisalcottingham@gmail.com>
2024-11-24 21:15:08 +0000

    fix: ordered list

---
+++ apps/videos-admin/src/components/OrderedList/OrderedList.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import { NextIntlClientProvider } from 'next-intl'
+import { EditProvider } from '../../app/[locale]/(dashboard)/videos/[videoId]/_EditProvider'
+import { OrderedList } from './OrderedList'
+import { DragEndEvent } from '@dnd-kit/core'
+import { OrderedItem } from './OrderedItem'
+
+describe('OrderedList', () => {
+  it('should provide dnd context', () => {
+    const onOrderUpdateMock = jest.fn()
+    render(
+      <NextIntlClientProvider locale="en">
+        <EditProvider initialState={{ isEdit: true }}>
+          <OrderedList onOrderUpdate={onOrderUpdateMock} items={[]}>
+            <OrderedItem id={'1'} label={'test'} idx={0}></OrderedItem>
+          </OrderedList>
+        </EditProvider>
+      </NextIntlClientProvider>
+    )
+
+    expect(screen.getByRole('status')).toBeInTheDocument()
+  })
+})
