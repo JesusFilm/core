8528fbf860cee9e668e17dfa0e6843ab1c3da10f
Edmond Shen <egsmond@gmail.com>
2024-11-24 21:23:25 +0000

    test: add tests for downloads

---
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Variants/Downloads/utils/bytesToSize.ts
@@ -0,0 +1,11 @@
+export function bytesToSize(bytes: number): string {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes === 0) return 'n/a'
+  const i = parseInt(
+    Math.floor(Math.log(bytes) / Math.log(1024)).toString(),
+    10
+  )
+
+  if (i === 0) return `${bytes} ${sizes[i]})`
+  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
+}
