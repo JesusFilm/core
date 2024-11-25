e81a1bbfca46efc512ea6f415df67d3f3a452dfe
Tataihono Nikora <tataihono.nikora@gmail.com>
2024-11-25 02:16:01 +0000

    fix: throw redirect error

--- apps/short-links-e2e/src/e2e/redirect.spec.ts
+++ apps/short-links-e2e/src/e2e/redirect.spec.ts
@@ -1,19 +1,7 @@
 import { expect, test } from '@playwright/test'
 
-/* 
-This is just a sample test
-Test that 'arclight' part of the URL
-*/
 test('redirect results in 404', async ({ page }) => {
-  await page.goto('/link-does-not-exist')
-
-  // Get and log the current URL
-  const url = page.url()
-  console.log('Current URL:', url)
-
-  // Test the response status
-  const response = await page.waitForResponse(
-    (response) => response.status() === 404
+  await expect(page.goto('/link-does-not-exist')).rejects.toThrow(
+    /ERR_HTTP_RESPONSE_CODE_FAILURE/
   )
-  expect(response.status()).toBe(404)
 })
