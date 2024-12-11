c4332f59f8a33c2e5fd59c28968c10e27160ae13
Mike Allison <dj.mikeallison@gmail.com>
2024-12-11 21:32:30 +0000

    chore: add mediavideo to videofields

--- apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoBlockEditor/VideoBlockEditor.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoBlockEditor/VideoBlockEditor.spec.tsx
@@ -55,6 +55,29 @@ const videoInternal: TreeBlock<VideoBlock> = {
     },
     variantLanguages: []
   },
+  mediaVideo: {
+    __typename: 'Video',
+    id: '2_0-FallingPlates',
+    title: [
+      {
+        __typename: 'VideoTitle',
+        value: 'FallingPlates'
+      }
+    ],
+    images: [
+      {
+        __typename: 'CloudflareImage',
+        mobileCinematicHigh:
+          'https://imagedelivery.net/tMY86qEHFACTO8_0kAeRFA/2_0-FallingPlates.mobileCinematicHigh.jpg/f=jpg,w=1280,h=600,q=95'
+      }
+    ],
+    variant: {
+      __typename: 'VideoVariant',
+      id: '2_0-FallingPlates-529',
+      hls: 'https://arc.gt/hls/2_0-FallingPlates/529'
+    },
+    variantLanguages: []
+  },
   posterBlockId: null,
   children: []
 }
@@ -114,6 +137,7 @@ const videoYouTube: TreeBlock<VideoBlock> = {
   action: null,
   source: VideoBlockSource.youTube,
   video: null,
+  mediaVideo: null,
   objectFit: null,
   posterBlockId: 'poster1.id',
   children: []
