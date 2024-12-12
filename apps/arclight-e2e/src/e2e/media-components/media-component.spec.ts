fd528cdd802df93f145bad5b14a4ac9f14e409cf
Tanner <tanner.fleming@gmail.com>
2024-12-12 15:55:13 +0000

    refactor: media component test

--- apps/arclight-e2e/src/e2e/media-components/media-component.spec.ts
+++ apps/arclight-e2e/src/e2e/media-components/media-component.spec.ts
@@ -4,60 +4,36 @@ import { getBaseUrl } from '../../framework/helpers'
 import { getObjectDiff } from '../../utils/media-component-utils'
 import testData from '../../utils/testData.json'
 
-test.fixme(
-  'compare single media component between environments',
-  async ({ request }) => {
-    const baseUrl = await getBaseUrl()
-    const compareUrl = 'https://api.arclight.org'
-    const mediaComponentId = testData.mediaComponentId
+test('compare single media component between environments', async ({
+  request
+}) => {
+  const baseUrl = await getBaseUrl()
+  const compareUrl = 'https://api.arclight.org'
+  const queryParams = new URLSearchParams({
+    apiKey: testData.apiKey,
+    mediaComponentId: testData.mediaComponentId
+  })
 
-    const queryParams = new URLSearchParams({
-      apiKey: testData.apiKey,
-      mediaComponentId: mediaComponentId
-    })
-
-    // Fetch responses from both environments
-    const [baseResponse, compareResponse] = await Promise.all([
-      request.get(
-        `${baseUrl}/v2/media-components/${mediaComponentId}?${queryParams}`
-      ),
-      request.get(
-        `${compareUrl}/v2/media-components/${mediaComponentId}?${queryParams}`
+  const [baseData, compareData] = await Promise.all([
+    request
+      .get(
+        `${baseUrl}/v2/media-components/${testData.mediaComponentId}?${queryParams}`
       )
-    ])
+      .then((res) => res.json()),
+    request
+      .get(
+        `${compareUrl}/v2/media-components/${testData.mediaComponentId}?${queryParams}`
+      )
+      .then((res) => res.json())
+  ])
 
-    expect(baseResponse.ok()).toBeTruthy()
-    expect(compareResponse.ok()).toBeTruthy()
+  delete baseData._links
+  delete compareData._links
 
-    if (baseResponse.ok() && compareResponse.ok()) {
-      const baseData = await baseResponse.json()
-      const compareData = await compareResponse.json()
+  const diffs = getObjectDiff(baseData, compareData)
 
-      delete baseData._links
-      delete compareData._links
-
-      if (!baseData.mediaComponentId) {
-        console.log(
-          `Media Component ${mediaComponentId} only exists in compare environment`
-        )
-        expect(false).toBeTruthy()
-      } else if (!compareData.mediaComponentId) {
-        console.log(
-          `Media Component ${mediaComponentId} only exists in base environment`
-        )
-        expect(false).toBeTruthy()
-      } else {
-        const differences = getObjectDiff(baseData, compareData)
-
-        if (differences.length > 0) {
-          console.log(
-            `Differences found in media component ${mediaComponentId}:`,
-            differences
-          )
-        }
-
-        expect(differences).toHaveLength(0)
-      }
-    }
-  }
-)
+  expect(
+    diffs,
+    `Differences found in media component ${testData.mediaComponentId}`
+  ).toHaveLength(0)
+})
