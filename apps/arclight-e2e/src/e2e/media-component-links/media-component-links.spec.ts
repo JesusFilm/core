d229a65edf2eceb65fb1e8d90024a84690e43a95
Tanner <tanner.fleming@gmail.com>
2024-12-13 14:37:27 +0000

    fix: media component links e2e tests

--- apps/arclight-e2e/src/e2e/media-component-links/media-component-links.spec.ts
+++ apps/arclight-e2e/src/e2e/media-component-links/media-component-links.spec.ts
@@ -1,91 +1,56 @@
-/* eslint-disable playwright/no-conditional-in-test */
 import { expect, test } from '@playwright/test'
 
 import { getBaseUrl } from '../../framework/helpers'
 import {
-  ApiResponse,
   convertArrayToObject,
   getObjectDiff
 } from '../../utils/media-component-utils'
 import testData from '../../utils/testData.json'
 
-test.skip('compare media component links between environments', async ({
+test('compare media component links between environments', async ({
   request
 }) => {
   const baseUrl = await getBaseUrl()
   const compareUrl = 'https://api.arclight.org'
+  const { apiKey } = testData
 
-  // Construct query parameters
-  const queryParams = new URLSearchParams({
-    apiKey: testData.apiKey
-  })
+  const queryParams = new URLSearchParams({ apiKey })
 
-  // Make parallel API requests
-  const [baseResponse, compareResponse] = await Promise.all([
-    request.get(`${baseUrl}/v2/media-component-links?${queryParams}`),
-    request.get(`${compareUrl}/v2/media-component-links?${queryParams}`)
+  const [baseData, compareData] = await Promise.all([
+    request
+      .get(`${baseUrl}/v2/media-component-links?${queryParams}`)
+      .then((res) => res.json()),
+    request
+      .get(`${compareUrl}/v2/media-component-links?${queryParams}`)
+      .then((res) => res.json())
   ])
 
-  expect(baseResponse.ok()).toBeTruthy()
-  expect(compareResponse.ok()).toBeTruthy()
+  expect(baseData._embedded.mediaComponentsLinks).toBeDefined()
+  expect(compareData._embedded.mediaComponentsLinks).toBeDefined()
 
-  if (baseResponse.ok() && compareResponse.ok()) {
-    const baseData: ApiResponse = await baseResponse.json()
-    const compareData: ApiResponse = await compareResponse.json()
-
-    // Normalize and sort containedBy arrays
-    const normalizeData = (data: any) => {
-      if (data._embedded?.mediaComponentsLinks) {
-        data._embedded.mediaComponentsLinks.forEach((link: any) => {
-          if (link.linkedMediaComponentIds?.containedBy) {
-            link.linkedMediaComponentIds.containedBy.sort()
-          }
-        })
+  // Sort because the order does not matter
+  const sortContainedBy = (data: any) => {
+    data._embedded.mediaComponentsLinks.forEach((link: any) => {
+      if (link.linkedMediaComponentIds?.containedBy) {
+        link.linkedMediaComponentIds.containedBy.sort()
       }
-      return data
-    }
-
-    const baseMediaComponentLinks = convertArrayToObject(
-      normalizeData(baseData)._embedded.mediaComponentsLinks,
-      'mediaComponentId'
-    )
-    const compareMediaComponentLinks = convertArrayToObject(
-      normalizeData(compareData)._embedded.mediaComponentsLinks,
-      'mediaComponentId'
-    )
-
-    const differences = getObjectDiff(
-      baseMediaComponentLinks,
-      compareMediaComponentLinks
-    )
-
-    // Track meaningful differences
-    const meaningfulDifferences = differences.filter((diffId) => {
-      if (
-        !baseMediaComponentLinks[diffId] ||
-        !compareMediaComponentLinks[diffId]
-      ) {
-        console.log(
-          `Media Component Link ${diffId} only exists in ${
-            !baseMediaComponentLinks[diffId] ? 'compare' : 'base'
-          } environment`
-        )
-        return true
-      }
-
-      const specificDiffs = getObjectDiff(
-        baseMediaComponentLinks[diffId].linkedMediaComponentIds,
-        compareMediaComponentLinks[diffId].linkedMediaComponentIds
-      )
-
-      if (specificDiffs.length > 0) {
-        console.log(`Differences in ${diffId}:`, specificDiffs)
-        return true
-      }
-
-      return false
     })
-
-    expect(meaningfulDifferences).toHaveLength(0)
+    return data
   }
+
+  const baseMediaComponentLinks = convertArrayToObject(
+    sortContainedBy(baseData),
+    'mediaComponentId'
+  )
+  const compareMediaComponentLinks = convertArrayToObject(
+    sortContainedBy(compareData),
+    'mediaComponentId'
+  )
+
+  const diffs = getObjectDiff(
+    baseMediaComponentLinks,
+    compareMediaComponentLinks
+  )
+
+  expect(diffs).toHaveLength(0)
 })
