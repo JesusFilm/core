35619b809e3e832a8f4bd2687aa77071f41e0ab6
Tanner Fleming <tanner.fleming@gmail.com>
2025-01-06 14:29:22 -0600

    feat: arclight e2e media languages (#4772)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/arclight-e2e/src/e2e/media-languages/media-language.spec.ts
+++ apps/arclight-e2e/src/e2e/media-languages/media-language.spec.ts
@@ -1,46 +1,36 @@
 import { expect, test } from '@playwright/test'
 
 import { getBaseUrl } from '../../framework/helpers'
-import testData from '../../utils/testData.json'
+import { getObjectDiff } from '../../utils/media-component-utils'
+import { apiKey, languageId } from '../../utils/testData.json'
 
-test.fixme(
-  'compare single media language between environments',
-  async ({ request }) => {
-    const baseUrl = await getBaseUrl()
-    const testLanguageId = testData.languageIds?.[0]
-
-    const queryParams = new URLSearchParams({
-      apiKey: testData.apiKey
-    })
-
-    const [baseData, compareData] = await Promise.all([
-      request
-        .get(`${baseUrl}/v2/media-languages/${testLanguageId}?${queryParams}`)
-        .then((res) => res.json()),
-      request
-        .get(
-          `https://api.arclight.org/v2/media-languages/${testLanguageId}?${queryParams}`
-        )
-        .then((res) => res.json())
-    ])
-
-    // Compare core fields
-    expect(baseData.languageId).toBe(compareData.languageId)
-    expect(baseData.iso3).toBe(compareData.iso3)
-    expect(baseData.bcp47).toBe(compareData.bcp47)
-    expect(baseData.name).toBe(compareData.name)
-    expect(baseData.counts).toEqual(compareData.counts)
-  }
-)
-
-test('verify 404 for non-existent language', async ({ request }) => {
+test('compare single media language between environments', async ({
+  request
+}) => {
   const baseUrl = await getBaseUrl()
+  const compareUrl = 'https://api.arclight.org'
   const queryParams = new URLSearchParams({
-    apiKey: testData.apiKey
+    apiKey,
+    metadataLanguageTags: 'en'
   })
 
-  const response = await request.get(
-    `${baseUrl}/v2/media-languages/999999?${queryParams}`
-  )
-  expect(response.status()).toBe(404)
+  const [baseResponse, compareResponse] = await Promise.all([
+    request.get(`${baseUrl}/v2/media-languages/${languageId}?${queryParams}`),
+    request.get(`${compareUrl}/v2/media-languages/${languageId}?${queryParams}`)
+  ])
+
+  expect(await baseResponse.ok()).toBe(true)
+  expect(await compareResponse.ok()).toBe(true)
+
+  const baseDataJson = await baseResponse.json()
+  const compareDataJson = await compareResponse.json()
+
+  delete baseDataJson._links
+  delete compareDataJson._links
+
+  const diffs = getObjectDiff(baseDataJson, compareDataJson)
+  expect(
+    diffs,
+    `Differences found in media language ${languageId}`
+  ).toHaveLength(0)
 })
--- apps/arclight-e2e/src/e2e/media-languages/media-languages.spec.ts
+++ apps/arclight-e2e/src/e2e/media-languages/media-languages.spec.ts
@@ -1,117 +1,40 @@
 import { expect, test } from '@playwright/test'
 
 import { getBaseUrl } from '../../framework/helpers'
-import testData from '../../utils/testData.json'
-
-interface MediaLanguage {
-  languageId: number
-  iso3: string
-  bcp47: string
-  name: string
-  nameNative: string | null
-  counts: {
-    speakerCount: { value: number; description: string }
-    countriesCount: { value: number; description: string }
-    series: { value: number; description: string }
-    featureFilm: { value: number; description: string }
-    shortFilm: { value: number; description: string }
-  }
-  audioPreview: {
-    url: string
-    audioBitrate: number
-    audioContainer: string
-    sizeInBytes: number
-  } | null
-  primaryCountryId: string
-  metadataLanguageTag: string
-  _links: {
-    self: {
-      href: string
-    }
-  }
-}
-
-interface MediaLanguagesResponse {
-  page: number
-  limit: number
-  pages: number
-  total: number
-  _embedded: {
-    mediaLanguages: MediaLanguage[]
-  }
-  _links: {
-    self: { href: string }
-    first: { href: string }
-    last: { href: string }
-    next?: { href: string }
-    previous?: { href: string }
-  }
-}
+import {
+  convertArrayToObject,
+  getObjectDiff
+} from '../../utils/media-component-utils'
+import { apiKey, mediaLanguages } from '../../utils/testData.json'
 
 test('compare media languages between environments', async ({ request }) => {
   const baseUrl = await getBaseUrl()
+  const compareUrl = 'https://api.arclight.org'
   const queryParams = new URLSearchParams({
-    apiKey: testData.apiKey,
-    limit: '5000'
+    apiKey,
+    ids: mediaLanguages.join(',')
   })
 
-  const [baseData, compareData] = await Promise.all([
-    request
-      .get(`${baseUrl}/v2/media-languages?${queryParams}`)
-      .then((res) => res.json() as Promise<MediaLanguagesResponse>),
-    request
-      .get(`https://api.arclight.org/v2/media-languages?${queryParams}`)
-      .then((res) => res.json() as Promise<MediaLanguagesResponse>)
+  const [baseResponse, compareResponse] = await Promise.all([
+    request.get(`${baseUrl}/v2/media-languages?${queryParams}`),
+    request.get(`${compareUrl}/v2/media-languages?${queryParams}`)
   ])
 
-  const baseLanguages = baseData._embedded.mediaLanguages
-  const compareLanguages = compareData._embedded.mediaLanguages
+  expect(await baseResponse.ok()).toBe(true)
+  expect(await compareResponse.ok()).toBe(true)
 
-  // Log count differences
-  console.log(`Base environment total: ${baseLanguages.length}`)
-  console.log(`Compare environment total: ${compareLanguages.length}`)
+  const baseData = await baseResponse.json()
+  const compareData = await compareResponse.json()
 
-  // Find languages that exist in base but not in compare
-  const languagesOnlyInBase = baseLanguages.filter(
-    (baseLanguage) =>
-      !compareLanguages.some(
-        (compareLanguage) =>
-          compareLanguage.languageId === baseLanguage.languageId
-      )
+  const baseLanguages = convertArrayToObject(
+    baseData._embedded.mediaLanguages,
+    'languageId'
+  )
+  const compareLanguages = convertArrayToObject(
+    compareData._embedded.mediaLanguages,
+    'languageId'
   )
 
-  // Find languages that exist in compare but not in base
-  const languagesOnlyInCompare = compareLanguages.filter(
-    (compareLanguage) =>
-      !baseLanguages.some(
-        (baseLanguage) => baseLanguage.languageId === compareLanguage.languageId
-      )
-  )
-
-  // Log differences
-  if (languagesOnlyInBase.length > 0) {
-    console.log('\nLanguage IDs only in base environment:')
-    console.log(languagesOnlyInBase.map((lang) => lang.languageId).join(', '))
-  }
-
-  if (languagesOnlyInCompare.length > 0) {
-    console.log('\nLanguage IDs only in compare environment:')
-    console.log(
-      languagesOnlyInCompare.map((lang) => lang.languageId).join(', ')
-    )
-  }
-
-  // Compare each language that exists in both environments
-  baseLanguages.forEach((baseLanguage) => {
-    const compareLanguage = compareLanguages.find(
-      (l) => l.languageId === baseLanguage.languageId
-    )
-
-    if (compareLanguage) {
-      expect(baseLanguage.iso3).toBe(compareLanguage.iso3)
-      expect(baseLanguage.bcp47).toBe(compareLanguage.bcp47)
-      expect(baseLanguage.name).toBe(compareLanguage.name)
-      expect(baseLanguage.counts).toEqual(compareLanguage.counts)
-    }
-  })
+  const diffs = getObjectDiff(baseLanguages, compareLanguages)
+  expect(diffs, 'Differences found in media languages').toHaveLength(0)
 })
