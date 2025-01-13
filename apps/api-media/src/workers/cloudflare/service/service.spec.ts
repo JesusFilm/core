9a7dfc05a40fdaec2279b41c874635651aa6ba15
Mike Allison <dj.mikeallison@gmail.com>
2025-01-13 22:06:49 +0000

    chore: enable cloudflare downloads

---
+++ apps/api-media/src/workers/cloudflare/service/service.spec.ts
@@ -0,0 +1,71 @@
+import Cloudflare from 'cloudflare'
+import { APIPromise } from 'cloudflare/core'
+import { Video } from 'cloudflare/resources/stream/stream'
+import { mockDeep } from 'jest-mock-extended'
+import clone from 'lodash/clone'
+
+import {
+  service
+} from './service'
+import { prismaMock } from '../../../../test/prismaMock'
+
+const mockCloudflare = mockDeep<Cloudflare>()
+
+jest.mock('cloudflare', () => ({
+  __esModule: true,
+  default: jest.fn(() => mockCloudflare)
+}))
+
+describe('workers/cloudflare/service', () => {
+  const originalEnv = clone(process.env)
+
+  beforeEach(() => {
+    process.env = originalEnv
+    process.env.CLOUDFLARE_ACCOUNT_ID = 'cf_account_id'
+    process.env.CLOUDFLARE_STREAM_TOKEN = 'cf_stream_token'
+  })
+
+  afterEach(() => {
+    process.env = originalEnv
+    jest.clearAllMocks()
+  })
+
+  describe('service', () => {
+    it('returns stream create response', async () => {
+      const video = {
+        id: 'streamMediaId',
+        name: 'name',
+        downloadable: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        uploadUrl: null,
+        userId: 'userId',
+        readyToStream: true
+      }
+      prismaMock.cloudflareVideo.findMany.mockResolvedValueOnce([video])
+      prismaMock.cloudflareVideo.update.mockResolvedValueOnce(video)
+      mockCloudflare.post.mockReturnValue({
+        asResponse: jest.fn().mockResolvedValueOnce({
+          headers: {
+            get(header: string) {
+              switch (header) {
+                case 'Location':
+                  return 'https://example.com'
+                case 'stream-media-id':
+                  return 'streamMediaId'
+              }
+            }
+          }
+        })
+      } as unknown as APIPromise<void>)
+      await service(undefined)
+      expect(mockCloudflare.stream.downloads.create).toHaveBeenCalledWith(
+        'streamMediaId',
+        {
+          account_id: 'cf_account_id',
+          body: {}
+        }
+      )
+    })
+  })
+})
