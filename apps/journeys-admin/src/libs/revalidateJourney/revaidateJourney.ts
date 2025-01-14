026240d862351f7a71efe932e973ba254c5eeb57
Kneesal <nisalcottingham@gmail.com>
2025-01-14 23:12:28 +0000

    fix: fail silently but expose response in cases to enforce success

--- apps/journeys-admin/src/libs/revalidateJourney/revaidateJourney.ts
+++ apps/journeys-admin/src/libs/revalidateJourney/revaidateJourney.ts
@@ -1,21 +1,23 @@
-import fetch from 'node-fetch'
+import fetch, { Response } from 'node-fetch'
 
 export async function revalidateJourney(args: {
   slug: string
   hostname?: string
-}): Promise<void> {
-  if (process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN == null) return
+}): Promise<Response | void> {
+  if (
+    process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN == null ||
+    process.env.JOURNEYS_URL
+  )
+    return
   const params = {
     ...args,
     accessToken: process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN
   }
-  try {
-    await fetch(
-      `${process.env.JOURNEYS_URL}/api/revalidate?${new URLSearchParams(
-        params
-      ).toString()}`
-    )
-  } catch (e) {
-    throw new Error(`Error revalidating: ${e.message}`)
-  }
+
+  const response = await fetch(
+    `${process.env.JOURNEYS_URL}/api/revalidate?${new URLSearchParams(
+      params
+    ).toString()}`
+  )
+  if (response.ok) return response
 }
