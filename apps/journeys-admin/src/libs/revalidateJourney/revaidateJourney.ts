d72f26749e8202887bb6edf0b9042027cb87d2a8
Kneesal <nisalcottingham@gmail.com>
2025-01-15 20:41:10 +0000

    fix: tests

--- apps/journeys-admin/src/libs/revalidateJourney/revaidateJourney.ts
+++ apps/journeys-admin/src/libs/revalidateJourney/revaidateJourney.ts
@@ -1,23 +1,25 @@
 import fetch, { Response } from 'node-fetch'
 
-export async function revalidateJourney(args: {
+export async function revalidateJourney({
+  slug,
+  hostname
+}: {
   slug: string
   hostname?: string
 }): Promise<Response | null | undefined> {
   if (
     process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN == null ||
-    process.env.JOURNEYS_URL
+    process.env.JOURNEYS_URL == null
   )
     return null
   const params = {
-    ...args,
-    accessToken: process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN
+    slug,
+    accessToken: process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN,
+    ...(hostname != null && { hostname })
   }
 
   const response = await fetch(
-    `${process.env.JOURNEYS_URL}/api/revalidate?${new URLSearchParams(
-      params
-    ).toString()}`
+    `${process.env.JOURNEYS_URL}/api/revalidate?${new URLSearchParams(params).toString()}`
   )
   if (response.ok) return response
 }
--- apps/journeys-admin/src/libs/revalidateJourney/revalidateJourney.spec.ts
+++ apps/journeys-admin/src/libs/revalidateJourney/revalidateJourney.spec.ts
@@ -1,5 +1,80 @@
+import fetch, { Response } from 'node-fetch'
+import { revalidateJourney } from './revaidateJourney'
+
+jest.mock('node-fetch', () => {
+  const originalModule = jest.requireActual('node-fetch')
+  return {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn()
+  }
+})
+const mockFetch = fetch as jest.MockedFunction<typeof fetch>
+
 describe('revalidateJourney', () => {
-  it('should revalidate a journey', () => {
-    expect(true).toBe(true)
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.clearAllMocks()
+    process.env = { ...OLD_ENV }
+  })
+
+  afterAll(() => {
+    process.env = OLD_ENV
+  })
+
+  it('should revalidate a journey', async () => {
+    process.env.JOURNEYS_URL = 'www.journeys.com'
+    process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN = 'some-token'
+
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      status: 200,
+      json: async () => await Promise.resolve()
+    } as unknown as Response)
+
+    const res = await revalidateJourney({
+      slug: 'my-cool-journey',
+      hostname: 'www.domain.com'
+    })
+    expect(mockFetch).toHaveBeenCalledWith(
+      'www.journeys.com/api/revalidate?slug=my-cool-journey&accessToken=some-token&hostname=www.domain.com'
+    )
+    expect(res).not.toBeNull()
+  })
+
+  it('should not revalidate on missing journey url', async () => {
+    process.env.JOURNEYS_REVALIDATE_ACCESS_TOKEN = 'some-token'
+
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      status: 200,
+      json: async () => await Promise.resolve()
+    } as unknown as Response)
+
+    const res = await revalidateJourney({
+      slug: 'my-cool-journey',
+      hostname: 'www.domain.com'
+    })
+    expect(mockFetch).not.toHaveBeenCalled()
+    expect(res).toBeNull()
+  })
+
+  it('should not revalidate on missing access token', async () => {
+    process.env.JOURNEYS_URL = 'www.journeys.com'
+
+    mockFetch.mockResolvedValueOnce({
+      ok: true,
+      status: 200,
+      json: async () => await Promise.resolve()
+    } as unknown as Response)
+
+    const res = await revalidateJourney({
+      slug: 'my-cool-journey',
+      hostname: 'www.domain.com'
+    })
+    expect(mockFetch).not.toHaveBeenCalled()
+    expect(res).toBeNull()
   })
 })
