ff0397b712a850d2135b70f193a671ac86fe00a6
jianwei1 <jianweichong08@gmail.com>
2025-01-16 00:30:53 +0000

    fix: added socialnode x and socialnodey fields to failing tests

--- apps/api-journeys/src/app/modules/journey/journey.resolver.spec.ts
+++ apps/api-journeys/src/app/modules/journey/journey.resolver.spec.ts
@@ -101,7 +101,9 @@ describe('JourneyResolver', () => {
     showDisplayTitle: null,
     menuButtonIcon: null,
     logoImageBlockId: null,
-    menuStepBlockId: null
+    menuStepBlockId: null,
+    socialNodeX: null,
+    socialNodeY: null
   }
   const journeyWithUserTeam = {
     ...journey,
