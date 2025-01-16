5604036a3eb95cb788a41436620c670082bf4f31
Tanner Fleming <tanner.fleming@gmail.com>
2025-01-16 17:42:51 -0600

    feat: e2e tests no qa (#4781)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ apps/arclight-e2e/src/e2e/metadata-language-tags/index.spec.ts
@@ -0,0 +1,62 @@
+import { expect, test } from '@playwright/test'
+
+import { createQueryParams, getBaseUrl } from '../../framework/helpers'
+
+test.describe('GET /v2/metadata-language-tags', () => {
+  test('returns all metadata language tags', async ({ request }) => {
+    const response = await request.get(
+      `${await getBaseUrl()}/v2/metadata-language-tags?${createQueryParams({})}`
+    )
+
+    expect(response.ok()).toBeTruthy()
+    const data = await response.json()
+
+    expect(data).toMatchObject({
+      _links: {
+        self: {
+          href: expect.stringMatching(/\/v2\/media-languages\/.+/)
+        }
+      },
+      _embedded: {
+        metadataLanguageTags: expect.arrayContaining([
+          {
+            tag: 'en',
+            name: 'English',
+            nameNative: 'English',
+            _links: {
+              self: {
+                href: expect.stringMatching(
+                  /\/v2\/metadata-language-tags\/en\?apiKey=.+/
+                )
+              },
+              metadataLanguageTags: {
+                href: expect.stringMatching(
+                  /\/v2\/metadata-language-tags\?apiKey=.+/
+                )
+              }
+            }
+          },
+          {
+            tag: 'es',
+            name: 'Spanish',
+            nameNative: 'Español',
+            _links: {
+              self: {
+                href: expect.stringMatching(
+                  /\/v2\/metadata-language-tags\/es\?apiKey=.+/
+                )
+              },
+              metadataLanguageTags: {
+                href: expect.stringMatching(
+                  /\/v2\/metadata-language-tags\?apiKey=.+/
+                )
+              }
+            }
+          }
+        ])
+      }
+    })
+
+    expect(data._embedded.metadataLanguageTags).toHaveLength(19)
+  })
+})
