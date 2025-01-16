63dbbfa5774806b34e5bc02416ab98bbd586937c
Tanner <tanner.fleming@gmail.com>
2025-01-16 17:10:27 +0000

    refactor: resources e2e

--- apps/arclight-e2e/src/e2e/resources/index.spec.ts
+++ apps/arclight-e2e/src/e2e/resources/index.spec.ts
@@ -1,126 +1,134 @@
-// import { expect, test } from '@playwright/test'
-// import type { APIRequestContext } from '@playwright/test'
+import { expect, test } from '@playwright/test'
+import type { APIRequestContext } from '@playwright/test'
 
-// import { createQueryParams, getBaseUrl } from '../../framework/helpers'
+import { createQueryParams, getBaseUrl } from '../../framework/helpers'
 
-// interface TestCase {
-//   params: Record<string, any>
-// }
+async function searchResources(
+  request: APIRequestContext,
+  params: Record<string, any>
+) {
+  const queryParams = createQueryParams(params)
+  const response = await request.get(
+    `${await getBaseUrl()}/v2/resources?${queryParams}`
+  )
+  return response
+}
 
-// const testCases = {
-//   searchCountry: {
-//     params: { term: 'United' }
-//   },
-//   searchLanguage: {
-//     params: { term: 'English' }
-//   },
-//   searchVideo: {
-//     params: { term: 'Paper Hats', metadataLanguageTags: 'en' }
-//   },
-//   withCustomApiKey: {
-//     params: { term: 'United', apiKey: 'custom-key' }
-//   }
-// }
+test.describe('GET /v2/resources', () => {
+  test('returns United States in countries', async ({ request }) => {
+    const response = await searchResources(request, { term: 'United' })
+    expect(response.ok()).toBeTruthy()
 
-// async function searchResources(request: APIRequestContext, testCase: TestCase) {
-//   const { params } = testCase
-//   const queryParams = createQueryParams(params)
-//   const response = await request.get(
-//     `${await getBaseUrl()}/v2/resources?${queryParams}`
-//   )
-//   return response
-// }
+    const data = await response.json()
+    expect(data).toMatchObject({
+      _links: {
+        self: {
+          href: expect.stringMatching(
+            /\/v2\/resources\?term=United&bulk=false&apiKey=.+/
+          )
+        }
+      },
+      _embedded: {
+        resources: {
+          mediaCountries: expect.arrayContaining([
+            {
+              countryId: 'US',
+              name: 'United States',
+              continentName: 'North America',
+              metadataLanguageTag: 'en',
+              longitude: -97,
+              latitude: 38,
+              _links: {
+                self: {
+                  href: expect.stringMatching(
+                    /\/v2\/media-countries\/US\?apiKey=.+/
+                  )
+                }
+              }
+            }
+          ])
+        }
+      }
+    })
 
-// test('search returns United States in countries', async ({ request }) => {
-//   const response = await searchResources(request, testCases.searchCountry)
-//   expect(response.ok()).toBeTruthy()
+    const countries = data._embedded.resources.mediaCountries
+    const usCountry = countries.find(
+      (country: any) => country.countryId === 'US'
+    )
+    expect(usCountry).toBeDefined()
+  })
 
-//   const data = await response.json()
-//   expect(data).toMatchObject({
-//     _embedded: {
-//       resources: {
-//         mediaCountries: expect.any(Array)
-//       }
-//     }
-//   })
+  test('returns English in languages', async ({ request }) => {
+    const response = await searchResources(request, { term: 'English' })
+    expect(response.ok()).toBeTruthy()
 
-//   const countries = data._embedded.resources.mediaCountries
-//   const usCountry = countries.find((country: any) =>
-//     country.name.toLowerCase().includes('united states')
-//   )
+    const data = await response.json()
+    expect(data).toMatchObject({
+      _links: {
+        self: {
+          href: expect.stringMatching(
+            /\/v2\/resources\?term=English&bulk=false&apiKey=.+/
+          )
+        }
+      },
+      _embedded: {
+        resources: {
+          mediaLanguages: expect.arrayContaining([
+            {
+              languageId: 529,
+              name: 'English',
+              nameNative: 'English',
+              metadataLanguageTag: 'en',
+              bcp47: 'en',
+              iso3: 'eng',
+              primaryCountryId: 'GB',
+              _links: {
+                self: {
+                  href: expect.stringMatching(
+                    /\/v2\/media-languages\/529\?apiKey=.+/
+                  )
+                }
+              }
+            }
+          ])
+        }
+      }
+    })
 
-//   expect(usCountry).toBeDefined()
-//   expect(usCountry).toMatchObject({
-//     countryId: expect.any(String),
-//     name: expect.stringContaining('United States'),
-//     _links: expect.any(Object)
-//   })
-// })
+    const languages = data._embedded.resources.mediaLanguages
+    const englishLanguage = languages.find(
+      (language: any) => language.languageId === 529
+    )
+    expect(englishLanguage).toBeDefined()
+  })
 
-// test('search returns English in languages', async ({ request }) => {
-//   const response = await searchResources(request, testCases.searchLanguage)
-//   expect(response.ok()).toBeTruthy()
+  test.fixme('search returns Paper Hats in videos', async ({ request }) => {
+    // Skipped because Algolia integration is not ready
+    const response = await searchResources(request, {
+      term: 'Paper Hats',
+      metadataLanguageTags: 'en'
+    })
+    expect(response.ok()).toBeTruthy()
 
-//   const data = await response.json()
-//   expect(data).toMatchObject({
-//     _embedded: {
-//       resources: {
-//         mediaLanguages: expect.any(Array)
-//       }
-//     }
-//   })
+    const data = await response.json()
+    expect(data).toMatchObject({
+      _embedded: {
+        resources: {
+          mediaComponents: expect.any(Array)
+        }
+      }
+    })
 
-//   const languages = data._embedded.resources.mediaLanguages
-//   const englishLanguage = languages.find(
-//     (language: any) => language.languageId === '529'
-//   )
+    const videos = data._embedded.resources.mediaComponents
+    const paperHatsVideo = videos.find((video: any) =>
+      video.title.toLowerCase().includes('paper hats')
+    )
 
-//   expect(englishLanguage).toBeDefined()
-//   expect(englishLanguage).toMatchObject({
-//     languageId: '529',
-//     name: expect.stringContaining('English'),
-//     _links: expect.any(Object)
-//   })
-// })
-
-// test('search returns Paper Hats in videos', async ({ request }) => {
-//   const response = await searchResources(request, testCases.searchVideo)
-//   expect(response.ok()).toBeTruthy()
-
-//   const data = await response.json()
-//   expect(data).toMatchObject({
-//     _embedded: {
-//       resources: {
-//         mediaComponents: expect.any(Array)
-//       }
-//     }
-//   })
-
-//   const videos = data._embedded.resources.mediaComponents
-//   const paperHatsVideo = videos.find((video: any) =>
-//     video.title.toLowerCase().includes('paper hats')
-//   )
-
-//   expect(paperHatsVideo).toBeDefined()
-//   expect(paperHatsVideo).toMatchObject({
-//     mediaComponentId: expect.any(String),
-//     title: expect.stringContaining('Paper Hats'),
-//     _links: expect.any(Object)
-//   })
-// })
-
-// test('search with custom API key', async ({ request }) => {
-//   const response = await searchResources(request, testCases.withCustomApiKey)
-//   expect(response.ok()).toBeTruthy()
-
-//   const data = await response.json()
-//   expect(data).toMatchObject({
-//     _embedded: {
-//       resources: expect.any(Object)
-//     },
-//     _links: expect.any(Object)
-//   })
-
-//   // API key specific checks
-//   expect(data._links.self.href).toContain('apiKey=custom-key')
-// })
+    expect(paperHatsVideo).toBeDefined()
+    expect(paperHatsVideo).toMatchObject({
+      mediaComponentId: expect.any(String),
+      title: expect.stringContaining('Paper Hats'),
+      _links: expect.any(Object)
+    })
+  })
+})
