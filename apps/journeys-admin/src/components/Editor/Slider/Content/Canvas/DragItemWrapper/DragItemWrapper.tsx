f470b6b4dc51dd686097daf3282f6528acb281c7
Dan <dan.chae@tandem.org.nz>
2025-01-28 22:22:05 +0000

    feat: divider width

--- apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/DragItemWrapper/DragItemWrapper.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Content/Canvas/DragItemWrapper/DragItemWrapper.tsx
@@ -113,7 +113,7 @@ export function DragItemWrapper({
         <Divider
           orientation="horizontal"
           sx={{
-            width: '240px',
+            width: '256px',
             height: '2px',
             mt: '6px',
             mb: '6px',
