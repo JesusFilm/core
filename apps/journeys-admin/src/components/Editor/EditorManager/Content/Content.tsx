469b208a0c92c3575864c8c00ca8c997771802d5
Levi Zustiak <levizustiak@gmail.com>
2025-01-29 22:58:52 +0000

    refactor: remove transition from Content

--- apps/journeys-admin/src/components/Editor/EditorManager/Content/Content.tsx
+++ apps/journeys-admin/src/components/Editor/EditorManager/Content/Content.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box'
 import type { ReactElement } from 'react'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { ActiveContent, useEditor } from '@core/journeys/ui/EditorProvider'
 
@@ -22,57 +21,19 @@ export function Content(): ReactElement {
       break
     default:
       content = <Canvas />
-      break
   }
 
   return (
-    <TransitionGroup
-      component={Box}
+    <Box
+      data-testid="Content"
       sx={{
-        position: 'relative',
-        flexGrow: 1,
-        '& .journey-edit-content-component-enter': {
-          opacity: 0
-        },
-        '& .journey-edit-content-component-enter-active': {
-          opacity: 1
-        },
-        '& .journey-edit-content-component-enter-done': {
-          opacity: 1
-        },
-        '& .journey-edit-content-component-exit': {
-          opacity: 1
-        },
-        '& .journey-edit-content-component-exit-active': {
-          opacity: 0
-        }
+        userSelect: 'none',
+        display: 'flex',
+        justifyContent: 'space-between',
+        height: '100%'
       }}
     >
-      <CSSTransition
-        key={activeContent}
-        timeout={600}
-        classNames="journey-edit-content-component"
-      >
-        <Box
-          data-testid="Content"
-          sx={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            userSelect: 'none',
-            display: 'flex',
-            justifyContent: 'space-between',
-            transition: (theme) =>
-              `${theme.transitions.create('opacity', {
-                duration: 300
-              })}`
-          }}
-        >
-          {content}
-        </Box>
-      </CSSTransition>
-    </TransitionGroup>
+      {content}
+    </Box>
   )
 }
