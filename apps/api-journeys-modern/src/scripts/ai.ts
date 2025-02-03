975e2116a48912a9f0ef9cdd06211a5bcd36710a
Kneesal <nisalcottingham@gmail.com>
2025-02-03 22:07:53 +0000

    fix: ai init

--- apps/api-journeys-modern/src/scripts/ai.ts
+++ apps/api-journeys-modern/src/scripts/ai.ts
@@ -1,5 +1,39 @@
-function ai() {
-  console.log('ai')
+import { createInterface } from 'node:readline/promises'
+
+import { openai } from '@ai-sdk/openai'
+import { CoreMessage, streamText } from 'ai'
+import dotenv from 'dotenv'
+
+const terminal = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+dotenv.config()
+
+const messages: CoreMessage[] = []
+
+async function ai() {
+  while (true) {
+    const userInput = await terminal.question('You: ')
+
+    messages.push({ role: 'user', content: userInput })
+
+    const result = streamText({
+      model: openai('gpt-4o'),
+      messages
+    })
+
+    let fullResponse = ''
+    process.stdout.write('\nAssistant: ')
+    for await (const delta of result.textStream) {
+      fullResponse += delta
+      process.stdout.write(delta)
+    }
+    process.stdout.write('\n\n')
+
+    messages.push({ role: 'assistant', content: fullResponse })
+  }
 }
 
-ai()
+ai().catch((e) => console.error(e))
