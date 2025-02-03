da83afb5017486601bd0aaf3accfa7bddb3bb4f9
Kneesal <nisalcottingham@gmail.com>
2025-02-03 21:44:21 +0000

    fix: blocks

--- apps/api-journeys-modern/src/schema/blocks/blocks.zod.ts
+++ apps/api-journeys-modern/src/schema/blocks/blocks.zod.ts
@@ -1,37 +1,23 @@
 import { z } from 'zod'
 
-import { ActionSchema } from './action/action.zod'
 import { ButtonBlockSchema } from './button/button.zod'
 import { CardBlockSchema } from './card/card.zod'
 import { IconBlockSchema } from './icon/icon.zod'
 import { ImageBlockSchema } from './image/image.zod'
 import { RadioOptionBlockSchema } from './radioOption/radioOption.zod'
 import { RadioQuestionBlockSchema } from './radioQuestion/radioQuestion.zod'
-import { SignUpBlockSchema } from './signUpBlock/signUpBlock.zod'
+import { SignUpBlockSchema } from './signUp/signUp.zod'
+import { StepBlockSchema } from './step/step.zod'
 import { TextResponseBlockSchema } from './textResponse/textResponse.zod'
 import { TypographyBlockSchema } from './typography/typography.zod'
 import { VideoBlockSchema } from './video/video.zod'
+import { VideoTriggerBlockSchema } from './videoTrigger/videoTrigger.zod'
 
 const BlockSchema = z.object({
   id: z.string(),
   journeyId: z.string(),
   parentBlockId: z.string().nullable(),
   parentOrder: z.number().nullable()
-})
-
-// StepBlock schema
-const StepBlockSchema = BlockSchema.extend({
-  nextBlockId: z.string().nullable(),
-  locked: z.boolean(),
-  x: z.number().nullable(),
-  y: z.number().nullable(),
-  slug: z.string().nullable()
-})
-
-// VideoTriggerBlock schema
-const VideoTriggerBlockSchema = BlockSchema.extend({
-  triggerStart: z.number(),
-  action: ActionSchema.nullable()
 })
 
 const BlockUnionSchema = z.union([
--- apps/api-journeys-modern/src/schema/blocks/step/step.zod.ts
+++ apps/api-journeys-modern/src/schema/blocks/step/step.zod.ts
@@ -1,15 +1,13 @@
 import { z } from 'zod'
 
-const StepBlock = z.object({
-  id: z.string(),
-  journeyId: z.string(),
+import { BlockSchema } from '../blocks.zod'
+
+const StepBlockSchema = BlockSchema.extend({
   nextBlockId: z.string().nullable(),
   locked: z.boolean(),
-  parentBlockId: z.string().nullable(),
-  parentOrder: z.number().nullable(),
   x: z.number().nullable(),
   y: z.number().nullable(),
   slug: z.string().nullable()
 })
 
-export { StepBlock }
+export { StepBlockSchema }
