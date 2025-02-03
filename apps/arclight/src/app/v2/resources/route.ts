fed53eb50a50c6130a821933e62830d114a34060
Tanner <tanner.fleming@gmail.com>
2025-02-03 18:00:14 +0000

    fix: make env var more specific

--- apps/arclight/src/app/v2/resources/route.ts
+++ apps/arclight/src/app/v2/resources/route.ts
@@ -64,7 +64,7 @@ type AlgoliaLanguageHit = {
 
 async function searchAlgolia(term: string) {
   const appID = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
-  const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_API_KEY
+  const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
   const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_VIDEO_VARIANTS
   if (!appID || !apiKey || !indexName) {
     throw new Error('Algolia environment variables are not set')
