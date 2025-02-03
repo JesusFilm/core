750a0145561c4d14546d688b596b501de65d048e
Edmond Shen <egsmond@gmail.com>
2025-02-03 22:15:25 +0000

    fix: add card describe

--- apps/api-journeys-modern/src/schema/blocks/card/card.zod.ts
+++ apps/api-journeys-modern/src/schema/blocks/card/card.zod.ts
@@ -4,11 +4,16 @@ import { ThemeModeSchema, ThemeNameSchema } from '../../../lib/theme/theme.zod'
 import { BlockSchema } from '../blocks.zod'
 
 const CardBlockSchema = BlockSchema.extend({
-  backgroundColor: z.string().nullable(),
-  coverBlockId: z.string().nullable(),
-  fullscreen: z.boolean(),
-  themeMode: ThemeModeSchema.nullable(),
-  themeName: ThemeNameSchema.nullable()
+  backgroundColor: z
+    .string()
+    .nullable()
+    .describe('Background color of the card block'),
+  coverBlockId: z.string().nullable().describe('ID of the cover block'),
+  fullscreen: z.boolean().describe('Indicates if the card block is fullscreen'),
+  themeMode: ThemeModeSchema.nullable().describe(
+    'Theme mode of the card block'
+  ),
+  themeName: ThemeNameSchema.nullable().describe('Theme name of the card block')
 })
 
 export { CardBlockSchema }
