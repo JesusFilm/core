0a8c6edc00aaaea3997498b76d65d33339e46fa6
Tanner <tanner.fleming@gmail.com>
2025-02-04 14:45:45 +0000

    fix: test for algolia worker

--- apps/api-languages/src/workers/algolia/service/service.spec.ts
+++ apps/api-languages/src/workers/algolia/service/service.spec.ts
@@ -1,94 +1,102 @@
+import { algoliasearch } from 'algoliasearch'
 import clone from 'lodash/clone'
 
-import { prisma } from '../../../lib/prisma'
+import { prismaMock } from '../../../../test/prismaMock'
 
 import { service } from '.'
-
-const prismaFindManySpy = jest
-  .fn()
-  .mockResolvedValueOnce([
-    {
-      id: '185021',
-      bcp47: 'eng'
-    }
-  ])
-  .mockResolvedValueOnce([
-    {
-      id: '185021',
-      iso3: 'eng',
-      bcp47: 'eng',
-      name: [
-        {
-          value: 'English, British',
-          languageId: '185021'
-        }
-      ],
-      countryLanguages: [
-        {
-          countryId: 'GB',
-          primary: true,
-          speakers: 100000
-        }
-      ]
-    }
-  ])
 
 const saveObjectsSpy = jest
   .fn()
   .mockReturnValue({ wait: jest.fn().mockResolvedValue({}) })
 
-const initIndexSpy = jest.fn().mockReturnValue({
-  saveObjects: saveObjectsSpy
-})
-
-jest.mock('algoliasearch', () => {
-  return jest.fn().mockImplementation(() => ({
-    initIndex: initIndexSpy
+jest.mock('algoliasearch', () => ({
+  algoliasearch: jest.fn().mockImplementation(() => ({
+    saveObjects: saveObjectsSpy
   }))
-})
+}))
 
 describe('algolia/service', () => {
   const originalEnv = clone(process.env)
 
   beforeEach(() => {
-    process.env = {
-      ...originalEnv,
-      ALGOLIA_API_KEY: 'key',
-      ALGOLIA_APPLICATION_ID: 'id',
-      ALGOLIA_INDEX: 'languages'
-    }
-    jest.clearAllMocks()
-    jest
-      .spyOn(prisma.language, 'findMany')
-      .mockImplementation(prismaFindManySpy)
-  })
-
-  afterEach(() => {
-    jest.restoreAllMocks()
+    process.env = originalEnv
+    jest.resetModules()
   })
 
   afterAll(() => {
     process.env = originalEnv
+    jest.resetAllMocks()
   })
 
-  it('should throw if environment variables not set', async () => {
-    process.env.ALGOLIA_API_KEY = undefined
+  it('should throw if no API key', async () => {
+    process.env.ALGOLIA_API_KEY_LANGUAGES = undefined
+    process.env.ALGOLIA_APPLICATION_ID = undefined
+    process.env.ALGOLIA_INDEX_LANGUAGES = undefined
     await expect(service()).rejects.toThrow(
       'algolia environment variables not set'
     )
   })
 
-  it('should index languages to Algolia', async () => {
+  it('should sync languages to Algolia', async () => {
+    process.env.ALGOLIA_API_KEY_LANGUAGES = 'key'
+    process.env.ALGOLIA_APPLICATION_ID = 'id'
+    process.env.ALGOLIA_INDEX_LANGUAGES = 'languages'
+
+    prismaMock.language.findMany.mockResolvedValueOnce([
+      {
+        id: '21754',
+        bcp47: 'zh-Hans',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        iso3: null,
+        hasVideos: false,
+        slug: null
+      },
+      {
+        id: '529',
+        bcp47: 'en',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        iso3: null,
+        hasVideos: false,
+        slug: null
+      }
+    ])
+
+    prismaMock.language.findMany.mockResolvedValueOnce([
+      {
+        id: '21754',
+        iso3: 'zho',
+        bcp47: 'zh-Hans',
+        hasVideos: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        slug: null,
+        name: [
+          { value: 'Chinese, Simplified', languageId: '529' },
+          { value: '简体中文', languageId: '21754' }
+        ],
+        countryLanguages: [
+          {
+            primary: true,
+            countryId: 'CN',
+            speakers: 1000000,
+            country: { id: 'CN' }
+          }
+        ]
+      } as any
+    ])
+
     await service()
 
-    expect(prismaFindManySpy).toHaveBeenNthCalledWith(1, {
+    expect(prismaMock.language.findMany).toHaveBeenNthCalledWith(1, {
       select: {
         id: true,
         bcp47: true
       }
     })
 
-    expect(prismaFindManySpy).toHaveBeenNthCalledWith(2, {
+    expect(prismaMock.language.findMany).toHaveBeenNthCalledWith(2, {
       include: {
         name: true,
         countryLanguages: {
@@ -102,25 +110,24 @@ describe('algolia/service', () => {
       }
     })
 
-    expect(initIndexSpy).toHaveBeenCalledWith('languages')
-
-    expect(saveObjectsSpy).toHaveBeenCalledWith([
-      {
-        objectID: '185021',
-        languageId: 185021,
-        iso3: 'eng',
-        bcp47: 'eng',
-        primaryCountryId: 'GB',
-        nameNative: 'English, British',
-        names: [
-          {
-            value: 'English, British',
-            languageId: '185021',
-            bcp47: 'eng'
-          }
-        ],
-        speakersCount: 100000
-      }
-    ])
+    expect(saveObjectsSpy).toHaveBeenCalledWith({
+      indexName: 'languages',
+      objects: [
+        {
+          objectID: '21754',
+          languageId: 21754,
+          iso3: 'zho',
+          bcp47: 'zh-Hans',
+          primaryCountryId: 'CN',
+          nameNative: '简体中文',
+          names: [
+            { value: 'Chinese, Simplified', languageId: '529', bcp47: 'en' },
+            { value: '简体中文', languageId: '21754', bcp47: 'zh-Hans' }
+          ],
+          speakersCount: 1000000
+        }
+      ],
+      waitForTasks: true
+    })
   })
 })
