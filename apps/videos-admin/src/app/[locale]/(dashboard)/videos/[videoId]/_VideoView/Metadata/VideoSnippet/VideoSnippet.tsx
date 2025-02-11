97483738fda143d59ff66bf6b527055c5c1d1b48
Levi Zustiak <levizustiak@gmail.com>
2025-02-11 16:59:27 +0000

    feat: update video snippet to handle missing description

--- apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/VideoSnippet/VideoSnippet.tsx
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/VideoSnippet/VideoSnippet.tsx
@@ -12,6 +12,16 @@ import { CancelButton } from '../../../../../../../../components/CancelButton'
 import { ResizableTextField } from '../../../../../../../../components/ResizableTextField'
 import { SaveButton } from '../../../../../../../../components/SaveButton'
 import { GetAdminVideo_AdminVideo_VideoSnippets as VideoSnippets } from '../../../../../../../../libs/useAdminVideo/useAdminVideo'
+import { useVideoStore } from '../../../../../../../../libs/useVideoStore'
+
+export const CREATE_VIDEO_SNIPPET = graphql(`
+  mutation CreateVideoSnippet($input: VideoTranslationCreateInput!) {
+    videoSnippetCreate(input: $input) {
+      id
+      value
+    }
+  }
+`)
 
 export const UPDATE_VIDEO_SNIPPET = graphql(`
   mutation UpdateVideoSnippet($input: VideoTranslationUpdateInput!) {
@@ -30,25 +40,45 @@ export function VideoSnippet({
   videoSnippets
 }: VideoSnippetProps): ReactElement {
   const t = useTranslations()
+  const [createVideoSnippet] = useMutation(CREATE_VIDEO_SNIPPET)
   const [updateVideoSnippet] = useMutation(UPDATE_VIDEO_SNIPPET)
+
+  const video = useVideoStore((state) => state.video)
 
   const validationSchema = object().shape({
     snippet: string().required(t('Snippet is required'))
   })
 
   async function handleUpdateVideoSnippet(values: FormikValues): Promise<void> {
-    if (videoSnippets == null) return
-    await updateVideoSnippet({
-      variables: {
-        input: {
-          id: videoSnippets[0].id,
-          value: values.snippet
+    if (video == null) return
+
+    if (videoSnippets.length === 0) {
+      await createVideoSnippet({
+        variables: {
+          input: {
+            videoId: video.id,
+            value: values.snippet,
+            primary: true,
+            languageId: '529'
+          }
         }
-      }
-    })
+      })
+    } else {
+      await updateVideoSnippet({
+        variables: {
+          input: {
+            id: videoSnippets[0].id,
+            value: values.snippet
+          }
+        }
+      })
+    }
   }
 
-  const snippet = unescape(videoSnippets?.[0].value).replace(/&#13;/g, '\n')
+  const snippet = unescape(videoSnippets?.[0]?.value ?? '').replace(
+    /&#13;/g,
+    '\n'
+  )
 
   return (
     <Formik
