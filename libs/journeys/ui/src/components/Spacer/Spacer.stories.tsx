f088d5bce4dfea655054f2351804c298aa1fdb79
Dan <dan.chae@tandem.org.nz>
2025-02-12 02:29:51 +0000

    feat: spacer story

--- libs/journeys/ui/src/components/Spacer/Spacer.stories.tsx
+++ libs/journeys/ui/src/components/Spacer/Spacer.stories.tsx
@@ -1,6 +1,9 @@
+import Typography from '@mui/material/Typography'
 import { Meta, StoryObj } from '@storybook/react'
 import { ComponentProps } from 'react'
 
+import { JourneyProvider } from '../../libs/JourneyProvider'
+import { JourneyFields as Journey } from '../../libs/JourneyProvider/__generated__/JourneyFields'
 import { journeyUiConfig } from '../../libs/journeyUiConfig'
 import { StoryCard } from '../StoryCard'
 
@@ -16,15 +19,21 @@ type Story = StoryObj<ComponentProps<typeof Spacer>>
 
 const Template: Story = {
   render: ({ ...args }) => (
-    <StoryCard>
-      <Spacer {...args} />
-    </StoryCard>
+    <JourneyProvider
+      value={{ journey: {} as unknown as Journey, variant: 'admin' }}
+    >
+      <StoryCard>
+        <Typography mb={4}>Text above Spacer</Typography>
+        <Spacer {...args} />
+        <Typography>Text below Spacer</Typography>
+      </StoryCard>
+    </JourneyProvider>
   )
 }
 
 export const Default: Story = {
   ...Template,
-  args: { spacing: 100 }
+  args: { spacing: 200 }
 }
 
 export default Demo
