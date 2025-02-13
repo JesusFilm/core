1c338c3e1ce1cd6d45b9181c749f57970946e3ea
Tanner <tanner.fleming@gmail.com>
2025-02-13 17:07:18 +0000

    fix: add batching and retries

--- apis/api-media/src/workers/seed/service/service.ts
+++ apis/api-media/src/workers/seed/service/service.ts
@@ -1,6 +1,6 @@
 import { seedTags } from './tag'
 import { seedTaxonomies } from './taxonomy'
-import { seedVideoLanguages } from './videoLanguage/videoLanguage'
+import { seedVideoLanguages } from './videoLanguage'
 
 export async function service(): Promise<void> {
   await seedTags()
