d041466370fca337d0456d32ef37465b87dd1fc1
Tanner <tanner.fleming@gmail.com>
2025-02-14 18:30:41 +0000

    Revert "fix: update routes to use new calls"
    This reverts commit b62e6a913c3c7bd54e0b66b580f455335dc3fed3.

--- apps/arclight/src/app/v2/media-languages/[languageId]/route.ts
+++ apps/arclight/src/app/v2/media-languages/[languageId]/route.ts
@@ -45,11 +45,9 @@ const GET_LANGUAGE = graphql(`
         primary
         suggested
       }
-      labeledVideoCounts {
-        seriesCount
-        featureFilmCount
-        shortFilmCount
-      }
+      seriesCount
+      featureFilmCount
+      shortFilmCount
     }
   }
 `)
@@ -132,21 +130,21 @@ export async function GET(
           .length,
         description: 'Number of countries'
       },
-      ...(language.labeledVideoCounts.seriesCount > 0 && {
+      ...(language.seriesCount > 0 && {
         series: {
-          value: language.labeledVideoCounts.seriesCount,
+          value: language.seriesCount,
           description: 'Series'
         }
       }),
-      ...(language.labeledVideoCounts.featureFilmCount > 0 && {
+      ...(language.featureFilmCount > 0 && {
         featureFilm: {
-          value: language.labeledVideoCounts.featureFilmCount,
+          value: language.featureFilmCount,
           description: 'Feature Film'
         }
       }),
-      ...(language.labeledVideoCounts.shortFilmCount > 0 && {
+      ...(language.shortFilmCount > 0 && {
         shortFilm: {
-          value: language.labeledVideoCounts.shortFilmCount,
+          value: language.shortFilmCount,
           description: 'Short Film'
         }
       })
