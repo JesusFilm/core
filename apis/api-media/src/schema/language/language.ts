11d50611c2e2c4330c8fca982ed3aa616d0c3b8d
Tanner <tanner.fleming@gmail.com>
2025-02-14 20:21:50 +0000

    fix: add old routes back to pass e2e

--- apis/api-media/src/schema/language/language.ts
+++ apis/api-media/src/schema/language/language.ts
@@ -26,6 +26,45 @@ LabeledVideoCounts.implement({
 Language.implement({
   externalFields: (t) => ({ id: t.id({ nullable: false }) }),
   fields: (t) => ({
+    seriesCount: t.int({
+      nullable: false,
+      resolve: async (parent) => {
+        return await prisma.videoVariant.count({
+          where: {
+            languageId: parent.id,
+            video: {
+              label: 'series'
+            }
+          }
+        })
+      }
+    }),
+    featureFilmCount: t.int({
+      nullable: false,
+      resolve: async (parent) => {
+        return await prisma.videoVariant.count({
+          where: {
+            languageId: parent.id,
+            video: {
+              label: 'featureFilm'
+            }
+          }
+        })
+      }
+    }),
+    shortFilmCount: t.int({
+      nullable: false,
+      resolve: async (parent) => {
+        return await prisma.videoVariant.count({
+          where: {
+            languageId: parent.id,
+            video: {
+              label: 'shortFilm'
+            }
+          }
+        })
+      }
+    }),
     labeledVideoCounts: t.field({
       type: LabeledVideoCounts,
       nullable: false,
