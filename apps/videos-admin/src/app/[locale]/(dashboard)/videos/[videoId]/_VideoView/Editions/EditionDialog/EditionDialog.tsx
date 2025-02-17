24bcfe9b679cc000a8e21a7cee7107cb613ed3ac
Levi Zustiak <levizustiak@gmail.com>
2025-02-17 23:02:45 +0000

    feat: add EditionDialog

---
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Editions/EditionDialog/EditionDialog.tsx
@@ -0,0 +1,73 @@
+import { useTranslations } from 'next-intl'
+import { ReactElement } from 'react'
+
+import { Dialog } from '@core/shared/ui/Dialog'
+
+import { EditionCreate } from './EditionCreate'
+import { EditionDelete } from './EditionDelete'
+import { EditionEdit } from './EditionEdit'
+import { EditionView } from './EditionView'
+
+export enum DialogAction {
+  VIEW = 'view',
+  CREATE = 'create',
+  EDIT = 'edit',
+  DELETE = 'delete'
+}
+
+export interface EditionDialogProps {
+  action: DialogAction | null
+  close: () => void
+  edition: any
+}
+
+export function EditionDialog({
+  action,
+  close,
+  edition
+}: EditionDialogProps): ReactElement {
+  const t = useTranslations()
+  const getTitle = (action: DialogAction | null): string => {
+    switch (action) {
+      case DialogAction.VIEW:
+        return t('View Edition')
+      case DialogAction.CREATE:
+        return t('Create Edition')
+      case DialogAction.EDIT:
+        return t('Edit Edition')
+      case DialogAction.DELETE:
+        return t('Delete Edition')
+      default:
+        return ''
+    }
+  }
+
+  const renderContent = () => {
+    switch (action) {
+      case DialogAction.VIEW:
+        return <EditionView edition={edition} />
+      case DialogAction.CREATE:
+        return <EditionCreate close={close} />
+      case DialogAction.EDIT:
+        return <EditionEdit edition={edition} close={close} />
+      case DialogAction.DELETE:
+        return <EditionDelete edition={edition} close={close} />
+      default:
+        return null
+    }
+  }
+
+  return (
+    <Dialog
+      open={action !== null}
+      onClose={close}
+      dialogTitle={{
+        title: getTitle(action),
+        closeButton: true
+      }}
+      divider
+    >
+      {renderContent()}
+    </Dialog>
+  )
+}
