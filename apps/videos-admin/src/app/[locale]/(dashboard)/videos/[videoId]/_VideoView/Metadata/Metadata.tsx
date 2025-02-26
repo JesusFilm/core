b79c3c186386caa1050d214a66cb68e3f58f7888
Kneesal <nisalcottingham@gmail.com>
2025-02-26 01:43:13 +0000

    fix: video editions

--- apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/Metadata.tsx
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Metadata/Metadata.tsx
@@ -26,8 +26,8 @@ export function Metadata({ video }: MetadataProps): ReactElement {
       </Section>
       <Section title={t('Image')} variant="outlined">
         <Stack gap={4}>
-          {/* <VideoImage video={video} />
-          <VideoImageAlt videoImageAlts={video.imageAlt} /> */}
+          <VideoImage video={video} />
+          <VideoImageAlt videoImageAlts={video.imageAlt} />
         </Stack>
       </Section>
       <Section title={t('Short Description')} variant="outlined">
