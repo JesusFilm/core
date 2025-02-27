c0b0852d23528a7a1157bfdcd66f954e5ea3510c
Mike Allison <dj.mikeallison@gmail.com>
2025-02-27 04:39:52 +0000

    feat: media-transcoder

--- apps/journeys-admin/next.config.js
+++ apps/journeys-admin/next.config.js
@@ -91,14 +91,14 @@ const nextConfig = {
     ignoreDuringBuilds: process.env.CI === 'true'
   },
   transpilePackages: ['shared-ui', 'shared-ui-dynamic'],
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/esbuild-linux-64/bin'
+    ]
+  },
   experimental: {
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/esbuild-linux-64/bin'
-      ]
-    },
     fallbackNodePolyfills: false
   }
 }
