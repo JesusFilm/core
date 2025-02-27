055b7834d3e7d2fbc77e0cd415d75ce552e7ae94
Levi Zustiak <levizustiak@gmail.com>
2025-02-27 00:14:44 +0000

    feat: add File component

---
+++ apps/videos-admin/src/components/File/File.stories.tsx
@@ -0,0 +1,59 @@
+import { ComponentPropsWithoutRef } from 'react'
+import { File as FileComponent } from './File'
+import { videosAdminConfig } from '../../libs/storybookConfig'
+import { Meta, StoryObj } from '@storybook/react'
+
+const noop = () => undefined
+
+const textFile = new File(['text file content'], 'file.txt', {
+  type: 'text/plain'
+})
+
+type StoryArgs = ComponentPropsWithoutRef<typeof FileComponent>
+
+const meta = {
+  ...videosAdminConfig,
+  title: 'Videos-Admin/File',
+  component: FileComponent,
+  parameters: {
+    tags: ['!autodocs']
+  }
+} satisfies Meta<StoryArgs>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    file: textFile
+  }
+}
+
+export const WithActions: Story = {
+  args: {
+    file: textFile,
+    actions: {
+      onDelete: noop,
+      onDownload: noop
+    }
+  }
+}
+
+export const TextFile: Story = {
+  args: {
+    file: textFile
+  }
+}
+
+export const ImageFile: Story = {
+  args: {
+    file: new File(['image file content'], 'file.png', { type: 'image/png' })
+  }
+}
+
+export const VideoFile: Story = {
+  args: {
+    file: new File(['video file content'], 'file.mp4', { type: 'video/mp4' })
+  }
+}
