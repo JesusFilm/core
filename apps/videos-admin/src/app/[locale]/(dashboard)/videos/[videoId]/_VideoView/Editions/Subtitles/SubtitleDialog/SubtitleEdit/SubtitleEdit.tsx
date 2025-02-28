6366e73b64de219380d31fa2ba514156f2f2bf5d
Levi Zustiak <levizustiak@gmail.com>
2025-02-28 19:41:03 +0000

    refactor: add request cancellation and support for srt

--- apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Editions/Subtitles/SubtitleDialog/SubtitleEdit/SubtitleEdit.tsx
+++ apps/videos-admin/src/app/[locale]/(dashboard)/videos/[videoId]/_VideoView/Editions/Subtitles/SubtitleDialog/SubtitleEdit/SubtitleEdit.tsx
@@ -2,12 +2,15 @@ import { gql, useMutation } from '@apollo/client'
 import { ResultOf, VariablesOf, graphql } from 'gql.tada'
 import { useTranslations } from 'next-intl'
 import { useSnackbar } from 'notistack'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useRef, useState } from 'react'
 
+import { GetAdminVideo_AdminVideo_VideoEditions } from '../../../../../../../../../../libs/useAdminVideo/useAdminVideo'
 import { useCreateR2AssetMutation } from '../../../../../../../../../../libs/useCreateR2Asset'
 import { useDeleteR2AssetMutation } from '../../../../../../../../../../libs/useDeleteR2Asset'
 import { useVideo } from '../../../../../../../../../../libs/VideoProvider'
+import { ArrayElement } from '../../../../../../../../../../types/array-types'
 import { SubtitleForm, SubtitleValidationSchema } from '../../SubtitleForm'
+import { getSubtitleR2Path } from '../getSubtitleR2Path'
 
 export const UPDATE_VIDEO_SUBTITLE = graphql(`
   mutation UpdateVideoSubtitle($input: VideoSubtitleUpdateInput!) {
@@ -32,11 +35,6 @@ export type UpdateVideoSubtitleVariables = VariablesOf<
 >
 export type UpdateVideoSubtitle = ResultOf<typeof UPDATE_VIDEO_SUBTITLE>
 
-const getFileName = (video, edition, languageId, file) => {
-  const extension = file.name.split('.').pop()
-  return `${video.id}/editions/${edition.id}/subtitles/${video.id}_${edition.id}_${languageId}.${extension}`
-}
-
 function extractSubtitleFileName(subtitle: any) {
   if (subtitle.value == null) return null
 
@@ -44,26 +42,22 @@ function extractSubtitleFileName(subtitle: any) {
   return fileName
 }
 
+type Edition = ArrayElement<GetAdminVideo_AdminVideo_VideoEditions>
+type Subtitle = ArrayElement<Edition['videoSubtitles']>
+
 interface SubtitleEditProps {
-  subtitle: any
-  edition: any
-  close: () => void
-  dialogState: {
-    loading: boolean
-    setLoading: (loading: boolean) => void
-  }
+  edition: Edition
+  subtitle: Subtitle
 }
 
 export function SubtitleEdit({
   subtitle,
-  edition,
-  close,
-  dialogState
+  edition
 }: SubtitleEditProps): ReactElement {
   const video = useVideo()
   const t = useTranslations()
   const { enqueueSnackbar } = useSnackbar()
-  const [file, setFile] = useState<File | null>(null)
+  const abortController = useRef<AbortController | null>(null)
   const [loading, setLoading] = useState(false)
 
   const [createR2Asset] = useCreateR2AssetMutation()
@@ -100,13 +94,12 @@ export function SubtitleEdit({
 
     const existingFileName = extractSubtitleFileName(subtitle)
 
-    // Create a function for the subtitle update to avoid code duplication
     const performSubtitleUpdate = async (currentSources: typeof sources) => {
       await updateVideoSubtitle({
         variables: {
           input: {
             id: subtitle.id,
-            edition: edition.name,
+            edition: edition.name ?? '',
             languageId: values.language,
             primary: values.primary,
             ...currentSources
@@ -129,7 +122,7 @@ export function SubtitleEdit({
 
     // Only upload if file exists in the form and the file is not the same as the existing file
     if (file != null && file?.name !== existingFileName) {
-      // Delete the existing file if it exists
+      // Delete existing r2 asset if it exists
       if (subtitle.value != null) {
         await deleteR2Asset({
           variables: {
@@ -143,15 +136,14 @@ export function SubtitleEdit({
         })
       }
 
-      const fileName = getFileName(video, edition, '529', file)
+      const fileName = getSubtitleR2Path(video, edition, '529', file)
 
       await createR2Asset({
         variables: {
           input: {
             videoId: video.id,
             fileName: fileName,
-            // TODO: figure out how to handle srt files
-            contentType: 'text/vtt'
+            contentType: file.type
           }
         },
         onCompleted: async (data) => {
@@ -159,30 +151,44 @@ export function SubtitleEdit({
 
           const uploadUrl = data.cloudflareR2Create.uploadUrl
 
-          const res = await fetch(uploadUrl, {
-            method: 'PUT',
-            body: file,
-            headers: {
-              'Content-Type': 'text/vtt'
-            }
-          })
+          try {
+            const res = await fetch(uploadUrl, {
+              method: 'PUT',
+              body: file,
+              headers: {
+                'Content-Type': file.type
+              },
+              signal: abortController.current?.signal
+            })
 
-          if (res.ok) {
-            if (data.cloudflareR2Create.publicUrl != null) {
-              sources.vttSrc = data.cloudflareR2Create.publicUrl
+            if (res.ok && data.cloudflareR2Create.publicUrl != null) {
+              if (file.type === 'text/vtt') {
+                sources.vttSrc = data.cloudflareR2Create.publicUrl
+              } else if (file.type === 'application/x-subrip') {
+                sources.srtSrc = data.cloudflareR2Create.publicUrl
+              }
+
               await performSubtitleUpdate(sources)
+            } else {
+              enqueueSnackbar(t('Failed to upload subtitle file.'), {
+                variant: 'error'
+              })
             }
-          } else {
+          } catch (e) {
             enqueueSnackbar(t('Failed to upload subtitle file.'), {
               variant: 'error'
             })
           }
         },
-        onError: () => {
-          enqueueSnackbar(t('Failed to create subtitle.'), {
-            variant: 'error'
-          })
-          dialogState.setLoading(false)
+        onError: (e) => {
+          if (e.message.includes('aborted')) {
+            enqueueSnackbar(t('Upload cancelled.'))
+          } else {
+            enqueueSnackbar(t('Failed to create r2 asset.'), {
+              variant: 'error'
+            })
+          }
+          setLoading(false)
         }
       })
     } else {
@@ -192,44 +198,13 @@ export function SubtitleEdit({
   }
 
   useEffect(() => {
-    const fetchSubtitleFile = async () => {
-      setLoading(true)
-      try {
-        // const res = await axios.get(subtitle.value)
-        const res = await fetch(subtitle.value, {
-          mode: 'cors',
-          method: 'GET',
-          headers: {
-            'Response-Type': 'text/vtt'
-          }
-        })
-
-        if (!res.ok) {
-          throw new Error('Failed to fetch subtitle file.')
-        }
-
-        const blob = await res.blob()
-        const file = new File([blob], subtitle.fileName, {
-          type: 'text/vtt'
-        })
-
-        // setFile(file)
-      } catch (e) {
-        enqueueSnackbar(t('Failed to fetch subtitle file.'), {
-          variant: 'error'
-        })
-        console.error(e)
+    return () => {
+      if (abortController.current != null) {
+        abortController.current.abort()
+        abortController.current = null
       }
-
-      setLoading(false)
     }
-
-    if (subtitle.vttSrc != null && file == null && !loading) {
-      void fetchSubtitleFile()
-    }
-
-    console.log({ subtitle })
-  }, [subtitle])
+  }, [])
 
   return (
     <SubtitleForm
@@ -238,10 +213,10 @@ export function SubtitleEdit({
       initialValues={{
         language: subtitle.language.id,
         primary: subtitle.primary,
-        file: null
+        file: null // TODO: update this to the existing file
       }}
       onSubmit={handleSubmit}
-      loading={dialogState.loading}
+      loading={loading}
     />
   )
 }
