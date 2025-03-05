154729bc00f1f9378914af3725f728c1cd7d7814
Mike Allison <dj.mikeallison@gmail.com>
2025-03-04 16:10:49 -0800

    feat: media transcoder (#5391)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- infrastructure/environments/prod/data.tf
+++ infrastructure/environments/prod/data.tf
@@ -53,3 +53,7 @@ data "aws_ssm_parameter" "doppler_journeys_admin_prod_token" {
 data "aws_ssm_parameter" "doppler_core_prod_token" {
   name = "/terraform/prd/DOPPLER_CORE_PROD_TOKEN"
 }
+
+data "aws_ssm_parameter" "doppler_media_transcoder_prod_token" {
+  name = "/terraform/prd/DOPPLER_MEDIA_TRANSCODER_PROD_TOKEN"
+}
