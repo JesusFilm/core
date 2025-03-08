f2bc95aa3db08df099382219923e61277acd9315
Mike Allison <dj.mikeallison@gmail.com>
2025-03-08 02:50:35 +0000

    add ignore

--- apps/journeys-admin-e2e/src/pages/journey-level-actions-page.ts
+++ apps/journeys-admin-e2e/src/pages/journey-level-actions-page.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { expect } from '@playwright/test'
 import dayjs from 'dayjs'
 import type { Page } from 'playwright-core'
