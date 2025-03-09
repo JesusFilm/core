e3eb624d2247edd6d87420717467c1bc9a28523a
Siyang <csiyang@yahoo.co.nz>
2025-03-09 20:25:15 +0000

    fix: test

--- libs/journeys/ui/src/components/Video/VideoControls/VideoSettings/QualityMenu/QualityMenu.tsx
+++ libs/journeys/ui/src/components/Video/VideoControls/VideoSettings/QualityMenu/QualityMenu.tsx
@@ -121,7 +121,7 @@ export function QualityMenu({
             YOUTUBE_QUALITY_LABELS[currentQuality] ?? currentQuality
 
           onQualityChanged(`${t('Auto')} (${displayQuality})`)
-        } else if ('vhs' in tech) {
+        } else if (tech != null && 'vhs' in tech) {
           const activeResolution = qualities.find(
             (q) => q.qualityLevel === qualityLevels.selectedIndex
           )?.resolution
