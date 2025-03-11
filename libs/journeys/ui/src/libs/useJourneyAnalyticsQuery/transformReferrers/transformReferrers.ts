907c402d0ffed1cde4b2eafbf928f44ed76d6ce1
Mike Allison <dj.mikeallison@gmail.com>
2025-03-11 22:25:34 +0000

    refactor: update referrer transformation with type casting and edge property rename

--- libs/journeys/ui/src/libs/useJourneyAnalyticsQuery/transformReferrers/transformReferrers.ts
+++ libs/journeys/ui/src/libs/useJourneyAnalyticsQuery/transformReferrers/transformReferrers.ts
@@ -56,7 +56,7 @@ export function transformReferrers(referrers?: JourneyReferrer[]): {
       nodes.push({
         id: referrer.property,
         type: 'Referrer',
-        data: referrer,
+        data: referrer as unknown as Record<string, unknown>,
         position: { x: -600, y: THREE_NODE_Y_POSITIONS[idx] },
         draggable: false
       })
@@ -66,7 +66,7 @@ export function transformReferrers(referrers?: JourneyReferrer[]): {
         source: referrer.property,
         target: 'SocialPreview',
         type: 'Referrer',
-        updatable: false
+        reconnectable: false
       })
     })
   } else {
@@ -79,7 +79,7 @@ export function transformReferrers(referrers?: JourneyReferrer[]): {
       nodes.push({
         id: referrer.property,
         type: 'Referrer',
-        data: referrer,
+        data: referrer as unknown as Record<string, unknown>,
         position: { x: -600, y: yPos },
         draggable: false
       })
@@ -89,7 +89,7 @@ export function transformReferrers(referrers?: JourneyReferrer[]): {
         source: referrer.property,
         target: 'SocialPreview',
         type: 'Referrer',
-        updatable: false
+        reconnectable: false
       })
     })
   }
