b840b84333a59364f0b2feb1ff704d1e48bbf223
Mike Allison <dj.mikeallison@gmail.com>
2025-03-14 23:26:18 +0000

    refactor: remove commented-out data import worker code
    - Removed the runDataImport function and its associated comments to clean up the codebase.
    - Maintained focus on the main function's execution logic without the data import worker.
    - Improved overall code readability by eliminating unnecessary commented code.

--- apis/api-languages/src/workers/server.ts
+++ apis/api-languages/src/workers/server.ts
@@ -50,41 +50,6 @@ function run({
   )
 }
 
-// Special function for data import worker which needs job data
-// function runDataImport(): void {
-//   const queueName = 'api-languages-data-import'
-//   const jobName = `${queueName}-job`
-
-//   // eslint-disable-next-line no-new
-//   new Worker(
-//     queueName,
-//     async (job) => {
-//       if (job.name !== jobName) return
-
-//       const childLogger = logger.child({
-//         queue: queueName,
-//         jobId: job.id
-//       })
-
-//       logger.info('started data import job')
-
-//       const { service } = await import(
-//         /* webpackChunkName: "data-import" */
-//         './dataImport/service'
-//       )
-
-//       await service(childLogger)
-
-//       logger.info('finished data import job')
-//     },
-//     {
-//       connection
-//     }
-//   )
-
-//   logger.info({ queue: queueName }, 'waiting for data import jobs')
-// }
-
 async function main(): Promise<void> {
   run(
     await import(
