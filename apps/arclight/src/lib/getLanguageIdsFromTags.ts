8c4c056bb909069710cd8d87153e78700b714226
Tanner <tanner.fleming@gmail.com>
2025-03-18 15:38:48 +0000

    fix: streamline languagetags

--- apps/arclight/src/lib/getLanguageIdsFromTags.ts
+++ apps/arclight/src/lib/getLanguageIdsFromTags.ts
@@ -50,48 +50,53 @@ function matchLocales(metadataLanguageTags: string[]): string | undefined {
   return metadataLanguageTags.find((tag) => tag.startsWith('en-'))
 }
 
+async function fetchLanguageId(languageTag: string): Promise<string> {
+  const { data } = await getApolloClient().query<
+    ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
+  >({
+    query: GET_LANGUAGE_ID_FROM_BCP47,
+    variables: { bcp47: languageTag }
+  })
+
+  return data.language?.id ?? ''
+}
+
 export async function getLanguageIdsFromTags(
   metadataLanguageTags: string[]
 ): Promise<LanguageIds | HTTPException> {
-  let metadataLanguageId = '529'
-  let fallbackLanguageId = ''
+  const DEFAULT_LANGUAGE_ID = '529'
 
-  console.log(metadataLanguageTags)
-  if (metadataLanguageTags.length > 0) {
-    const metadataLanguageTag = matchLocales(metadataLanguageTags)
-    console.log(metadataLanguageTag)
+  if (metadataLanguageTags.length === 0) {
+    return {
+      metadataLanguageId: DEFAULT_LANGUAGE_ID,
+      fallbackLanguageId: DEFAULT_LANGUAGE_ID
+    }
+  }
 
-    const { data } = await getApolloClient().query<
-      ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
-    >({
-      query: GET_LANGUAGE_ID_FROM_BCP47,
-      variables: { bcp47: metadataLanguageTag }
+  const metadataLanguageTag = matchLocales(metadataLanguageTags)
+  if (!metadataLanguageTag) {
+    return new HTTPException(400, {
+      message: JSON.stringify({
+        message: `Could not find a suitable language tag from the provided tags: ${metadataLanguageTags.join(', ')}`,
+        logref: 400
+      })
     })
-    if (data.language == null) {
-      const metadataTagsString = metadataLanguageTags.join(', ')
-      return new HTTPException(400, {
-        message: JSON.stringify({
-          message: `Parameter "metadataLanguageTags" of value "${metadataTagsString}" violated a constraint "Not acceptable metadata language tag(s): ${metadataTagsString}"`,
-          logref: 400
-        })
-      })
-    }
-
-    metadataLanguageId = data.language?.id
   }
 
-  if (metadataLanguageTags.length > 1) {
-    const fallbackLanguageTag = matchLocales(metadataLanguageTags)
-    if (fallbackLanguageTag) {
-      const { data } = await getApolloClient().query<
-        ResultOf<typeof GET_LANGUAGE_ID_FROM_BCP47>
-      >({
-        query: GET_LANGUAGE_ID_FROM_BCP47,
-        variables: { bcp47: fallbackLanguageTag }
+  const metadataLanguageId = await fetchLanguageId(metadataLanguageTag)
+  if (!metadataLanguageId) {
+    return new HTTPException(400, {
+      message: JSON.stringify({
+        message: `Parameter "metadataLanguageTags" of value "${metadataLanguageTags.join(', ')}" violated a constraint "Not acceptable metadata language tag(s): ${metadataLanguageTags.join(', ')}"`,
+        logref: 400
       })
-      fallbackLanguageId = data.language?.id ?? ''
-    }
+    })
   }
+
+  const fallbackLanguageId =
+    metadataLanguageTags.length > 1
+      ? await fetchLanguageId(metadataLanguageTag)
+      : DEFAULT_LANGUAGE_ID
 
   return { metadataLanguageId, fallbackLanguageId }
 }
