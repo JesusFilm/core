7f0dd4957250b3c69d25d21e67715e0b35d4a4ab
Levi Zustiak <levizustiak@gmail.com>
2025-03-24 17:01:05 +0000

    feat: use event enum instead of string list

--- apis/api-journeys-modern/src/schema/journeyEventsExportLog/inputs/JourneyEventsExportLogInput.ts
+++ apis/api-journeys-modern/src/schema/journeyEventsExportLog/inputs/JourneyEventsExportLogInput.ts
@@ -1,11 +1,15 @@
 import { builder } from '../../builder'
+import { EventType } from '../enums'
 
 export const JourneyEventsExportLogInput = builder.inputType(
   'JourneyEventsExportLogInput',
   {
     fields: (t) => ({
       journeyId: t.id({ required: true }),
-      eventsFilter: t.stringList(),
+      eventsFilter: t.field({
+        type: [EventType],
+        required: true
+      }),
       dateRangeStart: t.string(),
       dateRangeEnd: t.string()
     })
