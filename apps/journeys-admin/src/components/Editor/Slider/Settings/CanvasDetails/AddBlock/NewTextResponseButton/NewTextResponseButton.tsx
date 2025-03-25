f75dd130ee4d7b1098bc80cf96993e38c6fb3d54
jianwei1 <jianweichong08@gmail.com>
2025-03-25 04:04:40 +0000

    refactor: extracted mutations

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/NewTextResponseButton/NewTextResponseButton.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/NewTextResponseButton/NewTextResponseButton.tsx
@@ -33,102 +33,12 @@ import {
   removeBlocksFromCache,
   restoreBlocksToCache
 } from './utils/cacheUtils'
-
-export const TEXT_RESPONSE_BLOCK_CREATE = gql`
-  ${TEXT_RESPONSE_FIELDS}
-  mutation TextResponseBlockCreate($input: TextResponseBlockCreateInput!) {
-    textResponseBlockCreate(input: $input) {
-      id
-      parentBlockId
-      parentOrder
-      ...TextResponseFields
-    }
-  }
-`
-
-export const TEXT_RESPONSE_WITH_BUTTON_CREATE = gql`
-  ${TEXT_RESPONSE_FIELDS}
-  ${BUTTON_FIELDS}
-  ${ICON_FIELDS}
-  mutation TextResponseWithButtonCreate(
-    $textResponseInput: TextResponseBlockCreateInput!
-    $buttonInput: ButtonBlockCreateInput!
-    $iconInput1: IconBlockCreateInput!
-    $iconInput2: IconBlockCreateInput!
-    $buttonId: ID!
-    $journeyId: ID!
-    $buttonUpdateInput: ButtonBlockUpdateInput!
-  ) {
-    textResponse: textResponseBlockCreate(input: $textResponseInput) {
-      ...TextResponseFields
-    }
-    button: buttonBlockCreate(input: $buttonInput) {
-      ...ButtonFields
-    }
-    startIcon: iconBlockCreate(input: $iconInput1) {
-      ...IconFields
-    }
-    endIcon: iconBlockCreate(input: $iconInput2) {
-      ...IconFields
-    }
-    buttonUpdate: buttonBlockUpdate(
-      id: $buttonId
-      journeyId: $journeyId
-      input: $buttonUpdateInput
-    ) {
-      ...ButtonFields
-    }
-  }
-`
-
-export const TEXT_RESPONSE_WITH_BUTTON_DELETE = gql`
-  mutation TextResponseWithButtonDelete(
-    $textResponseId: ID!
-    $buttonId: ID!
-    $startIconId: ID!
-    $endIconId: ID!
-  ) {
-    endIcon: blockDelete(id: $endIconId) {
-      id
-      parentOrder
-    }
-    startIcon: blockDelete(id: $startIconId) {
-      id
-      parentOrder
-    }
-    button: blockDelete(id: $buttonId) {
-      id
-      parentOrder
-    }
-    textResponse: blockDelete(id: $textResponseId) {
-      id
-      parentOrder
-    }
-  }
-`
-
-export const TEXT_RESPONSE_WITH_BUTTON_RESTORE = gql`
-  ${BLOCK_FIELDS}
-  mutation TextResponseWithButtonRestore(
-    $textResponseId: ID!
-    $buttonId: ID!
-    $startIconId: ID!
-    $endIconId: ID!
-  ) {
-    textResponse: blockRestore(id: $textResponseId) {
-      ...BlockFields
-    }
-    button: blockRestore(id: $buttonId) {
-      ...BlockFields
-    }
-    startIcon: blockRestore(id: $startIconId) {
-      ...BlockFields
-    }
-    endIcon: blockRestore(id: $endIconId) {
-      ...BlockFields
-    }
-  }
-`
+import {
+  TEXT_RESPONSE_BLOCK_CREATE,
+  TEXT_RESPONSE_WITH_BUTTON_CREATE,
+  TEXT_RESPONSE_WITH_BUTTON_DELETE,
+  TEXT_RESPONSE_WITH_BUTTON_RESTORE
+} from './utils/mutations'
 
 export function NewTextResponseButton(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
