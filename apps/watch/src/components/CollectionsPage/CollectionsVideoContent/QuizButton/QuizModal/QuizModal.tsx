3589fc7710666002d622b808a871edd2a95070f9
Vlad <hi.vladimir@me.com>
2025-03-26 04:42:19 +0000

    fix: quizz modal loading indicator

--- apps/watch/src/components/CollectionsPage/CollectionsVideoContent/QuizButton/QuizModal/QuizModal.tsx
+++ apps/watch/src/components/CollectionsPage/CollectionsVideoContent/QuizButton/QuizModal/QuizModal.tsx
@@ -1,4 +1,5 @@
 import CloseIcon from '@mui/icons-material/Close'
+import CircularProgress from '@mui/material/CircularProgress'
 import Dialog from '@mui/material/Dialog'
 import IconButton from '@mui/material/IconButton'
 import Slide from '@mui/material/Slide'
@@ -40,9 +41,14 @@ export function QuizModal({ open, onClose }: QuizModalProps): ReactElement {
     >
       <div className="w-full h-[100vh] flex justify-center items-center px-1 sm:px-2 overflow-hidden bg-black/80 backdrop-blur-lg">
         <div className="w-full h-full -mt-6 flex justify-center items-center shadow-3 rounded-md overflow-hidden">
+          <div className="absolute inset-0 flex -z-1 items-center justify-center">
+            <div className="scale-200">
+              <CircularProgress color="secondary" />
+            </div>
+          </div>
           <iframe
             src="https://your.nextstep.is/embed/jf-videos-quizz?expand=false"
-            className="border-0 w-full h-full"
+            className="border-0 w-full h-full z-1"
             title="Next Step of Faith Quiz"
           />
         </div>
@@ -57,7 +63,8 @@ export function QuizModal({ open, onClose }: QuizModalProps): ReactElement {
             position: 'absolute',
             top: 30,
             left: 40,
-            color: 'white'
+            color: 'white',
+            zIndex: 10
           }}
         >
           <CloseIcon />
