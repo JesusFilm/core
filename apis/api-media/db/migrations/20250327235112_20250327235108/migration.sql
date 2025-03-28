bece172c9480bee237e7e85f4c0abc1625e2b0eb
Tanner <tanner.fleming@gmail.com>
2025-03-28 00:21:39 +0000

    fix: migrations in stage

---
+++ apis/api-media/db/migrations/20250327235112_20250327235108/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "BibleCitation" ALTER COLUMN "chapterStart" SET DEFAULT -1,
+ALTER COLUMN "chapterEnd" SET DEFAULT -1,
+ALTER COLUMN "verseStart" SET DEFAULT -1,
+ALTER COLUMN "verseEnd" SET DEFAULT -1;
