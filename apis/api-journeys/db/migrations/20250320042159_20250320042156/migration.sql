70602e4d367bcdd03af911ca9aff308311b8cfdb
Mike Allison <dj.mikeallison@gmail.com>
2025-04-01 23:22:26 +0000

    chore: remove obsolete migration files and clean up database schema

--- apis/api-journeys/db/migrations/20250320042159_20250320042156/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `placeholder` on the `Block` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Block" DROP COLUMN "placeholder";
