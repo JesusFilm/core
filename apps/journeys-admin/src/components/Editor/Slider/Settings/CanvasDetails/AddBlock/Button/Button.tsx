38673acfc73561562e6fad44833678a8f9cc2b80
jianwei1 <jianweichong08@gmail.com>
2025-04-07 23:02:13 +0000

    fix: button styling for mobile

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/Button/Button.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/AddBlock/Button/Button.tsx
@@ -112,7 +112,7 @@ export function Button({
                   justifyContent: 'space-between'
                 }}
               >
-                <Stack direction="row" gap={4} ml={3}>
+                <Stack direction="row" gap={4} ml={3} alignItems="center">
                   {icon}
                   <Typography>{t(value)}</Typography>
                 </Stack>
