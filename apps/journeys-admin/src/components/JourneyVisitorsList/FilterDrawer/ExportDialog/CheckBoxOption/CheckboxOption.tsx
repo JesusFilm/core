966c1070ab44d0ba25513a46d3720847c097e8ea
Tommy <thomasisaactayler@gmail.com>
2025-04-10 00:06:41 +0000

    refactor: clean up CheckboxOption and enhance DateRangePicker functionality
    - Removed unused translation import from CheckboxOption component.
    - Added console log for browser language detection in DateRangePicker component.
    - Updated DateRangePicker to prepare for locale support with a placeholder for adapterLocale.
    - Commented out the date format in DateRangePicker for potential future use.

--- apps/journeys-admin/src/components/JourneyVisitorsList/FilterDrawer/ExportDialog/CheckBoxOption/CheckboxOption.tsx
+++ apps/journeys-admin/src/components/JourneyVisitorsList/FilterDrawer/ExportDialog/CheckBoxOption/CheckboxOption.tsx
@@ -26,8 +26,6 @@ export function CheckboxOption({
   onClick,
   indeterminate
 }: CheckboxOptionProps): ReactElement {
-  const { t } = useTranslation('apps-journeys-admin')
-
   return (
     <FormControlLabel
       control={
