836afe296399f1e44de50704885b4bc31dbbd7e7
Tommy <thomasisaactayler@gmail.com>
2025-04-14 22:27:47 +0000

    fix: update journey level actions test to reflect changes in edit option label

--- apps/journeys-admin-e2e/src/e2e/discover/journey-level-actions.spec.ts
+++ apps/journeys-admin-e2e/src/e2e/discover/journey-level-actions.spec.ts
@@ -38,7 +38,7 @@ test.describe('Journey level actions', () => {
     await journeyPage.clickCreateCustomJourney() // click the create custom journey button
     await journeyPage.createAndVerifyCustomJourney() // creating the custom journey and verifing the created journey is updated in the active tab list
     await journeyLevelActions.clickThreeDotOfCreatedJourney(journeyName) // clicking on the three dot of created journey in the journey list
-    await journeyLevelActions.clickThreeDotOptions('Edit') // clicking on Edit option of the three dot options
+    await journeyLevelActions.clickThreeDotOptions('Edit Details') // clicking on Edit option of the three dot options
     await journeyPage.clickThreeDotBtnOfCustomJourney() // clicking on the three dot at top right corner of the custom journey page
     await journeyPage.clickEditDetailsInThreeDotOptions() // clicking on the title option of the three dot options
     await journeyLevelActions.enterTitle() // renaming the title on the title field in the 'edit title' popup
