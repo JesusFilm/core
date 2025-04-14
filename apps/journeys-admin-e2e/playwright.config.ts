0c3c061039bce061eaa2ef8f445710ac1db33d49
Kiran Chilakamarri <kiran.redhat@gmail.com>
2025-04-15 06:46:52 +1200

    test: remove fullyParallel

--- apps/journeys-admin-e2e/playwright.config.ts
+++ apps/journeys-admin-e2e/playwright.config.ts
@@ -13,8 +13,6 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   timeout: 3 * 60 * 1000,
   testDir: './src/e2e',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
