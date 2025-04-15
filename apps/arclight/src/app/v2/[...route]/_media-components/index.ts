b4a090be9d05a7259226fd292b59f04f9791c535
Tanner <tanner.fleming@gmail.com>
2025-04-15 19:44:39 +0000

    fix: add caching to media-compenents

--- apps/arclight/src/app/v2/[...route]/_media-components/index.ts
+++ apps/arclight/src/app/v2/[...route]/_media-components/index.ts
@@ -4,9 +4,13 @@ import { HTTPException } from 'hono/http-exception'
 import { timeout } from 'hono/timeout'
 
 import { getApolloClient } from '../../../../lib/apolloClient'
+import { generateCacheKey, getWithStaleCache } from '../../../../lib/cache'
 import { getLanguageIdsFromTags } from '../../../../lib/getLanguageIdsFromTags'
 
 import { mediaComponent } from './[mediaComponentId]'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 const GET_VIDEOS_WITH_FALLBACK = graphql(`
   query GetVideosWithFallback(
@@ -177,154 +181,164 @@ mediaComponents.openapi(route, async (c) => {
 
   const { metadataLanguageId, fallbackLanguageId } = languageResult
 
-  const { data } = await getApolloClient().query<
-    ResultOf<typeof GET_VIDEOS_WITH_FALLBACK>
-  >({
-    query: GET_VIDEOS_WITH_FALLBACK,
-    variables: {
-      metadataLanguageId,
-      fallbackLanguageId,
-      languageIds,
-      limit,
-      offset,
-      labels: subTypes,
-      ...(ids != null ? { ids } : {})
+  const cacheKey = generateCacheKey([
+    'media-components',
+    page.toString(),
+    limit.toString(),
+    ...(ids ?? []),
+    ...(languageIds ?? []),
+    ...metadataLanguageTags
+  ])
+
+  const response = await getWithStaleCache(cacheKey, async () => {
+    const { data } = await getApolloClient().query<
+      ResultOf<typeof GET_VIDEOS_WITH_FALLBACK>
+    >({
+      query: GET_VIDEOS_WITH_FALLBACK,
+      variables: {
+        metadataLanguageId,
+        fallbackLanguageId,
+        languageIds,
+        limit,
+        offset,
+        labels: subTypes,
+        ...(ids != null ? { ids } : {})
+      }
+    })
+
+    const videos = data.videos
+    const total = data.videosCount
+    const queryObject = {
+      ...c.req.query(),
+      page: page.toString(),
+      limit: limit.toString()
     }
-  })
 
-  const videos = data.videos
-  const total = data.videosCount
-  const queryObject = {
-    ...c.req.query(),
-    page: page.toString(),
-    limit: limit.toString()
-  }
+    const filteredVideos = videos.filter(
+      (video) =>
+        video.title[0]?.value != null ||
+        video.fallbackTitle[0]?.value != null ||
+        video.snippet[0]?.value != null ||
+        video.description[0]?.value != null
+    )
+    const lastPage =
+      Math.ceil(total / limit) === 0 ? 1 : Math.ceil(total / limit)
 
-  const filteredVideos = videos.filter(
-    (video) =>
-      video.title[0]?.value != null ||
-      video.fallbackTitle[0]?.value != null ||
-      video.snippet[0]?.value != null ||
-      video.description[0]?.value != null
-  )
-  const lastPage = Math.ceil(total / limit) === 0 ? 1 : Math.ceil(total / limit)
+    const mediaComponents = filteredVideos.map((video) => {
+      const isDownloadable =
+        video.label === 'collection' || video.label === 'series'
+          ? false
+          : (video.variant?.downloadable ?? false)
+      return {
+        mediaComponentId: video.id,
+        componentType: video.variant?.hls != null ? 'content' : 'container',
+        subType: video.label,
+        contentType: video.variant?.hls != null ? 'video' : 'none',
+        imageUrls: {
+          thumbnail:
+            video.images.find((image) => image.thumbnail != null)?.thumbnail ??
+            '',
+          videoStill:
+            video.images.find((image) => image.videoStill != null)
+              ?.videoStill ?? '',
+          mobileCinematicHigh:
+            video.images.find((image) => image.mobileCinematicHigh != null)
+              ?.mobileCinematicHigh ?? '',
+          mobileCinematicLow:
+            video.images.find((image) => image.mobileCinematicLow != null)
+              ?.mobileCinematicLow ?? '',
+          mobileCinematicVeryLow:
+            video.images.find((image) => image.mobileCinematicVeryLow != null)
+              ?.mobileCinematicVeryLow ?? ''
+        },
+        lengthInMilliseconds: video.variant?.lengthInMilliseconds ?? 0,
+        containsCount: video.childrenCount,
+        isDownloadable,
+        downloadSizes: isDownloadable
+          ? {
+              approximateSmallDownloadSizeInBytes:
+                video.variant?.downloads?.find(
+                  ({ quality }) => quality === 'low'
+                )?.size ?? 0,
+              approximateLargeDownloadSizeInBytes:
+                video.variant?.downloads?.find(
+                  ({ quality }) => quality === 'high'
+                )?.size ?? 0
+            }
+          : {},
+        bibleCitations: video.bibleCitations.map((citation) => ({
+          osisBibleBook: citation.osisId,
+          chapterStart: citation.chapterStart,
+          verseStart: citation.verseStart,
+          chapterEnd: citation.chapterEnd,
+          verseEnd: citation.verseEnd
+        })),
+        primaryLanguageId: Number(video.primaryLanguageId),
+        title: video.title[0]?.value ?? video.fallbackTitle[0]?.value ?? '',
+        shortDescription:
+          video.snippet[0]?.value ?? video.fallbackSnippet[0]?.value ?? '',
+        longDescription:
+          video.description[0]?.value ??
+          video.fallbackDescription[0]?.value ??
+          '',
+        studyQuestions: video.studyQuestions.map((question) => question.value),
+        metadataLanguageTag: video.title[0]?.language.bcp47 ?? 'en',
+        ...(expand.includes('languageIds')
+          ? { languageIds: video.availableLanguages.map((id) => Number(id)) }
+          : {})
+      }
+    })
 
-  const mediaComponents = filteredVideos.map((video) => {
-    const isDownloadable =
-      video.label === 'collection' || video.label === 'series'
-        ? false
-        : (video.variant?.downloadable ?? false)
+    const queryString = new URLSearchParams(queryObject).toString()
+    const firstQueryString = new URLSearchParams({
+      ...queryObject,
+      page: '1'
+    }).toString()
+    const lastQueryString = new URLSearchParams({
+      ...queryObject,
+      page: lastPage.toString()
+    }).toString()
+    const nextQueryString = new URLSearchParams({
+      ...queryObject,
+      page: (page + 1).toString()
+    }).toString()
+    const previousQueryString = new URLSearchParams({
+      ...queryObject,
+      page: (page - 1).toString()
+    }).toString()
+
     return {
-      mediaComponentId: video.id,
-      componentType: video.variant?.hls != null ? 'content' : 'container',
-      subType: video.label,
-      contentType: video.variant?.hls != null ? 'video' : 'none',
-      imageUrls: {
-        thumbnail:
-          video.images.find((image) => image.thumbnail != null)?.thumbnail ??
-          '',
-        videoStill:
-          video.images.find((image) => image.videoStill != null)?.videoStill ??
-          '',
-        mobileCinematicHigh:
-          video.images.find((image) => image.mobileCinematicHigh != null)
-            ?.mobileCinematicHigh ?? '',
-        mobileCinematicLow:
-          video.images.find((image) => image.mobileCinematicLow != null)
-            ?.mobileCinematicLow ?? '',
-        mobileCinematicVeryLow:
-          video.images.find((image) => image.mobileCinematicVeryLow != null)
-            ?.mobileCinematicVeryLow ?? ''
-      },
-      lengthInMilliseconds: video.variant?.lengthInMilliseconds ?? 0,
-      containsCount: video.childrenCount,
-      isDownloadable,
-      downloadSizes: isDownloadable
-        ? {
-            approximateSmallDownloadSizeInBytes:
-              video.variant?.downloads?.find(({ quality }) => quality === 'low')
-                ?.size ?? 0,
-            approximateLargeDownloadSizeInBytes:
-              video.variant?.downloads?.find(
-                ({ quality }) => quality === 'high'
-              )?.size ?? 0
+      page,
+      limit,
+      pages: lastPage,
+      total,
+      apiSessionId: '',
+      _links: {
+        self: {
+          href: `http://api.arclight.org/v2/media-components?${queryString}`
+        },
+        first: {
+          href: `http://api.arclight.org/v2/media-components?${firstQueryString}`
+        },
+        last: {
+          href: `http://api.arclight.org/v2/media-components?${lastQueryString}`
+        },
+        ...(page < lastPage && {
+          next: {
+            href: `http://api.arclight.org/v2/media-components?${nextQueryString}`
           }
-        : {},
-      bibleCitations: video.bibleCitations.map((citation) => ({
-        osisBibleBook: citation.osisId,
-        chapterStart: citation.chapterStart,
-        verseStart: citation.verseStart,
-        chapterEnd: citation.chapterEnd,
-        verseEnd: citation.verseEnd
-      })),
-      primaryLanguageId: Number(video.primaryLanguageId),
-      title: video.title[0]?.value ?? video.fallbackTitle[0]?.value ?? '',
-      shortDescription:
-        video.snippet[0]?.value ?? video.fallbackSnippet[0]?.value ?? '',
-      longDescription:
-        video.description[0]?.value ??
-        video.fallbackDescription[0]?.value ??
-        '',
-      studyQuestions: video.studyQuestions.map((question) => question.value),
-      metadataLanguageTag: video.title[0]?.language.bcp47 ?? 'en',
-      ...(expand.includes('languageIds')
-        ? { languageIds: video.availableLanguages.map((id) => Number(id)) }
-        : {})
+        }),
+        ...(page > 1 && {
+          previous: {
+            href: `http://api.arclight.org/v2/media-components?${previousQueryString}`
+          }
+        })
+      },
+      _embedded: {
+        mediaComponents
+      }
     }
   })
 
-  const queryString = new URLSearchParams(queryObject).toString()
-  const firstQueryString = new URLSearchParams({
-    ...queryObject,
-    page: '1'
-  }).toString()
-  const lastQueryString = new URLSearchParams({
-    ...queryObject,
-    page: lastPage.toString()
-  }).toString()
-  const nextQueryString = new URLSearchParams({
-    ...queryObject,
-    page: (page + 1).toString()
-  }).toString()
-  const previousQueryString = new URLSearchParams({
-    ...queryObject,
-    page: (page - 1).toString()
-  }).toString()
-
-  const response = {
-    page,
-    limit,
-    pages: lastPage,
-    total,
-    apiSessionId: '',
-    _links: {
-      self: {
-        href: `http://api.arclight.org/v2/media-components?${queryString}`
-      },
-      first: {
-        href: `http://api.arclight.org/v2/media-components?${firstQueryString}`
-      },
-      last: {
-        href: `http://api.arclight.org/v2/media-components?${lastQueryString}`
-      },
-      ...(page < lastPage
-        ? {
-            next: {
-              href: `http://api.arclight.org/v2/media-components?${nextQueryString}`
-            }
-          }
-        : {}),
-      ...(page > 1
-        ? {
-            previous: {
-              href: `http://api.arclight.org/v2/media-components?${previousQueryString}`
-            }
-          }
-        : {})
-    },
-    _embedded: {
-      mediaComponents
-    }
-  }
   return c.json(response)
 })
