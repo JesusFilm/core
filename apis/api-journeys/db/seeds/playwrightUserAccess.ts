05047c2d285dd363e730dc5061bce8df3233eb2e
Edmond Shen <egsmond@gmail.com>
2025-04-23 21:18:34 +0000

    fix: throw error if missing env

--- apis/api-journeys/db/seeds/playwrightUserAccess.ts
+++ apis/api-journeys/db/seeds/playwrightUserAccess.ts
@@ -16,37 +16,37 @@ const JOURNEY_SLUG = 'playwright-testing-journey'
 const PLAYWRIGHT_USER_DATA = [
   {
     journeyId: '9161c4ef-204d-4be9-8fdf-5bf514c13b9d',
-    userId: process.env.PLAYWRIGHT_USER_ID ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID,
     teamId: TEAM_ID,
     journeySlug: JOURNEY_SLUG
   },
   {
     journeyId: 'b89e62a1-bf4a-4392-a09c-01ac97af5431',
-    userId: process.env.PLAYWRIGHT_USER_ID_2 ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID_2,
     teamId: `${TEAM_ID}-2`,
     journeySlug: `${JOURNEY_SLUG}-2`
   },
   {
     journeyId: '6a8e9056-3fd4-47d0-b370-8e0331eb5785',
-    userId: process.env.PLAYWRIGHT_USER_ID_3 ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID_3,
     teamId: `${TEAM_ID}-3`,
     journeySlug: `${JOURNEY_SLUG}-3`
   },
   {
     journeyId: '6933d8e1-a6dd-4e6e-ae84-23178f62bd38',
-    userId: process.env.PLAYWRIGHT_USER_ID_4 ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID_4,
     teamId: `${TEAM_ID}-4`,
     journeySlug: `${JOURNEY_SLUG}-4`
   },
   {
     journeyId: '8fdc0fa3-0e6c-4c75-8024-fb2f70bf51e3',
-    userId: process.env.PLAYWRIGHT_USER_ID_5 ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID_5,
     teamId: `${TEAM_ID}-5`,
     journeySlug: `${JOURNEY_SLUG}-5`
   },
   {
     journeyId: '1858bb8f-83e0-4bbe-8629-9924abb5c75f',
-    userId: process.env.PLAYWRIGHT_USER_ID_6 ?? '',
+    userId: process.env.PLAYWRIGHT_USER_ID_6,
     teamId: `${TEAM_ID}-6`,
     journeySlug: `${JOURNEY_SLUG}-6`
   }
@@ -137,29 +137,39 @@ export async function playwrightUserAccess(): Promise<void> {
     }
   }
   for (const data of PLAYWRIGHT_USER_DATA) {
-    const {
-      journeyData,
-      teamData,
-      userTeamData,
-      journeyProfileData,
-      userRoleData
-    } = createUserAccessData(
-      data.userId,
-      data.teamId,
-      data.journeySlug,
-      data.journeyId
-    )
+    try {
+      if (data.userId === undefined) {
+        throw new Error(
+          `PLAYWRIGHT_USER_ID is not defined for teamId:  ${data.teamId}, journeySlug: ${data.journeySlug}`
+        )
+      }
 
-    console.log(`giving playwright user access to ${data.userId} started`)
+      const {
+        journeyData,
+        teamData,
+        userTeamData,
+        journeyProfileData,
+        userRoleData
+      } = createUserAccessData(
+        data.userId,
+        data.teamId,
+        data.journeySlug,
+        data.journeyId
+      )
 
-    await createPlayWrightUserAccess(
-      userTeamData,
-      teamData,
-      journeyData,
-      journeyProfileData,
-      userRoleData
-    )
+      console.log(`giving playwright user access to ${data.userId} started`)
 
-    console.log(`giving playwright user access to ${data.userId} ended`)
+      await createPlayWrightUserAccess(
+        userTeamData,
+        teamData,
+        journeyData,
+        journeyProfileData,
+        userRoleData
+      )
+
+      console.log(`giving playwright user access to ${data.userId} ended`)
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
