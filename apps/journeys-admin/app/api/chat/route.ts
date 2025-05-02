3ee026198066bee9625c828470eb62e546b965d4
Kneesal <nisalcottingham@gmail.com>
2025-05-02 03:55:44 +0000

    fix: tool

--- apps/journeys-admin/app/api/chat/route.ts
+++ apps/journeys-admin/app/api/chat/route.ts
@@ -1,15 +1,26 @@
 import { openai } from '@ai-sdk/openai'
+import { gql } from '@apollo/client'
 import { streamText } from 'ai'
 import { NextRequest } from 'next/server'
 
 // Allow streaming responses up to 30 seconds
-import { GET_ADMIN_JOURNEY } from '../../../pages/journeys/[journeyId]'
+import { JOURNEY_FIELDS } from '@core/journeys/ui/JourneyProvider/journeyFields'
+
 import { tools } from '../../../src/libs/ai/tools'
 import { createApolloClient } from '../../../src/libs/apolloClient'
 
 export const maxDuration = 30
 
 export const runtime = 'edge'
+
+const GET_ADMIN_JOURNEY = gql`
+  ${JOURNEY_FIELDS}
+  query GetAdminJourney($id: ID!) {
+    journey: adminJourney(id: $id, idType: databaseId) {
+      ...JourneyFields
+    }
+  }
+`
 
 export async function POST(req: NextRequest) {
   const { messages } = await req.json()
@@ -19,7 +30,6 @@ export async function POST(req: NextRequest) {
     return Response.json({ error: 'Missing token' }, { status: 400 })
 
   const apolloClient = createApolloClient(token.value)
-  console.log(apolloClient)
 
   const result = streamText({
     model: openai('gpt-4'),
