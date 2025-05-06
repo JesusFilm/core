bd7816e0bcf1a781cab965c1af77b8177afcbf55
Siyang <csiyang@yahoo.co.nz>
2025-05-06 00:28:42 +0000

    bring back algolia grid

--- apps/watch/src/components/VideoGrid/AlgoliaVideoGrid/AlgoliaVideoGrid.tsx
+++ apps/watch/src/components/VideoGrid/AlgoliaVideoGrid/AlgoliaVideoGrid.tsx
@@ -9,34 +9,33 @@ import {
 import { VideoGrid, VideoGridProps } from '../VideoGrid'
 
 export function AlgoliaVideoGrid(props: VideoGridProps): ReactElement {
-  // const {
-  //   items: algoliaVideos,
-  //   showMore,
-  //   isLastPage,
-  //   loading,
-  //   noResults,
-  //   sendEvent
-  // } = useAlgoliaVideos<CoreVideo>({ transformItems })
+  const {
+    items: algoliaVideos,
+    showMore,
+    isLastPage,
+    loading,
+    noResults,
+    sendEvent
+  } = useAlgoliaVideos<CoreVideo>({ transformItems })
 
-  // const handleClick =
-  //   (videoId?: string) =>
-  //   (event: MouseEvent): void => {
-  //     event.stopPropagation()
-  //     if (videoId == null) return
-  //     const item = algoliaVideos.filter((item) => item.id === videoId)
-  //     sendEvent('click', item, 'Video Clicked')
-  //   }
+  const handleClick =
+    (videoId?: string) =>
+    (event: MouseEvent): void => {
+      event.stopPropagation()
+      if (videoId == null) return
+      const item = algoliaVideos.filter((item) => item.id === videoId)
+      sendEvent('click', item, 'Video Clicked')
+    }
 
   return (
-    <></>
-    // <VideoGrid
-    //   videos={algoliaVideos}
-    //   loading={loading}
-    //   showMore={showMore}
-    //   hasNextPage={!isLastPage}
-    //   hasNoResults={noResults}
-    //   onCardClick={handleClick}
-    //   {...props}
-    // />
+    <VideoGrid
+      videos={algoliaVideos}
+      loading={loading}
+      showMore={showMore}
+      hasNextPage={!isLastPage}
+      hasNoResults={noResults}
+      onCardClick={handleClick}
+      {...props}
+    />
   )
 }
