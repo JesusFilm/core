49013b7e58ea48ac390eae3321ac26daa9a99b1a
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2025-05-06 13:38:54 +0000

    feat: discussion question components

--- apps/watch/src/components/NewContentPage/Chapters/Chapter/Chapter.tsx
+++ apps/watch/src/components/NewContentPage/Chapters/Chapter/Chapter.tsx
@@ -1,22 +1,42 @@
 import { ReactElement } from 'react'
 
-import { VideoChildFields } from '../../../../../__generated__/VideoChildFields'
+import {
+  VideoChildFields,
+  VideoChildFields_studyQuestions
+} from '../../../../../__generated__/VideoChildFields'
 
 import { ChapterQuestions } from './ChapterQuestions'
-import { ChapterQuotes } from './ChapterQuotes'
 
 interface ChapterProps {
   video: VideoChildFields
 }
 
 export function Chapter({ video }: ChapterProps): ReactElement {
+  const questions =
+    video?.studyQuestions.length > 0
+      ? video.studyQuestions
+      : ([
+          {
+            id: 0,
+            value:
+              'If you could ask the creator of this video a question, what would it be?'
+          }
+        ] as unknown as VideoChildFields_studyQuestions[])
+
   return (
     <div className="py-16 relative scroll-snap-start-always" id={video.slug}>
       <div data-testid="ChapterVideoPlayer"></div>
+<<<<<<< HEAD
       <div
         className="xl:flex w-full z-1 relative"
         data-testid="ChapterContent"
       ></div>
+=======
+      <div className="xl:flex w-full z-1 relative" data-testid="ChapterContent">
+        <ChapterMetadata video={video} />
+        <ChapterQuestions questions={questions} />
+      </div>
+>>>>>>> 69278c0d2 (feat: discussion question components)
     </div>
   )
 }
