e3888088f38498adf0ed9ba013964b0990544402
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2025-05-06 15:57:39 +0000

    test: ChapterCarousel and ChapterCard

--- apps/watch/src/components/NewContentPage/ChapterCarousel/ChapterCard/ChapterCard.tsx
+++ apps/watch/src/components/NewContentPage/ChapterCarousel/ChapterCard/ChapterCard.tsx
@@ -1,38 +1,13 @@
-import { Box, Skeleton } from '@mui/material'
 import Image from 'next/image'
-import NextLink from 'next/link'
 import { ReactElement } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
 
-import { VideoLabel } from '../../../../../__generated__/globalTypes'
 import { VideoChildFields } from '../../../../../__generated__/VideoChildFields'
 
-export function getSlug(
-  containerSlug: string | undefined,
-  label: VideoLabel | undefined,
-  variantSlug: string | undefined
-): string {
-  if (
-    containerSlug != null &&
-    label !== undefined &&
-    ![VideoLabel.collection, VideoLabel.series].includes(label)
-  ) {
-    return `/watch/${containerSlug}.html/${variantSlug ?? ''}.html`
-  } else {
-    const [videoId, languageId] = (variantSlug ?? '').split('/')
-    return `/watch/${videoId}.html/${languageId}.html`
-  }
-}
-
 interface ChapterCardProps {
-  containerSlug?: string
   video: VideoChildFields
 }
 
-export function ChapterCard({
-  containerSlug,
-  video
-}: ChapterCardProps): ReactElement {
+export function ChapterCard({ video }: ChapterCardProps): ReactElement {
   const handleNavigationClick = (slug: string) => {
     const element = document.getElementById(slug)
     if (element) {
@@ -40,19 +15,19 @@ export function ChapterCard({
     }
   }
 
-  return video?.images[0]?.mobileCinematicHigh != null ? (
+  return (
     <div
-      className="relative beveled h-[240px] flex flex-col justify-end w-full rounded-lg overflow-hidden cursor-pointer"
+      className="relative beveled max-w-[200px] h-[240px] flex flex-col justify-end w-full rounded-lg overflow-hidden cursor-pointer"
       onClick={() => handleNavigationClick(video.slug)}
       onKeyDown={(e) => e.key === 'Enter' && handleNavigationClick(video.slug)}
       tabIndex={0}
       role="button"
-      aria-label={`Navigate to ${video.title[0].value}`}
+      aria-label={`Navigate to ${video.slug}`}
       data-testid={`CarouselItem-${video.slug}`}
     >
       <Image
         fill
-        src={video.images[0].mobileCinematicHigh}
+        src={video.images[0].mobileCinematicHigh ?? ''}
         alt={video.imageAlt[0].value}
         className="absolute top-0 w-full h-[150px] object-cover overflow-hidden [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1)_50%,transparent_100%)] [mask-size:cover]"
       />
@@ -71,10 +46,5 @@ export function ChapterCard({
         </h3>
       </div>
     </div>
-  ) : (
-    <Box>
-      {/* TODO: Add proper fallback styles */}
-      <Skeleton />
-    </Box>
   )
 }
