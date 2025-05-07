d2d13b4ba2ac9e99f1b59d05d8faf676f9f1b6dc
Mike Allison <dj.mikeallison@gmail.com>
2025-05-07 23:04:31 +0000

    refactor: remove commented-out code and enhance card block processing
    - Eliminated unused commented-out code to improve code clarity and maintainability.
    - Continued to optimize the processing of card blocks by leveraging asynchronous operations for better performance.

--- apis/api-journeys-modern/src/schema/journeyAiTranslate/getCardBlocksContent/getCardBlocksContent.ts
+++ apis/api-journeys-modern/src/schema/journeyAiTranslate/getCardBlocksContent/getCardBlocksContent.ts
@@ -13,39 +13,6 @@ export async function getCardBlocksContent({
   const cardBlocksChildren = cardBlocks.map(({ id }) =>
     blocks.filter(({ parentBlockId }) => parentBlockId === id)
   )
-  // const result: string[] = []
-  // for (let i = 0; i < cardBlocks.length; i++) {
-  //   const cardBlock = cardBlocks[i]
-  //   let cardResult = `
-  // # Card
-  // - Card ID: ${cardBlock.id}
-  // `
-  //   if (cardBlock.coverBlockId) {
-  //     const coverBlock = cardBlocksChildren[i].find(
-  //       (block) => block.id === cardBlock.coverBlockId
-  //     )
-  //     if (coverBlock) {
-  //       cardResult += await getCoverBlockContent({
-  //         blocks,
-  //         coverBlock
-  //       })
-  //     }
-  //   }
-  //   const orderedBlocks = cardBlocksChildren[i].sort(
-  //     (a, b) => (a.parentOrder ?? 0) - (b.parentOrder ?? 0)
-  //   )
-  //   for (let j = 0; j < orderedBlocks.length; j++) {
-  //     const block = orderedBlocks[j]
-  //     if (block.id === cardBlock.id) {
-  //       continue
-  //     }
-  //     cardResult += await getBlockContent({
-  //       blocks,
-  //       block
-  //     })
-  //   }
-  //   result.push(cardResult)
-  // }
 
   const result = await Promise.all(
     cardBlocks.map(async (cardBlock, i) => {
