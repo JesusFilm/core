507b901d799810ba01db48d9ebf8e50e8f8bca72
Mike Allison <dj.mikeallison@gmail.com>
2025-05-07 16:39:22 -0700

    refactor: parallelize getting cardblock content (#6567)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apis/api-journeys-modern/src/schema/journeyAiTranslate/getCardBlocksContent/getCardBlocksContent.ts
+++ apis/api-journeys-modern/src/schema/journeyAiTranslate/getCardBlocksContent/getCardBlocksContent.ts
@@ -13,39 +13,40 @@ export async function getCardBlocksContent({
   const cardBlocksChildren = cardBlocks.map(({ id }) =>
     blocks.filter(({ parentBlockId }) => parentBlockId === id)
   )
-  const result: string[] = []
-  for (let i = 0; i < cardBlocks.length; i++) {
-    const cardBlock = cardBlocks[i]
-    let cardResult = `
+
+  const result = await Promise.all(
+    cardBlocks.map(async (cardBlock, i) => {
+      let cardResult = `
 # Card
 - Card ID: ${cardBlock.id}
 `
-    if (cardBlock.coverBlockId) {
-      const coverBlock = cardBlocksChildren[i].find(
-        (block) => block.id === cardBlock.coverBlockId
+      if (cardBlock.coverBlockId) {
+        const coverBlock = cardBlocksChildren[i].find(
+          (block) => block.id === cardBlock.coverBlockId
+        )
+        if (coverBlock) {
+          cardResult += await getCoverBlockContent({
+            blocks,
+            coverBlock
+          })
+        }
+      }
+      const orderedBlocks = cardBlocksChildren[i].sort(
+        (a, b) => (a.parentOrder ?? 0) - (b.parentOrder ?? 0)
       )
-      if (coverBlock) {
-        cardResult += await getCoverBlockContent({
+      for (let j = 0; j < orderedBlocks.length; j++) {
+        const block = orderedBlocks[j]
+        if (block.id === cardBlock.id) {
+          continue
+        }
+        cardResult += await getBlockContent({
           blocks,
-          coverBlock
+          block
         })
       }
-    }
-    const orderedBlocks = cardBlocksChildren[i].sort(
-      (a, b) => (a.parentOrder ?? 0) - (b.parentOrder ?? 0)
-    )
-    for (let j = 0; j < orderedBlocks.length; j++) {
-      const block = orderedBlocks[j]
-      if (block.id === cardBlock.id) {
-        continue
-      }
-      cardResult += await getBlockContent({
-        blocks,
-        block
-      })
-    }
-    result.push(cardResult)
-  }
+      return cardResult
+    })
+  )
 
   return result
 }
