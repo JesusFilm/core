1d26ee0367e3f652b2a7c71aa0b99a52e2e0b5db
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2025-05-07 17:48:30 +0000

    feat: rework to support content page

--- apps/watch/src/components/NewContentPage/Chapters/Chapter/Chapter.tsx
+++ apps/watch/src/components/NewContentPage/Chapters/Chapter/Chapter.tsx
@@ -2,7 +2,6 @@ import { ReactElement } from 'react'
 
 import { VideoChildFields } from '../../../../../__generated__/VideoChildFields'
 
-import { ChapterMetadata } from './ChapterMetadata'
 import { ChapterQuestions } from './ChapterQuestions'
 import { ChapterQuotes } from './ChapterQuotes'
 
@@ -14,9 +13,10 @@ export function Chapter({ video }: ChapterProps): ReactElement {
   return (
     <div className="py-16 relative scroll-snap-start-always">
       <div data-testid="ChapterVideoPlayer"></div>
-      <div className="xl:flex w-full z-1 relative" data-testid="ChapterContent">
-        <ChapterMetadata video={video} />
-      </div>
+      <div
+        className="xl:flex w-full z-1 relative"
+        data-testid="ChapterContent"
+      ></div>
     </div>
   )
 }
