5821402a55a07669d0f3d9986f2d0c83b09ab148
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2025-05-12 19:31:35 +0000

    test: add test case

--- apps/watch/src/components/NewContentPage/ContentHero/ContentHeader/ContentHeader.spec.tsx
+++ apps/watch/src/components/NewContentPage/ContentHero/ContentHeader/ContentHeader.spec.tsx
@@ -1,10 +1,12 @@
 import useScrollTrigger from '@mui/material/useScrollTrigger'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 import { ContentHeader } from './ContentHeader'
+import { VideoProvider } from '../../../../libs/videoContext'
 
-// Mock the useScrollTrigger hook
-jest.mock('@mui/material/useScrollTrigger')
+import { videos } from '../../../Videos/__generated__/testData'
+import { userEvent } from '@storybook/test'
+import { MockedProvider } from '@apollo/client/testing'
 
 describe('ContentHeader', () => {
   beforeEach(() => {
@@ -12,14 +14,33 @@ describe('ContentHeader', () => {
   })
 
   it('renders the header with a logo', () => {
-    ;(useScrollTrigger as jest.Mock).mockReturnValue(false)
-
-    render(<ContentHeader />)
+    render(
+      <VideoProvider value={{ content: videos[0] }}>
+        <ContentHeader />
+      </VideoProvider>
+    )
 
     const header = screen.getByRole('img', { name: 'JesusFilm Project' })
     expect(header).toBeInTheDocument()
 
     const link = screen.getByRole('link')
     expect(link).toHaveAttribute('href', 'https://www.jesusfilm.org/watch')
+  })
+
+  it('opens audio language dialog on language button click', async () => {
+    render(
+      <MockedProvider>
+        <VideoProvider value={{ content: videos[0] }}>
+          <ContentHeader />
+        </VideoProvider>
+      </MockedProvider>
+    )
+    const user = userEvent.setup()
+
+    await user.click(screen.getByTestId('LanguageOutlinedIcon'))
+
+    await waitFor(() =>
+      expect(screen.getByRole('combobox')).toHaveValue('English')
+    )
   })
 })
