270b1214cef105cc6c4e56f2e8f161be6c663c54
Mike Allison <dj.mikeallison@gmail.com>
2025-05-16 13:11:04 -0700

    fix: watch 404 on newly published videos (#6653)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/videos-admin/src/app/(dashboard)/videos/[videoId]/_VideoInformation/VideoInformation.spec.tsx
+++ apps/videos-admin/src/app/(dashboard)/videos/[videoId]/_VideoInformation/VideoInformation.spec.tsx
@@ -1,5 +1,5 @@
 import { MockedProvider } from '@apollo/client/testing'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import {
