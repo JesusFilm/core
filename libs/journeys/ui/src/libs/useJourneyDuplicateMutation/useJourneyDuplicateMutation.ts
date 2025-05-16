4424463546151474115d42e5ec521aea984cfa76
Edmond Shen <egsmond@gmail.com>
2025-05-16 03:50:51 +0000

    fix: remove language from mutation

--- libs/journeys/ui/src/libs/useJourneyDuplicateMutation/useJourneyDuplicateMutation.ts
+++ libs/journeys/ui/src/libs/useJourneyDuplicateMutation/useJourneyDuplicateMutation.ts
@@ -14,12 +14,6 @@ export const JOURNEY_DUPLICATE = gql`
   mutation JourneyDuplicate($id: ID!, $teamId: ID!) {
     journeyDuplicate(id: $id, teamId: $teamId) {
       id
-      language {
-        name {
-          primary
-          value
-        }
-      }
     }
   }
 `
