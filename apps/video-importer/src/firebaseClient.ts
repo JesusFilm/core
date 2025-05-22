b1246bc8717e1d71e734dd5d653f06a96bbb3f60
Tanner <tanner.fleming@gmail.com>
2025-05-22 03:58:05 +0000

    fix: firebase client initialization

--- apps/video-importer/src/firebaseClient.ts
+++ apps/video-importer/src/firebaseClient.ts
@@ -1,12 +1,6 @@
 import { getApp, getApps, initializeApp } from 'firebase/app'
 import 'dotenv/config'
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-}
 const requiredEnvVars = [
   'FIREBASE_API_KEY',
   'FIREBASE_AUTH_DOMAIN',
@@ -20,5 +14,11 @@ for (const envVar of requiredEnvVars) {
   }
 }
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.FIREBASE_APP_ID
+}
 export const firebaseClient =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
