fed13cc8f7c0ba1821fdd5a143396c852d236290
Siyang Cao <67619008+csiyang@users.noreply.github.com>
2025-05-28 11:57:05 +1200

    feat: refactor language switcher behind flag (#6756)

--- apps/watch/src/libs/getLanguageIdFromLocale/getLanguageIdFromLocale.ts
+++ apps/watch/src/libs/getLanguageIdFromLocale/getLanguageIdFromLocale.ts
@@ -1,36 +1,4 @@
-/**
- * Type for supported UI language codes based on the i18n config
- */
-const UI_LANGUAGE_CODES = [
-  'en',
-  'es',
-  'fr',
-  'id',
-  'th',
-  'ja',
-  'ko',
-  'ru',
-  'tr',
-  'zh',
-  'zh-Hans-CN'
-]
-
-/**
- * Mapping of UI language codes to their corresponding language IDs
- */
-export const UI_LANGUAGE_IDS: Readonly<Record<string, string>> = {
-  en: '529', // English
-  es: '21028', // Spanish (Latin American)
-  fr: '496', // French
-  id: '16639', // Indonesian (Standard)
-  th: '13169', // Thai
-  ja: '7083', // Japanese
-  ko: '3804', // Korean
-  ru: '3934', // Russian
-  tr: '1942', // Turkish
-  zh: '20615', // Mandarin China
-  'zh-Hans-CN': '21754' // Chinese Simplified
-} as const
+import { LANGUAGE_MAPPINGS } from '../../config/locales'
 
 /**
  * Gets the language ID for a given UI language code.
@@ -44,9 +12,8 @@ export const UI_LANGUAGE_IDS: Readonly<Record<string, string>> = {
 export const getLanguageIdFromLocale = (
   locale: string | undefined | null
 ): string => {
-  if (locale != null && UI_LANGUAGE_CODES.includes(locale)) {
-    return UI_LANGUAGE_IDS[locale]
-  } else {
-    return UI_LANGUAGE_IDS['en']
+  if (locale && LANGUAGE_MAPPINGS[locale]?.languageId) {
+    return LANGUAGE_MAPPINGS[locale].languageId
   }
+  return LANGUAGE_MAPPINGS['en'].languageId
 }
