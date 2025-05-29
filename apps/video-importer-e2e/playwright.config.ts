a0e2b6db843a538030678b1f232dda2735f5300a
Tanner <tanner.fleming@gmail.com>
2025-05-29 04:48:29 +0000

    fix: added some basic tests

--- apps/video-importer-e2e/playwright.config.ts
+++ apps/video-importer-e2e/playwright.config.ts
@@ -2,12 +2,6 @@
 import { defineConfig } from '@playwright/test'
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
@@ -30,22 +24,13 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure'
-    // video: 'retain-on-failure'
-    // video: 'on',
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'cli',
-      testMatch: /.*\.spec\.ts/
+      testMatch: /src\/e2e\/.*\.spec\.ts/
     }
   ]
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
