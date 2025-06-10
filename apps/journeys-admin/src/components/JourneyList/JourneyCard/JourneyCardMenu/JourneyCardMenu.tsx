6940f2e9d5823262a54f17880662420a38b10f46
Tommy <thomasisaactayler@gmail.com>
2025-06-10 02:07:53 +0000

    style: enhance clickable area of button in JourneyCardMenu
    - Added a pseudo-element to increase the clickable area of the button for better user interaction.
    - Adjusted styles to ensure the button remains visually centered and accessible.

--- apps/journeys-admin/src/components/JourneyList/JourneyCard/JourneyCardMenu/JourneyCardMenu.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyCard/JourneyCardMenu/JourneyCardMenu.tsx
@@ -179,7 +179,18 @@ export function JourneyCardMenu({
           alignItems: 'center',
           justifyContent: 'center',
           cursor: 'pointer',
-          position: 'relative'
+          position: 'relative',
+          // increases the clickable area of the button
+          '&::before': {
+            content: '""',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: { xs: '52px', sm: '40px' },
+            height: { xs: '52px', sm: '44px' },
+            zIndex: 1
+          }
         }}
       >
         <IconButton
