8ab44152df698a6b1461dad31de8ff806d864b02
Nisal Cottingham <nisalcottingham@gmail.com>
2025-06-11 03:06:46 +0000

    fix: bible citations and tests

--- apps/watch/src/components/NewContentPage/BibleCitations/BibleCitations.tsx
+++ apps/watch/src/components/NewContentPage/BibleCitations/BibleCitations.tsx
@@ -1,9 +1,3 @@
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
-import { alpha, styled } from '@mui/material/styles'
-import SvgIcon from '@mui/material/SvgIcon'
-import Typography from '@mui/material/Typography'
 import Image from 'next/image'
 import { ReactElement } from 'react'
 import { A11y, FreeMode, Mousewheel } from 'swiper/modules'
@@ -11,72 +5,17 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { VideoContentFields_bibleCitations as BibleCitation } from '../../../../__generated__/VideoContentFields'
 
-const DEFAULT_COLLECTION_ID = '4924556'
+import { FreeResourceCard, type FreeResourceProps } from './FreeResourceCard'
 
-const StyledSwiper = styled(Swiper)({})
-const StyledSwiperSlide = styled(SwiperSlide)({ maxWidth: '400px' })
-const CitationCard = styled(Stack)({
-  boxSizing: 'border-box',
-  height: '400px',
-  width: '400px',
-  justifyContent: 'flex-end',
-  borderRadius: 8,
-  maskSize: 'cover',
-  backgroundColor: 'rgba(0, 0, 0, 0.1)',
-  border: '1px solid rgba(255, 255, 255, 0.1)'
-})
-
-interface FreeResourceProps {
-  imageUrl?: string
-  bgColor?: string
-  heading: string
-  text: string
-  cta?: { label: string; onClick: () => void; icon?: typeof SvgIcon }
-}
-
-function FreeResourceCard({
-  imageUrl,
-  bgColor,
-  heading,
-  text,
-  cta
-}: FreeResourceProps): ReactElement {
-  const { icon: CtaIcon } = cta ?? {}
-
-  return (
-    <CitationCard>
-      <Stack sx={{ p: 8, alignItems: 'flex-start' }}>
-        <Typography variant="subtitle1" sx={{ textTransform: 'uppercase' }}>
-          {heading}
-        </Typography>
-        <Typography variant="h5" sx={{ mt: 2, mb: 4 }}>
-          {text}
-        </Typography>
-        {cta != null && (
-          <Button
-            role="link"
-            size="xsmall"
-            startIcon={CtaIcon ? <CtaIcon fontSize="small" /> : undefined}
-            onClick={cta.onClick}
-            sx={{
-              bgcolor: 'common.white',
-              color: 'common.black',
-              borderRadius: 8,
-              fontWeight: 'bold',
-              textTransform: 'uppercase',
-              '&:hover': {
-                backgroundColor: ({ palette }) =>
-                  alpha(palette.common.white, 0.8)
-              }
-            }}
-          >
-            {cta.label}
-          </Button>
-        )}
-      </Stack>
-    </CitationCard>
-  )
-}
+const bibleImages = [
+  'https://images.unsplash.com/photo-1480869799327-03916a613b29?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/16/unsplash_526360a842e20_1.JPG?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1497333558196-daaff02b56d0?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1555892727-55b51e5fceae?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1631125915973-e0d155a14e4e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1659260145900-1ac1afc45dcf?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1535979863199-3c77338429a0?q=80&w=1660&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+]
 
 interface BibleCitationsProps {
   bibleCitations: BibleCitation[]
@@ -88,13 +27,8 @@ export function BibleCitations({
   freeResource
 }: BibleCitationsProps): ReactElement {
   return (
-    <Stack
-      direction="row"
-      spacing={2}
-      sx={{ zIndex: 1 }}
-      data-testid="BibleCitations"
-    >
-      <StyledSwiper
+    <div className="flex flex-row gap-2 z-10" data-testid="BibleCitations">
+      <Swiper
         modules={[Mousewheel, FreeMode, A11y]}
         grabCursor
         observeParents
@@ -105,43 +39,35 @@ export function BibleCitations({
         slidesPerView={'auto'}
         pagination={{ clickable: true }}
         spaceBetween={48}
-        sx={{
-          width: '100%'
-        }}
+        className="w-full"
       >
         {bibleCitations.map((citation, i) => (
-          <StyledSwiperSlide key={i}>
-            <CitationCard>
-              <Box sx={{ p: 8 }}>
-                {/* <Image
-                  fill
-                  src={`https://source.unsplash.com/random`}
-                  alt="Bible Citation"
-                  style={{
-                    objectFit: 'cover'
-                  }}
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                /> */}
-
-                <Typography
-                  sx={{
-                    fontFamily: 'Montserrat',
-                    fontSize: '.75rem',
-                    fontWeight: 'bold',
-                    textTransform: 'uppercase',
-                    color: ({ palette }) => alpha(palette.common.white, 0.8)
-                  }}
-                >{`${citation.bibleBook.name[0].value} ${citation.chapterStart}:${citation.verseStart}`}</Typography>
-              </Box>
-            </CitationCard>
-          </StyledSwiperSlide>
+          <SwiperSlide key={i} className="max-w-[400px]">
+            <div
+              className="relative h-[400px] w-[400px] flex flex-col justify-end rounded-lg overflow-hidden border border-white/10 bg-black/10"
+              style={{ backgroundColor: '#1A1815' }}
+            >
+              <Image
+                fill
+                src={bibleImages[i]}
+                alt="Bible Citation"
+                className="absolute top-0 object-cover overflow-hidden rounded-lg  [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1)_20%,transparent_100%)] [mask-size:cover]"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+              <div className="p-8 z-10">
+                <span className="relative font-bold text-xs uppercase tracking-wider text-white/80">
+                  {`${citation.bibleBook.name[0].value} ${citation.chapterStart}:${citation.verseStart}`}
+                </span>
+              </div>
+            </div>
+          </SwiperSlide>
         ))}
         {freeResource != null && (
-          <StyledSwiperSlide key="free-resource">
+          <SwiperSlide key="free-resource" className="max-w-[400px]">
             <FreeResourceCard {...freeResource} />
-          </StyledSwiperSlide>
+          </SwiperSlide>
         )}
-      </StyledSwiper>
-    </Stack>
+      </Swiper>
+    </div>
   )
 }
