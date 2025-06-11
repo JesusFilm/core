588bbc9176b40bd6b2c40eae72ecf568e5cb2b9c
Mike Allison <dj.mikeallison@gmail.com>
2025-06-11 22:04:26 +0000

    feat: update block types to include shareable classNames for Tailwind CSS integration

--- apis/api-journeys-modern/src/schema/block/radioQuestion/radioQuestion.ts
+++ apis/api-journeys-modern/src/schema/block/radioQuestion/radioQuestion.ts
@@ -1,30 +1,6 @@
 import { builder } from '../../builder'
 import { Block } from '../block'
 
-interface RadioQuestionBlockClassNamesType {
-  self: string
-}
-
-const RadioQuestionBlockClassNamesRef =
-  builder.objectRef<RadioQuestionBlockClassNamesType>(
-    'RadioQuestionBlockClassNames'
-  )
-
-export const RadioQuestionBlockClassNames = builder.objectType(
-  RadioQuestionBlockClassNamesRef,
-  {
-    fields: (t) => ({
-      self: t.string({
-        nullable: false,
-        directives: { shareable: true },
-        description: 'Tailwind class names for the radio question block',
-        resolve: (classNames: RadioQuestionBlockClassNamesType) =>
-          classNames.self
-      })
-    })
-  }
-)
-
 export const RadioQuestionBlock = builder.prismaObject('Block', {
   interfaces: [Block],
   variant: 'RadioQuestionBlock',
@@ -43,13 +19,6 @@ export const RadioQuestionBlock = builder.prismaObject('Block', {
     parentOrder: t.exposeInt('parentOrder', {
       nullable: true,
       directives: { shareable: true }
-    }),
-    classNames: t.field({
-      type: RadioQuestionBlockClassNamesRef,
-      nullable: false,
-      directives: { shareable: true },
-      resolve: ({ classNames }) =>
-        classNames as unknown as RadioQuestionBlockClassNamesType
     })
   })
 })
