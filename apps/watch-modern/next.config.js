653ff7129724d2d37ab76253560e03ded893d315
Siyang <csiyang@yahoo.co.nz>
2025-06-16 04:40:56 +0000

    watch modern cursor rules

--- apps/watch-modern/next.config.js
+++ apps/watch-modern/next.config.js
@@ -64,10 +64,6 @@ const nextConfig = {
       ]
     }
   },
-  // compiler: {
-  //   // For other options, see https://nextjs.org/docs/architecture/nextjs-compiler#emotion
-  //   emotion: true
-  // }
   async redirects() {
     return [
       {
