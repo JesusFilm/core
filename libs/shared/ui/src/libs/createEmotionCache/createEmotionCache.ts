c471e1737ff42a44dccf6eb41e460577e9ffdaf6
Mike Allison <dj.mikeallison@gmail.com>
2025-06-29 08:13:41 +0000

    fix: update Emotion cache key from 'mui' to 'css' for improved compatibility

--- libs/shared/ui/src/libs/createEmotionCache/createEmotionCache.ts
+++ libs/shared/ui/src/libs/createEmotionCache/createEmotionCache.ts
@@ -14,7 +14,7 @@ export function createEmotionCache({
   prepend = false
 }: EmotionCacheProps): EmotionCache {
   return createEmotionCacheNext({
-    key: 'mui',
+    key: 'css',
     prepend,
     stylisPlugins: rtl === true ? [prefixer, rtlPlugin] : [],
     enableCssLayer: true
