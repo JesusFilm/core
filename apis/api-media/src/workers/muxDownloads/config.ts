830ca4ec47fb0b150d029b5ab5f0dbd59f3e5917
Mike Allison <dj.mikeallison@gmail.com>
2025-07-02 01:10:33 +0000

    chore: update mux download schedule to run every 2 hours at 15 minutes past the hour and simplify query conditions

--- apis/api-media/src/workers/muxDownloads/config.ts
+++ apis/api-media/src/workers/muxDownloads/config.ts
@@ -1,6 +1,6 @@
 export const queueName = 'api-media-mux-downloads'
 export const jobName = `${queueName}-job`
 
-// Run every 6 hours to update download sizes from Mux
-const EVERY_6_HOURS = '0 0 */6 * * *'
-export const repeat = EVERY_6_HOURS
+// Run every 2 hours at 15 minutes past the hour to update download sizes from Mux
+const EVERY_2_HOURS_AT_15_MIN = '0 15 */2 * * *'
+export const repeat = EVERY_2_HOURS_AT_15_MIN
