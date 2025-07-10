7da7a90157ada77ed942106d595db33053802665
Siyang <csiyang@yahoo.co.nz>
2025-07-10 01:46:51 +0000

    remove redundant test

--- apps/watch/src/components/VideoContentPage/AudioLanguageButton/AudioLanguageButton.spec.tsx
+++ apps/watch/src/components/VideoContentPage/AudioLanguageButton/AudioLanguageButton.spec.tsx
@@ -6,34 +6,7 @@ import { videos } from '../../Videos/__generated__/testData'
 
 import { AudioLanguageButton } from '.'
 
-// Mock the cookie handler
-jest.mock('../../../libs/cookieHandler', () => ({
-  getCookie: jest.fn()
-}))
-
-const mockGetCookie = jest.mocked(
-  require('../../../libs/cookieHandler').getCookie
-)
-
 describe('AudioLanguageButton', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
-  it('falls back to English when no cookie is found', () => {
-    mockGetCookie.mockReturnValue(undefined)
-
-    const { getByText } = render(
-      <MockedProvider>
-        <VideoProvider value={{ content: videos[0] }}>
-          <AudioLanguageButton componentVariant="button" />
-        </VideoProvider>
-      </MockedProvider>
-    )
-
-    expect(getByText('English')).toBeInTheDocument()
-  })
-
   it('renders audio language as a button', async () => {
     const { getByRole } = render(
       <MockedProvider>
