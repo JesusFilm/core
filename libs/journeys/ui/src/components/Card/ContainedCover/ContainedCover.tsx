b3d483a40edc115d62f14e9fa3f245c313b5428c
Edmond Shen <egsmond@gmail.com>
2025-07-13 22:40:01 +0000

    fix: remove rtl on webkit mask

--- libs/journeys/ui/src/components/Card/ContainedCover/ContainedCover.tsx
+++ libs/journeys/ui/src/components/Card/ContainedCover/ContainedCover.tsx
@@ -281,11 +281,11 @@ export function ContainedCover({
                 pl: { sm: 50 },
                 WebkitMask: {
                   xs: overlayGradient('bottom'),
-                  sm: overlayGradient(rtl ? 'right' : 'left')
+                  sm: overlayGradient('right')
                 },
                 mask: {
                   xs: overlayGradient('bottom'),
-                  sm: overlayGradient(rtl ? 'left' : 'right')
+                  sm: overlayGradient('right')
                 },
                 backgroundColor: `${backgroundColor}`
               }}
