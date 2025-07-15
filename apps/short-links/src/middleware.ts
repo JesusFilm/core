60bd7a37d1547c534dad90db434d9b9a518326a7
Tanner <tanner.fleming@gmail.com>
2025-07-15 21:00:56 +0000

    fix: redirect to arclight

--- apps/short-links/src/middleware.ts
+++ apps/short-links/src/middleware.ts
@@ -17,41 +17,16 @@ export const config = {
 export function middleware(request: NextRequest) {
   const hostname = request.nextUrl.hostname
   const pathname = request.nextUrl.pathname
+  
+  const baseUrl =
+    hostname === 'arc.gt'
+      ? 'https://api.arclight.org'
+      : 'https://core-stage.arclight.org'
 
-  // Handle by convention redirects for arc.gt and stage.arc.gt
+  // Redirect all requests for arc.gt and stg.arc.gt
   if (hostname === 'arc.gt' || hostname === 'stg.arc.gt') {
-    if (
-      pathname.startsWith('/hls/') ||
-      pathname.startsWith('/dl/') ||
-      pathname.startsWith('/dh/') ||
-      pathname.startsWith('/s/')
-    ) {
-      const baseUrl =
-        hostname === 'arc.gt'
-          ? 'https://api.arclight.org'
-          : 'https://core-stage.arclight.org'
-
-      const parts = pathname.split('/')
-      if (parts.length !== 4) {
-        return new NextResponse('Invalid URL format', { status: 400 })
-      }
-
-      const [, type, param1, param2] = parts
-
-      switch (type) {
-        case 'hls':
-          return NextResponse.redirect(
-            `${baseUrl}/hls/${param1}/${param2}`,
-            307
-          )
-        case 'dl':
-          return NextResponse.redirect(`${baseUrl}/dl/${param1}/${param2}`, 307)
-        case 'dh':
-          return NextResponse.redirect(`${baseUrl}/dh/${param1}/${param2}`, 307)
-        case 's':
-          return NextResponse.redirect(`${baseUrl}/s/${param1}/${param2}`, 307)
-      }
-    }
+    const redirectUrl = `${baseUrl}${pathname}${request.nextUrl.search}`
+    return NextResponse.redirect(redirectUrl, 307)
   }
 
   const url = new URL(`/${hostname}${pathname}`, request.url)
