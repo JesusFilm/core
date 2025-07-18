c17797754686759d71b66fe8814aad50054eb382
Kneesal <nisalcottingham@gmail.com>
2025-07-18 01:00:42 +0000

    fix: null check in part 2

--- apps/watch/pages/watch/[part1]/[part2].tsx
+++ apps/watch/pages/watch/[part1]/[part2].tsx
@@ -155,15 +155,19 @@ export const getStaticProps: GetStaticProps<Part2PageProps> = async (
       }
     }
 
-    const { data: videoAudioLanguagesData } = await client.query<
-      GetLanguagesSlug,
-      GetLanguagesSlugVariables
-    >({
-      query: GET_LANGUAGES_SLUG,
-      variables: {
-        id: contentData.content.id
-      }
-    })
+    let videoAudioLanguagesData: GetLanguagesSlug | undefined
+    if (contentData.content.variant?.slug != null) {
+      const { data } = await client.query<
+        GetLanguagesSlug,
+        GetLanguagesSlugVariables
+      >({
+        query: GET_LANGUAGES_SLUG,
+        variables: {
+          id: contentData.content.id
+        }
+      })
+      videoAudioLanguagesData = data
+    }
 
     // required for auto-subtitle
     let subtitleData: GetSubtitles | undefined
