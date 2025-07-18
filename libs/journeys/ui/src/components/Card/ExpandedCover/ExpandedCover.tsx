21d74fed16ae57afa3db0932c2cf72ea80cc54a4
Edmond Shen <egsmond@gmail.com>
2025-07-18 05:02:08 +0000

    fix: undo

--- libs/journeys/ui/src/components/Card/ExpandedCover/ExpandedCover.tsx
+++ libs/journeys/ui/src/components/Card/ExpandedCover/ExpandedCover.tsx
@@ -39,7 +39,7 @@ export function ExpandedCover({
   const background =
     backgroundColor != null
       ? imageBlock?.src != null
-        ? `${backgroundColor}4D`
+        ? backgroundColor
         : backgroundColor
       : 'unset'
 
