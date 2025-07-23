0668e3a9956b12ddfbb29af491cb0fbb5459d6c4
Tataihono Nikora <tataihono.nikora@gmail.com>
2025-07-23 02:21:37 +0000

    refactor: simplify Playwright configuration and update test assertions
    - Removed unused comments and streamlined the Playwright configuration for better clarity.
    - Updated the test assertion in watch.spec.ts to use a more flexible text matching method.

--- apps/watch-modern-e2e/src/e2e/page/watch.spec.ts
+++ apps/watch-modern-e2e/src/e2e/page/watch.spec.ts
@@ -2,7 +2,5 @@ import { expect, test } from '@playwright/test'
 
 test('Watch Page', async ({ page }) => {
   await page.goto('/watch')
-  await expect(page.locator('p')).toHaveText(
-    'Watch, learn and share the gospel in over 2000 languages'
-  )
+  await expect(page.getByText(/Watch.*gospel.*languages/)).toBeVisible()
 })
