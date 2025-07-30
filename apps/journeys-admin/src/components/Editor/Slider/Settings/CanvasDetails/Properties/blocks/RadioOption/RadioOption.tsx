e8a4d97aadafd8bf44598415af83281d59656983
Kneesal <nisalcottingham@gmail.com>
2025-07-30 23:31:25 +0000

    fix: null check

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/RadioOption/RadioOption.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/RadioOption/RadioOption.tsx
@@ -1,50 +1,38 @@
 import Box from '@mui/material/Box'
 import { useTranslation } from 'next-i18next'
-import { ReactElement, useEffect, useMemo } from 'react'
+import { ReactElement, useEffect } from 'react'
 
 import type { TreeBlock } from '@core/journeys/ui/block'
 import { useEditor } from '@core/journeys/ui/EditorProvider'
 import Image3Icon from '@core/shared/ui/icons/Image3'
 import LinkIcon from '@core/shared/ui/icons/Link'
 
-import {
-  BlockFields_RadioOptionBlock as RadioOptionBlock,
-  BlockFields_RadioQuestionBlock as RadioQuestionBlock
-} from '../../../../../../../../../__generated__/BlockFields'
+import { BlockFields_RadioOptionBlock as RadioOptionBlock } from '../../../../../../../../../__generated__/BlockFields'
 import { Accordion } from '../../Accordion'
 import { Action } from '../../controls/Action'
 import { getAction } from '../../controls/Action/utils/actions'
 
 import { RadioOptionImage } from './RadioOptionImage/RadioOptionImage'
 
-export function RadioOption({
-  id,
-  action
-}: TreeBlock<RadioOptionBlock>): ReactElement {
+export function RadioOption(props: TreeBlock<RadioOptionBlock>): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
   const {
-    state: { selectedBlock, selectedStep },
+    state: { selectedBlock },
     dispatch
   } = useEditor()
-  const selectedAction = getAction(t, action?.__typename)
-  const parentBlock = useMemo(() => {
-    return selectedStep?.children.find(
-      (child) => child.id === selectedBlock?.parentBlockId
-    )
-  }, [selectedBlock, selectedStep])
-  const gridView = (parentBlock as RadioQuestionBlock)?.gridView ?? false
+  const selectedAction = getAction(t, props.action?.__typename)
 
   useEffect(() => {
     dispatch({
       type: 'SetSelectedAttributeIdAction',
-      selectedAttributeId: `${id}-radio-option-action`
+      selectedAttributeId: `${props.id}-radio-option-action`
     })
-  }, [dispatch, id])
+  }, [dispatch, props.id])
 
   return (
     <Box data-testid="RadioOptionProperties">
       <Accordion
-        id={`${id}-radio-option-action`}
+        id={`${props.id}-radio-option-action`}
         icon={<LinkIcon />}
         name={t('Action')}
         value={selectedAction.label}
@@ -52,7 +40,7 @@ export function RadioOption({
         <Action />
       </Accordion>
       <Accordion
-        id={`${id}-radio-option-image`}
+        id={`${props.id}-radio-option-image`}
         icon={<Image3Icon />}
         name={t('Background')}
         value={
@@ -60,9 +48,7 @@ export function RadioOption({
           t('No image')
         }
       >
-        <RadioOptionImage
-          radioOptionBlock={selectedBlock as TreeBlock<RadioOptionBlock>}
-        />
+        <RadioOptionImage radioOptionBlock={{ ...props }} />
       </Accordion>
     </Box>
   )
