e22edcd16989764dce881e3a313bc74bbb78e285
Kiran Chilakamarri <kiran.redhat@gmail.com>
2025-07-31 22:56:11 +1200

    chore: update Playwright configurations across multiple applications
    - Increase retry attempts on CI from 2 to 3 for improved test resilience.
    - Set workers to 1 instead of undefined on CI to optimize resource allocation during test execution.

--- apps/short-links-e2e/playwright.config.ts
+++ apps/short-links-e2e/playwright.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
   testDir: './src/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 8 : undefined,
+  retries: process.env.CI ? 3 : 0,
+  workers: process.env.CI ? 8 : 1,
   reporter: 'html',
   use: {
     baseURL:
--- apps/watch-e2e/src/e2e/filters.spec.ts
+++ apps/watch-e2e/src/e2e/filters.spec.ts
@@ -53,7 +53,8 @@ test('Filters', async ({ page }) => {
 
   await page.press('body', 'Tab')
 
-  await page.waitForLoadState('domcontentloaded')
+  // Wait for the URL to be updated with filter parameters
+  await page.waitForURL(/configure.*languageId.*subtitles/, { timeout: 20000 })
 
   await expect(page).toHaveURL(
     '/watch/videos?configure%5BruleContexts%5D%5B0%5D=all_videos_page&menu%5BlanguageId%5D=5848&menu%5Bsubtitles%5D=529'
