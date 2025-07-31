21b57384aded70b3af54e9b81b77303d7a517267
Mike Allison <dj.mikeallison@gmail.com>
2025-07-31 21:19:53 +0000

    feat: introduce Journey and JourneyCollection schemas with related input types and enums, streamline imports in index files

---
+++ apis/api-journeys-modern/src/schema/journey/enums/journeyMenuButtonIcon.ts
@@ -0,0 +1,10 @@
+import { JourneyMenuButtonIcon as PrismaJourneyMenuButtonIcon } from '.prisma/api-journeys-modern-client'
+
+import { builder } from '../../builder'
+
+export const JourneyMenuButtonIcon = builder.enumType(
+  PrismaJourneyMenuButtonIcon,
+  {
+    name: 'JourneyMenuButtonIcon'
+  }
+)
---
+++ apis/api-journeys-modern/src/schema/journey/inputs/journeysFilter.ts
@@ -0,0 +1,13 @@
+import { builder } from '../../builder'
+
+export const JourneysFilter = builder.inputType('JourneysFilter', {
+  fields: (t) => ({
+    featured: t.boolean({ required: false }),
+    template: t.boolean({ required: false }),
+    ids: t.idList({ required: false }),
+    tagIds: t.idList({ required: false }),
+    languageIds: t.idList({ required: false }),
+    limit: t.int({ required: false }),
+    orderByRecent: t.boolean({ required: false })
+  })
+})
