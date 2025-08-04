8d61c14b253fb335ecc420ead14be73dc34adb32
Mike Allison <dj.mikeallison@gmail.com>
2025-08-03 20:02:42 -0700

    refactor: resolve n1 video variant fields #5 (#7324)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apis/api-media/src/schema/video/videoSubtitle/videoSubtitle.ts
+++ apis/api-media/src/schema/video/videoSubtitle/videoSubtitle.ts
@@ -6,7 +6,7 @@ import { Language } from '../../language'
 import { VideoSubtitleCreateInput } from './inputs/videoSubtitleCreate'
 import { VideoSubtitleUpdateInput } from './inputs/videoSubtitleUpdate'
 
-builder.prismaObject('VideoSubtitle', {
+export const VideoSubtitle = builder.prismaObject('VideoSubtitle', {
   fields: (t) => ({
     id: t.exposeID('id', { nullable: false }),
     languageId: t.exposeID('languageId', { nullable: false }),
