06a071d224382dad0f32086c03e542491ede58d5
Kneesal <nisalcottingham@gmail.com>
2025-08-05 00:08:09 +0000

    feat: customize init

---
+++ apps/journeys-admin/pages/templates/[journeyId]/customize.tsx
@@ -0,0 +1,15 @@
+import { GetServerSideProps } from 'next'
+
+function CustomizePage({ journeyId }) {
+  return <div>{'Hello World'}</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  return {
+    props: {
+      journeyId: params?.journeyId
+    }
+  }
+}
+
+export default CustomizePage
