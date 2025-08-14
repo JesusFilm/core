b1952ffd72dea65aaef5f8ff2c8529c2d722e0b8
jianwei1 <jianweichong08@gmail.com>
2025-08-14 21:41:21 +0000

    chore: add health check parameters to ALB target group in locals.tf for arclight and journeys-admin

--- apps/journeys-admin/infrastructure/locals.tf
+++ apps/journeys-admin/infrastructure/locals.tf
@@ -42,7 +42,11 @@ locals {
       dns_name = var.ecs_config.alb.dns_name
     }
     alb_target_group = merge(var.ecs_config.alb_target_group, {
-      port = local.port
+      port                             = local.port
+      health_check_interval            = 5
+      health_check_timeout             = 3
+      health_check_healthy_threshold   = 2
+      health_check_unhealthy_threshold = 2
     })
     auto_scaling = {
       max_capacity = 4
