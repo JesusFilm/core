2aa4f5bed4fff4ad7f4b919146b8991be3c26577
Edmond Shen <egsmond@gmail.com>
2025-08-15 03:43:24 +0000

    fix: revert langfuse env

--- apps/journeys-admin/src/libs/ai/langfuse/server.ts
+++ apps/journeys-admin/src/libs/ai/langfuse/server.ts
@@ -1,7 +1,11 @@
 import { Langfuse } from 'langfuse'
 import { LangfuseExporter } from 'langfuse-vercel'
 
-export const langfuseEnvironment = 'nes600'
+export const langfuseEnvironment =
+  process.env.VERCEL_ENV ??
+  process.env.DD_ENV ??
+  process.env.NODE_ENV ??
+  'development'
 
 export const langfuseExporter = new LangfuseExporter({
   environment: langfuseEnvironment
