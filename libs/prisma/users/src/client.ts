5f3ebff9653202113e7a05de88f9e4bf4198e785
Mike Allison <dj.mikeallison@gmail.com>
2025-08-15 18:21:23 +0000

    fix: update output directory paths in tsconfig and clean up client exports for Prisma analytics and users
    - Changed output directory in tsconfig.lib.json for both analytics and users to correct relative path.
    - Removed unused exports for PrismaClient and Prisma from client.ts files in both analytics and users.

--- libs/prisma/users/src/client.ts
+++ libs/prisma/users/src/client.ts
@@ -1,10 +1,6 @@
 import { Prisma, PrismaClient } from '.prisma/api-users-client'
 
-export * from '.prisma/api-users-client'
-export { Prisma as PrismaUsers }
-export { PrismaClient as PrismaClientUsers }
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
+export * from '.prisma/api-users-client'
 export const prisma = globalForPrisma.prisma || new PrismaClient()
-export const prismaUsers = globalForPrisma.prisma || new PrismaClient()
