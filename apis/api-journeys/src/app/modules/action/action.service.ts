67e96190e5dba3b145f36b5e1019e8d66cc8cac2
Mike Allison <dj.mikeallison@gmail.com>
2025-08-22 18:26:26 +0000

    feat: implement email and navigation action updates with validation

--- apis/api-journeys/src/app/modules/action/action.service.ts
+++ apis/api-journeys/src/app/modules/action/action.service.ts
@@ -15,10 +15,90 @@ import { BlockService } from '../block/block.service'
 
 import { ACTION_UPDATE_RESET } from './actionUpdateReset'
 
+const emailSchema = z.object({
+  email: z.string().email()
+})
 @Injectable()
 export class ActionService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly blockService: BlockService
   ) {}
+
+  async emailActionUpdate(
+    id: string,
+    block: Block,
+    input: EmailActionInput
+  ): Promise<Action> {
+    try {
+      await emailSchema.parse({ email: input.email })
+    } catch {
+      throw new GraphQLError('must be a valid email', {
+        extensions: { code: 'BAD_USER_INPUT' }
+      })
+    }
+
+    return await this.prismaService.action.upsert({
+      where: { parentBlockId: id },
+      create: {
+        ...input,
+        parentBlock: { connect: { id: block.id } }
+      },
+      update: {
+        ...ACTION_UPDATE_RESET,
+        ...input
+      },
+      include: { parentBlock: { include: { action: true } } }
+    })
+  }
+
+  async navigateToBlockActionUpdate(
+    id: string,
+    block: Block,
+    input: NavigateToBlockActionInput
+  ): Promise<Action> {
+    const stepBlock = await this.blockService.findParentStepBlock(
+      block.parentBlockId as string
+    )
+    if (stepBlock != null && stepBlock.id === input.blockId)
+      throw new GraphQLError('blockId cannot be the parent step block id', {
+        extensions: { code: 'BAD_USER_INPUT' }
+      })
+
+    const inputWithBlockConnection = {
+      ...omit(input, 'blockId'),
+      block: { connect: { id: input.blockId } }
+    }
+    return await this.prismaService.action.upsert({
+      where: { parentBlockId: id },
+      create: {
+        ...inputWithBlockConnection,
+        parentBlock: { connect: { id: block.id } }
+      },
+      update: {
+        ...ACTION_UPDATE_RESET,
+        ...inputWithBlockConnection
+      },
+      include: { parentBlock: { include: { action: true } } }
+    })
+  }
+
+  async linkActionUpdate(
+    id: string,
+    block: Block,
+    input: LinkActionInput
+  ): Promise<Action> {
+    return await this.prismaService.action.upsert({
+      where: { parentBlockId: id },
+      create: {
+        ...input,
+        parentBlock: { connect: { id: block.id } }
+      },
+      update: {
+        ...ACTION_UPDATE_RESET,
+        ...input
+      },
+      include: { parentBlock: { include: { action: true } } }
+    })
+  }
 }
