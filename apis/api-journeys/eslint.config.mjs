6a4848d12a92a4aee0409514bd0592e8bb4720e3
Mike Allison <dj.mikeallison@gmail.com>
2025-08-29 14:41:59 -0700

    refactor: standardize eslint configs (#7560)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apis/api-journeys/eslint.config.mjs
+++ apis/api-journeys/eslint.config.mjs
@@ -1,33 +1,7 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import reactPlugin from 'eslint-plugin-react'
-import baseConfig from '../../eslint.config.mjs'
-import eslintPluginReact from 'eslint-plugin-react'
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended
-})
+import apiConfig from '../../libs/shared/eslint/api.mjs'
 
 export default [
-  ...baseConfig,
-  {
-    ignores: [
-      'apis/api-journeys/eslint.config.js',
-      'apis/api-journeys/webpack.config.js'
-    ]
-  },
-  ...compat.extends('plugin:@nx/react-typescript'),
-  {
-    plugins: {
-      react: eslintPluginReact
-    },
-
-    rules: {
-      ...reactPlugin.configs['jsx-runtime'].rules
-    }
-  },
-  { rules: { 'i18next/no-literal-string': 'off' } },
+  ...apiConfig,
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {
@@ -35,9 +9,6 @@ export default [
       '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/require-await': 'off'
-    },
-    languageOptions: {
-      parserOptions: { project: ['apis/api-journeys/tsconfig.*?.json'] }
     }
   }
 ]
