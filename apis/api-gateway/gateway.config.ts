48e239df86b41e9a53a354432d7d8e1cc2633791
Mike Allison <dj.mikeallison@gmail.com>
2025-09-12 02:00:46 +0000

    refactor: remove polling interval for supergraph updates in development configuration

--- apis/api-gateway/gateway.config.ts
+++ apis/api-gateway/gateway.config.ts
@@ -6,8 +6,6 @@ import { commonConfig } from './src/common.config'
 export const gatewayConfig = defineConfig({
   ...commonConfig,
   supergraph: './apis/api-gateway/schema.graphql',
-  // Poll for supergraph changes in development so we don't need restarts
-  pollingInterval: 5_000,
   graphiql: {
     title: '[DEV] api-gateway'
   }
