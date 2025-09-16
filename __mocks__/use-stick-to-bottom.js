320dfe9015faeff78325fdc293da9bb27e86fa4e
Edmond Shen <egsmond@gmail.com>
2025-09-16 21:33:14 +0000

    chore: remove mock for use-stick-to-bottom package

--- __mocks__/use-stick-to-bottom.js
+++
@@ -1,4 +0,0 @@
-// Mock for use-stick-to-bottom package
-export const StickToBottom = ({ children }) => children
-export const useStickToBottomContext = () => ({})
-export default { StickToBottom, useStickToBottomContext }
