4fd3338f03ef99f27fb3572fd0ed4ea16d54c776
Edmond Shen <egsmond@gmail.com>
2025-09-16 02:57:18 +0000

    refactor: remove commented-out code for Langfuse tracing environment

--- apps/journeys/instrumentation.ts
+++ apps/journeys/instrumentation.ts
@@ -3,13 +3,6 @@ import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
 
 import { langfuseEnvironment } from './src/lib/ai/langfuse/server'
 
-// Set the Langfuse tracing environment early in the application lifecycle
-// const langfuseEnvironment =
-//   process.env.VERCEL_ENV ??
-//   process.env.DD_ENV ??
-//   process.env.NODE_ENV ??
-//   'development'
-
 process.env.LANGFUSE_TRACING_ENVIRONMENT = langfuseEnvironment
 
 const shouldExportSpan: ShouldExportSpan = ({ otelSpan }) => {
