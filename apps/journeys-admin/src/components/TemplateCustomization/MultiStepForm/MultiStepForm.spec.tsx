9203740d240531b1e92efafa84d65a7625a48bee
Ur-imazing <urim027@gmail.com>
2025-09-25 04:21:31 +0000

    feat: add Edit3 icon as startIcon for edit manually button

--- apps/journeys-admin/src/components/TemplateCustomization/MultiStepForm/MultiStepForm.spec.tsx
+++ apps/journeys-admin/src/components/TemplateCustomization/MultiStepForm/MultiStepForm.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import { MultiStepForm } from './MultiStepForm'
 import { JourneyFields as Journey } from '../../../../__generated__/JourneyFields'
