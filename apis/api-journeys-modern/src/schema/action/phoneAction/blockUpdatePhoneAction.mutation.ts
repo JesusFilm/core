ac734b3e90ff99fabd0cadfcbb8b0ef350576c81
Siyang <csiyang@yahoo.co.nz>
2025-09-25 23:19:11 +0000

    refactor phone regex

--- apis/api-journeys-modern/src/schema/action/phoneAction/blockUpdatePhoneAction.mutation.ts
+++ apis/api-journeys-modern/src/schema/action/phoneAction/blockUpdatePhoneAction.mutation.ts
@@ -10,7 +10,7 @@ import { canBlockHaveAction } from '../canBlockHaveAction'
 import { PhoneActionInput } from './inputs'
 import { PhoneActionRef } from './phoneAction'
 
-const phoneRegex = /^\+[1-9]\d{1,14}$/
+const phoneRegex = /^(\+[1-9]\d{1,14}|\d{1,15})$/
 
 const ACTION_UPDATE_RESET: Prisma.ActionUpdateInput = {
   url: null,
