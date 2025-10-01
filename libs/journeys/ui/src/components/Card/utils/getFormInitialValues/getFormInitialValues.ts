b4381568367a2b9f36116459c79482813897bad5
Mike Allison <dj.mikeallison@gmail.com>
2025-10-01 00:02:21 +0000

    refactor: remove console log from getFormInitialValues function
    - Eliminated debug console log for initialValues in getFormInitialValues to clean up the code and improve performance.

--- libs/journeys/ui/src/components/Card/utils/getFormInitialValues/getFormInitialValues.ts
+++ libs/journeys/ui/src/components/Card/utils/getFormInitialValues/getFormInitialValues.ts
@@ -23,7 +23,5 @@ export function getFormInitialValues(children: TreeBlock[]): FormikValues {
     initialValues[block.id] = []
   })
 
-  console.log('initialValues', initialValues)
-
   return initialValues
 }
