f892ba27293ba50bade20744e4135d6af6748d86
Mike Allison <dj.mikeallison@gmail.com>
2025-10-09 23:40:32 +0000

    feat: implement multiselect option deletion handling
    - Added logic to update the parent multiselect's max value when an option is deleted, ensuring it reflects the remaining options.
    - Introduced a new GraphQL mutation for updating multiselect blocks.
    - Enhanced tests to cover scenarios for multiselect option deletion and its impact on parent block properties.

--- apps/journeys-admin/src/components/Editor/utils/useBlockDeleteCommand/useBlockDeleteCommand.spec.tsx
+++ apps/journeys-admin/src/components/Editor/utils/useBlockDeleteCommand/useBlockDeleteCommand.spec.tsx
@@ -1,4 +1,5 @@
 import { MockedProvider } from '@apollo/client/testing'
+import { gql } from '@apollo/client'
 import { fireEvent, renderHook, screen, waitFor } from '@testing-library/react'
 
 import { CommandProvider } from '@core/journeys/ui/CommandProvider'
@@ -19,6 +20,13 @@ import {
 import { CommandUndoItem } from '../../Toolbar/Items/CommandUndoItem'
 
 import { useBlockDeleteCommand } from './useBlockDeleteCommand'
+import {
+  BlockFields_CardBlock as CardBlock,
+  BlockFields_StepBlock as StepBlock,
+  BlockFields_MultiselectBlock as MultiselectBlock,
+  BlockFields_MultiselectOptionBlock as MultiselectOptionBlock
+} from '../../../../__generated__/BlockFields'
+import { BLOCK_DELETE } from '../../../../libs/useBlockDeleteMutation/useBlockDeleteMutation'
 
 describe('useBlockDeleteCommand', () => {
   const initiatEditorState = {
@@ -123,5 +131,274 @@ describe('useBlockDeleteCommand', () => {
     await waitFor(() => {
       expect(useBlockRestoreMutationMockResult).toHaveBeenCalled()
     })
+  })
+
+  it('should reduce parent multiselect max when deleting an option', async () => {
+    const multiselect: TreeBlock<MultiselectBlock> = {
+      __typename: 'MultiselectBlock',
+      id: 'multiselect1',
+      parentBlockId: 'card1.id',
+      parentOrder: 0,
+      min: 0,
+      max: 2,
+      children: []
+    }
+    const option1: TreeBlock<MultiselectOptionBlock> = {
+      __typename: 'MultiselectOptionBlock',
+      id: 'option1',
+      parentBlockId: multiselect.id,
+      parentOrder: 0,
+      label: 'A',
+      children: []
+    }
+    const option2: TreeBlock<MultiselectOptionBlock> = {
+      __typename: 'MultiselectOptionBlock',
+      id: 'option2',
+      parentBlockId: multiselect.id,
+      parentOrder: 1,
+      label: 'B',
+      children: []
+    }
+    multiselect.children = [option1, option2]
+
+    const card: TreeBlock<CardBlock> = {
+      __typename: 'CardBlock',
+      id: 'card1.id',
+      parentBlockId: 'stepId',
+      parentOrder: 0,
+      coverBlockId: null,
+      backgroundColor: null,
+      themeMode: null,
+      themeName: null,
+      fullscreen: false,
+      backdropBlur: null,
+      children: [multiselect]
+    }
+    const step: TreeBlock<StepBlock> = {
+      __typename: 'StepBlock',
+      id: 'stepId',
+      parentBlockId: 'journey-id',
+      parentOrder: 0,
+      locked: true,
+      nextBlockId: null,
+      slug: null,
+      children: [card]
+    }
+
+    const multiselectUpdateDoc = gql`
+      mutation MultiselectBlockUpdate(
+        $id: ID!
+        $input: MultiselectBlockUpdateInput!
+      ) {
+        multiselectBlockUpdate(id: $id, input: $input) {
+          __typename
+          id
+          parentBlockId
+          parentOrder
+          min
+          max
+        }
+      }
+    `
+
+    const blockDeleteMockResult = jest.fn(() => ({
+      data: {
+        blockDelete: [
+          {
+            __typename: 'MultiselectOptionBlock',
+            id: option1.id,
+            parentOrder: option1.parentOrder
+          }
+        ]
+      }
+    }))
+    const multiselectUpdateMockResult = jest.fn(() => ({
+      data: {
+        multiselectBlockUpdate: {
+          __typename: 'MultiselectBlock',
+          id: multiselect.id,
+          parentBlockId: multiselect.parentBlockId,
+          parentOrder: multiselect.parentOrder,
+          min: multiselect.min,
+          max: 1
+        }
+      }
+    }))
+
+    const { result } = renderHook(() => useBlockDeleteCommand(), {
+      wrapper: ({ children }) => (
+        <MockedProvider
+          mocks={[
+            {
+              request: {
+                query: BLOCK_DELETE,
+                variables: { id: option1.id }
+              },
+              result: blockDeleteMockResult
+            },
+            {
+              request: {
+                query: multiselectUpdateDoc,
+                variables: {
+                  id: multiselect.id,
+                  input: { max: 1 }
+                }
+              },
+              result: multiselectUpdateMockResult
+            }
+          ]}
+        >
+          <EditorProvider
+            initialState={{
+              steps: [step],
+              selectedStep: step,
+              selectedBlock: card
+            }}
+          >
+            <JourneyProvider
+              value={{ journey: { ...defaultJourney, id: 'journey-id' } }}
+            >
+              <CommandProvider>
+                <CommandUndoItem variant="icon-button" />
+                {children}
+              </CommandProvider>
+            </JourneyProvider>
+          </EditorProvider>
+        </MockedProvider>
+      )
+    })
+
+    result.current.addBlockDelete(option1)
+
+    await waitFor(() => {
+      expect(blockDeleteMockResult).toHaveBeenCalled()
+    })
+    await waitFor(() => {
+      expect(multiselectUpdateMockResult).toHaveBeenCalled()
+    })
+  })
+
+  it('should not change max when parent max is null', async () => {
+    const multiselect: TreeBlock<MultiselectBlock> = {
+      __typename: 'MultiselectBlock',
+      id: 'multiselect2',
+      parentBlockId: 'card1.id',
+      parentOrder: 0,
+      min: 0,
+      max: null,
+      children: []
+    }
+    const option1: TreeBlock<MultiselectOptionBlock> = {
+      __typename: 'MultiselectOptionBlock',
+      id: 'optA',
+      parentBlockId: multiselect.id,
+      parentOrder: 0,
+      label: 'A',
+      children: []
+    }
+    const option2: TreeBlock<MultiselectOptionBlock> = {
+      __typename: 'MultiselectOptionBlock',
+      id: 'optB',
+      parentBlockId: multiselect.id,
+      parentOrder: 1,
+      label: 'B',
+      children: []
+    }
+    multiselect.children = [option1, option2]
+
+    const card: TreeBlock<CardBlock> = {
+      __typename: 'CardBlock',
+      id: 'card1.id',
+      parentBlockId: 'stepId',
+      parentOrder: 0,
+      coverBlockId: null,
+      backgroundColor: null,
+      themeMode: null,
+      themeName: null,
+      fullscreen: false,
+      backdropBlur: null,
+      children: [multiselect]
+    }
+    const step: TreeBlock<StepBlock> = {
+      __typename: 'StepBlock',
+      id: 'stepId',
+      parentBlockId: 'journey-id',
+      parentOrder: 0,
+      locked: true,
+      nextBlockId: null,
+      slug: null,
+      children: [card]
+    }
+
+    const multiselectUpdateDoc = gql`
+      mutation MultiselectBlockUpdate(
+        $id: ID!
+        $input: MultiselectBlockUpdateInput!
+      ) {
+        multiselectBlockUpdate(id: $id, input: $input) {
+          __typename
+          id
+          parentBlockId
+          parentOrder
+          min
+          max
+        }
+      }
+    `
+
+    const blockDeleteMockResult = jest.fn(() => ({
+      data: {
+        blockDelete: [
+          {
+            __typename: 'MultiselectOptionBlock',
+            id: option1.id,
+            parentOrder: option1.parentOrder
+          }
+        ]
+      }
+    }))
+    const multiselectUpdateMockResult = jest.fn(() => ({ data: {} }))
+
+    const { result } = renderHook(() => useBlockDeleteCommand(), {
+      wrapper: ({ children }) => (
+        <MockedProvider
+          mocks={[
+            {
+              request: {
+                query: BLOCK_DELETE,
+                variables: { id: option1.id }
+              },
+              result: blockDeleteMockResult
+            }
+            // Intentionally do NOT include a multiselect update mock, since update should not fire when max is null
+          ]}
+        >
+          <EditorProvider
+            initialState={{
+              steps: [step],
+              selectedStep: step,
+              selectedBlock: card
+            }}
+          >
+            <JourneyProvider
+              value={{ journey: { ...defaultJourney, id: 'journey-id' } }}
+            >
+              <CommandProvider>
+                <CommandUndoItem variant="icon-button" />
+                {children}
+              </CommandProvider>
+            </JourneyProvider>
+          </EditorProvider>
+        </MockedProvider>
+      )
+    })
+
+    result.current.addBlockDelete(option1)
+
+    await waitFor(() => {
+      expect(blockDeleteMockResult).toHaveBeenCalled()
+    })
+    // Ensure no unexpected multiselect update was executed
+    // Apollo would throw an Unhandled error if an unmocked request was made during the test
   })
 })
