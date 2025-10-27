43087df203a5c0407bae5612151b2e340c270383
jaco-brink <jacobus.brink@tandem.org.nz>
2025-10-27 20:44:03 +0000

    fix: test if this is needed still

--- apps/journeys/setupTests.ts
+++ apps/journeys/setupTests.ts
@@ -7,23 +7,3 @@ jest.mock('next/router', () => require('next-router-mock'))
 
 if (process.env.CI === 'true')
   jest.retryTimes(3, { logErrorsBeforeRetry: true })
-
-// Mock HTMLCanvasElement.getContext for tests
-HTMLCanvasElement.prototype.getContext = jest.fn((contextId: string) => {
-  if (contextId === '2d') {
-    return {
-      createImageData: jest.fn(() => ({
-        data: new Uint8ClampedArray(32 * 32 * 4)
-      })),
-      putImageData: jest.fn(),
-      fillRect: jest.fn(),
-      fillStyle: ''
-    } as unknown as CanvasRenderingContext2D
-  }
-  return null
-}) as any
-
-// Mock HTMLCanvasElement.toDataURL
-HTMLCanvasElement.prototype.toDataURL = jest.fn(
-  () => 'data:image/webp;base64,mock'
-)
