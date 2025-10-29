046faa234a2aa2feea02ac0ffbb5a1682ca4ffb1
Mike Allison <dj.mikeallison@gmail.com>
2025-10-29 18:49:21 +0000

    fix: rename EKS node group resource for clarity

--- infrastructure/modules/aws/eks/main.tf
+++ infrastructure/modules/aws/eks/main.tf
@@ -212,7 +212,7 @@ data "aws_ami" "eks-worker" {
 #   }
 # }
 
-resource "aws_eks_node_group" "az_2a" {
+resource "aws_eks_node_group" "az_2a_ondemand" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "jfp-eks-node-group-2a-${var.env}"
   node_role_arn   = aws_iam_role.eks-node.arn
