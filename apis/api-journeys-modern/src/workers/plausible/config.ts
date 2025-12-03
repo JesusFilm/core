40c10ad6c592de4742f5cfc9927d8a665ced6409
Mike Allison <dj.mikeallison@gmail.com>
2025-12-03 23:03:47 +0000

    refactor: update job scheduling logic to support one-off jobs and remove daily repeat configuration

--- apis/api-journeys-modern/src/workers/plausible/config.ts
+++ apis/api-journeys-modern/src/workers/plausible/config.ts
@@ -1,10 +1,7 @@
 export const queueName = 'api-journeys-plausible'
 export const jobName = `${queueName}-job`
 
-// Run daily at 05:00 UTC to backfill missing Plausible sites
-const DAILY_AT_FIVE_AM = '0 0 5 * * *'
-export const repeat = DAILY_AT_FIVE_AM
-
+// Run a one-off backfill job on startup to mirror legacy behaviour
 export const jobData = {
   __typename: 'plausibleCreateSites'
 } as const
