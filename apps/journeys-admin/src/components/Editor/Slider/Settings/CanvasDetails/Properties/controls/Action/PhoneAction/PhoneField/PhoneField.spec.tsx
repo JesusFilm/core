72f153e0af423e5920522efb5b748da9235b3c34
jianwei1 <jianweichong08@gmail.com>
2025-12-03 21:50:37 +0000

    fix: type check

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/PhoneAction/PhoneField/PhoneField.spec.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/controls/Action/PhoneAction/PhoneField/PhoneField.spec.tsx
@@ -18,8 +18,10 @@ describe('PhoneField', () => {
       />
     )
 
-    const callingCodeInput = screen.getByLabelText('Country')
-    const phoneInput = screen.getByLabelText('Phone Number')
+    const callingCodeInput = screen.getByLabelText(
+      'Country'
+    ) as HTMLInputElement
+    const phoneInput = screen.getByLabelText('Phone Number') as HTMLInputElement
 
     expect(callingCodeInput.value).toBe('+7')
     expect(phoneInput.value).toBe('3333')
