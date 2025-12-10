9a800b2074eab55a6ac2de2b29dd09e700d1992c
jaco-brink <jacobus.brink@tandem.org.nz>
2025-12-11 10:15:53 +1300

    feat: add floating add button when active journeys showing in mobile view only (#8492)

--- apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyList.tsx
@@ -116,7 +116,9 @@ export function JourneyList({
           sortOrder={sortOrder}
         />
       </Box>
-      {activeTab === 'active' && <AddJourneyFab />}
+      {activeTab === 'active' && currentContentType === 'journeys' && (
+        <AddJourneyFab />
+      )}
     </>
   )
 }
