294a0c75b4c995a3af4f4e224d072b686f21efd3
Mike Allison <dj.mikeallison@gmail.com>
2025-12-15 18:20:48 +0000

    chore: remove helm installation commands from deploy scripts and update namespace in Doppler README

--- infrastructure/kube/alb/deploy.sh
+++ infrastructure/kube/alb/deploy.sh
@@ -1,5 +1,3 @@
-helm repo add eks https://aws.github.io/eks-charts
-helm repo update eks
 aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
     --policy-document file://iam_policy.json
@@ -11,11 +9,3 @@ eksctl create iamserviceaccount \
   --role-name AmazonEKSLoadBalancerControllerRole \
   --attach-policy-arn=arn:aws:iam::410965620680:policy/AWSLoadBalancerControllerIAMPolicy \
   --approve    
-helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
-  -n kube-system \
-  --set clusterName=jfp-eks-prod \
-  --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller \
-  --set region=us-east-2 \
-  --set vpcId=vpc-0b722b0a1f7789afd \
-  --kube-context=prod
