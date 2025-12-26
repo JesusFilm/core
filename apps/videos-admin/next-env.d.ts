1dad06037a17eadca0b547d42027f03fce951f0f
Mike Allison <dj.mikeallison@gmail.com>
2025-12-26 23:30:26 +0000

    chore: remove unused next-env.d.ts files from multiple apps

--- apps/videos-admin/next-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-/* eslint-disable @nx/enforce-module-boundaries, import/no-useless-path-segments */
-import '../../dist/apps/videos-admin/.next/types/routes.d.ts'
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
