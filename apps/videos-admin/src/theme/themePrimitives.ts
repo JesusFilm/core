b96ef04231b6797d27ea12ef59ad6cdbf385f3d8
Mike Allison <dj.mikeallison@gmail.com>
2025-12-26 23:50:39 +0000

    chore: remove commented-out type definitions in themePrimitives.ts
    - Deleted unused type definitions for ColorRange and PaletteColor to clean up the codebase.

--- apps/videos-admin/src/theme/themePrimitives.ts
+++ apps/videos-admin/src/theme/themePrimitives.ts
@@ -6,22 +6,6 @@ declare module '@mui/material/Paper' {
     highlighted: true
   }
 }
-// declare module '@mui/material/styles/createPalette' {
-//   interface ColorRange {
-//     50: string
-//     100: string
-//     200: string
-//     300: string
-//     400: string
-//     500: string
-//     600: string
-//     700: string
-//     800: string
-//     900: string
-//   }
-
-//   interface PaletteColor extends ColorRange {}
-// }
 
 const defaultTheme = createTheme()
 
