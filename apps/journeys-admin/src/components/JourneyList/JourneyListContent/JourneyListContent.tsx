62da83fb39a3a4be09de5d9175f014957ce98fc8
Ur-imazing <urim027@gmail.com>
2026-01-13 03:51:23 +0000

    chore: remove journey provider context wrapper from journey cards

--- apps/journeys-admin/src/components/JourneyList/JourneyListContent/JourneyListContent.tsx
+++ apps/journeys-admin/src/components/JourneyList/JourneyListContent/JourneyListContent.tsx
@@ -11,17 +11,14 @@ import { useTranslation } from 'next-i18next'
 import { useSnackbar } from 'notistack'
 import { ReactElement, useEffect, useMemo, useState } from 'react'
 
-import { JourneyProvider } from '@core/journeys/ui/JourneyProvider'
-
 import { JourneyStatus } from '../../../../__generated__/globalTypes'
-import { JourneyFields } from '../../../../__generated__/JourneyFields'
 import { useAdminJourneysQuery } from '../../../libs/useAdminJourneysQuery'
 import { ActivePriorityList } from '../ActiveJourneyList/ActivePriorityList'
 import { AddJourneyButton } from '../ActiveJourneyList/AddJourneyButton'
 import { JourneyCard } from '../JourneyCard'
-import type { JourneyListEvent, JourneyListProps } from '../JourneyList'
+import type { JourneyListEvent } from '../JourneyList'
 import type { ContentType, JourneyStatusFilter } from '../JourneyListView'
-import { JourneySort, SortOrder } from '../JourneySort'
+import { SortOrder } from '../JourneySort'
 import { sortJourneys } from '../JourneySort/utils/sortJourneys'
 import { LoadingJourneyList } from '../LoadingJourneyList'
 
@@ -586,18 +583,11 @@ export function JourneyListContent({
                       xl: contentType === 'templates' ? 2.4 : 3
                     }}
                   >
-                    <JourneyProvider
-                      value={{
-                        journey: journey as unknown as JourneyFields,
-                        variant: 'admin'
-                      }}
-                    >
-                      <JourneyCard
-                        key={journey.id}
-                        journey={journey}
-                        refetch={refetch}
-                      />
-                    </JourneyProvider>
+                    <JourneyCard
+                      key={journey.id}
+                      journey={journey}
+                      refetch={refetch}
+                    />
                   </Grid>
                 ))}
               </Grid>
