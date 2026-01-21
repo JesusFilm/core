ba7a3b6e59a594ca9ac30ed37332a5b279f07220
jaycnz <ijaygt@gmail.com>
2026-01-21 01:14:50 +0000

    refactor: separate minimum duration logic

--- apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoLibrary/VideoFromMux/AddByFile/utils/getVideoDuration/getVideoDuration.ts
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/Drawer/VideoLibrary/VideoFromMux/AddByFile/utils/getVideoDuration/getVideoDuration.ts
@@ -3,13 +3,11 @@ export const getVideoDuration = (file: File): Promise<number> => {
       const video = document.createElement('video')
       video.preload = 'metadata'
       const blobUrl = URL.createObjectURL(file)
-      console.log(blobUrl)
 
       video.src = blobUrl
 
       video.onloadedmetadata = () => {
         URL.revokeObjectURL(blobUrl)
-        console.log(video.duration)
         resolve(video.duration)
       }
 
