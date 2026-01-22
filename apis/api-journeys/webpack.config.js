79f970dcc17e55f6efd6be42d9a202a8a34fb625
Mike Allison <dj.mikeallison@gmail.com>
2026-01-22 22:59:58 +0000

    chore: update dependencies and remove unused webpack configuration
    - Updated various dependencies in `package.json` for `@nestjs` packages and others to their latest versions.
    - Removed the unused webpack IgnorePlugin configuration from `webpack.config.js` as it was no longer necessary.

--- apis/api-journeys/webpack.config.js
+++ apis/api-journeys/webpack.config.js
@@ -1,24 +1,8 @@
 const { composePlugins, withNx } = require('@nx/webpack')
-const webpack = require('webpack')
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
-  // Ignore optional NestJS peer dependencies that aren't used
-  config.plugins = [
-    ...(config.plugins || []),
-    new webpack.IgnorePlugin({
-      checkResource(resource) {
-        const optionalDeps = [
-          '@nestjs/websockets',
-          '@nestjs/microservices',
-          '@as-integrations/fastify',
-          '@apollo/gateway',
-          'class-validator',
-          'class-transformer'
-        ]
-        return optionalDeps.some((dep) => resource.startsWith(dep))
-      }
-    })
-  ]
+  // Note: This was added by an Nx migration. Webpack builds are required to have a corresponding Webpack config file.
+  // See: https://nx.dev/recipes/webpack/webpack-config-setup
   return config
 })
