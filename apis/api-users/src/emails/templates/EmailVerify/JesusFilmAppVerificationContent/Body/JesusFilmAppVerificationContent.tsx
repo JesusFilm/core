ccc263fe92642a3d78694b1cabd2f635ba13a47e
Kneesal <nisalcottingham@gmail.com>
2026-02-02 22:55:57 +0000

    fix: support apple autocomp

--- apis/api-users/src/emails/templates/EmailVerify/JesusFilmAppVerificationContent/Body/JesusFilmAppVerificationContent.tsx
+++ apis/api-users/src/emails/templates/EmailVerify/JesusFilmAppVerificationContent/Body/JesusFilmAppVerificationContent.tsx
@@ -15,15 +15,15 @@ export const JesusFilmAppVerificationContent = ({
         <Row>
           <th>
             <Text className="mt-0 mb-[24px] text-center text-[16px] leading-[28px] font-semibold">
-              Verify your email address to complete your account setup!
+              Verify your email address to complete your account setup.
             </Text>
           </th>
         </Row>
         <Row>
-          <Text className="mt-[24px] mb-[0px] text-center text-[14px] leading-[24px] font-[400]">
-            Enter the following code in the app:
+          <Text className="mt-[24px] mb-[8px] text-center text-[14px] leading-[24px] font-[400]">
+            Your verification code is
           </Text>
-          <Text className="my-0 text-center text-[14px] leading-[24px] font-[400]">
+          <Text className="my-0 text-center text-[14px] leading-[24px] font-[400] text-[#C52D3A]">
             {token}
           </Text>
         </Row>
