e7de15a3353fedb39897f14af9a77a81513a8f5a
Mike Allison <dj.mikeallison@gmail.com>
2026-02-04 20:42:04 +0000

    feat: enhance AuthenticatedUser type with language roles
    - Added languageUserRoles field to the AuthenticatedUser type in both schema.graphql and schema.graphql for API_LANGUAGES.
    - Implemented resolver for languageUserRoles in the user schema to fetch user-specific language roles from the database.
    - This update improves the user model by integrating language role management into the authentication process.

--- apis/api-languages/src/schema/user/user.ts
+++ apis/api-languages/src/schema/user/user.ts
@@ -24,3 +24,27 @@ builder.externalRef('User', builder.selection<{ id: string }>('id')).implement({
     })
   })
 })
+
+builder
+  .externalRef('AuthenticatedUser', builder.selection<{ id: string }>('id'))
+  .implement({
+    externalFields: (t) => ({
+      id: t.id({ nullable: false })
+    }),
+    fields: (t) => ({
+      languageUserRoles: t.field({
+        type: [LanguageRole],
+        nullable: false,
+        resolve: async (data) => {
+          return (
+            (
+              await prisma.userLanguageRole.findUnique({
+                where: { userId: data.id }
+              })
+            )?.roles ?? []
+          )
+        }
+      })
+    })
+  })
+  
