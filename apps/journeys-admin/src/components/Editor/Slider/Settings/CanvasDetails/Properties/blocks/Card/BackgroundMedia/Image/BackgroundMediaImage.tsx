d59edf8633a6910a0158aa7e1e8759c5a1ac0654
jianwei1 <jianweichong08@gmail.com>
2026-02-05 11:49:26 +1300

    feat: add customize toggle for background media (#8674)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Card/BackgroundMedia/Image/BackgroundMediaImage.tsx
+++ apps/journeys-admin/src/components/Editor/Slider/Settings/CanvasDetails/Properties/blocks/Card/BackgroundMedia/Image/BackgroundMediaImage.tsx
@@ -33,6 +33,7 @@ import { blockRestoreUpdate } from '../../../../../../../../../../libs/useBlockR
 import { useCoverBlockDeleteMutation } from '../../../../../../../../../../libs/useCoverBlockDeleteMutation'
 import { useCoverBlockRestoreMutation } from '../../../../../../../../../../libs/useCoverBlockRestoreMutation'
 import { ImageSource } from '../../../../../../Drawer/ImageSource'
+import { BlockCustomizationToggle } from '../../../../controls/BlockCustomizationToggle'
 
 import { FocalPoint } from './FocalPoint'
 import { ZoomImage } from './ZoomImage/ZoomImage'
@@ -345,23 +346,27 @@ export function BackgroundMediaImage({
     }
   }
 
+  const imageCoverBlock =
+    coverBlock?.__typename === 'ImageBlock' ? coverBlock : null
+
   return (
     <Stack gap={4}>
       <ImageSource
-        selectedBlock={
-          coverBlock?.__typename === 'ImageBlock' ? coverBlock : null
-        }
+        selectedBlock={imageCoverBlock}
         onChange={handleChange}
         onDelete={async () => deleteImageBlock()}
       />
       <FocalPoint
-        imageBlock={coverBlock?.__typename === 'ImageBlock' ? coverBlock : null}
+        imageBlock={imageCoverBlock}
         updateImageBlock={updateImageBlock}
       />
       <ZoomImage
-        imageBlock={coverBlock?.__typename === 'ImageBlock' ? coverBlock : null}
+        imageBlock={imageCoverBlock}
         updateImageBlock={updateImageBlock}
       />
+      {journey?.template && imageCoverBlock != null && (
+        <BlockCustomizationToggle block={imageCoverBlock} />
+      )}
     </Stack>
   )
 }
