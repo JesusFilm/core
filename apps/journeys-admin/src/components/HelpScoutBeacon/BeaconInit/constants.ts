6a85d8b2d93f157fc0f2bce34adf79029c7480ef
Edmond Shen <72168666+edmonday@users.noreply.github.com>
2026-02-11 11:57:08 +1300

    feat: toolbar improvements (#8627)
    Co-authored-by: VincentHan12 <hanvincent@outlook.com>
    Co-authored-by: David Li <davidshgli3115@gmail.com>
    Co-authored-by: jaycnz <ijaygt@gmail.com>
    Co-authored-by: David Li <77130600+GunnerTeh@users.noreply.github.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jay <145623517+jaycnz@users.noreply.github.com>

--- apps/journeys-admin/src/components/HelpScoutBeacon/BeaconInit/constants.ts
+++ apps/journeys-admin/src/components/HelpScoutBeacon/BeaconInit/constants.ts
@@ -5,16 +5,18 @@ export const DESKTOP_APP_BAR_GAP = `${desktopAppBarGap}px`
 export const DESKTOP_CONTAINER_PADDING = `${desktopContainerPadding}px`
 export const DESKTOP_CONTAINER_WIDTH = '326px'
 export const DESKTOP_CONTAINER_HEIGHT = `calc(100vh - ${desktopAppBarGap}px)`
-export const DESKTOP_CONTAINER_MAX_HEIGHT = `calc(100vh - ${
-  desktopAppBarGap + desktopContainerPadding
-}px)`
+export const DESKTOP_CONTAINER_MAX_HEIGHT = `calc(100vh - ${desktopAppBarGap + desktopContainerPadding}px)`
 
 export const MOBILE_APP_BAR_GAP = '60px'
-export const MOBILE_CONTAINER_WIDTH = '100%'
-export const MOBILE_CONTAINER_HEIGHT = `calc(100svh - ${MOBILE_APP_BAR_GAP})`
+export const MOBILE_CONTAINER_PADDING = '20px'
+export const MOBILE_CONTAINER_BOTTOM = '80px'
+export const MOBILE_CONTAINER_HEIGHT = `calc(100svh - ${MOBILE_APP_BAR_GAP} - ${MOBILE_CONTAINER_BOTTOM})`
 export const MOBILE_CONTAINER_MAX_HEIGHT = '100svh'
 
 // shows beacon icon so that close button displays more consistent on mobile
 export const BEACON_STYLE = `icon`
 // hides beacon icon from visibility
 export const BEACON_ICON_DISPLAY = 'none !important'
+
+// hides helpscout beacon's default close button
+export const BEACON_CLOSE_BUTTON_DISPLAY = 'none !important'
