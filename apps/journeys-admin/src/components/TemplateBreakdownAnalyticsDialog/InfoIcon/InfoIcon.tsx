6b78892cef65c8f02165c246b30892b30b6e60e6
David Li <davidshgli3115@gmail.com>
2026-02-11 22:17:36 +0000

    fix: fised padding space for mobile

--- apps/journeys-admin/src/components/TemplateBreakdownAnalyticsDialog/InfoIcon/InfoIcon.tsx
+++ apps/journeys-admin/src/components/TemplateBreakdownAnalyticsDialog/InfoIcon/InfoIcon.tsx
@@ -10,7 +10,7 @@ export function InfoIcon(): ReactElement {
   const { t } = useTranslation('apps-journeys-admin')
 
   return (
-    <Box sx={{ mx: 2 }}>
+    <Box sx={{ mx: 2, mr: 10 }}>
       <Item
         variant="icon-button"
         icon={<InformationCircleContainedIcon />}
