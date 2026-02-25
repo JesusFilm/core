563aadbece95fe92676acce6a2765f3b0522c110
edmonday <egsmond@gmail.com>
2026-02-25 00:27:52 +0000

    chore: codegen

---
+++ apps/journeys-admin/__generated__/TemplateVideoUploadCreateMuxVideoUploadByFileMutation.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: TemplateVideoUploadCreateMuxVideoUploadByFileMutation
+// ====================================================
+
+export interface TemplateVideoUploadCreateMuxVideoUploadByFileMutation_createMuxVideoUploadByFile {
+  __typename: "MuxVideo";
+  uploadUrl: string | null;
+  id: string;
+}
+
+export interface TemplateVideoUploadCreateMuxVideoUploadByFileMutation {
+  createMuxVideoUploadByFile: TemplateVideoUploadCreateMuxVideoUploadByFileMutation_createMuxVideoUploadByFile;
+}
+
+export interface TemplateVideoUploadCreateMuxVideoUploadByFileMutationVariables {
+  name: string;
+}
