ea8fd024960a101bd31c7ebd896a4929fa318a28
Ur-imazing <urim027@gmail.com>
2026-03-19 01:01:18 +0000

    feat: add sync to google sheet button for mobile

---
+++ apps/journeys-admin/src/components/JourneyVisitorsList/GoogleSheetsSyncButton/GoogleSheetsSyncButton.tsx
@@ -0,0 +1,49 @@
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import { useTranslation } from 'next-i18next'
+import { ReactElement } from 'react'
+
+import ArrowRefresh6 from '@core/shared/ui/icons/ArrowRefresh6'
+
+import { Tooltip } from '../../Tooltip/Tooltip'
+
+interface GoogleSheetsSyncButtonProps {
+  disabled: boolean
+  onSyncClick: () => void
+}
+
+export function GoogleSheetsSyncButton({
+  disabled,
+  onSyncClick
+}: GoogleSheetsSyncButtonProps): ReactElement {
+  const { t } = useTranslation('apps-journeys-admin')
+
+  const disabledTooltip = t(
+    'Only team members and journey owners can export data.'
+  )
+
+  return (
+    <Box sx={{ display: { sm: 'block', md: 'none' } }}>
+      {disabled ? (
+        <Tooltip title={disabledTooltip} placement="bottom">
+          <span>
+            <IconButton
+              aria-label={`${t('Sync to Google Sheets')} - ${disabledTooltip}`}
+              onClick={onSyncClick}
+              disabled
+            >
+              <ArrowRefresh6 />
+            </IconButton>
+          </span>
+        </Tooltip>
+      ) : (
+        <IconButton
+          aria-label={t('Sync to Google Sheets')}
+          onClick={onSyncClick}
+        >
+          <ArrowRefresh6 />
+        </IconButton>
+      )}
+    </Box>
+  )
+}
