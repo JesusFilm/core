ce44648438986d86a339a9a869b547963a40d148
Mike Allison <dj.mikeallison@gmail.com>
2026-04-08 00:42:24 +0000

    feat: update GraphQL schema to include iconBlockUpdate mutation and modify existing iconBlockCreate mutation for modern API

---
+++ apis/api-journeys-modern/src/schema/block/icon/iconBlockUpdate.mutation.ts
@@ -0,0 +1,27 @@
+import { builder } from '../../builder'
+import { authorizeBlockUpdate, update } from '../service'
+
+import { IconBlock } from './icon'
+import { IconBlockUpdateInput } from './inputs'
+
+builder.mutationField('iconBlockUpdate', (t) =>
+  t.withAuth({ $any: { isAuthenticated: true, isAnonymous: true } }).field({
+    type: IconBlock,
+    nullable: true,
+    override: { from: 'api-journeys' },
+    args: {
+      id: t.arg({ type: 'ID', required: true }),
+      input: t.arg({ type: IconBlockUpdateInput, required: true }),
+      journeyId: t.arg({
+        type: 'ID',
+        required: false,
+        description: 'drop this parameter after merging teams'
+      })
+    },
+    resolve: async (_parent, args, context) => {
+      const { id, input } = args
+      await authorizeBlockUpdate(id, context.user)
+      return await update(id, { ...input })
+    }
+  })
+)
