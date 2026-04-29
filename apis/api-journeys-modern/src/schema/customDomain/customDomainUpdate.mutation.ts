95fcaf9f984be304499af097c91ad12a837c4144
Mike Allison <dj.mikeallison@gmail.com>
2026-04-29 19:29:37 +0000

    refactor(customDomainUpdate): simplify authentication and error handling logic

--- apis/api-journeys-modern/src/schema/customDomain/customDomainUpdate.mutation.ts
+++ apis/api-journeys-modern/src/schema/customDomain/customDomainUpdate.mutation.ts
@@ -10,48 +10,45 @@ import { canAccessCustomDomain } from './customDomain.acl'
 import { CustomDomainUpdateInput } from './inputs'
 
 builder.mutationField('customDomainUpdate', (t) =>
-  t
-    .withAuth({ $any: { isAuthenticated: true, isAnonymous: true } })
-    .prismaField({
-      type: CustomDomainRef,
-      nullable: false,
-      override: { from: 'api-journeys' },
-      args: {
-        id: t.arg({ type: 'ID', required: true }),
-        input: t.arg({ type: CustomDomainUpdateInput, required: true })
-      },
-      resolve: async (query, _parent, args, context) => {
-        const { id, input } = args
+  t.withAuth({ isAuthenticated: true }).prismaField({
+    type: CustomDomainRef,
+    nullable: false,
+    override: { from: 'api-journeys' },
+    args: {
+      id: t.arg({ type: 'ID', required: true }),
+      input: t.arg({ type: CustomDomainUpdateInput, required: true })
+    },
+    resolve: async (query, _parent, args, context) => {
+      const { id, input } = args
 
-        const customDomain = await prisma.customDomain.findUnique({
-          where: { id },
-          include: { team: { include: { userTeams: true } } }
-        })
+      const customDomain = await prisma.customDomain.findUnique({
+        where: { id },
+        include: { team: { include: { userTeams: true } } }
+      })
 
-        if (customDomain == null) {
-          throw new GraphQLError('custom domain not found', {
-            extensions: { code: 'NOT_FOUND' }
-          })
-        }
-
-        if (!canAccessCustomDomain(Action.Update, customDomain, context.user)) {
-          throw new GraphQLError(
-            'user is not allowed to update custom domain',
-            { extensions: { code: 'FORBIDDEN' } }
-          )
-        }
-
-        return await prisma.customDomain.update({
-          ...query,
-          where: { id },
-          data: {
-            routeAllTeamJourneys: input.routeAllTeamJourneys ?? undefined,
-            journeyCollection:
-              input.journeyCollectionId != null
-                ? { connect: { id: input.journeyCollectionId } }
-                : undefined
-          }
+      if (customDomain == null) {
+        throw new GraphQLError('custom domain not found', {
+          extensions: { code: 'NOT_FOUND' }
         })
       }
-    })
+
+      if (!canAccessCustomDomain(Action.Update, customDomain, context.user)) {
+        throw new GraphQLError('user is not allowed to update custom domain', {
+          extensions: { code: 'FORBIDDEN' }
+        })
+      }
+
+      return await prisma.customDomain.update({
+        ...query,
+        where: { id },
+        data: {
+          routeAllTeamJourneys: input.routeAllTeamJourneys ?? undefined,
+          journeyCollection:
+            input.journeyCollectionId != null
+              ? { connect: { id: input.journeyCollectionId } }
+              : undefined
+        }
+      })
+    }
+  })
 )
