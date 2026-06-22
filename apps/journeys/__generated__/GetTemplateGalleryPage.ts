aedae01097051cc7f7cdebbdfa8aa3dd5e08d411
Tanner Fleming <tanner.fleming@gmail.com>
2026-06-22 20:55:45 +0000

    Fix template gallery media typename

--- apps/journeys/__generated__/GetTemplateGalleryPage.ts
+++ apps/journeys/__generated__/GetTemplateGalleryPage.ts
@@ -10,7 +10,7 @@ import { TemplateGalleryPageMediaType } from "./globalTypes";
 // ====================================================
 
 export interface GetTemplateGalleryPage_templateGalleryPageBySlug_media {
-  __typename: "TemplateGalleryPageMedia";
+  __typename: "TemplateGalleryPageMediaPublic";
   id: string;
   /**
    * Discriminator for which underlying field is populated.
